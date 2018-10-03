df9e714770920eb87ae1c9c34115ecd6242bd62e
swalker125 <sam.walker@digit-hull.com>
2018-10-03 10:40:13 +0100

    FPL-10 added enterFactorsAffectingParenting page object. created e2e tests for optional text areas, fully filling in the application section and leaving the section blank. updated spreadsheet with necessary fields and permissions.

---
+++ e2e/paths/enterFactorsAffectingParenting_test.js
@@ -0,0 +1,26 @@
+const config = require('../config.js');
+
+Feature('EnterFactorsAffectingParenting');
+
+Before((I, caseViewPage) => {
+	I.logInAndCreateCase(config.localAuthorityEmail, config.localAuthorityPassword, config.eventSummary, config.eventDescription);
+	I.waitForElement('.tabs', 10);
+	caseViewPage.goToNewActions(config.applicationActions.enterParentingFactors);
+	I.waitForElement('ccd-case-edit-page', 5);
+});
+
+Scenario('Leaving factors affecting parenting section blank in c110a', (I) => {
+	I.continueAndSubmit(config.eventSummary, config.eventDescription);
+	I.see(`updated with event: ${config.applicationActions.enterParentingFactors}`);
+});
+
+Scenario('There is an optional textArea when I answer yes to a question in factors affecting parenting section', (I, enterFactorsAffectingParentingPage) => {
+	enterFactorsAffectingParentingPage.enterYesForAlcoholOrDrugAbuse();
+	I.seeElement('textArea');
+});
+
+Scenario('Filling in factors affecting parenting sections of c110a', (I, enterFactorsAffectingParentingPage) => {
+	enterFactorsAffectingParentingPage.enterFactorsAffectingParenting();
+	I.continueAndSubmit(config.eventSummary, config.eventDescription);
+	I.see(`updated with event: ${config.applicationActions.enterParentingFactors}`);
+});
