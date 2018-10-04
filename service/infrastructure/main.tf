16df60d3bd6eaef5c93b3eef078e37b20d82e665
Kurt Plaschke <k.plaschke@hull.ac.uk>
2018-10-04 14:11:47 +0100

    initial commit for service

---
+++ service/infrastructure/main.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {}
+
+locals {
+  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+}
+
+module "spring-boot-template" {
+  source              = "git@github.com:hmcts/moj-module-webapp?ref=master"
+  product             = "${var.product}-${var.component}"
+  location            = "${var.location_app}"
+  env                 = "${var.env}"
+  ilbIp               = "${var.ilbIp}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  subscription        = "${var.subscription}"
+  capacity            = "${var.capacity}"
+  common_tags         = "${var.common_tags}"
+
+  app_settings = {
+    LOGBACK_REQUIRE_ALERT_LEVEL = false
+    LOGBACK_REQUIRE_ERROR_CODE  = false
+  }
+}
