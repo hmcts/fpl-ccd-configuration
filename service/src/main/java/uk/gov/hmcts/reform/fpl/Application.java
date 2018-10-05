2395abf2af6a81d39e59c1b32554baf10a3b43a7
Kurt <kurtplaschke@hotmail.co.uk>
2018-10-05 12:46:44 +0100

    FPL-47: Initiate Java service subproject (#20)

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.fpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
