f72e9b662b6cd0c047e8c2359bca085da77aee70
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-11 00:15:59 +0100

    Move CodeceptJS config to JS file and tweak it

---
+++ codecept.conf.js
@@ -0,0 +1,36 @@
+exports.config = {
+  output: "./output",
+  helpers: {
+    Puppeteer: {
+      show: process.env.SHOW_BROWSER_WINDOW || false,
+      restart: false,
+      chrome: {
+        ignoreHTTPSErrors: true
+      }
+    }
+  },
+  include: {
+    config: "./e2e/config.js",
+    I: "./e2e/steps_file.js",
+    loginPage: "./e2e/pages/login/loginPage.js",
+    createCasePage: "./e2e/pages/createCase/createCase.js",
+    addEventSummaryPage: "./e2e/pages/createCase/addEventSummary.js",
+    caseViewPage: "./e2e/pages/caseView/caseView.js",
+    selectHearingPage: "./e2e/pages/selectHearing/selectHearing.js",
+    enterGroundsPage: "./e2e/pages/enterGrounds/enterGrounds.js",
+    enterFactorsAffectingParentingPage: "./e2e/pages/enterFactorsAffectingParenting/enterFactorsAffectingParenting.js",
+    enterInternationalElementsPage: "./e2e/pages/internationalElements/enterInternationalElements.js",
+    enterRiskAndHarmToChildPage: "./e2e/pages/enterRiskAndHarmToChild/enterRiskAndHarmToChild.js",
+    uploadDocumentsPage: "./e2e/pages/uploadDocuments/uploadDocuments.js",
+    enterApplicantPage: "./e2e/pages/enterApplicant/enterApplicant.js",
+    enterChildrenPage: "./e2e/pages/enterChildren/enterChildren.js",
+    enterOtherProceedingsPage: "./e2e/pages/enterOtherProceedings/enterOtherProceedings.js"
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    }
+  },
+  tests: "./e2e/paths/*_test.js",
+  timeout: 10000,
+};
