4c36d146843b053445b8134d2bfcac52c8de6701
Damian Dunajski <damdun@gmail.com>
2018-10-11 11:16:12 +0100

    Use 2 spaces for indentations by default (#36)

--- e2e/fixtures/solicitor.js
+++ e2e/fixtures/solicitor.js
@@ -1,8 +1,8 @@
 module.exports = {
-	name: 'John Smith',
-	mobileNumber: '07000000000',
-	telephoneNumber: '00000000000',
-	email: 'solicitor@email.com',
-	dx: '160010 Kingsway 7 ',
-	reference: 'reference',
+  name: 'John Smith',
+  mobileNumber: '07000000000',
+  telephoneNumber: '00000000000',
+  email: 'solicitor@email.com',
+  dx: '160010 Kingsway 7 ',
+  reference: 'reference',
 };
--- e2e/pages/login/loginPage.js
+++ e2e/pages/login/loginPage.js
@@ -2,16 +2,16 @@ const I = actor();
 
 module.exports = {
 
-	fields: {
-		username: '#username',
-		password: '#password',
-	},
-	submitButton: 'Sign in',
+  fields: {
+    username: '#username',
+    password: '#password',
+  },
+  submitButton: 'Sign in',
 
-	signIn(username, password) {
-		I.amOnPage('http://localhost:3451');
-		I.fillField(this.fields.username, username);
-		I.fillField(this.fields.password, password);
-		I.click(this.submitButton);
-	},
+  signIn(username, password) {
+    I.amOnPage('http://localhost:3451');
+    I.fillField(this.fields.username, username);
+    I.fillField(this.fields.password, password);
+    I.click(this.submitButton);
+  },
 };
