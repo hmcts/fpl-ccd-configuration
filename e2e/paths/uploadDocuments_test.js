d3f4d512ff35a954b664661abaf90b3cab18d268
Kurt Plaschke <k.plaschke@hull.ac.uk>
2018-10-16 14:48:45 +0100

    FPL-68 added information displayed on the evidence tab based on what is selected in the annex documents part of the form. if no information is entered, the title is displayed but the box is empty

--- e2e/paths/uploadDocuments_test.js
+++ e2e/paths/uploadDocuments_test.js
@@ -7,19 +7,25 @@ Before((I, caseViewPage) => {
   caseViewPage.goToNewActions(config.applicationActions.uploadDocuments);
 });
 
-Scenario('Selecting to follow for a document produces a textarea', (I, uploadDocumentsPage) => {
-  I.selectOption(uploadDocumentsPage.fields.socialWorkChronologyStatus, 'To follow');
-  I.fillField(uploadDocumentsPage.fields.socialWorkChronologyReason, 'mock reason');
+Scenario('Selecting social work chronology document to follow in the c110a application', (I, uploadDocumentsPage, caseViewPage) => {
+  uploadDocumentsPage.selectSocialWorkChronologyToFollow();
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.uploadDocuments);
+  caseViewPage.selectTab(caseViewPage.tabs.evidence);
+  I.seeDocument('Social work chronology', '', 'To follow', 'mock reason');
 });
 
-Scenario('All documents are able to be uploaded', (I, uploadDocumentsPage) => {
-  uploadDocumentsPage.uploadSocialWorkChronology(config.testFile);
+Scenario('Uploading all files in the c110a application', (I, uploadDocumentsPage, caseViewPage) => {
+  uploadDocumentsPage.selectSocialWorkChronologyToFollow(config.testFile);
   uploadDocumentsPage.uploadSocialWorkStatement(config.testFile);
   uploadDocumentsPage.uploadSocialWorkAssessment(config.testFile);
   uploadDocumentsPage.uploadCarePlan(config.testFile);
   uploadDocumentsPage.uploadAdditionalDocuments(config.testFile);
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.uploadDocuments);
+  caseViewPage.selectTab(caseViewPage.tabs.evidence);
+  I.seeDocument('Social work chronology', '', 'To follow', 'mock reason');
+  I.seeDocument('Social work statement', 'mockFile.txt', 'Attached');
+  I.seeDocument('Social work assessment', 'mockFile.txt', 'Attached');
+  I.seeDocument('Care plan', 'mockFile.txt', 'Attached');
 });
