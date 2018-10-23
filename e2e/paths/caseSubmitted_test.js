70f2203c85e9dcc09a461a1c3ade78d4eea4f56e
swalker125 <sam.walker@digit-hull.com>
2018-10-23 17:25:33 +0100

    fpl-95: added submit case workaround

---
+++ e2e/paths/caseSubmitted_test.js
@@ -0,0 +1,19 @@
+const config = require('../config.js');
+
+//Temporary placeholder tests.
+
+Feature('Submit Case');
+
+Before((I, caseViewPage) => {
+  I.logInAndCreateCase(config.localAuthorityEmail, config.localAuthorityPassword, config.eventSummary, config.eventDescription);
+  I.selectOption(caseViewPage.actionsDropdown, config.applicationActions.submitCase);
+  I.click(caseViewPage.goButton);
+});
+
+
+Scenario('Submitting case', (I, caseViewPage) => {
+  I.click('Submit');
+  I.waitForElement('.tabs', 10);
+  I.seeEventSubmissionConfirmation(config.applicationActions.submitCase);
+  I.dontSee(caseViewPage.actionsDropdown);
+});
