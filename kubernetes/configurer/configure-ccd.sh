93f64f899bb30e8b0e16cecb7a9fc867a82e9c4a
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-24 01:39:47 +0100

    Include FPL configuration processing in pipeline

--- kubernetes/configurer/configure-ccd.sh
+++ kubernetes/configurer/configure-ccd.sh
@@ -2,6 +2,8 @@
 
 dir=$(dirname ${0})
 
-$dir/utils/idam-create-caseworker.sh local-authority@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-localAuthority
-$dir/utils/ccd-add-role.sh caseworker-publiclaw-localAuthority
-$dir/utils/ccd-import-definition.sh ${dir}/../../FPL_CCD_DEFINITION_TEMPLATEx.xlsx
+${dir}/utils/idam-create-caseworker.sh local-authority@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-localAuthority
+${dir}/utils/ccd-add-role.sh caseworker-publiclaw-localAuthority
+
+${dir}/utils/fpl-process-definition.sh
+${dir}/utils/ccd-import-definition.sh ${dir}/../../ccd-definition.xlsx
---
+++ kubernetes/configurer/utils/fpl-process-definition.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+dir=$(dirname ${0})
+root_dir=$(realpath ${dir}/../../..)
+
+definition_input_dir_name=ccd-definition
+definition_output_file_name=ccd-definition.xlsx
+
+definition_input_dir=${root_dir}/${definition_input_dir_name}
+definition_output_file=${root_dir}/${definition_output_file_name}
+
+if [[ ! -e ${definition_output_file} ]]; then
+   touch ${definition_output_file}
+fi
+
+docker run -it --rm --name json2xlsx \
+  -v ${definition_input_dir}:/tmp/${definition_input_dir_name} \
+  -v ${definition_output_file}:/tmp/${definition_output_file_name} \
+  docker.artifactory.reform.hmcts.net/ccd/ccd-definition-processor:latest \
+  json2xlsx -D /tmp/${definition_input_dir_name} -o /tmp/${definition_output_file_name}
