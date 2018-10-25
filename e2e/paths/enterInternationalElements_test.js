dc90f53ba8bb6cf1ce77d949153d104d0691d3a5
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-25 13:53:20 +0100

    Configure new tab

--- e2e/paths/enterInternationalElements_test.js
+++ e2e/paths/enterInternationalElements_test.js
@@ -7,16 +7,37 @@ Before((I, caseViewPage) => {
   caseViewPage.goToNewActions(config.applicationActions.enterInternationalElement);
 });
 
-Scenario('completing half of the international elements section of the c110a application', (I, enterInternationalElementsPage) => {
+Scenario('completing half of the international elements section of the c110a' +
+  ' application', (I, enterInternationalElementsPage, caseViewPage) => {
   enterInternationalElementsPage.halfFillForm();
   I.see('Give reason');
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.enterInternationalElement);
+  caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
+  I.seeAnswerInTab(1, 'International elements', 'Is there anyone in the' +
+    ' genogram outside the UK who has been assessed as a possible carer?', 'Yes');
+  I.seeAnswerInTab(2, 'International elements', 'Give reason', 'test');
+  I.seeAnswerInTab(3, 'International elements', 'Are you aware of any' +
+    ' significant events that have happened outside the UK?', 'Yes');
+  I.seeAnswerInTab(4, 'International elements', 'Give reason', 'test');
 });
 
-Scenario('completed international elements of the c110a application', (I, enterInternationalElementsPage) => {
+Scenario('completed international elements of the c110a application', (I, enterInternationalElementsPage, caseViewPage) => {
   enterInternationalElementsPage.fillForm();
   I.see('Give reason');
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.enterInternationalElement);
+  caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
+  I.seeAnswerInTab(1, 'International elements', 'Is there anyone in the' +
+    ' genogram outside the UK who has been assessed as a possible carer?', 'Yes');
+  I.seeAnswerInTab(2, 'International elements', 'Give reason', 'test');
+  I.seeAnswerInTab(3, 'International elements', 'Are you aware of any' +
+    ' significant events that have happened outside the UK?', 'Yes');
+  I.seeAnswerInTab(4, 'International elements', 'Give reason', 'test');
+  I.seeAnswerInTab(5, 'International elements', 'Are you aware of any issues' +
+    ' with the jurisdiction of this case - for example under the Brussels 2' +
+    ' regulation?', 'No');
+  I.seeAnswerInTab(6, 'International elements', 'Are you aware of any' +
+    ' proceedings outside the UK?', 'Yes');
+  I.seeAnswerInTab(7, 'International elements', 'Give reason', 'test');
 });
--- e2e/paths/enterRiskAndHarmToChild_test.js
+++ e2e/paths/enterRiskAndHarmToChild_test.js
@@ -7,18 +7,33 @@ Before((I, caseViewPage) => {
   caseViewPage.goToNewActions(config.applicationActions.enterRisk);
 });
 
-Scenario('complete half of the enter risk and harm to children in the c110a application', (I, enterRiskAndHarmToChildPage) => {
+Scenario('complete half of the enter risk and harm to children in the c110a' +
+  ' application', (I, enterRiskAndHarmToChildPage, caseViewPage) => {
   enterRiskAndHarmToChildPage.completePhyiscalHarm();
   enterRiskAndHarmToChildPage.completeEmotionalHarm();
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.enterRisk);
+  caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
+  I.seeAnswerInTab(1, 'Risks and harm to children', 'Physical harm including' +
+    ' non-accidental injury', 'Yes');
+  I.seeAnswerInTab(2, 'Risks and harm to children', 'Select all that apply', 'Past harm');
+  I.seeAnswerInTab(3, 'Risks and harm to children', 'Emotional harm', 'No');
 });
 
-Scenario('complete entering risk and harm to children in the c110a application', (I, enterRiskAndHarmToChildPage) => {
+Scenario('complete entering risk and harm to children in the c110a' +
+  ' application', (I, enterRiskAndHarmToChildPage, caseViewPage) => {
   enterRiskAndHarmToChildPage.completePhyiscalHarm();
   enterRiskAndHarmToChildPage.completeEmotionalHarm();
   enterRiskAndHarmToChildPage.completeSexualAbuse();
   enterRiskAndHarmToChildPage.completeNeglect();
   I.continueAndSubmit(config.eventSummary, config.eventDescription);
   I.seeEventSubmissionConfirmation(config.applicationActions.enterRisk);
+  caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
+  I.seeAnswerInTab(1, 'Risks and harm to children', 'Physical harm including' +
+  ' non-accidental injury', 'Yes');
+  I.seeAnswerInTab(2, 'Risks and harm to children', 'Select all that apply', 'Past harm');
+  I.seeAnswerInTab(3, 'Risks and harm to children', 'Emotional harm', 'No');
+  I.seeAnswerInTab(4, 'Risks and harm to children', 'Sexual abuse', 'No');
+  I.seeAnswerInTab(5, 'Risks and harm to children', 'Neglect', 'Yes');
+  I.seeAnswerInTab(6, 'Risks and harm to children', 'Select all that apply', ['Past harm', 'Future risk of harm']);
 });
