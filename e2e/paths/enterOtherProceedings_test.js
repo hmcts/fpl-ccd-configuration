dc2f7f33c2b02803e64071cbaaddafb7bd9a3b64
Kurt Plaschke <k.plaschke@hull.ac.uk>
2018-10-25 13:11:46 +0100

    resolved some travis issues with spacing

--- e2e/paths/enterOtherProceedings_test.js
+++ e2e/paths/enterOtherProceedings_test.js
@@ -19,23 +19,23 @@ Scenario('Select not aware of any ongoing or previous proceedings', (I, enterOth
 
 Scenario('Select yes for ongoing or previous proceedings and fill in information',
   (I, enterOtherProceedingsPage, caseViewPage) => {
-  enterOtherProceedingsPage.selectYesForProceeding();
-  enterOtherProceedingsPage.enterProceedingInformation(otherProceedingData);
-  I.continueAndSubmit(config.eventSummary, config.eventDescription);
-  I.seeEventSubmissionConfirmation(config.applicationActions.enterOtherProceedings);
-  caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
-  I.seeAnswerInTab(1, 'Other proceedings', 'Are you aware of any ongoing or' +
-    ' previous proceedings?', 'Yes');
-  I.seeAnswerInTab(2, 'Other proceedings', 'Proceeding status', 'Ongoing');
-  I.seeAnswerInTab(3, 'Other proceedings', 'Case number', '000000');
-  I.seeAnswerInTab(4, 'Other proceedings', 'Started', '01/01/01');
-  I.seeAnswerInTab(5, 'Other proceedings', 'Ended', '02/01/01');
-  I.seeAnswerInTab(6, 'Other proceedings', 'Orders made', 'Yes');
-  I.seeAnswerInTab(7, 'Other proceedings', 'Judge', 'District Judge Martin' +
-    ' Brown');
-  I.seeAnswerInTab(8, 'Other proceedings', 'Children subject to proceedings', 'Joe Bloggs');
-  I.seeAnswerInTab(9, 'Other proceedings', 'Guardian', 'John Smith');
-  I.seeAnswerInTab(10, 'Other proceedings', 'Same guardian needed?', 'Yes');
-});
+    enterOtherProceedingsPage.selectYesForProceeding();
+    enterOtherProceedingsPage.enterProceedingInformation(otherProceedingData);
+    I.continueAndSubmit(config.eventSummary, config.eventDescription);
+    I.seeEventSubmissionConfirmation(config.applicationActions.enterOtherProceedings);
+    caseViewPage.selectTab(caseViewPage.tabs.legalOpinion);
+    I.seeAnswerInTab(1, 'Other proceedings', 'Are you aware of any ongoing or' +
+      ' previous proceedings?', 'Yes');
+    I.seeAnswerInTab(2, 'Other proceedings', 'Proceeding status', 'Ongoing');
+    I.seeAnswerInTab(3, 'Other proceedings', 'Case number', '000000');
+    I.seeAnswerInTab(4, 'Other proceedings', 'Started', '01/01/01');
+    I.seeAnswerInTab(5, 'Other proceedings', 'Ended', '02/01/01');
+    I.seeAnswerInTab(6, 'Other proceedings', 'Orders made', 'Yes');
+    I.seeAnswerInTab(7, 'Other proceedings', 'Judge', 'District Judge Martin' +
+      ' Brown');
+    I.seeAnswerInTab(8, 'Other proceedings', 'Children subject to proceedings', 'Joe Bloggs');
+    I.seeAnswerInTab(9, 'Other proceedings', 'Guardian', 'John Smith');
+    I.seeAnswerInTab(10, 'Other proceedings', 'Same guardian needed?', 'Yes');
+  });
 
 
