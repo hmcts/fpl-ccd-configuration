f15ec95f4e723d637fe0d6b45dc6c9ca6e261d52
swalker125 <sam.walker@digit-hull.com>
2018-10-26 10:25:19 +0100

    FPL-88: added courtadmin user to configuration

--- bin/add-users-and-roles.sh
+++ bin/add-users-and-roles.sh
@@ -3,5 +3,7 @@
 dir=$(dirname ${0})
 
 ${dir}/../ccd-docker/bin/ccd-add-role.sh caseworker-publiclaw-localAuthority
+${dir}/../ccd-docker/bin/ccd-add-role.sh caseworker-publiclaw-courtadmin
 
 ${dir}/../ccd-docker/bin/idam-create-caseworker.sh caseworker,caseworker-publiclaw,caseworker-publiclaw-localAuthority local-authority@example.com
+${dir}/../ccd-docker/bin/idam-create-caseworker.sh caseworker,caseworker-publiclaw,caseworker-publiclaw-courtadmin hmcts-admin@gov.uk
