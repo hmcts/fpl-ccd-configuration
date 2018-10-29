1c637ecc8f843ea4baac4cff49cc970146b12cd4
swalker125 <sam.walker@digit-hull.com>
2018-10-29 10:42:10 +0000

    FPL-88: fixed linting errors. updated eslint with babel parser

--- e2e/pages/caseList/caseList.js
+++ e2e/pages/caseList/caseList.js
@@ -3,6 +3,7 @@ const I = actor();
 module.exports = {
   openExistingCase(caseId) {
     let href = `/case/PUBLICLAW/Shared_Storage_DRAFTType/${caseId.replace(/\D/g, '')}`;
-    I.click(locate('a').withAttr({href: href}));
+    let caseLink = this.locate('a').withAttr({href: href});
+    I.click(caseLink);
   },
 };
