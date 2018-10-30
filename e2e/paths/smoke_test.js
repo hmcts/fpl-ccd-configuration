9ea19568881a410efe08dff47d807849535699f9
Damian Dunajski <damian.dunajski@hmcts.net>
2018-10-30 12:54:36 +0000

    Add E2E smoke test to login as court admin

--- e2e/paths/smoke_test.js
+++ e2e/paths/smoke_test.js
@@ -6,3 +6,8 @@ Scenario('Sign in as local authority', (I, loginPage) => {
   loginPage.signIn(config.localAuthorityEmail, config.localAuthorityPassword);
   I.see('Create new case');
 });
+
+Scenario('Sign in as court admin', (I, loginPage) => {
+  loginPage.signIn(config.hmctsAdminEmail, config.hmctsAdminPassword);
+  I.see('Create new case');
+});
