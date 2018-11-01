7e91b1848e8b3e6fefe45d043a82c21175e51815
James Nelson <Jamesnelson117@gmail.com>
2018-11-01 17:18:00 +0000

    FPL-77: Add event to capture FamilyMan case number (#74)

--- e2e/config.js
+++ e2e/config.js
@@ -24,6 +24,7 @@ module.exports = {
     uploadDocuments: 'Annex documents',
     submitCase: 'Submit application',
   },
+  addFamilyManCaseNumber: 'Add case number',
   testFile: './e2e/fixtures/mockFile.txt',
   presidentsGuidanceUrl: 'https://www.judiciary.uk/wp-content/uploads/2013/03/President%E2%80%99s-Guidance-on-Allocation-and-Gatekeeping.pdf',
   scheduleUrl: 'https://www.judiciary.uk/wp-content/uploads/2013/03/Schedule-to-the-President%E2%80%99s-Guidance-on-Allocation-and-Gatekeeping.pdf',
