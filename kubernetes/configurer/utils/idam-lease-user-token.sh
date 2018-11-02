f0a5c7424ffc0ac5c63c0959dca11cab8c86bafd
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-02 18:30:08 +0000

    Use full name of switch that prints custom output message

--- kubernetes/configurer/utils/idam-lease-user-token.sh
+++ kubernetes/configurer/utils/idam-lease-user-token.sh
@@ -5,7 +5,7 @@ set -e
 id=${1}
 role=${2}
 
-curl -k --fail --silent -w "%{url_effective}: %{http_code} response received in %{time_total}s\n" -X POST \
+curl -k --fail --silent --write-out "%{url_effective}: %{http_code} response received in %{time_total}s\n" -X POST \
   ${IDAM_API_BASE_URL}/testing-support/lease \
   -Fid=${id} \
   -Frole=${role}
