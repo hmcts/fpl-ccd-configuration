2b4fef23066a95be5c45262161c5764ae7c846a6
Sivakumar Sirigiri <sivakumarsirigiri@Sivakumars-MacBook-Pro.local>
2018-11-05 11:03:20 +0000

    attendinghearing page fields removed and updated with locators

--- e2e/pages/attendingHearing/attendingHearing.js
+++ e2e/pages/attendingHearing/attendingHearing.js
@@ -2,48 +2,30 @@ const I = actor();
 
 module.exports = {
 
-  fields: {
-    interpreter: {
-      yes: '#hearingPreferences_interpreter-Yes',
-      details: '#hearingPreferences_interpreterDetails',
-    },
-    intermediaryNo: '#hearingPreferences_intermediary-No',
-    litigationCapacityNo: '#hearingPreferences_litigation-No',
-    learningDisability: {
-      yes: '#hearingPreferences_learningDisability-Yes',
-      details: '#hearingPreferences_learningDisabilityDetails',
-    },
-    welshNo: '#hearingPreferences_welsh-No',
-    security: {
-      yes: '#hearingPreferences_extraSecurityMeasures-Yes',
-      details: '#hearingPreferences_extraSecurityMeasuresDetails',
-    },
-  },
-
   enterInterpreter(details = 'French translator') {
-    I.click(this.fields.interpreter.yes);
-    I.fillField(this.fields.interpreter.details, details);
+    I.click('#hearingPreferences_interpreter-Yes');
+    I.fillField('#hearingPreferences_interpreterDetails', details);
   },
 
   enterIntermediary() {
-    I.click(this.fields.intermediaryNo);
+    I.click('#hearingPreferences_intermediary-No');
   },
 
   enterLitigationIssues() {
-    I.click(this.fields.litigationCapacityNo);
+    I.click('#hearingPreferences_litigation-No');
   },
 
   enterLearningDisability(details = 'learning difficulty') {
-    I.click(this.fields.learningDisability.yes);
-    I.fillField(this.fields.learningDisability.details, details);
+    I.click('#hearingPreferences_learningDisability-Yes');
+    I.fillField('#hearingPreferences_learningDisabilityDetails', details);
   },
 
   enterWelshProceedings() {
-    I.click(this.fields.welshNo);
+    I.click('#hearingPreferences_welsh-No');
   },
 
   enterExtraSecurityMeasures(details = 'Separate waiting rooms') {
-    I.click(this.fields.security.yes);
-    I.fillField(this.fields.security.details, details);
+    I.click('#hearingPreferences_extraSecurityMeasures-Yes');
+    I.fillField('#hearingPreferences_extraSecurityMeasuresDetails', details);
   },
 };
