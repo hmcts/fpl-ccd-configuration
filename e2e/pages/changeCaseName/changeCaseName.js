a43fed76172fbebb822035436f0ce5a43c75a70d
James Nelson <Jamesnelson117@gmail.com>
2018-11-06 11:58:07 +0000

    FPL-96: Implemented add case name functionality

---
+++ e2e/pages/changeCaseName/changeCaseName.js
@@ -0,0 +1,12 @@
+const I = actor();
+
+module.exports = {
+
+  fields: {
+    caseName: '#caseName',
+  },
+
+  changeCaseName(caseName='Craigavon council v Smith') {
+    I.fillField(this.fields.caseName, caseName);
+  },
+};
--- e2e/pages/createCase/createCase.js
+++ e2e/pages/createCase/createCase.js
@@ -6,13 +6,24 @@ module.exports = {
     jurisdiction: 'jurisdiction',
     caseType: 'case-type',
     event: 'event',
+    enterCaseName: {
+      caseName: '#caseName'
+    }
   },
+  testCaseName: 'Barnet Council v Smith',
+  continueButton: 'Continue',  
   startButton: 'Start',
+
+  enterCaseName() {
+    I.fillField(this.fields.enterCaseName.caseName, this.testCaseName);
+  },
 
   createNewCase() {
     I.selectOption(this.fields.jurisdiction, 'Public Law DRAFT');
     I.selectOption(this.fields.caseType, 'Shared_Storage_DRAFT_v0.3');
     I.selectOption(this.fields.event, 'Open case');
     I.click(this.startButton);
+    this.enterCaseName();
+    I.click(this.continueButton);
   },
 };
