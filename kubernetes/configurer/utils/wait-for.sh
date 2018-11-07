1a006d2477629b269b2b60d7bdb835c49f375cd1
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-07 13:30:52 +0000

    Use wait-for.sh script in smoke tests

--- kubernetes/configurer/utils/wait-for.sh
+++ kubernetes/configurer/utils/wait-for.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+service_base_urls=${@}
+
 max_health_check_attempts=30
 
 function checkHealth {
-  for service_base_url in ${@}; do
+  for service_base_url in ${service_base_urls}; do
     curl -k --fail --silent --output /dev/null --head ${service_base_url}/health
     if [ $? -ne 0 ]; then
       exit 1
