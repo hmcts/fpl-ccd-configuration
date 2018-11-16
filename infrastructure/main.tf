65c4495941da7f2554e30fea48ec4cf1fd6cc035
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-16 17:19:59 +0000

    Create vault in terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,12 +3,20 @@ provider "azurerm" {}
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "aat" : "saat" : var.env}"
+  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
 
+  vault_name = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "${var.product}-aat" : "${var.product}-saat" : "${var.product}-${var.env}"}"
+
+  # URLs
   IDAM_S2S_AUTH_URL = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
   DOCUMENT_MANAGEMENT_URL = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
   CORE_CASE_DATA_API_URL = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name = "${local.vault_name}"
+  resource_group_name = "${local.vault_name}"
 }
 
 module "case-service" {
