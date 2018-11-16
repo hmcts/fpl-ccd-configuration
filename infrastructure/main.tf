8d405721f10a136a0ba69306f02ba697acec0399
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-16 16:12:01 +0000

    Add missing service env vars

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,13 @@ provider "azurerm" {}
 
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.ase_name}"
+
+  IDAM_S2S_AUTH_URL = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
+  DOCUMENT_MANAGEMENT_URL = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
+  CORE_CASE_DATA_API_URL = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
 }
 
 module "case-service" {
@@ -15,6 +22,11 @@ module "case-service" {
   common_tags         = "${var.common_tags}"
 
   app_settings = {
+    IDAM_S2S_AUTH_URL = "${local.IDAM_S2S_AUTH_URL}"
+    IDAM_S2S_AUTH_TOTP_SECRET = "AABBCCDDEEFFGGHH"
+    DOCUMENT_MANAGEMENT_URL = "${local.DOCUMENT_MANAGEMENT_URL}"
+    CORE_CASE_DATA_API_URL = "${local.CORE_CASE_DATA_API_URL}"
+
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
   }
