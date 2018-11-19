6c47c9dbf94037c8c736c27711022865e7e5f55a
swalker125 <sam.walker@digit-hull.com>
2018-11-19 15:53:36 +0000

    FPL-208: Refactored tests to assert Local Authority event present in case
    Changed BASE_URL value to fpl-service

--- e2e/paths/caseSubmitted_test.js
+++ e2e/paths/caseSubmitted_test.js
@@ -6,12 +6,15 @@ Before((I, caseViewPage) => {
   I.logInAndCreateCase(config.localAuthorityEmail, config.localAuthorityPassword, config.eventSummary, config.eventDescription);
   I.selectOption(caseViewPage.actionsDropdown, config.applicationActions.submitCase);
   I.click(caseViewPage.goButton);
-});
-
-
-Scenario('Submitting case', (I, caseViewPage) => {
   I.click('Submit');
   I.waitForElement('.tabs');
+});
+
+Scenario('Check add local authority event occurred', (I) => {
+  I.see('Add Local Authority');
+});
+
+Scenario('Submitting case', (I, caseViewPage) => {
   I.seeEventSubmissionConfirmation(config.applicationActions.submitCase);
   I.dontSee(caseViewPage.actionsDropdown);
 });
