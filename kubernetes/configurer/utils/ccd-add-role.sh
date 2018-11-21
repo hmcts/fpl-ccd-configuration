05cdf39df6d9adbffa0ab3b537f2de58d248e06a
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-21 13:53:54 +0000

    Run S2S OTP generation in docker

--- kubernetes/configurer/utils/ccd-add-role.sh
+++ kubernetes/configurer/utils/ccd-add-role.sh
@@ -7,7 +7,7 @@ role=${1}
 dir=$(dirname ${0})
 
 userToken=$(${dir}/idam-lease-user-token.sh 1 ccd-import)
-serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(oathtool --totp -b ${CCD_CONFIGURER_S2S_SECRET}))
+serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_CONFIGURER_S2S_SECRET}))
 
 curl -k --fail --show-error --silent --output /dev/null -X PUT \
   ${CCD_DEFINITION_STORE_API_BASE_URL}/api/user-role \
