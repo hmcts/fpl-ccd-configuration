c8346a13633edc8047d4a2eae94df2bf5eeb8774
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-23 15:56:41 +0000

    Fix checkstyle and test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/Generator.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/Generator.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.populatedCaseDetails;
 
+@SuppressWarnings("HideUtilityClassConstructor")
 public class Generator {
 
     public static void main(String[] args) throws IOException {
