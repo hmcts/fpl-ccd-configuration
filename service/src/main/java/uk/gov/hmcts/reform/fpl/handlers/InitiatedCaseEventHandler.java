21af0e067d5852196cf9c12520dd93c8aa440d69
swalker125 <sam.walker@digit-hull.com>
2018-11-26 16:59:25 +0000

    FPL-208: Added component annotation to event handler

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/InitiatedCaseEventHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/InitiatedCaseEventHandler.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.fpl.handlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.fpl.events.InitiatedCaseEvent;
 import uk.gov.hmcts.reform.fpl.service.LocalAuthorityUserService;
 
+@Component
 public class InitiatedCaseEventHandler {
 
     private final LocalAuthorityUserService localAuthorityUserService;
@@ -23,7 +25,7 @@ public class InitiatedCaseEventHandler {
         String caseId = event.getCallbackRequest().getCaseDetails().getId().toString();
         String caseLocalAuthority = event.getCallbackRequest().getCaseDetails().getData()
             .get("caseLocalAuthority").toString();
-        
+
         localAuthorityUserService.grantUserAccess(authorization, userId, caseId, caseLocalAuthority);
     }
 }
