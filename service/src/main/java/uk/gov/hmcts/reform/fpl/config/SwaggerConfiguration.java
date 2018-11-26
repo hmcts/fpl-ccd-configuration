1cd97284b44a92def2c01fff21d951c5e415abe7
Damian Dunajski <damdun@gmail.com>
2018-11-26 16:38:49 +0000

    Remove unused dependencies, redundant annotations and disable unneeded endpoints in integration tests (#114)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/SwaggerConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.fpl.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -9,6 +10,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.fpl.Application;
 
+@Profile("default")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
