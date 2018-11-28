4501832fba7dfa6ecf68a3e59f7c900926236b06
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-28 14:06:22 +0000

    Add configuration for resource group and vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,10 +8,32 @@ locals {
 
   vault_name = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "${var.raw_product}-aat" : "${var.raw_product}-saat" : "${var.raw_product}-${var.env}"}"
 
+  tags = "${merge(var.common_tags,
+    map("Team Contact", "${var.team_contact}")
+  )}"
+
   # URLs
   IDAM_S2S_AUTH_URL = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
   DOCUMENT_MANAGEMENT_URL = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
   CORE_CASE_DATA_API_URL = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.env}"
+  location = "${var.location}"
+
+  tags = "${local.tags}"
+}
+
+module "fpl-vault" {
+  source = "git@github.com:contino/moj-module-key-vault?ref=master"
+  name = "fpl-${var.env}"
+  product = "${var.product}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
 }
 
 data "azurerm_key_vault" "key_vault" {
