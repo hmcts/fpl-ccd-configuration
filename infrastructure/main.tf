9c2cb75b92e7542f18567e4808cf803044c12b6a
Damian Dunajski <damian.dunajski@hmcts.net>
2018-11-29 01:09:14 +0000

    Update terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,36 +8,31 @@ locals {
 
   vault_name = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview") ? "${var.raw_product}-aat" : "${var.raw_product}-saat" : "${var.raw_product}-${var.env}"}"
 
-//  tags = "${merge(var.common_tags,
-//    map("Team Contact", "${var.team_contact}")
-//  )}"
-
   # URLs
   IDAM_S2S_AUTH_URL = "http://rpe-service-auth-provider-${local.local_env}.service.${local.local_ase}.internal"
   DOCUMENT_MANAGEMENT_URL = "http://dm-store-${local.local_env}.service.${local.local_ase}.internal"
   CORE_CASE_DATA_API_URL = "http://ccd-data-store-api-${local.local_env}.service.${local.local_ase}.internal"
 }
 
-//resource "azurerm_resource_group" "rg" {
-//  name     = "${var.product}-${var.env}"
-//  location = "${var.location}"
-//
-//  tags = "${local.tags}"
-//}
-
-data "azurerm_key_vault" "key_vault" {
-  name = "${local.vault_name}"
-  resource_group_name = "${local.vault_name}"
-}
-
 data "azurerm_key_vault_secret" "s2s_secret" {
   name = "fpl-case-service-s2s-secret"
-  vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  vault_uri = "${module.key-vault.key_vault_uri}"
 }
 
 data "azurerm_key_vault_secret" "local_authority_name_mapping" {
   name = "local-authority-name-mapping"
-  vault_uri = "${data.azurerm_key_vault.key_vault.vault_uri}"
+  vault_uri = "${module.key-vault.key_vault_uri}"
+}
+
+module "key-vault" {
+  source = "git@github.com:hmcts/cnp-module-key-vault.git?ref=master"
+  name = "fpl-${var.env}"
+  product = "${var.product}"
+  env = "${var.env}"
+  tenant_id = "${var.tenant_id}"
+  object_id = "${var.jenkins_AAD_objectId}"
+  resource_group_name = "${module.case-service.resource_group_name}"
+  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
 }
 
 module "case-service" {
@@ -61,15 +56,4 @@ module "case-service" {
     LOGBACK_REQUIRE_ALERT_LEVEL = false
     LOGBACK_REQUIRE_ERROR_CODE  = false
   }
-}
-
-module "fpl-vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault.git?ref=master"
-  name = "fpl-${var.env}"
-  product = "${var.product}"
-  env = "${var.env}"
-  tenant_id = "${var.tenant_id}"
-  object_id = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.case-service.resource_group_name}"
-  product_group_object_id = "68839600-92da-4862-bb24-1259814d1384"
 }
