12f062ff15826e6582dfd1ca3c6b46afe55fc4f0
Damian Dunajski <damdun@gmail.com>
2018-11-29 15:35:16 +0000

    FPL-264: Configure demo environment (#120)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -32,6 +32,18 @@ variable "common_tags" {
   type = "map"
 }
 
+variable "team_contact" {
+  default = "#fpl-tech"
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
 variable "idam_api_url" {
   type = "string"
 }
