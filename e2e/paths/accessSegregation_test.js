a62906ff2e5bcc5012bff9572525eb43070aa9cc
swalker125 <sam.walker@digit-hull.com>
2018-12-03 11:14:39 +0000

    FPL-218: Refactored e2e with url env

--- e2e/paths/accessSegregation_test.js
+++ e2e/paths/accessSegregation_test.js
@@ -1,7 +1,10 @@
+/*global process*/
+
 const config = require('../config.js');
 
 let caseId;
 let caseUrl;
+let baseUrl = process.env.URL || 'http://localhost:3451';
 
 Feature('Cases visible only to respective local authority and admin');
 
@@ -16,18 +19,18 @@ Before(async (I, caseViewPage) => {
 
 Scenario('Different user in the same local authority can see case created', async (I, loginPage) => {
   loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
-  I.navigateToUrl('http://localhost:3451/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
+  I.navigateToUrl(baseUrl + '/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
   I.see(caseId);
 });
 
 Scenario('Different user in a different local authority cannot see case created', (I, loginPage) => {
   loginPage.signIn(config.hillingdonLocalAuthorityEmailUserOne, config.localAuthorityPassword);
-  I.navigateToUrl('http://localhost:3451/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
+  I.navigateToUrl(baseUrl + '/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
   I.seeInCurrentUrl('error');
 });
 
 Scenario('HMCTS admin user can see the case', (I, loginPage) => {
   loginPage.signIn(config.hmctsAdminEmail, config.hmctsAdminPassword);
-  I.navigateToUrl('http://localhost:3451/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
+  I.navigateToUrl(baseUrl + '/case/PUBLICLAW/Shared_Storage_DRAFTType/' + caseUrl);
   I.see(caseId);
 });
