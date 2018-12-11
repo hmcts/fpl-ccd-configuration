1c0a85a0219314ef3b0c1bff109f21da1ec0c8ee
Damian Dunajski <damian.dunajski@hmcts.net>
2018-12-11 00:27:55 +0000

    Refactor code to have user ids lookup in LocalAuthorityUserLookupConfiguration class

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/config/LocalAuthorityUserLookupConfigurationTest.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.fpl.config;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.fpl.exceptions.UnknownLocalAuthorityCodeException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LocalAuthorityUserLookupConfigurationTest {
+
+    private static final String LOCAL_AUTHORITY_CODE = "example";
+    private static final String LOCAL_AUTHORITY_USER_IDS = "1,2,3";
+
+    private static final String CONFIG = String.format("%s=>%s", LOCAL_AUTHORITY_CODE, LOCAL_AUTHORITY_USER_IDS);
+
+    private LocalAuthorityUserLookupConfiguration configuration = new LocalAuthorityUserLookupConfiguration(CONFIG);
+
+    @Test
+    void shouldThrowNullPointerExceptionUponInitialisationWhenMappingValueIsEmpty() {
+        Assertions.assertThatThrownBy(() -> new LocalAuthorityCodeLookupConfiguration(CONFIG + ";fake=>"))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("Mapping value cannot be empty");
+    }
+
+    @Test
+    void shouldThrowNullPointerExceptionWhenLocalAuthorityCodeIsNull() throws IllegalArgumentException {
+        assertThatThrownBy(() ->
+            configuration.getUserIds(null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("Local authority code cannot be null");
+    }
+
+    @Test
+    void shouldThrowCustomExceptionWhenLocalAuthorityCodeDoesNotExist() throws IllegalArgumentException {
+        assertThatThrownBy(() ->
+            configuration.getUserIds("FT"))
+            .isInstanceOf(UnknownLocalAuthorityCodeException.class)
+            .hasMessage("Local authority 'FT' was not found");
+    }
+
+    @Test
+    void shouldReturnLocalAuthorityUserIdsWhenLocalAuthorityCodeExists() {
+        List<String> localAuthorityUserIds = configuration.getUserIds(LOCAL_AUTHORITY_CODE);
+
+        assertThat(localAuthorityUserIds).containsExactly(LOCAL_AUTHORITY_USER_IDS.split(","));
+    }
+
+}
