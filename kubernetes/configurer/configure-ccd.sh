36b4162a4424a19f2417000319f1804aa5ca6cf3
mh-hmcts <43960742+mh-hmcts@users.noreply.github.com>
2018-12-13 23:59:39 +0000

    Improve user, role and definition configuration scripts (#136)

--- kubernetes/configurer/configure-ccd.sh
+++ kubernetes/configurer/configure-ccd.sh
@@ -6,16 +6,5 @@ dir=$(dirname ${0})
 
 ${dir}/utils/wait-for.sh ${SERVICE_AUTH_PROVIDER_API_BASE_URL} ${IDAM_API_BASE_URL} ${CCD_DEFINITION_STORE_API_BASE_URL} ${CCD_USER_PROFILE_API_BASE_URL}
 
-${dir}/utils/idam-create-caseworker.sh damian@swansea.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh kurt@swansea.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh james@swansea.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh sam@hillingdon.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh siva@hillingdon.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh hmcts-admin@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-courtadmin "(hmcts-admin)"
-${dir}/utils/idam-create-caseworker.sh cafcass@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-cafcass "(cafcass)"
-${dir}/utils/ccd-add-role.sh caseworker-publiclaw-solicitor
-${dir}/utils/ccd-add-role.sh caseworker-publiclaw-courtadmin
-${dir}/utils/ccd-add-role.sh caseworker-publiclaw-cafcass
-
-${dir}/utils/fpl-process-definition.sh
-${dir}/utils/ccd-import-definition.sh ${dir}/../../ccd-definition.xlsx
+${dir}/add-users-and-roles.sh
+${dir}/import-ccd-definition.sh
