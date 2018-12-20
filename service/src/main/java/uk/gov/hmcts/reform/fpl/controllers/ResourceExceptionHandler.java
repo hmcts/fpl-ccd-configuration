cfb8dda0a89da0b9a71602edb7f50e4e11556d1b
Damian Dunajski <damian.dunajski@hmcts.net>
2018-12-20 11:52:12 +0000

    Revert to 'ResponseEntity<AboutToStartOrSubmitCallbackResponse>' to fix exception handler

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ResourceExceptionHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ResourceExceptionHandler.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.fpl.controllers;
 import com.google.common.collect.ImmutableList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -14,13 +15,13 @@ public class ResourceExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(ResourceExceptionHandler.class);
 
     @ExceptionHandler(value = AboutToStartOrSubmitCallbackException.class)
-    public AboutToStartOrSubmitCallbackResponse handleAboutToStartOrSubmitCallbackException(AboutToStartOrSubmitCallbackException exception) {
+    public ResponseEntity<AboutToStartOrSubmitCallbackResponse> handleAboutToStartOrSubmitCallbackException(AboutToStartOrSubmitCallbackException exception) {
         logger.error(exception.getMessage(), exception);
 
-        return AboutToStartOrSubmitCallbackResponse.builder()
+        return ResponseEntity.ok(AboutToStartOrSubmitCallbackResponse.builder()
             .errors(ImmutableList.<String>builder()
                 .add(exception.getUserMessage())
                 .build())
-            .build();
+            .build());
     }
 }
