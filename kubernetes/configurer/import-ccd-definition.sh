c8de260dbbd162ad111332e765683ab622437d3f
mh-hmcts <mh@hmcts.net>
2019-01-11 15:42:53 +0000

    reuse definition processor script

--- kubernetes/configurer/import-ccd-definition.sh
+++ kubernetes/configurer/import-ccd-definition.sh
@@ -3,6 +3,7 @@
 set -eu
 
 dir=$(dirname ${0})
+root_dir=$(realpath ${dir}/../..)
 
-${dir}/utils/fpl-process-definition.sh
-${dir}/utils/ccd-import-definition.sh ${dir}/../../ccd-definition.xlsx
+${dir}/utils/fpl-process-definition.sh ${root_dir}/ccd-definition ${root_dir} ccd-definition.xlsx
+${dir}/utils/ccd-import-definition.sh ${root_dir}/ccd-definition.xlsx
