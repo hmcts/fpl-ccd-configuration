09d5811c6adfded201f96dd78935ca2d50c0b9a5
mh-hmcts <mh@hmcts.net>
2019-01-14 15:49:47 +0000

    beautify the code

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -8,11 +8,11 @@ dev_config_dir=${root_dir}/ccd-definition
 prod_definition_filename=CCD_FPL_PROD.xlsx
 utils_dir=${root_dir}/kubernetes/configurer/utils
 
-#create build folder, copy development config and remove the users
+# create build folder, copy development config and remove the users
 mkdir -p ${prod_config_dir}
 cp -a ${dev_config_dir}/. ${prod_config_dir}
 rm ${prod_config_dir}/UserProfile.json
 
-#build the ccd definition file
+# build the ccd definition file
 export CCD_DEF_CASE_SERVICE_BASE_URL=http://fpl-case-service-prod.service.core-compute-prod.internal
 $utils_dir/fpl-process-definition.sh ${prod_config_dir} ${build_dir} ${prod_definition_filename}
--- kubernetes/configurer/utils/fpl-process-definition.sh
+++ kubernetes/configurer/utils/fpl-process-definition.sh
@@ -4,10 +4,10 @@ set -eu
 
 definition_dir=${1}
 output_dir=${2}
-definition_output_file_name=${3}
+definition_output_filename=${3}
 
 definition_input_dir=${definition_dir}
-definition_output_file=${output_dir}/${definition_output_file_name}
+definition_output_file=${output_dir}/${definition_output_filename}
 
 if [[ ! -e ${definition_output_file} ]]; then
    touch ${definition_output_file}
@@ -15,7 +15,7 @@ fi
 
 docker run --rm --name json2xlsx \
   -v ${definition_input_dir}:/tmp/ccd-definition \
-  -v ${definition_output_file}:/tmp/${definition_output_file_name} \
+  -v ${definition_output_file}:/tmp/${definition_output_filename} \
   -e CCD_DEF_CASE_SERVICE_BASE_URL \
   docker.artifactory.reform.hmcts.net/ccd/ccd-definition-processor:c480382 \
-  json2xlsx -D /tmp/ccd-definition -o /tmp/${definition_output_file_name}
+  json2xlsx -D /tmp/ccd-definition -o /tmp/${definition_output_filename}
