b3f54e630c37de95ceb55f393714c3603405d1ba
swalker125 <sam.walker@digit-hull.com>
2019-01-15 15:27:55 +0000

    [WIP] FPL-227: Added aboutToSubmit callback to orders event populating show hide field.
    Currently callback controller needs refactoring. Issue with show hide found where EPO Reason fields are shown but disappear after clicking continue and previous

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/EnterGroundsStartCallbackController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/EnterGroundsStartCallbackController.java
@@ -1,18 +1,18 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
-import com.google.common.collect.ImmutableMap;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Api
 @RestController
@@ -28,20 +28,19 @@ public class EnterGroundsStartCallbackController {
     public AboutToStartOrSubmitCallbackResponse handleAboutToStartEvent(
         @RequestBody CallbackRequest callbackrequest) {
         CaseDetails caseDetails = callbackrequest.getCaseDetails();
-        Map<String, Object> ordersAndDirections = (Map<String, Object>) caseDetails.getData().get("orders");
+        Map<String, Object> data = caseDetails.getData();
+        Optional<List<String>> orderType = Optional.ofNullable((Map<String, Object>) data.get("orders"))
+            .map(orders -> (List<String>) orders.get("orderType"));
 
-        if (ordersAndDirections.get("orderType").toString().equals("[EMERGENCY_PROTECTION_ORDER]" )) {
-            Map<String, Object> data = caseDetails.getData();
-            data.put("grounds", ImmutableMap.<String, String>builder()
-                .put("EPO_REASONING_SHOW", "SHOW_FIELD")
-                .build());
-
-            return AboutToStartOrSubmitCallbackResponse.builder()
-                .data(data)
-                .build();
+        if (orderType.toString().contains("EMERGENCY_PROTECTION_ORDER")) {
+                data.put("EPO_REASONING_SHOW", "SHOW_FIELD");
         }
+        System.out.println("data = " + AboutToStartOrSubmitCallbackResponse.builder()
+            .data(data)
+            .build());
 
         return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(data)
             .build();
     }
 }
