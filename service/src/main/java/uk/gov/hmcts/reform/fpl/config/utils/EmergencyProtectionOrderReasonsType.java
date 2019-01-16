a8aad5733bc0351219dc23d7f375405700e497a1
James Nelson <Jamesnelson117@gmail.com>
2019-01-16 14:59:57 +0000

    FPL-227: Updates to PDF template, tests to reflect EPO data structure changes

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
@@ -2,11 +2,11 @@ package uk.gov.hmcts.reform.fpl.config.utils;
 
 public enum EmergencyProtectionOrderReasonsType {
 
-    PLACED_IN_PROVIDED_ACCOMMODATION("There???s reasonable cause to believe the child is likely to suffer"
-        + " significant harm if they???re not moved to accommodation provided by you, or on your behalf"),
-    KEPT_IN_CURRENT_ACCOMMODATION("There???s reasonable cause to believe the child is likely to suffer significant"
-        + " harm if they don???t stay in their current accommodation"),
-    URGENT_ACCESS_TO_CHILD("You???re making enquiries and need urgent access to the child to find out about their"
+    PLACED_IN_PROVIDED_ACCOMMODATION("There’s reasonable cause to believe the child is likely to suffer"
+        + " significant harm if they’re not moved to accommodation provided by you, or on your behalf"),
+    KEPT_IN_CURRENT_ACCOMMODATION("There’s reasonable cause to believe the child is likely to suffer significant"
+        + " harm if they don’t stay in their current accommodation"),
+    URGENT_ACCESS_TO_CHILD("You’re making enquiries and need urgent access to the child to find out about their"
         + " welfare, and access is being unreasonably refused");
 
     private final String label;
