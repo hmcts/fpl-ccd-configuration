6ac9a462846feefec8da35575fa94a7bd20ccf35
swalker125 <sam.walker@digit-hull.com>
2019-01-17 12:23:21 +0000

    FPL-227: Removed unsued autowired import

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/EnterGroundsStartCallbackController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/EnterGroundsStartCallbackController.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
