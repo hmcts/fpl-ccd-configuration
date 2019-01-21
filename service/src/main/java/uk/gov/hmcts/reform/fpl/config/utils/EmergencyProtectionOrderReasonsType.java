98ac2c62361f35c0b59b12a55ac85800ae0bb124
swalker125 <sam.walker@digit-hull.com>
2019-01-21 11:58:26 +0000

    FPL-227: Refactored EPOReasons list element codes

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
@@ -2,10 +2,10 @@ package uk.gov.hmcts.reform.fpl.config.utils;
 
 public enum EmergencyProtectionOrderReasonsType {
 
-    POSSIBLE_HARM_IF_NOT_PLACED_IN_PROVIDED_ACCOMMODATION(
+    HARM_IF_NOT_MOVED_TO_NEW_ACCOMMODATION(
         "There’s reasonable cause to believe the child is likely to suffer significant harm if they’re not moved"
             + " to accommodation provided by you, or on your behalf"),
-    POSSIBLE_HARM_IF_KEPT_IN_CURRENT_ACCOMMODATION("There’s reasonable cause to believe the child is likely to"
+    HARM_IF_KEPT_IN_CURRENT_ACCOMMODATION("There’s reasonable cause to believe the child is likely to"
         + " suffer significant harm if they don’t stay in their current accommodation"),
     URGENT_ACCESS_TO_CHILD_IS_OBSTRUCTED("You’re making enquiries and need urgent access to the child to find out"
         + " about their welfare, and access is being unreasonably refused");
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/OrdersNeededAboutToSubmitCallbackController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/OrdersNeededAboutToSubmitCallbackController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.config.utils.OrderType;
 
 import java.util.List;
 import java.util.Map;
@@ -23,7 +24,6 @@ public class OrdersNeededAboutToSubmitCallbackController {
     @SuppressWarnings("unchecked")
     public AboutToStartOrSubmitCallbackResponse handleAboutToStartEvent(
         @RequestBody CallbackRequest callbackrequest) {
-        final String epo = "EMERGENCY_PROTECTION_ORDER";
         final String showEpoFieldId = "EPO_REASONING_SHOW";
         CaseDetails caseDetails = callbackrequest.getCaseDetails();
         Map<String, Object> data = caseDetails.getData();
@@ -33,7 +33,7 @@ public class OrdersNeededAboutToSubmitCallbackController {
 
         if (orderType.isPresent()) {
             orderType.ifPresent(orderTypes -> {
-                if (orderTypes.contains(epo)) {
+                if (orderTypes.contains(OrderType.EMERGENCY_PROTECTION_ORDER.name())) {
                     data.put(showEpoFieldId, ImmutableList.of("SHOW_FIELD"));
 
                 } else if (data.containsKey(showEpoFieldId)) {
