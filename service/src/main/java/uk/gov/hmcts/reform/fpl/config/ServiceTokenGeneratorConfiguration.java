6195946d2ec674978961e4f3f1f1adf50e1c6eb2
Damian Dunajski <damdun@gmail.com>
2019-05-03 14:59:25 +0100

    Upgrade application dependencies

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/ServiceTokenGeneratorConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/ServiceTokenGeneratorConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.fpl.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -11,7 +10,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
 @Lazy
-@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
