e0b77303f7c323491e3c1b87f9c0df3a879535bd
bridgeth <b.hodgson@kainos.com>
2019-05-27 14:04:29 +0100

    FPLA-57 - Adding E2E tests for gatekeeper role

--- e2e/paths/accessSegregation_test.js
+++ e2e/paths/accessSegregation_test.js
@@ -3,7 +3,7 @@ const config = require('../config.js');
 let caseId;
 let hillingdonCaseId;
 
-Feature('Cases visible only to respective local authority and admin').retry(2);
+Feature('Cases visible only to respective local authority and admin');//.retry(2);
 
 Before(async (I, caseViewPage, submitApplicationPage) => {
   I.logInAndCreateCase(config.swanseaLocalAuthorityEmailUserOne, config.localAuthorityPassword);
@@ -24,30 +24,35 @@ Scenario('Different user in the same local authority can see case created', asyn
   loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('Different user in a different local authority cannot see case created', (I, loginPage) => {
   loginPage.signIn(config.hillingdonLocalAuthorityEmailUserOne, config.localAuthorityPassword);
   I.navigateToCaseDetails(caseId);
   I.seeInCurrentUrl('error');
+  I.signOut();
 });
 
 Scenario('HMCTS admin user can see the case', (I, loginPage) => {
   loginPage.signIn(config.hmctsAdminEmail, config.hmctsAdminPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('CAFCASS user can see the case', (I, loginPage) => {
   loginPage.signIn(config.cafcassEmail, config.cafcassPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('gatekeeper user can see submitted cases', (I, loginPage) => {
   loginPage.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('Gatekeeper can login and see all cases across all courts', (I, loginPage) => {
