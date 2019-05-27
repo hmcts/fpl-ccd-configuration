a0bf52927c0ae8ca3edb7f9874174a36fc20b200
bridgeth <b.hodgson@kainos.com>
2019-05-27 18:19:52 +0100

    FPLA-57 - Adding E2E tests for gatekeeper role

--- e2e/paths/accessSegregation_test.js
+++ e2e/paths/accessSegregation_test.js
@@ -20,39 +20,40 @@ Before(async (I, caseViewPage, submitApplicationPage) => {
   I.signOut();
 });
 
-Scenario('Different user in the same local authority can see case created', async (I, loginPage) => {
-  loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
-  I.navigateToCaseDetails(caseId);
-  I.see(caseId);
+Scenario('Different user in the same local authority can see case created', async (I, loginPage, caseViewPage, submitApplicationPage) => {
+  I.logInAndCreateCase(config.swanseaLocalAuthorityEmailUserOne, config.localAuthorityPassword);
+  let swanseaCaseId = await I.grabTextFrom('.heading-h1');
+  caseViewPage.goToNewActions(config.applicationActions.submitCase);
+  submitApplicationPage.giveConsent();
+  I.continueAndSubmit();
   I.signOut();
+  loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
+  I.navigateToCaseDetails(swanseaCaseId);
+  I.see(swanseaCaseId);
 });
 
 Scenario('Different user in a different local authority cannot see case created', (I, loginPage) => {
   loginPage.signIn(config.hillingdonLocalAuthorityEmailUserOne, config.localAuthorityPassword);
   I.navigateToCaseDetails(caseId);
   I.seeInCurrentUrl('error');
-  I.signOut();
 });
 
 Scenario('HMCTS admin user can see the case', (I, loginPage) => {
   loginPage.signIn(config.hmctsAdminEmail, config.hmctsAdminPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
-  I.signOut();
 });
 
 Scenario('CAFCASS user can see the case', (I, loginPage) => {
   loginPage.signIn(config.cafcassEmail, config.cafcassPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
-  I.signOut();
 });
 
 Scenario('gatekeeper user can see submitted cases', (I, loginPage) => {
   loginPage.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
-  I.signOut();
 });
 
 Scenario('Gatekeeper can login and see all cases across all courts', (I, loginPage) => {
@@ -61,5 +62,4 @@ Scenario('Gatekeeper can login and see all cases across all courts', (I, loginPa
   I.see(caseId);
   I.navigateToCaseDetails(hillingdonCaseId);
   I.see(hillingdonCaseId);
-  I.signOut();
 });
