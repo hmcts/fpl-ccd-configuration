7c81f5e94bc7a00aad3941d62f8b932333c6b215
Damian Dunajski <damdun@gmail.com>
2019-05-30 15:42:42 +0100

    Revert test and remove logging of env variables

--- e2e/paths/accessSegregation_test.js
+++ e2e/paths/accessSegregation_test.js
@@ -1,7 +1,6 @@
 const config = require('../config.js');
 
 let caseId;
-let hillingdonCaseId;
 
 Feature('Cases visible only to respective local authority and admin').retry(2);
 
@@ -12,24 +11,12 @@ Before(async (I, caseViewPage, submitApplicationPage) => {
   submitApplicationPage.giveConsent();
   I.continueAndSubmit();
   I.signOut();
-  I.logInAndCreateCase(config.hillingdonLocalAuthorityEmailUserOne, config.localAuthorityPassword);
-  hillingdonCaseId = await I.grabTextFrom('.heading-h1');
-  caseViewPage.goToNewActions(config.applicationActions.submitCase);
-  submitApplicationPage.giveConsent();
-  I.continueAndSubmit();
-  I.signOut();
 });
 
-Scenario('Different user in the same local authority can see case created', async (I, loginPage, caseViewPage, submitApplicationPage) => {
-  I.logInAndCreateCase(config.swanseaLocalAuthorityEmailUserOne, config.localAuthorityPassword);
-  let swanseaCaseId = await I.grabTextFrom('.heading-h1');
-  caseViewPage.goToNewActions(config.applicationActions.submitCase);
-  submitApplicationPage.giveConsent();
-  I.continueAndSubmit();
-  I.signOut();
+Scenario('Different user in the same local authority can see case created', async (I, loginPage) => {
   loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
-  I.navigateToCaseDetails(swanseaCaseId);
-  I.see(swanseaCaseId);
+  I.navigateToCaseDetails(caseId);
+  I.see(caseId);
 });
 
 Scenario('Different user in a different local authority cannot see case created', (I, loginPage) => {
@@ -50,16 +37,8 @@ Scenario('CAFCASS user can see the case', (I, loginPage) => {
   I.see(caseId);
 });
 
-Scenario('gatekeeper user can see submitted cases', (I, loginPage) => {
+Scenario('Gatekeeper user can see the case', (I, loginPage) => {
   loginPage.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
-});
-
-Scenario('Gatekeeper can login and see all cases across all courts', (I, loginPage) => {
-  loginPage.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
-  I.navigateToCaseDetails(caseId);
-  I.see(caseId);
-  I.navigateToCaseDetails(hillingdonCaseId);
-  I.see(hillingdonCaseId);
 });
