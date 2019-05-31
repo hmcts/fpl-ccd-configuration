a33f2610539be284c3d522df11bedaf7dbbd4034
bridgeth <b.hodgson@kainos.com>
2019-05-31 15:47:09 +0100

    FPLA-57 - Added wiring for CCD UI.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ApplicantController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ApplicantController.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.fpl.controllers;
 
 import com.google.common.collect.ImmutableList;
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.service.MapperService;
 import uk.gov.hmcts.reform.fpl.utils.PBANumberHelper;
 
 import java.util.List;
@@ -23,20 +21,15 @@ public class ApplicantController {
 
     private static final String PBA_NUMBER_CCD_KEY = "pbaNumber";
 
-    private final MapperService mapperService;
-
-    @Autowired
-    public ApplicantController(MapperService mapperService) {
-        this.mapperService = mapperService;
-    }
-
     @PostMapping("/mid-event")
     public AboutToStartOrSubmitCallbackResponse handleMidEvent(@RequestBody CallbackRequest callbackrequest) {
+        System.out.println("START: ENTER APPLICANT: MID EVENT");
         CaseDetails caseDetails = callbackrequest.getCaseDetails();
 
         // if pba number is entered, ensure it always starts with PBA before its validated
         String pbaNumberData = (String) caseDetails.getData().get(PBA_NUMBER_CCD_KEY);
         if (pbaNumberData != null) {
+            System.out.println("PBA NUMBER ENTERED");
             String newPbaNumberData = PBANumberHelper.updatePBANumber(pbaNumberData);
             caseDetails.getData().put(PBA_NUMBER_CCD_KEY, newPbaNumberData);
         }
@@ -53,14 +46,19 @@ public class ApplicantController {
     private List<String> validate(CaseDetails caseDetails) {
         ImmutableList.Builder<String> errors = ImmutableList.builder();
 
+        System.out.println("PBA NUMBER VALIDATION");
         // validate PBA number
         String pbaNumberData = (String) caseDetails.getData().get(PBA_NUMBER_CCD_KEY);
-        List<String> pbaNumberErrors = PBANumberHelper.validatePBANumber(pbaNumberData);
 
-        if (!pbaNumberErrors.isEmpty()) {
-            errors.addAll(pbaNumberErrors.stream().collect(Collectors.toList()));
+        if (pbaNumberData != null) {
+            List<String> pbaNumberErrors = PBANumberHelper.validatePBANumber(pbaNumberData);
+
+            if (!pbaNumberErrors.isEmpty()) {
+                errors.addAll(pbaNumberErrors.stream().collect(Collectors.toList()));
+            }
         }
 
+        System.out.println("END: ENTER APPLICANT: MID EVENT");
         return errors.build();
     }
 
