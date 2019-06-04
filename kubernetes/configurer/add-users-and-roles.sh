be3664b6072d2713e65626627a623771ce9f95bf
sivas23 <sivas23@gmail.com>
2019-06-04 18:28:13 +0100

    added gatekeeper email to nightly pipeline (#302)
    * added gatekeeper email to nightly pipeline
    
    * removed GATEKEEPER_USER_USERNAME environment variable from config and jenkins CNP files

--- kubernetes/configurer/add-users-and-roles.sh
+++ kubernetes/configurer/add-users-and-roles.sh
@@ -12,7 +12,7 @@ ${dir}/utils/idam-create-caseworker.sh sam@hillingdon.gov.uk caseworker,casework
 ${dir}/utils/idam-create-caseworker.sh siva@hillingdon.gov.uk caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor "(local-authority)"
 ${dir}/utils/idam-create-caseworker.sh hmcts-admin@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-courtadmin "(hmcts-admin)"
 ${dir}/utils/idam-create-caseworker.sh cafcass@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-cafcass "(cafcass)"
-${dir}/utils/idam-create-caseworker.sh gatekeeper@example.com caseworker,caseworker-publiclaw,caseworker-publiclaw-gatekeeper "(gatekeeper)"
+${dir}/utils/idam-create-caseworker.sh gatekeeper@mailnesia.com caseworker,caseworker-publiclaw,caseworker-publiclaw-gatekeeper "(gatekeeper)"
 ${dir}/utils/ccd-add-role.sh caseworker-publiclaw-solicitor
 ${dir}/utils/ccd-add-role.sh caseworker-publiclaw-courtadmin
 ${dir}/utils/ccd-add-role.sh caseworker-publiclaw-cafcass
