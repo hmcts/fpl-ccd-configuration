b5fc5009d394a5ca1cc2cbb6eb3a181bb5f89a1f
James Nelson <Jamesnelson117@gmail.com>
2019-06-05 14:27:26 +0100

    FPLA-314: E2E tests updated to reflect other proceedings changes

--- e2e/pages/enterOtherProceedings/enterOtherProceedings.js
+++ e2e/pages/enterOtherProceedings/enterOtherProceedings.js
@@ -1,42 +1,73 @@
 const I = actor();
 
 module.exports = {
-  fields: {
-    onGoingProceeding: {
-      yes: '#proceeding_onGoingProceeding-Yes',
-      no: '#proceeding_onGoingProceeding-No',
-    },
-    proceedingStatus: '#proceeding_proceedingStatus',
-    caseNumber: '#proceeding_caseNumber',
-    started: '#proceeding_started',
-    ended: '#proceeding_ended',
-    ordersMade: '#proceeding_ordersMade',
-    judge: '#proceeding_judge',
-    children: '#proceeding_children',
-    guardian: '#proceeding_guardian',
-    sameGuardianNeeded: {
-      yes: '#proceeding_sameGuardianNeeded-Yes',
-      no: '#proceeding_sameGuardianNeeded-No',
-    },
+  state: {
+    contextPrefix: '',
+  },
+
+  fields: function() {
+    const prefix = this.state.contextPrefix;
+
+    return {
+      onGoingProceeding: {
+        yes: '#proceeding_onGoingProceeding-YES',
+        no: '#proceeding_onGoingProceeding-NO',
+        dontKnow: '#proceeding_onGoingProceeding-DONT_KNOW',
+      },
+      proceedingStatus: {
+        previous: `#proceeding_${prefix}proceedingStatus-Previous`,
+        ongoing: `#proceeding_${prefix}proceedingStatus-Ongoing`,
+      },
+      caseNumber: `#proceeding_${prefix}caseNumber`,
+      started: `#proceeding_${prefix}started`,
+      ended: `#proceeding_${prefix}ended`,
+      ordersMade: `#proceeding_${prefix}ordersMade`,
+      judge: `#proceeding_${prefix}judge`,
+      children: `#proceeding_${prefix}children`,
+      guardian: `#proceeding_${prefix}guardian`,
+      sameGuardianNeeded: {
+        yes: `#proceeding_${prefix}sameGuardianNeeded-Yes`,
+        no: `#proceeding_${prefix}sameGuardianNeeded-No`,
+      },
+    };
+  },
+
+  addProceedingButton: 'Add new',
+
+  addNewProceeding() {
+    if (this.state.contextPrefix === 'additionalProceedings_0_') {
+      throw new Error('Adding more proceedings is not supported in the test');
+    }
+
+    I.click(this.addProceedingButton);
+    this.state.contextPrefix = 'additionalProceedings_0_';
   },
 
   selectYesForProceeding() {
-    I.click(this.fields.onGoingProceeding.yes);
+    I.click(this.fields().onGoingProceeding.yes);
   },
 
   selectNoForProceeding() {
-    I.click(this.fields.onGoingProceeding.no);
+    I.click(this.fields().onGoingProceeding.no);
+  },
+
+  selectOngoingProceedingStatus(status = 'ongoing') {
+    if(status == 'ongoing') {
+      I.click(this.fields().proceedingStatus.ongoing);
+    } else if (status == 'previous') {
+      I.click(this.fields().proceedingStatus.previous);
+    }
   },
 
   enterProceedingInformation(otherProceedingData) {
-    I.selectOption(this.fields.proceedingStatus, otherProceedingData.proceedingStatus);
-    I.fillField(this.fields.caseNumber, otherProceedingData.caseNumber);
-    I.fillField(this.fields.started, otherProceedingData.started);
-    I.fillField(this.fields.ended, otherProceedingData.ended);
-    I.fillField(this.fields.ordersMade, otherProceedingData.ordersMade);
-    I.fillField(this.fields.judge, otherProceedingData.judge);
-    I.fillField(this.fields.children, otherProceedingData.children);
-    I.fillField(this.fields.guardian, otherProceedingData.guardian);
-    I.click(this.fields.sameGuardianNeeded.yes);
+    this.selectOngoingProceedingStatus(otherProceedingData.proceedingStatus);
+    I.fillField(this.fields().caseNumber, otherProceedingData.caseNumber);
+    I.fillField(this.fields().started, otherProceedingData.started);
+    I.fillField(this.fields().ended, otherProceedingData.ended);
+    I.fillField(this.fields().ordersMade, otherProceedingData.ordersMade);
+    I.fillField(this.fields().judge, otherProceedingData.judge);
+    I.fillField(this.fields().children, otherProceedingData.children);
+    I.fillField(this.fields().guardian, otherProceedingData.guardian);
+    I.click(this.fields().sameGuardianNeeded.yes);
   },
 };
