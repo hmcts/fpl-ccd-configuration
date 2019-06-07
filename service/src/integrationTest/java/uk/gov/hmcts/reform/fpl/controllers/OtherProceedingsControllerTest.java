31e7dbd4727ce08d320c75a4087f1fa8b5ab3f31
James Nelson <Jamesnelson117@gmail.com>
2019-06-07 10:33:28 +0100

    FPLA-314: Added error validation on other proceedings to ensure onGoingProceeding is provided

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/OtherProceedingsControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/OtherProceedingsControllerTest.java
@@ -1,4 +1,87 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.model.Proceeding;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("integration-test")
+@WebMvcTest(CaseInitiationController.class)
+@OverrideAutoConfiguration(enabled = true)
+
 public class OtherProceedingsControllerTest {
+    private static final String AUTH_TOKEN = "Bearer token";
+    private static final String USER_ID = "1";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private static final String ERROR_MESSAGE = "You must say if there are any other proceedings relevant to this case";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnWithErrorWhenOnGoingProceedingIsEmptyString() throws Exception {
+        String onGoingProceeding = "";
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = makeRequest(createProceeding(onGoingProceeding));
+
+        assertThat(callbackResponse.getErrors()).contains(ERROR_MESSAGE);
+    }
+
+    @Test
+    void shouldReturnNoErrorsWhenPOnGoingProceedingIsPresent() throws Exception {
+        String onGoingProceeding = "Yes";
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = makeRequest(createProceeding(onGoingProceeding));
+
+        assertThat(callbackResponse.getErrors()).doesNotContain(ERROR_MESSAGE);
+    }
+
+    private Proceeding createProceeding(String onGoingProceeding) {
+        return new Proceeding(onGoingProceeding, "", "", "", "",
+            "", "", "", "", "", "");
+    }
+
+    private AboutToStartOrSubmitCallbackResponse makeRequest(Proceeding proceeding) throws Exception {
+        HashMap<String, Object> map = MAPPER.readValue(MAPPER.writeValueAsString(proceeding),
+            new TypeReference<Map<String, Object>>() {
+            });
+
+        CallbackRequest request = CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder()
+                .id(12345L)
+                .data(ImmutableMap.<String, Object>builder().put("proceeding", map).build())
+                .build())
+            .build();
+
+        MvcResult response = mockMvc
+            .perform(post("/callback/enter-other-proceedings/mid-event")
+                .header("authorization", AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(MAPPER.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andReturn();
+
+        return MAPPER.readValue(response.getResponse()
+            .getContentAsByteArray(), AboutToStartOrSubmitCallbackResponse.class);
+    }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/OtherProceedingsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/OtherProceedingsController.java
@@ -1,4 +1,44 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Api
+@RestController
+@RequestMapping("callback/enter-other-proceedings")
 public class OtherProceedingsController {
+
+    private static final String ERROR_MESSAGE = "You must say if there are any other proceedings relevant to this case";
+
+    @Autowired
+
+    @SuppressWarnings("unchecked")
+    @PostMapping("/mid-event")
+    public AboutToStartOrSubmitCallbackResponse handleMidEvent(@RequestBody CallbackRequest callbackrequest) {
+        CaseDetails caseDetails = callbackrequest.getCaseDetails();
+
+        Map<String, Object> proceedingData = (Map<String, Object>) caseDetails.getData().get("proceeding");
+        List<String> validationErrors = new ArrayList<String>();
+        String onGoingProceeding = (String)proceedingData.get("onGoingProceeding");
+
+        if (onGoingProceeding == null || "".equals(onGoingProceeding)) {
+            validationErrors.add(ERROR_MESSAGE);
+        }
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDetails.getData())
+            .errors(validationErrors)
+            .build();
+    }
 }
