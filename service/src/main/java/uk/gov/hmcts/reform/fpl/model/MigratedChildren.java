dff520d4557c5d1d43cb71c7f2d9c6038f3a3a12
swalker125 <sam.walker@digit-hull.com>
2019-07-03 15:36:36 +0100

    added JsonCreator annotation to MigratedChilren Object

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/MigratedChildren.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/MigratedChildren.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.reform.fpl.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class MigratedChildren {
     private final ChildParty party;
+
+    @JsonCreator
+    private MigratedChildren(@JsonProperty("party") ChildParty party) {
+        this.party = party;
+    }
 }
