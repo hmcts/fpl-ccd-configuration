010fbe893833312c6ae047a392dab61e101026d1
bridgeth <b.hodgson@kainos.com>
2019-07-05 11:42:10 +0100

    FPLA-384 - Added unit test for Migrated Hearing Service

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/HearingMigrationServiceTest.java
@@ -0,0 +1,54 @@
+package uk.gov.hmcts.reform.fpl.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class HearingMigrationServiceTest {
+
+    @InjectMocks
+    private HearingMigrationService classUnderTest;
+
+    @Test
+    void shouldSetMigratedHearingToYesWhenNoHearingDataPresent() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(createData("data", "some data"))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse response = classUnderTest.setMigratedValue(caseDetails);
+        assertThat(response.getData()).containsEntry("hearingMigrated", "Yes");
+    }
+
+    @Test
+    void shouldSetMigratedHearingToYesWhenHearing1Exists() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(createData("Hearing1", "some value"))
+            .build();
+        AboutToStartOrSubmitCallbackResponse response = classUnderTest.setMigratedValue(caseDetails);
+        assertThat(response.getData()).containsEntry("hearingMigrated", "Yes");
+    }
+
+    @Test
+    void shouldSetMigratedHearingToNoWhenOldHearingExists() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(createData("hearing", "some value"))
+            .build();
+        AboutToStartOrSubmitCallbackResponse response = classUnderTest.setMigratedValue(caseDetails);
+        assertThat(response.getData()).containsEntry("hearingMigrated", "No");
+    }
+
+    private Map<String, Object> createData(String key, String value) {
+        Map<String, Object> data = new HashMap<>();
+        data.put(key, value);
+        return data;
+    }
+}
