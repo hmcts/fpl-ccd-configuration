5c9905f7dcad3b7490562b22bf1d8f026448bf3f
James Nelson <Jamesnelson117@gmail.com>
2019-07-18 11:01:54 +0100

    FPLA-387: Removed JsonCreator annoations from child party models

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/EmailAddress.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/EmailAddress.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.model.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +8,7 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EmailAddress {
     private final String email;
     private final String emailUsageType;
--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/Party.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/Party.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.model.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import uk.gov.hmcts.reform.fpl.model.Address;
@@ -15,6 +16,7 @@ public class Party {
     public final String firstName;
     public final String lastName;
     public final String organisationName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     public final Date dateOfBirth;
     public final Address address;
     public final EmailAddress email;
