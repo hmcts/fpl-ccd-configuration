83196364d896cdc9e624b72fb2d9ad1954fcd6fd
Damian Dunajski <damdun@gmail.com>
2019-08-15 12:49:27 +0100

    Configure CCD in preview pipeline

--- bin/configurer/utils/ccd-import-definition.sh
+++ bin/configurer/utils/ccd-import-definition.sh
@@ -5,7 +5,7 @@ set -eu
 dir=$(dirname ${0})
 
 userToken=$(${dir}/idam-lease-user-token.sh 1 ccd-import)
-serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_CONFIGURER_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
+serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_DEFINITION_STORE_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
 
 response=$(curl -k --silent --show-error -X POST \
   ${CCD_DEFINITION_STORE_API_BASE_URL:-http://localhost:4451}/import \
--- bin/configurer/utils/idam-lease-user-token.sh
+++ bin/configurer/utils/idam-lease-user-token.sh
@@ -13,11 +13,10 @@ if [[ "${IDAM_VERSION:-tactical}" == 'tactical' ]]; then
 else
   importerUsername=${CCD_CONFIGURER_IMPORTER_USERNAME}
   importerPassword=${CCD_CONFIGURER_IMPORTER_PASSWORD}
-  clientId=${CCD_CONFIGURER_IDAM_CLIENT_ID}
-  clientSecret=${CCD_CONFIGURER_IDAM_CLIENT_SECRET}
+  clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET}
   redirectUri=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect
 
-  code=$(curl -k --fail --show-error --silent -X POST -u "${importerUsername}:${importerPassword}" "${IDAM_API_BASE_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=${clientId}" -d "" | jq -r .code)
+  code=$(curl -k --fail --show-error --silent -X POST -u "${importerUsername}:${importerPassword}" "${IDAM_API_BASE_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | jq -r .code)
 
-  curl -k --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" -u "${clientId}:${clientSecret}" "${IDAM_API_BASE_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | jq -r .access_token
+  curl -k --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" -u "ccd_gateway:${clientSecret}" "${IDAM_API_BASE_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | jq -r .access_token
 fi
