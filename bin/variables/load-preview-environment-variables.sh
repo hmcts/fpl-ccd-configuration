8c28e0bc231c1655d68b5b41c69cd4336d53c743
Damian Dunajski <damdun@gmail.com>
2019-08-15 14:03:53 +0100

    Set URL in gradle

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -10,7 +10,6 @@ echo 'export IDAM_VERSION=strategic'
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=https://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://definition-store-api-fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
-echo "export URL=https://case-management-web-fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
