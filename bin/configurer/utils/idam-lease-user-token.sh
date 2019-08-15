c0ef15768f141c3059d0980435739d60532a514c
Damian Dunajski <damdun@gmail.com>
2019-08-15 13:25:56 +0100

    Move all preview variables to one file and use docker to run jq command

--- bin/configurer/utils/idam-lease-user-token.sh
+++ bin/configurer/utils/idam-lease-user-token.sh
@@ -16,7 +16,7 @@ else
   clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET}
   redirectUri=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect
 
-  code=$(curl -k --fail --show-error --silent -X POST -u "${importerUsername}:${importerPassword}" "${IDAM_API_BASE_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | jq -r .code)
+  code=$(curl -k --fail --show-error --silent -X POST -u "${importerUsername}:${importerPassword}" "${IDAM_API_BASE_URL}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive stedolan/jq -r .code)
 
-  curl -k --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" -u "ccd_gateway:${clientSecret}" "${IDAM_API_BASE_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | jq -r .access_token
+  curl -k --fail --show-error --silent -X POST -H "Content-Type: application/x-www-form-urlencoded" -u "ccd_gateway:${clientSecret}" "${IDAM_API_BASE_URL}/oauth2/token?code=${code}&redirect_uri=${redirectUri}&grant_type=authorization_code" -d "" | docker run --rm --interactive stedolan/jq -r .access_token
 fi
