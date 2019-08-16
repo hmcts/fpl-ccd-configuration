5d8aa49bebc0d70bfbb8cbb58632d3d82087dc20
James Nelson <Jamesnelson117@gmail.com>
2019-08-16 09:22:37 +0100

    FPLA-295: Reverted case submission controller about to start

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/CaseSubmissionControllerAboutToStartTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/CaseSubmissionControllerAboutToStartTest.java
@@ -20,7 +20,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.reform.fpl.utils.ResourceReader.readBytes;
 
 @ActiveProfiles("integration-test")
 @WebMvcTest(CaseSubmissionController.class)
@@ -60,57 +59,5 @@ class CaseSubmissionControllerAboutToStartTest {
             .containsEntry("caseName", "title")
             .containsEntry("submissionConsentLabel",
                 "I, Emma Taylor, believe that the facts stated in this application are true.");
-    }
-
-    @Test
-    void shouldReturnErrorsWhenNoCaseDataIsProvided() throws Exception {
-        CallbackRequest request = CallbackRequest.builder().caseDetails(CaseDetails.builder()
-            .data(ImmutableMap.<String, Object>builder()
-                .put("caseName", "title")
-                .build()).build())
-            .build();
-
-        MvcResult response = mockMvc
-            .perform(post("/callback/case-submission/about-to-start")
-                .header("authorization", AUTH_TOKEN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(MAPPER.writeValueAsString(request)))
-            .andExpect(status().isOk())
-            .andReturn();
-
-        AboutToStartOrSubmitCallbackResponse callbackResponse = MAPPER.readValue(response.getResponse()
-            .getContentAsByteArray(), AboutToStartOrSubmitCallbackResponse.class);
-
-        assertThat(callbackResponse.getErrors()).containsOnly("In the orders section:",
-            "- Select at least one type of order",
-            "In the children section:",
-            "- You need to add details to children",
-            "In the applicant section:",
-            "- You need to add details to applicant",
-            "In the hearing section:",
-            "- You need to add details to hearing",
-            "In the documents section:",
-            "- Tell us the status of all documents including those that you haven't uploaded"
-        );
-    }
-
-    @Test
-    void shouldReturnNoErrorsWhenMandatoryFieldsAreProvidedInCaseData() throws Exception {
-        AboutToStartOrSubmitCallbackResponse response =
-            makeSubmitCaseRequest("core-case-data-store-api/callback-request.json");
-        assertThat(response.getErrors()).isEmpty();
-    }
-
-    private AboutToStartOrSubmitCallbackResponse makeSubmitCaseRequest(String resourcePath) throws Exception {
-        MvcResult response =  mockMvc
-            .perform(post("/callback/case-submission/about-to-start")
-                .header("authorization", AUTH_TOKEN)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(readBytes(resourcePath)))
-            .andExpect(status().isOk())
-            .andReturn();
-
-        return MAPPER.readValue(response.getResponse()
-            .getContentAsByteArray(), AboutToStartOrSubmitCallbackResponse.class);
     }
 }
