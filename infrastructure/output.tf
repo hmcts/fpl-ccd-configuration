0df3f737da7f4025ffac586c2d1ee5162881a157
Damian Dunajski <damdun@gmail.com>
2019-08-19 12:38:35 +0100

    Remove vault from Terraform outputs

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
 output "microserviceName" {
   value = "${var.component}"
 }
-
-output "vaultName" {
-  value = "${local.vault_name}"
-}
-
-output "vaultUri" {
-  value = "${module.key-vault.key_vault_uri}"
-}
