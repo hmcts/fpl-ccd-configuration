e8a41beb36beca49ee56f6450d051da7b7aefdbb
Damian Dunajski <damdun@gmail.com>
2019-08-23 14:38:45 +0100

    Move draft delete test

--- e2e/tests/localAuthorityDeletesApplication_test.js
+++ e2e/tests/localAuthorityDeletesApplication_test.js
@@ -1,12 +1,12 @@
-const config = require('../../config.js');
+const config = require('../config.js');
 
-Feature('Delete Case').retry(2);
+Feature('Application draft');
 
 Before((I) => {
   I.logInAndCreateCase(config.swanseaLocalAuthorityEmailUserOne, config.localAuthorityPassword);
 });
 
-Scenario('Can delete an application and not have actions within it anymore', (I, caseViewPage, deleteApplicationEventPage) => {
+Scenario('local authority deletes application', (I, caseViewPage, deleteApplicationEventPage) => {
   caseViewPage.goToNewActions(config.applicationActions.deleteApplication);
   deleteApplicationEventPage.tickDeletionConsent();
   I.click('Continue');
