72557a0cd646d646921a8cc258ab2d11f2dba608
Damian Dunajski <damdun@gmail.com>
2019-08-27 12:48:11 +0100

    Add sign out step in access segregation config

--- e2e/tests/accessSegregation_test.js
+++ e2e/tests/accessSegregation_test.js
@@ -15,7 +15,6 @@ Before(async (I, caseViewPage, submitApplicationEventPage) => {
     caseId = await I.grabTextFrom('.heading-h1');
     console.log(`Case ${caseId} has been submitted`);
 
-    I.wait(2); // in seconds; time needed for access grant calls to complete
     I.signOut();
   }
 });
@@ -24,34 +23,40 @@ Scenario('Different user in the same local authority can see case created', asyn
   loginPage.signIn(config.swanseaLocalAuthorityEmailUserTwo, config.localAuthorityPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('Different user in a different local authority cannot see case created', (I, loginPage) => {
   loginPage.signIn(config.hillingdonLocalAuthorityEmailUserOne, config.localAuthorityPassword);
   I.navigateToCaseDetails(caseId);
   I.seeInCurrentUrl('error');
+  I.signOut();
 });
 
 Scenario('HMCTS admin user can see the case', (I, loginPage) => {
   loginPage.signIn(config.hmctsAdminEmail, config.hmctsAdminPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('CAFCASS user can see the case', (I, loginPage) => {
   loginPage.signIn(config.cafcassEmail, config.cafcassPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('Gatekeeper user can see the case', (I, loginPage) => {
   loginPage.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
 
 Scenario('Judiciary user can see the case', (I, loginPage) => {
   loginPage.signIn(config.judiciaryEmail, config.judiciaryPassword);
   I.navigateToCaseDetails(caseId);
   I.see(caseId);
+  I.signOut();
 });
