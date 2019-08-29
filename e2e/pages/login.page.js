37acaf4e0a9c23917153867bbb58a068de62445e
Damian Dunajski <damian.dunajski@hmcts.net>
2019-08-29 16:29:59 +0100

    Refactor E2E tests and make tests more stable (#417)

--- e2e/pages/login.page.js
+++ e2e/pages/login.page.js
@@ -1,5 +1,3 @@
-/*global process*/
-
 const I = actor();
 
 module.exports = {
@@ -11,7 +9,6 @@ module.exports = {
   submitButton: 'input[value="Sign in"]',
 
   signIn(username, password) {
-    I.amOnPage(process.env.URL || 'http://localhost:3451');
     I.waitForElement(this.submitButton);
     I.fillField(this.fields.username, username);
     I.fillField(this.fields.password, password);
