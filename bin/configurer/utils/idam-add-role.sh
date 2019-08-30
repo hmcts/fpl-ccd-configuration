fc1ab427db88c22b19571c3ca6cdb0f2b4b26739
Adam Komisarek <adam.komisarek@hmcts.net>
2019-08-30 15:04:40 +0100

    Removed indendation

--- bin/configurer/utils/idam-add-role.sh
+++ bin/configurer/utils/idam-add-role.sh
@@ -2,30 +2,32 @@
 
 set -eu
 
-if [ "${ENVIRONMENT:-local}" = "local" ]; then
-  dir=$(dirname ${0})
+if [ "${ENVIRONMENT:-local}" != "local" ]; then
+  exit 0;
+fi
 
-  ID=${1}
+dir=$(dirname ${0})
 
-  apiToken=$(${dir}/idam-authenticate.sh "${IDAM_ADMIN_USER}" "${IDAM_ADMIN_PASSWORD}")
+ID=${1}
 
-  echo -e "\nCreating IDAM role: ${ID}"
+apiToken=$(${dir}/idam-authenticate.sh "${IDAM_ADMIN_USER}" "${IDAM_ADMIN_PASSWORD}")
 
-  STATUS=$(curl --silent --output /dev/null --write-out '%{http_code}' -H 'Content-Type: application/json' -H "Authorization: AdminApiAuthToken ${apiToken}" \
-    ${IDAM_API_BASE_URL:-http://localhost:5000}/roles -d '{
-    "id": "'${ID}'",
-    "name": "'${ID}'",
-    "description": "'${ID}'",
-    "assignableRoles": [ ],
-    "conflictingRoles": [ ]
-  }')
+echo -e "\nCreating IDAM role: ${ID}"
 
-  if [ $STATUS -eq 201 ]; then
-    echo "Role created sucessfully"
-  elif [ $STATUS -eq 409 ]; then
-    echo "Role already exists!"
-  else
-    echo "ERROR: HTTPCODE = $STATUS"
-    exit 1
-  fi
+STATUS=$(curl --silent --output /dev/null --write-out '%{http_code}' -H 'Content-Type: application/json' -H "Authorization: AdminApiAuthToken ${apiToken}" \
+  ${IDAM_API_BASE_URL:-http://localhost:5000}/roles -d '{
+  "id": "'${ID}'",
+  "name": "'${ID}'",
+  "description": "'${ID}'",
+  "assignableRoles": [ ],
+  "conflictingRoles": [ ]
+}')
+
+if [ $STATUS -eq 201 ]; then
+  echo "Role created sucessfully"
+elif [ $STATUS -eq 409 ]; then
+  echo "Role already exists!"
+else
+  echo "ERROR: HTTPCODE = $STATUS"
+  exit 1
 fi
