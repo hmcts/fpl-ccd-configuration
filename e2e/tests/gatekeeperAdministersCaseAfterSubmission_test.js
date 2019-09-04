13ae3dd4921f48221df6c96cba0b2560d9783d83
Becky Brennan <rebeccab@kainos.com>
2019-09-04 07:13:10 +0100

    adding retry as venue sometimes not entered fully

--- e2e/tests/gatekeeperAdministersCaseAfterSubmission_test.js
+++ e2e/tests/gatekeeperAdministersCaseAfterSubmission_test.js
@@ -3,7 +3,7 @@ const hearingDetails = require('../fixtures/hearingTypeDetails.js');
 
 let caseId;
 
-Feature('Gatekeeper Case administration after submission');
+Feature('Gatekeeper Case administration after submission').retry(2);
 
 Before(async (I, caseViewPage, submitApplicationEventPage) => {
   if (!caseId) {
