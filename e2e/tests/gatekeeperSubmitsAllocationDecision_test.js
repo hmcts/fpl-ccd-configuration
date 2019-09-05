2b26130329149939064fc03e4d9988072b9101f1
Toireasa Moley <toireasa.moley@hmcts.net>
2019-09-05 15:58:14 +0100

    updating e2e test

--- e2e/tests/gatekeeperSubmitsAllocationDecision_test.js
+++ e2e/tests/gatekeeperSubmitsAllocationDecision_test.js
@@ -1,21 +1,25 @@
 const config = require('../config.js');
 
 let caseId;
-caseId = '1567521062611916';
 
 Feature('Application draft (populated draft)');
 
-Before(async (I) => {
+Before(async (I, caseViewPage, submitApplicationEventPage) => {
   if (!caseId) {
-    await I.logInAndCreateCase(config.gateKeeperEmail, config.gateKeeperPassword);
+    await I.logInAndCreateCase(config.swanseaLocalAuthorityEmailUserOne, config.localAuthorityPassword);
+    await I.enterMandatoryFields();
+    caseViewPage.goToNewActions(config.applicationActions.submitCase);
+    submitApplicationEventPage.giveConsent();
+    I.continueAndSubmit();
 
     // eslint-disable-next-line require-atomic-updates
     caseId = await I.grabTextFrom('.heading-h1');
-    console.log(`Application draft ${caseId} has been created`);
-  } else {
+    console.log(`Case ${caseId} has been submitted`);
+
+    I.signOut();
     await I.signIn(config.gateKeeperEmail, config.gateKeeperPassword);
-    await I.navigateToCaseDetails(caseId);
   }
+  await I.navigateToCaseDetails(caseId);
 });
 
 Scenario('gatekeeper enters allocation decision', (I, caseViewPage, enterAllocationDecisionEventPage) => {
