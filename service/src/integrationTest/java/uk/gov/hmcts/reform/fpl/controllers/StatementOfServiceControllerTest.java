441e7dd46d8188c8d78a4a6c5016b9053dc558d5
Becky Brennan <rebeccab@kainos.com>
2019-09-09 14:37:23 +0100

    controller test mocking user details service

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/StatementOfServiceControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/StatementOfServiceControllerTest.java
@@ -2,10 +2,12 @@ package uk.gov.hmcts.reform.fpl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,8 +15,11 @@ import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.service.UserDetailsService;
+import uk.gov.service.notify.NotificationClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,8 +31,16 @@ class StatementOfServiceControllerTest {
     private static final String AUTH_TOKEN = "Bearer token";
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
+    @MockBean
+    private UserDetailsService userDetailsService;
+
     @Autowired
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void mockUserNameRetrieval() {
+        given(userDetailsService.getUserName(AUTH_TOKEN)).willReturn("Emma Taylor");
+    }
 
     @Test
     void shouldPrepopulateRecipient() throws Exception {
