527d154d9c07ab839910164a83fc02482283cabe
James Nelson <Jamesnelson117@gmail.com>
2019-09-12 15:01:07 +0100

    FPLA-634: Forced date generator service to use UK date locale

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/DateFormatterService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/DateFormatterService.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Locale;
 
 @Service
 public class DateFormatterService {
     public String formatLocalDateToString(LocalDate date, FormatStyle style) {
-        return date.format(DateTimeFormatter.ofLocalizedDate(style));
+        return date.format(DateTimeFormatter.ofLocalizedDate(style).localizedBy(Locale.UK));
     }
 }
