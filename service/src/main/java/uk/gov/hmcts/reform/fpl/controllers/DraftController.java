ed0b116054658aa64a02c161b6946a0a822062a9
swalker125 <sam.walker@digit-hull.com>
2019-09-25 11:00:28 +0100

    directions populated on send to gatekeeper event
    C permissions removed on allParties collection for gatekeeper

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/DraftController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/DraftController.java
@@ -21,12 +21,9 @@ import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.Order;
 import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.configuration.OrderDefinition;
 import uk.gov.hmcts.reform.fpl.service.DocmosisDocumentGeneratorService;
-import uk.gov.hmcts.reform.fpl.service.OrdersLookupService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -36,8 +33,6 @@ import java.util.Map;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-import static java.util.UUID.randomUUID;
-import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 import static org.apache.commons.lang3.StringUtils.isBlank;
@@ -49,65 +44,16 @@ public class DraftController {
     private final ObjectMapper mapper;
     private final DocmosisDocumentGeneratorService documentGeneratorService;
     private final UploadDocumentService uploadDocumentService;
-    private final OrdersLookupService ordersLookupService;
 
     //TODO: show hide entire direction for Is this direction needed?
 
     @Autowired
     public DraftController(ObjectMapper mapper,
                            DocmosisDocumentGeneratorService documentGeneratorService,
-                           UploadDocumentService uploadDocumentService,
-                           OrdersLookupService ordersLookupService) {
+                           UploadDocumentService uploadDocumentService) {
         this.mapper = mapper;
         this.documentGeneratorService = documentGeneratorService;
         this.uploadDocumentService = uploadDocumentService;
-        this.ordersLookupService = ordersLookupService;
-    }
-
-    private String booleanToYesOrNo(boolean value) {
-        return value ? "Yes" : "No";
-    }
-
-    @PostMapping("/about-to-start")
-    public AboutToStartOrSubmitCallbackResponse handleAboutToStart(
-        @RequestBody CallbackRequest callbackrequest) throws IOException {
-        CaseDetails caseDetails = callbackrequest.getCaseDetails();
-        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
-
-        //pre populate all standard directions
-        if (caseData.getAllParties() == null) {
-            OrderDefinition standardDirectionOrder = ordersLookupService.getStandardDirectionOrder();
-
-            Map<String, List<Element<Direction>>> directions = standardDirectionOrder.getDirections()
-                .stream()
-                .map(direction ->
-                    Element.<Direction>builder()
-                        .id(randomUUID())
-                        .value(Direction.builder()
-                            .type(direction.getTitle())
-                            .text(direction.getText())
-                            .assignee(direction.getAssignee())
-                            .readOnly(booleanToYesOrNo(direction.getDisplay().isShowDateOnly()))
-                            .build())
-                        .build()
-                )
-                .collect(groupingBy(element -> element.getValue().getAssignee()));
-
-            directions.forEach((key, value) -> caseDetails.getData().put(key, value));
-        } else {
-
-            // overwrites role collections with values from order object
-            //TODO: Yes / No is not accurate for each direction
-            Map<String, List<Element<Direction>>> directions = caseData.getStandardDirectionOrder().getDirections()
-                .stream()
-                .collect(groupingBy(directionElement -> directionElement.getValue().getAssignee()));
-
-            directions.forEach((key, value) -> caseDetails.getData().put(key, value));
-        }
-
-        return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(caseDetails.getData())
-            .build();
     }
 
     @PostMapping("/mid-event")
@@ -156,31 +102,16 @@ public class DraftController {
         CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
 
         List<Element<Direction>> directions = new ArrayList<>();
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getAllParties()));
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getCourtDirections()));
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getLocalAuthorityDirections()));
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getCafcassDirections()));
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getOtherPartiesDirections()));
-        directions.addAll(addReadOnlyValuesToDirections(caseData.getParentsAndRespondentsDirections()));
+        directions.addAll(caseData.getAllParties());
+        directions.addAll(caseData.getCourtDirections());
+        directions.addAll(caseData.getLocalAuthorityDirections());
+        directions.addAll(caseData.getCafcassDirections());
+        directions.addAll(caseData.getOtherPartiesDirections());
+        directions.addAll(caseData.getParentsAndRespondentsDirections());
 
         caseDetails.getData().put("standardDirectionOrder", Order.builder().directions(directions).build());
-        caseDetails.getData().put("allParties", addReadOnlyValuesToDirections(caseData.getAllParties()));
 
         return caseDetails;
-    }
-
-
-    //TODO: need better way of adding Yes / No
-    private List<Element<Direction>> addReadOnlyValuesToDirections(List<Element<Direction>> directions) {
-        directions.forEach(direction -> {
-            if (direction.getValue().getType().equals("Mandatory order title")) {
-                direction.getValue().setReadOnly("Yes");
-            } else {
-                direction.getValue().setReadOnly("No");
-            }
-        });
-
-        return directions;
     }
 
     @SuppressWarnings("LineLength")
