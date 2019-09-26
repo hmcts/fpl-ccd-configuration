494db487c8f8a9cf9628f3a995f49c5ba0fd4a6f
Toireasa Moley <toireasa.moley@hmcts.net>
2019-09-26 13:29:59 +0100

    Updating script

--- bin/configurer/utils/get-user-ids.sh
+++ bin/configurer/utils/get-user-ids.sh
@@ -11,10 +11,12 @@ export PGPASSWORD=openidm
 
 userids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') from managedObjects WHERE fullobject->>'sn' = '(local-authority)';" openidm)
 
-echo user ids are: $userids
+useridswithoutwhitespace=$(echo $userids | tr -d ' ')
 
-sed -i '' "s/31,32,33/$userids/" "../../../service/src/main/resources/application.yaml"
-sed -i '' "s/31,32,33/$userids/" "../../../build/resources/main/application.yaml"
+echo user ids are $useridswithoutwhitespace
+
+sed -i '' "s/31,32,33/$useridswithoutwhitespace/" "../../../service/src/main/resources/application.yaml"
+sed -i '' "s/31,32,33/$useridswithoutwhitespace/" "../../../build/resources/main/application.yaml"
 
 echo test script finished
 
