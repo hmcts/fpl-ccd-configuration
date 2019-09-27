af0ccd58ff063c2f13310164777a37887ffa86d2
Damian Dunajski <damdun@gmail.com>
2019-09-27 14:06:27 +0200

    Increase wait for timeout to 10s

--- README.md
+++ README.md
@@ -86,4 +86,3 @@ See [fpl-service](service/README.md) for more information.
 
 ## License
 This project is licensed under the MIT License - see the [LICENSE](LICENSE.md) file for details.
-
--- codecept.conf.js
+++ codecept.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     Puppeteer: {
       show: process.env.SHOW_BROWSER_WINDOW || false,
       restart: false,
-      waitForTimeout: 7500,
+      waitForTimeout: 10000,
       chrome: {
         ignoreHTTPSErrors: true,
         args: process.env.PROXY_SERVER ? [
