5f5c18effe234ef82440a0454b2b90e662fa1fa7
Toireasa Moley <toireasa.moley@hmcts.net>
2019-09-27 14:31:51 +0100

    Updating script to create yaml file

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -2,8 +2,18 @@
 
 set -eu
 
+SPRING_PROFILES=local,user-mappings
+
 userids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') from managedObjects WHERE fullobject->>'sn' = '(local-authority)';" openidm)
 
 useridsformatted=$(echo $userids | tr -d ' ')
 
-sed -i '' "s/31,32,33/$useridsformatted/" "../../../service/src/main/resources/application-user-mappings.yaml"
+cd ../../../service/src/main/resources
+cat > application-user-mappings.yaml <<EOL
+spring:
+  profiles: ${SPRING_PROFILES}
+
+fpl:
+  local_authority_user:
+    mapping: 'FPLA=>0;SA=>${useridsformatted};HN=>34,35'
+EOL
