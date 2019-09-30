8c76a3f1bd29da3ea6f506fe8f8e7e8e2dd4e51f
Toireasa Moley <toireasa.moley@hmcts.net>
2019-09-30 12:17:00 +0100

    snake case and directory refactor

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -2,18 +2,22 @@
 
 set -eu
 
-swanseauserids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%swansea%';" openidm)
-hillingdonuserids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%hillingdon%';" openidm)
+dir=$(dirname ${0})
+root_dir=$(realpath ${dir}/../../..)
+resources_dir=${root_dir}/service/src/main/resources
 
-swanseauseridsformatted=$(echo $swanseauserids | tr -d ' ')
-hillingdonuseridsformatted=$(echo $hillingdonuserids | tr -d ' ')
+swansea_user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%swansea%';" openidm)
+hillingdon_user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%hillingdon%';" openidm)
 
-cd ../../../service/src/main/resources
+swansea_user_ids_formatted=$(echo $swansea_user_ids | tr -d ' ')
+hillingdon_user_ids_formatted=$(echo $hillingdon_user_ids | tr -d ' ')
+
+cd $resources_dir
 cat > application-user-mappings.yaml <<EOL
 spring:
   profiles: user-mappings
 
 fpl:
   local_authority_user:
-    mapping: 'FPLA=>0;SA=>${swanseauseridsformatted};HN=>${hillingdonuseridsformatted}'
+    mapping: 'FPLA=>0;SA=>${swansea_user_ids_formatted};HN=>${hillingdon_user_ids_formatted}'
 EOL
