87523a0c2c62facb6a20fb7dba2fdea05ed08631
Damian Dunajski <damdun@gmail.com>
2019-09-30 13:28:01 +0100

    Refactor script

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -6,16 +6,12 @@ dir=$(dirname ${0})
 root_dir=$(realpath ${dir}/../../..)
 resources_dir=${root_dir}/service/src/main/resources
 
-function GetUserIds()
-{
-    user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';" openidm)
+function get_user_ids() {
+  docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';" openidm
 }
 
-GetUserIds swansea
-swansea_user_ids_formatted=$(echo ${user_ids} | tr -d ' ')
-
-GetUserIds hillingdon
-hillingdon_user_ids_formatted=$(echo ${user_ids} | tr -d ' ')
+swansea_user_ids=$(echo $(get_user_ids swansea) | tr -d ' ')
+hillingdon_user_ids=$(echo $(get_user_ids hillingdon) | tr -d ' ')
 
 cd $resources_dir
 cat > application-user-mappings.yaml <<EOL
@@ -24,5 +20,5 @@ spring:
 
 fpl:
   local_authority_user:
-    mapping: 'FPLA=>0;SA=>${swansea_user_ids_formatted};HN=>${hillingdon_user_ids_formatted}'
+    mapping: 'FPLA=>0;SA=>${swansea_user_ids};HN=>${hillingdon_user_ids}'
 EOL
