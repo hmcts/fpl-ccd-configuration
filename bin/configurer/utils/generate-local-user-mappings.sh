86fc6c5f6404e7505e0c164f4ee21d2c6959718e
Toireasa Moley <toireasa.moley@hmcts.net>
2019-09-30 13:05:05 +0100

    Changes to script

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -6,11 +6,16 @@ dir=$(dirname ${0})
 root_dir=$(realpath ${dir}/../../..)
 resources_dir=${root_dir}/service/src/main/resources
 
-swansea_user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%swansea%';" openidm)
-hillingdon_user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%hillingdon%';" openidm)
+function GetUserIds()
+{
+    user_ids=$(docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';" openidm)
+}
 
-swansea_user_ids_formatted=$(echo $swansea_user_ids | tr -d ' ')
-hillingdon_user_ids_formatted=$(echo $hillingdon_user_ids | tr -d ' ')
+GetUserIds swansea
+swansea_user_ids_formatted=$(echo $user_ids | tr -d ' ')
+
+GetUserIds hillingdon
+hillingdon_user_ids_formatted=$(echo $user_ids | tr -d ' ')
 
 cd $resources_dir
 cat > application-user-mappings.yaml <<EOL
@@ -21,3 +26,6 @@ fpl:
   local_authority_user:
     mapping: 'FPLA=>0;SA=>${swansea_user_ids_formatted};HN=>${hillingdon_user_ids_formatted}'
 EOL
+
+
+
