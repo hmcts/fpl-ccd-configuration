bff4e091be0a470afe950b6b7a5e2c73b6775355
Lewis Birks <lewisb@kainos.com>
2019-10-03 16:40:30 +0100

    Updated create caseworker script to update if a users roles change

--- bin/configurer/utils/idam-create-caseworker.sh
+++ bin/configurer/utils/idam-create-caseworker.sh
@@ -1,16 +1,86 @@
 #!/usr/bin/env bash
 
+######################
+## FUNCTIONS
+######################
+
+get_user_id_and_role() {
+  docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db --username openidm --tuples-only --command "SELECT data.roles FROM managedObjects mo, LATERAL (SELECT regexp_replace(string_agg((element::json -> '_ref')::text, ','), '( *\\w*\\/)|(\")', '', 'g') AS roles FROM json_array_elements_text(mo.fullobject->'effectiveRoles') as data(element)) data WHERE mo.fullobject ->> 'userName'='${1}';" openidm
+}
+
+create_user_request() {
+  response=$(
+    curl --insecure --show-error --silent --output /dev/null --write-out "%{http_code}" -X POST \
+      "${IDAM_API_BASE_URL:-http://localhost:5000}"/testing-support/accounts \
+      -H "Content-Type: application/json" \
+      -d '{
+          "email":"'"${email}"'",
+          "forename":"'"${email}"'",
+          "surname":"'"${surname}"'",
+          "password":"Password12",
+          "levelOfAccess":1,
+          "roles": [
+            '"${rolesJson}"'
+          ],
+          "userGroup": {"code": "caseworker"}}
+        '
+  )
+
+  echo "$response"
+}
+
+delete_user_request() {
+  response=$(curl --insecure --show-error --silent --output /dev/null --write-out "%{http_code}" -X DELETE \
+    "${IDAM_API_BASE_URL:-http://localhost:5000}"/testing-support/accounts/"${email}")
+  echo "$response"
+}
+
+recreate_user() {
+  echo -e "Checking IDAM user: ${email}"
+  _roles=$(echo $(get_user_id_and_role "${email}") | tr -d [:space:]) # Remove whitespace
+  if [[ "$_roles" == "$rolesStr" ]]; then
+    echo -e "Roles equal\nNo need to delete"
+    exit 0 # Nothing to delete and user already exists so just exit
+  else
+    echo -e "Deleting IDAM user: ${email}"
+    deleteResponse=$(delete_user_request)
+    if [[ "$deleteResponse" -eq 404 ]]; then
+      echo -e "User ${email} doesn't exist"
+      exit 1
+    elif [[ "$deleteResponse" -ne 200 && "$deleteResponse" -ne 204 ]]; then
+      echo "Unexpected HTTP status code from IDAM: ${deleteResponse}"
+      exit 1
+    else
+      echo -e "Recreating IDAM user: ${email}"
+      createResponse=$(create_user_request)
+      if [[ "$createResponse" -eq 403 ]]; then
+        echo "User ${email} already exists"
+        exit 1
+      elif [[ "$createResponse" -ne 201 ]]; then
+        echo "Unexpected HTTP status code from IDAM: ${createResponse}"
+        exit 1
+      else
+        echo "User ${email} - updated in IDAM"
+      fi
+    fi
+  fi
+}
+
+######################
+## MAIN
+######################
+
 set -eu
 
 email=${1}
 rolesStr=${2}
 surname=${3:-"Tester"}
 
-IFS=',' read -ra roles <<< ${rolesStr}
+IFS=',' read -ra roles <<<"${rolesStr}"
 
 rolesJson=''
 for role in ${roles[@]}; do
-  if [[ ! -z ${rolesJson} ]] ; then
+  if [[ ! -z ${rolesJson} ]]; then
     rolesJson="${rolesJson},"
   fi
   rolesJson=${rolesJson}'{"code":"'${role}'"}'
@@ -18,29 +88,21 @@ done
 
 echo -e "\nCreating IDAM user: ${email}"
 
-userCreationResponse=$(curl --insecure --show-error --silent --output /dev/null --write-out "%{http_code}" -X POST \
-  ${IDAM_API_BASE_URL:-http://localhost:5000}/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{
-  "email":"'${email}'",
-  "forename":"'${email}'",
-  "surname":"'${surname}'",
-  "password":"'Password12'",
-  "levelOfAccess":1,
-  "roles": [
-    '${rolesJson}'
-  ],
-  "userGroup": {"code": "caseworker"}}
-')
+userCreationResponse=$(create_user_request)
 
 # Unfortunately trying to create the same user throws 403, so we don't know what went wrong
 if [[ $userCreationResponse -eq 403 ]]; then
   echo "User ${email} already exists"
-elif [[ $userCreationResponse -ne 201  ]]; then
+  recreate_user
+elif [[ $userCreationResponse -ne 201 ]]; then
   echo "Unexpected HTTP status code from IDAM: ${userCreationResponse}"
   exit 1
 else
   echo "User ${email} - added to IDAM"
 fi
 
-
+# if user exists
+#   check roles ✅
+#   if roles are the same do nothing ✅
+#   otherwise delete ✅
+#   and create user ✅ with same id ❌ <-- Apparently having the same id doesn't matter on local
