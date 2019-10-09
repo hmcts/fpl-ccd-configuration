e9c0c828f771b9a3c5eb0a11da151183a4726786
James Nelson <Jamesnelson117@gmail.com>
2019-10-09 13:13:57 +0100

    FPLA-753: Notice of proceeding e2e test updates

--- e2e/pages/events/createNoticeOfProceedingsEvent.page.js
+++ e2e/pages/events/createNoticeOfProceedingsEvent.page.js
@@ -6,6 +6,15 @@ module.exports = {
       c6: locate('input').withAttr({id: 'proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_PARTIES'}),
       c6a: locate('input').withAttr({id: 'proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_NON_PARTIES'}),
     },
+    judgeTitleRadioGroup: {
+      groupName: "#judgeAndLegalAdvisor_judgeTitle",
+      herHonourJudge: 'Her Honour Judge',
+      hisHonourJudge: 'His Honour Judge',
+      deputyDistrictJudge: 'Deputy District Judge',
+      magistrates: "Magistrates (JP)"
+    },
+    judgeLastName: '#judgeAndLegalAdvisor_judgeLastName',
+    legalAdvisorName: '#judgeAndLegalAdvisor_legalAdvisorName'
   },
 
   checkC6() {
@@ -15,4 +24,18 @@ module.exports = {
   checkC6A() {
     I.checkOption(this.fields.proceedingType.c6a);
   },
+
+  selectJudgeTitle() {
+      within(this.fields.judgeTitleRadioGroup.groupName, () => {
+        I.click(locate('label').withText(this.fields.judgeTitleRadioGroup.herHonourJudge));
+      });
+  },
+
+  enterJudgeLastName(judgeLastName) {
+    I.fillField(this.fields.judgeLastName, judgeLastName);
+  },
+
+  enterLegalAdvisorName(legalAdvisorName) {
+    I.fillField(this.fields.legalAdvisorName, legalAdvisorName);
+  }
 };
--- e2e/tests/hmctsAdministersCaseAfterSubmission_test.js
+++ e2e/tests/hmctsAdministersCaseAfterSubmission_test.js
@@ -116,43 +116,7 @@ Scenario('HMCTS admin sends email to gatekeeper with a link to the case', async 
   I.seeEventSubmissionConfirmation(config.administrationActions.sendToGatekeeper);
 });
 
-Scenario('HMCTS admin creates notice of proceedings documents', async (I, caseViewPage, createNoticeOfProceedingsEventPage, enterFamilyManCaseNumberEventPage, addHearingBookingDetailsEventPage) => {
-  await caseViewPage.goToNewActions(config.administrationActions.addFamilyManCaseNumber);
-  enterFamilyManCaseNumberEventPage.enterCaseID();
-  await I.completeEvent('Save and continue');
-
-    await caseViewPage.goToNewActions(config.administrationActions.addHearingBookingDetails);
-    await addHearingBookingDetailsEventPage.enterHearingDetails(hearingDetails[0]);
-    await I.addAnotherElementToCollection();
-    await addHearingBookingDetailsEventPage.enterHearingDetails(hearingDetails[1]);
-    await I.completeEvent('Save and continue', { summary: 'summary', description: 'description' });
-    I.seeEventSubmissionConfirmation(config.administrationActions.addHearingBookingDetails);
-    caseViewPage.selectTab(caseViewPage.tabs.hearings);
-    I.seeAnswerInTab(1, 'Hearing 1', 'Type of hearing', hearingDetails[0].caseManagement);
-    I.seeAnswerInTab(2, 'Hearing 1', 'Venue', hearingDetails[0].venue);
-    I.seeAnswerInTab(3, 'Hearing 1', 'Date', '1 Jan 2050');
-    I.seeAnswerInTab(4, 'Hearing 1', 'Pre-hearing attendance', hearingDetails[0].preHearingAttendance);
-    I.seeAnswerInTab(5, 'Hearing 1', 'Hearing time', hearingDetails[0].time);
-    I.seeAnswerInTab(6, 'Hearing 1', 'Hearing needs booked', hearingDetails[0].type.interpreter);
-    I.seeAnswerInTab(6, 'Hearing 1', '', hearingDetails[0].type.welsh);
-    I.seeAnswerInTab(6, 'Hearing 1', '', hearingDetails[0].type.somethingElse);
-    I.seeAnswerInTab(7, 'Hearing 1', 'Give details', hearingDetails[0].giveDetails);
-    I.seeAnswerInTab(8, 'Hearing 1', 'Judge or magistrate\'s title', hearingDetails[0].judgeTitle);
-    I.seeAnswerInTab(9, 'Hearing 1', 'Judge or magistrate\'s last name', hearingDetails[0].lastName);
-
-    I.seeAnswerInTab(1, 'Hearing 2', 'Type of hearing', hearingDetails[1].caseManagement);
-    I.seeAnswerInTab(2, 'Hearing 2', 'Venue', hearingDetails[1].venue);
-    I.seeAnswerInTab(3, 'Hearing 2', 'Date', '2 Feb 2060');
-    I.seeAnswerInTab(4, 'Hearing 2', 'Pre-hearing attendance', hearingDetails[1].preHearingAttendance);
-    I.seeAnswerInTab(5, 'Hearing 2', 'Hearing time', hearingDetails[1].time);
-    I.seeAnswerInTab(6, 'Hearing 2', 'Hearing needs booked', hearingDetails[1].type.interpreter);
-    I.seeAnswerInTab(6, 'Hearing 2', '', hearingDetails[1].type.welsh);
-    I.seeAnswerInTab(6, 'Hearing 2', '', hearingDetails[1].type.somethingElse);
-    I.seeAnswerInTab(7, 'Hearing 2', 'Give details', hearingDetails[1].giveDetails);
-    I.seeAnswerInTab(8, 'Hearing 2', 'Judge or magistrate\'s title', hearingDetails[1].judgeTitle);
-    I.seeAnswerInTab(9, 'Hearing 2', 'Judge or magistrate\'s last name', hearingDetails[1].lastName);
-
-
+Scenario('HMCTS admin creates notice of proceedings documents', async (I, caseViewPage, createNoticeOfProceedingsEventPage) => {
   await caseViewPage.goToNewActions(config.administrationActions.createNoticeOfProceedings);
   createNoticeOfProceedingsEventPage.checkC6();
   createNoticeOfProceedingsEventPage.checkC6A();
