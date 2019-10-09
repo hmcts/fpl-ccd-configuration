a08ef6fc95fc0345e44a4d241fcffa45d64c3a5b
Becky Brennan <rebeccab@kainos.com>
2019-10-09 15:34:23 +0100

    judiciary court admin permissions

--- bin/configurer/add-users.sh
+++ bin/configurer/add-users.sh
@@ -11,5 +11,5 @@ ${dir}/utils/idam-create-caseworker.sh "sam@hillingdon.gov.uk" "caseworker,casew
 ${dir}/utils/idam-create-caseworker.sh "siva@hillingdon.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
 ${dir}/utils/idam-create-caseworker.sh "hmcts-admin@example.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-courtadmin" "(hmcts-admin)"
 ${dir}/utils/idam-create-caseworker.sh "cafcass@example.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-cafcass" "(cafcass)"
-${dir}/utils/idam-create-caseworker.sh "judiciary@mailnesia.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-judiciary,caseworker-publiclaw-gatekeeper" "(judiciary)"
+${dir}/utils/idam-create-caseworker.sh "judiciary@mailnesia.com" ",caseworker-publiclaw-courtadmin, caseworker,caseworker-publiclaw,caseworker-publiclaw-judiciary,caseworker-publiclaw-gatekeeper" "(judiciary)"
 ${dir}/utils/idam-create-caseworker.sh "gatekeeper@mailnesia.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-gatekeeper,caseworker-publiclaw-courtadmin" "(gatekeeper)"
