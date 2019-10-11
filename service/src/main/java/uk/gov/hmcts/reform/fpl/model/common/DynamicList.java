fb2a44aed50a2a580126f11c2410da9fe347e8af
Lewis Birks <lewisb@kainos.com>
2019-10-11 15:51:12 +0100

    Allow creation of a dynamic list with a blank initial default value

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/DynamicList.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/DynamicList.java
@@ -29,6 +29,18 @@ public class DynamicList {
     /**
      * Converts a list of elements to the appropriate structure to then be handled correctly by CCD.
      *
+     * <p>The selected value will be blank by default and have a code value of -1.
+     *
+     * @param elements elements to convert into options for the dropdown
+     * @return a {@link DynamicList} to be sent to CCD
+     */
+    public static <T extends DynamicElementParser> DynamicList toDynamicList(Collection<T> elements) {
+        return toDynamicList(elements, -1);
+    }
+
+    /**
+     * Converts a list of elements to the appropriate structure to then be handled correctly by CCD.
+     *
      * @param elements elements to convert into options for the dropdown
      * @param index    index of the element that will become the default selected value
      * @param <T>      a class that implements {@link DynamicElementParser#toDynamicElement()}
@@ -40,6 +52,15 @@ public class DynamicList {
             .map(DynamicElementParser::toDynamicElement)
             .collect(Collectors.toList());
 
-        return DynamicList.builder().listItems(items).value(items.get(index)).build();
+        DynamicListElement value;
+
+        if (index > 0) {
+            value = items.get(0);
+        } else {
+            // Create an empty selected value
+            value = DynamicListElement.builder().code("-1").label("").build();
+        }
+
+        return DynamicList.builder().listItems(items).value(value).build();
     }
 }
