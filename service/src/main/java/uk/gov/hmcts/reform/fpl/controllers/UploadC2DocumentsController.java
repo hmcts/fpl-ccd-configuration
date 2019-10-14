debaed4cc735a1e845bc203f137b3fa88a18d545
James Nelson <Jamesnelson117@gmail.com>
2019-10-14 09:44:21 +0100

    FPLA-600: Code review fixes

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsController.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+
 @Api
 @RestController
 @RequestMapping("/callback/upload-c2")
@@ -41,8 +43,8 @@ public class UploadC2DocumentsController {
     }
 
     private List<Element<C2DocumentBundle>> buildC2DocumentBundle(CaseData caseData) {
-        List<Element<C2DocumentBundle>> c2DocumentBundle = caseData.getC2DocumentBundle() == null
-            ? Lists.newArrayList() : caseData.getC2DocumentBundle();
+        List<Element<C2DocumentBundle>> c2DocumentBundle = defaultIfNull(caseData.getC2DocumentBundle(),
+            Lists.newArrayList());
 
         c2DocumentBundle.add(Element.<C2DocumentBundle>builder()
             .id(UUID.randomUUID())
