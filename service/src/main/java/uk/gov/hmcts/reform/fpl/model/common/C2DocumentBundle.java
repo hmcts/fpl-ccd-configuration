ef0df49cbc2ec99ef18616da5f1a960a4e6fe7ba
James Nelson <Jamesnelson117@gmail.com>
2019-10-15 20:49:35 +0100

    FPLA-600: Added CaseEventToField for controlling mandatory description on c2DocumentBundle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/C2DocumentBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/C2DocumentBundle.java
@@ -8,4 +8,6 @@ import lombok.Data;
 public class C2DocumentBundle {
     private final DocumentReference document;
     private final String description;
+    private final String uploadedDateTime;
+    private final String author;
 }
