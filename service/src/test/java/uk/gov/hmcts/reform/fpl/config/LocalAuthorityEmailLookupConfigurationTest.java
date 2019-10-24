5d45e19436f6ce14d10fa7b4591dd403fa0d7aa5
Toireasa Moley <toireasa.moley@hmcts.net>
2019-10-24 15:52:36 +0100

    Fixing test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/config/LocalAuthorityEmailLookupConfigurationTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/config/LocalAuthorityEmailLookupConfigurationTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LocalAuthorityEmailLookupConfigurationTest {
 
     private static final String LOCAL_AUTHORITY_CODE = "example";
-    private static final String LOCAL_AUTHORITY_EMAIL = "cafcass@example.com";
+    private static final String LOCAL_AUTHORITY_EMAIL = "localauthority@example.com";
 
     private static final String CONFIG = String.format("%s=>%s", LOCAL_AUTHORITY_CODE, LOCAL_AUTHORITY_EMAIL);
 
