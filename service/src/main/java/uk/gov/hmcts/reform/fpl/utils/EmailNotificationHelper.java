dc6ae1f13786fdc1db2f986c4469c87e90fd90fd
eau <euedofia@gmil.com>
2019-10-27 13:55:52 +0000

    FPLA-640 code review updates

--- service/src/main/java/uk/gov/hmcts/reform/fpl/utils/EmailNotificationHelper.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/EmailNotificationHelper.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.fpl.utils;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.util.CollectionUtils;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.Respondent;
 import uk.gov.hmcts.reform.fpl.model.RespondentParty;
@@ -9,16 +8,19 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.Objects;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.springframework.util.CollectionUtils.isEmpty;
+
 public class EmailNotificationHelper {
 
     private EmailNotificationHelper() {
     }
 
     public static String buildSubjectLine(final CaseData caseData) {
-        final String lastName = (StringUtils.isNotBlank(getRespondents1Lastname(caseData))
+        final String lastName = (isNotBlank(getRespondents1Lastname(caseData))
             ? String.format("%1$s", getRespondents1Lastname(caseData)) : "");
 
-        final String familyManCaseNumber = (StringUtils.isNotBlank(caseData.getFamilyManCaseNumber())
+        final String familyManCaseNumber = (isNotBlank(caseData.getFamilyManCaseNumber())
             ? String.format("%1$s", caseData.getFamilyManCaseNumber()) : "");
 
         return String.format("%1$s%2$s%3$s", lastName,
@@ -27,7 +29,7 @@ public class EmailNotificationHelper {
     }
 
     private static String getRespondents1Lastname(final CaseData caseData) {
-        return CollectionUtils.isEmpty(caseData.getRespondents1()) ? "" : caseData.getRespondents1()
+        return isEmpty(caseData.getRespondents1()) ? "" : caseData.getRespondents1()
             .stream().filter(Objects::nonNull)
             .map(Element::getValue).filter(Objects::nonNull)
             .findFirst().map(Respondent::getParty)
