6c30ca2669862624cfb00a1700e95e1104710aab
eau <euedofia@gmil.com>
2019-10-27 20:39:58 +0000

    FPLA-603 keep to existing static import convention

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/EmailNotificationService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/EmailNotificationService.java
@@ -1,16 +1,17 @@
 package uk.gov.hmcts.reform.fpl.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 import uk.gov.service.notify.NotificationClient;
 import uk.gov.service.notify.NotificationClientException;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Service
 public class EmailNotificationService {
@@ -42,8 +43,8 @@ public class EmailNotificationService {
                                  final String reference) {
         logger.debug("Sending submission notification (with template id: {}) to {}", templateId, emails);
 
-        if (!CollectionUtils.isEmpty(emails)) {
-            emails.stream().filter(Objects::nonNull).forEach(email ->
+        if (!isEmpty(emails)) {
+            emails.stream().filter(StringUtils::isNotBlank).forEach(email ->
                 sendNotification(templateId, email, parameters, reference));
         }
     }
