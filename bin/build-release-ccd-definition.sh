e03ffe658c15dcce1f1711605b2070309e2338e1
akomisarek <Adam.Komisarek@hmcts.net>
2019-10-29 13:55:28 +0000

    fix: create missing directory to fix the pipeline build (#567)

--- bin/build-release-ccd-definition.sh
+++ bin/build-release-ccd-definition.sh
@@ -20,6 +20,8 @@ config_dir=${root_dir}/ccd-definition
 build_dir=${root_dir}/build/ccd-release-config
 release_definition_output_file=${build_dir}/ccd-fpl-${environment}.xlsx
 
+mkdir -p ${build_dir}
+
 # build the ccd definition file
 export CCD_DEF_CASE_SERVICE_BASE_URL=http://fpl-case-service-${environment}.service.core-compute-${environment}.internal
 ${root_dir}/bin/configurer/utils/fpl-process-definition.sh ${config_dir} ${release_definition_output_file} "${excludedFilenamePatterns}"
