ace4f19ff6c72fb8bef1449188e1149459e05073
Damian Dunajski <damdun@gmail.com>
2019-10-30 10:22:06 +0000

    Toggle off SDO and notice of proceedings events

--- e2e/tests/gatekeeperAdministersCaseAfterGatekeeping_test.js
+++ e2e/tests/gatekeeperAdministersCaseAfterGatekeeping_test.js
@@ -83,7 +83,7 @@ Scenario('Gatekeeper enters hearing details and submits', async (I, caseViewPage
 });
 
 // Disabled until SDO goes live
-Scenario('Gatekeeper drafts standard directions', async (I, caseViewPage, draftStandardDirectionsEventPage) => {
+xScenario('Gatekeeper drafts standard directions', async (I, caseViewPage, draftStandardDirectionsEventPage) => {
   await caseViewPage.goToNewActions(config.administrationActions.draftStandardDirections);
   await draftStandardDirectionsEventPage.enterJudgeAndLegalAdvisor('Smith', 'Bob Ross');
   await draftStandardDirectionsEventPage.enterDatesForDirections(directions[0]);
@@ -98,7 +98,7 @@ Scenario('Gatekeeper drafts standard directions', async (I, caseViewPage, draftS
   I.seeAnswerInTab(6, 'Directions 1', 'Due date and time', '1 Jan 2050, 12:00:00 PM');
 });
 
-Scenario('Gatekeeper submits final version of standard directions', async (I, caseViewPage, draftStandardDirectionsEventPage) => {
+xScenario('Gatekeeper submits final version of standard directions', async (I, caseViewPage, draftStandardDirectionsEventPage) => {
   await caseViewPage.goToNewActions(config.administrationActions.draftStandardDirections);
   await draftStandardDirectionsEventPage.enterJudgeAndLegalAdvisor('Smith', 'Bob Ross');
   await draftStandardDirectionsEventPage.enterDatesForDirections(directions[0]);
