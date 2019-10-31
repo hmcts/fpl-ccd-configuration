18eb783fc8f6e5d50b68f70272076f9bf4a96f29
swalker125 <sam.walker@digit-hull.com>
2019-10-31 13:58:52 +0000

    refactor c6 and c6a checkbox ids in e2e

--- e2e/pages/events/createNoticeOfProceedingsEvent.page.js
+++ e2e/pages/events/createNoticeOfProceedingsEvent.page.js
@@ -4,8 +4,8 @@ const judgeAndLegalAdvisor = require('../../fragments/judgeAndLegalAdvisor');
 module.exports = {
   fields: {
     proceedingType: {
-      c6: locate('input').withAttr({id: 'noticeOfProceedings-proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_PARTIES'}),
-      c6a: locate('input').withAttr({id: 'noticeOfProceedings-proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_NON_PARTIES'}),
+      c6: locate('input').withAttr({id: 'noticeOfProceedings_proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_PARTIES'}),
+      c6a: locate('input').withAttr({id: 'noticeOfProceedings_proceedingTypes-NOTICE_OF_PROCEEDINGS_FOR_NON_PARTIES'}),
     },
   },
 
