96c2346e12871027ebb0cdc4a9b199162fc94eb7
Etimbuk <euedofia@gmail.com>
2019-11-07 10:44:28 +0000

    FPLA-603 Notify parties of Order made on C21

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/C21OrderEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/C21OrderEmailContentProvider.java
@@ -1,43 +1,74 @@
 package uk.gov.hmcts.reform.fpl.service.email.content;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import net.logstash.logback.encoder.org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.config.LocalAuthorityNameLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.service.DateFormatterService;
 import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
 
-import java.util.HashMap;
+import java.time.format.FormatStyle;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.JURISDICTION;
+import static uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper.buildSubjectLine;
 
 @Service
-public class C21OrderEmailContentProvider extends C2UploadedEmailContentProvider {
+public class C21OrderEmailContentProvider extends AbstractEmailContentProvider {
 
     private final HearingBookingService hearingBookingService;
     private final LocalAuthorityNameLookupConfiguration localAuthorityNameLookupConfiguration;
+    private final ObjectMapper objectMapper;
+    private final DateFormatterService dateFormatterService;
 
     protected C21OrderEmailContentProvider(@Value("${ccd.ui.base.url}")String uiBaseUrl,
                                            ObjectMapper objectMapper,
                                            HearingBookingService hearingBookingService,
                                            LocalAuthorityNameLookupConfiguration
-                                               localAuthorityNameLookupConfiguration) {
-        super(uiBaseUrl, objectMapper);
+                                               localAuthorityNameLookupConfiguration,
+                                           DateFormatterService dateFormatterService) {
+        super(uiBaseUrl);
+        this.objectMapper = objectMapper;
         this.hearingBookingService = hearingBookingService;
         this.localAuthorityNameLookupConfiguration = localAuthorityNameLookupConfiguration;
+        this.dateFormatterService = dateFormatterService;
     }
 
     public Map<String, Object> buildC21OrderNotification(final CaseDetails caseDetails,
                                                          final String localAuthorityCode) {
         // Validation within our frontend ensures that the following data is present
-        CaseData caseData = super.objectMapper.convertValue(caseDetails.getData(), CaseData.class);
+        CaseData caseData = objectMapper.convertValue(caseDetails.getData(), CaseData.class);
 
-        Map<String, Object> c21NotificationParams = new HashMap<>(super.buildC2UploadNotification(caseDetails));
-        c21NotificationParams.put("localAuthorityOrCafcass",
-            localAuthorityNameLookupConfiguration.getLocalAuthorityName(localAuthorityCode));
-        c21NotificationParams.put("hearingDate",
-            hearingBookingService.getMostUrgentHearingBooking(caseData.getHearingDetails()));
+        final String subjectLine = buildSubjectLine(caseData);
 
-        return c21NotificationParams;
+        return ImmutableMap.of(
+            "subjectLine", subjectLine,
+            "localAuthorityOrCafcass", localAuthorityNameLookupConfiguration.getLocalAuthorityName(localAuthorityCode),
+            "hearingDetailsCallout", buildSubjectLineWithHearingBookingDateSuffix(subjectLine, caseData),
+            "reference", String.valueOf(caseDetails.getId()),
+            "caseUrl", uiBaseUrl + "/case/" + JURISDICTION + "/" + CASE_TYPE + "/" + caseDetails.getId()
+
+        );
+    }
+
+    private String buildSubjectLineWithHearingBookingDateSuffix(final String subjectLine, final CaseData caseData) {
+        String hearingDate = "";
+        if (!ObjectUtils.isEmpty(caseData.getHearingDetails())) {
+            hearingDate = " hearing " + dateFormatterService.formatLocalDateToString(
+                hearingBookingService.getMostUrgentHearingBooking(caseData.getHearingDetails()).getDate(),
+                FormatStyle.MEDIUM);
+        }
+
+        return Stream.of(subjectLine, hearingDate)
+            .filter(StringUtils::isNotBlank)
+            .collect(joining(", "));
     }
 }
