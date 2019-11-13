4db852d2e56d975046dd978fed0cb67163883cc0
Toireasa Moley <toireasa.moley@hmcts.net>
2019-11-13 13:12:50 +0000

    Adding test for document upload

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsControllerMidEventTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsControllerMidEventTest.java
@@ -15,10 +15,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.service.DateFormatterService;
 import uk.gov.hmcts.reform.fpl.service.UserDetailsService;
-import static org.assertj.core.api.Assertions.assertThat;
 
+import static java.util.Objects.isNull;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +31,9 @@ class UploadC2DocumentsControllerMidEventTest {
     private static final String USER_ID = "1";
     private static final String USER_NAME = "Emma Taylor";
     private static final String ERROR_MESSAGE = "A document must be uploaded";
+    private static final String DOCUMENT_URL = "http://localhost/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4";
+    private static final String BINARY_URL = "http://localhost/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4/binary";
+    private static final String FILENAME = "file.pdf";
 
     @MockBean
     private UserDetailsService userDetailsService;
@@ -49,7 +52,7 @@ class UploadC2DocumentsControllerMidEventTest {
 
     @Test
     void shouldReturnAnErrorWhenDocumentIsNotUploaded() throws Exception {
-        CallbackRequest request = createCallbackRequestWithTempC2BundleAndNoDocument();
+        CallbackRequest request = createCallbackRequestWithTempC2Bundle(null,null,null);
 
         AboutToStartOrSubmitCallbackResponse callbackResponse = makeRequest(request);
 
@@ -57,36 +60,35 @@ class UploadC2DocumentsControllerMidEventTest {
     }
 
     @Test
-    void shouldNotReturnAnErrorWhenHearingDateIsSetToTomorrow() throws Exception {
-        CallbackRequest request = createCallbackRequestWithTempC2Bundle();
+    void shouldNotReturnAnErrorWhenDocumentIsUploaded() throws Exception {
+        CallbackRequest request = createCallbackRequestWithTempC2Bundle(DOCUMENT_URL,BINARY_URL,FILENAME);
 
         AboutToStartOrSubmitCallbackResponse callbackResponse = makeRequest(request);
 
         assertThat(callbackResponse.getErrors()).doesNotContain(ERROR_MESSAGE);
     }
 
-    private CallbackRequest createCallbackRequestWithTempC2Bundle() {
+    private CallbackRequest createCallbackRequestWithTempC2Bundle(String documentUrl, String binaryUrl, String filename) {
         return CallbackRequest.builder()
             .caseDetails(CaseDetails.builder()
                 .data(ImmutableMap.of(
-                    "temporaryC2Document", ImmutableMap.of(
-                        "document", ImmutableMap.of(
-                            "document_url", "http://localhost/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4",
-                            "document_binary_url",
-                            "http://localhost/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4/binary",
-                            "document_filename", "file.pdf"),
-                        "description", "Test description")))
+                    "temporaryC2Document",
+                    documentOrEmptyMap(documentUrl, binaryUrl, filename)))
                 .build())
             .build();
     }
 
-    private CallbackRequest createCallbackRequestWithTempC2BundleAndNoDocument() {
-        return CallbackRequest.builder()
-            .caseDetails(CaseDetails.builder()
-                .data(ImmutableMap.of(
-                    "temporaryC2Document", ImmutableMap.of()))
-                .build())
-            .build();
+    private ImmutableMap documentOrEmptyMap(String documentUrl, String binaryUrl, String filename)
+    {
+        if(isNull(documentUrl)) {
+            return ImmutableMap.of();
+        }
+        else {
+            return ImmutableMap.of(
+                "document_url", documentUrl,
+                "document_binary_url", binaryUrl,
+                "document_filename", filename);
+        }
     }
 
     private AboutToStartOrSubmitCallbackResponse makeRequest(CallbackRequest request) throws Exception {
