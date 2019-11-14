4d306881411162143d4be1c7a528c53dc6c9797b
Lewis Birks <lewisb@kainos.com>
2019-11-14 17:28:23 +0000

    fixed future date validator

--- service/src/main/java/uk/gov/hmcts/reform/fpl/validation/validators/time/HasFutureDateValidator.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/validation/validators/time/HasFutureDateValidator.java
@@ -8,10 +8,8 @@ import javax.validation.ConstraintValidatorContext;
 
 public class HasFutureDateValidator implements ConstraintValidator<HasFutureDate, LocalDateTime> {
 
-    private static final LocalDateTime NOW = LocalDateTime.now();
-
     @Override
     public boolean isValid(LocalDateTime value, ConstraintValidatorContext context) {
-        return value.isAfter(NOW);
+        return value.isAfter(LocalDateTime.now());
     }
 }
