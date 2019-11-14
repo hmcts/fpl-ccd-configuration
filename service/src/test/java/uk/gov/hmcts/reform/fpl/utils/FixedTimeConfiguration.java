166019704585e815299bcbf804e5ac9210263977
Joe Zazzaro-Francis <joez@Gabbana.local>
2019-11-14 11:04:20 +0000

    Renamed time configuration testing helper

--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/FixedTimeConfiguration.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/FixedTimeConfiguration.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.fpl;
+package uk.gov.hmcts.reform.fpl.utils;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +13,7 @@ import java.time.ZonedDateTime;
 
 @Configuration
 @Import(TestConfiguration.class)
-public class TestTimeConfig {
+public class FixedTimeConfiguration {
     public static final LocalDateTime NOW = ZonedDateTime.now(ZoneId.of("Europe/London")).toLocalDateTime();
 
     @Bean
