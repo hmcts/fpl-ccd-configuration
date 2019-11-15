e3d8fa1f0e18fd7e1927d9a340bd131fa9e99198
James Nelson <Jamesnelson117@gmail.com>
2019-11-15 15:30:29 +0000

    FPLA-657: Resolved bad merge

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/DraftCMOServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/DraftCMOServiceTest.java
@@ -1,168 +1,134 @@
-package uk.gov.hmcts.reform.fpl.controllers;
+package uk.gov.hmcts.reform.fpl.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.CaseManagementOrder;
-import uk.gov.hmcts.reform.fpl.model.Direction;
 import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.fpl.model.common.dynamic.DynamicListElement;
-import uk.gov.hmcts.reform.fpl.service.DraftCMOService;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 import static java.util.UUID.fromString;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.fpl.enums.DirectionAssignee.ALL_PARTIES;
 import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createDirection;
 import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createHearingBooking;
 
-@ActiveProfiles("integration-test")
-@WebMvcTest(DraftCMOController.class)
-@OverrideAutoConfiguration(enabled = true)
-@SuppressWarnings("unchecked")
-class DraftCMOControllerTest {
-    private static final String AUTH_TOKEN = "Bearer token";
-    private static final String USER_ID = "1";
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {JacksonAutoConfiguration.class, DateFormatterService.class, DraftCMOService.class})
+class DraftCMOServiceTest {
 
     @Autowired
     private DraftCMOService draftCMOService;
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper mapper;
+    private DateFormatterService dateFormatterService;
 
     private final LocalDate date = LocalDate.now();
-    private final List<Element<HearingBooking>> hearingDetails = createHearingBookings(date);
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDate(
-        FormatStyle.MEDIUM).localizedBy(Locale.UK);
 
     @Test
-    void aboutToStartCallbackShouldPopulateHearingDatesListAndDirections() throws Exception {
-        Map<String, Object> data = ImmutableMap.of(
-            "hearingDetails", hearingDetails,
-            "caseManagementOrder", ImmutableMap.of("directions", createDirection(ALL_PARTIES)));
-
-        List<String> expected = Arrays.asList(
-            date.plusDays(5).format(dateTimeFormatter),
-            date.plusDays(2).format(dateTimeFormatter),
-            date.format(dateTimeFormatter));
-
-        AboutToStartOrSubmitCallbackResponse callbackResponse = getResponse(data, "about-to-start");
-
-        assertThat(getHearingDates(callbackResponse)).isEqualTo(expected);
-        assertThat(callbackResponse.getData()).containsKey("allParties");
-    }
-
-    @Test
-    void aboutToSubmitShouldPopulateHiddenHearingDateFieldAndCmoDirection() throws Exception {
-        List<Element<HearingBooking>> hearingDetails = createHearingBookings(date);
-
-        DynamicList dynamicHearingDates = draftCMOService.buildDynamicListFromHearingDetails(hearingDetails);
-
-        dynamicHearingDates
-            .setValue(
-                DynamicListElement.builder()
-                    .code(fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"))
-                    .label(date.plusDays(5).toString())
-                    .build());
-
-        Map<String, Object> data = ImmutableMap.of("cmoHearingDateList", dynamicHearingDates,
-            "allParties", createDirection(null));
-
-        AboutToStartOrSubmitCallbackResponse callbackResponse = getResponse(data, "about-to-submit");
-
-        assertThat(callbackResponse.getData()).doesNotContainKey("cmoHearingDateList");
-
-        CaseData caseData = mapper.convertValue(callbackResponse.getData(), CaseData.class);
-        CaseManagementOrder caseManagementOrder = caseData.getCaseManagementOrder();
-
-        assertThat(caseManagementOrder).extracting("id", "hearingDate")
-            .containsExactly(fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"), date.plusDays(5).toString());
-
-        List<Element<Direction>> responseDirections = caseManagementOrder.getDirections();
-
-        assertThat(callbackResponse.getData()).doesNotContainKey("allParties");
-        assertThat(responseDirections.size()).isEqualTo(1);
-        assertThat(responseDirections).isEqualTo(createDirection(ALL_PARTIES));
-    }
-
-    private List<String> getHearingDates(AboutToStartOrSubmitCallbackResponse callbackResponse) {
-        Map<String, Object> cmoHearingResponse = mapper.convertValue(
-            callbackResponse.getData().get("cmoHearingDateList"), Map.class);
-
-        List<Map<String, Object>> listItemMap = mapper.convertValue(cmoHearingResponse.get("list_items"), List.class);
-
-        return listItemMap.stream()
-            .map(element -> mapper.convertValue(element, DynamicListElement.class))
-            .map(DynamicListElement::getLabel).collect(Collectors.toList());
-    }
-
-    private AboutToStartOrSubmitCallbackResponse getResponse(
-        final Map<String, Object> data, final String endpoint) throws Exception {
-        CallbackRequest request = CallbackRequest.builder()
-            .caseDetails(CaseDetails.builder()
-                .data(data)
-                .build())
+    void shouldReturnHearingDateDynamicListWhenCaseDetailsHasHearingDate() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(ImmutableMap.of("hearingDetails", createHearingBookings(date)))
             .build();
 
-        MvcResult response = makeRequest(request, endpoint);
+        DynamicList hearingList = draftCMOService.getHearingDateDynamicList(caseDetails);
 
-        return mapper.readValue(response.getResponse()
-            .getContentAsByteArray(), AboutToStartOrSubmitCallbackResponse.class);
+        assertThat(hearingList.getListItems())
+            .containsAll(Arrays.asList(
+                DynamicListElement.builder()
+                    .code(fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"))
+                    .label(formatLocalDateToMediumStyle(5))
+                    .build(),
+                DynamicListElement.builder()
+                    .code(fromString("6b3ee98f-acff-4b64-bb00-cc3db02a24b2"))
+                    .label(formatLocalDateToMediumStyle(2))
+                    .build(),
+                DynamicListElement.builder()
+                    .code(fromString("ecac3668-8fa6-4ba0-8894-2114601a3e31"))
+                    .label(formatLocalDateToMediumStyle(0))
+                    .build()));
     }
 
-    private MvcResult makeRequest(CallbackRequest request, String endpoint) throws Exception {
-        return mockMvc
-            .perform(post("/callback/draft-cmo/" + endpoint)
-                .header("authorization", AUTH_TOKEN)
-                .header("user-id", USER_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(mapper.writeValueAsString(request)))
-            .andExpect(status().isOk())
-            .andReturn();
+    @Test
+    void shouldReturnHearingDateDynamicListWhenCmoHasPreviousSelectedValue() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(ImmutableMap.of(
+                "hearingDetails", createHearingBookings(date),
+                "caseManagementOrder", CaseManagementOrder.builder()
+                    .hearingDate(formatLocalDateToMediumStyle(2))
+                    .id(fromString("6b3ee98f-acff-4b64-bb00-cc3db02a24b2"))
+                    .build()
+            )).build();
+
+        DynamicList hearingList = draftCMOService.getHearingDateDynamicList(caseDetails);
+
+        assertThat(hearingList.getValue())
+            .isEqualTo(DynamicListElement.builder()
+                .code(fromString("6b3ee98f-acff-4b64-bb00-cc3db02a24b2"))
+                .label(formatLocalDateToMediumStyle(2))
+                .build());
     }
 
-    private List<Element<HearingBooking>> createHearingBookings(LocalDate date) {
+    @Test
+    void shouldReturnCaseManagementOrderWhenProvidedCaseDetails() {
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(ImmutableMap.of("cmoHearingDateList", getDynamicList(), "allParties",
+                createDirection(null)))
+            .build();
+
+        CaseManagementOrder caseManagementOrder = draftCMOService.getCaseManagementOrder(caseDetails);
+
+        assertThat(caseManagementOrder).isNotNull()
+            .extracting("id", "hearingDate").containsExactly(
+            fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"),
+            formatLocalDateToMediumStyle(5));
+
+        assertThat(caseManagementOrder.getDirections()).isEqualTo(createDirection(ALL_PARTIES));
+    }
+
+    private DynamicList getDynamicList() {
+        DynamicList dynamicList = draftCMOService.buildDynamicListFromHearingDetails(createHearingBookings(date));
+
+        DynamicListElement listElement = DynamicListElement.builder()
+            .label(formatLocalDateToMediumStyle(5))
+            .code(fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"))
+            .build();
+
+        dynamicList.setValue(listElement);
+        return dynamicList;
+    }
+
+    private List<Element<HearingBooking>> createHearingBookings(LocalDate now) {
         return ImmutableList.of(
             Element.<HearingBooking>builder()
                 .id(fromString("b15eb00f-e151-47f2-8e5f-374cc6fc2657"))
-                .value(createHearingBooking(date.plusDays(5)))
+                .value(createHearingBooking(now.plusDays(5)))
                 .build(),
             Element.<HearingBooking>builder()
-                .id(UUID.randomUUID())
-                .value(createHearingBooking(date.plusDays(2)))
+                .id(fromString("6b3ee98f-acff-4b64-bb00-cc3db02a24b2"))
+                .value(createHearingBooking(now.plusDays(2)))
                 .build(),
             Element.<HearingBooking>builder()
-                .id(UUID.randomUUID())
-                .value(createHearingBooking(date))
+                .id(fromString("ecac3668-8fa6-4ba0-8894-2114601a3e31"))
+                .value(createHearingBooking(now))
                 .build()
         );
+    }
+
+    private String formatLocalDateToMediumStyle(int i) {
+        return dateFormatterService.formatLocalDateToString(date.plusDays(i), FormatStyle.MEDIUM);
     }
 }
