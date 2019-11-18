7e184c3d1ea5592597a661ef2fd417eb6c2e7ff8
Toireasa Moley <toireasa.moley@hmcts.net>
2019-11-18 11:13:39 +0000

    Adding case role

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserService.java
@@ -1,17 +1,22 @@
 package uk.gov.hmcts.reform.fpl.service;
 
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.CaseAccessApi;
+import uk.gov.hmcts.reform.ccd.client.CaseUserApi;
+import uk.gov.hmcts.reform.ccd.client.model.CaseUser;
 import uk.gov.hmcts.reform.ccd.client.model.UserId;
 import uk.gov.hmcts.reform.fpl.config.LocalAuthorityUserLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.exceptions.NoAssociatedUsersException;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Service
 public class LocalAuthorityUserService {
@@ -21,19 +26,42 @@ public class LocalAuthorityUserService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final CaseAccessApi caseAccessApi;
+    private final CaseUserApi caseUserApi;
     private final LocalAuthorityUserLookupConfiguration localAuthorityUserLookupConfiguration;
     private final AuthTokenGenerator authTokenGenerator;
+    private final IdamClient client;
 
     @Autowired
     public LocalAuthorityUserService(CaseAccessApi caseAccessApi,
                                      LocalAuthorityUserLookupConfiguration localAuthorityUserLookupConfiguration,
-                                     AuthTokenGenerator authTokenGenerator) {
+                                     AuthTokenGenerator authTokenGenerator,
+                                     CaseUserApi caseUserApi,
+                                     IdamClient idamClient) {
         this.caseAccessApi = caseAccessApi;
         this.localAuthorityUserLookupConfiguration = localAuthorityUserLookupConfiguration;
         this.authTokenGenerator = authTokenGenerator;
+        this.caseUserApi = caseUserApi;
+        this.client = idamClient;
     }
 
     public void grantUserAccess(String authorization, String creatorUserId, String caseId, String caseLocalAuthority) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Set<String> caseRoles = Set.of("[LASOLICITOR]");
+        try {
+            String authentication = client.authenticateUser("fpl-system-update@mailnesia.com", "Password12");
+            caseUserApi.updateCaseRolesForUser(authentication, authTokenGenerator.generate(), caseId, creatorUserId,
+                new CaseUser(creatorUserId, caseRoles));
+            logger.info("Added case roles {} to user {}", caseRoles, creatorUserId);
+        } catch (FeignException exception) {
+            logger.warn(String.format("Error adding case roles %s to user %s", caseRoles, creatorUserId), exception);
+        }
+
+
         findUserIds(caseLocalAuthority).stream()
             .filter(userId -> !Objects.equals(userId, creatorUserId))
             .forEach(userId -> {
