63190ac94a8a6d00cc0be84282022a997ea563a2
swalker125 <sam.walker@digit-hull.com>
2019-11-19 06:52:54 +0000

    initial implementation of UI for respondent directions

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ComplyWithDirectionsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ComplyWithDirectionsController.java
@@ -14,13 +14,21 @@ import uk.gov.hmcts.reform.fpl.enums.DirectionAssignee;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.Direction;
 import uk.gov.hmcts.reform.fpl.model.DirectionResponse;
+import uk.gov.hmcts.reform.fpl.model.Respondent;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.service.DirectionHelperService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+import static net.logstash.logback.encoder.org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+import static net.logstash.logback.encoder.org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.fpl.enums.DirectionAssignee.ALL_PARTIES;
+import static uk.gov.hmcts.reform.fpl.enums.DirectionAssignee.PARENTS_AND_RESPONDENTS;
 
 @Api
 @RestController
@@ -48,6 +56,49 @@ public class ComplyWithDirectionsController {
 
         sortedDirections.forEach((assignee, directions) -> directionHelperService
             .addAssigneeDirectionKeyValuePairsToCaseData(assignee, allPartyDirections, caseDetails));
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDetails.getData())
+            .build();
+    }
+
+    @PostMapping("/on-behalf/about-to-start")
+    public AboutToStartOrSubmitCallbackResponse handleAboutToStart2(@RequestBody CallbackRequest callbackrequest) {
+        CaseDetails caseDetails = callbackrequest.getCaseDetails();
+        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+
+        List<Element<Direction>> respondentsDirections = directionHelperService
+            .getDirectionsForAssignee(caseData.getStandardDirectionOrder().getDirections(), PARENTS_AND_RESPONDENTS);
+
+        caseDetails.getData().put("parentsAndRespondentsCustom", respondentsDirections);
+
+        List<Element<Respondent>> respondents = defaultIfNull(caseData.getRespondents1(), emptyList());
+
+        StringBuilder sb = new StringBuilder();
+
+        if (isNotEmpty(respondents)) {
+            AtomicInteger i = new AtomicInteger(1);
+
+            respondents.forEach(y -> {
+                    sb.append("Respondent")
+                        .append(" ")
+                        .append(i)
+                        .append(" ")
+                        .append("-")
+                        .append(" ")
+                        .append(y.getValue().getParty().firstName)
+                        .append(" ")
+                        .append(y.getValue().getParty().lastName)
+                        .append("\n");
+
+                    i.incrementAndGet();
+                });
+
+        } else {
+            sb.append("No respondents on the case");
+        }
+
+        caseDetails.getData().put("respondents1_label", sb.toString());
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDetails.getData())
