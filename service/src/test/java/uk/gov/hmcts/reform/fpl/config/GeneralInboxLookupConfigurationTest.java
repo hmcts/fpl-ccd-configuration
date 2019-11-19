210f5a51129d532f167ceca98aeffabbaf9513ad
amit <amitk@kainos.com>
2019-11-19 15:50:05 +0000

    removed blank line

--- service/src/test/java/uk/gov/hmcts/reform/fpl/config/GeneralInboxLookupConfigurationTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/config/GeneralInboxLookupConfigurationTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GeneralInboxLookupConfigurationTest {
-
     private static final String GENERAL_INBOX = "FamilyPublicLaw+generalInbox@gmail.com";
 
     private GeneralInboxLookupConfiguration generalInboxLookupConfiguration
         = new GeneralInboxLookupConfiguration(GENERAL_INBOX);
 
     @Test
-    void shouldReturnGeneralEmail() {
+    void shouldReturnGeneralInbox() {
         String generalInbox = generalInboxLookupConfiguration.getGeneralInbox();
 
         assertThat(generalInbox).isEqualTo(GENERAL_INBOX);
