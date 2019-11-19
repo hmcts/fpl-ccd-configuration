0c89ca227c4a522ee85e2bdd1365c489adbd1ebe
Etimbuk U <Gogetter@users.noreply.github.com>
2019-11-19 15:58:54 +0000

    feat: Notify parties of Order made on C21 (FPLA-603) (#593)
    * fpla-603- notify parties on c21
    
    update notification for c21 (FPLA-936)

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/C21OrderControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/C21OrderControllerTest.java
@@ -25,16 +25,30 @@ import uk.gov.hmcts.reform.fpl.service.DateFormatterService;
 import uk.gov.hmcts.reform.fpl.service.DocmosisDocumentGeneratorService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 import uk.gov.hmcts.reform.fpl.utils.FixedTimeConfiguration;
+import uk.gov.service.notify.NotificationClient;
 
+import java.time.LocalDate;
+import java.time.format.FormatStyle;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.CASE_TYPE;
+import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.JURISDICTION;
+import static uk.gov.hmcts.reform.fpl.NotifyTemplates.C21_ORDER_NOTIFICATION_TEMPLATE;
 import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.C21;
 import static uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle.HER_HONOUR_JUDGE;
+
+import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createC21Orders;
+import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createHearingBookings;
+import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createRespondents;
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.callbackRequest;
 import static uk.gov.hmcts.reform.fpl.utils.DocumentManagementStoreLoader.document;
 
@@ -44,12 +58,23 @@ import static uk.gov.hmcts.reform.fpl.utils.DocumentManagementStoreLoader.docume
 class C21OrderControllerTest {
     private static final String AUTH_TOKEN = "Bearer token";
     private static final String USER_ID = "1";
+    private static final String LOCAL_AUTHORITY_CODE = "example";
+    private static final String CAFCASS_EMAIL_ADDRESS = "cafcass@cafcass.com";
+    private static final String CAFCASS_NAME = "cafcass";
+    private static final String LOCAL_AUTHORITY_EMAIL_ADDRESS = "local-authority@local-authority.com";
+    private static final String LOCAL_AUTHORITY_NAME = "Example Local Authority";
+    private static final String FAMILY_MAN_CASE_NUMBER = "SACCCCCCCC5676576567";
+
+    private final LocalDate dateIn3Months = LocalDate.now().plusMonths(3);
 
     @MockBean
     private DocmosisDocumentGeneratorService docmosisDocumentGeneratorService;
 
     @MockBean
     private UploadDocumentService uploadDocumentService;
+
+    @MockBean
+    private NotificationClient notificationClient;
 
     @Autowired
     private DateFormatterService dateFormatterService;
@@ -123,6 +148,20 @@ class C21OrderControllerTest {
         assertThat(c21Orders.get(0).getValue()).isEqualTo(expectedOrder);
     }
 
+    @Test
+    void shouldTriggerC21EventWhenSubmitted() throws Exception {
+        String expectedCaseReference = "19898989";
+        makeSubmittedRequest(buildCallbackRequest());
+
+        verify(notificationClient, times(1)).sendEmail(
+            eq(C21_ORDER_NOTIFICATION_TEMPLATE), eq(LOCAL_AUTHORITY_EMAIL_ADDRESS),
+            eq(expectedC21LocalAuthorityParameters()), eq(expectedCaseReference));
+
+        verify(notificationClient, times(1)).sendEmail(
+            eq(C21_ORDER_NOTIFICATION_TEMPLATE), eq(CAFCASS_EMAIL_ADDRESS),
+            eq(expectedC21CafcassParameters()), eq(expectedCaseReference));
+    }
+
     private AboutToStartOrSubmitCallbackResponse makeRequest(CallbackRequest request, String endpoint)
         throws Exception {
         MvcResult mvc = mockMvc
@@ -136,5 +175,59 @@ class C21OrderControllerTest {
 
         return mapper.readValue(mvc.getResponse()
             .getContentAsByteArray(), AboutToStartOrSubmitCallbackResponse.class);
+    }
+
+    private void makeSubmittedRequest(CallbackRequest request)
+        throws Exception {
+        mockMvc
+            .perform(post("/callback/create-order/submitted")
+                .header("authorization", AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+            .andExpect(status().isOk())
+            .andReturn();
+    }
+
+    private CallbackRequest buildCallbackRequest() {
+        return CallbackRequest.builder()
+            .caseDetails(CaseDetails.builder()
+                .id(19898989L)
+                .data(ImmutableMap.of(
+                    "c21Orders", createC21Orders(),
+                    "hearingDetails", createHearingBookings(dateIn3Months),
+                    "respondents1", createRespondents(),
+                    "caseLocalAuthority", LOCAL_AUTHORITY_CODE,
+                    "familyManCaseNumber", FAMILY_MAN_CASE_NUMBER))
+                .build())
+            .build();
+    }
+
+    private Map<String, Object> commonNotificationParameters() {
+        final String documentUrl = "http://fake-document-gateway/documents/79ec80ec-7be6-493b-b4e6-f002f05b7079/binary";
+        final String subjectLine = "Jones, " + FAMILY_MAN_CASE_NUMBER;
+
+        return ImmutableMap.<String, Object>builder()
+            .put("subjectLine", subjectLine)
+            .put("linkToDocument", documentUrl)
+            .put("hearingDetailsCallout", subjectLine + ", hearing " + dateFormatterService.formatLocalDateToString(
+                dateIn3Months, FormatStyle.MEDIUM))
+            .put("reference", "19898989")
+            .put("caseUrl", "http://fake-url/case/" + JURISDICTION + "/" + CASE_TYPE + "/19898989")
+            .build();
+    }
+
+    private Map<String, Object> expectedC21CafcassParameters() {
+        return ImmutableMap.<String, Object>builder()
+            .putAll(commonNotificationParameters())
+            .put("localAuthorityOrCafcass", CAFCASS_NAME)
+            .build();
+    }
+
+    private Map<String, Object> expectedC21LocalAuthorityParameters() {
+        return ImmutableMap.<String, Object>builder()
+            .putAll(commonNotificationParameters())
+            .put("localAuthorityOrCafcass", LOCAL_AUTHORITY_NAME)
+            .build();
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/C21OrderController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/C21OrderController.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.reform.fpl.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +14,8 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.document.domain.Document;
+import uk.gov.hmcts.reform.fpl.config.GatewayConfiguration;
+import uk.gov.hmcts.reform.fpl.events.C21OrderEvent;
 import uk.gov.hmcts.reform.fpl.interfaces.C21CaseOrderGroup;
 import uk.gov.hmcts.reform.fpl.model.C21Order;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
@@ -22,10 +26,13 @@ import uk.gov.hmcts.reform.fpl.service.DocmosisDocumentGeneratorService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 import uk.gov.hmcts.reform.fpl.service.ValidateGroupService;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.C21;
 
+@Slf4j
 @Api
 @RequestMapping("/callback/create-order")
 @RestController
@@ -35,18 +42,24 @@ public class C21OrderController {
     private final ValidateGroupService validateGroupService;
     private final DocmosisDocumentGeneratorService docmosisDocumentGeneratorService;
     private final UploadDocumentService uploadDocumentService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final GatewayConfiguration gatewayConfiguration;
 
     @Autowired
     public C21OrderController(ObjectMapper mapper,
                               CreateC21OrderService service,
                               ValidateGroupService validateGroupService,
                               DocmosisDocumentGeneratorService docmosisDocumentGeneratorService,
-                              UploadDocumentService uploadDocumentService) {
+                              UploadDocumentService uploadDocumentService,
+                              ApplicationEventPublisher applicationEventPublisher,
+                              GatewayConfiguration gatewayConfiguration) {
         this.mapper = mapper;
         this.service = service;
         this.validateGroupService = validateGroupService;
         this.docmosisDocumentGeneratorService = docmosisDocumentGeneratorService;
         this.uploadDocumentService = uploadDocumentService;
+        this.applicationEventPublisher = applicationEventPublisher;
+        this.gatewayConfiguration = gatewayConfiguration;
     }
 
     @PostMapping("/about-to-start")
@@ -96,6 +109,17 @@ public class C21OrderController {
             .build();
     }
 
+    @PostMapping("/submitted")
+    public void handleSubmittedEvent(@RequestHeader(value = "authorization") String authorization,
+                                     @RequestHeader(value = "user-id") String userId,
+                                     @RequestBody CallbackRequest callbackRequest) {
+        CaseData caseData = mapper.convertValue(callbackRequest.getCaseDetails().getData(), CaseData.class);
+        String mostRecentUploadedDocumentUrl = service.mostRecentUploadedC21DocumentUrl(caseData.getC21Orders());
+
+        applicationEventPublisher.publishEvent(new C21OrderEvent(callbackRequest, authorization, userId,
+            concatGatewayConfigurationUrlAndMostRecentUploadedC21DocumentPath(mostRecentUploadedDocumentUrl)));
+    }
+
     private Document getDocument(String authorization,
                                  String userId,
                                  CaseData caseData) {
@@ -104,5 +128,18 @@ public class C21OrderController {
 
         return uploadDocumentService.uploadPDF(userId, authorization, document.getBytes(),
             C21.getDocumentTitle());
+    }
+
+    private String concatGatewayConfigurationUrlAndMostRecentUploadedC21DocumentPath(
+        final String mostRecentUploadedC21DocumentUrl) {
+        final String gatewayUrl = gatewayConfiguration.getUrl();
+
+        try {
+            URI uri = new URI(mostRecentUploadedC21DocumentUrl);
+            return gatewayUrl + uri.getPath();
+        } catch (URISyntaxException e) {
+            log.error(mostRecentUploadedC21DocumentUrl + " url incorrect.", e);
+        }
+        return "";
     }
 }
