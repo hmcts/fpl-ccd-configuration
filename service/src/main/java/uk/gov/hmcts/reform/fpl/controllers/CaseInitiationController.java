54d5736bedba186ab5d142bcbec88ef712f1563c
Toireasa Moley <toireasa.moley@hmcts.net>
2019-11-20 15:17:26 +0000

    Adding case initiation controller

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/CaseInitiationController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/CaseInitiationController.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.fpl.controllers;
 
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
