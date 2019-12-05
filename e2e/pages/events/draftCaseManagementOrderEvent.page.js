a3c09bc28acb426434e0ecb61d268dae647f1513
Lewis Birks <lewisb@kainos.com>
2019-12-05 13:25:05 +0000

    Fixed e2e tests

--- e2e/pages/events/draftCaseManagementOrderEvent.page.js
+++ e2e/pages/events/draftCaseManagementOrderEvent.page.js
@@ -43,34 +43,29 @@ module.exports = {
     I.selectOption(this.fields.cmoHearingDateList, date);
   },
 
-  validatePreviousSelectedHearingDate(date) {
-    I.waitForElement(this.fields.cmoHearingDateList);
-    I.see(date, this.fields.cmoHearingDateList);
-  },
-
   async enterDirection(direction) {
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('allPartiesCustom', direction.title, direction.description);
     await directions.enterDate('allPartiesCustom', direction.dueDate);
-    await I.click('Continue');
+    await I.retryUntilExists(() => I.click('Continue'), '#localAuthorityDirectionsLabelCMO');
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('localAuthorityDirectionsCustom', direction.title, direction.description);
     await directions.enterDate('localAuthorityDirectionsCustom', direction.dueDate);
-    await I.click('Continue');
+    await I.retryUntilExists(() => I.click('Continue'), '#respondentsDirectionLabelCMO');
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('respondentDirectionsCustom', direction.title, direction.description);
     await I.selectOption(this.fields.respondentDirectionsCustom.assigneeDropdown, 'Respondent 1');
     await directions.enterDate('respondentDirectionsCustom', direction.dueDate);
-    await I.click('Continue');
+    await I.retryUntilExists(() => I.click('Continue'), '#cafcassDirectionsLabelCMO');
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('cafcassDirectionsCustom', direction.title, direction.description);
     await directions.enterDate('cafcassDirectionsCustom', direction.dueDate);
-    await I.click('Continue');
+    await I.retryUntilExists(() => I.click('Continue'), '#otherPartiesDirectionLabelCMO');
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('otherPartiesDirectionsCustom', direction.title, direction.description);
     I.selectOption(this.fields.otherPartiesDirectionsCustom.assigneeDropdown, 'Person 1');
     await directions.enterDate('otherPartiesDirectionsCustom', direction.dueDate);
-    await I.click('Continue');
+    await I.retryUntilExists(() => I.click('Continue'), '#courtDirectionsLabelCMO');
     await I.addAnotherElementToCollection();
     await directions.enterTitleAndDescription('courtDirectionsCustom', direction.title, direction.description);
     await directions.enterDate('courtDirectionsCustom', direction.dueDate);
