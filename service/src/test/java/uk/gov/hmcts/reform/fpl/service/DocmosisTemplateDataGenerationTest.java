4a009bfc427c2abafa25eb3d289d41627dcdf1ad
Lewis Birks <lewisb@kainos.com>
2019-12-06 11:33:49 +0000

    Code review comments

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/DocmosisTemplateDataGenerationTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/DocmosisTemplateDataGenerationTest.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.fpl.service.DocmosisTemplateDataGeneration.generateDraftWatermarkEncodedString;
 
 class DocmosisTemplateDataGenerationTest {
 
@@ -18,12 +17,6 @@ class DocmosisTemplateDataGenerationTest {
             throw new NotImplementedException("Not testing this method");
         }
     };
-
-    @Test
-    void shouldGenerateEncodedStringWhenGenerateDraftWatermarkCalled() throws IOException {
-        String encodedImgString = generateDraftWatermarkEncodedString();
-        assertThat(encodedImgString).isNotEqualTo("");
-    }
 
     @Test
     void shouldPopulateAMapWithWaterMarkEncoding() throws IOException {
