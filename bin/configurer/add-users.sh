9d22a56a6925d0678d2389fd19ad56508f95a0da
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2019-12-06 18:23:40 +0000

    Updated add-users script: moved users to external file

--- bin/configurer/add-users.sh
+++ bin/configurer/add-users.sh
@@ -4,17 +4,6 @@ set -eu
 
 dir=$(dirname ${0})
 
-${dir}/utils/idam-create-caseworker.sh "damian@swansea.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "kurt@swansea.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "james@swansea.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "sam@hillingdon.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "siva@hillingdon.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "raghu@swindon.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "sam@swindon.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "raghu@wiltshire.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "sam@wiltshire.gov.uk" "caseworker,caseworker-publiclaw,caseworker-publiclaw-solicitor" "(local-authority)"
-${dir}/utils/idam-create-caseworker.sh "hmcts-admin@example.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-courtadmin" "(hmcts-admin)"
-${dir}/utils/idam-create-caseworker.sh "cafcass@example.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-cafcass" "(cafcass)"
-${dir}/utils/idam-create-caseworker.sh "judiciary@mailnesia.com" ",caseworker-publiclaw-courtadmin, caseworker,caseworker-publiclaw,caseworker-publiclaw-judiciary,caseworker-publiclaw-gatekeeper" "(judiciary)"
-${dir}/utils/idam-create-caseworker.sh "gatekeeper@mailnesia.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-gatekeeper,caseworker-publiclaw-courtadmin" "(gatekeeper)"
-${dir}/utils/idam-create-caseworker.sh "fpl-system-update@mailnesia.com" "caseworker,caseworker-publiclaw,caseworker-publiclaw-systemupdate" "(system-update)"
+jq -r '.[] | .email + " " + .roles + " " +  .lastName' ${dir}/users.json | while read args; do
+  ${dir}/utils/idam-create-caseworker.sh $args
+done
