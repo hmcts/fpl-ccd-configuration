b66afcc10e089f1e2cfd3dc7cc53641d9f2a3f54
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2019-12-09 12:06:28 +0000

    Added generating mock response from users.json file and database call

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -4,34 +4,42 @@ set -eu
 
 dir=$(dirname ${0})
 root_dir=$(realpath ${dir}/../../..)
-resources_dir=${root_dir}/service/src/main/resources
-wiremock_dir=${root_dir}/docker/wiremock
-swansea_org_mock_file=${wiremock_dir}/__files/swanseaOrg.json
 
-usernames=(damian kurt james)
+mock_file=${root_dir}/docker/wiremock/__files/mockResponse.json
+mock_tmp_file=${root_dir}/docker/wiremock/__files/mockResponse.tmp.json
+users_file=${root_dir}/bin/configurer/users.json
+users_ids_tmp_file=${dir}/userIds.json.tmp
 
-function get_user_ids() {
-  docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';" openidm
+user_mappings_file=${root_dir}/service/src/main/resources/application-user-mappings.yaml
+
+function query_db() {
+  docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "$1" openidm
 }
 
-swansea_user_ids=$(echo $(get_user_ids swansea) | tr -d ' ')
+function get_all_users_email_id_mappings() {
+  query_db "SELECT json_object(array_agg(fullobject->>'mail'), array_agg(fullobject->>'_id')) FROM managedObjects WHERE fullobject->>'mail' IS NOT NULL;"
+}
 
-echo "[]" > $swansea_org_mock_file
-for user_id in ${swansea_user_ids//,/ }
+function get_users_ids_by_name_like() {
+  query_db "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';"
+}
+
+cp $users_file $mock_file
+echo $(get_all_users_email_id_mappings) >> $users_ids_tmp_file
+
+for i in `seq 1 $(jq '. | length' $mock_file)`
 do
-  jq --argjson userInfo "$(jq --arg userId "$user_id" '.userIdentifier=$userId' ${wiremock_dir}/templates/userTemplate.json)" '. += [$userInfo]' $swansea_org_mock_file > swanseaOrg.json.tmp && mv swanseaOrg.json.tmp $swansea_org_mock_file
+  user_id=$(jq -r --arg email $(jq -r --argjson index $i '.[$index-1].email' $mock_file) '.[$email]' $users_ids_tmp_file)
+  jq -r --argjson index $i --arg user_id $user_id 'if $user_id=="null" then .[$index-1].userIdentifier="" else .[$index-1].userIdentifier=$user_id end | .[$index-1].firstName=.[$index-1].email | .[$index-1].idamStatus="ACTIVE" | .[$index-1].idamStatusCode="0" | .[$index-1].idamMessage="" | .[$index-1].roles|=split(",")' $mock_file > $mock_tmp_file && mv $mock_tmp_file $mock_file
 done
 
-for i in `seq 1 $(jq '. | length' $swansea_org_mock_file)`
-do
-  jq --argjson index $i --arg email "${usernames[$i-1]}@swansea.gov.uk" '.[$index-1].email=$email | .[$index-1].firstName=$email' $swansea_org_mock_file > swanseaOrg.json.tmp && mv swanseaOrg.json.tmp $swansea_org_mock_file
-done
+rm $users_ids_tmp_file
 
-hillingdon_user_ids=$(echo $(get_user_ids hillingdon) | tr -d ' ')
-swindon_user_ids=$(echo $(get_user_ids swindon) | tr -d ' ')
-wiltshire_user_ids=$(echo $(get_user_ids wiltshire) | tr -d ' ')
+hillingdon_user_ids=$(echo $(get_users_ids_by_name_like hillingdon) | tr -d ' ')
+swindon_user_ids=$(echo $(get_users_ids_by_name_like swindon) | tr -d ' ')
+wiltshire_user_ids=$(echo $(get_users_ids_by_name_like wiltshire) | tr -d ' ')
 
-cat > ${resources_dir}/application-user-mappings.yaml <<EOL
+cat > $user_mappings_file <<EOL
 spring:
   profiles: user-mappings
 
