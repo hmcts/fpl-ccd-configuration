3c2f1e76e0de762a91a571fc4f7df053e93284a0
Adam Komisarek <adam.komisarek@hmcts.net>
2019-12-09 19:13:00 +0000

    fixing build.

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/TestConfiguration.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.fpl.controllers;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-
-@EnableFeignClients("uk.gov.hmcts.reform.rd.client")
-@org.springframework.boot.test.context.TestConfiguration
-public class TestConfiguration {
-}
--- service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.reform.fpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.idam.client", "uk.gov.hmcts.reform.rd.client"})
+@ComponentScan
 @EnableAsync
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
