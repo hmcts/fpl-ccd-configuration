e763b9657e7984c30ec1fcc30568502ab6a1a786
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2019-12-10 14:03:52 +0000

    Wiremock response generated for selected organisation

--- bin/configurer/utils/generate-local-user-mappings.sh
+++ bin/configurer/utils/generate-local-user-mappings.sh
@@ -16,28 +16,32 @@ function query_db() {
   docker run -e PGPASSWORD='openidm' --rm --network ccd-network postgres:11-alpine psql --host shared-db  --username openidm --tuples-only  --command "$1" openidm
 }
 
-function get_all_users_email_id_mappings() {
-  query_db "SELECT json_object(array_agg(fullobject->>'mail'), array_agg(fullobject->>'_id')) FROM managedObjects WHERE fullobject->>'mail' IS NOT NULL;"
+function get_users_email_id_mappings() {
+  query_db "SELECT json_object(array_agg(fullobject->>'mail'), array_agg(fullobject->>'_id')) FROM managedObjects WHERE fullobject->>'mail' IS NOT NULL AND fullobject->>'userName' LIKE '%$1%';"
 }
 
-function get_users_ids_by_name_like() {
+function get_users_ids() {
   query_db "SELECT string_agg(fullobject->>'_id',',') FROM managedObjects WHERE fullobject->>'userName' LIKE '%$1%';"
 }
 
-cp $users_file $mock_file
-echo $(get_all_users_email_id_mappings) >> $users_ids_tmp_file
+function create_mock_response() {
+  jq --arg organisation $1 '[.[] | select(.email | contains($organisation))]' $users_file > $mock_file
+  echo $(get_users_email_id_mappings $1) > $users_ids_tmp_file
 
-for i in `seq 1 $(jq '. | length' $mock_file)`
-do
-  user_id=$(jq -r --arg email $(jq -r --argjson index $i '.[$index-1].email' $mock_file) '.[$email]' $users_ids_tmp_file)
-  jq -r --argjson index $i --arg user_id $user_id 'if $user_id=="null" then .[$index-1].userIdentifier="" else .[$index-1].userIdentifier=$user_id end | .[$index-1].firstName=.[$index-1].email | .[$index-1].idamStatus="ACTIVE" | .[$index-1].idamStatusCode="0" | .[$index-1].idamMessage="" | .[$index-1].roles|=split(",")' $mock_file > $mock_tmp_file && mv $mock_tmp_file $mock_file
-done
+  for i in `seq 1 $(jq '. | length' $mock_file)`
+  do
+    email=$(jq -r --argjson index $i '.[$index-1].email' $mock_file)
+    user_id=$(jq -r --arg email $email '.[$email]' $users_ids_tmp_file)
+    jq -r --argjson index $i --arg user_id $user_id '.[$index-1].userIdentifier=$user_id | .[$index-1].firstName=.[$index-1].email | .[$index-1].idamStatus="ACTIVE" | .[$index-1].idamStatusCode="0" | .[$index-1].idamMessage="" | .[$index-1].roles|=split(",")' $mock_file > $mock_tmp_file && mv $mock_tmp_file $mock_file
+  done
 
-rm $users_ids_tmp_file
+  rm $users_ids_tmp_file
+}
 
-hillingdon_user_ids=$(echo $(get_users_ids_by_name_like hillingdon) | tr -d ' ')
-swindon_user_ids=$(echo $(get_users_ids_by_name_like swindon) | tr -d ' ')
-wiltshire_user_ids=$(echo $(get_users_ids_by_name_like wiltshire) | tr -d ' ')
+create_mock_response swansea
+hillingdon_user_ids=$(echo $(get_users_ids hillingdon) | tr -d ' ')
+swindon_user_ids=$(echo $(get_users_ids swindon) | tr -d ' ')
+wiltshire_user_ids=$(echo $(get_users_ids wiltshire) | tr -d ' ')
 
 cat > $user_mappings_file <<EOL
 spring:
