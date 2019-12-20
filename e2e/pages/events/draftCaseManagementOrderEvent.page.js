59dbbf773e50c18a32069e4bbd4e6d80ee40fceb
swalker125 <sam.walker@digit-hull.com>
2019-12-20 15:52:58 +0000

    update e2e test

--- e2e/pages/events/draftCaseManagementOrderEvent.page.js
+++ e2e/pages/events/draftCaseManagementOrderEvent.page.js
@@ -31,7 +31,7 @@ module.exports = {
 
   staticFields: {
     statusRadioGroup: {
-      groupName: '#caseManagementOrder_status',
+      groupName: '#caseManagementOrder_LocalAuthority_status',
       sendToJudge: 'Yes, send this to the judge',
       partiesReview: 'No, parties need to review it',
       selfReview: 'No, I need to make changes',
