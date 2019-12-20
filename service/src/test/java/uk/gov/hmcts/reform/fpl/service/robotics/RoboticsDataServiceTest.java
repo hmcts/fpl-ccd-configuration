877391e838e94b8e8fc4f1b4e73830d8f60ca773
Etimbuk <euedofia@gmail.com>
2019-12-20 18:45:53 +0000

    FPLA-720 initial grounds works -- tests

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/robotics/RoboticsDataServiceTest.java
@@ -0,0 +1,8 @@
+package uk.gov.hmcts.reform.fpl.service.robotics;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class RoboticsDataServiceTest {
+}
