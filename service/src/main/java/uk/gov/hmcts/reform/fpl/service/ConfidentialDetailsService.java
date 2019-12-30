d9e469e04741cee0d60be05474341dc277d6c9b6
swalker125 <sam.walker@digit-hull.com>
2019-12-30 16:44:06 +0000

    addressed code review comments

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/ConfidentialDetailsService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ConfidentialDetailsService.java
@@ -23,12 +23,12 @@ public class ConfidentialDetailsService {
         details.stream()
             .filter(element -> element.getValue() != null)
             .forEach(value -> {
-                if (personType == Respondent.class) {
+                if (personType.isInstance(Respondent.class)) {
                     if (respondentDetailsAreHidden(value)) {
                         confidentialList.add(value);
                     }
                 }
-                if (personType == Child.class) {
+                if (personType.isInstance(Child.class)) {
                     if (childDetailsAreHidden(value)) {
                         confidentialList.add(value);
                     }
@@ -38,14 +38,14 @@ public class ConfidentialDetailsService {
         return confidentialList;
     }
 
-    private <T> boolean respondentDetailsAreHidden(Element<T> x) {
-        Respondent respondent = mapper.convertValue(x.getValue(), Respondent.class);
+    private <T> boolean respondentDetailsAreHidden(Element<T> element) {
+        Respondent respondent = mapper.convertValue(element.getValue(), Respondent.class);
         return respondent.getParty() != null && respondent.getParty().getContactDetailsHidden() != null
             && respondent.getParty().getContactDetailsHidden().equals("Yes");
     }
 
-    private <T> boolean childDetailsAreHidden(Element<T> x) {
-        Child child = mapper.convertValue(x.getValue(), Child.class);
+    private <T> boolean childDetailsAreHidden(Element<T> element) {
+        Child child = mapper.convertValue(element.getValue(), Child.class);
         return child.getParty() != null && child.getParty().getDetailsHidden() != null
             && child.getParty().getDetailsHidden().equals("Yes");
     }
