24108fe491545d35cda43230e9686411f37c9432
Adam Komisarek <adam.komisarek@hmcts.net>
2019-12-31 11:48:29 +0000

    fix: add logs for debug

--- bin/configurer/utils/ccd-add-role.sh
+++ bin/configurer/utils/ccd-add-role.sh
@@ -7,8 +7,11 @@ dir=$(dirname ${0})
 role=${1}
 
 userToken=$(${dir}/idam-lease-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12})
+
+echo "User token: ${userToken}"
 serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
 
+echo "Service token: ${serviceToken}"
 echo "Creating CCD role: ${role}"
 
 curl --insecure --fail --show-error --silent --output /dev/null -X PUT \
