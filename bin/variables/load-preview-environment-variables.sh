3a1f0e364c6554524d96317a1102f067fe79d039
Adam Komisarek <adam.komisarek@hmcts.net>
2019-12-31 13:15:17 +0000

    imp: use IDAM on preview when AAT idam is down

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -8,9 +8,14 @@ echo 'export ENVIRONMENT=preview'
 
 # urls
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
-echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
-echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://definition-store-api-fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
+echo "export IDAM_API_BASE_URL=http://api-idam-fpl.service.core-compute-preview.internal"
+echo "export CCD_IDAM_REDIRECT_URL=https://case-management-web-fpl-case-service-pr-${pr}.service.core-compute-preview.internal/oauth2redirect"
+echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://definition-store-api-fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
 
 # definition placeholders
 echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
+
+# secrets - unset to fallback to defaults
+unset CCD_API_GATEWAY_IDAM_CLIENT_SECRET
+unset CCD_CONFIGURER_IMPORTER_USERNAME
+unset CCD_CONFIGURER_IMPORTER_PASSWORD
