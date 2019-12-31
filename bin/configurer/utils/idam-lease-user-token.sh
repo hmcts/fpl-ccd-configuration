4e21c1b7aa651087cba00206b232c217ecf72704
Adam Komisarek <adam.komisarek@hmcts.net>
2019-12-31 12:15:05 +0000

    fix: hardcode importer username/password

--- bin/configurer/utils/idam-lease-user-token.sh
+++ bin/configurer/utils/idam-lease-user-token.sh
@@ -2,10 +2,9 @@
 
 set -eu
 
-username=${1}
-password=${2}
-clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
-redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
+username=ccd.docker.default@hmcts.net
+password=Password12
+redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect }
 
 code=$(curl --insecure --fail --show-error --silent -X POST --user "${username}:${password}" "${IDAM_API_BASE_URL:-http://localhost:5000}/oauth2/authorize?redirect_uri=${redirectUri}&response_type=code&client_id=ccd_gateway" -d "" | docker run --rm --interactive stedolan/jq -r .code)
 
