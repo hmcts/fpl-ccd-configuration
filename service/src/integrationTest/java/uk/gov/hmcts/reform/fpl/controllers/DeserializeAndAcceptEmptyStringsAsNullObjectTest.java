886f3480ee28feffaebc7c972ca986068e5e18a9
Toireasa Moley <toireasa.moley@hmcts.net>
2020-01-08 17:49:54 +0000

    Adding assert to method

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DeserializeAndAcceptEmptyStringsAsNullObjectTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DeserializeAndAcceptEmptyStringsAsNullObjectTest.java
@@ -6,11 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.fpl.enums.DirectionAssignee;
+import uk.gov.hmcts.reform.fpl.enums.ParentsAndRespondentsDirectionAssignee;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Direction;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.utils.ResourceReader;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ActiveProfiles("integration-test")
 @WebMvcTest(DeserializeAndAcceptEmptyStringsAsNullObjectTest.class)
@@ -23,7 +30,12 @@ class DeserializeAndAcceptEmptyStringsAsNullObjectTest {
 
     @Test
     void shouldDeserializeAndAcceptEmptyStringsAsNullObject() throws IOException {
-        mapper.readValue(ResourceReader.readString("fixtures/caseDataWithEmptyStringsInsteadOfNull.json"), CaseData.class);
+        CaseData data = mapper.readValue(ResourceReader.readString("fixtures/caseDataWithEmptyStringsInsteadOfNull.json"), CaseData.class);
+
+        ParentsAndRespondentsDirectionAssignee parentsAndRespondentsDirectionAssignee = data.getCourtDirections().get(0).getValue().getParentsAndRespondentsAssignee();
+
+        assertNull(parentsAndRespondentsDirectionAssignee);
+
 
     }
 }
