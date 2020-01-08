f7fec73c6836e9c807a9f114fe8387ab1fb5724b
Toireasa Moley <toireasa.moley@hmcts.net>
2020-01-08 16:08:45 +0000

    Adding integration test

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DeserializeAndAcceptEmptyStringsAsNullObjectTest.java
@@ -0,0 +1,28 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+
+import java.io.File;
+import java.io.IOException;
+
+@ActiveProfiles("integration-test")
+@WebMvcTest(DeserializeAndAcceptEmptyStringsAsNullObjectTest.class)
+@OverrideAutoConfiguration(enabled = true)
+@SuppressWarnings("unchecked")
+class DeserializeAndAcceptEmptyStringsAsNullObjectTest {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    void shouldDeserializeAndAcceptEmptyStringsAsNullObject() throws IOException {
+        CaseData caseData = mapper.readValue(new
+            File("/Users/toireasam/Documents/FPLA/fpl-ccd-configuration/service/src/integrationTest/resources/fixtures/caseDataWithEmptyStringsInsteadOfNull.json"), CaseData.class);
+    }
+}
