5474f8ee0961008b063e930410b53b9c880fc685
Toireasa Moley <toireasa.moley@hmcts.net>
2020-01-09 10:31:48 +0000

    Fixing code review comments:

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DeserializeAndAcceptEmptyStringsAsNullObjectTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DeserializeAndAcceptEmptyStringsAsNullObjectTest.java
@@ -63,6 +63,7 @@ class DeserializeAndAcceptEmptyStringsAsNullObjectTest {
                         .respondingOnBehalfOf("")
                         .build())
                     .responses(null)
-                    .build()).build();
+                    .build())
+            .build();
     }
 }
