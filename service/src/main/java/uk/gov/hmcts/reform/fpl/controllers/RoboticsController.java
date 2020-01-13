4f66af8fe987cf28118bf3a4381151ce87a6c313
Etimbuk <euedofia@gmail.com>
2020-01-13 11:55:32 +0000

    FPLA-998 robotics support controller with resendCaseDataNotification impl

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RoboticsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RoboticsController.java
@@ -10,29 +10,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.events.robotics.ResendFailedRoboticNotificationEvent;
-import uk.gov.hmcts.reform.fpl.model.ccd.CoreCaseApiSearchParameter;
 import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
 
 @Api
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RoboticsController {
-    public static final String JURISDICTION_ID = "PUBLICLAW";
-    public static final String CASE_TYPE_ID = "CARE_SUPERVISION_EPO";
-
     private final CoreCaseDataService coreCaseDataService;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @PostMapping("/sendRPAEmailByID/{caseId}")
     @Secured("caseworker-publiclaw-systemupdate")
     public void resendCaseDataNotification(@PathVariable ("caseId") String caseId) {
-        CoreCaseApiSearchParameter coreCaseSearchParameter = CoreCaseApiSearchParameter.builder()
-            .caseId(caseId)
-            .caseType(CASE_TYPE_ID)
-            .jurisdiction(JURISDICTION_ID)
-            .build();
-
-        CaseDetails caseDetails = coreCaseDataService.performCaseSearch(coreCaseSearchParameter);
+        CaseDetails caseDetails = coreCaseDataService.findCaseDetailsById(caseId);
         applicationEventPublisher.publishEvent(new ResendFailedRoboticNotificationEvent(caseDetails));
     }
 }
