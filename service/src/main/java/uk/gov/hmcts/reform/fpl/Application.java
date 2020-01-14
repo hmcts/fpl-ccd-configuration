8a647938f1d45b08926aaac7aca45a6d1dc3c72e
Etimbuk <euedofia@gmail.com>
2020-01-14 08:53:54 +0000

    FPLA-998 revert changes

--- service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = {
-    "uk.gov.hmcts.reform.idam.client",
-    "uk.gov.hmcts.reform.rd.client"})
+@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.idam.client", "uk.gov.hmcts.reform.rd.client"})
 @ComponentScan
 @EnableAsync
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
