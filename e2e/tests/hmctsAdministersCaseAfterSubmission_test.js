8204e40c16e0ef1cd6848001f8e52f5384483eb4
James Nelson <Jamesnelson117@gmail.com>
2020-01-15 11:15:49 +0000

    FPLA-830: Fixed e2e

--- e2e/tests/hmctsAdministersCaseAfterSubmission_test.js
+++ e2e/tests/hmctsAdministersCaseAfterSubmission_test.js
@@ -30,7 +30,7 @@ Before(async (I, caseViewPage, submitApplicationEventPage) => {
   await I.navigateToCaseDetails(caseId);
 });
 
-Scenario('HMCTS admin enters FamilyMan reference number@f', async (I, caseViewPage, loginPage, enterFamilyManCaseNumberEventPage) => {
+Scenario('HMCTS admin enters FamilyMan reference number', async (I, caseViewPage, loginPage, enterFamilyManCaseNumberEventPage) => {
   await caseViewPage.goToNewActions(config.administrationActions.addFamilyManCaseNumber);
   enterFamilyManCaseNumberEventPage.enterCaseID();
   await I.completeEvent('Save and continue');
@@ -138,7 +138,7 @@ Scenario('HMCTS admin enters hearing details and submits', async (I, caseViewPag
   I.seeAnswerInTab(4, 'Judge and legal advisor', 'Legal advisor\'s full name', hearingDetails[1].judgeAndLegalAdvisor.legalAdvisorName);
 });
 
-Scenario('HMCTS admin creates multiple orders for the case@f', async (I, caseViewPage, createOrderEventPage) => {
+Scenario('HMCTS admin creates multiple orders for the case', async (I, caseViewPage, createOrderEventPage) => {
   for(let i = 0; i < orders.length; i++) {
     await caseViewPage.goToNewActions(config.administrationActions.createOrder);
     await orderFunctions.createOrder(I, createOrderEventPage, orders[i]);
