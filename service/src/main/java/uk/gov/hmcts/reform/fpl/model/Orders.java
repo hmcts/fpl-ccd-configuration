3b8d2cc79e8fa6f37668e91dcb6fcc81c0d51a99
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-01-16 17:35:26 +0000

    refactor: update email content provider to use caseData (#784)
    changes to AbstractEmailContentProvider to make use of caseData + code improvements
    moved getFirstRespondentLastName() to new helper class PeopleInCaseHelper

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/Orders.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/Orders.java
@@ -3,6 +3,8 @@ package uk.gov.hmcts.reform.fpl.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.reform.fpl.config.utils.EmergencyProtectionOrderDirectionsType;
+import uk.gov.hmcts.reform.fpl.config.utils.EmergencyProtectionOrdersType;
 import uk.gov.hmcts.reform.fpl.enums.OrderType;
 
 import java.util.List;
@@ -16,6 +18,7 @@ public class Orders {
     @NotNull(message = "Select at least one type of order")
     @Size(min = 1, message = "Select at least one type of order")
     private final List<OrderType> orderType;
+    private final List<EmergencyProtectionOrdersType> emergencyProtectionOrders;
     private final String directions;
-    private final List<String> emergencyProtectionOrderDirections;
+    private final List<EmergencyProtectionOrderDirectionsType> emergencyProtectionOrderDirections;
 }
