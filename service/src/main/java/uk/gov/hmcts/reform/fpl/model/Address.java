d88f31487fa1e4c5919311c4273fc8a0ada74334
James Nelson <Jamesnelson117@gmail.com>
2020-01-21 14:11:13 +0000

    feat: emergency protection order (FPLA-830) (#788)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/Address.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/Address.java
@@ -1,26 +1,34 @@
 package uk.gov.hmcts.reform.fpl.model;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
 import lombok.Builder;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import uk.gov.hmcts.reform.fpl.validation.groups.epoordergroup.EPOAddressGroup;
 
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotBlank;
+import javax.validation.groups.Default;
+
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Address {
-    @NotBlank(message = "Enter a valid address for the contact")
+    @NotBlank(message = "Enter a valid address for the contact", groups = { Default.class, EPOAddressGroup.class })
     private final String addressLine1;
     private final String addressLine2;
     private final String addressLine3;
     private final String postTown;
     private final String county;
-    @NotBlank(message = "Enter a postcode for the contact")
+    @NotBlank(message = "Enter a postcode for the contact", groups = { Default.class, EPOAddressGroup.class })
     private final String postcode;
     private final String country;
 
@@ -39,5 +47,21 @@ public class Address {
         this.county = county;
         this.postcode = postcode;
         this.country = country;
+    }
+
+    @JsonIgnore
+    public String getAddressAsString() {
+        ImmutableList<String> addressAsList = ImmutableList.of(
+            defaultIfNull(getAddressLine1(), ""),
+            defaultIfNull(getAddressLine2(), ""),
+            defaultIfNull(getAddressLine3(), ""),
+            defaultIfNull(getPostTown(), ""),
+            defaultIfNull(getCounty(), ""),
+            defaultIfNull(getPostcode(), ""),
+            defaultIfNull(getCountry(), ""));
+
+        return addressAsList.stream()
+            .filter(StringUtils::isNotBlank)
+            .collect(Collectors.joining(", "));
     }
 }
---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/validation/validators/time/TimeRangeValidator.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.fpl.validation.validators.time;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.fpl.service.time.Time;
+import uk.gov.hmcts.reform.fpl.validation.interfaces.time.TimeDifference;
+import uk.gov.hmcts.reform.fpl.validation.interfaces.time.TimeRange;
+
+import java.time.LocalDateTime;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+public class TimeRangeValidator implements ConstraintValidator<TimeRange, LocalDateTime> {
+
+    private TimeDifference maxDate;
+    private final Time time;
+
+    @Autowired
+    public TimeRangeValidator(Time time) {
+        this.time = time;
+    }
+
+    @Override
+    public void initialize(TimeRange annotation) {
+        maxDate = annotation.maxDate();
+    }
+
+    @Override
+    public boolean isValid(LocalDateTime value, ConstraintValidatorContext context) {
+        LocalDateTime now = time.now();
+        LocalDateTime rangeEnd = now.plus(maxDate.amount(), maxDate.unit());
+
+        return !value.isAfter(rangeEnd) || value.isEqual(now);
+    }
+}
