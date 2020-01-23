6d36f17a4337fb9f6a06b254d8be7b23af92d4f5
Joe Zazzaro-Francis <joez@Gabbana.local>
2020-01-23 16:06:26 +0000

    Attempts to ignore EqualsAndHashCode from sonarcloud

--- service/src/main/java/uk/gov/hmcts/reform/fpl/events/CaseManagementOrderRejectedEvent.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/events/CaseManagementOrderRejectedEvent.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.fpl.events;
 
 import lombok.EqualsAndHashCode;
+import lombok.Generated;
 import lombok.Value;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 
 @Value
-@EqualsAndHashCode(callSuper = true)
+@Generated
+@EqualsAndHashCode(callSuper = true) //NOSONAR
 public class CaseManagementOrderRejectedEvent extends CallbackEvent {
     public CaseManagementOrderRejectedEvent(CallbackRequest callbackRequest, String authorization, String userId) {
         super(callbackRequest, authorization, userId);
