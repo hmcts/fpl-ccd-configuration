ef83b211b66c7e67cf0a3cd53067cb41a779ac34
Etimbuk <euedofia@gmail.com>
2020-02-03 12:41:00 +0000

    FPLA-1274 format and validate telephone and mobile number for Applicants

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/robotics/Applicant.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/robotics/Applicant.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder(toBuilder = true)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -12,7 +15,13 @@ public class Applicant {
     private final String contactName;
     private final String jobTitle;
     private final Address address;
+
+    @Pattern(regexp = "^\\+?\\d{10,24}$", message = "is invalid")
     private final String mobileNumber;
+
+    @Pattern(regexp = "^\\+?\\d{10,24}$", message = "is invalid")
     private final String telephoneNumber;
+
+    @Email(message = "is invalid")
     private final String email;
 }
