6b9cee493a6ad21abf305b37c8cb005b86a58cfc
Etimbuk <euedofia@gmail.com>
2020-02-03 21:57:23 +0000

    FPLA-1274 removed @Email annotation from robotics.Applicant

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/robotics/Applicant.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/robotics/Applicant.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -22,6 +21,5 @@ public class Applicant {
     @Pattern(regexp = "^\\+?\\d{10,24}$", message = "is invalid")
     private final String telephoneNumber;
 
-    @Email(message = "is invalid")
     private final String email;
 }
