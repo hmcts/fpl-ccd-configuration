bb2f455fb478553bb67659c2347762644de2381e
Sam Walker <sam.walker@hmcts.net>
2020-02-03 15:56:21 +0000

    fixed javascript spacing issues

--- e2e/helpers/placement_helper.js
+++ e2e/helpers/placement_helper.js
@@ -1,15 +1,13 @@
 const config = require('../config.js');
 
-const assertCafcassCannotSeePlacementOrder = async (I, caseViewPage) => {
-    I.signOut();
-    await I.signIn(config.cafcassEmail, config.cafcassPassword);
-
-    await I.navigateToCaseDetails(caseId);
-
-    caseViewPage.selectTab(caseViewPage.tabs.placement);
-    I.dontSee('Placement order');
+const assertCafcassCannotSeePlacementOrder = async (I, caseViewPage, caseId) => {
+  I.signOut();
+  await I.signIn(config.cafcassEmail, config.cafcassPassword);
+  await I.navigateToCaseDetails(caseId);
+  caseViewPage.selectTab(caseViewPage.tabs.placement);
+  I.dontSee('Placement order');
 };
 
 module.exports = {
-    assertCafcassCannotSeePlacementOrder
+  assertCafcassCannotSeePlacementOrder,
 };
