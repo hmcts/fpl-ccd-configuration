d606c13a92d7c391ba0a2dd3628e80bc64e71d89
James Nelson <Jamesnelson117@gmail.com>
2020-02-05 16:39:06 +0000

    FPLA-1170: caseDetailsHelper formatCCDCaseNumber will now throw an exception if arg exceeds 16 digits

--- service/src/main/java/uk/gov/hmcts/reform/fpl/utils/CaseDetailsHelper.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/CaseDetailsHelper.java
@@ -8,6 +8,10 @@ public class CaseDetailsHelper {
     }
 
     public static String formatCCDCaseNumber(Long caseNumber) {
+        if (String.valueOf(caseNumber).length() != 16) {
+            throw new IllegalArgumentException("CCD Case number must be 16 digits long");
+        }
+
         return String.join("-", Splitter.fixedLength(4).splitToList(String.valueOf(caseNumber)));
     }
 }
--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CaseDetailsHelperTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CaseDetailsHelperTest.java
@@ -3,9 +3,12 @@ package uk.gov.hmcts.reform.fpl.utils;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static uk.gov.hmcts.reform.fpl.utils.CaseDetailsHelper.formatCCDCaseNumber;
 
 class CaseDetailsHelperTest {
+    private static final String EXCEPTION_MESSAGE = "CCD Case number must be 16 digits long";
+
     @Test
     void shouldFormatCaseNumberWhen16DigitsProvided() {
         String formattedCaseNumber = formatCCDCaseNumber(1234123412341234L);
@@ -13,8 +16,29 @@ class CaseDetailsHelperTest {
     }
 
     @Test
-    void shouldFormatCaseNumberWhen9DigitsProvided() {
-        String formattedCaseNumber = formatCCDCaseNumber(123412341L);
-        assertThat(formattedCaseNumber).isEqualTo("1234-1234-1");
+    void shouldThrowAnExceptionIfCaseNumberExceeds16Digits() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            formatCCDCaseNumber(12341234123412341L);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo(EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void shouldThrowAnExceptionIfCaseNumberIsLessThan16Digits() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            formatCCDCaseNumber(123412341234123L);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo(EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void shouldThrowAnExceptionIfCaseNumberIsNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            formatCCDCaseNumber(null);
+        });
+
+        assertThat(exception.getMessage()).isEqualTo(EXCEPTION_MESSAGE);
     }
 }
