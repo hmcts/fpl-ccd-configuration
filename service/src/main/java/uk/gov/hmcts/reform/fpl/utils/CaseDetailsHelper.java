0ae233739d4f5b277fa6f8c74a2c807226fbffea
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-02-10 10:32:45 +0000

    feat: general letter + cover sheet for print and post (FPLA-1170/1086) (#844)
    Service for creating general/cover letter to be used by SendLetter for print and post

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/CaseDetailsHelper.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.fpl.utils;
+
+import com.google.common.base.Splitter;
+
+public class CaseDetailsHelper {
+    private CaseDetailsHelper() {
+        // NO OP
+    }
+
+    public static String formatCCDCaseNumber(Long caseNumber) {
+        String ccdCaseNumber = String.valueOf(caseNumber);
+            
+        if (ccdCaseNumber.length() != 16) {
+            throw new IllegalArgumentException("CCD Case number must be 16 digits long");
+        }
+        
+        return String.join("-", Splitter.fixedLength(4).splitToList(ccdCaseNumber));
+    }
+}
