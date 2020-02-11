e51a9ccd10f9eac038016fd85daf2a20c70709eb
Etimbuk U <Gogetter@users.noreply.github.com>
2020-02-11 17:31:48 +0000

    impr:  hearing details mandatory before sealed SDO can be submitted (#866)

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DraftOrdersControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DraftOrdersControllerTest.java
@@ -59,6 +59,7 @@ import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.DRAFT;
 import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.SEALED;
 import static uk.gov.hmcts.reform.fpl.service.HearingBookingService.HEARING_DETAILS_KEY;
 import static uk.gov.hmcts.reform.fpl.utils.DocumentManagementStoreLoader.document;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @ActiveProfiles("integration-test")
 @WebMvcTest(DraftOrdersController.class)
@@ -260,20 +261,23 @@ class DraftOrdersControllerTest extends AbstractControllerTest {
         byte[] pdf = {1, 2, 3, 4, 5};
         Document document = document();
 
+        private static final String SEALED_ORDER_FILE_NAME = "standard-directions-order.pdf";
+        private static final String DRAFT_ORDER_FILE_NAME = "draft-standard-directions-order.pdf";
+
         DocumentTests() throws IOException {
             //NO - OP
         }
 
         @BeforeEach
         void setup() {
-            DocmosisDocument docmosisDocument = new DocmosisDocument("standard-directions-order.pdf", pdf);
+            DocmosisDocument docmosisDocument = new DocmosisDocument(SEALED_ORDER_FILE_NAME, pdf);
 
             given(documentGeneratorService.generateDocmosisDocument(any(), any())).willReturn(docmosisDocument);
         }
 
         @Test
         void midEventShouldGenerateDraftStandardDirectionDocument() {
-            given(uploadDocumentService.uploadPDF(userId, userAuthToken, pdf, "draft-standard-directions-order.pdf"))
+            given(uploadDocumentService.uploadPDF(userId, userAuthToken, pdf, DRAFT_ORDER_FILE_NAME))
                 .willReturn(document);
 
             List<Element<Direction>> directions = buildDirections(
@@ -306,7 +310,7 @@ class DraftOrdersControllerTest extends AbstractControllerTest {
 
         @Test
         void aboutToSubmitShouldPopulateHiddenCCDFieldsInStandardDirectionOrderToPersistData() {
-            given(uploadDocumentService.uploadPDF(userId, userAuthToken, pdf, "standard-directions-order.pdf"))
+            given(uploadDocumentService.uploadPDF(userId, userAuthToken, pdf, SEALED_ORDER_FILE_NAME))
                 .willReturn(document);
 
             UUID uuid = UUID.randomUUID();
@@ -323,14 +327,7 @@ class DraftOrdersControllerTest extends AbstractControllerTest {
                     .build())
                 .build());
 
-            List<Element<Direction>> directionWithShowHideValuesRemoved = List.of(Element.<Direction>builder()
-                .id(uuid)
-                .value(Direction.builder()
-                    .directionType("Identify alternative carers")
-                    .assignee(LOCAL_AUTHORITY)
-                    .readOnly("Yes")
-                    .build())
-                .build());
+            List<Element<Direction>> directionWithShowHideValuesRemoved = buildDirectionWithShowHideValuesRemoved(uuid);
 
             Order order = Order.builder()
                 .orderStatus(OrderStatus.SEALED)
@@ -341,6 +338,11 @@ class DraftOrdersControllerTest extends AbstractControllerTest {
                     .data(createCaseDataMap(directionWithShowHideValuesRemoved)
                         .put("standardDirectionOrder", order)
                         .put("judgeAndLegalAdvisor", JudgeAndLegalAdvisor.builder().build())
+                        .put(HEARING_DETAILS_KEY, wrapElements(HearingBooking.builder()
+                            .startDate(LocalDateTime.of(2020, 10, 20, 11, 11, 11))
+                            .endDate(LocalDateTime.of(2020, 11, 20, 11, 11, 11))
+                            .venue("EXAMPLE")
+                            .build()))
                         .build())
                     .build())
                 .build();
@@ -358,6 +360,46 @@ class DraftOrdersControllerTest extends AbstractControllerTest {
             assertThat(caseData.getStandardDirectionOrder().getOrderDoc()).isNotNull();
             assertThat(caseData.getStandardDirectionOrder().getJudgeAndLegalAdvisor()).isNotNull();
             assertThat(caseData.getJudgeAndLegalAdvisor()).isNull();
+        }
+
+        @Test
+        void aboutToSubmitShouldReturnErrorsWhenNoHearingDetailsExistsForSealedOrder() {
+            given(uploadDocumentService.uploadPDF(userId, userAuthToken, pdf, SEALED_ORDER_FILE_NAME))
+                .willReturn(document());
+
+            UUID uuid = UUID.randomUUID();
+
+            List<Element<Direction>> directionWithShowHideValuesRemoved = buildDirectionWithShowHideValuesRemoved(uuid);
+
+            Order order = Order.builder()
+                .orderStatus(OrderStatus.SEALED)
+                .build();
+
+            CallbackRequest request = CallbackRequest.builder()
+                .caseDetails(CaseDetails.builder()
+                    .data(createCaseDataMap(directionWithShowHideValuesRemoved)
+                        .put("standardDirectionOrder", order)
+                        .put("judgeAndLegalAdvisor", JudgeAndLegalAdvisor.builder().build())
+                        .build())
+                    .build())
+                .build();
+
+            AboutToStartOrSubmitCallbackResponse response = postAboutToSubmitEvent(request);
+
+            assertThat(response.getErrors())
+                .containsOnly("This standard directions order does not have a hearing associated with it. "
+                    + "Please enter a hearing date and resubmit the SDO");
+        }
+
+        private List<Element<Direction>> buildDirectionWithShowHideValuesRemoved(UUID uuid) {
+            return List.of(Element.<Direction>builder()
+                .id(uuid)
+                .value(Direction.builder()
+                    .directionType("Identify alternative carers")
+                    .assignee(LOCAL_AUTHORITY)
+                    .readOnly("Yes")
+                    .build())
+                .build());
         }
     }
 }
