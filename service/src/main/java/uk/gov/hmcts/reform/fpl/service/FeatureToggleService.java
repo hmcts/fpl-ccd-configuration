bee7eadf288c5edcdc9c9131af92856e288a8a45
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-02-13 12:14:51 +0000

    Updated feature toggle service

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
@@ -2,24 +2,28 @@ package uk.gov.hmcts.reform.fpl.service;
 
 import com.launchdarkly.client.LDClient;
 import com.launchdarkly.client.LDUser;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class FeatureToggleService {
 
-    private static final String LD_USER_ID = "FPLA";
-
     private final LDClient ldClient;
+    private final String ldUserKey;
+
+    @Autowired
+    public FeatureToggleService(LDClient ldClient, @Value("${ld.user_key}") String ldUserKey) {
+        this.ldClient = ldClient;
+        this.ldUserKey = ldUserKey;
+    }
 
     public boolean isXeroxPrintingEnabled() {
         return ldClient.boolVariation("xerox-printing", getUser(), false);
     }
 
     private LDUser getUser() {
-        return new LDUser.Builder(LD_USER_ID)
+        return new LDUser.Builder(ldUserKey)
             .custom("timestamp", String.valueOf(System.currentTimeMillis()))
             .build();
     }
