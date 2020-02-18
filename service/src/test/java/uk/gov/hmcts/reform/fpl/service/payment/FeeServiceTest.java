e561c5643453136997af2639e10adbedca996305
Lewis Birks <lewisb@kainos.com>
2020-02-18 15:39:42 +0000

    service tests

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/FeeServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/FeeServiceTest.java
@@ -1,10 +1,11 @@
 package uk.gov.hmcts.reform.fpl.service.payment;
 
+import feign.FeignException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,18 +14,23 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.fnp.client.FeesRegisterApi;
 import uk.gov.hmcts.reform.fnp.model.fee.FeeResponse;
 import uk.gov.hmcts.reform.fnp.model.fee.FeeType;
-import uk.gov.hmcts.reform.fpl.service.config.LookupTestConfig;
+import uk.gov.hmcts.reform.fpl.testbeans.TestFeeConfig;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fnp.model.fee.FeeType.CARE_ORDER;
+import static uk.gov.hmcts.reform.fnp.model.fee.FeeType.PLACEMENT;
+import static uk.gov.hmcts.reform.fnp.model.fee.FeeType.SUPERVISION_ORDER;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {
-    FeeService.class, LookupTestConfig.class,
+    FeeService.class, TestFeeConfig.class,
 })
 class FeeServiceTest {
 
@@ -37,40 +43,49 @@ class FeeServiceTest {
     @Nested
     class MakeRequest {
 
+
         @ParameterizedTest
         @NullAndEmptySource
         void shouldReturnAnEmptyListWhenNullOrEmptyListIsPassed(List<FeeType> list) {
             assertThat(feeService.getFees(list)).isEmpty();
         }
 
-        @ParameterizedTest
-        @EnumSource(FeeType.class)
-        void shouldGetAFeeResponseWhenGivenAPopulated(FeeType feeType) {
-            // TODO: 17/02/2020 mock something
+        @Test
+        void shouldGetAFeeResponseWhenGivenAPopulated() {
+            FeeResponse expectedFeeResponse = expectedFeeResponse();
 
-            List<FeeResponse> fees = feeService.getFees(List.of(feeType));
-            FeeResponse actual = fees.get(0);
+            when(feesRegisterApi.findFee("default", "miscellaneous", "family", "family court", "KLM", "private law"))
+                .thenReturn(expectedFeeResponse);
 
-            assertThat(fees).hasSize(1);
-            assertThat(actual.getCode()).isEqualTo("FEE0327");
-            assertThat(actual.getDescription()).isEqualTo("example");
-            assertThat(actual.getVersion()).isEqualTo(1);
-            assertThat(actual.getAmount()).isEqualTo(BigDecimal.valueOf(255));
-            FeeResponse feeResponse = new FeeResponse();
+            List<FeeResponse> fees = feeService.getFees(List.of(CARE_ORDER, SUPERVISION_ORDER, PLACEMENT));
+
+            assertThat(fees).hasSize(3);
+            assertThat(fees).containsOnly(expectedFeeResponse);
         }
 
         @Test
-        void shouldFilterOutNullValuesWhenThereIsAnErrorInTheResponse() {
-            // TODO: 17/02/2020 mock something
+        void shouldFilterOutValuesWhenThereIsAnErrorInTheResponse() {
+            // TODO: 18/02/2020 what do in case of error
+            when(feesRegisterApi.findFee(anyString(), anyString(), anyString(), anyString(), anyString(), anyString()))
+                .thenThrow(new FeignException.BadRequest("", null));
 
             List<FeeResponse> feeResponses = feeService.getFees(List.of(CARE_ORDER));
 
             assertThat(feeResponses).isEmpty();
         }
 
-        private String buildURI() {
-            return "http://localhost:8080/fees-register/fees/lookup?service=private%20law&jurisdiction1=family"
-                + "&jurisdiction2=family%20court&channel=default&event=miscellaneous&keyword=KLM";
+        @AfterEach
+        void resetInvocations() {
+            reset(feesRegisterApi);
+        }
+
+        private FeeResponse expectedFeeResponse() {
+            FeeResponse feeResponse = new FeeResponse();
+            feeResponse.setCode("FEE0327");
+            feeResponse.setAmount(BigDecimal.valueOf(255));
+            feeResponse.setDescription("example");
+            feeResponse.setVersion(1);
+            return feeResponse;
         }
     }
 
