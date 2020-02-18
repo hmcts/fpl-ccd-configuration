0b758c1ba909fb562e1e4d4d306cfb9056f18946
Lewis Birks <lewisb@kainos.com>
2020-02-18 14:42:36 +0000

    map instead of switch

--- service/src/main/java/uk/gov/hmcts/reform/fnp/model/fee/FeeType.java
+++ service/src/main/java/uk/gov/hmcts/reform/fnp/model/fee/FeeType.java
@@ -4,6 +4,8 @@ import com.google.common.collect.ImmutableList;
 import uk.gov.hmcts.reform.fpl.enums.OrderType;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import static java.util.stream.Collectors.toUnmodifiableList;
 
@@ -19,6 +21,12 @@ public enum FeeType {
     PLACEMENT,
     SUPERVISION_ORDER;
 
+    private static final Map<OrderType, FeeType> orderToFeeMap = buildOrderFeeMap();
+
+    public static FeeType fromOrderType(OrderType orderType) {
+        return orderToFeeMap.get(orderType);
+    }
+
     public static List<FeeType> fromOrderType(List<OrderType> orderTypes) {
         if (orderTypes == null) {
             return ImmutableList.of();
@@ -26,27 +34,19 @@ public enum FeeType {
 
         return orderTypes.stream()
             .map(FeeType::fromOrderType)
+            .filter(Objects::nonNull)
             .collect(toUnmodifiableList());
     }
 
-    public static FeeType fromOrderType(OrderType orderType) {
-        switch (orderType) {
-            case CARE_ORDER:
-                return CARE_ORDER;
-            case INTERIM_CARE_ORDER:
-                return INTERIM_CARE_ORDER;
-            case SUPERVISION_ORDER:
-                return SUPERVISION_ORDER;
-            case INTERIM_SUPERVISION_ORDER:
-                return INTERIM_SUPERVISION_ORDER;
-            case EDUCATION_SUPERVISION_ORDER:
-                return EDUCATION_SUPERVISION_ORDER;
-            case EMERGENCY_PROTECTION_ORDER:
-                return EMERGENCY_PROTECTION_ORDER;
-            case OTHER:
-                return OTHER;
-            default:
-                throw new IllegalStateException("Unexpected value: " + orderType);
-        }
+    private static Map<OrderType, FeeType> buildOrderFeeMap() {
+        return Map.of(
+            OrderType.CARE_ORDER, CARE_ORDER,
+            OrderType.INTERIM_CARE_ORDER, INTERIM_CARE_ORDER,
+            OrderType.SUPERVISION_ORDER, SUPERVISION_ORDER,
+            OrderType.INTERIM_SUPERVISION_ORDER, INTERIM_SUPERVISION_ORDER,
+            OrderType.EDUCATION_SUPERVISION_ORDER, EDUCATION_SUPERVISION_ORDER,
+            OrderType.EMERGENCY_PROTECTION_ORDER, EMERGENCY_PROTECTION_ORDER,
+            OrderType.OTHER, OTHER
+        );
     }
 }
