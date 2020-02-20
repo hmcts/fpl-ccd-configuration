f680eeb4ef9e7e73c4aa866bdc73c12fba2971d3
Toireasa Moley <toireasa.moley@hmcts.net>
2020-02-20 15:13:12 +0000

    Adding people in case tests

--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/PeopleInCaseHelperTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/PeopleInCaseHelperTest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.fpl.utils;
 
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Judge;
 import uk.gov.hmcts.reform.fpl.model.Respondent;
 import uk.gov.hmcts.reform.fpl.model.RespondentParty;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
@@ -8,9 +11,10 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle.*;
 import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createRespondents;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
-import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.getFirstRespondentLastName;
+import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.*;
 
 class PeopleInCaseHelperTest {
     @Test
@@ -42,5 +46,47 @@ class PeopleInCaseHelperTest {
 
         String respondentName = getFirstRespondentLastName(respondents);
         assertThat(respondentName).isEmpty();
+    }
+
+    @Test
+    void shouldReturnAllocatedJudgeOtherTitleWhenOtherIsSelected() {
+        CaseData data = buildCaseDataWithAllocatedJudge(OTHER);
+
+        String title = getAllocatedJudgeTitle(data);
+        assertThat(title).isEqualTo("Other title");
+    }
+
+    @Test
+    void shouldReturnAllocatedJudgeTitleWhenOtherIsNotSelected() {
+        CaseData data = buildCaseDataWithAllocatedJudge(DISTRICT_JUDGE);
+
+        String title = getAllocatedJudgeTitle(data);
+        assertThat(title).isEqualTo(DISTRICT_JUDGE.toString());
+    }
+
+    @Test
+    void shouldReturnAllocatedJudgeFullNameWhenMagistratesSelected() {
+        CaseData data = buildCaseDataWithAllocatedJudge(MAGISTRATES);
+
+        String title = getAllocatedJudgeName(data);
+        assertThat(title).isEqualTo("Judge full name");
+    }
+
+    @Test
+    void shouldReturnAllocatedJudgeLastName() {
+        CaseData data = buildCaseDataWithAllocatedJudge(DEPUTY_DISTRICT_JUDGE);
+
+        String title = getAllocatedJudgeName(data);
+        assertThat(title).isEqualTo("Judge last name");
+    }
+
+    private CaseData buildCaseDataWithAllocatedJudge(JudgeOrMagistrateTitle title){
+        return CaseData.builder().allocatedJudge(Judge.builder()
+            .judgeTitle(title)
+            .otherTitle("Other title")
+            .judgeFullName("Judge full name")
+            .judgeLastName("Judge last name")
+            .build())
+            .build();
     }
 }
