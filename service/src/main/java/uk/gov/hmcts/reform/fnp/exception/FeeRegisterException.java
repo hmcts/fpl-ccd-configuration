caa4e5891e498a5a5203995658850bd2258df069
Lewis Birks <lewisb@kainos.com>
2020-02-26 09:42:51 +0000

    removed additional FeeRegisterException constructors

--- service/src/main/java/uk/gov/hmcts/reform/fnp/exception/FeeRegisterException.java
+++ service/src/main/java/uk/gov/hmcts/reform/fnp/exception/FeeRegisterException.java
@@ -1,31 +1,14 @@
 package uk.gov.hmcts.reform.fnp.exception;
 
+import lombok.Getter;
+
+@Getter
 public class FeeRegisterException extends RuntimeException {
 
     private final int status;
 
-    public FeeRegisterException() {
-        super();
-        this.status =  -1;
-    }
-
-    public FeeRegisterException(int status) {
-        super();
-        this.status = status;
-    }
-
-    public FeeRegisterException(int status, String message) {
-        super(message);
-        this.status = status;
-    }
-
     public FeeRegisterException(int status, String message, Throwable cause) {
         super(message, cause);
-        this.status = status;
-    }
-
-    public FeeRegisterException(int status, Throwable cause) {
-        super(cause);
         this.status = status;
     }
 }
