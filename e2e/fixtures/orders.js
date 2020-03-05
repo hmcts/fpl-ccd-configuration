f618817a02f8aefcfb16261fd65976ab1055bb3f
Joe Zazzaro-Francis <joez@Gabbana.local>
2020-03-05 13:41:17 +0000

    comment to explain why no date of issue

--- e2e/fixtures/orders.js
+++ e2e/fixtures/orders.js
@@ -5,6 +5,7 @@ module.exports = [
     title: 'Example Order Title',
     details: 'Example order details here - Lorem ipsum dolor sit amet...',
     document: 'blank_order_c21.pdf',
+    //no date of issue to test default pre-population with today's date
     //future work: extract judgeAndLegalAdvisor to separate fixture
     judgeAndLegalAdvisor: {
       judgeTitle: 'Her Honour Judge',
