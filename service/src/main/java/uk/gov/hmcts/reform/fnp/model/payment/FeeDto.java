b8139982543cdf2253991b2f34ab6900006906f7
pd-dudkiewicz <58216515+pd-dudkiewicz@users.noreply.github.com>
2020-03-06 10:09:23 +0000

    feat: new c2 upload screens and integration with payments api (FPLA-1191 FPLA-1280 FPLA-1219) (#919)

---
+++ service/src/main/java/uk/gov/hmcts/reform/fnp/model/payment/FeeDto.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.fnp.model.payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import uk.gov.hmcts.reform.fnp.model.fee.FeeResponse;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class FeeDto {
+    private BigDecimal calculatedAmount;
+    private Integer version;
+    private String code;
+
+    public static FeeDto fromFeeResponse(FeeResponse feeResponse) {
+        return FeeDto.builder()
+            .calculatedAmount(feeResponse.getAmount())
+            .version(feeResponse.getVersion())
+            .code(feeResponse.getCode())
+            .build();
+    }
+}
