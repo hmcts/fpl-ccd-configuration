cb80e738c6b70b04bb3840d645f166fe253e316b
Etimbuk <euedofia@gmail.com>
2020-03-31 09:00:57 +0100

    FPLA-1524 fix failing e2e

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/C2DocumentBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/common/C2DocumentBundle.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.model.common;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.fpl.enums.C2ApplicationType;
@@ -12,10 +11,7 @@ public class C2DocumentBundle {
     private final String nameOfRepresentative;
     private final String pbaNumber;
     private final String clientCode;
-
-    @JsonProperty(value = "fileReference")
     private final String customerReference;
-
     private final DocumentReference document;
     private final String description;
     private final String uploadedDateTime;
