d0e21448f4b19cf854060ab1a80ee2c47bcc7a32
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-04-06 16:31:52 +0100

    fix attempt: rollback dependencies bump + fast fail on build

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/LaunchDarklyConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/LaunchDarklyConfiguration.java
@@ -1,15 +1,23 @@
 package uk.gov.hmcts.reform.fpl.config;
 
 import com.launchdarkly.client.LDClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class LaunchDarklyConfiguration {
 
     @Bean
     public LDClient ldClient(@Value("${ld.sdk_key}") String sdkKey) {
-        return new LDClient(sdkKey);
+        var ldClient = new LDClient(sdkKey);
+        log.error("LD SDK key: " + sdkKey);
+        if (!ldClient.initialized()) {
+            //throw new RuntimeException("LaunchDarkly initialization failed.");
+        }
+
+        return ldClient;
     }
 }
