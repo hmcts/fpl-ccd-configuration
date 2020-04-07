ae3134c1e9b42904cba0d4c08bc62b986b149834
James Nelson <Jamesnelson117@gmail.com>
2020-04-07 09:15:51 +0100

    FPLA-1321: Reverted changes to hearing details

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/HearingBookingDetailsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/HearingBookingDetailsController.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
 import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
 import uk.gov.hmcts.reform.fpl.service.ValidateGroupService;
 import uk.gov.hmcts.reform.fpl.validation.groups.HearingBookingDetailsGroup;
@@ -22,10 +21,7 @@ import uk.gov.hmcts.reform.fpl.validation.groups.HearingBookingDetailsGroup;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
-import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.fpl.service.HearingBookingService.HEARING_DETAILS_KEY;
-import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.buildAllocatedJudgeLabel;
 
 @Api
 @RestController
@@ -46,35 +42,6 @@ public class HearingBookingDetailsController {
         List<Element<HearingBooking>> pastHearings = service.getPastHearings(hearingDetails);
 
         hearingDetails.removeAll(pastHearings);
-
-        if (isNotEmpty(caseData.getAllocatedJudge())) {
-            String assignedJudgeLabel = buildAllocatedJudgeLabel(caseData.getAllocatedJudge());
-
-            hearingDetails = hearingDetails.stream()
-                .map(element -> {
-                    HearingBooking.HearingBookingBuilder hearingBookingBuilder = HearingBooking.builder();
-                    HearingBooking hearingBooking = element.getValue();
-
-                    JudgeAndLegalAdvisor.JudgeAndLegalAdvisorBuilder judgeAndLegalAdvisorBuilder
-                        = JudgeAndLegalAdvisor.builder();
-
-                    judgeAndLegalAdvisorBuilder.allocatedJudgeLabel(assignedJudgeLabel);
-
-                    hearingBookingBuilder.type(hearingBooking.getType())
-                        .typeDetails(hearingBooking.getTypeDetails())
-                        .venue(hearingBooking.getVenue())
-                        .startDate(hearingBooking.getStartDate())
-                        .endDate(hearingBooking.getEndDate())
-                        .hearingNeedsBooked(hearingBooking.getHearingNeedsBooked())
-                        .hearingNeedsDetails(hearingBooking.getHearingNeedsDetails())
-                        .judgeAndLegalAdvisor(judgeAndLegalAdvisorBuilder.build());
-
-                    return Element.<HearingBooking>builder()
-                        .id(element.getId())
-                        .value(hearingBookingBuilder.build())
-                        .build();
-                }).collect(toList());
-        }
 
         caseDetails.getData().put(HEARING_DETAILS_KEY, hearingDetails);
 
