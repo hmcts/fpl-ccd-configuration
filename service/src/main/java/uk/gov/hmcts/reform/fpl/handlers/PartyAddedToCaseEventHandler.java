7a311cb5d1415b096c98763dbb3e22ff3212838e
eau <euedofia@gmil.com>
2020-04-08 23:13:25 +0100

    FPLA-1249 impr and updates

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PartyAddedToCaseEventHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PartyAddedToCaseEventHandler.java
@@ -1,20 +1,15 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.events.PartyAddedToCaseEvent;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Representative;
 import uk.gov.hmcts.reform.fpl.model.event.EventData;
-import uk.gov.hmcts.reform.fpl.service.RepresentativeService;
 import uk.gov.hmcts.reform.fpl.service.email.content.PartyAddedToCaseContentProvider;
 import uk.gov.hmcts.reform.fpl.service.representative.RepresentativeNotificationService;
 
-import java.util.List;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.PARTY_ADDED_TO_CASE_BY_EMAIL_NOTIFICATION_TEMPLATE;
@@ -25,8 +20,6 @@ import static uk.gov.hmcts.reform.fpl.enums.RepresentativeServingPreferences.EMA
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PartyAddedToCaseEventHandler {
-    private final ObjectMapper objectMapper;
-    private final RepresentativeService representativeService;
     private final PartyAddedToCaseContentProvider partyAddedToCaseContentProvider;
     private final RepresentativeNotificationService representativeNotificationService;
 
@@ -37,21 +30,15 @@ public class PartyAddedToCaseEventHandler {
 
         Map<String, Object> servedByEmailParameters = partyAddedToCaseContentProvider
             .getPartyAddedToCaseNotificationParameters(caseDetails, EMAIL);
+
+        representativeNotificationService.sendToRepresentativesByServedPreference(EMAIL,
+            PARTY_ADDED_TO_CASE_BY_EMAIL_NOTIFICATION_TEMPLATE, servedByEmailParameters, eventData);
+
         Map<String, Object> servedByDigitalServiceParameters = partyAddedToCaseContentProvider
             .getPartyAddedToCaseNotificationParameters(caseDetails, DIGITAL_SERVICE);
 
-        CaseData caseData = objectMapper.convertValue(caseDetails.getData(), CaseData.class);
-        CaseData caseDataBefore = objectMapper.convertValue(event.getCallbackRequest().getCaseDetailsBefore().getData(),
-            CaseData.class);
-
-        List<Representative> representativesServedByDigitalService = representativeService.getUpdatedRepresentatives(
-            caseData.getRepresentatives(), caseDataBefore.getRepresentatives(), DIGITAL_SERVICE);
-        List<Representative> representativesServedByEmail = representativeService.getUpdatedRepresentatives(
-            caseData.getRepresentatives(), caseDataBefore.getRepresentatives(), EMAIL);
-
-        representativeNotificationService.sendNotificationToRepresentatives(eventData, servedByEmailParameters,
-            representativesServedByEmail, PARTY_ADDED_TO_CASE_BY_EMAIL_NOTIFICATION_TEMPLATE);
-        representativeNotificationService.sendNotificationToRepresentatives(eventData, servedByDigitalServiceParameters,
-            representativesServedByDigitalService, PARTY_ADDED_TO_CASE_THROUGH_DIGITAL_SERVICE_NOTIFICATION_TEMPLATE);
+        representativeNotificationService.sendToRepresentativesByServedPreference(DIGITAL_SERVICE,
+            PARTY_ADDED_TO_CASE_THROUGH_DIGITAL_SERVICE_NOTIFICATION_TEMPLATE, servedByDigitalServiceParameters,
+            eventData);
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/RepresentativeNotificationHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/RepresentativeNotificationHandler.java
@@ -1,16 +1,12 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.fpl.enums.IssuedOrderType;
-import uk.gov.hmcts.reform.fpl.enums.RepresentativeServingPreferences;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.Representative;
 import uk.gov.hmcts.reform.fpl.model.event.EventData;
-import uk.gov.hmcts.reform.fpl.service.RepresentativeService;
 import uk.gov.hmcts.reform.fpl.service.email.content.OrderIssuedEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.representative.RepresentativeNotificationService;
 
@@ -23,8 +19,6 @@ import static uk.gov.hmcts.reform.fpl.NotifyTemplates.ORDER_ISSUED_NOTIFICATION_
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RepresentativeNotificationHandler {
-    private final ObjectMapper objectMapper;
-    private final RepresentativeService representativeService;
     private final OrderIssuedEmailContentProvider orderIssuedEmailContentProvider;
     private final RepresentativeNotificationService representativeNotificationService;
 
@@ -41,20 +35,6 @@ public class RepresentativeNotificationHandler {
                 ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_REPRESENTATIVES);
         } else {
             log.debug("No notification sent to representatives (none require serving)");
-        }
-    }
-
-    public void sendToRepresentativesByServedPreference(final RepresentativeServingPreferences servedPreference,
-                                                        final String templateId,
-                                                        final Map<String, Object> templateParameters,
-                                                        final EventData eventData) {
-        CaseData caseData = objectMapper.convertValue(eventData.getCaseDetails().getData(), CaseData.class);
-        List<Representative> representatives = representativeService.getRepresentativesByServedPreference(
-            caseData.getRepresentatives(), servedPreference);
-
-        if (!representatives.isEmpty()) {
-            representativeNotificationService.sendNotificationToRepresentatives(eventData, templateParameters,
-                representatives, templateId);
         }
     }
 }
