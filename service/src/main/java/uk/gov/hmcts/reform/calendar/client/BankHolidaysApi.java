f02be7968c6e669583c3b63b647e48a88c74ddb3
Tomasz Powroznik <tomas.powroznik@gmail.com>
2020-04-15 17:10:59 +0100

    Fix issues reported by sonar

--- service/src/main/java/uk/gov/hmcts/reform/calendar/client/BankHolidaysApi.java
+++ service/src/main/java/uk/gov/hmcts/reform/calendar/client/BankHolidaysApi.java
@@ -1,14 +1,13 @@
 package uk.gov.hmcts.reform.calendar.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.hmcts.reform.calendar.model.BankHolidays;
 import uk.gov.hmcts.reform.fpl.config.FeignConfiguration;
 
 @FeignClient(name = "bank-holidays-api", url = "${bankHolidays.api.url}", configuration = FeignConfiguration.class)
 public interface BankHolidaysApi {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/bank-holidays.json")
+    @GetMapping(value = "/bank-holidays.json")
     BankHolidays retrieveAll();
 }
