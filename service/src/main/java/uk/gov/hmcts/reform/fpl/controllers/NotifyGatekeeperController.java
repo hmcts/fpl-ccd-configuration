9b271018aa7313c3024b71b19f1a6d6b96990bd7
Lewis Birks <lewisb@kainos.com>
2020-04-15 15:18:43 +0100

    Add logic to guard against unnecessary statements

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/NotifyGatekeeperController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/NotifyGatekeeperController.java
@@ -19,6 +19,11 @@ import uk.gov.hmcts.reform.fpl.request.RequestData;
 import uk.gov.hmcts.reform.fpl.service.ValidateGroupService;
 import uk.gov.hmcts.reform.fpl.validation.groups.ValidateFamilyManCaseNumberGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static uk.gov.hmcts.reform.fpl.enums.State.SUBMITTED;
+
 @Api
 @RestController
 @RequestMapping("/callback/notify-gatekeeper")
@@ -32,23 +37,28 @@ public class NotifyGatekeeperController {
     //TODO: can we validate a hearing has been added at this point? Saves some nasty exceptions in the case of
     // no hearing being present when populating standard directions FPLA-1516
     @PostMapping("/about-to-start")
-    public AboutToStartOrSubmitCallbackResponse handleAboutToStartEvent(
-        @RequestBody CallbackRequest callbackRequest) {
+    public AboutToStartOrSubmitCallbackResponse handleAboutToStartEvent(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
 
+        List<String> errors = new ArrayList<>();
+        if (SUBMITTED.getValue().equals(caseDetails.getState())) {
+            errors = validateGroupService.validateGroup(caseData, ValidateFamilyManCaseNumberGroup.class);
+        }
+
+        caseDetails.getData().remove("gateKeeperEmail");
+
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDetails.getData())
-            .errors(validateGroupService.validateGroup(caseData, ValidateFamilyManCaseNumberGroup.class))
+            .errors(errors)
             .build();
     }
 
     @PostMapping("/submitted")
-    public void handleSubmittedEvent(
-        @RequestBody CallbackRequest callbackRequest) {
-
-        applicationEventPublisher.publishEvent(
-            new PopulateStandardDirectionsEvent(callbackRequest, requestData));
+    public void handleSubmittedEvent(@RequestBody CallbackRequest callbackRequest) {
+        if (SUBMITTED.getValue().equals(callbackRequest.getCaseDetails().getState())) {
+            applicationEventPublisher.publishEvent(new PopulateStandardDirectionsEvent(callbackRequest, requestData));
+        }
         applicationEventPublisher.publishEvent(new NotifyGatekeeperEvent(callbackRequest, requestData));
     }
 }
