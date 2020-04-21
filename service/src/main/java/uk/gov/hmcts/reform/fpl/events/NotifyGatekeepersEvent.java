3c06355b5379f73602c312e87b78f1f6fc6c4baf
James Nelson <Jamesnelson117@gmail.com>
2020-04-21 09:20:12 +0100

    feat: notify to more than one gatekeeper (FPLA-970) (#1087)
    * FPLA-970: Send to gatekeeper event now supports multiple gatekeeper emails
    
    * FPLA-970: Added e2e tests
    
    * FPLA-970: Minor fixes
    
    * FPLA-970: Code review fixes
    
    * FPLA-970: Code review comments
    
    Co-authored-by: douglasrice <douglasrice@yahoo.com>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/events/NotifyGatekeepersEvent.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/events/NotifyGatekeepersEvent.java
@@ -3,9 +3,9 @@ package uk.gov.hmcts.reform.fpl.events;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.fpl.request.RequestData;
 
-public class NotifyGatekeeperEvent extends CallbackEvent {
+public class NotifyGatekeepersEvent extends CallbackEvent {
 
-    public NotifyGatekeeperEvent(CallbackRequest callbackRequest, RequestData requestData) {
+    public NotifyGatekeepersEvent(CallbackRequest callbackRequest, RequestData requestData) {
         super(callbackRequest, requestData);
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/GatekeeperEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/GatekeeperEmailContentProvider.java
@@ -9,7 +9,9 @@ import uk.gov.hmcts.reform.fpl.config.LocalAuthorityNameLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.service.email.content.base.CasePersonalisedContentProvider;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class GatekeeperEmailContentProvider extends CasePersonalisedContentProvider {
@@ -29,5 +31,16 @@ public class GatekeeperEmailContentProvider extends CasePersonalisedContentProvi
         return super.getCasePersonalisationBuilder(caseDetails.getId(), caseData)
             .put("localAuthority", config.getLocalAuthorityName(localAuthorityCode))
             .build();
+    }
+
+    public String buildRecipientsLabel(List<String> emailList, String recipientEmail) {
+        String formattedRecipients = emailList.stream()
+            .filter(email -> !recipientEmail.equals(email))
+            .collect(Collectors.joining(", "));
+
+        if (!formattedRecipients.isEmpty()) {
+            return String.format("%s has also received this notification", formattedRecipients);
+        }
+        return "";
     }
 }
