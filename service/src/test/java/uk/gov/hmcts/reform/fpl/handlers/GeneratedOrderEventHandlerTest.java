6efcaaddc614c3aa935659d86a8eb416d2e5c637
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-04-21 11:33:55 +0100

    feat: modify order issued notification for admin (FPLA-1287) (#1086)
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>

--- service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/GeneratedOrderEventHandlerTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/GeneratedOrderEventHandlerTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.fpl.handlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,19 +20,15 @@ import uk.gov.hmcts.reform.fpl.service.InboxLookupService;
 import uk.gov.hmcts.reform.fpl.service.RepresentativeService;
 import uk.gov.hmcts.reform.fpl.service.config.LookupTestConfig;
 import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
-import uk.gov.hmcts.reform.fpl.service.email.content.C2UploadedEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.email.content.HmctsEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.email.content.OrderIssuedEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.representative.RepresentativeNotificationService;
 
 import java.util.List;
-import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.CASE_TYPE;
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.JURISDICTION;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.ORDER_GENERATED_NOTIFICATION_TEMPLATE_FOR_LA_AND_DIGITAL_REPRESENTATIVES;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_ADMIN;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_REPRESENTATIVES;
@@ -45,9 +40,8 @@ import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.DOCUMENT_CONTENTS;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_CODE;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_EMAIL_ADDRESS;
-import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.getExpectedOrderNotificationParameters;
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.callbackRequest;
-import static uk.gov.hmcts.reform.fpl.utils.OrderIssuedNotificationTestHelper.getExpectedParametersForAdminWhenNoRepresentativesServedByPost;
+import static uk.gov.hmcts.reform.fpl.utils.OrderIssuedNotificationTestHelper.getExpectedCaseUrlParameters;
 import static uk.gov.hmcts.reform.fpl.utils.OrderIssuedNotificationTestHelper.getExpectedParametersForRepresentatives;
 import static uk.gov.hmcts.reform.fpl.utils.matchers.JsonMatcher.eqJson;
 
@@ -59,19 +53,9 @@ class GeneratedOrderEventHandlerTest {
 
     final String mostRecentUploadedDocumentUrl =
         "http://fake-document-gateway/documents/79ec80ec-7be6-493b-b4e6-f002f05b7079/binary";
-    final String subjectLine = "Lastname, SACCCCCCCC5676576567";
-    final Map<String, Object> c2Parameters = ImmutableMap.<String, Object>builder()
-        .put("subjectLine", subjectLine)
-        .put("hearingDetailsCallout", subjectLine)
-        .put("reference", "12345")
-        .put("caseUrl", "null/case/" + JURISDICTION + "/" + CASE_TYPE + "/12345")
-        .build();
 
     @MockBean
     private RequestData requestData;
-
-    @MockBean
-    private C2UploadedEmailContentProvider c2UploadedEmailContentProvider;
 
     @MockBean
     private OrderIssuedEmailContentProvider orderIssuedEmailContentProvider;
@@ -101,18 +85,11 @@ class GeneratedOrderEventHandlerTest {
         given(inboxLookupService.getNotificationRecipientEmail(caseDetails, LOCAL_AUTHORITY_CODE))
             .willReturn(LOCAL_AUTHORITY_EMAIL_ADDRESS);
 
-        given(c2UploadedEmailContentProvider.buildC2UploadNotification(caseDetails))
-            .willReturn(c2Parameters);
-
-        given(orderIssuedEmailContentProvider.buildOrderNotificationParameters(
+        given(orderIssuedEmailContentProvider.buildParametersWithCaseUrl(
             callbackRequest().getCaseDetails(), LOCAL_AUTHORITY_CODE, DOCUMENT_CONTENTS, GENERATED_ORDER))
-            .willReturn(getExpectedOrderNotificationParameters());
+            .willReturn(getExpectedCaseUrlParameters(BLANK_ORDER.getLabel(), true));
 
-        given(orderIssuedEmailContentProvider.buildNotificationParametersForHmctsAdmin(
-            callbackRequest().getCaseDetails(), LOCAL_AUTHORITY_CODE, DOCUMENT_CONTENTS, GENERATED_ORDER))
-            .willReturn(getExpectedParametersForAdminWhenNoRepresentativesServedByPost(true));
-
-        given(orderIssuedEmailContentProvider.buildNotificationParametersForRepresentatives(
+        given(orderIssuedEmailContentProvider.buildParametersWithoutCaseUrl(
             callbackRequest().getCaseDetails(), LOCAL_AUTHORITY_CODE, DOCUMENT_CONTENTS, GENERATED_ORDER))
             .willReturn(getExpectedParametersForRepresentatives(BLANK_ORDER.getLabel(), true));
     }
@@ -130,10 +107,10 @@ class GeneratedOrderEventHandlerTest {
             requestData, mostRecentUploadedDocumentUrl, DOCUMENT_CONTENTS));
 
         verify(notificationService).sendEmail(
-            ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_ADMIN,
-            COURT_EMAIL_ADDRESS,
-            getExpectedParametersForAdminWhenNoRepresentativesServedByPost(true),
-            "12345");
+            eq(ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_ADMIN),
+            eq(COURT_EMAIL_ADDRESS),
+            eqJson(getExpectedCaseUrlParameters(BLANK_ORDER.getLabel(), true)),
+            eq("12345"));
 
         verify(notificationService).sendEmail(
             eq(ORDER_ISSUED_NOTIFICATION_TEMPLATE_FOR_REPRESENTATIVES),
@@ -144,13 +121,13 @@ class GeneratedOrderEventHandlerTest {
         verify(notificationService).sendEmail(
             eq(ORDER_GENERATED_NOTIFICATION_TEMPLATE_FOR_LA_AND_DIGITAL_REPRESENTATIVES),
             eq(LOCAL_AUTHORITY_EMAIL_ADDRESS),
-            eqJson(getExpectedOrderNotificationParameters()),
+            eqJson(getExpectedCaseUrlParameters(BLANK_ORDER.getLabel(), true)),
             eq("12345"));
 
         verify(notificationService).sendEmail(
             eq(ORDER_GENERATED_NOTIFICATION_TEMPLATE_FOR_LA_AND_DIGITAL_REPRESENTATIVES),
             eq("fred@flinstone.com"),
-            eqJson(getExpectedOrderNotificationParameters()),
+            eqJson(getExpectedCaseUrlParameters(BLANK_ORDER.getLabel(), true)),
             eq("12345"));
     }
 
