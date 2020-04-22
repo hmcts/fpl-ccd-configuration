47f8ce76ad3bc6b75ca7fb9f6edf49b4e2b4c03a
Etimbuk <euedofia@gmail.com>
2020-04-22 16:21:31 +0100

    FPLA-290 initial bits for test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionTemplateDataGenerationServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionTemplateDataGenerationServiceTest.java
@@ -1,15 +1,23 @@
 package uk.gov.hmcts.reform.fpl.service.casesubmission;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.service.UserDetailsService;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Map;
+
 import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.populatedCaseDetails;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {CaseSubmissionTemplateDataGenerationService.class, JacksonAutoConfiguration.class})
@@ -17,13 +25,27 @@ public class CaseSubmissionTemplateDataGenerationServiceTest {
     @MockBean
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private CaseSubmissionTemplateDataGenerationService templateDataGenerationService;
+
     @BeforeEach
     void init() {
         given(userDetailsService.getUserName()).willReturn("Professor");
     }
 
     @Test
-    void shouldReturnExpectedTemplateDataWhenAllDataPresent() {
+    void shouldReturnExpectedTemplateDataWhenAllDataPresent() throws IOException {
+        Map<String, Object> returnedTemplateData = templateDataGenerationService.getTemplateData(
+            prepareCaseData(), false);
+    }
 
+    private CaseData prepareCaseData() {
+        CaseData caseData = objectMapper.convertValue(populatedCaseDetails().getData(), CaseData.class);
+        caseData.setDateSubmitted(LocalDate.now());
+
+        return caseData;
     }
 }
---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/casesubmission/SampleCaseSubmissionTestDataHelper.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.fpl.service.casesubmission;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SampleCaseSubmissionTestDataHelper {
+    private SampleCaseSubmissionTestDataHelper() {
+    }
+
+    public static Map<String, Object> expectedDCaseSubmissionTemplateData() {
+        return new HashMap<>();
+    }
+}
