fdbe20790ae946d0dce7151b2103dcaf90023303
Etimbuk <euedofia@gmail.com>
2020-04-22 11:18:13 +0100

    FPLA-290 move document generation into separate service -> CaseSubmissionDocumentGenerationService

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/CaseSubmissionService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/CaseSubmissionService.java
@@ -1,150 +1,30 @@
 package uk.gov.hmcts.reform.fpl.service;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.document.domain.Document;
-import uk.gov.hmcts.reform.fpl.config.utils.EmergencyProtectionOrderDirectionsType;
-import uk.gov.hmcts.reform.fpl.config.utils.EmergencyProtectionOrdersType;
-import uk.gov.hmcts.reform.fpl.enums.OrderType;
-import uk.gov.hmcts.reform.fpl.model.Applicant;
-import uk.gov.hmcts.reform.fpl.model.ApplicantParty;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Orders;
-import uk.gov.hmcts.reform.fpl.model.Respondent;
-import uk.gov.hmcts.reform.fpl.model.RespondentParty;
 import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
-import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisSubmittedForm;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
 
-import static java.lang.String.format;
-import static java.util.stream.Collectors.joining;
-import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.C110A;
-import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.DATE;
-import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateToString;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class CaseSubmissionService extends DocmosisTemplateDataGeneration<DocmosisSubmittedForm> {
-    private static final String NEW_LINE = "\n";
-    private static final String DEFAULT_STRING = "-";
-
+public class CaseSubmissionService  {
     private final DocmosisDocumentGeneratorService docmosisDocumentGeneratorService;
     private final UploadDocumentService uploadDocumentService;
-    private final UserDetailsService userDetailsService;
+    private final CaseSubmissionDocumentGenerationService documentGenerationService;
 
     public Document generateSubmittedFormPDF(final CaseData caseData, final String pdfFileName)
             throws IOException {
-        DocmosisSubmittedForm submittedFormData = getTemplateData(caseData);
+        DocmosisSubmittedForm submittedFormData = documentGenerationService.getTemplateData(caseData);
 
         DocmosisDocument document = docmosisDocumentGeneratorService.generateDocmosisDocument(submittedFormData, C110A);
 
         return uploadDocumentService.uploadPDF(document.getBytes(), pdfFileName);
-    }
-
-    public DocmosisSubmittedForm getTemplateData(final CaseData caseData) throws IOException {
-        DocmosisSubmittedForm.Builder applicationFormBuilder = DocmosisSubmittedForm.builder();
-
-        applicationFormBuilder
-                .applicantOrganisations(getApplicantsOrganisations(caseData.getAllApplicants()))
-                .respondentNames(getRespondentsNames(caseData.getAllRespondents()))
-                .submittedDate(formatLocalDateToString(caseData.getDateSubmitted(), DATE))
-                .ordersNeeded(getOrdersNeeded(caseData.getOrders()))
-                .directionsNeeded(getDirectionsNeeded(caseData.getOrders()))
-                .allocation(caseData.getAllocationProposal())
-                .hearing(caseData.getHearing())
-                .hearingPreferences(caseData.getHearingPreferences())
-                .internationalElement(caseData.getInternationalElement())
-                .risks(caseData.getRisks())
-                .factorsParenting(caseData.getFactorsParenting())
-                .proceeding(caseData.getProceeding())
-                .userFullName(userDetailsService.getUserName());
-        applicationFormBuilder.courtseal(format(BASE_64, generateCourtSealEncodedString()));
-
-        return applicationFormBuilder.build();
-    }
-
-    private String getApplicantsOrganisations(final List<Element<Applicant>> applicants) {
-        return applicants.stream()
-            .map(Element::getValue)
-            .filter(Objects::nonNull)
-            .map(Applicant::getParty)
-            .filter(Objects::nonNull)
-            .map(ApplicantParty::getOrganisationName)
-            .filter(StringUtils::isNotBlank)
-            .collect(joining(NEW_LINE));
-    }
-
-    private String getRespondentsNames(final List<Element<Respondent>> respondents) {
-        return respondents.stream()
-            .map(Element::getValue)
-            .filter(Objects::nonNull)
-            .map(Respondent::getParty)
-            .filter(Objects::nonNull)
-            .map(RespondentParty::getFullName)
-            .filter(StringUtils::isNotBlank)
-            .collect(joining(NEW_LINE));
-    }
-
-    private String getOrdersNeeded(final Orders orders) {
-        StringBuilder sb = new StringBuilder();
-
-        if (orders != null && isNotEmpty(orders.getOrderType())) {
-            sb.append(orders.getOrderType().stream()
-                    .map(OrderType::getLabel)
-                    .collect(joining(NEW_LINE)));
-
-            if (StringUtils.isNotEmpty(orders.getOtherOrder())) {
-                sb.append(orders.getOtherOrder());
-                sb.append(NEW_LINE);
-            }
-
-            if (isNotEmpty(orders.getEmergencyProtectionOrders())) {
-                sb.append(orders.getEmergencyProtectionOrders().stream()
-                    .map(EmergencyProtectionOrdersType::getLabel)
-                    .collect(joining(NEW_LINE)));
-
-                if (StringUtils.isNotEmpty(orders.getEmergencyProtectionOrderDetails())) {
-                    sb.append(orders.getEmergencyProtectionOrderDetails());
-                }
-            }
-
-        }
-
-        return StringUtils.isNotEmpty(sb.toString()) ? sb.toString() : DEFAULT_STRING;
-    }
-
-    private String getDirectionsNeeded(final Orders orders) {
-        StringBuilder sb = new StringBuilder();
-        if (orders != null && (isNotEmpty(orders.getOrderType()) || StringUtils.isNotEmpty(orders.getDirections()))) {
-
-            if (isNotEmpty(orders.getEmergencyProtectionOrderDirections())) {
-                sb.append(orders.getEmergencyProtectionOrderDirections().stream()
-                    .map(EmergencyProtectionOrderDirectionsType::getLabel)
-                    .collect(joining(NEW_LINE)));
-            }
-
-            if (StringUtils.isNotEmpty(orders.getEmergencyProtectionOrderDirectionDetails())) {
-                sb.append(orders.getEmergencyProtectionOrderDirectionDetails());
-                sb.append(NEW_LINE);
-            }
-
-            if (StringUtils.isNotEmpty(orders.getDirections())) {
-                sb.append(orders.getDirections());
-                sb.append(NEW_LINE);
-            }
-
-            if (StringUtils.isNotEmpty(orders.getDirectionDetails())) {
-                sb.append(orders.getDirectionDetails());
-            }
-        }
-
-        return StringUtils.isNotEmpty(sb.toString()) ? sb.toString() : DEFAULT_STRING;
     }
 }
---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/CaseSubmissionDocumentGenerationServiceTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.reform.fpl.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.fpl.service.config.LookupTestConfig;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {CaseSubmissionDocumentGenerationService.class, JacksonAutoConfiguration.class,
+    LookupTestConfig.class})
+public class CaseSubmissionDocumentGenerationServiceTest {
+    @MockBean
+    private UserDetailsService userDetailsService;
+}
