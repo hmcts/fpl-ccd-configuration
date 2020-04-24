87d322593f96697262d3c2d2aacdacd62c6488c4
Etimbuk <euedofia@gmail.com>
2020-04-24 13:02:12 +0100

    FPLA-290 test updates

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionService.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.document.domain.Document;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
+import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisCaseSubmission;
 import uk.gov.hmcts.reform.fpl.service.DocmosisDocumentGeneratorService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 
 import java.io.IOException;
-import java.util.Map;
 
 import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.C110A;
 
@@ -23,10 +23,10 @@ public class CaseSubmissionService  {
 
     public Document generateSubmittedFormPDF(final CaseData caseData, final String pdfFileName)
             throws IOException {
-        Map<String, Object> submittedFormData = documentGenerationService.getTemplateData(caseData, false);
+        DocmosisCaseSubmission submittedCase = documentGenerationService.getTemplateData(caseData);
 
         DocmosisDocument document = docmosisDocumentGeneratorService.generateDocmosisDocument(
-            submittedFormData, C110A);
+            submittedCase, C110A);
 
         return uploadDocumentService.uploadPDF(document.getBytes(), pdfFileName);
     }
--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionTemplateDataGenerationServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/casesubmission/CaseSubmissionTemplateDataGenerationServiceTest.java
@@ -10,16 +10,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisCaseSubmission;
 import uk.gov.hmcts.reform.fpl.service.UserDetailsService;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
-import static uk.gov.hmcts.reform.fpl.service.casesubmission.SampleCaseSubmissionTestDataHelper.COURT_SEAL_KEY;
-import static uk.gov.hmcts.reform.fpl.service.casesubmission.SampleCaseSubmissionTestDataHelper.DRAFT_WATERMARK_KEY;
 import static uk.gov.hmcts.reform.fpl.service.casesubmission.SampleCaseSubmissionTestDataHelper.expectedDCaseSubmissionTemplateData;
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.populatedCaseDetails;
 
@@ -42,28 +40,10 @@ public class CaseSubmissionTemplateDataGenerationServiceTest {
 
     @Test
     void shouldReturnExpectedTemplateDataWithCourtSealWhenAllDataPresent() throws IOException {
-        Map<String, Object> returnedTemplateData = templateDataGenerationService.getTemplateData(
-            prepareCaseData(), false);
+        DocmosisCaseSubmission returnedCaseSubmission = templateDataGenerationService.getTemplateData(
+            prepareCaseData());
 
-        assertThat(returnedTemplateData).isEqualTo(expectedDCaseSubmissionTemplateData());
-    }
-
-    @Test
-    void shouldReturnTemplateDataWithCourtSealAsNotNullWhenCaseIsNotDraft() throws IOException {
-        Map<String, Object> returnedTemplateData = templateDataGenerationService.getTemplateData(
-            prepareCaseData(), false);
-
-        assertThat(returnedTemplateData).hasEntrySatisfying(COURT_SEAL_KEY,
-            value -> assertThat(value).isNotNull());
-    }
-
-    @Test
-    void shouldReturnTemplateDataWithDraftWaterAsNotNullWhenCaseIsInDraft() throws IOException {
-        Map<String, Object> returnedTemplateData = templateDataGenerationService.getTemplateData(
-            prepareCaseData(), true);
-
-        assertThat(returnedTemplateData).hasEntrySatisfying(DRAFT_WATERMARK_KEY,
-            value -> assertThat(value).isNotNull());
+        assertThat(returnedCaseSubmission).isEqualTo(expectedDCaseSubmissionTemplateData());
     }
 
     private CaseData prepareCaseData() {
