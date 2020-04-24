85e825fbe79e7a016b5b7fa11e06aa511597093f
Sam Walker <sam.walker@hmcts.net>
2020-04-24 11:45:58 +0100

    impr: cmo generation to use java models (FPLA-1488) (#1107)
    * refactored java code to use java models for cmo generation
    
    * small fixes
    
    * updated docmosis template with changes
    
    * added abstract data class, renamed cmo and sdo generation classes
    
    * updated template data call to use new overload method
    
    * updated docmosis template generation service
    
    * reduced number of times files are read to only once.
    small clean up
    
    * addressed code review comments
    
    * ignore boolean variables in casemanagement order
    
    * reverted changes from master branch
    
    * removed case management order number from docmosis doc
    
    * improvements to directions method in cmo generation
    
    * refactored buildDocmosisDirections method
    
    Co-authored-by: Lewis Birks <lewisb@kainos.com>
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DraftOrdersControllerMidEventTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/DraftOrdersControllerMidEventTest.java
@@ -16,6 +16,7 @@ import uk.gov.hmcts.reform.fpl.model.Judge;
 import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
+import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisData;
 import uk.gov.hmcts.reform.fpl.service.DocmosisDocumentGeneratorService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 import uk.gov.hmcts.reform.fpl.service.time.Time;
@@ -56,7 +57,7 @@ public class DraftOrdersControllerMidEventTest extends AbstractControllerTest {
 
     private Document document = document();
 
-    public DraftOrdersControllerMidEventTest() {
+    DraftOrdersControllerMidEventTest() {
         super("draft-standard-directions");
     }
 
@@ -64,7 +65,9 @@ public class DraftOrdersControllerMidEventTest extends AbstractControllerTest {
     void setup() {
         DocmosisDocument docmosisDocument = new DocmosisDocument(SEALED_ORDER_FILE_NAME, PDF);
 
-        given(documentGeneratorService.generateDocmosisDocument(any(), any())).willReturn(docmosisDocument);
+        given(documentGeneratorService.generateDocmosisDocument(any(DocmosisData.class), any()))
+            .willReturn(docmosisDocument);
+
         given(uploadDocumentService.uploadPDF(PDF, DRAFT_ORDER_FILE_NAME)).willReturn(document);
     }
 
