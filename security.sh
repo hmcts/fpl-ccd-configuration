af13078e467022fd12b84d507e513bc7eae24ab9
Doug Rice <douglasrice@yahoo.com>
2020-04-28 12:50:04 +0100

    FPLA-1661: amendments

--- security.sh
+++ security.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo ${URL}
-zap-api-scan.py -t ${URL}/v2/api-docs -f openapi -u ${Rules} -P 1001
+zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 cp /zap/api-report.html functional-output/
