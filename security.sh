d5f735d289292278de01143c05b00960a9710cc9
Doug Rice <douglasrice@yahoo.com>
2020-04-28 15:15:28 +0100

    FPLS-1661: Trying new report name

--- security.sh
+++ security.sh
@@ -3,5 +3,5 @@ echo ${URL}
 zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-cp /zap/api-report.html functional-output/
+cp /zap/api-report.html functional-output/zap-security-report.html
 zap-cli -p 1001 alerts -l Informational
