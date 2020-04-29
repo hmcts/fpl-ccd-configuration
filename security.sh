845e64d0278741455b13a1e3b9b68c3e942b663e
Doug Rice <douglasrice@yahoo.com>
2020-04-29 14:55:37 +0100

    FPLA-1661: Change to test for url only in config

--- security.sh
+++ security.sh
@@ -2,7 +2,8 @@
 echo ${URL_FOR_SECURITY_SCAN}
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001
+#zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -u ${SECURITY_RULES} -P 1001
+zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN} -f openapi -u ${SECURITY_RULES} -P 1001
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
