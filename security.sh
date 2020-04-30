34275faade133e3b371a025d6c7137a624722723
Doug Rice <douglasrice@yahoo.com>
2020-04-30 11:37:56 +0100

    FPLA-1661: false positives

--- security.sh
+++ security.sh
@@ -4,7 +4,10 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 zap-api-scan.py -t "${URL_FOR_SECURITY_SCAN}/v2/api-docs" -f openapi -S -s -d -u ${SECURITY_RULES} -P 1001 -l FAIL
 cat zap.out
+echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+mkdir -p functional-output
+chmod a+wx functional-output
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp /zap/api-report.html functional-output/zap-security-report.html
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
