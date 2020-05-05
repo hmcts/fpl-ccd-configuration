2ba15486b4347e8f8e92efc7cffc502aab4b8326
satya <1426759+satyachundur@users.noreply.github.com>
2020-05-05 14:34:55 +0100

    Update security.sh

--- security.sh
+++ security.sh
@@ -6,7 +6,7 @@ zap-api-scan.py -t "${URL_FOR_SECURITY_SCAN}/v2/api-docs" -f openapi -S -s -d -u
 cat zap.out
 echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-curl --fail http://0.0.0.0:1001/a1/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp report.json functional-output/report.json
 cp /zap/api-report.html functional-output/zap-security-report.html
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
