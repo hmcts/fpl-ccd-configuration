2390bbd9b08534efa0453dfdc2b6c19d8623e592
satya <1426759+satyachundur@users.noreply.github.com>
2020-05-05 11:55:25 +0100

    removing zap-cli

--- security.sh
+++ security.sh
@@ -2,11 +2,12 @@
 echo ${URL_FOR_SECURITY_SCAN}
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-api-scan.py -t "${URL_FOR_SECURITY_SCAN}/v2/api-docs" -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL -s
+zap-api-scan.py -t "${URL_FOR_SECURITY_SCAN}/v2/api-docs" -f openapi -r api-report.html -S -l FAIL -s -d -u ${SECURITY_RULES} -P 1001
 cat zap.out
 echo "ZAP has successfully started"
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+#zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+curl --fail http://0.0.0.0:100/a1/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp /zap/report.json functional-output/report.json
+cat /zap/report.json
 cp /zap/api-report.html functional-output/zap-security-report.html
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
