66d40d1bb45206877459842753f3e17255b52f5f
Sam Walker <sam.walker@hmcts.net>
2020-05-07 12:41:42 +0100

    impr: rework ActionCaseManagementOrder about to submit (FPLA-1469) (#1143)

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/DocumentService.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.fpl.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.document.domain.Document;
+import uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates;
+import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
+import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisOrder;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DocumentService {
+    private final DocmosisDocumentGeneratorService documentGeneratorService;
+    private final UploadDocumentService uploadDocumentService;
+
+    public <T extends DocmosisOrder> Document getDocumentFromDocmosisOrderTemplate(T templateData,
+                                                                                   DocmosisTemplates template) {
+        DocmosisDocument document = documentGeneratorService.generateDocmosisDocument(templateData, template);
+
+        String documentTitle = getDocumentTitle(templateData.getDraftbackground(), document);
+
+        return uploadDocumentService.uploadPDF(document.getBytes(), documentTitle);
+    }
+
+    private String getDocumentTitle(String draftBackground, DocmosisDocument document) {
+        return draftBackground == null ? document.getDocumentTitle() : document.getDraftDocumentTile();
+    }
+}
