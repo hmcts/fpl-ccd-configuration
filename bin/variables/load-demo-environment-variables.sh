521582b1a2457e4acdbaa245e04d753e8a15cf0e
Mounika Ammineni <mounika.ammineni@hmcts.net>
2020-05-11 16:40:27 +0100

    Update xui env specific attributes

--- bin/variables/load-demo-environment-variables.sh
+++ bin/variables/load-demo-environment-variables.sh
@@ -7,7 +7,7 @@ echo 'export ENVIRONMENT=demo'
 # urls
 echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-demo.service.core-compute-demo.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.demo.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://www-ccd.demo.platform.hmcts.net/oauth2redirect"
+echo "export CCD_IDAM_REDIRECT_URL=https://xui-webapp-demo.service.core-compute-demo.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://ccd-definition-store-api-demo.service.core-compute-demo.internal"
 
 # definition placeholders
