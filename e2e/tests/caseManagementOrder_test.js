0bc75ed541b0c5eda8a098dd8dfa6279cc9e5f89
James Nelson <Jamesnelson117@gmail.com>
2020-05-12 10:13:37 +0100

    FPLA-1598: e2e test fixes

--- e2e/tests/caseManagementOrder_test.js
+++ e2e/tests/caseManagementOrder_test.js
@@ -28,7 +28,7 @@ BeforeSuite(async (I, caseViewPage, submitApplicationEventPage, enterFamilyManCa
 
   //gatekeeper adds allocated judge
   await caseViewPage.goToNewActions(config.applicationActions.allocatedJudge);
-  await allocatedJudgeEventPage.enterAllocatedJudge('Moley');
+  await allocatedJudgeEventPage.enterAllocatedJudge('Moley', 'moley@example.com');
   await I.completeEvent('Save and continue');
 
   await caseViewPage.goToNewActions(config.administrationActions.addHearingBookingDetails);
