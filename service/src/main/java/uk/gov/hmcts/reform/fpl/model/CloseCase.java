62a903125733f794646b1a7843469fd41b5224cf
Lewis Birks <lewisb@kainos.com>
2020-05-13 16:43:17 +0100

    display field in history tab for checking purposes, merged the 2 fields for a cleaner java model

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CloseCase.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CloseCase.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.fpl.model;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Builder;
 import lombok.Data;
@@ -13,43 +14,48 @@ import uk.gov.hmcts.reform.fpl.validation.groups.CloseCaseGroup;
 import java.time.LocalDate;
 import javax.validation.constraints.PastOrPresent;
 
+import static uk.gov.hmcts.reform.fpl.enums.YesNo.NO;
+import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.enums.ccd.fixedlists.CloseCaseReason.DEPRIVATION_OF_LIBERTY;
-import static uk.gov.hmcts.reform.fpl.enums.ccd.fixedlists.CloseCaseReason.FINAL_ORDER;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CloseCase {
-    private final YesNo showFullReason;
+    // This field is hidden so runs into our favourite CCD issue of not persisting, we are ignoring the
+    // deserialization of the object as we can infer what it is supposed to be from which reason field is populated
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private YesNo showFullReason;
     @PastOrPresent(message = "The close case date must be in the past", groups = CloseCaseGroup.class)
-    private final LocalDate date;
+    private LocalDate date;
+    private String details;
     @JsonIgnore
     private CloseCaseReason reason;
-    private final String details;
+
+    @JsonGetter("fullReason")
+    public CloseCaseReason getFullReason() {
+        return showFullReason == YES ? reason : null;
+    }
 
     @JsonSetter("fullReason")
     public void setFullReason(CloseCaseReason fullReason) {
         if (reason != null) {
             this.reason = fullReason;
+            this.showFullReason = YES;
         }
+    }
 
+    @JsonGetter("partialReason")
+    public CloseCaseReason getPartialReason() {
+        return showFullReason == NO ? reason : null;
     }
 
     @JsonSetter("partialReason")
     public void setPartialReason(CloseCaseReason partialReason) {
         if (partialReason != null) {
             this.reason = partialReason;
+            this.showFullReason = NO;
         }
-    }
-
-    @JsonGetter("fullReason")
-    public CloseCaseReason getFullReason() {
-        return reason;
-    }
-
-    @JsonGetter("partialReason")
-    public CloseCaseReason getPartialReason() {
-        return reason == FINAL_ORDER ? null : reason;
     }
 
     public boolean hasDeprivationOfLiberty() {
