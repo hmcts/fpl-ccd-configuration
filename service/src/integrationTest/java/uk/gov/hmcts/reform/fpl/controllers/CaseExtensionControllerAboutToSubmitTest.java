d3a60a2cb0a0ec1d5024ddb09330dc7a37b81640
Toireasa Moley <toireasa.moley@hmcts.net>
2020-05-14 13:58:36 +0100

    Adding controller tests

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/CaseExtensionControllerAboutToSubmitTest.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("integration-test")
+@WebMvcTest(AllocationDecisionController.class)
+@OverrideAutoConfiguration(enabled = true)
+class CaseExtensionControllerAboutToSubmitTest extends AbstractControllerTest {
+
+    CaseExtensionControllerAboutToSubmitTest() {
+        super("case-extension");
+    }
+
+    @Test
+    void shouldPopulateCaseCompletionDateWhenSubmittingWithOtherDate() {
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("extensionDateOther", LocalDate.of(2030, 11, 12));
+        data.put("caseExtensionTimeList", "otherExtension");
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(data)
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postAboutToSubmitEvent(caseDetails);
+
+        assertThat(callbackResponse.getData().get("caseCompletionDate")).isEqualTo("2030-11-12");
+    }
+
+    @Test
+    void shouldPopulateCaseCompletionDateWhenSubmittingWith8WeekExtensionOther() {
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("caseExtensionTimeList", "EightWeekExtension");
+        data.put("caseExtensionTimeConfirmationList", "EightWeekExtensionDateOther");
+        data.put("eightWeeksExtensionDateOther", LocalDate.of(2030, 11, 12));
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(data)
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postAboutToSubmitEvent(caseDetails);
+
+        assertThat(callbackResponse.getData().get("caseCompletionDate")).isEqualTo("2030-11-12");
+    }
+}
