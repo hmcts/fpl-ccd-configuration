04e4ba8993ebe9196fffe3e90a52a018d684cd6c
Mounika Ammineni <mounika.ammineni@hmcts.net>
2020-05-14 11:03:41 +0100

    Adding complex type permissions

--- e2e/tests/judiciaryAdministersCaseAfterSubmission_test.js
+++ e2e/tests/judiciaryAdministersCaseAfterSubmission_test.js
@@ -33,6 +33,7 @@ Scenario('Judiciary adds allocated judge', async (I, caseViewPage, allocatedJudg
   caseViewPage.selectTab(caseViewPage.tabs.casePeople);
   I.seeInTab(['Allocated Judge', 'Judge or magistrate\'s title'], 'Her Honour Judge');
   I.seeInTab(['Allocated Judge', 'Last name'], 'Moley');
+  I.seeInTab(['Allocated Judge', 'Email Address'], 'moley@example.com');
 });
 
 Scenario('Judiciary enters hearing details and submits', async (I, caseViewPage, loginPage, addHearingBookingDetailsEventPage) => {
