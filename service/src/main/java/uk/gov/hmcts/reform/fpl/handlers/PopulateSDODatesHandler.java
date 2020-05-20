a9373a0a2533b6c39ab13143118f7faea9814148
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2020-05-20 15:45:30 +0100

    pulled StandardDirectionsService from FPLA-1790

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateSDODatesHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateSDODatesHandler.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,29 +9,78 @@ import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.enums.DirectionAssignee;
 import uk.gov.hmcts.reform.fpl.events.PopulateSDODatesEvent;
-import uk.gov.hmcts.reform.fpl.service.StandardDirectionOrderPopulationService;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Direction;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.service.CommonDirectionService;
+import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
+import uk.gov.hmcts.reform.fpl.service.StandardDirectionsService;
 import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
 public class PopulateSDODatesHandler {
     private final CoreCaseDataService coreCaseDataService;
-    private final StandardDirectionOrderPopulationService standardDirectionOrderPopulationService;
+    private final StandardDirectionsService standardDirectionsService;
+    private final CommonDirectionService commonDirectionService;
+    private final ObjectMapper mapper;
+    private final HearingBookingService hearingBookingService;
 
     @Async
     @EventListener
-    public void populateSDODates(PopulateSDODatesEvent event) throws IOException {
+    public void populateSDODates(PopulateSDODatesEvent event) {
         CaseDetails caseDetails = event.getCallbackRequest().getCaseDetails();
-        if (standardDirectionOrderPopulationService.hearingDetailsAdded(caseDetails)) {
-            coreCaseDataService.triggerEvent(caseDetails.getJurisdiction(),
-                caseDetails.getCaseTypeId(),
-                caseDetails.getId(),
-                "populateSDO",
-                standardDirectionOrderPopulationService.populateDates(caseDetails));
+
+        getFirstHearing(caseDetails).ifPresent(firstHearing ->
+        {
+            try {
+                coreCaseDataService.triggerEvent(caseDetails.getJurisdiction(),
+                    caseDetails.getCaseTypeId(),
+                    caseDetails.getId(),
+                    "populateSDO",
+                    populateDates(firstHearing, caseDetails.getData()));
+            } catch (IOException e) {
+                //TODO: this exception should be handled in JsonOrdersLookupService
+            }
+        });
+    }
+
+    private Optional<HearingBooking> getFirstHearing(CaseDetails caseDetails) {
+        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+
+        return hearingBookingService.getFirstHearing(caseData.getHearingDetails());
+    }
+
+    private Map<String, Object> populateDates(HearingBooking hearingBooking, Map<String, Object> data) throws IOException {
+        List<Element<Direction>> standardDirections = standardDirectionsService.getDirections(hearingBooking);
+
+        commonDirectionService.sortDirectionsByAssignee(standardDirections).forEach(
+            (assignee, directionsConfigForAssignee) -> populateEmptyDates(data, assignee, directionsConfigForAssignee));
+
+        return data;
+    }
+
+    private void populateEmptyDates(Map<String, Object> data, DirectionAssignee assignee,
+                                    List<Element<Direction>> directionsConfigForAssignee) {
+        List<Element<Direction>> directionsForAssignee = mapper.convertValue(data.get(assignee.getValue()),
+            new TypeReference<>() {});
+        for (int directionIndex = 0; directionIndex < directionsForAssignee.size(); directionIndex++) {
+            var direction = directionsForAssignee.get(directionIndex).getValue();
+            if (direction.getDateToBeCompletedBy() == null) {
+                direction.setDateToBeCompletedBy(directionsConfigForAssignee.get(directionIndex)
+                    .getValue()
+                    .getDateToBeCompletedBy());
+            }
         }
+        data.put(assignee.getValue(), directionsForAssignee);
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateStandardDirectionsHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateStandardDirectionsHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -7,16 +8,27 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.events.PopulateStandardDirectionsEvent;
-import uk.gov.hmcts.reform.fpl.service.StandardDirectionOrderPopulationService;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Direction;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.service.CommonDirectionService;
+import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
+import uk.gov.hmcts.reform.fpl.service.StandardDirectionsService;
 import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PopulateStandardDirectionsHandler {
     private final CoreCaseDataService coreCaseDataService;
-    private final StandardDirectionOrderPopulationService standardDirectionOrderPopulationService;
+    private final StandardDirectionsService standardDirectionsService;
+    private final CommonDirectionService commonDirectionService;
+    private final ObjectMapper mapper;
+    private final HearingBookingService hearingBookingService;
 
     @Async
     @EventListener
@@ -27,6 +39,19 @@ public class PopulateStandardDirectionsHandler {
             caseDetails.getCaseTypeId(),
             caseDetails.getId(),
             "populateSDO",
-            standardDirectionOrderPopulationService.populateStandardDirections(event.getCallbackRequest()));
+            populateStandardDirections(caseDetails));
+    }
+
+    private Map<String, Object> populateStandardDirections(CaseDetails caseDetails) throws IOException {
+        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+        HearingBooking hearingBooking = hearingBookingService.getFirstHearing(caseData.getHearingDetails())
+            .orElse(null);
+
+        List<Element<Direction>> standardDirections = standardDirectionsService.getDirections(hearingBooking);
+        commonDirectionService.sortDirectionsByAssignee(standardDirections)
+            .forEach((directionAssignee, directionsElements) -> caseDetails.getData().put(directionAssignee.getValue(),
+                directionsElements));
+
+        return caseDetails.getData();
     }
 }
