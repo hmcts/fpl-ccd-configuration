abda0ef45ac2b8f6f9e4c4b8c85acf481c69455f
Lewis Birks <lewisb@kainos.com>
2020-05-21 11:12:50 +0100

    introduce new case fields to CaseData and update JsonProperty of State enums

--- service/src/main/java/uk/gov/hmcts/reform/fpl/enums/State.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/enums/State.java
@@ -1,16 +1,25 @@
 package uk.gov.hmcts.reform.fpl.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
 public enum State {
+    @JsonProperty("Open")
     OPEN("Open"),
+
+    @JsonProperty("Submitted")
     SUBMITTED("Submitted"),
+
+    @JsonProperty("Gatekeeping")
     GATEKEEPING("Gatekeeping"),
+
     PREPARE_FOR_HEARING("PREPARE_FOR_HEARING"),
     CLOSED("CLOSED"),
+
+    @JsonProperty("Deleted")
     DELETED("Deleted");
 
     private final String value;
--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -11,6 +11,7 @@ import lombok.Data;
 import uk.gov.hmcts.reform.fpl.enums.C2ApplicationType;
 import uk.gov.hmcts.reform.fpl.enums.CaseExtensionTime;
 import uk.gov.hmcts.reform.fpl.enums.EPOType;
+import uk.gov.hmcts.reform.fpl.enums.State;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
 import uk.gov.hmcts.reform.fpl.model.common.Document;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentBundle;
@@ -69,6 +70,7 @@ import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 import static uk.gov.hmcts.reform.fpl.enums.CMOStatus.SEND_TO_JUDGE;
+import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.DATE;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateToString;
 
@@ -77,6 +79,7 @@ import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateT
 @AllArgsConstructor
 @HasDocumentsIncludedInSwet(groups = UploadDocumentsGroup.class)
 public class CaseData {
+    private final State state;
     @NotBlank(message = "Enter a case name")
     private final String caseName;
     private final String caseLocalAuthority;
@@ -440,4 +443,10 @@ public class CaseData {
 
     private final CloseCase closeCase;
     private final String deprivationOfLiberty;
+    private final String closeCaseFromOrder;
+
+    @JsonIgnore
+    public boolean isClosedFromOrder() {
+        return YES.getValue().equals(closeCaseFromOrder);
+    }
 }
