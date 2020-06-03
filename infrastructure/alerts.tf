7f3c9e7499161d52b92c7ec896326ee333f868c5
James Nelson <Jamesnelson117@gmail.com>
2020-06-03 10:36:53 +0100

    feat: app insights integration with slack (FPLA-1752) (#1299)

--- infrastructure/alerts.tf
+++ infrastructure/alerts.tf
@@ -19,8 +19,8 @@ module "fpl-performance-alert" {
   app_insights_name          = "${var.product}-${var.component}-appinsights-${var.env}"
   alert_name                 = "${var.product}-performance-alert"
   alert_desc                 = "Requests that took longer than 1 seconds to complete"
-  app_insights_query         = "requests | where url !contains '/health' and success == 'True' and duration > 1000 | project timestamp, name, operation_Id, duration | sort by duration nulls last"
-  custom_email_subject       = "Alert: performance errors"
+  app_insights_query         = "requests | where url !contains '/health' and success == 'True' and duration > 1000 | project timestamp, operation_Id, name, duration | sort by duration nulls last"
+  custom_email_subject       = "Alert: Performance errors"
   frequency_in_minutes       = 5
   time_window_in_minutes     = 5
   severity_level             = "2"
@@ -31,20 +31,38 @@ module "fpl-performance-alert" {
   enabled                    = "${var.enable_alerts}"
 }
 
-module "fpl-grant-case-access-failure-alert" {
+module "fpl-exceptions-alert" {
   source                     = "git@github.com:hmcts/cnp-module-metric-alert"
   location                   = "${var.appinsights_location}"
   app_insights_name          = "${var.product}-${var.component}-appinsights-${var.env}"
-  alert_name                 = "${var.product}-grant-case-access-failure"
-  alert_desc                 = "Grant case access failure"
-  app_insights_query         = "exceptions | where type contains 'GrantCaseAccessException' | project timestamp , outerMessage"
-  custom_email_subject       = "Alert: Grant case access failed"
+  alert_name                 = "${var.product}-exceptions-alert"
+  alert_desc                 = "All exceptions within FPL"
+  app_insights_query         = "exceptions | where operation_Name !contains "health" | project timestamp, operation_Id, outerMessage, operation_Name"
+  custom_email_subject       = "Alert: FPL all exceptions"
   frequency_in_minutes       = 5
   time_window_in_minutes     = 5
-  severity_level             = "1"
+  severity_level             = "3"
   action_group_name          = "${var.product}-support"
   trigger_threshold_operator = "GreaterThan"
   trigger_threshold          = 0
+  resourcegroup_name         = "${local.alert_resource_group_name}"
+  enabled                    = "${var.enable_alerts}"
+}
+
+module "fpl-health-failure-alert" {
+  source                     = "git@github.com:hmcts/cnp-module-metric-alert"
+  location                   = "${var.appinsights_location}"
+  app_insights_name          = "${var.product}-${var.component}-appinsights-${var.env}"
+  alert_name                 = "${var.product}-health-failure-alert"
+  alert_desc                 = "Failed health requests"
+  app_insights_query         = "requests | where url contains "health" | where resultCode != "200" | project timestamp, operation_Id, resultCode"
+  custom_email_subject       = "Alert: Health failure"
+  frequency_in_minutes       = 5
+  time_window_in_minutes     = 5
+  severity_level             = "3"
+  action_group_name          = "${var.product}-support"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 3
   resourcegroup_name         = "${local.alert_resource_group_name}"
   enabled                    = "${var.enable_alerts}"
 }
