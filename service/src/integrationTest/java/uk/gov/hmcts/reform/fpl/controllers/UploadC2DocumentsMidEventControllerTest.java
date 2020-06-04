aba28b91392bb5d6f855b9416a8e973724d05be1
Etimbuk U <Gogetter@users.noreply.github.com>
2020-06-04 09:52:42 +0100

    chore(deps): bump launchdarkly-java-server sdk to version 5.0.0 (#1305)

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsMidEventControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsMidEventControllerTest.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
-import com.launchdarkly.client.LDClient;
+import com.launchdarkly.sdk.server.LDClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.fpl.service;
 
-import com.launchdarkly.client.LDClient;
-import com.launchdarkly.client.LDUser;
+import com.launchdarkly.sdk.LDUser;
+import com.launchdarkly.sdk.server.LDClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
