e0717022ccdb3c99a3019ddafed08e6f86a9ccf6
Doug Rice <douglasrice@yahoo.com>
2020-06-05 19:27:05 +0100

    test

--- security.sh
+++ security.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# This file is called by the jenkins nightly pipeline -> file: Jenkins_nightly -> function: enableSecurityScan()
-echo ${URL_FOR_SECURITY_SCAN}
+#setting encoding for Python 2 / 3 compatibilities
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-zap-api-scan.py -t "${URL_FOR_SECURITY_SCAN}/v2/api-docs" -f openapi -S -s -d -u ${SECURITY_RULES} -P 1001 -l FAIL
+echo ${TEST_URL}
+zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL
 cat zap.out
-echo "ZAP has successfully started"
-zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-cp report.json functional-output/report.json
-cp /zap/api-report.html functional-output/zap-security-report.html
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
+cp report.json functional-output/
+cp api-report.html functional-output/
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
