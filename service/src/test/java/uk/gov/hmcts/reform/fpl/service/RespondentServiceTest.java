6204235a908c469b0e772d6fa23b45548809a985
James Nelson <Jamesnelson117@gmail.com>
2020-06-26 13:31:06 +0100

    FPLA-1927: Code review comments

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/RespondentServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/RespondentServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.model.Respondent;
 import uk.gov.hmcts.reform.fpl.model.RespondentParty;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
@@ -13,6 +12,7 @@ import java.util.List;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
@@ -61,8 +61,8 @@ class RespondentServiceTest {
             List<Element<Respondent>> updatedRespondents = service.setPersistRepresentativeFlag(getRespondents());
             List<Respondent> respondentsList = unwrapElements(updatedRespondents);
 
-            assertThat(respondentsList.get(0).getPersistRepresentedBy()).isEqualTo(YesNo.YES.getValue());
-            assertThat(respondentsList.get(1).getPersistRepresentedBy()).isEqualTo(YesNo.YES.getValue());
+            assertThat(respondentsList.get(0).getPersistRepresentedBy()).isEqualTo(YES.getValue());
+            assertThat(respondentsList.get(1).getPersistRepresentedBy()).isEqualTo(YES.getValue());
         }
 
         private List<Element<Respondent>> getRespondents() {
