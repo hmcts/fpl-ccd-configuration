d6440bea9b194a0a9c5417ef41400305637cae74
Tomasz Powroznik <tomas.powroznik@gmail.com>
2020-06-29 10:07:10 +0100

    impr: Refactor order related e2e to not run tests in a loop

--- e2e/tests/judiciaryAdministersCaseAfterSubmission_test.js
+++ e2e/tests/judiciaryAdministersCaseAfterSubmission_test.js
@@ -1,5 +1,11 @@
 const config = require('../config.js');
-const orders = require('../fixtures/orders.js');
+const blankOrder = require('../fixtures/orders/blankOrder.js');
+const interimSuperVisionOrder = require('../fixtures/orders/interimSupervision.js');
+const finalSuperVisionOrder = require('../fixtures/orders/finalSupervisionOrder.js');
+const emergencyProtectionOrder = require('../fixtures/orders/emergencyProtectionOrder.js');
+const interimCareOrder = require('../fixtures/orders/interimCareOrder.js');
+const finalCareOrder = require('../fixtures/orders/finalCareOrder.js');
+const dischargeOfCareOrder = require('../fixtures/orders/dischargeOfCareOrder.js');
 const orderFunctions = require('../helpers/generated_order_helper');
 const mandatoryWithMultipleChildren = require('../fixtures/mandatoryWithMultipleChildren.json');
 
@@ -22,13 +28,38 @@ BeforeSuite(async (I, caseViewPage, submitApplicationEventPage, enterFamilyManCa
 
 Before(async I => await I.navigateToCaseDetails(caseId));
 
-
-Scenario('Judiciary creates multiple orders for the case', async (I, caseViewPage, addHearingBookingDetailsEventPage, createOrderEventPage) => {
-  for (let i = 0; i < orders.length; i++) {
-    const defaultIssuedDate = new Date();
-    await caseViewPage.goToNewActions(config.administrationActions.createOrder);
-    await orderFunctions.createOrder(I, createOrderEventPage, orders[i], true);
-    I.seeEventSubmissionConfirmation(config.administrationActions.createOrder);
-    await orderFunctions.assertOrder(I, caseViewPage, orders[i], i + 1, defaultIssuedDate, true);
-  }
+Scenario('Judiciary creates blank order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, blankOrder);
 });
+
+Scenario('Judiciary creates interim supervision order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, interimSuperVisionOrder);
+});
+
+Scenario('Judiciary creates final supervision order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, finalSuperVisionOrder);
+});
+
+Scenario('Judiciary creates emergency protection order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, emergencyProtectionOrder);
+});
+
+Scenario('Judiciary creates interim care order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, interimCareOrder);
+});
+
+Scenario('Judiciary creates final care order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, finalCareOrder);
+});
+
+Scenario('Judiciary creates discharge of care order', async (I, caseViewPage, createOrderEventPage) => {
+  await verifyOrderCreation(I, caseViewPage, createOrderEventPage, dischargeOfCareOrder);
+});
+
+const verifyOrderCreation = async function(I, caseViewPage, createOrderEventPage, order){
+  await caseViewPage.goToNewActions(config.administrationActions.createOrder);
+  const defaultIssuedDate = new Date();
+  await orderFunctions.createOrder(I, createOrderEventPage, order);
+  I.seeEventSubmissionConfirmation(config.administrationActions.createOrder);
+  await orderFunctions.assertOrder(I, caseViewPage, order, defaultIssuedDate);
+};
