8c798e794c5991833253a1ec5d2a3ea22ea54bc5
Kiran Varma <varma.ap@gmail.com>
2020-07-08 22:42:42 +0100

    added test

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/NewHearingsAddedEventHandlerTest.java
@@ -0,0 +1,82 @@
+package uk.gov.hmcts.reform.fpl.handlers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.model.notify.hearing.NewNoticeOfHearingTemplate;
+import uk.gov.hmcts.reform.fpl.model.order.selector.Selector;
+import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
+import uk.gov.hmcts.reform.fpl.service.InboxLookupService;
+import uk.gov.hmcts.reform.fpl.service.config.LookupTestConfig;
+import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
+import uk.gov.hmcts.reform.fpl.service.time.Time;
+import uk.gov.hmcts.reform.fpl.utils.FixedTimeConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.fpl.NotifyTemplates.NOTICE_OF_NEW_HEARING;
+import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_CODE;
+import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_EMAIL_ADDRESS;
+import static uk.gov.hmcts.reform.fpl.utils.CaseDataGeneratorHelper.createHearingBooking;
+import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.callbackRequest;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+
+@ContextConfiguration(classes = {
+    NewHearingsAddedEventHandler.class,
+    LookupTestConfig.class,
+    FixedTimeConfiguration.class
+})
+class NewHearingsAddedEventHandlerTest {
+    private static final String CASE_REFERENCE = "12345";
+
+    @Autowired
+    private NewHearingsAddedEventHandler newHearingsAddedEventHandler;
+
+    @Autowired
+    private Time time;
+
+    @MockBean
+    private NotificationService notificationService;
+
+    @MockBean
+    private HearingBookingService hearingBookingService;
+
+    @MockBean
+    private InboxLookupService inboxLookupService;
+
+    private LocalDateTime futureDate;
+
+    @BeforeEach
+    void setUp() {
+        futureDate = time.now().plusDays(1);
+    }
+    @Test
+    void shouldSendNotificationToLAWhenNewHearingIsAdded() {
+        CallbackRequest callbackRequest = callbackRequest();
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+
+        List<Element<HearingBooking>> hearingBookings = List.of(
+            element(UUID.randomUUID(), createHearingBooking(futureDate.plusDays(5), futureDate.plusDays(6))));
+
+        when(hearingBookingService.getSelectedHearings(any(Selector.class), any())).thenReturn(hearingBookings);
+        when(inboxLookupService.getNotificationRecipientEmail(caseDetails, LOCAL_AUTHORITY_CODE))
+            .thenReturn(LOCAL_AUTHORITY_EMAIL_ADDRESS);
+
+        verify(notificationService).sendEmail(
+            NOTICE_OF_NEW_HEARING,
+            LOCAL_AUTHORITY_EMAIL_ADDRESS,
+            any(NewNoticeOfHearingTemplate.class),
+            CASE_REFERENCE);
+    }
+}
