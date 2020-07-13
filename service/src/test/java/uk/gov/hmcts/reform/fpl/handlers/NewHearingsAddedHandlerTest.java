4a30599e07125a08648aa97d5f81418bfe5f36a2
Kiran Varma <varma.ap@gmail.com>
2020-07-13 16:38:32 +0100

    fix merge conflict issues - my bad

--- service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/NewHearingsAddedHandlerTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/NewHearingsAddedHandlerTest.java
@@ -31,6 +31,7 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.NOTICE_OF_NEW_HEARING;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.CAFCASS_EMAIL_ADDRESS;
@@ -93,7 +94,7 @@ class NewHearingsAddedHandlerTest {
 
         newHearingsAddedHandler.sendEmailToLA(new NewHearingsAdded(callbackRequest, hearingBookings));
 
-        verify(notificationService).sendEmail(
+        verify(notificationService, times(1)).sendEmail(
             NOTICE_OF_NEW_HEARING,
             LOCAL_AUTHORITY_EMAIL_ADDRESS,
             noticeOfHearingTemplate,
@@ -109,7 +110,7 @@ class NewHearingsAddedHandlerTest {
 
         newHearingsAddedHandler.sendEmailToCafcass(new NewHearingsAdded(callbackRequest, hearingBookings));
 
-        verify(notificationService).sendEmail(
+        verify(notificationService, times(1)).sendEmail(
             NOTICE_OF_NEW_HEARING,
             CAFCASS_EMAIL_ADDRESS,
             noticeOfHearingTemplate,
@@ -126,7 +127,7 @@ class NewHearingsAddedHandlerTest {
 
         newHearingsAddedHandler.sendEmailToRepresentatives(new NewHearingsAdded(callbackRequest, hearingBookings));
 
-        verify(representativeNotificationService)
+        verify(representativeNotificationService, times(1))
             .sendToRepresentativesByServedPreference(
                 RepresentativeServingPreferences.EMAIL,
                 NOTICE_OF_NEW_HEARING,
@@ -134,7 +135,7 @@ class NewHearingsAddedHandlerTest {
                 eventData
             );
 
-        verify(representativeNotificationService)
+        verify(representativeNotificationService, times(1))
             .sendToRepresentativesByServedPreference(
                 RepresentativeServingPreferences.DIGITAL_SERVICE,
                 NOTICE_OF_NEW_HEARING,
