1eec44721cd4c44f256baf3d226bd410a2245601
Lewis Birks <lewisb@kainos.com>
2020-07-13 13:27:52 +0100

    more deprecations

--- service/src/main/java/uk/gov/hmcts/reform/fpl/enums/CMOStatus.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/enums/CMOStatus.java
@@ -13,11 +13,15 @@ public enum CMOStatus {
     @Deprecated
     PARTIES_REVIEW,
     /**
-     * Indicates that the CMO has been returned to LA.
+     * Indicates that the CMO is still being worked on by the LA or has been sent back to the LA by the judge.
+     *
+     * @deprecated no longer used in the new CMO status model but preserved for old cases.
      */
+    @Deprecated
     SELF_REVIEW,
     /**
      * Indicates that the CMO has been approved.
      */
-    APPROVED
+    APPROVED,
+
 }
