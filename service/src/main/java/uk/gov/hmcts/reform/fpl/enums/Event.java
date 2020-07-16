1c1c61e5e17fe1fc0b72893a189ce57b2a36152e
James Nelson <Jamesnelson117@gmail.com>
2020-07-16 13:37:45 +0100

    FPLA-1816: Revert unintended change

--- service/src/main/java/uk/gov/hmcts/reform/fpl/enums/Event.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/enums/Event.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Event {
-    DRAFT_CASE_MANAGEMENT_ORDER("draftcmo"),
+    DRAFT_CASE_MANAGEMENT_ORDER("draftCMO"),
     ACTION_CASE_MANAGEMENT_ORDER("actionCMO");
 
     private final String id;
