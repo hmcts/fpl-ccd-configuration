950db717ae3c9e60033739d7db64e0b82338296a
Joe Zazzaro-Francis <joez@kainos.com>
2020-07-20 11:17:52 +0100

    ccd definitions for review CMO event

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ReviewAgreedCMOController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ReviewAgreedCMOController.java
@@ -1,4 +1,33 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+
+@Api
+@RestController
+@RequestMapping("/callback/review-cmo")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ReviewAgreedCMOController {
+
+    private final ObjectMapper mapper;
+
+    @PostMapping("/about-to-start")
+    public AboutToStartOrSubmitCallbackResponse handleAboutToStart(@RequestBody CallbackRequest callbackRequest) {
+        CaseDetails caseDetails = callbackRequest.getCaseDetails();
+        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDetails.getData())
+            .build();
+    }
 }
