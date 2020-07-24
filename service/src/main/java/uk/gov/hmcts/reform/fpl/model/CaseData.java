9006c00368297dedda84e90316478a383d6fbd36
Toireasa Moley <toireasa.moley@hmcts.net>
2020-07-24 11:12:14 +0100

    Fixing code smells

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -602,7 +602,7 @@ public class CaseData {
 
     public boolean hasFutureHearing(List<Element<HearingBooking>> hearingBookings) {
         return isNotEmpty(hearingBookings) && hearingBookings.stream()
-            .anyMatch(futureHearing -> futureHearing.getValue().startsAfterToday());
+            .anyMatch(hearing -> hearing.getValue().startsAfterToday());
     }
 
     private final DocumentReference submittedForm;
