223d168c429e86f1fa30bdc71f4c980a5ebac390
Joe Zazzaro-Francis <joez@kainos.com>
2020-07-28 10:40:18 +0100

    About to submit tests

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReviewCMOControllerAboutToSubmitTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReviewCMOControllerAboutToSubmitTest.java
@@ -3,13 +3,44 @@ package uk.gov.hmcts.reform.fpl.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.fpl.controllers.cmo.ReviewCMOController;
+import uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
+import uk.gov.hmcts.reform.fpl.model.ReviewDecision;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
+import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
+import uk.gov.hmcts.reform.fpl.model.order.CaseManagementOrder;
+import uk.gov.hmcts.reform.fpl.service.DocumentSealingService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.reform.fpl.enums.CMOReviewOutcome.JUDGE_REQUESTED_CHANGES;
+import static uk.gov.hmcts.reform.fpl.enums.CMOReviewOutcome.SEND_TO_ALL_PARTIES;
+import static uk.gov.hmcts.reform.fpl.enums.CMOStatus.RETURNED;
+import static uk.gov.hmcts.reform.fpl.enums.CMOStatus.SEND_TO_JUDGE;
+import static uk.gov.hmcts.reform.fpl.enums.HearingType.CASE_MANAGEMENT;
+import static uk.gov.hmcts.reform.fpl.model.order.CaseManagementOrder.sealFrom;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.fpl.utils.TestDataHelper.testDocumentReference;
 
 @ActiveProfiles("integration-test")
 @WebMvcTest(ReviewCMOController.class)
 @OverrideAutoConfiguration(enabled = true)
 public class ReviewCMOControllerAboutToSubmitTest extends AbstractControllerTest {
+
+    @MockBean
+    private DocumentSealingService documentSealingService;
 
     protected ReviewCMOControllerAboutToSubmitTest() {
         super("review-cmo");
@@ -17,16 +48,68 @@ public class ReviewCMOControllerAboutToSubmitTest extends AbstractControllerTest
 
     @Test
     void shouldSetReturnStatusAndRequestedChangesWhenJudgeRejectsOrder() {
+        CaseManagementOrder cmo = buildCMO();
+
+        CaseData caseData = CaseData.builder()
+            .draftUploadedCMOs(List.of(element(cmo)))
+            .reviewCMODecision(ReviewDecision.builder()
+                .changesRequestedByJudge("Please change XYZ")
+                .decision(JUDGE_REQUESTED_CHANGES).build())
+            .build();
+
+        CallbackRequest callbackRequest = CallbackRequest.builder().caseDetails(asCaseDetails(caseData)).build();
+
+        AboutToStartOrSubmitCallbackResponse response = postAboutToSubmitEvent(callbackRequest);
+
+        CaseData responseData = mapper.convertValue(response.getData(), CaseData.class);
+
+        CaseManagementOrder returnedCMO = responseData.getDraftUploadedCMOs().get(0).getValue();
+        assertThat(returnedCMO.getStatus()).isEqualTo(RETURNED);
+        assertThat(returnedCMO.getRequestedChanges()).isEqualTo("Please change XYZ");
 
     }
 
     @Test
-    void shouldSealPDFAndAddCMOToListWhenJudgeApprovesOrder() {
+    void shouldSealPDFAndAddToSealedCMOsListWhenJudgeApprovesOrder() throws Exception {
+        DocumentReference sealedCMODocument = testDocumentReference();
+        given(documentSealingService.sealDocument(any(DocumentReference.class))).willReturn(sealedCMODocument);
 
+        UUID cmoId = UUID.fromString("51d02c7f-2a51-424b-b299-a90b98bb1774");
+        CaseManagementOrder cmo = buildCMO();
+
+        CaseData caseData = CaseData.builder()
+            .draftUploadedCMOs(List.of(element(cmoId, cmo)))
+            .hearingDetails(List.of(element(hearing(cmoId))))
+            .reviewCMODecision(ReviewDecision.builder().decision(SEND_TO_ALL_PARTIES).build()).build();
+
+        CallbackRequest callbackRequest = CallbackRequest.builder().caseDetails(asCaseDetails(caseData)).build();
+
+        AboutToStartOrSubmitCallbackResponse response = postAboutToSubmitEvent(callbackRequest);
+
+        CaseData responseData = mapper.convertValue(response.getData(), CaseData.class);
+
+        assertThat(responseData.getDraftUploadedCMOs()).isEmpty();
+        assertThat(responseData.getSealedCMOs()).isNotEmpty();
+        assertThat(responseData.getSealedCMOs().get(0).getValue()).isEqualTo(
+            sealFrom(sealedCMODocument, hearing(cmoId), LocalDate.now()));
     }
 
-    @Test
-    void shouldUpdateDraftCMOListToReflectJudgeDecision() {
+    private CaseManagementOrder buildCMO() {
+        return CaseManagementOrder.builder()
+            .hearing("Test hearing 25th December 2020")
+            .order(testDocumentReference())
+            .status(SEND_TO_JUDGE).build();
+    }
 
+    private HearingBooking hearing(UUID cmoId) {
+        return HearingBooking.builder()
+            .type(CASE_MANAGEMENT)
+            .startDate(LocalDateTime.now())
+            .judgeAndLegalAdvisor(JudgeAndLegalAdvisor.builder()
+                .judgeTitle(JudgeOrMagistrateTitle.HER_HONOUR_JUDGE)
+                .judgeLastName("Judy")
+                .build())
+            .caseManagementOrderId(cmoId)
+            .build();
     }
 }
