1c9fb23cc800d3a987116534b9a55c410a5a0693
James Nelson <Jamesnelson117@gmail.com>
2020-07-30 12:58:29 +0100

    FPLA-1129: Revert test name change

--- e2e/tests/localAuthoritySubmitsApplication_test.js
+++ e2e/tests/localAuthoritySubmitsApplication_test.js
@@ -10,7 +10,7 @@ const uploadDocumentsHelper = require('../helpers/upload_case_documents_helper.j
 
 let caseId;
 
-Feature('Application draft');
+Feature('Application draft (populated draft)');
 
 BeforeSuite(async I => caseId = await I.logInAndCreateCase(config.swanseaLocalAuthorityUserOne));
 
