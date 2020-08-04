ab6c2f04594d57a4e9601f8159b6eb951f9622d7
Kiran Varma <varma.ap@gmail.com>
2020-08-04 14:49:53 +0100

    add unit test to helper

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/ConfidentialDetailsHelperTest.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.fpl.utils;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.fpl.model.Address;
+import uk.gov.hmcts.reform.fpl.model.Other;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.fpl.utils.ConfidentialDetailsHelper.addConfidentialDetailsForItems;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+
+class ConfidentialDetailsHelperTest {
+
+    private static final String CONFIDENTIAL = "Yes";
+    private static final UUID ID = randomUUID();
+    private static final String TELEPHONE_NUMBER = "01227 831393";
+
+    @Test
+    void shouldAddConfidentialDetailsForOther() {
+        List<Element<Other>> others = List.of(otherWithConfidentialFields(ID, CONFIDENTIAL));
+        Element<Other> othersNotConfidential = otherWithRemovedConfidentialFields(ID);
+
+        Other confidentialOthers = addConfidentialDetailsForItems(others, othersNotConfidential);
+
+        assertThat(confidentialOthers.getTelephone()).isEqualTo(TELEPHONE_NUMBER);
+    }
+
+    @Test
+    void shouldNotAddConfidentialDetailsForNonMatchingItem() {
+        List<Element<Other>> others = List.of(otherWithConfidentialFields(ID, CONFIDENTIAL));
+        Element<Other> othersNotConfidential = otherWithRemovedConfidentialFields(randomUUID());
+
+        Other confidentialOthers = addConfidentialDetailsForItems(others, othersNotConfidential);
+
+        assertThat(confidentialOthers).isEqualTo(otherWithRemovedConfidentialFields(ID).getValue());
+    }
+
+    private Other.OtherBuilder baseOtherBuilder(String detailsHidden) {
+        return Other.builder()
+            .name("John Smith")
+            .detailsHidden(detailsHidden);
+    }
+
+    private Element<Other> otherWithRemovedConfidentialFields(UUID id) {
+        return element(id, baseOtherBuilder(CONFIDENTIAL).build());
+    }
+
+    private Element<Other> otherWithConfidentialFields(UUID id, String detailsHidden) {
+        return element(id, baseOtherBuilder(detailsHidden)
+            .address(Address.builder().addressLine1("Address Line 1").build())
+            .telephone(TELEPHONE_NUMBER)
+            .build());
+    }
+}
