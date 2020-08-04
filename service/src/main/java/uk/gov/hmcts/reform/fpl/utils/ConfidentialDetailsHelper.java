16395427b53e7849b2f71aa3129072f1a070da4d
Kiran Varma <varma.ap@gmail.com>
2020-08-04 14:16:43 +0100

    fix style issues

--- service/src/main/java/uk/gov/hmcts/reform/fpl/utils/ConfidentialDetailsHelper.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/ConfidentialDetailsHelper.java
@@ -5,6 +5,11 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 import java.util.List;
 
 public class ConfidentialDetailsHelper {
+
+    private ConfidentialDetailsHelper() {
+        // NO OP
+    }
+
     public static <T> T addConfidentialDetailsForItems(List<Element<T>> confidential, Element<T> element) {
         return confidential.stream()
             .filter(item -> item.getId().equals(element.getId()))
