2bdb1235c0acf905457155b526394167cb532484
Lewis Birks <lewisb@kainos.com>
2020-08-06 15:25:41 +0100

    update e2e tests

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CourtAdminDocument.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CourtAdminDocument.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.reform.fpl.model;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 
 @Data
-@RequiredArgsConstructor
 public class CourtAdminDocument {
     private final String documentTitle;
     private final DocumentReference document;
