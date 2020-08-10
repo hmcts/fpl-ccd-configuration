3cf1b38e688cf498b0f0b7e44a1fa53009cda8bc
Tomasz Powroznik <tomas.powroznik@gmail.com>
2020-08-10 13:39:11 +0100

    fix: Do not show emails in logs

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/MaskHelper.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.fpl.utils;
+
+import java.util.Optional;
+
+public class MaskHelper {
+
+    private MaskHelper() {
+    }
+
+    public static String maskEmail(String text, String email) {
+        return Optional.ofNullable(text)
+                .map(t -> t.replaceAll(email, maskEmail(email)))
+                .orElse(null);
+    }
+
+    public static String maskEmail(String email) {
+        return Optional.ofNullable(email)
+                .map(e -> e.replaceAll("[^@]", "*"))
+                .orElse("");
+    }
+
+}
