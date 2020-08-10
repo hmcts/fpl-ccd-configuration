225aaa335b5fb3f1a1ab2132b42a9ea0e0d0464d
Adam Komisarek <adam.komisarek@hmcts.net>
2020-08-10 12:24:12 +0100

    fix: pass c2 document instead of c110a in c2 notification

--- service/src/main/java/uk/gov/hmcts/reform/fpl/events/C2UploadedEvent.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/events/C2UploadedEvent.java
@@ -1,10 +1,18 @@
 package uk.gov.hmcts.reform.fpl.events;
 
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+
 
 public class C2UploadedEvent extends CallbackEvent {
+    private final C2DocumentBundle uploadedBundle;
 
-    public C2UploadedEvent(CallbackRequest callbackRequest) {
+    public C2UploadedEvent(CallbackRequest callbackRequest, C2DocumentBundle c2DocumentBundle) {
         super(callbackRequest);
+        uploadedBundle = c2DocumentBundle;
+    }
+
+    public C2DocumentBundle getUploadedBundle() {
+        return uploadedBundle;
     }
 }
