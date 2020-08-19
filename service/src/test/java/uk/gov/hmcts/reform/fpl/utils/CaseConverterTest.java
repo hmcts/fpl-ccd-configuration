b3f462358cbac63d9a4422cfd0b846d3d33d5d39
Kiran Varma <varma.ap@gmail.com>
2020-08-19 22:01:21 +0100

    add unit test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CaseConverterTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CaseConverterTest.java
@@ -1,19 +1,20 @@
 package uk.gov.hmcts.reform.fpl.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 
-import java.time.LocalDate;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.callbackRequest;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {JacksonAutoConfiguration.class, CaseConverter.class})
@@ -22,26 +23,28 @@ class CaseConverterTest {
     @Autowired
     private CaseConverter caseConverter;
 
+    @MockBean
+    private ObjectMapper mapper;
+
     @Test
     void shouldConvertCaseDataToMap() {
-        assertThat(caseConverter.convertToMap(getCaseData())).containsKey("caseLocalAuthority");
-        assertThat(caseConverter.convertToMap(getCaseData())).containsKey("hearingDetails");
+        caseConverter.convertToMap(getCaseData());
+        verify(mapper).convertValue(eq(getCaseData()), eq(Map.class));
     }
 
     @Test
     void shouldConvertCaseDetailsToCaseData() {
-        String localAuthority = "LA1";
-        CallbackRequest request = callbackRequest(Map.of("localAuthority", localAuthority));
+        caseConverter.convertToCaseData(getCaseDetails());
+        verify(mapper).convertValue(eq(getCaseDetails().getData()), eq(CaseData.class));
+    }
 
-        assertThat(caseConverter.convertToCaseData(request.getCaseDetails()).getCaseLocalAuthority()).isEqualTo(localAuthority);
+    @SuppressWarnings("unchecked")
+    private CaseDetails getCaseDetails() {
+        Map<String, Object> data = mapper.convertValue(getCaseData(), Map.class);
+        return CaseDetails.builder().data(data).build();
     }
 
     private CaseData getCaseData() {
-        return CaseData.builder()
-            .caseLocalAuthority("example")
-            .familyManCaseNumber("123")
-            .dateSubmitted(LocalDate.now())
-            .dateOfIssue(LocalDate.now())
-            .build();
+        return CaseData.builder().build();
     }
 }
