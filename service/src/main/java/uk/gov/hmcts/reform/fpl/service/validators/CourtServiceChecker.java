0141990611ea9a80892c65957d4eddacb8eec048
tomaszpowroznik <tomas.powroznik@gmail.com>
2020-08-19 12:58:15 +0100

    chore:update logging framework (#1519)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/validators/CourtServiceChecker.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/validators/CourtServiceChecker.java
@@ -7,7 +7,7 @@ import uk.gov.hmcts.reform.fpl.model.HearingPreferences;
 import java.util.List;
 
 import static java.util.Collections.emptyList;
-import static net.logstash.logback.encoder.org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 import static uk.gov.hmcts.reform.fpl.service.validators.EventCheckerHelper.anyNonEmpty;
 
 @Component
