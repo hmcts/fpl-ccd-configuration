dc87bf2e26a6a7d2f5da163e99c9d31064024a26
Kiran Varma <varma.ap@gmail.com>
2020-08-20 18:26:05 +0100

    refactor DocmosisGeneratedOrder

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/docmosis/DischargeCareOrderGenerationService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/docmosis/DischargeCareOrderGenerationService.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.reform.fpl.model.Child;
 import uk.gov.hmcts.reform.fpl.model.OrderTypeAndDocument;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisDischargeOfCareOrder;
-import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisGeneratedOrder.DocmosisGeneratedOrderBuilder;
+import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisGeneratedOrder;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisOrder;
 import uk.gov.hmcts.reform.fpl.model.order.generated.GeneratedOrder;
 import uk.gov.hmcts.reform.fpl.service.DischargeCareOrderService;
@@ -28,15 +28,15 @@ public class DischargeCareOrderGenerationService extends GeneratedOrderTemplateD
     private final HmctsCourtLookupConfiguration hmctsCourtLookupConfiguration;
     private final DischargeCareOrderService dischargeCareOrder;
 
-    @SuppressWarnings("rawtypes")
     @Override
-    DocmosisGeneratedOrderBuilder populateCustomOrderFields(CaseData caseData) {
+    DocmosisGeneratedOrder populateCustomOrderFields(CaseData caseData) {
         OrderTypeAndDocument orderTypeAndDocument = caseData.getOrderTypeAndDocument();
 
         return DocmosisDischargeOfCareOrder.builder()
             .orderTitle(getFullOrderType(orderTypeAndDocument))
             .childrenAct("Section 39(1) Children Act 1989")
-            .careOrders(extractCareOrders(caseData));
+            .careOrders(extractCareOrders(caseData))
+            .build();
     }
 
     @Override
