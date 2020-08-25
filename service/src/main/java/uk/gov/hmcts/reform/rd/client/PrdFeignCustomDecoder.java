8170f43626221b0514d615f8b0f4b7c262141d2e
Tomasz Powroznik <tomas.powroznik@gmail.com>
2020-08-25 15:27:51 +0100

    Add feign retries for PRD client

---
+++ service/src/main/java/uk/gov/hmcts/reform/rd/client/PrdFeignCustomDecoder.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.rd.client;
+
+
+import feign.Response;
+import feign.RetryableException;
+import feign.codec.ErrorDecoder;
+import org.bouncycastle.util.Arrays;
+
+import java.util.Optional;
+
+public class PrdFeignCustomDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultErrorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        final Exception exception = defaultErrorDecoder.decode(methodKey, response);
+
+        final Optional<Retry> retry = Optional.ofNullable(response
+            .request()
+            .requestTemplate()
+            .methodMetadata()
+            .method()
+            .getAnnotation(Retry.class));
+
+        if (retry.isPresent()) {
+            int[] statuses = retry.map(Retry::exclude).orElse(new int[]{});
+            if (!Arrays.contains(statuses, response.status())) {
+                return new RetryableException(response.status(), response.reason(), response.request().httpMethod(), exception, null, response.request());
+            }
+        }
+
+        return exception;
+    }
+
+
+}
