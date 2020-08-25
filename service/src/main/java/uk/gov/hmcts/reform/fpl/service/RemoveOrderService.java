4a7e6851d9ca3ab0669aa4e55e6232782d54f548
Lewis Birks <lewisb@kainos.com>
2020-08-25 17:36:02 +0100

    filter to blank orders

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/RemoveOrderService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/RemoveOrderService.java
@@ -6,13 +6,19 @@ import uk.gov.hmcts.reform.fpl.model.common.dynamic.DynamicList;
 import uk.gov.hmcts.reform.fpl.model.order.generated.GeneratedOrder;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static uk.gov.hmcts.reform.fpl.enums.GeneratedOrderType.BLANK_ORDER;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.asDynamicList;
 
 @Service
 public class RemoveOrderService {
     public DynamicList getDropDownListOfExistingOrders(List<Element<GeneratedOrder>> orders) {
-        return asDynamicList(orders, GeneratedOrder::asLabel);
+        List<Element<GeneratedOrder>> blankOrders = orders.stream()
+            .filter(order -> BLANK_ORDER.getLabel().equals(order.getValue().getType()))
+            .collect(Collectors.toList());
+
+        return asDynamicList(blankOrders, GeneratedOrder::asLabel);
     }
 
     public void hideOrder(List<Element<GeneratedOrder>> orders, List<Element<GeneratedOrder>> hiddenOrders,
