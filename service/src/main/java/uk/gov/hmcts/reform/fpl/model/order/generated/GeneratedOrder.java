de2748fcbbae022bba4d1801c1378eeff7bedb90
Lewis Birks <lewisb@kainos.com>
2020-08-26 10:08:47 +0100

    remove abstract class as we are now only removing blank orders

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/order/generated/GeneratedOrder.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/order/generated/GeneratedOrder.java
@@ -3,22 +3,19 @@ package uk.gov.hmcts.reform.fpl.model.order.generated;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import uk.gov.hmcts.reform.fpl.json.converter.BasicChildConverter;
 import uk.gov.hmcts.reform.fpl.model.Child;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
-import uk.gov.hmcts.reform.fpl.model.order.RemovableOrder;
 
 import java.util.List;
 
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Builder(toBuilder = true)
-public class GeneratedOrder extends RemovableOrder {
+public class GeneratedOrder {
     private final String type;
     private final String title;
     private final String details;
@@ -31,8 +28,8 @@ public class GeneratedOrder extends RemovableOrder {
     private final String courtName;
     @JsonSerialize(contentConverter = BasicChildConverter.class)
     private final List<Element<Child>> children;
+    private String removalReason;
 
-    @Override
     public String asLabel() {
         return defaultIfNull(title, type) + " - " + dateOfIssue;
     }
