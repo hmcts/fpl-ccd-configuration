1265c03096a65468841242a073ef3b90be45484e
James Nelson <Jamesnelson117@gmail.com>
2020-08-28 16:15:38 +0100

    feat: new final hearing and issue resolution state (FPLA-2043 & FPLA-2044) (#1534)

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleServiceTest.java
@@ -142,6 +142,15 @@ class FeatureToggleServiceTest {
     }
 
     @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldMakeCorrectCallForNewCaseStateModel(Boolean toggleState) {
+        givenToggle(toggleState);
+
+        assertThat(service.isNewCaseStateModelEnabled()).isEqualTo(toggleState);
+        verify(ldClient).boolVariation(eq("new-case-state-model"), any(LDUser.class), eq(false));
+    }
+
+    @ParameterizedTest
     @MethodSource("userAttributesTestSource")
     void shouldNotAccumulateAttributesBetweenRequests(Runnable functionToTest, Runnable accumulateFunction,
                                                       List<UserAttribute> attributes) {
