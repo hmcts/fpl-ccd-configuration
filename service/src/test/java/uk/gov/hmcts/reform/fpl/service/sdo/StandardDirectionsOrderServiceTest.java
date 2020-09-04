00bac20dfe561e430adbad2249cd19eb49a3e311
Lewis Birks <lewisb@kainos.com>
2020-09-04 12:18:43 +0100

    service test

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/sdo/StandardDirectionsOrderServiceTest.java
@@ -0,0 +1,118 @@
+package uk.gov.hmcts.reform.fpl.service.sdo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.fpl.enums.OrderStatus;
+import uk.gov.hmcts.reform.fpl.model.StandardDirectionOrder;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
+import uk.gov.hmcts.reform.fpl.request.RequestData;
+import uk.gov.hmcts.reform.fpl.service.DocumentSealingService;
+import uk.gov.hmcts.reform.fpl.service.docmosis.DocumentConversionService;
+import uk.gov.hmcts.reform.fpl.service.time.Time;
+import uk.gov.hmcts.reform.fpl.utils.FixedTimeConfiguration;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.fpl.Constants.USER_AUTH_TOKEN;
+import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.DRAFT;
+import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.SEALED;
+
+@ExtendWith(MockitoExtension.class)
+class StandardDirectionsOrderServiceTest {
+    private static final String USER_NAME = "Adam";
+    private static final DocumentReference SEALED_DOC = DocumentReference.builder().filename("sealed.pdf").build();
+    private static final DocumentReference WORD_DOC = DocumentReference.builder().filename("word.docx").build();
+    private static final DocumentReference PDF_DOC = DocumentReference.builder().filename("doc.pdf").build();
+    private static final Time TIME = new FixedTimeConfiguration().stoppedTime();
+
+    @Mock
+    private DocumentConversionService conversionService;
+    @Mock
+    private DocumentSealingService sealingService;
+    @Mock
+    private IdamClient idamClient;
+    @Mock
+    private RequestData requestData;
+    private StandardDirectionsOrderService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new StandardDirectionsOrderService(conversionService, sealingService, TIME, idamClient, requestData);
+        given(idamClient.getUserInfo(USER_AUTH_TOKEN)).willReturn(UserInfo.builder().name(USER_NAME).build());
+        given(requestData.authorisation()).willReturn(USER_AUTH_TOKEN);
+    }
+
+    @Test
+    void shouldNotSealDocumentWhenSDOIsDraft() throws Exception {
+        StandardDirectionOrder order = buildStandardDirectionOrder(PDF_DOC, DRAFT);
+
+        StandardDirectionOrder builtOrder = service.buildOrderFromUpload(order);
+
+        StandardDirectionOrder expectedOrder = buildStandardDirectionOrder(
+            PDF_DOC, DRAFT, TIME.now().toLocalDate(), USER_NAME
+        );
+
+        assertThat(builtOrder).isEqualTo(expectedOrder);
+        verify(sealingService, never()).sealDocument(any(DocumentReference.class));
+        verify(conversionService, never()).convertToPdf(any(DocumentReference.class));
+    }
+
+    @Test
+    void shouldSealDocumentWhenSDOIsToBeSealed() throws Exception {
+        given(conversionService.convertToPdf(PDF_DOC)).willCallRealMethod();
+        mockSealingService();
+
+        StandardDirectionOrder order = buildStandardDirectionOrder(PDF_DOC, SEALED);
+
+        StandardDirectionOrder builtOrder = service.buildOrderFromUpload(order);
+
+        StandardDirectionOrder expectedOrder = buildStandardDirectionOrder(
+            SEALED_DOC, SEALED, TIME.now().toLocalDate(), USER_NAME
+        );
+
+        assertThat(builtOrder).isEqualTo(expectedOrder);
+
+        verify(sealingService).sealDocument(PDF_DOC);
+    }
+
+    @Test
+    void shouldConvertWordDocumentAndSealWhenSDOIsSetToSeal() throws Exception {
+        given(conversionService.convertToPdf(WORD_DOC)).willReturn(PDF_DOC);
+        mockSealingService();
+
+        StandardDirectionOrder order = buildStandardDirectionOrder(WORD_DOC, SEALED);
+
+        service.buildOrderFromUpload(order);
+
+        verify(conversionService).convertToPdf(WORD_DOC);
+        verify(sealingService).sealDocument(PDF_DOC);
+    }
+
+    private StandardDirectionOrder buildStandardDirectionOrder(DocumentReference document, OrderStatus status) {
+        return buildStandardDirectionOrder(document, status, null, null);
+    }
+
+    private StandardDirectionOrder buildStandardDirectionOrder(DocumentReference document, OrderStatus status,
+                                                               LocalDate dateOfUpload, String uploader) {
+        return StandardDirectionOrder.builder()
+            .orderDoc(document)
+            .orderStatus(status)
+            .dateOfUpload(dateOfUpload)
+            .uploader(uploader)
+            .build();
+    }
+
+    private void mockSealingService() throws Exception {
+        given(sealingService.sealDocument(PDF_DOC)).willReturn(SEALED_DOC);
+    }
+}
