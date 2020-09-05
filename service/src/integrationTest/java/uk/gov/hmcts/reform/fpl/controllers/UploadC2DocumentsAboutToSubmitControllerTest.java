91a605de802d20997fe57bd05ad855fe636242c9
Kiran Varma <varma.ap@gmail.com>
2020-09-05 14:27:55 +0100

    added unit test

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsAboutToSubmitControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadC2DocumentsAboutToSubmitControllerTest.java
@@ -10,10 +10,14 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.document.domain.Document;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.utils.TestDataHelper;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +26,8 @@ import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.callbackRequ
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.DATE_TIME;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateTimeBaseUsingFormat;
 import static uk.gov.hmcts.reform.fpl.utils.DocumentManagementStoreLoader.document;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @ActiveProfiles("integration-test")
 @WebMvcTest(UploadC2DocumentsController.class)
@@ -87,6 +93,13 @@ class UploadC2DocumentsAboutToSubmitControllerTest extends AbstractControllerTes
         assertThat(documentBundle.getDocument().getFilename()).isEqualTo(document.originalDocumentName);
         assertThat(documentBundle.getDocument().getBinaryUrl()).isEqualTo(document.links.binary.href);
         assertThat(documentBundle.getDescription()).isEqualTo(description);
+
+        List<SupportingEvidenceBundle> supportingBundle = unwrapElements(documentBundle.getSupportingEvidenceBundle());
+        if (!supportingBundle.isEmpty()) {
+            SupportingEvidenceBundle supportingEvidenceBundle = supportingBundle.get(0);
+            assertThat(supportingEvidenceBundle.getName()).isEqualTo("Supporting document");
+            assertThat(supportingEvidenceBundle.getNotes()).isEqualTo("Document notes");
+        }
     }
 
     private Map<String, Object> createTemporaryC2Document() {
@@ -99,13 +112,26 @@ class UploadC2DocumentsAboutToSubmitControllerTest extends AbstractControllerTes
                     "document_binary_url",
                     "http://localhost/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4/binary",
                     "document_filename", "file.pdf"),
-                "description", "Test description"));
+                "description", "Test description",
+                "supportingEvidenceBundle", wrapElements(createSupportingEvidenceBundle())
+            )
+        );
     }
 
     private CaseDetails createCase(Map<String, Object> data) {
         return CaseDetails.builder()
             .data(data)
             .id(CASE_ID)
+            .build();
+    }
+
+    private SupportingEvidenceBundle createSupportingEvidenceBundle() {
+        return SupportingEvidenceBundle.builder()
+            .name("Supporting document")
+            .notes("Document notes")
+            .dateTimeReceived(LocalDateTime.now().minusDays(1))
+            .dateTimeUploaded(LocalDateTime.now())
+            .document(TestDataHelper.testDocumentReference())
             .build();
     }
 }
