24965dad5d97eb08430026599bdaa893c58e00b9
Kiran Varma <varma.ap@gmail.com>
2020-09-07 15:21:01 +0100

    made validation generic

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ValidateDocumentBundle.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.fpl.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.fpl.validation.groups.DateOfIssueGroup;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ValidateDocumentBundle {
+    private final ValidateGroupService validatorService;
+
+    public <T> List<String> validateBundle(List<T> documentBundle) {
+        return documentBundle.stream()
+            .map(e -> validatorService.validateGroup(e, DateOfIssueGroup.class))
+            .flatMap(List::stream)
+            .collect(Collectors.toList());
+    }
+}
