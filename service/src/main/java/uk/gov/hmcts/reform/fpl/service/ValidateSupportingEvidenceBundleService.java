f26e7755b2f30c5a8edc8c6b7c5a7eb158f1e9e3
Kiran Varma <varma.ap@gmail.com>
2020-09-08 13:10:15 +0100

    not need for generics

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/ValidateSupportingEvidenceBundleService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ValidateSupportingEvidenceBundleService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.fpl.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,7 +13,7 @@ import java.util.stream.Collectors;
 public class ValidateSupportingEvidenceBundleService {
     private final ValidateGroupService validatorService;
 
-    public <T> List<String> validateBundle(List<T> documentBundle) {
+    public List<String> validateBundle(List<SupportingEvidenceBundle> documentBundle) {
         return documentBundle.stream()
             .map(validatorService::validateGroup)
             .flatMap(List::stream)
