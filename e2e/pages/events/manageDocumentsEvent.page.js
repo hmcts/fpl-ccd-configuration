c5f46fc817205bba0384b9ac740e4937b9120979
Toireasa Moley <toireasa.moley@hmcts.net>
2020-09-08 14:39:07 +0200

    Deprecating uploadStandardDirections event

--- e2e/pages/events/manageDocumentsEvent.page.js
+++ e2e/pages/events/manageDocumentsEvent.page.js
@@ -1,52 +1,51 @@
-const { I } = inject();
-const config = require('../../config');
-
-module.exports = {
-  fields: function(index, supportingDocumentType) {
-    return {
-      documentType: {
-        furtherEvidence: '#manageDocument_type-FURTHER_EVIDENCE_DOCUMENTS',
-        correspondence: '#manageDocument_type-CORRESPONDENCE',
-        c2: '#manageDocument_type-C2'
-      },
-
-      supportingEvidenceDocuments: {
-        name: `#${supportingDocumentType}_${index}_name`,
-        notes: `#${supportingDocumentType}_${index}_notes`,
-        dateAndTime: {
-          day: `${supportingDocumentType}_${index}_dateTimeReceived-day`,
-          month: `${supportingDocumentType}_${index}_dateTimeReceived-month`,
-          year: `${supportingDocumentType}_${index}_dateTimeReceived-year`,
-          hour: `${supportingDocumentType}_${index}_dateTimeReceived-hour`,
-          minute: `${supportingDocumentType}_${index}_dateTimeReceived-minute`,
-          second: `${supportingDocumentType}_${index}_dateTimeReceived-second`,
-        },
-        document: `${supportingDocumentType}_${index}_document`,
-      },
-    };
-  },
-
-  selectFurtherEvidence() {
-    I.click(this.fields(elementIndex, type).documentType.furtherEvidence);
-  },
-
-  selectCorrespondence() {
-    I.click(this.fields(elementIndex, type).documentType.correspondence);
-  },
-
-  selectC2SupportingDocuments() {
-    I.click(this.fields(elementIndex, type).documentType.c2);
-  },
-
-  enterDocumentName(documentName) {
-    I.fillField(this.fields(elementIndex, type).name, documentName);
-  },
-
-  enterDocumentName(notes) {
-    I.fillField(this.fields(elementIndex, type).notes, notes);
-  },
-
-  uploadDocument() {
-    I.fillField(this.fields(elementIndex, type).document, 'Document');
-  },
-};
+// const { I } = inject();
+//
+// module.exports = {
+//   fields: function(index, supportingDocumentType) {
+//     return {
+//       documentType: {
+//         furtherEvidence: '#manageDocument_type-FURTHER_EVIDENCE_DOCUMENTS',
+//         correspondence: '#manageDocument_type-CORRESPONDENCE',
+//         c2: '#manageDocument_type-C2'
+//       },
+//
+//       supportingEvidenceDocuments: {
+//         name: `#${supportingDocumentType}_${index}_name`,
+//         notes: `#${supportingDocumentType}_${index}_notes`,
+//         dateAndTime: {
+//           day: `${supportingDocumentType}_${index}_dateTimeReceived-day`,
+//           month: `${supportingDocumentType}_${index}_dateTimeReceived-month`,
+//           year: `${supportingDocumentType}_${index}_dateTimeReceived-year`,
+//           hour: `${supportingDocumentType}_${index}_dateTimeReceived-hour`,
+//           minute: `${supportingDocumentType}_${index}_dateTimeReceived-minute`,
+//           second: `${supportingDocumentType}_${index}_dateTimeReceived-second`,
+//         },
+//         document: `${supportingDocumentType}_${index}_document`,
+//       },
+//     };
+//   },
+//
+//   selectFurtherEvidence() {
+//     I.click(this.fields(elementIndex, type).documentType.furtherEvidence);
+//   },
+//
+//   selectCorrespondence() {
+//     I.click(this.fields(elementIndex, type).documentType.correspondence);
+//   },
+//
+//   selectC2SupportingDocuments() {
+//     I.click(this.fields(elementIndex, type).documentType.c2);
+//   },
+//
+//   enterDocumentName(documentName) {
+//     I.fillField(this.fields(elementIndex, type).name, documentName);
+//   },
+//
+//   enterDocumentName(notes) {
+//     I.fillField(this.fields(elementIndex, type).notes, notes);
+//   },
+//
+//   uploadDocument() {
+//     I.fillField(this.fields(elementIndex, type).document, 'Document');
+//   },
+// };
