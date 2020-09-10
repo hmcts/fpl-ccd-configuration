2d6b8084ad4bd2d239e5b9b1bbd43190bce31739
Kiran Varma <varma.ap@gmail.com>
2020-09-10 21:36:42 +0100

    first commit

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/UploadC2DocumentsServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/UploadC2DocumentsServiceTest.java
@@ -3,15 +3,28 @@ package uk.gov.hmcts.reform.fpl.service;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import uk.gov.hmcts.reform.fpl.enums.C2ApplicationType;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.request.RequestData;
+import uk.gov.hmcts.reform.fpl.utils.FixedTimeConfiguration;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static uk.gov.hmcts.reform.fpl.Constants.USER_AUTH_TOKEN;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @ExtendWith(SpringExtension.class)
@@ -19,13 +32,33 @@ import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
     UploadC2DocumentsService.class,
     ValidateSupportingEvidenceBundleService.class,
     ValidateGroupService.class,
-    LocalValidatorFactoryBean.class
+    LocalValidatorFactoryBean.class,
+    FixedTimeConfiguration.class
 })
 class UploadC2DocumentsServiceTest {
     private static final String ERROR_MESSAGE = "Date of time received cannot be in the future";
 
     @Autowired
     private UploadC2DocumentsService service;
+
+    @MockBean
+    private IdamClient idamClient;
+
+    @MockBean
+    private RequestData requestData;
+
+    @Test
+    void shouldBuildExpectedC2DocumentBundle() {
+        given(idamClient.getUserInfo(USER_AUTH_TOKEN)).willReturn(UserInfo.builder().name("Emma Taylor").build());
+        given(requestData.authorisation()).willReturn(USER_AUTH_TOKEN);
+
+        List<Element<C2DocumentBundle>> list = service.buildC2DocumentBundle(createCaseDataWithC2DocumentBundle());
+
+        List<C2DocumentBundle> listOfC2Bundle = unwrapElements(list);
+
+        assertThat(listOfC2Bundle).first()
+            .isEqualToComparingFieldByField(createC2DocumentBundle());
+    }
 
     @Test
     void shouldReturnErrorsWhenTheDateOfIssueIsInFuture() {
@@ -39,6 +72,8 @@ class UploadC2DocumentsServiceTest {
 
     private C2DocumentBundle createC2DocumentBundle() {
         return C2DocumentBundle.builder()
+            .author("Elon Musk")
+            .type(C2ApplicationType.WITH_NOTICE)
             .supportingEvidenceBundle(wrapElements(createSupportingEvidenceBundleWithInvalidDateReceived()))
             .build();
     }
@@ -54,6 +89,14 @@ class UploadC2DocumentsServiceTest {
             .name("Supporting document")
             .notes("Document notes")
             .dateTimeReceived(LocalDateTime.now().plusDays(1))
+            .build();
+    }
+
+    private CaseData createCaseDataWithC2DocumentBundle() {
+        return CaseData.builder()
+            .c2DocumentBundle(wrapElements(createC2DocumentBundle()))
+            .temporaryC2Document(createC2DocumentBundle())
+            .c2ApplicationType(Map.of("type",C2ApplicationType.WITH_NOTICE))
             .build();
     }
 }
