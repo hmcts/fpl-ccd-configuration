53ee2073f3c1d64e7f13230fd77914a61d4011ec
Joe Zazzaro-Francis <joez@kainos.com>
2020-09-15 16:44:14 +0100

    Remove suppressions

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -80,7 +80,6 @@ import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
 @Builder(toBuilder = true)
 @AllArgsConstructor
 @HasDocumentsIncludedInSwet(groups = UploadDocumentsGroup.class)
-@SuppressWarnings({"java:S1874", "java:S1133"}) // Remove once deprecations dealt with
 public class CaseData {
     private final Long id;
     private final State state;
