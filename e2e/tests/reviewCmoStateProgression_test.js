9c6c7ac321185322ee477d08559c711fe4662bf7
Lewis Birks <lewisb@kainos.com>
2020-09-21 11:00:57 +0100

    use new method to reduce number of actions for cmo state progression

--- e2e/tests/reviewCmoStateProgression_test.js
+++ e2e/tests/reviewCmoStateProgression_test.js
@@ -1,3 +1,4 @@
+const config = require('../config');
 const issueResolution = require('../fixtures/caseData/issueResolution.json');
 const finalHearing = require('../fixtures/caseData/finalHearing.json');
 const cmoHelper = require('../helpers/cmo_helper');
@@ -9,15 +10,13 @@ Feature('Review CMO state progression');
 Scenario('Judge transitions CMO to issue resolution case state', async (I, caseViewPage, caseListPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage) => {
   caseId = await I.submitNewCaseWithData(issueResolution);
   await cmoHelper.judgeSendsReviewedCmoToAllParties(I, caseId, caseViewPage, caseListPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage);
-  caseListPage.navigate();
-  caseListPage.changeStateFilter('Issue resolution');
-  I.click(caseListPage.locateCase(caseId));
+  caseViewPage.selectTab(caseViewPage.tabs.history);
+  await I.seeEndStateForEvent(config.applicationActions.reviewAgreedCmo, 'Issue resolution');
 });
 
 Scenario('Judge transitions CMO to final hearing case state', async (I, caseViewPage, caseListPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage) => {
   caseId = await I.submitNewCaseWithData(finalHearing);
   await cmoHelper.judgeSendsReviewedCmoToAllParties(I, caseId, caseViewPage, caseListPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage);
-  caseListPage.navigate();
-  caseListPage.changeStateFilter('Final hearing');
-  I.click(caseListPage.locateCase(caseId));
+  caseViewPage.selectTab(caseViewPage.tabs.history);
+  await I.seeEndStateForEvent(config.applicationActions.reviewAgreedCmo, 'Final hearing');
 });
