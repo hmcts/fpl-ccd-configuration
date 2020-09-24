872e16aa8771abb0bb413f12ef82c106b382a9c8
James Nelson <Jamesnelson117@gmail.com>
2020-09-24 13:53:21 +0100

    FPLA-2228: Test name update

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/cmo/ReviewCMOServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/cmo/ReviewCMOServiceTest.java
@@ -304,7 +304,7 @@ class ReviewCMOServiceTest {
     }
 
     @Test
-    void shouldReturnCaseManagementStateWhenNextHearingIsNotOfTypeIssueResolutionOrFinal() {
+    void shouldReturnCaseManagementStateWhenNextHearingIsNotOfIssueResolutionOrFinalType() {
         given(featureToggleService.isNewCaseStateModelEnabled()).willReturn(true);
 
         List<Element<HearingBooking>> hearingBookings = List.of(
