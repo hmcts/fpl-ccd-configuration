f3612f80b58c6ba864166a17c49dd05ab52c1cce
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2020-09-30 09:26:56 +0100

    fix: sdo regression (#1691)

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/StandardDirectionsOrderControllerUploadRouteMidEventTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/StandardDirectionsOrderControllerUploadRouteMidEventTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.StandardDirectionOrder;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
@@ -28,30 +29,29 @@ public class StandardDirectionsOrderControllerUploadRouteMidEventTest extends Ab
             .preparedSDO(DOCUMENT)
             .build();
 
-        AboutToStartOrSubmitCallbackResponse response = postMidEvent(asCaseDetails(caseData), "upload-route");
+        CallbackRequest request = toCallBackRequest(asCaseDetails(caseData), asCaseDetails(CaseData.builder().build()));
+        AboutToStartOrSubmitCallbackResponse response = postMidEvent(request, "upload-route");
 
         StandardDirectionOrder expectedOrder = StandardDirectionOrder.builder().orderDoc(DOCUMENT).build();
-        StandardDirectionOrder builtOrder = mapper.convertValue(
-            response.getData().get("standardDirectionOrder"),
-            StandardDirectionOrder.class
-        );
+        StandardDirectionOrder builtOrder = extractCaseData(response).getStandardDirectionOrder();
 
         assertThat(builtOrder).isEqualTo(expectedOrder);
     }
 
     @Test
     void shouldAddAlreadyUploadedDocumentToConstructedStandardDirectionOrder() {
-        CaseData caseData = CaseData.builder()
+        CaseData caseDataBefore = CaseData.builder()
             .standardDirectionOrder(StandardDirectionOrder.builder().orderDoc(DOCUMENT).build())
             .build();
 
-        AboutToStartOrSubmitCallbackResponse response = postMidEvent(asCaseDetails(caseData), "upload-route");
+        CallbackRequest request = toCallBackRequest(
+            asCaseDetails(CaseData.builder().build()),
+            asCaseDetails(caseDataBefore)
+        );
+        AboutToStartOrSubmitCallbackResponse response = postMidEvent(request, "upload-route");
 
         StandardDirectionOrder expectedOrder = StandardDirectionOrder.builder().orderDoc(DOCUMENT).build();
-        StandardDirectionOrder builtOrder = mapper.convertValue(
-            response.getData().get("standardDirectionOrder"),
-            StandardDirectionOrder.class
-        );
+        StandardDirectionOrder builtOrder = extractCaseData(response).getStandardDirectionOrder();
 
         assertThat(builtOrder).isEqualTo(expectedOrder);
     }
