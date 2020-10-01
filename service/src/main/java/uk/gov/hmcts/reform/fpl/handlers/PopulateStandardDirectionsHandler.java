2a5a3bfab6e76f1931960082a9013ab5b56273dc
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-10-01 11:48:01 +0100

    refactor: move populateStandardDirections from handler to service (FPLA-1512) (#1682)
    * refactor populateStandardDirections from handler into directions service + add unit test

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateStandardDirectionsHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/PopulateStandardDirectionsHandler.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -9,42 +8,37 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.events.PopulateStandardDirectionsEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.HearingBooking;
-import uk.gov.hmcts.reform.fpl.service.HearingBookingService;
+import uk.gov.hmcts.reform.fpl.model.Direction;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.service.CaseConverter;
 import uk.gov.hmcts.reform.fpl.service.StandardDirectionsService;
 import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
 
+import java.util.List;
 import java.util.Map;
-
-import static uk.gov.hmcts.reform.fpl.model.Directions.getAssigneeToDirectionMapping;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class PopulateStandardDirectionsHandler {
     private final CoreCaseDataService coreCaseDataService;
     private final StandardDirectionsService standardDirectionsService;
-    private final ObjectMapper mapper;
-    private final HearingBookingService hearingService;
+    private final CaseConverter caseConverter;
 
     @Async
     @EventListener
     public void populateStandardDirections(PopulateStandardDirectionsEvent event) {
         CaseDetails caseDetails = event.getCallbackRequest().getCaseDetails();
+        CaseData caseData = caseConverter.convert(caseDetails);
+        Map<String, List<Element<Direction>>> populatedDirections
+            = standardDirectionsService.populateStandardDirections(caseData);
+
+        caseDetails.getData().putAll(populatedDirections);
 
         coreCaseDataService.triggerEvent(caseDetails.getJurisdiction(),
             caseDetails.getCaseTypeId(),
             caseDetails.getId(),
             "populateSDO",
-            populateStandardDirections(caseDetails.getData()));
-    }
-
-    private Map<String, Object> populateStandardDirections(Map<String, Object> data) {
-        CaseData caseData = mapper.convertValue(data, CaseData.class);
-        HearingBooking hearingBooking = hearingService.getFirstHearing(caseData.getHearingDetails()).orElse(null);
-
-        getAssigneeToDirectionMapping(standardDirectionsService.getDirections(hearingBooking))
-            .forEach((assignee, directionsElements) -> data.put(assignee.getValue(), directionsElements));
-
-        return data;
+            caseDetails.getData()
+        );
     }
 }
