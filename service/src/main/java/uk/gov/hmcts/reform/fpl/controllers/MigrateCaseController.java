b9164c419f3ef3e1dd500d9baa51b7632b801866
James Nelson <Jamesnelson117@gmail.com>
2020-10-06 10:12:13 +0100

    chore: remove CMO approved order (FPLA-2268) (#1714)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
@@ -12,11 +12,13 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.order.generated.GeneratedOrder;
+import uk.gov.hmcts.reform.fpl.model.order.CaseManagementOrder;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Api
 @RestController
@@ -32,8 +34,15 @@ public class MigrateCaseController {
         Map<String, Object> data = caseDetails.getData();
         CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
 
-        if ("PO20C50001".equals(caseData.getFamilyManCaseNumber())) {
-            data.put("orderCollection", removeInterimCareOrder(caseData.getOrderCollection()));
+        if ("LE20C50002".equals(caseData.getFamilyManCaseNumber())) {
+            data.put("hearingDetails", removeHearingLinkedToCmo(caseData.getHearingDetails(),
+                caseData.getSealedCMOs().get(0).getId()));
+
+            if (caseData.getSealedCMOs().size() > 1) {
+                data.put("sealedCMOs", removeCaseManagementOrder(caseData.getSealedCMOs()));
+            } else {
+                data.remove("sealedCMOs");
+            }
         }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
@@ -41,10 +50,19 @@ public class MigrateCaseController {
             .build();
     }
 
-    private List<Element<GeneratedOrder>> removeInterimCareOrder(List<Element<GeneratedOrder>> orders) {
-        if ("Interim care order".equals(orders.get(0).getValue().getType())) {
-            orders.remove(0);
-        }
+    private List<Element<CaseManagementOrder>> removeCaseManagementOrder(List<Element<CaseManagementOrder>> orders) {
+        orders.remove(0);
         return orders;
+    }
+
+    private List<Element<HearingBooking>> removeHearingLinkedToCmo(List<Element<HearingBooking>> hearingBookings,
+                                                                   UUID elementId) {
+        for (Element<HearingBooking> hearingBooking : hearingBookings) {
+            if (elementId.equals(hearingBooking.getValue().getCaseManagementOrderId())) {
+                hearingBooking.getValue().setCaseManagementOrderId(null);
+            }
+        }
+
+        return hearingBookings;
     }
 }
