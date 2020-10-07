6d985e81c45421eeaeced8720639105c56f74b5c
Samuel Vrech <samuelzero@gmail.com>
2020-10-07 10:51:30 +0100

    FPLA-2202: Add exclusion clause to ICO

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/docmosis/CareOrderGenerationServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/docmosis/CareOrderGenerationServiceTest.java
@@ -1,8 +1,7 @@
 package uk.gov.hmcts.reform.fpl.service.docmosis;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,6 +12,7 @@ import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.OrderTypeAndDocument;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisGeneratedOrder;
+import uk.gov.hmcts.reform.fpl.model.order.generated.ExclusionClause;
 import uk.gov.hmcts.reform.fpl.model.order.generated.FurtherDirections;
 import uk.gov.hmcts.reform.fpl.model.order.generated.InterimEndDate;
 import uk.gov.hmcts.reform.fpl.service.CaseDataExtractionService;
@@ -26,7 +26,6 @@ import static uk.gov.hmcts.reform.fpl.enums.GeneratedOrderSubtype.FINAL;
 import static uk.gov.hmcts.reform.fpl.enums.GeneratedOrderSubtype.INTERIM;
 import static uk.gov.hmcts.reform.fpl.enums.GeneratedOrderType.CARE_ORDER;
 import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.DRAFT;
-import static uk.gov.hmcts.reform.fpl.enums.OrderStatus.SEALED;
 import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.enums.ccd.fixedlists.InterimEndDateType.END_OF_PROCEEDINGS;
 
@@ -35,31 +34,51 @@ import static uk.gov.hmcts.reform.fpl.enums.ccd.fixedlists.InterimEndDateType.EN
     LookupTestConfig.class, HearingVenueLookUpService.class, JacksonAutoConfiguration.class,
     FixedTimeConfiguration.class, ChildrenService.class})
 class CareOrderGenerationServiceTest extends AbstractOrderGenerationServiceTest {
+
+    public static final String EXAMPLE_EXCLUSION_CLAUSE = "Example Exclusion Clause";
+    public static final OrderStatus ORDER_STATUS = DRAFT;
+
     @Autowired
     private CareOrderGenerationService service;
 
-    @ParameterizedTest
-    @EnumSource(GeneratedOrderSubtype.class)
-    void shouldGetTemplateDataWhenGivenPopulatedCaseData(GeneratedOrderSubtype subtype) {
-        OrderStatus orderStatus = SEALED;
-        CaseData caseData = getCase(subtype, orderStatus);
+    @Test
+    void shouldGetTemplateDataWhenGivenPopulatedCaseDataInterim() {
+        CaseData caseData = getCase(INTERIM, ORDER_STATUS);
 
         DocmosisGeneratedOrder templateData = service.getTemplateData(caseData);
 
-        DocmosisGeneratedOrder expectedData = getExpectedDocument(subtype, orderStatus);
+        DocmosisGeneratedOrder expectedData = enrichWithStandardData(CARE_ORDER, INTERIM, ORDER_STATUS,
+            DocmosisGeneratedOrder.builder()
+                .children(getChildren())
+                .orderType(CARE_ORDER)
+                .localAuthorityName(LOCAL_AUTHORITY_NAME)
+                .orderTitle("Interim care order")
+                .childrenAct("Section 38 Children Act 1989")
+                .exclusionClause(EXAMPLE_EXCLUSION_CLAUSE)
+                .orderDetails("It is ordered that the children are "
+                    + "placed in the care of Example Local Authority until the end of the proceedings.").build());
 
         assertThat(templateData).isEqualToComparingFieldByField(expectedData);
     }
 
-    @ParameterizedTest
-    @EnumSource(GeneratedOrderSubtype.class)
-    void shouldGetTemplateDataWhenGivenPopulatedCaseDataInDraft(GeneratedOrderSubtype subtype) {
-        OrderStatus orderStatus = DRAFT;
-        CaseData caseData = getCase(subtype, orderStatus);
+    @Test
+    void shouldGetTemplateDataWhenGivenPopulatedCaseDataFinal() {
+        CaseData caseData = getCase(FINAL, ORDER_STATUS);
 
         DocmosisGeneratedOrder templateData = service.getTemplateData(caseData);
 
-        DocmosisGeneratedOrder expectedData = getExpectedDocument(subtype, orderStatus);
+        var orderBuilder = DocmosisGeneratedOrder.builder()
+            .children(getChildren())
+            .orderType(CARE_ORDER)
+            .localAuthorityName(LOCAL_AUTHORITY_NAME)
+            .orderTitle("Care order")
+            .childrenAct("Section 31 Children Act 1989")
+            .orderDetails("It is ordered that the children are placed in the care of Example Local Authority.");
+
+        DocmosisGeneratedOrder expectedData = enrichWithStandardData(
+            CARE_ORDER, INTERIM, ORDER_STATUS, orderBuilder.build()
+        );
+
         assertThat(templateData).isEqualToComparingFieldByField(expectedData);
     }
 
@@ -74,35 +93,14 @@ class CareOrderGenerationServiceTest extends AbstractOrderGenerationServiceTest 
                 .directionsNeeded("Yes")
                 .directions("Example Directions")
                 .build())
-            .orderAppliesToAllChildren(YES.getValue());
-
-        if (subtype == INTERIM) {
-            caseBuilder.interimEndDate(InterimEndDate.builder().type(END_OF_PROCEEDINGS).build());
-        }
+            .orderAppliesToAllChildren(YES.getValue())
+            .orderExclusionClause(ExclusionClause.builder()
+                .exclusionClauseNeeded("Yes")
+                .exclusionClause(EXAMPLE_EXCLUSION_CLAUSE)
+                .build())
+            .interimEndDate(InterimEndDate.builder().type(END_OF_PROCEEDINGS).build());
 
         return caseBuilder.build();
     }
 
-    private DocmosisGeneratedOrder getExpectedDocument(GeneratedOrderSubtype subtype, OrderStatus orderStatus) {
-        var orderBuilder = DocmosisGeneratedOrder.builder()
-            .children(getChildren())
-            .orderType(CARE_ORDER)
-            .localAuthorityName(LOCAL_AUTHORITY_NAME);
-
-        if (subtype == INTERIM) {
-            orderBuilder
-                .orderTitle("Interim care order")
-                .childrenAct("Section 38 Children Act 1989")
-                .orderDetails("It is ordered that the children are "
-                    + "placed in the care of Example Local Authority until the end of the proceedings.");
-
-        } else if (subtype == FINAL) {
-            orderBuilder
-                .orderTitle("Care order")
-                .childrenAct("Section 31 Children Act 1989")
-                .orderDetails("It is ordered that the children are placed in the care of Example Local Authority.");
-        }
-
-        return enrichWithStandardData(CARE_ORDER, subtype, orderStatus, orderBuilder.build());
-    }
 }
