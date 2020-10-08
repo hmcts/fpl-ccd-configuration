47cb59e5e5f13af29d82079ff70beacb00002e10
Toireasa Moley <toireasa.moley@hmcts.net>
2020-10-08 15:56:04 +0100

    Adding retry

--- service/src/main/java/uk/gov/hmcts/reform/fnp/client/PaymentApi.java
+++ service/src/main/java/uk/gov/hmcts/reform/fnp/client/PaymentApi.java
@@ -5,14 +5,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.fnp.model.payment.CreditAccountPaymentRequest;
-import uk.gov.hmcts.reform.fpl.config.FeignConfiguration;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.reform.ccd.client.CoreCaseDataApi.SERVICE_AUTHORIZATION;
 
-@FeignClient(name = "payment-api", url = "${payment.api.url}", configuration = FeignConfiguration.class)
+@FeignClient(name = "payment-api", url = "${payment.api.url}", configuration = PaymentFeignConfiguration.class)
 public interface PaymentApi {
 
+    @Retry(include = {500})
     @PostMapping("/credit-account-payments")
     CreditAccountPaymentRequest createCreditAccountPayment(
         @RequestHeader(AUTHORIZATION) String authorisation,
--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/PaymentServiceRetryTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/PaymentServiceRetryTest.java
@@ -1,86 +1,40 @@
 package uk.gov.hmcts.reform.fpl.service.payment;
 
 import feign.FeignException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.fnp.client.PaymentApi;
-import uk.gov.hmcts.reform.fnp.exception.PaymentsApiException;
-import uk.gov.hmcts.reform.fnp.model.payment.CreditAccountPaymentRequest;
-import uk.gov.hmcts.reform.fnp.model.payment.FeeDto;
-import uk.gov.hmcts.reform.fpl.config.LocalAuthorityNameLookupConfiguration;
-import uk.gov.hmcts.reform.fpl.model.FeesData;
-import uk.gov.hmcts.reform.fpl.request.RequestData;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.fnp.model.payment.enums.Currency.GBP;
-import static uk.gov.hmcts.reform.fnp.model.payment.enums.Service.FPL;
-import static uk.gov.hmcts.reform.fpl.enums.C2ApplicationType.WITH_NOTICE;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {PaymentService.class})
+@ContextConfiguration
 class PaymentServiceRetryTest {
 
     @Autowired
     @Qualifier("PaymentApi")
     private PaymentApi paymentApi;
 
-    @MockBean
-    private FeeService feeService;
-
-    @MockBean
-    private AuthTokenGenerator authTokenGenerator;
-
-    @MockBean
-    private RequestData requestData;
-
-    @MockBean
-    private LocalAuthorityNameLookupConfiguration localAuthorityNameLookupConfiguration;
-
-    private PaymentService paymentService;
-
-    private static final Long CASE_ID = 1L;
-    FeeDto feeForC2WithNotice = FeeDto.builder().calculatedAmount(BigDecimal.TEN).build();
-
-    @BeforeEach
-    void setup() {
-        paymentService = new PaymentService(feeService, paymentApi, authTokenGenerator, requestData, localAuthorityNameLookupConfiguration,
-            "TEST_SITE_ID");
-        when(feeService.getFeesDataForC2(WITH_NOTICE)).thenReturn(buildFeesData(feeForC2WithNotice));
-    }
-
     @Test
     public void testRetry() {
-
-        CreditAccountPaymentRequest expectedPaymentRequest = testCreditAccountPaymentRequestBuilder()
-            .customerReference("1")
-            .amount(feeForC2WithNotice.getCalculatedAmount())
-            .fees(List.of(feeForC2WithNotice))
-            .build();
-
         Mockito.doThrow(FeignException.InternalServerError.class).when(paymentApi).createCreditAccountPayment(
             any(), any(), any());
 
         try {
-            paymentService.callPaymentsApi(expectedPaymentRequest);
-        } catch (PaymentsApiException ex) {
+            paymentApi.createCreditAccountPayment(any(), any(), any());
+        } catch (FeignException.InternalServerError ex) {
         } finally {
             verify(paymentApi, times(3)).createCreditAccountPayment(any(), any(), any());
 
@@ -100,24 +54,4 @@ class PaymentServiceRetryTest {
             return paymentApi;
         }
     }
-
-    private CreditAccountPaymentRequest.CreditAccountPaymentRequestBuilder testCreditAccountPaymentRequestBuilder() {
-        return CreditAccountPaymentRequest.builder()
-            .accountNumber("PBA123")
-            .caseReference("clientCode")
-            .ccdCaseNumber(String.valueOf(CASE_ID))
-            .currency(GBP)
-            .description("Payment for case: " + CASE_ID)
-            .organisationName("Example Local Authority")
-            .service(FPL)
-            .siteId("TEST_SITE_ID");
-    }
-
-    private FeesData buildFeesData(FeeDto feeDto) {
-        return FeesData.builder()
-            .totalAmount(feeDto.getCalculatedAmount())
-            .fees(List.of(feeDto))
-            .build();
-    }
-
 }
