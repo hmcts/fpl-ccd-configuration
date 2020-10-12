06d7197ec3e2c6b8118956bacb1618d1628f29ea
Joe Zazzaro-Francis <joez@kainos.com>
2020-10-12 12:27:37 +0100

    Fixed allocated judge not being pulled through into notice of hearing

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/docmosis/NoticeOfHearingGenerationService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/docmosis/NoticeOfHearingGenerationService.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.reform.fpl.config.HmctsCourtLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.HearingVenue;
+import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisHearingBooking;
 import uk.gov.hmcts.reform.fpl.model.docmosis.DocmosisNoticeOfHearing;
 import uk.gov.hmcts.reform.fpl.service.CaseDataExtractionService;
@@ -19,6 +20,7 @@ import static uk.gov.hmcts.reform.fpl.enums.DocmosisImages.CREST;
 import static uk.gov.hmcts.reform.fpl.enums.HearingType.OTHER;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.DATE;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateToString;
+import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.getSelectedJudge;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -33,6 +35,9 @@ public class NoticeOfHearingGenerationService {
         String hearingVenue = venue.getAddress() != null
             ? hearingVenueLookUpService.buildHearingVenue(venue) : hearingBooking.getCustomPreviousVenue();
 
+        JudgeAndLegalAdvisor judgeAndLegalAdvisor = getSelectedJudge(hearingBooking.getJudgeAndLegalAdvisor(),
+            caseData.getAllocatedJudge());
+
         return DocmosisNoticeOfHearing.builder()
             .familyManCaseNumber(caseData.getFamilyManCaseNumber())
             .courtName(hmctsCourtLookupConfiguration.getCourt(caseData.getCaseLocalAuthority()).getName())
@@ -44,7 +49,7 @@ public class NoticeOfHearingGenerationService {
                 .hearingVenue(hearingVenue)
                 .preHearingAttendance(dataService.extractPrehearingAttendance(hearingBooking))
                 .build())
-            .judgeAndLegalAdvisor(dataService.getJudgeAndLegalAdvisor(hearingBooking.getJudgeAndLegalAdvisor()))
+            .judgeAndLegalAdvisor(dataService.getJudgeAndLegalAdvisor(judgeAndLegalAdvisor))
             .postingDate(formatLocalDateToString(LocalDate.now(), DATE))
             .additionalNotes(hearingBooking.getAdditionalNotes())
             .courtseal(COURT_SEAL.getValue())
