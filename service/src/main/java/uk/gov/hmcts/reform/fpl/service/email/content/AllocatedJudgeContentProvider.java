bfa164ec3d755e351c82571301399bd22a86c1da
Aliveni Choppa <aliveni.ch@gmail.com>
2020-10-14 14:33:46 +0100

    Add Family man ID to the judge allocation email template

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/AllocatedJudgeContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/AllocatedJudgeContentProvider.java
@@ -18,6 +18,7 @@ public class AllocatedJudgeContentProvider extends AbstractEmailContentProvider 
         allocatedJudgeTemplate.setJudgeName(caseData.getAllocatedJudge().getJudgeName());
         allocatedJudgeTemplate.setCaseName(caseData.getCaseName());
         allocatedJudgeTemplate.setCaseUrl(getCaseUrl(caseData.getId()));
+        allocatedJudgeTemplate.setFamilyManCaseNumber(caseData.getFamilyManCaseNumber());
 
         return allocatedJudgeTemplate;
     }
