87c11bf6d79da8e2b788ae46ee195156368ce71e
Lewis Birks <lewisb@kainos.com>
2020-10-20 12:20:46 +0100

    update event data model

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/event/UploadCMOEventData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/event/UploadCMOEventData.java
@@ -1,41 +1,91 @@
 package uk.gov.hmcts.reform.fpl.model.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import lombok.Value;
+import uk.gov.hmcts.reform.fpl.enums.CMOType;
 import uk.gov.hmcts.reform.fpl.enums.YesNo;
+import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.utils.ElementUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+import static uk.gov.hmcts.reform.fpl.enums.CMOType.AGREED;
+
+// TODO: 19/10/2020 Delete deprecated when FPLA-2019 toggled
 @Value
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class UploadCMOEventData {
 
-    // Uploaded document
+    // Uploaded documents (Page 2)
     DocumentReference uploadedCaseManagementOrder;
+    DocumentReference replacementCMO;
+    List<Element<SupportingEvidenceBundle>> cmoSupportingDocs;
 
-    // Dynamic lists
-    Object hearingsWithoutApprovedCMO;
+    // Dynamic lists (Page 1)
+    Object pastHearingsForCMO;
+    Object futureHearingsForCMO;
+
+    // Approved or draft CMO (Page 1)
+    CMOType cmoUploadType;
 
     // Readonly info
-    String cmoJudgeInfo;
-    String cmoHearingInfo;
+    String cmosSentToJudge; // (Page 1)
+    String cmoHearingInfo; // (Page 2)
+    DocumentReference previousCMO; // (Page 2)
+    String cmoJudgeInfo; // (Page 3)
+    DocumentReference cmoToSend; // (Page 3)
+    @Deprecated
     String multiHearingsWithCMOs;
+    @Deprecated
     String singleHearingWithCMO;
 
     // Conditional control fields
+    YesNo showCMOsSentToJudge; // (Page 1)
+    YesNo showReplacementCMO; // (Page 2)
+    @Deprecated
     NumberOfHearingsOptions numHearingsWithoutCMO;
+    @Deprecated
     YesNo showHearingsSingleTextArea;
+    @Deprecated
     YesNo showHearingsMultiTextArea;
+
+    public List<Element<SupportingEvidenceBundle>> getCmoSupportingDocs() {
+        return defaultIfNull(cmoSupportingDocs, new ArrayList<>());
+    }
+
+    @JsonIgnore
+    public boolean isAgreed() {
+        // TODO: 19/10/2020 remove second condition when FPLA-2019 is toggled on
+        return AGREED == cmoUploadType || null == cmoUploadType;
+    }
+
+    @JsonIgnore
+    public UUID getSelectedHearingId(ObjectMapper mapper) {
+        Object dynamicList = defaultIfNull(pastHearingsForCMO, futureHearingsForCMO);
+        return ElementUtils.getDynamicListValueCode(dynamicList, mapper);
+    }
 
     public static String[] transientFields() {
         return new String[]{
-            "uploadedCaseManagementOrder", "hearingsWithoutApprovedCMO", "cmoJudgeInfo", "cmoHearingInfo",
+            "showCMOsSentToJudge", "cmosSentToJudge", "cmoUploadType", "pastHearingsForCMO", "futureHearingsForCMO",
+            "cmoHearingInfo", "showReplacementCMO", "previousCMO", "uploadedCaseManagementOrder", "replacementCMO",
+            "cmoSupportingDocs", "cmoJudgeInfo", "cmoToSend",
+            // TODO: 19/10/2020 Delete after this comment when FPLA-2019 is toggled on
             "numHearingsWithoutCMO", "singleHearingWithCMO", "multiHearingsWithCMOs", "showHearingsSingleTextArea",
             "showHearingsMultiTextArea"
         };
     }
 
+    @Deprecated
     public enum NumberOfHearingsOptions {
         SINGLE,
         MULTI,
