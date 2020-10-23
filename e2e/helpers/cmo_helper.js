0e2083cf8a1eb68767206991d4a170a393038dc0
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2020-10-23 15:07:52 +0100

    feat: upload draft cmo (FPLA-2019) (#1775)
    * convert map to object
    
    * fix tests
    
    * update tab links
    
    * new ccd defs
    
    * permissions
    
    * more permission updates, case event to complex types, and searchable things
    
    * update models
    
    * updated cmo service
    
    * updated controller
    
    * new notification template
    
    * update event data model
    
    * sort the hearings so they are displayed ordered by their start date
    
    * update existing tests
    
    * update show conditions to remove grey indent
    
    * ux feedback
    
    * service unit tests
    
    * model unit tests
    
    * update from refactor
    
    * content provider test
    
    * event handler test
    
    * integration tests
    
    * checkstyle :)
    
    * e2e tests
    
    * fix review cmo service
    
    * fix e2e tests
    
    * clean helper 🧼
    
    * specify hearing
    
    * merge nested if
    
    * additional tests
    
    * toggle tests
    
    * actually test supporting docs
    
    * invert if and inject mocks
    
    * assert case data
    
    * remove spring dependency in test
    
    * assert against template
    
    * assert keys are removed v2
    
    * persist docs with order when agreed

--- e2e/helpers/cmo_helper.js
+++ e2e/helpers/cmo_helper.js
@@ -1,23 +1,23 @@
 const config = require('../config');
 
-const localAuthoritySendsAgreedCmo = async function (I, caseViewPage, uploadCaseManagementOrderEventPage, hearingDate, multiHearings) {
-  await caseViewPage.goToNewActions(config.applicationActions.uploadCMO);
-
-  if (multiHearings) {
-    await uploadCaseManagementOrderEventPage.associateHearing(hearingDate);
-    await I.retryUntilExists(() => I.click('Continue'), '#uploadedCaseManagementOrder');
-  }
-
-  await uploadCaseManagementOrderEventPage.uploadCaseManagementOrder(config.testWordFile);
-  await I.completeEvent('Submit');
+const localAuthoritySendsAgreedCmo = async (I, caseViewPage, uploadCMOEventPage, hearing, supportingDocs) => {
+  await uploadCMO(I, caseViewPage, uploadCMOEventPage, hearing, supportingDocs, () => {
+    uploadCMOEventPage.selectAgreedCMO();
+    uploadCMOEventPage.selectPastHearing(hearing);
+  });
 };
 
-const judgeSendsReviewedCmoToAllParties = async function(I, caseId, caseViewPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage) {
+const localAuthorityUploadsDraftCmo = async (I, caseViewPage, uploadCMOEventPage, hearing, supportingDocs) => {
+  await uploadCMO(I, caseViewPage, uploadCMOEventPage, hearing, supportingDocs, () => {
+    uploadCMOEventPage.selectDraftCMO();
+    uploadCMOEventPage.selectFutureHearing(hearing);
+  });
+};
+
+const judgeSendsReviewedCmoToAllParties = async (I, caseId, caseViewPage, uploadCaseManagementOrderEventPage, reviewAgreedCaseManagementOrderEventPage) => {
   await I.navigateToCaseDetailsAs(config.swanseaLocalAuthorityUserOne, caseId);
-  await localAuthoritySendsAgreedCmo(I, caseViewPage, uploadCaseManagementOrderEventPage, '1 January 2020', true);
-  I.seeEventSubmissionConfirmation(config.applicationActions.uploadCMO);
-  await localAuthoritySendsAgreedCmo(I, caseViewPage, uploadCaseManagementOrderEventPage);
-  I.seeEventSubmissionConfirmation(config.applicationActions.uploadCMO);
+  await localAuthoritySendsAgreedCmo(I, caseViewPage, uploadCaseManagementOrderEventPage, 'Case management hearing, 1 January 2020');
+  await localAuthoritySendsAgreedCmo(I, caseViewPage, uploadCaseManagementOrderEventPage, 'Final hearing, 1 March 2020');
   await I.navigateToCaseDetailsAs(config.judicaryUser, caseId);
   await caseViewPage.goToNewActions(config.applicationActions.reviewAgreedCmo);
   reviewAgreedCaseManagementOrderEventPage.selectCMOToReview('1 January 2020');
@@ -27,7 +27,24 @@ const judgeSendsReviewedCmoToAllParties = async function(I, caseId, caseViewPage
   I.seeEventSubmissionConfirmation(config.applicationActions.reviewAgreedCmo);
 };
 
+const uploadCMO = async (I, caseViewPage, uploadCMOEventPage, hearing, supportingDocs, selectHearing) => {
+  await caseViewPage.goToNewActions(config.applicationActions.uploadCMO);
+  await I.waitForSelector(uploadCMOEventPage.fields.cmoUploadType.id);
+  selectHearing();
+  await I.retryUntilExists(() => I.click('Continue'), '#uploadedCaseManagementOrder');
+  uploadCMOEventPage.checkCMOInfo(hearing);
+  await uploadCMOEventPage.uploadCaseManagementOrder(config.testWordFile);
+  if (supportingDocs) {
+    await uploadCMOEventPage.attachSupportingDocs(supportingDocs);
+  }
+  await I.retryUntilExists(() => I.click('Continue'), 'ccd-read-document-field');
+  uploadCMOEventPage.reviewInfo('mockFile.docx', 'Her Honour Judge Reed');
+  await I.completeEvent('Submit');
+  I.seeEventSubmissionConfirmation(config.applicationActions.uploadCMO);
+};
+
 module.exports = {
   localAuthoritySendsAgreedCmo,
+  localAuthorityUploadsDraftCmo,
   judgeSendsReviewedCmoToAllParties,
 };
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
@@ -87,6 +87,10 @@ public class FeatureToggleService {
         return ldClient.boolVariation("send-grid", createLDUser(), false);
     }
 
+    public boolean isUploadDraftCMOEnabled() {
+        return ldClient.boolVariation("upload-draft-cmo", createLDUser(), false);
+    }
+
     private LDUser createLDUser() {
         return createLDUser(Map.of());
     }
