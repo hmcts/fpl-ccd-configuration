267b5bb3e831131705dfee07e3c9ffd18aae6012
James Nelson <Jamesnelson117@gmail.com>
2020-10-27 16:32:27 +0000

    FPLA-2173: Fixed validator tests

--- service/src/main/java/uk/gov/hmcts/reform/fpl/validation/validators/HasRemainingFinalOrdersValidator.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/validation/validators/HasRemainingFinalOrdersValidator.java
@@ -17,10 +17,10 @@ public class HasRemainingFinalOrdersValidator implements ConstraintValidator<Has
 
         return caseData.getChildren1().stream()
             .map(Element::getValue)
-            .allMatch(this::hasFinalFields);
+            .noneMatch(this::hasFinalFields);
     }
 
     private boolean hasFinalFields(Child child) {
-        return child.getFinalOrderIssuedType() != null && child.getFinalOrderIssued() != null;
+        return child.getFinalOrderIssuedType() != null || child.getFinalOrderIssued() != null;
     }
 }
