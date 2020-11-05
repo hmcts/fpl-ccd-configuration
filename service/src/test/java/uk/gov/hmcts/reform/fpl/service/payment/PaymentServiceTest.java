e0db82c49afd8cfed35f31b13cba6a007705ff16
Nikhil Koranne <nikhilkkainos.com@Geona.local>
2020-11-05 17:08:51 +0000

    feat:FPLA-1895 removed unused imports

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/PaymentServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/payment/PaymentServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
