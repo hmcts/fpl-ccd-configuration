68511f1b96aafa401da1676ffcca5ea7a3bf65b4
aliveni <aliveni.choppa@hmcts.net>
2020-11-06 14:32:43 +0000

    chore: FPLA-2364 Update case state (#1842)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.fpl.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,19 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.HearingBooking;
-import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.order.CaseManagementOrder;
+import uk.gov.hmcts.reform.fpl.enums.State;
 
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Api
 @RestController
 @RequestMapping("/callback/migrate-case")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class MigrateCaseController {
 
     private final ObjectMapper mapper;
@@ -32,33 +29,15 @@ public class MigrateCaseController {
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         Map<String, Object> data = caseDetails.getData();
-        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
 
-        if (1602246223743823L == caseDetails.getId()) {
-            data.put("hearingDetails", removeHearingLinkedToCmo(caseData.getHearingDetails(),
-                caseData.getSealedCMOs().get(0).getId()));
-
-            data.put("sealedCMOs", removeCaseManagementOrder(caseData.getSealedCMOs()));
+        if (1603370139459131L == caseDetails.getId()) {
+            log.info("Updating case reference {} state from '{}' to '{}'",
+                caseDetails.getId(), caseDetails.getState(), State.CASE_MANAGEMENT);
+            data.put("state", State.CASE_MANAGEMENT);
         }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(data)
             .build();
-    }
-
-    private List<Element<CaseManagementOrder>> removeCaseManagementOrder(List<Element<CaseManagementOrder>> orders) {
-        orders.remove(0);
-        return orders;
-    }
-
-    private List<Element<HearingBooking>> removeHearingLinkedToCmo(List<Element<HearingBooking>> hearingBookings,
-                                                                   UUID elementId) {
-        for (Element<HearingBooking> hearingBooking : hearingBookings) {
-            if (elementId.equals(hearingBooking.getValue().getCaseManagementOrderId())) {
-                hearingBooking.getValue().setCaseManagementOrderId(null);
-            }
-        }
-
-        return hearingBookings;
     }
 }
