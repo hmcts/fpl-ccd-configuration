f75c47710e694196b49d11d87439e4dbfbeafef9
Aliveni Choppa <aliveni.ch@gmail.com>
2020-11-06 13:02:41 +0000

    Update case state

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
@@ -11,14 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.HearingBooking;
-import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.order.CaseManagementOrder;
+import uk.gov.hmcts.reform.fpl.enums.State;
 
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Api
 @RestController
@@ -32,33 +27,13 @@ public class MigrateCaseController {
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         Map<String, Object> data = caseDetails.getData();
-        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
 
-        if (1602246223743823L == caseDetails.getId()) {
-            data.put("hearingDetails", removeHearingLinkedToCmo(caseData.getHearingDetails(),
-                caseData.getSealedCMOs().get(0).getId()));
-
-            data.put("sealedCMOs", removeCaseManagementOrder(caseData.getSealedCMOs()));
+        if (1603370139459131L == caseDetails.getId()) {
+            data.put("state", State.CASE_MANAGEMENT);
         }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(data)
             .build();
-    }
-
-    private List<Element<CaseManagementOrder>> removeCaseManagementOrder(List<Element<CaseManagementOrder>> orders) {
-        orders.remove(0);
-        return orders;
-    }
-
-    private List<Element<HearingBooking>> removeHearingLinkedToCmo(List<Element<HearingBooking>> hearingBookings,
-                                                                   UUID elementId) {
-        for (Element<HearingBooking> hearingBooking : hearingBookings) {
-            if (elementId.equals(hearingBooking.getValue().getCaseManagementOrderId())) {
-                hearingBooking.getValue().setCaseManagementOrderId(null);
-            }
-        }
-
-        return hearingBookings;
     }
 }
