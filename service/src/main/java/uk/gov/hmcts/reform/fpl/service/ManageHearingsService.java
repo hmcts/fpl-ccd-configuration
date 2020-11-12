8c809f85f12e134627062dc21ce4ac97f8fef5f9
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-11-12 14:37:12 +0000

    feat: judge email address entry in manage hearings (FPLA-2030) (#1853)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsService.java
@@ -50,7 +50,10 @@ import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.findElement;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.getDynamicListSelectedValue;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.buildAllocatedJudgeLabel;
+import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.formatJudgeTitleAndName;
+import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.getHearingJudge;
 import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.getJudgeForTabView;
+import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.getLegalAdvisorName;
 import static uk.gov.hmcts.reform.fpl.utils.JudgeAndLegalAdvisorHelper.prepareJudgeFields;
 
 @Service
@@ -221,7 +224,6 @@ public class ManageHearingsService {
         }
     }
 
-
     public void sendNoticeOfHearing(CaseData caseData, HearingBooking hearingBooking) {
         if (YES.getValue().equals(caseData.getSendNoticeOfHearing())) {
             DocmosisNoticeOfHearing notice = noticeOfHearingGenerationService.getTemplateData(caseData, hearingBooking);
@@ -301,6 +303,10 @@ public class ManageHearingsService {
             .venueCustomAddress(caseData.getHearingVenueCustom())
             .startDate(caseData.getHearingStartDate())
             .endDate(caseData.getHearingEndDate())
+            .allocatedJudgeLabel(caseData.getAllocatedJudge() != null
+                ? formatJudgeTitleAndName(caseData.getAllocatedJudge().toJudgeAndLegalAdvisor()) : null)
+            .hearingJudgeLabel(getHearingJudge(caseData.getJudgeAndLegalAdvisor()))
+            .legalAdvisorLabel(getLegalAdvisorName(caseData.getJudgeAndLegalAdvisor()))
             .judgeAndLegalAdvisor(getJudgeForTabView(caseData.getJudgeAndLegalAdvisor(), caseData.getAllocatedJudge()))
             .additionalNotes(caseData.getNoticeOfHearingNotes())
             .build();
@@ -330,6 +336,10 @@ public class ManageHearingsService {
             .customPreviousVenue(customPreviousVenue)
             .startDate(caseData.getHearingStartDate())
             .endDate(caseData.getHearingEndDate())
+            .allocatedJudgeLabel(caseData.getAllocatedJudge() != null
+                ? formatJudgeTitleAndName(caseData.getAllocatedJudge().toJudgeAndLegalAdvisor()) : null)
+            .hearingJudgeLabel(getHearingJudge(caseData.getJudgeAndLegalAdvisor()))
+            .legalAdvisorLabel(getLegalAdvisorName(caseData.getJudgeAndLegalAdvisor()))
             .judgeAndLegalAdvisor(getJudgeForTabView(caseData.getJudgeAndLegalAdvisor(), caseData.getAllocatedJudge()))
             .previousHearingVenue(caseData.getPreviousHearingVenue())
             .additionalNotes(caseData.getNoticeOfHearingNotes())
