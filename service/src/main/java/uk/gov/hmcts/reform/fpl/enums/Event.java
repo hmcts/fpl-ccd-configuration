7c0cea5f17071f11186f1438df630e1b9dfd1199
Toireasa Moley <toireasa.moley@hmcts.net>
2020-11-19 16:32:17 +0000

    Adding event name change

--- service/src/main/java/uk/gov/hmcts/reform/fpl/enums/Event.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/enums/Event.java
@@ -32,7 +32,7 @@ public enum Event {
     COURT_SERVICES("attendingHearing", "Court services needed", List.of(OPEN, RETURNED)),
     SUBMIT_APPLICATION("submitApplication", "Submit application", List.of(OPEN, RETURNED)),
     CASE_NAME("changeCaseName", "Change case name", List.of(OPEN, RETURNED)),
-    SUPPORTING_DOCUMENTS("uploadSupportingDocuments", "Upload supporting documents", List.of(OPEN));
+    SUPPORTING_DOCUMENTS("uploadSupportingDocuments", "Upload documents", List.of(OPEN));
 
     private final String id;
     private String name;
