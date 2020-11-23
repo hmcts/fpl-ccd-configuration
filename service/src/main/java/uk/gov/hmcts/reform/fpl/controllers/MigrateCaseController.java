97b6a312c112aa0a1bd1cbcc3a3b6c11ca87c671
Lewis Birks <lewisb@kainos.com>
2020-11-23 11:31:13 +0000

    remove c2 bundle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,35 +16,31 @@ import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.List;
-import java.util.Map;
 
 @Api
 @RestController
 @RequestMapping("/callback/migrate-case")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
-public class MigrateCaseController {
-
-    private final ObjectMapper mapper;
+public class MigrateCaseController extends CallbackController {
 
     @PostMapping("/about-to-submit")
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
-        Map<String, Object> data = caseDetails.getData();
-        CaseData caseData = mapper.convertValue(caseDetails.getData(), CaseData.class);
+        CaseData caseData = getCaseData(caseDetails);
 
-        if ("PO20C50014".equals(caseData.getFamilyManCaseNumber())) {
+        if ("CF20C50014".equals(caseData.getFamilyManCaseNumber())) {
             log.info("Removing c2 document bundle from case reference {}", caseDetails.getId());
-            data.put("c2DocumentBundle", removeC2Document(caseData.getC2DocumentBundle()));
+            caseDetails.getData().put("c2DocumentBundle", removeC2Document(caseData.getC2DocumentBundle()));
         }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
-            .data(data)
+            .data(caseDetails.getData())
             .build();
     }
 
     private List<Element<C2DocumentBundle>> removeC2Document(List<Element<C2DocumentBundle>> documentBundle) {
-        documentBundle.remove(0);
+        documentBundle.remove(1);
         return documentBundle;
     }
 }
