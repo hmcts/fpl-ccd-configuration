c564c612dfb2d5531a226cb79ef5651ffbd22752
James Nelson <Jamesnelson117@gmail.com>
2020-11-23 15:51:49 +0000

    FPLA-2344: e2e fixes

--- e2e/tests/hmctsSuperUserAdministersCase_test.js
+++ e2e/tests/hmctsSuperUserAdministersCase_test.js
@@ -5,6 +5,8 @@ const caseManagementCaseData = require('../fixtures/caseData/prepareForHearing.j
 const closedCaseData = require('../fixtures/caseData/closedCase.json');
 const orderFunctions = require('../helpers/generated_order_helper');
 const blankOrder = require('../fixtures/orders/blankOrder.js');
+const finalHearingCaseData = require('../fixtures/caseData/finalHearing.json');
+const moment = require('moment');
 
 Feature('Case administration by super user');
 
@@ -41,6 +43,33 @@ Scenario('HMCTS super user removes an order from a case', async ({I, caseViewPag
     document: 'Blank order (C21).pdf',
   };
   await orderFunctions.assertOrder(I, caseViewPage, order, issuedDate, false, true);
+});
+
+Scenario('HMCTS super user removes a cmo from a case', async ({I, caseViewPage, removeOrderEventPage}) => {
+  const newCaseId = await I.submitNewCaseWithData(finalHearingCaseData);
+
+  await I.signIn(config.hmctsSuperUser);
+  await I.navigateToCaseDetailsAs(config.hmctsSuperUser, newCaseId);
+
+  await caseViewPage.goToNewActions(config.superUserActions.removeOrder);
+
+  let order = finalHearingCaseData.caseData.sealedCMOs[0].value;
+  const labelToSelect = 'Case management order - ' + moment(order.dateIssued).format('D MMMM YYYY');
+  removeOrderEventPage.selectOrderToRemove(labelToSelect);
+  await I.goToNextPage();
+  removeOrderEventPage.addRemoveOrderReason('Entered incorrect order');
+  await I.completeEvent('Submit');
+  I.seeEventSubmissionConfirmation(config.superUserActions.removeOrder);
+
+  caseViewPage.selectTab(caseViewPage.tabs.orders);
+  const sealedCMO = 'Removed case management orders 1';
+
+  I.seeInTab([sealedCMO, 'Order'], 'mockFile.pdf');
+  I.seeInTab([sealedCMO, 'Hearing'], 'Case management hearing, 1 January 2020');
+  I.seeInTab([sealedCMO, 'Date sent'], '27 Aug 2020');
+  I.seeInTab([sealedCMO, 'Date issued'], '27 Aug 2020');
+  I.seeInTab([sealedCMO, 'Judge'], 'Her Honour Judge Reed');
+  I.seeInTab([sealedCMO, 'Reason for removal'], 'Entered incorrect order');
 });
 
 Scenario('HMCTS super user changes state from case management to final hearing', async ({I, caseViewPage, changeCaseStateEventPage}) => {
