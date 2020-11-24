ef119afc5826bafdd45b2aeeddffbd6302252b5e
Lewis Birks <lewisb@kainos.com>
2020-11-24 15:35:57 +0000

    migration id

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/MigrateCaseController.java
@@ -21,20 +21,27 @@ import uk.gov.hmcts.reform.fpl.model.Others;
 @Slf4j
 public class MigrateCaseController extends CallbackController {
 
+    private static final String MIGRATION_ID_KEY = "migrationId";
+
     @PostMapping("/about-to-submit")
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         CaseData caseData = getCaseData(caseDetails);
 
-        if ("CF20C50024".equals(caseData.getFamilyManCaseNumber())) {
+        if ("FPLA-2429".equals(getMigrationId(caseDetails)) && "CF20C50024".equals(caseData.getFamilyManCaseNumber())) {
             log.info("Removing others from case reference {}", caseDetails.getId());
             Others others = caseData.getOthers();
             caseDetails.getData().put("others", nullFirstOther(others));
+            caseDetails.getData().remove(MIGRATION_ID_KEY);
         }
 
         return AboutToStartOrSubmitCallbackResponse.builder()
             .data(caseDetails.getData())
             .build();
+    }
+
+    private Object getMigrationId(CaseDetails caseDetails) {
+        return caseDetails.getData().get(MIGRATION_ID_KEY);
     }
 
     private Others nullFirstOther(Others others) {
