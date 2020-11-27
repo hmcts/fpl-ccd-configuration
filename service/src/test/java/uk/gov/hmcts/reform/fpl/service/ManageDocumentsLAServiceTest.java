f7ed8cbfd758e0f86cf721ede5fbb40c944b3af8
Joe Zazzaro-Francis <joez@kainos.com>
2020-11-27 17:38:16 +0000

    More service tests

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/ManageDocumentsLAServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/ManageDocumentsLAServiceTest.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.CourtBundle;
 import uk.gov.hmcts.reform.fpl.model.HearingFurtherEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.ManageDocumentLA;
 import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -71,5 +73,35 @@ public class ManageDocumentsLAServiceTest {
             .build();
 
         assertThat(manageDocumentLAService.getFurtherEvidenceCollection(caseData)).isEqualTo(hearingEvidence);
+    }
+
+    @Test
+    void shouldReturnNewCourtBundleListWithCourtBundleWhenNoExistingCourtBundlesPresentForSelectedHearing() {
+        UUID selectedHearingId = randomUUID();
+
+        CaseData caseData = CaseData.builder()
+            .manageDocumentsCourtBundle(CourtBundle.builder().hearing("Test hearing").build())
+            .courtBundleHearingList(selectedHearingId.toString())
+            .build();
+
+        assertThat(manageDocumentLAService.buildCourtBundleList(caseData))
+            .isEqualTo(List.of(element(selectedHearingId, caseData.getManageDocumentsCourtBundle())));
+    }
+
+    @Test
+    void shouldReturnEditedCourtBundleListWithCourtBundleWhenExistingCourtBundlePresentForSelectedHearing() {
+        UUID selectedHearingId = randomUUID();
+        List<Element<CourtBundle>> courtBundleList = new ArrayList<>();
+        courtBundleList.add(element(selectedHearingId, CourtBundle.builder().hearing("Test hearing").build()));
+
+        CourtBundle editedBundle = CourtBundle.builder().hearing("Edited hearing").build();
+        CaseData caseData = CaseData.builder()
+            .courtBundleList(courtBundleList)
+            .manageDocumentsCourtBundle(editedBundle)
+            .courtBundleHearingList(selectedHearingId.toString())
+            .build();
+
+        assertThat(unwrapElements(manageDocumentLAService.buildCourtBundleList(caseData)))
+            .containsExactly(editedBundle);
     }
 }
