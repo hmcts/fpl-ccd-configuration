5d5270791eb5562c955f23670304840c7ffb5974
Samuel Vrech <samuelzero@gmail.com>
2020-11-27 14:45:04 +0000

    chore: dummy5 - more tests + increased

--- e2e/tests/gatekeeperAdministersCaseAfterGatekeeping_test.js
+++ e2e/tests/gatekeeperAdministersCaseAfterGatekeeping_test.js
@@ -8,7 +8,7 @@ const gatekeepingNoHearingDetails = require('../fixtures/caseData/gatekeepingNoH
 let caseId;
 let submittedAt;
 
-Feature('Gatekeeper Case administration after gatekeeping @failure @debug-failure');
+Feature('Gatekeeper Case administration after gatekeeping @failure @debug-failure @now-test');
 
 BeforeSuite(async ({I}) => {
   caseId = await I.submitNewCaseWithData(gatekeepingNoHearingDetails);
