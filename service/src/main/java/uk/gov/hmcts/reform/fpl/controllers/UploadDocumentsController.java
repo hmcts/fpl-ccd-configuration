f712d6148e619072e433df83efe486532eb96563
Toireasa Moley <toireasa.moley@hmcts.net>
2020-12-02 17:38:15 +0000

    Adding convert to collection code

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadDocumentsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadDocumentsController.java
@@ -10,30 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType;
-import uk.gov.hmcts.reform.fpl.model.ApplicationDocument;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.common.Document;
-import uk.gov.hmcts.reform.fpl.model.common.DocumentSocialWorkOther;
-import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.service.ApplicationDocumentsService;
 import uk.gov.hmcts.reform.fpl.service.DocumentsValidatorService;
 import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentsService;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-
-import static java.util.Objects.isNull;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.CARE_PLAN;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.CHECKLIST_DOCUMENT;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.SOCIAL_WORK_CHRONOLOGY;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.SOCIAL_WORK_STATEMENT;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.SWET;
-import static uk.gov.hmcts.reform.fpl.enums.ApplicationDocumentType.THRESHOLD;
-import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
 
 @Api
 @RestController
@@ -72,53 +55,10 @@ public class UploadDocumentsController extends CallbackController {
                 caseDataBefore.getApplicationDocuments()));
         } else {
             // New document event is not enabled so move old collection to new
-            addDocumentsToApplicationDocumentCollection(caseData, caseDetails);
+            caseDetails.getData().put("applicationDocuments", applicationDocumentsService.convertOldDocumentsToApplicationDocumentCollection(caseData));
         }
 
         return respond(caseDetails);
-    }
-
-    private void addDocumentsToApplicationDocumentCollection(CaseData caseData, CaseDetails caseDetails) {
-        List<Element<ApplicationDocument>> applicationDocuments = new ArrayList<>();
-
-        Map<Document, ApplicationDocumentType> documentsToProcess = new LinkedHashMap<>();
-        documentsToProcess.put(caseData.getSocialWorkChronologyDocument(), SOCIAL_WORK_CHRONOLOGY);
-        documentsToProcess.put(caseData.getSocialWorkStatementDocument(), SOCIAL_WORK_STATEMENT);
-        documentsToProcess.put(caseData.getSocialWorkAssessmentDocument(), SOCIAL_WORK_STATEMENT);
-        documentsToProcess.put(caseData.getSocialWorkCarePlanDocument(), CARE_PLAN);
-        documentsToProcess.put(caseData.getSocialWorkEvidenceTemplateDocument(), SWET);
-        documentsToProcess.put(caseData.getThresholdDocument(), THRESHOLD);
-        documentsToProcess.put(caseData.getChecklistDocument(), CHECKLIST_DOCUMENT);
-
-        //this maps to other
-        //need to loop through these
-        List<Element<DocumentSocialWorkOther>> otherDocuments = caseData.getOtherSocialWorkDocuments();
-
-        for (Map.Entry<Document, ApplicationDocumentType> document : documentsToProcess.entrySet()) {
-            Document applicationDocument = document.getKey();
-            ApplicationDocumentType documentType = document.getValue();
-
-            if(!isNull(applicationDocument.getDocumentStatus()) || !isNull(applicationDocument.getTypeOfDocument())) {
-                //cater for if status is to follow
-                ApplicationDocument updatedDocument = convertOldDocumentToNewApplicationDocument(applicationDocument, documentType);
-                applicationDocuments.add(element(updatedDocument));
-            }
-        }
-
-        caseDetails.getData().put("applicationDocuments", applicationDocuments);
-    }
-
-    private ApplicationDocument convertOldDocumentToNewApplicationDocument(Document document, ApplicationDocumentType documentType) {
-
-        ApplicationDocument applicationDocument = ApplicationDocument.builder()
-            .document(document.getTypeOfDocument())
-            .dateTimeUploaded(document.getDateTimeUploaded())
-            .uploadedBy(document.getUploadedBy())
-            .documentType(documentType)
-            .includedInSWET(null)
-            .build();
-
-        return applicationDocument;
     }
 }
 
