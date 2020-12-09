83fee16b1d325af225878d0405e2710a852a4941
Joe Zazzaro-Francis <joez@kainos.com>
2020-12-09 18:21:59 +0000

    Fix court bundle model builder

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CourtBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CourtBundle.java
@@ -1,17 +1,16 @@
 package uk.gov.hmcts.reform.fpl.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentMetaData;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 
+import java.time.LocalDateTime;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
-@Builder(toBuilder = true)
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourtBundle extends DocumentMetaData {
     private String hearing;
     private DocumentReference document;
@@ -20,5 +19,16 @@ public class CourtBundle extends DocumentMetaData {
     @Override
     public DocumentReference getTypeOfDocument() {
         return document;
+    }
+
+    @Builder(toBuilder = true)
+    public CourtBundle(DocumentReference document,
+                       LocalDateTime dateTimeUploaded,
+                       String uploadedBy,
+                       String hearing) {
+        super.dateTimeUploaded = dateTimeUploaded;
+        super.uploadedBy = uploadedBy;
+        this.hearing = hearing;
+        this.document = document;
     }
 }
