cbdce55dbe4e4deae21a53581619cf7ad85e37fc
Toireasa Moley <toireasa.moley@hmcts.net>
2020-12-14 15:52:50 +0000

    Adding tests

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/NewJudicialMessageReplyEventHandlerTest.java
@@ -0,0 +1,57 @@
+package uk.gov.hmcts.reform.fpl.handlers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.fpl.events.NewJudicialMessageReplyEvent;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.JudicialMessage;
+import uk.gov.hmcts.reform.fpl.model.notify.NewJudicialMessageReplyTemplate;
+import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
+import uk.gov.hmcts.reform.fpl.service.email.content.JudicialMessageReplyContentProvider;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.reform.fpl.NotifyTemplates.NEW_JUDICIAL_MESSAGE_REPLY_TEMPLATE;
+import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.caseData;
+
+@ExtendWith(MockitoExtension.class)
+class NewJudicialMessageReplyEventHandlerTest {
+    @Mock
+    private NotificationService notificationService;
+
+    @Mock
+    private JudicialMessageReplyContentProvider newJudicialMessageReplyContentProvider;
+
+    @InjectMocks
+    private NewJudicialMessageReplyEventHandler newJudicialMessageReplyEventHandler;
+
+    @Test
+    void shouldNotifyJudicialMessageRecipientWhenNewJudicialMessageReplyCreated() {
+        String recipient = "David@fpla.com";
+
+        JudicialMessage judicialMessage = JudicialMessage.builder()
+            .sender("Paul@fpla.com")
+            .recipient(recipient)
+            .build();
+
+        CaseData caseData = caseData();
+
+        final NewJudicialMessageReplyTemplate expectedParameters = NewJudicialMessageReplyTemplate.builder().build();
+
+        given(newJudicialMessageReplyContentProvider.buildNewJudicialMessageReplyTemplate(caseData, judicialMessage))
+            .willReturn(expectedParameters);
+
+        newJudicialMessageReplyEventHandler.notifyJudicialMessageRecipientOfReply(
+            new NewJudicialMessageReplyEvent(caseData, judicialMessage)
+        );
+
+        verify(notificationService).sendEmail(
+            NEW_JUDICIAL_MESSAGE_REPLY_TEMPLATE,
+            recipient,
+            expectedParameters,
+            caseData.getId());
+    }
+}
