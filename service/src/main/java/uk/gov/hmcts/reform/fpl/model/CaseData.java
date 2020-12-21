f801804d909c89e4f3c5968ab053ef1b8d6c4113
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2020-12-21 12:02:22 +0000

    fix: allow adojurned hearing CMOs to be approved (#2019)
    Co-authored-by: tomaszpowroznik <tomas.powroznik@gmail.com>
    Co-authored-by: karthikkannankainos <43203806+karthikkannankainos@users.noreply.github.com>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -64,6 +64,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -558,6 +559,13 @@ public class CaseData {
     }
 
     @JsonIgnore
+    public List<Element<HearingBooking>> getAllHearings() {
+        return Stream.of(defaultIfNull(hearingDetails, new ArrayList<Element<HearingBooking>>()),
+            defaultIfNull(cancelledHearingDetails, new ArrayList<Element<HearingBooking>>()))
+            .flatMap(Collection::stream).collect(toList());
+    }
+
+    @JsonIgnore
     public List<Element<HearingBooking>> getPastHearings() {
         return defaultIfNull(hearingDetails, new ArrayList<Element<HearingBooking>>()).stream()
             .filter(hearingBooking -> !hearingBooking.getValue().startsAfterToday())
@@ -603,7 +611,7 @@ public class CaseData {
 
     @JsonIgnore
     public Optional<HearingBooking> getNextHearingAfterCmo(UUID cmoID) {
-        LocalDateTime currentCmoStartDate = unwrapElements(hearingDetails).stream()
+        LocalDateTime currentCmoStartDate = unwrapElements(getAllHearings()).stream()
             .filter(hearingBooking -> cmoID.equals(hearingBooking.getCaseManagementOrderId()))
             .map(HearingBooking::getStartDate)
             .findAny()
