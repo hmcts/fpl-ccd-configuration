65f55f2e6b156bb35c2d8ce165f3a37cfd658e34
nkoranne <62436878+nkoranne@users.noreply.github.com>
2021-01-04 16:43:18 +0000

    Dev/fpla 2288 : Remove Feature toggle for bulk user assignment  (#2037)
    * initial pass for removal of toggle
    
    * fix:FPLA-2288 fixed the failing test shouldThrowsExceptionWhenLocalAuthorityUsersCannotBeFetch by adding MockitoSettings
    
    * fix:FPLA-2288 addressed review comment, removed givenPRDWillAnswer
    
    Co-authored-by: Lewis Birks <lewisb@kainos.com>

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/LocalAuthorityUserServiceTest.java
@@ -1,17 +1,14 @@
 package uk.gov.hmcts.reform.fpl.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.fpl.enums.CaseRole;
-import uk.gov.hmcts.reform.fpl.request.RequestData;
 
 import java.util.Set;
 
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static uk.gov.hmcts.reform.fpl.enums.CaseRole.CREATOR;
@@ -21,42 +18,17 @@ import static uk.gov.hmcts.reform.fpl.enums.CaseRole.LASOLICITOR;
 class LocalAuthorityUserServiceTest {
 
     private static final String CASE_ID = "1";
-    private static final String USER_ID = "2";
     private static final String LOCAL_AUTHORITY = "example";
     private static final Set<CaseRole> CASE_ROLES = Set.of(LASOLICITOR, CREATOR);
 
     @Mock
-    private RequestData requestData;
-
-    @Mock
     private CaseRoleService caseRoleService;
-
-    @Mock
-    private FeatureToggleService featureToggleService;
 
     @InjectMocks
     private LocalAuthorityUserService localAuthorityUserService;
 
-    @BeforeEach
-    void setup() {
-        given(requestData.userId()).willReturn(USER_ID);
-    }
-
-    @Test
-    void shouldGrantCaseRolesToAllLocalAuthorityUsers() {
-        given(featureToggleService.isCaseUserBulkAssignmentEnabled()).willReturn(false);
-
-        localAuthorityUserService.grantUserAccessWithCaseRole(CASE_ID, LOCAL_AUTHORITY);
-
-        verify(caseRoleService).grantAccessToUser(CASE_ID, USER_ID, CASE_ROLES);
-        verify(caseRoleService).grantAccessToLocalAuthority(CASE_ID, LOCAL_AUTHORITY, CASE_ROLES, Set.of(USER_ID));
-        verifyNoMoreInteractions(caseRoleService);
-    }
-
     @Test
     void shouldGrantCaseRolesToAllLocalAuthorityUsersWithCaseAssignmentRoles() {
-        given(featureToggleService.isCaseUserBulkAssignmentEnabled()).willReturn(true);
-
         localAuthorityUserService.grantUserAccessWithCaseRole(CASE_ID, LOCAL_AUTHORITY);
 
         verify(caseRoleService).grantCaseAssignmentToLocalAuthority(CASE_ID, LOCAL_AUTHORITY, CASE_ROLES);
