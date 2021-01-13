796a6be406be32dd99da96d5d07a579a92f7bb02
Lewis Birks <lewisb@kainos.com>
2021-01-13 15:35:17 +0000

    preempt slow population of create case dropdown fields

--- e2e/pages/events/openApplicationEvent.page.js
+++ e2e/pages/events/openApplicationEvent.page.js
@@ -18,7 +18,9 @@ module.exports = {
     // wait until the dropdown is populated
     I.waitForElement(`${this.fields.jurisdiction} > option[value="${config.definition.jurisdiction}"]`, 30);
     I.selectOption(this.fields.jurisdiction, config.definition.jurisdictionFullDesc);
+    I.waitForElement(`${this.fields.caseType} > option[value="${config.definition.caseType}"]`, 30);
     I.selectOption(this.fields.caseType, config.definition.caseTypeFullDesc);
+    I.waitForElement(`${this.fields.event} > option[value="openCase"]`, 30);
     I.selectOption(this.fields.event, 'Start application');
     await I.retryUntilExists(() => I.goToNextPage(this.startButton), this.enterCaseNamePage.caseName);
     this.enterCaseName(caseName);
