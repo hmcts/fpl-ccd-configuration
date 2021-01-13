aac0ed88654fc6375c03fdf1bb78c78aafe8abe2
Lewis Birks <lewisb@kainos.com>
2021-01-13 11:57:22 +0000

    increase case list search timeouts, was being rather slow

--- e2e/pages/caseList.page.js
+++ e2e/pages/caseList.page.js
@@ -19,13 +19,13 @@ module.exports = {
   },
 
   changeStateFilter(desiredState) {
-    this.searchForCase(desiredState);
+    this.setInitialSearchFields(desiredState);
     I.click(this.fields.search);
   },
 
   async searchForCasesWithHandledEvidences(submittedAt, state = 'Any') {
-    this.searchForCase(state);
-    I.waitForElement(this.fields.evidenceHandled);
+    this.setInitialSearchFields(state);
+    I.waitForElement(this.fields.evidenceHandled, 30);
     await I.fillDate(submittedAt);
     I.click(this.fields.evidenceHandled);
     I.click(this.fields.search);
@@ -37,16 +37,16 @@ module.exports = {
   },
 
   searchForCasesWithName(caseName, state='Any') {
-    this.searchForCase(state);
+    this.setInitialSearchFields(state);
     // wait for our filters to load
-    I.waitForVisible(this.fields.caseName, 10);
+    I.waitForVisible(this.fields.caseName, 30);
     I.fillField(this.fields.caseName, caseName);
     I.click(this.fields.search);
   },
 
-  searchForCase(state='Any') {
+  setInitialSearchFields(state='Any') {
     // wait for initial filters to load
-    I.waitForVisible(this.fields.jurisdiction, 20);
+    I.waitForVisible(this.fields.jurisdiction, 30);
     I.selectOption(this.fields.jurisdiction, config.definition.jurisdictionFullDesc);
     I.selectOption(this.fields.caseType, config.definition.caseTypeFullDesc);
     I.selectOption(this.fields.caseState, state);
