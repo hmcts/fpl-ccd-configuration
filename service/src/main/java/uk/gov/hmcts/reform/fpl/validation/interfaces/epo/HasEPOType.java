33e98fe2afe53229c1dc0dd72d4aae4983ed3a5a
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2021-01-14 16:40:29 +0000

    impr: add epo exclusion information (FPLA-1106) (#2056)
    add epo exclusion information (FPLA-1106)

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/validation/interfaces/epo/HasEPOType.java
@@ -0,0 +1,19 @@
+package uk.gov.hmcts.reform.fpl.validation.interfaces.epo;
+
+import uk.gov.hmcts.reform.fpl.validation.validators.epo.HasEPOTypeValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = { HasEPOTypeValidator.class })
+public @interface HasEPOType {
+    String message() default "Select the type of EPO you need.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
