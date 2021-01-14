a9703866e7729aa55817576dd3c0dfc6fc9ff0be
Aliveni Choppa <aliveni.ch@gmail.com>
2021-01-14 15:13:06 +0000

    Add tests for judge message email notifications

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/handlers/NewJudicialMessageEventHandlerEmailTemplateTest.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.reform.fpl.handlers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.fpl.events.NewJudicialMessageEvent;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Respondent;
+import uk.gov.hmcts.reform.fpl.model.RespondentParty;
+import uk.gov.hmcts.reform.fpl.model.judicialmessage.JudicialMessage;
+import uk.gov.hmcts.reform.fpl.service.CaseUrlService;
+import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
+import uk.gov.hmcts.reform.fpl.service.email.content.JudicialMessageContentProvider;
+import uk.gov.hmcts.reform.fpl.testingsupport.email.EmailTemplateTest;
+
+import static uk.gov.hmcts.reform.fpl.testingsupport.email.EmailContent.emailContent;
+import static uk.gov.hmcts.reform.fpl.testingsupport.email.SendEmailResponseAssert.assertThat;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
+import static uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper.buildCallout;
+import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.getFirstRespondentLastName;
+
+@SpringBootTest(classes = {
+    NewJudicialMessageEventHandler.class,
+    NotificationService.class,
+    JudicialMessageContentProvider.class,
+    ObjectMapper.class,
+    CaseUrlService.class
+})
+public class NewJudicialMessageEventHandlerEmailTemplateTest extends EmailTemplateTest {
+
+    public static final Respondent RESPONDENT = Respondent.builder().party(RespondentParty.builder()
+        .lastName("Watson").build())
+        .build();
+
+    @Autowired
+    private NewJudicialMessageEventHandler underTest;
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldNotifyJudicialMessageRecipientWhenANewMessageIsSent(boolean withUrgency) {
+        CaseData caseData = CaseData.builder()
+            .id(123L)
+            .respondents1(wrapElements(RESPONDENT))
+            .build();
+
+        JudicialMessage judicialMessage = JudicialMessage.builder()
+            .sender("paul@fpla.com")
+            .recipient("david@fpla.com")
+            .urgency(withUrgency ? "High" : null)
+            .latestMessage("some query")
+            .build();
+
+        underTest.notifyJudicialMessageRecipient(new NewJudicialMessageEvent(caseData, judicialMessage));
+
+        assertThat(response())
+            .hasSubject("New message, " + getFirstRespondentLastName(caseData))
+            .hasBody(emailContent()
+                .start()
+                .line("You've received a message about:")
+                .line()
+                .line(buildCallout(caseData))
+                .line(" ")
+                .line("Enquiry from: paul@fpla.com")
+                .line(withUrgency ? "Response requested: High" : " ")
+                .line("Message: some query")
+                .line()
+                .line("To respond, sign in to:")
+                .line()
+                .line("http://fake-url/cases/case-details/123#JudicialMessagesTab")
+                .line()
+                .line("HM Courts & Tribunals Service")
+                .end("Do not reply to this email. If you need to contact us, "
+                    + "call 0330 808 4424 or email contactfpl@justice.gov.uk")
+            );
+    }
+}
