f6b29498159c94b78cbb5f2daacb6415ffe0fe4d
Joe Zazzaro-Francis <joez@kainos.com>
2021-01-20 17:49:13 +0000

    Suppress maximum inheritance warning

--- service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/UnknownLocalAuthorityDomainException.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/UnknownLocalAuthorityDomainException.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.exceptions;
 
+@SuppressWarnings("squid:MaximumInheritanceDept")
 public class UnknownLocalAuthorityDomainException extends LogAsWarningException {
     public UnknownLocalAuthorityDomainException(String message) {
         super("The email address was not linked to a known Local Authority", message);
