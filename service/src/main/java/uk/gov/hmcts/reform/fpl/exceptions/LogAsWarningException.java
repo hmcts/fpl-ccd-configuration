15afc4c422b45134f0f7540815ee98d7ee4f5fcb
Joe Zazzaro-Francis <joez@kainos.com>
2021-01-20 17:04:41 +0000

    Change abstract class constructor to protected

--- service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/LogAsWarningException.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/LogAsWarningException.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.reform.fpl.exceptions;
 
 public abstract class LogAsWarningException extends AboutToStartOrSubmitCallbackException {
-    public LogAsWarningException(String userMessage, String message) {
+    protected LogAsWarningException(String userMessage, String message) {
         super(userMessage, message);
     }
 }
