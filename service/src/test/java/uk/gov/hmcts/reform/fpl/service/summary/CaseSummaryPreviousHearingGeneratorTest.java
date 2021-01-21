b54a5b7e8f668df50a3d91ccd6644c3dddf3fdd0
Samuel Vrech <samuelzero@gmail.com>
2021-01-21 13:06:55 +0000

    FPLA-2170: Update summary tab (formerly overview) fix utest

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/summary/CaseSummaryPreviousHearingGeneratorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/summary/CaseSummaryPreviousHearingGeneratorTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.service.summary;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.fpl.enums.HearingType;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
@@ -12,12 +13,22 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
 
 class CaseSummaryPreviousHearingGeneratorTest {
 
+    private static final LocalDateTime NOW = LocalDateTime.of(2012, 10, 12, 13, 20, 44);
+
+    private final Time time = mock(Time.class);
+
     private final CaseSummaryPreviousHearingGenerator underTest =
-        new CaseSummaryPreviousHearingGenerator(mock(Time.class));
+        new CaseSummaryPreviousHearingGenerator(time);
+
+    @BeforeEach
+    void setUp() {
+        when(time.now()).thenReturn(NOW);
+    }
 
     @Test
     void testNoHearings() {
@@ -27,16 +38,21 @@ class CaseSummaryPreviousHearingGeneratorTest {
     }
 
     @Test
-    void testSingleHearings() {
+    void testSingleHearingsWithNoSealedCmo() {
         SyntheticCaseSummary actual = underTest.generate(CaseData.builder()
             .hearingDetails(List.of(
                 element(HearingBooking.builder()
                     .type(HearingType.CASE_MANAGEMENT)
-                    .startDate(LocalDateTime.of(2012, 10, 12, 13, 20, 44))
+                    .startDate(NOW.minusHours(1))
+                    .endDate(NOW.minusSeconds(1))
                     .build())
             ))
             .build());
 
-        assertThat(actual).isEqualTo(SyntheticCaseSummary.emptySummary());
+        assertThat(actual).isEqualTo(SyntheticCaseSummary.builder()
+            .caseSummaryHasPreviousHearing("Yes")
+            .caseSummaryPreviousHearingDate(NOW.toLocalDate())
+            .caseSummaryPreviousHearingType("Case management")
+            .build());
     }
 }
