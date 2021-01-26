1408d9a3dae75419f7ad735e2d6560453a32f491
Lewis Birks <lewisb@kainos.com>
2021-01-26 16:26:48 +0000

    cleanup

--- e2e/tests/smoke_test.js
+++ e2e/tests/smoke_test.js
@@ -4,11 +4,9 @@ const moment = require('moment');
 Feature('Smoke tests @smoke-tests');
 
 Scenario('Sign in as local authority and create a case', async ({I, caseListPage}) => {
-  // await I.goToPage(process.env.URL || 'http://localhost:3333');
   const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:mm:ss')})`;
   const caseId = await I.logInAndCreateCase(config.swanseaLocalAuthorityUserOne, caseName);
   I.navigateToCaseList();
-  // await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), caseListPage.locateCase(caseId), false, 20);
   caseListPage.searchForCasesWithName(caseName, 'Open');
   I.seeCaseInSearchResult(caseId);
 });
