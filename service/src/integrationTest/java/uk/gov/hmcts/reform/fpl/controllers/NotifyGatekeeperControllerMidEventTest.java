9e32af457d4da0330658de3e6e145659b47084c6
James Nelson <Jamesnelson117@gmail.com>
2021-01-28 15:34:01 +0000

    FPLA-2449: Added int test for notify gatekeeper mid event

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/NotifyGatekeeperControllerMidEventTest.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.common.EmailAddress;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+
+@ActiveProfiles("integration-test")
+@WebMvcTest(NotifyGatekeeperController.class)
+@OverrideAutoConfiguration(enabled = true)
+class NotifyGatekeeperControllerMidEventTest extends AbstractControllerTest {
+    NotifyGatekeeperControllerMidEventTest() {
+        super("notify-gatekeeper");
+    }
+
+    @Test
+    void shouldReturnErrorsWhenGatekeeperEmailsAreInValid() {
+        CaseData caseData = CaseData.builder()
+            .gatekeeperEmails(List.of(
+                element(EmailAddress.builder().email("email@example.com").build()),
+                element(EmailAddress.builder().email("<John Doe> johndoe@email.com").build()),
+                element(EmailAddress.builder().email("email@example.name").build()),
+                element(EmailAddress.builder().email("very.unusual.”@”.unusual.com@example.com").build())))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postMidEvent(asCaseDetails(caseData));
+
+        assertThat(callbackResponse.getErrors()).contains(
+            "Gatekeeper 2: Enter a valid email address",
+            "Gatekeeper 4: Enter a valid email address");
+    }
+
+    @Test
+    void shouldNotReturnErrorsWhenGatekeeperEmailsAreValid() {
+        CaseData caseData = CaseData.builder()
+            .gatekeeperEmails(List.of(
+                element(EmailAddress.builder().email("email@example.com").build()),
+                element(EmailAddress.builder().email("email@example.name").build())))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postMidEvent(asCaseDetails(caseData));
+
+        assertThat(callbackResponse.getErrors()).isNull();
+    }
+}
