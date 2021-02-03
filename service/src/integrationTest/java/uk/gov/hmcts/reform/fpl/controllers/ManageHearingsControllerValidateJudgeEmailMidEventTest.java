e718cf66733b4d6f74ae1f68f69760901079052d
Toireasa Moley <toireasa.moley@hmcts.net>
2021-02-03 16:40:11 +0000

    Adding tests for manage hearings

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ManageHearingsControllerValidateJudgeEmailMidEventTest.java
@@ -0,0 +1,66 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Judge;
+import uk.gov.hmcts.reform.fpl.model.common.JudgeAndLegalAdvisor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("integration-test")
+@OverrideAutoConfiguration(enabled = true)
+@WebMvcTest(ManageHearingsController.class)
+public class ManageHearingsControllerValidateJudgeEmailMidEventTest extends ManageHearingsControllerTest {
+
+    ManageHearingsControllerValidateJudgeEmailMidEventTest() {
+        super("manage-hearings");
+    }
+
+    @Test
+    void shouldNotReturnAValidationErrorWhenJudgeEmailIsValid() {
+        CaseData caseData = CaseData.builder()
+            .judgeAndLegalAdvisor(JudgeAndLegalAdvisor.builder()
+                .judgeEmailAddress("email@example.com")
+                .build())
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postMidEvent(asCaseDetails(caseData), "validate-judge-email");
+
+        assertThat(callbackResponse.getErrors()).isNull();
+    }
+
+    @Test
+    void shouldReturnAValidationErrorWhenJudgeEmailIsInvalid() {
+        CaseData caseData = CaseData.builder()
+            .judgeAndLegalAdvisor(JudgeAndLegalAdvisor.builder()
+                .judgeTitle(JudgeOrMagistrateTitle.HIS_HONOUR_JUDGE)
+                .judgeEmailAddress("<John Doe> johndoe@email.com")
+                .build())
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postMidEvent(asCaseDetails(caseData), "validate-judge-email");
+
+        assertThat(callbackResponse.getErrors()).contains(
+            "Enter an email address in the correct format, for example name@example.com");
+    }
+
+    @Test
+    void shouldNotReturnAValidationErrorWhenAllocatedJudgeIsUsed() {
+        CaseData caseData = CaseData.builder()
+            .allocatedJudge(Judge.builder()
+                .judgeEmailAddress("email@email.com")
+                .build())
+            .judgeAndLegalAdvisor(JudgeAndLegalAdvisor.builder()
+                .build())
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postMidEvent(asCaseDetails(caseData), "validate-judge-email");
+
+        assertThat(callbackResponse.getErrors()).isNull();
+    }
+}
