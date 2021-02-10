7924fce1333b89e5427ad1ba55beda42f4791023
Lewis Birks <lewisb@kainos.com>
2021-02-10 14:00:33 +0000

    fix models

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/HearingFurtherEvidenceBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/HearingFurtherEvidenceBundle.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.interfaces.ConfidentialBundle;
 
@@ -15,14 +16,28 @@ import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 
 @Data
 @Builder(toBuilder = true)
+@Jacksonized
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HearingFurtherEvidenceBundle implements ConfidentialBundle {
     private String hearingName;
     private List<Element<SupportingEvidenceBundle>> supportingEvidenceBundle;
 
+    /*
+    If want it the way listed in the docs
+    Add a json ignore to the getSupportingEvidenceBundle
+    add new getter that returns hmcts view (maybe as a new field):
+        all confidential
+    update getLABundle so that:
+        only confidential uploaded by la
+    update perms for all collections
+
+    have setters that merge everything, i.e. should just need to be the hmcts confidential docs view (all
+    confidential docs) and non confidential docs
+     */
+
     @Override
     public List<Element<SupportingEvidenceBundle>> getSupportingEvidenceBundle() {
-        return defaultIfNull(supportingEvidenceBundle, new ArrayList<>());
+        return defaultIfNull(this.supportingEvidenceBundle, new ArrayList<>());
     }
 
     @JsonGetter(value = "supportingEvidenceLA")
