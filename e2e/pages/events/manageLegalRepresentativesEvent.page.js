11e49ac72815f357802e87438c8f2eedf982c919
marianadpereira <mariana.pereira@hmcts.net>
2021-02-11 12:54:44 +0000

    .

--- e2e/pages/events/manageLegalRepresentativesEvent.page.js
+++ e2e/pages/events/manageLegalRepresentativesEvent.page.js
@@ -36,7 +36,7 @@ module.exports = {
     if(legalRepresentative.telephone) {
       I.fillField(this.fields(elementIndex).legalRepresentative.telephone, legalRepresentative.telephone);
     }
-    I.runAccessibility;
+    I.runAccessibilityTest();
   },
 
   async setRolePreferences(rolePreferences) {
--- e2e/pages/paymentHistory.page.js
+++ e2e/pages/paymentHistory.page.js
@@ -16,5 +16,6 @@ module.exports = {
     I.seeElement(paymentLocator);
     I.click(`${paymentLocator}/td/a`);
     I.seeElement(pbaLocator);
+    I.runAccessibilityTest();
   },
 };
