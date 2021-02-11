0321427d9611559e9a6eac04f1a7fd96e6b9e031
marianadpereira <mariana.pereira@hmcts.net>
2021-02-11 13:52:26 +0000

    fixed declaration of config file

--- e2e/helpers/JSWait.js
+++ e2e/helpers/JSWait.js
@@ -1,6 +1,6 @@
 const {getAccessibilityTestResult} = require('./accessibility/runner');
 const {generateAccessibilityReport} = require('../tests/reporter/customReporter');
-const testConfig = require('e2e/config.js');
+const testConfig = require('../config.js');
 
 class JSWait extends codecept_helper {
 
