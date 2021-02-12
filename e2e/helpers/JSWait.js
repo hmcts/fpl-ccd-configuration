0d84e951d17ccef5ff6943d8d821824a1e91e1c0
marianadpereira <mariana.pereira@hmcts.net>
2021-02-12 17:35:14 +0000

    ..

--- e2e/helpers/JSWait.js
+++ e2e/helpers/JSWait.js
@@ -1,6 +1,6 @@
 const {getAccessibilityTestResult} = require('./accessibility/runner');
 const {generateAccessibilityReport} = require('../tests/reporter/customReporter');
-const testConfig = require('../../config.js');
+const testConfig = require('../config.js');
 
 class JSWait extends Helper {
 
