ea44e81c48c9620f6bc2dadd39bb72b4581a2ac2
marianadpereira <mariana.pereira@hmcts.net>
2021-02-15 22:01:25 +0000

    adding little by little accessibility tests

--- e2e/pages/eventSummary.page.js
+++ e2e/pages/eventSummary.page.js
@@ -7,8 +7,8 @@ module.exports = {
     description: '#field-trigger-description',
   },
 
-  async provideSummary(summary, description) {
-    await I.runAccessibilityTest();
+  provideSummary(summary, description) {
+    //await I.runAccessibilityTest();
     I.fillField(this.fields.summary, summary);
     I.fillField(this.fields.description, description);
   },
