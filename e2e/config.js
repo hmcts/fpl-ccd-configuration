cc798f89342f54fa145a0e69a3877f501a50511e
marianadpereira <mariana.pereira@hmcts.net>
2021-02-22 16:06:35 +0000

    reverting TESTS_FOR_ACCESSIBILITY

--- e2e/config.js
+++ e2e/config.js
@@ -83,7 +83,7 @@ module.exports = {
   mockedPayment: (process.env.MOCKED_PAYMENTS || 'true').toLowerCase() === 'true',
   ctscEmail: process.env.CTSC_EMAIL || 'FamilyPublicLaw+ctsc@gmail.com',
   TestOutputDir: process.env.E2E_OUTPUT_DIR || './output',
-  TestForAccessibility: process.env.SKIP_FOR_ACCESSIBILITY === 'true',
+  TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
   definition: {
     jurisdiction: 'PUBLICLAW',
     jurisdictionFullDesc: 'Public Law',
