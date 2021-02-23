b7367e161565f2bb05c1bde6d3403751958075e9
Lewis Birks <lewisb@kainos.com>
2021-02-23 12:18:55 +0000

    remove use in MigrateCaseController

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -13,22 +13,11 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.order.HearingOrder;
-import uk.gov.hmcts.reform.fpl.service.document.ConfidentialDocumentsSplitter;
 import uk.gov.hmcts.reform.fpl.service.removeorder.SealedCMORemovalAction;
 
-import java.util.List;
-import java.util.Map;
-
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentLAService.CORRESPONDING_DOCUMENTS_COLLECTION_LA_KEY;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentLAService.FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_LA_KEY;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService.C2_DOCUMENTS_COLLECTION_KEY;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService.CORRESPONDING_DOCUMENTS_COLLECTION_KEY;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService.FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_KEY;
-import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService.HEARING_FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_KEY;
 
 @Api
 @RestController
@@ -37,7 +26,6 @@ import static uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService.HEA
 @Slf4j
 public class MigrateCaseController extends CallbackController {
     private static final String MIGRATION_ID_KEY = "migrationId";
-    private final ConfidentialDocumentsSplitter splitter;
     private final SealedCMORemovalAction sealedCMORemovalAction;
 
     @PostMapping("/about-to-submit")
@@ -57,45 +45,8 @@ public class MigrateCaseController extends CallbackController {
             run2706(caseDetails);
         }
 
-        if ("FPLA-2722".equals(migrationId)) {
-            log.info("Performing migration ({}) for case {}", migrationId, caseDetails.getId());
-            run2417(caseDetails);
-        }
-
         caseDetails.getData().remove(MIGRATION_ID_KEY);
         return respond(caseDetails);
-    }
-
-    private void run2417(CaseDetails caseDetails) {
-        CaseData caseData = getCaseData(caseDetails);
-
-        caseDetails.getData().put(
-            HEARING_FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_KEY, caseData.getHearingFurtherEvidenceDocuments()
-        );
-        caseDetails.getData().put(
-            C2_DOCUMENTS_COLLECTION_KEY, caseData.getC2DocumentBundle()
-        );
-        caseDetails.getData().putAll(updateSupportingDocs(
-            caseData.getFurtherEvidenceDocuments(), FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_KEY
-        ));
-        caseDetails.getData().putAll(updateSupportingDocs(
-            caseData.getFurtherEvidenceDocumentsLA(), FURTHER_EVIDENCE_DOCUMENTS_COLLECTION_LA_KEY
-        ));
-        caseDetails.getData().putAll(updateSupportingDocs(
-            caseData.getCorrespondenceDocuments(), CORRESPONDING_DOCUMENTS_COLLECTION_KEY
-        ));
-        caseDetails.getData().putAll(updateSupportingDocs(
-            caseData.getCorrespondenceDocumentsLA(), CORRESPONDING_DOCUMENTS_COLLECTION_LA_KEY
-        ));
-    }
-
-    private Map<String, Object> updateSupportingDocs(List<Element<SupportingEvidenceBundle>> supportingDocs,
-                                                     String key) {
-        if (isEmpty(supportingDocs)) {
-            return Map.of();
-        }
-
-        return splitter.splitIntoAllAndNonConfidential(supportingDocs, key);
     }
 
     private void run2706(CaseDetails caseDetails) {
