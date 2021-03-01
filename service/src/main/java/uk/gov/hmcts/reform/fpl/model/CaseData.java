d9d0b4f698c6fad8b7f54cc5808e740d2107fb27
Soorya <soorya11@hotmail.co.uk>
2021-03-01 13:59:21 +0000

    * Changes "Organisation's details" to "Applicant's details"  * Minor changes to hint text for Applicant's details section

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -135,9 +135,9 @@ public class CaseData {
     @NotNull(message = "Add the grounds for the application", groups = EPOGroup.class)
     @Valid
     private final GroundsForEPO groundsForEPO;
-    @NotEmpty(message = "Add your organisation's details")
+    @NotEmpty(message = "Add the applicant's details")
     @Valid
-    private final List<@NotNull(message = "Add your organisation's details") Element<Applicant>> applicants;
+    private final List<@NotNull(message = "Add the applicant's details") Element<Applicant>> applicants;
 
     @Valid
     @NotEmpty(message = "Add the respondents' details")
