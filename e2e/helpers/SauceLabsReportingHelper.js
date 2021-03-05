f5ad9f21bea27764876ecb843ca903cee2c14f5f
Rob Doherty <robdohertykelly@gmail.com>
2021-03-05 15:29:14 +0000

    TA-453: fix crossbrowser SauceLabsReportingHelper.js job-name

--- e2e/helpers/SauceLabsReportingHelper.js
+++ e2e/helpers/SauceLabsReportingHelper.js
@@ -5,7 +5,7 @@ const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
-  console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=probate-frontend');
+  console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=fpl-ccd-configuration');
   return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
