a643b880b9ace7c9cbb4ac536546c3d9089ea74f
Toireasa Moley <toireasa.moley@hmcts.net>
2021-03-11 11:13:50 +0000

    Adding tests

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/SupplementsBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/SupplementsBundle.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
+import uk.gov.hmcts.reform.fpl.enums.Supplements;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 
 import java.time.LocalDateTime;
@@ -13,7 +14,7 @@ import java.time.LocalDateTime;
 @Jacksonized
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class SupplementsBundle {
-    private final String name;
+    private final Supplements name;
     private final String notes;
     private LocalDateTime dateTimeUploaded;
     private final DocumentReference document;
