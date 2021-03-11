b8a4dc90b30bfc03cdfa31b074adaba2f25ac295
Soorya <soorya11@hotmail.co.uk>
2021-03-11 16:34:14 +0000

    * Initial commit

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/C2UploadedEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/C2UploadedEmailContentProvider.java
@@ -9,11 +9,8 @@ import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.notify.BaseCaseNotifyData;
 import uk.gov.hmcts.reform.fpl.model.notify.allocatedjudge.AllocatedJudgeTemplateForC2;
 import uk.gov.hmcts.reform.fpl.model.notify.c2uploaded.C2UploadedTemplate;
-import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
 import uk.gov.hmcts.reform.fpl.service.email.content.base.AbstractEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.time.Time;
-
-import java.util.List;
 
 import static uk.gov.hmcts.reform.fpl.enums.TabUrlAnchor.C2;
 import static uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper.buildCallout;
@@ -24,24 +21,16 @@ import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.getFirstResponden
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class C2UploadedEmailContentProvider extends AbstractEmailContentProvider {
-    private final FeatureToggleService featureToggleService;
+
     private final Time time;
 
     public C2UploadedTemplate getNotifyData(final CaseData caseData, final DocumentReference latestC2) {
-        List<String> applicationTypes;
-
-        if (featureToggleService.isUploadAdditionalApplicationsEnabled()) {
-            applicationTypes = caseData.getLastC2DocumentBundle().getApplicationTypes();
-        } else {
-            applicationTypes = null;
-        }
-
         return C2UploadedTemplate.builder()
             .callout(buildCallout(caseData))
             .respondentLastName(getFirstRespondentLastName(caseData.getRespondents1()))
             .caseUrl(getCaseUrl(caseData.getId(), C2))
             .documentUrl(getDocumentUrl(latestC2))
-            .applicationTypes(applicationTypes)
+            .applicationTypes(caseData.getLastC2DocumentBundle().getApplicationTypes())
             .build();
     }
 
