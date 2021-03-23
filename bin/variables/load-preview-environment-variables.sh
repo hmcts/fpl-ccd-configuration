072100ade8466f99885a4eae38ca41d4810b774b
Tomasz Powroznik <tomas.powroznik@gmail.com>
2021-03-23 17:08:28 +0000

    test

--- bin/variables/load-preview-environment-variables.sh
+++ bin/variables/load-preview-environment-variables.sh
@@ -7,9 +7,9 @@ pr=${1}
 echo 'export ENVIRONMENT=preview'
 
 # urls
-echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
-echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
-echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
+echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-demo.service.core-compute-demo.internal"
+echo "export IDAM_API_BASE_URL=https://idam-api.demo.platform.hmcts.net"
+echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-demo.service.core-compute-demo.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://ccd-definition-store-fpl-case-service-pr-${pr}.service.core-compute-preview.internal"
 
 # definition placeholders
