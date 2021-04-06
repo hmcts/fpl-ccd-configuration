22c0ebb8a8d87203f431b0ba740d47a1e12a2096
Soorya <soorya11@hotmail.co.uk>
2021-04-06 13:10:44 +0100

    Initial commit to share some code

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/notify/submittedcase/RespondentSolicitorTemplate.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.fpl.model.notify.submittedcase;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import uk.gov.hmcts.reform.fpl.model.notify.SharedNotifyTemplate;
+
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder(toBuilder = true)
+@Data
+public class RespondentSolicitorTemplate extends SharedNotifyTemplate {
+    private String representative;
+    private String localAuthority;
+}
---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/RespondentSolicitorContentProvider.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.fpl.service.email.content;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.RespondentSolicitor;
+import uk.gov.hmcts.reform.fpl.model.notify.submittedcase.RespondentSolicitorTemplate;
+import uk.gov.hmcts.reform.fpl.service.email.content.base.SharedNotifyContentProvider;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+
+public class RespondentSolicitorContentProvider extends SharedNotifyContentProvider {
+
+    public RespondentSolicitorTemplate buildNotifyRespondentSolicitorTemplate(CaseData caseData, RespondentSolicitor representative) {
+        return RespondentSolicitorTemplate.builder()
+            .representative(representative.getFirstName() + " " + representative.getLastName())
+            .localAuthority(caseData.getCaseLocalAuthorityName())
+            .build();
+    }
+}
