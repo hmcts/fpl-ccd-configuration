b8671929c1a2d50ec413bfcd296aff35b30f70e5
Joe Zazzaro-Francis <joez@kainos.com>
2021-04-06 12:44:50 +0100

    Fix e2e checkstyle

--- e2e/fixtures/respondents.js
+++ e2e/fixtures/respondents.js
@@ -34,7 +34,7 @@ module.exports = [
         },
         town: 'Reading',
         postcode: 'RG4 7AA',
-      }
+      },
     },
   },
   {
@@ -87,7 +87,7 @@ module.exports = [
       lastName: 'Bear',
       email: 'rupert@bear.com',
       unregisteredOrganisation: {
-        name: "Swansea Local Branch",
+        name: 'Swansea Managing Office',
         address: {
           lookupOption: 'The Tower, Trawler Rd, Maritime Quarter, Swansea',
           buildingAndStreet: {
