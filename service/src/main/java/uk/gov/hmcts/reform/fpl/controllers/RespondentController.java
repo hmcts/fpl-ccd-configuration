aaa0a4b8bf41a47eec9e00fb979d3903def899e5
Joe Zazzaro-Francis <joez@kainos.com>
2021-04-06 16:14:12 +0100

    re-add uncommented method (it is required)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RespondentController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RespondentController.java
@@ -66,7 +66,7 @@ public class RespondentController extends CallbackController {
         List<String> combinedValidationErrors = Stream.concat(emailErrors.stream(), futureDOBErrors.stream())
             .collect(Collectors.toList());
 
-        //caseDetails.getData().put(RESPONDENTS_KEY, respondentService.removeHiddenFields(caseData.getRespondents1()));
+        caseDetails.getData().put(RESPONDENTS_KEY, respondentService.removeHiddenFields(caseData.getRespondents1()));
         return respond(caseDetails, combinedValidationErrors);
     }
 
@@ -83,13 +83,10 @@ public class RespondentController extends CallbackController {
         // can either do before or after but have to update case details manually either way as if there is no
         // confidential info then caseDetails won't be updated in the confidential details method and as such just
         // passing the updated list to the method won't work
-        caseDetails.getData().put(RESPONDENTS_KEY, respondentService.persistRepresentativesRelationship(
-            caseData.getAllRespondents(), caseDataBefore.getAllRespondents()
-        ));
+        List<Element<Respondent>> respondents = respondentService.persistRepresentativesRelationship(
+            caseData.getAllRespondents(), caseDataBefore.getAllRespondents());
 
-        //caseData = getCaseData(caseDetails);
-
-        //caseDetails.getData().put(RESPONDENTS_KEY, respondentService.removeHiddenFields(caseData.getRespondents1()));
+        caseDetails.getData().put(RESPONDENTS_KEY, respondentService.removeHiddenFields(respondents));
         return respond(caseDetails);
     }
 
