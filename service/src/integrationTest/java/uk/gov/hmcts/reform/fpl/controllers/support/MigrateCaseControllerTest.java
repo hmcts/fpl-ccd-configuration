24aa48ee6b454a401b5f595743e54ffda8900dff
Imran Saeed <muhamed.imran@hmcts.net>
2021-04-08 11:58:33 +0100

    bugfix:fpla-2947

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -14,6 +14,7 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.List;
 import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
