9ccd7694dbacec0257195638f9382ba3ce3185da
Imran Saeed <77970048+imran-hmcts@users.noreply.github.com>
2021-04-08 08:33:51 +0100

    chore:NOJIRA remove code for fpla-2885 migration (#2467)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -12,10 +12,6 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.service.CaseAccessService;
-
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Api
 @RestController
@@ -24,44 +20,13 @@ import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 @Slf4j
 public class MigrateCaseController extends CallbackController {
     private static final String MIGRATION_ID_KEY = "migrationId";
-    private static final String FMN_ERROR_MESSAGE = "Unexpected FMN ";
-
-    private final CaseAccessService caseAccessService;
 
     @PostMapping("/about-to-submit")
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         Object migrationId = caseDetails.getData().get(MIGRATION_ID_KEY);
 
-        if ("FPLA-2885".equals(migrationId)) {
-            run2885(caseDetails);
-        }
-
         caseDetails.getData().remove(MIGRATION_ID_KEY);
         return respond(caseDetails);
-    }
-
-    private void run2885(CaseDetails caseDetails) {
-        CaseData caseData = getCaseData(caseDetails);
-
-        if (isEmpty(caseData.getCancelledHearingDetails())) {
-            throw new IllegalArgumentException("Case does not contain cancelled hearing bookings");
-        }
-
-        caseData.getCancelledHearingDetails().forEach(hearingBookingElement -> {
-            switch (hearingBookingElement.getValue().getCancellationReason()) {
-                case "OT8":
-                    hearingBookingElement.getValue().setCancellationReason("IN1");
-                    break;
-                case "OT9":
-                    hearingBookingElement.getValue().setCancellationReason("OT8");
-                    break;
-                case "OT10":
-                    hearingBookingElement.getValue().setCancellationReason("OT9");
-                    break;
-            }
-        });
-
-        caseDetails.getData().put("cancelledHearingDetails", caseData.getCancelledHearingDetails());
     }
 }
