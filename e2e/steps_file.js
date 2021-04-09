a699e0237b30eed0f4a8054f42fd79806a81cdb6
Joe Z-F <joe.zazzaro-francis@hmcts.net>
2021-04-09 14:44:33 +0100

    feat: respondent journey (FPLA-2778/9) (#2383)
    * initial work on removing fields + adding new respondent solicitor section
    
    * ccd toggle + show hide conditions based on org selection
    
    * Updated leaf name
    
    * all fields optional from CCD def, added java models, started e2e test update
    
    * small e2e updates
    
    * remove import
    
    * working e2e flow (xpath could be improved)
    
    * fix case submission mid event validation test
    
    * updated e2e xpath
    
    * fix respondentcheck tests
    
    * rework of unregistered organisation input
    
    * fix validation NPE
    
    * fix respondent checker tests
    
    * fix code review comments + e2e tests + email field not-null validation
    
    * added extra test for respondent checker
    
    * Added unit tests for respondent validation
    
    * fix respondent checker test
    
    * seeTextInTab not working for organisation
    
    * updated complex type labels + added comment for e2e problem
    
    * Adding validation for outlook emails case and changing messages
    
    * Tidy up validation
    
    * Adding validation to respondent controller
    
    * Fixing issue with validation
    
    * Removing organisation from e2e
    
    * Adding tests to controller
    
    * Fixing checkstyle
    
    * Removing comment
    
    * use case data instead of map
    
    * remove header+label fields for regional office
    
    * fix failing returned application e2e
    
    * remove fields that are hidden by user change of mind
    
    * fix failing unit tests due to NPE
    
    * Added managing office to e2e flow for respondent journey
    
    * Fix e2e checkstyle
    
    * fix code smell
    
    * commented out hidden value method, for testing
    
    * re-add uncommented method (it is required)
    
    * impr: respondent solicitor in tab (FPLA-2854) (#2431)
    
    * Updated label for unregistered organisation and added e2e checks for managing office + unregistered org
    
    * Added feature toggle + tests for it
    
    * Add validation for no more than 10 respondents
    
    Co-authored-by: toireasam <toireasa.moley@hmcts.net>
    Co-authored-by: Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
    Co-authored-by: Lewis Birks <lewisb@kainos.com>
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>
    Co-authored-by: Rob Doherty-Kelly <Rob-Doherty@users.noreply.github.com>

--- e2e/steps_file.js
+++ e2e/steps_file.js
@@ -173,8 +173,32 @@ module.exports = function () {
       return `${selector}//*[contains(@class,"complex-panel-simple-field") and .//th/span[text()="${fieldName}"]]`;
     },
 
+    organisationTabFieldSelector(pathToField) {
+      let path = [].concat(pathToField);
+      let fieldName = path.splice(-1, 1)[0];
+      let selector = '//mat-tab-body';
+
+      path.forEach(step => {
+        selector = `${selector}//*[@class="complex-panel" and .//*[@class="complex-panel-title" and .//*[text()="${step}"]]]`;
+      }, this);
+
+      return `${selector}//*[contains(@class,"complex-panel-compound-field") and ..//*[text()="${fieldName}:"]]`;
+    },
+
     seeInTab(pathToField, fieldValue) {
       const fieldSelector = this.tabFieldSelector(pathToField);
+
+      if (Array.isArray(fieldValue)) {
+        fieldValue.forEach((value, index) => {
+          this.seeElement(locate(`${fieldSelector}//tr[${index + 1}]`).withText(value));
+        });
+      } else {
+        this.seeElement(locate(fieldSelector).withText(fieldValue));
+      }
+    },
+
+    seeOrganisationInTab(pathToField, fieldValue) {
+      const fieldSelector = this.organisationTabFieldSelector(pathToField);
 
       if (Array.isArray(fieldValue)) {
         fieldValue.forEach((value, index) => {
