ecca51b8759abcaf87f9f7710a297f1d40ccc855
Imran Saeed <muhamed.imran@hmcts.net>
2021-04-15 17:45:03 +0100

    bugfix:fpla-2982 migration logic for missing Id

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -12,6 +12,14 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
+import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Api
 @RestController
@@ -26,7 +34,59 @@ public class MigrateCaseController extends CallbackController {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         Object migrationId = caseDetails.getData().get(MIGRATION_ID_KEY);
 
+        if ("FPLA-2982".equals(migrationId)) {
+            run2982(caseDetails);
+        }
+
         caseDetails.getData().remove(MIGRATION_ID_KEY);
         return respond(caseDetails);
+    }
+
+    private void run2982(CaseDetails caseDetails) {
+        CaseData caseData = getCaseData(caseDetails);
+        List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle =
+            caseData.getAdditionalApplicationsBundle();
+
+        if (additionalApplicationsBundle.stream()
+            .noneMatch(bundle -> bundle.getValue().getC2DocumentBundle().getId() == null)) {
+            throw new IllegalArgumentException("No c2DocumentBundle found with missing Id");
+        }
+
+        List<Element<AdditionalApplicationsBundle>> fixedAdditionalApplicationsBundle =
+            additionalApplicationsBundle.stream().map(this::fixMissingId).collect(Collectors.toList());
+        caseDetails.getData().put("additionalApplicationsBundle", fixedAdditionalApplicationsBundle);
+    }
+
+    private Element<AdditionalApplicationsBundle> fixMissingId(Element<AdditionalApplicationsBundle> bundle) {
+        C2DocumentBundle c2DocumentBundle = bundle.getValue().getC2DocumentBundle();
+
+        if (c2DocumentBundle.getId() == null) {
+
+            /*
+            C2DocumentBundle fixedC2DocumentBundle = C2DocumentBundle.builder()
+                .id(UUID.randomUUID())
+                .author(c2DocumentBundle.getAuthor())
+                .c2AdditionalOrdersRequested(c2DocumentBundle.getC2AdditionalOrdersRequested())
+                .clientCode(c2DocumentBundle.getClientCode())
+                .description(c2DocumentBundle.getDescription())
+                .document(c2DocumentBundle.getDocument())
+                .fileReference(c2DocumentBundle.getFileReference())
+                .nameOfRepresentative(c2DocumentBundle.getNameOfRepresentative())
+                .parentalResponsibilityType(c2DocumentBundle.getParentalResponsibilityType())
+                .pbaNumber(c2DocumentBundle.getPbaNumber())
+                .supplementsBundle(c2DocumentBundle.getSupplementsBundle())
+                .supportingEvidenceBundle(c2DocumentBundle.getSupportingEvidenceBundle())
+                .type(c2DocumentBundle.getType())
+                .uploadedDateTime(c2DocumentBundle.getUploadedDateTime())
+                .usePbaPayment(c2DocumentBundle.getUsePbaPayment())
+                .build();
+
+                bundle.getValue().setC2DocumentBundle(fixedC2DocumentBundle);
+             */
+
+            bundle.getValue().getC2DocumentBundle().setId(UUID.randomUUID());
+        }
+
+        return bundle;
     }
 }
