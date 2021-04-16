efce6e6622b8f56aeddab1c1b41bb5a9c795196d
Toireasa Moley <toireasa.moley@hmcts.net>
2021-04-16 11:01:35 +0100

    changing inversion

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendDocumentService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendDocumentService.java
@@ -80,13 +80,13 @@ public class SendDocumentService {
     private List<Recipient> getNotRepresentedRespondents(CaseData caseData) {
         return unwrapElements(caseData.getRespondents1()).stream()
             .filter(respondent -> ObjectUtils.isEmpty(respondent.getRepresentedBy())
-                && !hasLegalRepresentation(respondent))
+                && hasNoLegalRepresentation(respondent))
             .map(Respondent::getParty)
             .collect(toList());
     }
 
-    private boolean hasLegalRepresentation(Respondent respondent) {
-        return YES.getValue().equals(respondent.getLegalRepresentation());
+    private boolean hasNoLegalRepresentation(Respondent respondent) {
+        return !YES.getValue().equals(respondent.getLegalRepresentation());
     }
 
 }
