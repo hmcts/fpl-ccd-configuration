dbb13012f5ad3bbbf4111e6cacbbdd14dbe2ec79
Imran Saeed <muhamed.imran@hmcts.net>
2021-04-19 08:56:29 +0100

    bugfix:fpla-2982

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.fpl.controllers.support;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
@@ -29,11 +31,28 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
     class Fpla2982 {
         String migrationId = "FPLA-2982";
 
-        @Test
-        void shouldMigrateMissingC2IdCase() {
+        @ParameterizedTest
+        @ValueSource(longs = {
+            1598429153622508L,
+            1615191831533551L,
+            1594384486007055L,
+            1601977974423857L,
+            1615571327261140L,
+            1615476016828466L,
+            1616507805759840L,
+            1610015759403189L,
+            1615994076934396L,
+            1611613172339094L,
+            1612440806991994L,
+            1607004182103389L,
+            1617045146450299L,
+            1612433400114865L,
+            1615890702114702L,
+            1610018233059619L})
+        void shouldMigrateMissingC2IdCase(Long caseId) {
             CaseDetails caseDetails = caseDetails(migrationId,
                 wrapElements(createAdditionalApplicationBundle(createC2DocumentBundle(null)))
-                , 1234567L);
+                , caseId);
 
             CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
 
@@ -44,14 +63,41 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
         }
 
         @Test
+        void shouldThrowExceptionForInvalidCaseId() {
+            CaseDetails caseDetails = caseDetails(migrationId,
+                wrapElements(createAdditionalApplicationBundle(createC2DocumentBundle(UUID.randomUUID())))
+                , 1234L);
+
+            assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
+                .getRootCause()
+                .hasMessage("Invalid case Id");
+        }
+
+        @Test
         void shouldThrowExceptionIfNoC2WithNullIdFound() {
             CaseDetails caseDetails = caseDetails(migrationId,
                 wrapElements(createAdditionalApplicationBundle(createC2DocumentBundle(UUID.randomUUID())))
-                , 7891L);
+                , 1601977974423857L);
 
             assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
                 .getRootCause()
                 .hasMessage("No c2DocumentBundle found with missing Id");
+        }
+
+        @Test
+        void shouldNotChangeCaseIfNotExpectedMigrationId() {
+            String incorrectMigrationId = "FPLA-9876";
+
+            CaseDetails caseDetails = caseDetails(incorrectMigrationId,
+                wrapElements(createAdditionalApplicationBundle(createC2DocumentBundle(null)))
+                , 1615890702114702L);
+
+            CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
+
+            assertThat(!extractedCaseData.getAdditionalApplicationsBundle().isEmpty());
+
+            extractedCaseData.getAdditionalApplicationsBundle()
+                .forEach(bundle -> assertThat(bundle.getValue().getC2DocumentBundle().getId() == null));
         }
 
         private C2DocumentBundle createC2DocumentBundle(UUID id) {
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -43,7 +43,31 @@ public class MigrateCaseController extends CallbackController {
     }
 
     private void run2982(CaseDetails caseDetails) {
+        final List<Long> validCases = List.of(
+            1598429153622508L,
+            1615191831533551L,
+            1594384486007055L,
+            1601977974423857L,
+            1615571327261140L,
+            1615476016828466L,
+            1616507805759840L,
+            1610015759403189L,
+            1615994076934396L,
+            1611613172339094L,
+            1612440806991994L,
+            1607004182103389L,
+            1617045146450299L,
+            1612433400114865L,
+            1615890702114702L,
+            1610018233059619L
+        );
+
         CaseData caseData = getCaseData(caseDetails);
+
+        if (!validCases.contains(caseData.getId())) {
+            throw new IllegalArgumentException("Invalid case Id");
+        }
+
         List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle =
             caseData.getAdditionalApplicationsBundle();
 
@@ -61,29 +85,6 @@ public class MigrateCaseController extends CallbackController {
         C2DocumentBundle c2DocumentBundle = bundle.getValue().getC2DocumentBundle();
 
         if (c2DocumentBundle.getId() == null) {
-
-            /*
-            C2DocumentBundle fixedC2DocumentBundle = C2DocumentBundle.builder()
-                .id(UUID.randomUUID())
-                .author(c2DocumentBundle.getAuthor())
-                .c2AdditionalOrdersRequested(c2DocumentBundle.getC2AdditionalOrdersRequested())
-                .clientCode(c2DocumentBundle.getClientCode())
-                .description(c2DocumentBundle.getDescription())
-                .document(c2DocumentBundle.getDocument())
-                .fileReference(c2DocumentBundle.getFileReference())
-                .nameOfRepresentative(c2DocumentBundle.getNameOfRepresentative())
-                .parentalResponsibilityType(c2DocumentBundle.getParentalResponsibilityType())
-                .pbaNumber(c2DocumentBundle.getPbaNumber())
-                .supplementsBundle(c2DocumentBundle.getSupplementsBundle())
-                .supportingEvidenceBundle(c2DocumentBundle.getSupportingEvidenceBundle())
-                .type(c2DocumentBundle.getType())
-                .uploadedDateTime(c2DocumentBundle.getUploadedDateTime())
-                .usePbaPayment(c2DocumentBundle.getUsePbaPayment())
-                .build();
-
-                bundle.getValue().setC2DocumentBundle(fixedC2DocumentBundle);
-             */
-
             bundle.getValue().getC2DocumentBundle().setId(UUID.randomUUID());
         }
 
