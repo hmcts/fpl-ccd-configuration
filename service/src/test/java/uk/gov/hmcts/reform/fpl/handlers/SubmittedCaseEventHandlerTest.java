b0a7903f1d72c4c2ff5a4659e41de778991e91f7
samuelzerozero <50642657+samuelzerozero@users.noreply.github.com>
2021-04-23 14:42:55 +0100

    FPLA-2834: Limit Add & Edit on submission for 'Respondent Details' (#2494)

--- service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/SubmittedCaseEventHandlerTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/SubmittedCaseEventHandlerTest.java
@@ -6,13 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.ccd.model.Organisation;
 import uk.gov.hmcts.reform.fnp.exception.PaymentsApiException;
 import uk.gov.hmcts.reform.fnp.exception.RetryablePaymentException;
 import uk.gov.hmcts.reform.fpl.config.CafcassLookupConfiguration;
@@ -20,39 +17,28 @@ import uk.gov.hmcts.reform.fpl.enums.State;
 import uk.gov.hmcts.reform.fpl.events.FailedPBAPaymentEvent;
 import uk.gov.hmcts.reform.fpl.events.SubmittedCaseEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Respondent;
-import uk.gov.hmcts.reform.fpl.model.RespondentSolicitor;
-import uk.gov.hmcts.reform.fpl.model.UnregisteredOrganisation;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
-import uk.gov.hmcts.reform.fpl.model.notify.submittedcase.RespondentSolicitorTemplate;
 import uk.gov.hmcts.reform.fpl.model.notify.submittedcase.SubmitCaseCafcassTemplate;
 import uk.gov.hmcts.reform.fpl.model.notify.submittedcase.SubmitCaseHmctsTemplate;
 import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
 import uk.gov.hmcts.reform.fpl.service.email.content.CafcassEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.service.email.content.HmctsEmailContentProvider;
-import uk.gov.hmcts.reform.fpl.service.email.content.RespondentSolicitorContentProvider;
 import uk.gov.hmcts.reform.fpl.service.payment.PaymentService;
 
-import static java.util.Collections.emptyList;
 import static org.junit.jupiter.params.provider.EnumSource.Mode.EXCLUDE;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.CAFCASS_SUBMISSION_TEMPLATE;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.HMCTS_COURT_SUBMISSION_TEMPLATE;
-import static uk.gov.hmcts.reform.fpl.NotifyTemplates.REGISTERED_RESPONDENT_SUBMISSION_TEMPLATE;
-import static uk.gov.hmcts.reform.fpl.NotifyTemplates.UNREGISTERED_RESPONDENT_SOLICICTOR;
 import static uk.gov.hmcts.reform.fpl.enums.ApplicationType.C110A_APPLICATION;
 import static uk.gov.hmcts.reform.fpl.enums.State.OPEN;
 import static uk.gov.hmcts.reform.fpl.enums.State.RETURNED;
 import static uk.gov.hmcts.reform.fpl.enums.State.SUBMITTED;
-import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_CODE;
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.caseData;
-import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.assertions.AnnotationAssertion.assertClass;
 
 @ExtendWith(SpringExtension.class)
@@ -72,9 +58,6 @@ class SubmittedCaseEventHandlerTest {
 
     @Mock
     private CafcassEmailContentProvider cafcassEmailContentProvider;
-
-    @Mock
-    private RespondentSolicitorContentProvider respondentSolicitorContentProvider;
 
     @Mock
     private ApplicationEventPublisher applicationEventPublisher;
@@ -128,197 +111,6 @@ class SubmittedCaseEventHandlerTest {
             expectedEmail,
             expectedTemplate,
             caseData.getId());
-    }
-
-    @Test
-    void shouldSendEmailToUnregisteredSolicitor() {
-        final String expectedEmail = "test@test.com";
-        final CaseData caseDataBefore = caseData();
-
-        final CaseData caseData = CaseData.builder().respondents1(
-            wrapElements(Respondent.builder()
-                .legalRepresentation(YES.getValue())
-                .solicitor(RespondentSolicitor.builder()
-                    .email(expectedEmail)
-                    .unregisteredOrganisation(UnregisteredOrganisation.builder().name("Unregistered Org Name").build())
-                    .build()).build())
-        ).build();
-
-        final RespondentSolicitorTemplate expectedTemplate = RespondentSolicitorTemplate.builder().build();
-        final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-        when(respondentSolicitorContentProvider.buildRespondentSolicitorSubmissionNotification(any(CaseData.class), any(
-            RespondentSolicitor.class))).thenReturn(expectedTemplate);
-
-        submittedCaseEventHandler.notifyUnregisteredSolicitors(submittedCaseEvent);
-
-        verify(notificationService).sendEmail(
-            UNREGISTERED_RESPONDENT_SOLICICTOR,
-            expectedEmail,
-            expectedTemplate,
-            caseData.getId());
-    }
-
-    @Nested
-    class RespondentSolicitorNotification {
-
-        @Test
-        void shouldSendEmailToRegisteredSolicitors() {
-            final String recipient1 = "solicitor1@test.com";
-            final String recipient2 = "solicitor2@test.com";
-
-            final RespondentSolicitor solicitor1 = RespondentSolicitor.builder()
-                .email(recipient1)
-                .firstName("First")
-                .lastName("Respondent")
-                .organisation(Organisation.builder().organisationID("111").organisationName("org1").build())
-                .build();
-
-            final RespondentSolicitor solicitor2 = RespondentSolicitor.builder()
-                .email(recipient2)
-                .firstName("Second")
-                .lastName("Respondent")
-                .organisation(Organisation.builder().organisationID("222").organisationName("org2").build())
-                .build();
-
-            final CaseData caseData = caseData().toBuilder()
-                .respondents1(wrapElements(
-                    Respondent.builder().legalRepresentation("Yes").solicitor(solicitor1).build(),
-                    Respondent.builder().legalRepresentation("Yes").solicitor(solicitor2).build()))
-                .build();
-
-            final CaseData caseDataBefore = caseData();
-            final RespondentSolicitorTemplate expectedTemplate = RespondentSolicitorTemplate.builder().build();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            when(respondentSolicitorContentProvider.buildRespondentSolicitorSubmissionNotification(
-                caseData, solicitor1)).thenReturn(expectedTemplate);
-
-            when(respondentSolicitorContentProvider.buildRespondentSolicitorSubmissionNotification(
-                caseData, solicitor2)).thenReturn(expectedTemplate);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verify(notificationService).sendEmail(
-                REGISTERED_RESPONDENT_SUBMISSION_TEMPLATE,
-                recipient1,
-                expectedTemplate,
-                caseData.getId());
-
-            verify(notificationService).sendEmail(
-                REGISTERED_RESPONDENT_SUBMISSION_TEMPLATE,
-                recipient2,
-                expectedTemplate,
-                caseData.getId());
-        }
-
-        @ParameterizedTest
-        @NullAndEmptySource
-        @ValueSource(strings = {"No"})
-        void shouldNotSendEmailWhenLegalRepresentationIsNotSetToTheRespondent(String legalRepresentation) {
-            final Respondent respondent = Respondent.builder().legalRepresentation(legalRepresentation).build();
-
-            final CaseData caseData = caseData().toBuilder()
-                .respondents1(wrapElements(respondent)).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
-        @Test
-        void shouldNotSendEmailWhenRespondentsWithSolicitorAreEmpty() {
-            final Respondent respondentWithoutSolicitor = Respondent.builder().build();
-
-            final CaseData caseData = caseData().toBuilder()
-                .respondents1(wrapElements(respondentWithoutSolicitor)).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
-        @Test
-        void shouldNotSendEmailWhenRegisteredSolicitorsAreEmpty() {
-            final Respondent respondentWithUnregisteredSolicitor = Respondent.builder().solicitor(
-                RespondentSolicitor.builder()
-                    .unregisteredOrganisation(UnregisteredOrganisation.builder().name("org name").build()).build())
-                .build();
-
-            final CaseData caseData = caseData().toBuilder()
-                .respondents1(wrapElements(respondentWithUnregisteredSolicitor)).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
-        @ParameterizedTest
-        @NullAndEmptySource
-        void shouldNotSendEmailWhenRegisteredSolicitorsEmailsAreEmpty(String solicitorEmail) {
-            final Respondent respondent1 = Respondent.builder()
-                .solicitor(
-                    RespondentSolicitor.builder()
-                        .firstName("First")
-                        .lastName("Respondent1")
-                        .email(solicitorEmail)
-                        .organisation(Organisation.builder().organisationID("123").build())
-                        .build())
-                .build();
-
-            final CaseData caseData = caseData().toBuilder().respondents1(wrapElements(respondent1)).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
-        @ParameterizedTest
-        @NullAndEmptySource
-        void shouldNotSendEmailWhenRegisteredSolicitorsOrganisationIdIsEmpty(String organisationId) {
-            final Respondent respondent = Respondent.builder()
-                .solicitor(
-                    RespondentSolicitor.builder()
-                        .firstName("First")
-                        .lastName("Respondent1")
-                        .email("test@test.com")
-                        .organisation(Organisation.builder().organisationID(organisationId).build())
-                        .build())
-                .build();
-
-            final CaseData caseData = caseData().toBuilder().respondents1(wrapElements(respondent)).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
-        @Test
-        void shouldNotSendEmailWhenNoRespondentsExist() {
-            final CaseData caseData = caseData().toBuilder().respondents1(emptyList()).build();
-
-            final CaseData caseDataBefore = caseData();
-            final SubmittedCaseEvent submittedCaseEvent = new SubmittedCaseEvent(caseData, caseDataBefore);
-
-            submittedCaseEventHandler.notifyRegisteredRespondentSolicitors(submittedCaseEvent);
-
-            verifyNoInteractions(respondentSolicitorContentProvider);
-        }
-
     }
 
     @Nested
@@ -433,9 +225,7 @@ class SubmittedCaseEventHandlerTest {
         assertClass(SubmittedCaseEventHandler.class).hasAsyncMethods(
             "notifyAdmin",
             "notifyCafcass",
-            "makePayment",
-            "notifyUnregisteredSolicitors",
-            "notifyRegisteredRespondentSolicitors");
+            "makePayment");
     }
 
 }
