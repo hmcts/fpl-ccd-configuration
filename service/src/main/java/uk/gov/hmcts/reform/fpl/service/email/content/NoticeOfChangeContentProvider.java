1cc21f0139fc4b21a81bdd102ec5e4eed339eef7
Soorya Suresh <soorya11@hotmail.co.uk>
2021-05-07 15:10:21 +0100

    feat: confirmation of successful noc (FPLA-2993) (#2562)
    * FPLA-2829: Permissions for NoC
    
    * FPLA-2829: Ensuring respondentPolicy0-9 is always present on casedetails after submission
    
    * FPLA-2829: WIP e2e test
    
    * FPLA-2829: Minor e2e cleanup
    
    * Minor test name updates
    
    * FPLA-2829: Minor name change
    
    * FPLA-2829: UX content suggestion
    
    * FPLA-2829: Further content changes from UX. Code review fixes
    
    * FPLA-2829: Toggled NoC questions
    
    * FPLA-2829: Code review changes
    
    * FPLA-2829: Code review fixes
    
    * FPLA-2838: WIP
    
    * First attempt at endpoint
    
    * FPLA-2838: System user case event call
    
    * FPLA-2838: WIP
    
    * Exlclude sonar
    
    * FPLA-2829: accessibility testing for NoC pages
    
    * FPLA-2838: Refactor
    
    * FPLA-2838: Removed unneeded defintion changes
    
    * FPLA-2838: Tested respondent policy service
    
    * FPLA-2838: Removed duplicate client
    
    * FPLA-2838: Notice of change controller returns caseDetailsMap
    
    * FPLA-2838: Added solicitor role test
    
    * Adding logs for testing purpose
    
    * FPLA-2838: Fixed sonar cloud bug
    
    * FPLA-2838: Minor revert
    
    * Adding fix for surname
    
    * working
    
    * Changing test log
    
    * Checking NOC email
    
    * FPLA-2838: e2e tests
    
    * Improve test coverage
    
    * Test improvements
    
    * Small fixes and improvements
    
    * Remove jave crash logs
    
    * remove redundant access control
    
    * change noc callbacks so that supplementary data is updated correclty
    
    * FPLA-2838: Revert notification config
    
    * FPLA-2838: Revert config
    
    * FPLA-2838: Build fix
    
    * FPLA-2838: move userCompletesNoC() e2e test steps to noticeOfChange.page.js to support future code reuse
    
    * * The old and new solicitor for a Notice of Change now get notifications regarding their access to the case
    
    * Minor corrections after merge
    
    * * Renamed a class
    
    * * Improve test coverage
    
    * * Actually improve test coverage...
    
    * FPLA-2838: noc e2e refactor
    
    * FPLA-2838: NoC e2e test update
    
    * * Code review suggestions
     * Merged two functions for building templates into one
    
    * * Further code review suggestions
    
    * feat: Update rolicitor access (FPLA-2840)
    
    * Address code review comments
    
    * Apply respondent post submission validation conditionally, based on notice of change toggle
    
    * remove unnecessary awaits
    
    * Update e2e test
    
    * * Fix bug where email not sent if no previous representation
     * Improved tests using code review suggestions
    
    * Change the way how no access to case is verified in e2e
    
    * Allow only hmcts admin to change respondent solicitor representation via manage respondents event in post submitted state
    
    * Address code review comments
    
    * * Ensure email is sent to correct people...
    
     * Tweaked some tests to check email is only sent once
    
     * Corrections to rebase
    
    * * Remove duplicate file from rebase
    
    * * Correction to merge by removing duplicated blocks
    
    * * Minor change to an email template
    
    Co-authored-by: James Nelson <Jamesnelson117@gmail.com>
    Co-authored-by: Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
    Co-authored-by: Toireasa Moley <toireasa.moley@hmcts.net>
    Co-authored-by: Rob Doherty-Kelly <Rob-Doherty@users.noreply.github.com>
    Co-authored-by: Rob Doherty <robdohertykelly@gmail.com>
    Co-authored-by: Tomasz Powroznik <tomas.powroznik@gmail.com>
    Co-authored-by: samuelzerozero <50642657+samuelzerozero@users.noreply.github.com>
    Co-authored-by: Lewis Birks <lewisb@kainos.com>
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/NoticeOfChangeContentProvider.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.fpl.service.email.content;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.RespondentSolicitor;
+import uk.gov.hmcts.reform.fpl.model.notify.noticeofchange.NoticeOfChangeRespondentSolicitorTemplate;
+import uk.gov.hmcts.reform.fpl.service.email.content.base.SharedNotifyContentProvider;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class NoticeOfChangeContentProvider extends SharedNotifyContentProvider {
+
+    public NoticeOfChangeRespondentSolicitorTemplate buildNoticeOfChangeRespondentSolicitorTemplate(
+        CaseData caseData, RespondentSolicitor solicitor) {
+
+        return NoticeOfChangeRespondentSolicitorTemplate.builder()
+            .salutation(getSalutation(solicitor))
+            .caseName(caseData.getCaseName())
+            .ccdNumber(caseData.getId().toString())
+            .caseUrl(getCaseUrl(caseData.getId()))
+            .build();
+    }
+
+    private String getSalutation(RespondentSolicitor solicitor) {
+        final String representativeName = solicitor.getFullName();
+        return isBlank(representativeName) ? EMPTY : "Dear " + representativeName;
+    }
+}
