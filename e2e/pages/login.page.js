e6c5d39e88ece63f1c5d845d2da6048024d2833a
tomaszpowroznik <tomas.powroznik@gmail.com>
2021-05-17 17:40:00 +0100

    chore: Fix e2e signout issue (#2643)
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>

--- e2e/pages/login.page.js
+++ e2e/pages/login.page.js
@@ -9,11 +9,9 @@ module.exports = {
   submitButton: 'input[value="Sign in"]',
 
   async signIn(user) {
-    if(!await I.waitForSelector(this.submitButton)){
-      throw `Element ${this.submitButton} not found`;
-    }
     I.fillField(this.fields.username, user.email);
     I.fillField(this.fields.password, user.password);
     I.click(this.submitButton);
   },
+
 };
