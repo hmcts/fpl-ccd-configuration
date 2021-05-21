37b4b701aba8fec43aa3eb34cb874ce57c2aa012
James Nelson <Jamesnelson117@gmail.com>
2021-05-21 13:50:27 +0100

    FPLA-2812: Fixed orderDocumentGeneratorHolderTest

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/OrderDocumentGeneratorHolderTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/OrderDocumentGeneratorHolderTest.java
@@ -19,6 +19,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.model.order.Order.C21_BLANK_ORDER;
 import static uk.gov.hmcts.reform.fpl.model.order.Order.C23_EMERGENCY_PROTECTION_ORDER;
 import static uk.gov.hmcts.reform.fpl.model.order.Order.C32_CARE_ORDER;
+import static uk.gov.hmcts.reform.fpl.model.order.Order.C47A_APPOINTMENT_OF_A_CHILDRENS_GUARDIAN;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -36,6 +37,8 @@ class OrderDocumentGeneratorHolderTest {
     private C21BlankOrderDocumentParameterGenerator c21BlankOrderDocumentParameterGenerator;
     @Mock
     private C23EPODocumentParameterGenerator c23EPODocumentParameterGenerator;
+    @Mock
+    private C47AAppointmentOfAChildrensGuardianParameterGenerator c47AAppointmentOfAChildrensGuardianParameterGenerator;
 
     // Additional Document Collectors
     @Mock
@@ -48,14 +51,15 @@ class OrderDocumentGeneratorHolderTest {
     void setUp() {
         generators = List.of(
             c32CareOrderDocumentParameterGenerator, c21BlankOrderDocumentParameterGenerator,
-            c23EPODocumentParameterGenerator
+            c23EPODocumentParameterGenerator, c47AAppointmentOfAChildrensGuardianParameterGenerator
         );
         collectors = List.of(c23EPOAdditionalDocumentsCollector);
 
         typeToGenerator = Map.of(
             C21_BLANK_ORDER, c21BlankOrderDocumentParameterGenerator,
             C32_CARE_ORDER, c32CareOrderDocumentParameterGenerator,
-            C23_EMERGENCY_PROTECTION_ORDER, c23EPODocumentParameterGenerator
+            C23_EMERGENCY_PROTECTION_ORDER, c23EPODocumentParameterGenerator,
+            C47A_APPOINTMENT_OF_A_CHILDRENS_GUARDIAN, c47AAppointmentOfAChildrensGuardianParameterGenerator
         );
 
         typeToAdditionalDocsCollector = Map.of(
