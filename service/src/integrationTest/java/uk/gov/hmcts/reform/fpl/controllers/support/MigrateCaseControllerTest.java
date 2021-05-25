ba45eadd8812ba4dbc085e6cfbf5e2d94bd5820f
Toireasa Moley <toireasa.moley@hmcts.net>
2021-05-25 15:26:24 +0100

    Replace c2 doc with supporting doc

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -8,16 +8,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.AbstractCallbackTest;
 import uk.gov.hmcts.reform.fpl.enums.State;
-import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.PBAPayment;
 import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static uk.gov.hmcts.reform.fpl.enums.C2ApplicationType.WITH_NOTICE;
 import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 import static uk.gov.hmcts.reform.fpl.utils.TestDataHelper.testDocumentReference;
 
@@ -46,30 +44,48 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
 
         @Test
         void shouldReplaceC2DocumentWithSupportingDocument() {
-            List<Element<AdditionalApplicationsBundle>> additionalApplications = wrapElements(buildAdditionalApplicationsBundle(YES));
+            List<Element<AdditionalApplicationsBundle>> additionalApplications = wrapElements(buildAdditionalApplicationsBundle());
 
             CaseDetails caseDetails = caseDetails(additionalApplications, migrationId);
             CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
 
-            C2DocumentBundle expectedBundle = C2DocumentBundle.builder().document(supportingDocument)
+            C2DocumentBundle expectedBundle = C2DocumentBundle.builder()
+                .document(supportingDocument)
                 .type(WITH_NOTICE)
+                .supportingEvidenceBundle(null)
+                .usePbaPayment(YES.getValue())
                 .build();
 
-            assertThat(extractedCaseData.getAdditionalApplicationsBundle().get(0).getValue().getPbaPayment())
-                .isEqualTo(additionalApplications.get(0).getValue().getPbaPayment());
+            List<AdditionalApplicationsBundle> extractedApplicationBundle = unwrapElements(extractedCaseData.getAdditionalApplicationsBundle());
 
-            System.out.println("Here's what it is" + additionalApplications.get(0).getValue().getC2DocumentBundle());
-//            assertThat(extractedCaseData.getAdditionalApplicationsBundle().get(0).getValue().getC2DocumentBundle())
-//                .isEqualTo(additionalApplications.get(0).getValue().getC2DocumentBundle()).isEqualTo(expectedBundle);
-            //assert supporting evidence is gone
-            //assert document is changed
-            //assert that no other additional application bundles are changed
+            assertThat(extractedApplicationBundle.get(0).getC2DocumentBundle()).isEqualTo(expectedBundle);
+        }
+
+        @Test
+        void shouldReplaceC2DocumentWithSupportingDocumentAtIndex0AndLeaveOtherElementsUnModified() {
+            List<Element<AdditionalApplicationsBundle>> additionalApplications = wrapElements(buildAdditionalApplicationsBundle(), buildAdditionalApplicationsBundle());
+
+            CaseDetails caseDetails = caseDetails(additionalApplications, migrationId);
+            CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
+
+            C2DocumentBundle expectedBundle = C2DocumentBundle.builder()
+                .document(supportingDocument)
+                .type(WITH_NOTICE)
+                .supportingEvidenceBundle(null)
+                .usePbaPayment(YES.getValue())
+                .build();
+
+            List<AdditionalApplicationsBundle> extractedApplicationBundle = unwrapElements(extractedCaseData.getAdditionalApplicationsBundle());
+
+            assertThat(extractedApplicationBundle.size()).isEqualTo(2);
+            assertThat(extractedApplicationBundle.get(0).getC2DocumentBundle()).isEqualTo(expectedBundle);
+            assertThat(extractedApplicationBundle.get(1)).isEqualTo(additionalApplications.get(1).getValue());
         }
 
         @Test
         void shouldNotMigrateCaseIfMigrationIdIsIncorrect() {
             String incorrectMigrationId = "FPLA-1111";
-            List<Element<AdditionalApplicationsBundle>> additionalApplications = wrapElements(buildAdditionalApplicationsBundle(YES));
+            List<Element<AdditionalApplicationsBundle>> additionalApplications = wrapElements(buildAdditionalApplicationsBundle());
 
             CaseDetails caseDetails = caseDetails(additionalApplications, incorrectMigrationId);
             CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
@@ -101,14 +117,12 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
             return caseDetails;
         }
 
-        private AdditionalApplicationsBundle buildAdditionalApplicationsBundle(YesNo usePbaPayment) {
+        private AdditionalApplicationsBundle buildAdditionalApplicationsBundle() {
                     return AdditionalApplicationsBundle.builder()
-                        .pbaPayment(PBAPayment.builder().usePbaPayment(usePbaPayment.getValue()).build())
                         .c2DocumentBundle(C2DocumentBundle.builder()
                             .type(WITH_NOTICE)
                             .document(c2Document)
-                            .supplementsBundle(new ArrayList<>())
-                            .usePbaPayment(usePbaPayment.getValue())
+                            .usePbaPayment(YES.getValue())
                             .supportingEvidenceBundle(wrapElements(SupportingEvidenceBundle.builder()
                             .document(supportingDocument)
                                 .build()))
