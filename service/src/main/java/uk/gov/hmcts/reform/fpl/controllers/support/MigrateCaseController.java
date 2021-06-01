7bd79078dd1395141d8ea39020c7d937e33e0745
Toireasa Moley <toireasa.moley@hmcts.net>
2021-06-01 16:48:36 +0100

    Adding code to revert migration

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -13,13 +13,21 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.SupportingEvidenceBundle;
 import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.service.casesubmission.CaseSubmissionService;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @Api
 @RestController
@@ -39,6 +47,7 @@ public class MigrateCaseController extends CallbackController {
             run3037(caseDetails);
         }
 
+        //CHANGE JIRA NUM
         if ("FPLA-3093".equals(migrationId)) {
             run3093(caseDetails);
         }
@@ -70,6 +79,7 @@ public class MigrateCaseController extends CallbackController {
         caseDetails.setData(data);
     }
 
+    //CHANGE JIRA NUM
     private void run3093(CaseDetails caseDetails) {
         CaseData caseData = getCaseData(caseDetails);
 
@@ -82,10 +92,102 @@ public class MigrateCaseController extends CallbackController {
                     caseData.getFamilyManCaseNumber(), additionalApplicationsBundle.size()));
         }
 
-        additionalApplicationsBundle.remove(0);
+        Element<AdditionalApplicationsBundle> firstBundle = additionalApplicationsBundle.get(0);
+        String ID_FIRST_BUNDLE = "94de5661-bd45-463a-8557-e3f6c4fc63d0";
+        if (firstBundle.getId().equals(UUID.fromString(ID_FIRST_BUNDLE))) {
+
+            firstBundle.getValue().setC2DocumentBundle(firstBundle.getValue()
+                .getC2DocumentBundle()
+                .toBuilder()
+                .supportingEvidenceBundle(buildSupportingEvidenceBundle())
+                .document(DocumentReference.builder()
+                    .filename("S45C-921052410100.pdf")
+                    .url("http://dm-store:8080/documents/5ddafb9c-1396-44c1-a4dc-25204b0989f0")
+                    .binaryUrl("http://dm-store:8080/documents/5ddafb9c-1396-44c1-a4dc-25204b0989f0/binary")
+                    .build()).build());
+
+            //DO I NEED TO ADD LA
+            additionalApplicationsBundle.set(0, firstBundle);
+
+        } else {
+            throw new IllegalStateException(String
+                .format("Migration failed on case %s: Expected" + ID_FIRST_BUNDLE + "but got %s",
+                    caseData.getFamilyManCaseNumber(), firstBundle.getId()));
+        }
+
+        Element<AdditionalApplicationsBundle> secondBundle = additionalApplicationsBundle.get(1);
+        String ID_SECOND_BUNDLE = "ff827f79-f013-485f-8a31-0c56e2632834";
+
+        if (secondBundle.getId().equals(UUID.fromString(ID_SECOND_BUNDLE))) {
+            swapC2WithSupportingDocument(secondBundle.getValue(), additionalApplicationsBundle);
+        } else {
+            throw new IllegalStateException(String
+                .format("Migration failed on case %s: Expected" + ID_SECOND_BUNDLE + "but got %s",
+                    caseData.getFamilyManCaseNumber(), secondBundle.getId()));
+        }
 
         Map<String, Object> data = caseDetails.getData();
-
         data.put("additionalApplicationsBundle", additionalApplicationsBundle);
+    }
+
+    private List<Element<SupportingEvidenceBundle>> buildSupportingEvidenceBundle() {
+        SupportingEvidenceBundle firstBundle = SupportingEvidenceBundle.builder()
+            .name("Position Statement for C2")
+            .document(DocumentReference.builder()
+                .url("http://dm-store:8080/documents/d448738d-51fe-439e-9a0b-5ecc0bb378b6")
+                .binaryUrl("http://dm-store:8080/documents/d448738d-51fe-439e-9a0b-5ecc0bb378b6/binary")
+                .filename("Position Statement for C2.docx")
+                .build())
+            .uploadedBy("HMCTS")
+            .dateTimeUploaded(LocalDateTime.of(2021, 05, 24, 10, 23, 32))
+            .build();
+
+        SupportingEvidenceBundle secondBundle = SupportingEvidenceBundle.builder()
+            .name("Draft LOI")
+            .document(DocumentReference.builder()
+                .url("http://dm-store:8080/documents/d8357dbc-e3bd-464f-8edb-aac0fe1c58c2")
+                .binaryUrl("http://dm-store:8080/documents/d8357dbc-e3bd-464f-8edb-aac0fe1c58c2/binary")
+                .filename("AMO0030002 Draft LOI.docx")
+                .build())
+            .uploadedBy("HMCTS")
+            .dateTimeUploaded(LocalDateTime.of(2021, 05, 24, 10, 23, 32))
+            .build();
+
+        SupportingEvidenceBundle thirdBundle = SupportingEvidenceBundle.builder()
+            .name("CV")
+            .document(DocumentReference.builder()
+                .url("http://dm-store:8080/documents/6b6b5071-e097-4074-b317-b00dc3cfa89c")
+                .binaryUrl("http://dm-store:8080/documents/6b6b5071-e097-4074-b317-b00dc3cfa89c/binary")
+                .filename("AMO0030002 Medico-legal_CV-May2021.doc")
+                .build())
+            .uploadedBy("HMCTS")
+            .dateTimeUploaded(LocalDateTime.of(2021, 05, 24, 10, 23, 32))
+            .build();
+
+        return wrapElements(firstBundle, secondBundle, thirdBundle);
+
+    }
+
+    private List<Element<AdditionalApplicationsBundle>> swapC2WithSupportingDocument(AdditionalApplicationsBundle application,
+                                                                                     List<Element<AdditionalApplicationsBundle>> additionalApplicationsBundle) {
+        C2DocumentBundle c2DocumentBundle = application.getC2DocumentBundle();
+
+        Optional<DocumentReference> documentReference = c2DocumentBundle.getSupportingEvidenceBundle().stream().filter(
+            supportingEvidenceBundle -> supportingEvidenceBundle.getId().equals("3f3a183e-44ab-4e63-ac27-0ca40f3058ff")
+        ).map(Element::getValue).map(SupportingEvidenceBundle::getDocument).findFirst();
+
+        if (documentReference.isPresent()) {
+            application.setC2DocumentBundle(application.getC2DocumentBundle().toBuilder().document(documentReference.get()).build());
+        }
+
+        c2DocumentBundle.getSupportingEvidenceBundle()
+            .removeIf(supportingEvidenceBundle -> supportingEvidenceBundle.getId()
+                .toString()
+                .equals("3f3a183e-44ab-4e63-ac27-0ca40f3058ff")
+            );
+
+        additionalApplicationsBundle.set(1, element(application));
+
+        return additionalApplicationsBundle;
     }
 }
