86f595348f0aaf305e5701149ae3e6b08750ff97
Lewis Birks <lewisb@kainos.com>
2021-06-04 13:18:51 +0100

    update interlocutory order uploaded notification * admin: `6d0ab7cb-8f1e-4ac0-b51d-11cf0b6ee568`

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/handlers/AdditionalApplicationsUploadedEventHandlerEmailTemplateTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/handlers/AdditionalApplicationsUploadedEventHandlerEmailTemplateTest.java
@@ -1,45 +1,55 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.fpl.enums.HearingType;
 import uk.gov.hmcts.reform.fpl.events.AdditionalApplicationsUploadedEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.notify.additionalapplicationsuploaded.AdditionalApplicationsUploadedTemplate;
+import uk.gov.hmcts.reform.fpl.model.Child;
+import uk.gov.hmcts.reform.fpl.model.ChildParty;
+import uk.gov.hmcts.reform.fpl.model.HearingBooking;
+import uk.gov.hmcts.reform.fpl.model.Respondent;
+import uk.gov.hmcts.reform.fpl.model.RespondentParty;
+import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
+import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.model.common.OtherApplicationsBundle;
 import uk.gov.hmcts.reform.fpl.request.RequestData;
-import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
+import uk.gov.hmcts.reform.fpl.service.CaseUrlService;
+import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
 import uk.gov.hmcts.reform.fpl.service.email.content.AdditionalApplicationsUploadedEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.testingsupport.email.EmailTemplateTest;
+import uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.mockito.BDDMockito.given;
-
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.CASE_TYPE;
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.JURISDICTION;
+import static uk.gov.hmcts.reform.fpl.enums.C2ApplicationType.WITH_NOTICE;
+import static uk.gov.hmcts.reform.fpl.enums.OtherApplicationType.C1_WITH_SUPPLEMENT;
+import static uk.gov.hmcts.reform.fpl.enums.TabUrlAnchor.OTHER_APPLICATIONS;
 import static uk.gov.hmcts.reform.fpl.enums.UserRole.LOCAL_AUTHORITY;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.fpl.testingsupport.email.EmailContent.emailContent;
 import static uk.gov.hmcts.reform.fpl.testingsupport.email.SendEmailResponseAssert.assertThat;
-import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.caseData;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
-@SpringBootTest(classes = {
-    AdditionalApplicationsUploadedEventHandler.class,
-    NotificationService.class,
-    ObjectMapper.class
+@ContextConfiguration(classes = {
+    AdditionalApplicationsUploadedEventHandler.class, EmailNotificationHelper.class,
+    AdditionalApplicationsUploadedEmailContentProvider.class, CaseUrlService.class
 })
-
 public class AdditionalApplicationsUploadedEventHandlerEmailTemplateTest extends EmailTemplateTest {
-    private final String respondentLastName = "Smith";
-    private final String calloutText = "Smith, SACCCCCCCC5676576567";
-    private final String caseUrl = "null/case/" + JURISDICTION + "/" + CASE_TYPE + "/12345#Other%20applications";
-    private final List<String> applicationTypes = Arrays.asList("C2 (With notice)",
-        "C13A - Special guardianship order");
+    private static final long CASE_ID = 12345L;
+    private static final String FAMILY_MAN_CASE_NUMBER = "FAM_NUM";
+    private static final String CHILD_LAST_NAME = "Jones";
+    private static final String RESPONDENT_LAST_NAME = "Smith";
 
     @Autowired
     private AdditionalApplicationsUploadedEventHandler underTest;
@@ -51,41 +61,55 @@ public class AdditionalApplicationsUploadedEventHandlerEmailTemplateTest extends
     private RequestData requestData;
 
     @MockBean
-    private AdditionalApplicationsUploadedEmailContentProvider additionalApplicationsUploadedEmailContentProvider;
+    private FeatureToggleService toggleService;
 
-    @Test
-    void notifyAdmin() {
-        CaseData caseData = caseData();
+    @ParameterizedTest
+    @MethodSource("subjectLineSource")
+    void notifyAdmin(boolean toggle, String name) {
+        given(toggleService.isEldestChildLastNameEnabled()).willReturn(toggle);
 
         given(requestData.authorisation()).willReturn(AUTH_TOKEN);
 
-        given(idamClient.getUserInfo(AUTH_TOKEN)).willReturn(
-            UserInfo.builder().sub("hmcts-non-admin@test.com").roles(LOCAL_AUTHORITY.getRoleNames()).build());
+        CaseData caseData = CaseData.builder()
+            .id(CASE_ID)
+            .familyManCaseNumber(FAMILY_MAN_CASE_NUMBER)
+            .hearingDetails(wrapElements(HearingBooking.builder()
+                .startDate(LocalDateTime.of(2020, 2, 20, 20, 20, 0))
+                .type(HearingType.CASE_MANAGEMENT)
+                .build()))
+            .respondents1(wrapElements(Respondent.builder()
+                .party(RespondentParty.builder().lastName(RESPONDENT_LAST_NAME).build())
+                .build()))
+            .children1(wrapElements(Child.builder()
+                .party(ChildParty.builder().dateOfBirth(LocalDate.now()).lastName(CHILD_LAST_NAME).build())
+                .build()))
+            .additionalApplicationsBundle(wrapElements(AdditionalApplicationsBundle.builder()
+                .c2DocumentBundle(C2DocumentBundle.builder().type(WITH_NOTICE).supplementsBundle(List.of()).build())
+                .otherApplicationsBundle(OtherApplicationsBundle.builder()
+                    .applicationType(C1_WITH_SUPPLEMENT)
+                    .supplementsBundle(List.of())
+                    .build())
+                .build()))
+            .build();
 
-        given(additionalApplicationsUploadedEmailContentProvider.getNotifyData(caseData))
-            .willReturn(
-                AdditionalApplicationsUploadedTemplate.builder()
-                    .callout(calloutText)
-                    .respondentLastName(respondentLastName)
-                    .caseUrl(caseUrl)
-                    .applicationTypes(applicationTypes)
-                    .build()
-            );
+        given(idamClient.getUserInfo(AUTH_TOKEN)).willReturn(
+            UserInfo.builder().sub("hmcts-non-admin@test.com").roles(LOCAL_AUTHORITY.getRoleNames()).build()
+        );
 
         underTest.notifyAdmin(new AdditionalApplicationsUploadedEvent(caseData));
 
         assertThat(response())
-            .hasSubject("New application uploaded, " + respondentLastName)
+            .hasSubject("New application uploaded, " + name)
             .hasBody(emailContent()
                 .line("New applications have been made for the case:")
                 .line()
-                .callout(calloutText)
+                .callout(RESPONDENT_LAST_NAME + ", " + FAMILY_MAN_CASE_NUMBER + ", hearing 20 Feb 2020")
                 .line()
                 .h1("Applications")
                 .line()
                 .line()
                 .list("C2 (With notice)")
-                .list("C13A - Special guardianship order")
+                .list("C1 - With supplement")
                 .line()
                 .h1("Next steps")
                 .line("You need to:")
@@ -94,8 +118,14 @@ public class AdditionalApplicationsUploadedEventHandlerEmailTemplateTest extends
                     "send a message to the judge or legal adviser",
                     "send a copy to relevant parties")
                 .line()
-                .end("To review the application, sign in to " + caseUrl)
+                .end("To review the application, sign in to " + caseDetailsUrl(CASE_ID, OTHER_APPLICATIONS))
             );
     }
 
+    private static Stream<Arguments> subjectLineSource() {
+        return Stream.of(
+            Arguments.of(true, CHILD_LAST_NAME),
+            Arguments.of(false, RESPONDENT_LAST_NAME)
+        );
+    }
 }
