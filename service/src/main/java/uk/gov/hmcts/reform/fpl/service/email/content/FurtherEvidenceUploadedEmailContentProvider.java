5f75e95e035e03fcffa3d31009e279bc3c42c443
Lewis Birks <lewisb@kainos.com>
2021-06-07 10:37:14 +0100

    update LA uploaded evidence docs notification * LA + respondent solicitors: 917c07b2-b414-41ab-9d0b-14a570fad413

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/FurtherEvidenceUploadedEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/FurtherEvidenceUploadedEmailContentProvider.java
@@ -1,24 +1,29 @@
 package uk.gov.hmcts.reform.fpl.service.email.content;
 
-import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.notify.furtherevidence.FurtherEvidenceDocumentUploadedData;
 import uk.gov.hmcts.reform.fpl.service.email.content.base.AbstractEmailContentProvider;
+import uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper;
 
 import static uk.gov.hmcts.reform.fpl.enums.TabUrlAnchor.DOCUMENTS;
 import static uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper.buildSubjectLineWithHearingBookingDateSuffix;
-import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.getFirstRespondentLastName;
 
-@Service
+@Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class FurtherEvidenceUploadedEmailContentProvider extends AbstractEmailContentProvider {
+    private final EmailNotificationHelper helper;
+
     public FurtherEvidenceDocumentUploadedData buildParameters(CaseData caseData, String sender) {
         return FurtherEvidenceDocumentUploadedData.builder()
             .caseUrl(getCaseUrl(caseData.getId(), DOCUMENTS))
-            .callout(buildSubjectLineWithHearingBookingDateSuffix(caseData.getFamilyManCaseNumber(),
-                caseData.getRespondents1(),
-                caseData.getFirstHearing().orElse(null)))
+            .callout(buildSubjectLineWithHearingBookingDateSuffix(
+                caseData.getFamilyManCaseNumber(), caseData.getRespondents1(), caseData.getFirstHearing().orElse(null)
+            ))
             .userName(sender)
-            .respondentLastName(getFirstRespondentLastName(caseData))
+            .lastName(helper.getSubjectLineLastName(caseData))
             .build();
     }
 }
