8b28617f65eeb47cd8181c168945c395711c74f3
Rob Doherty <robdohertykelly@gmail.com>
2021-06-10 17:22:39 +0100

    chore: use Set instead of array in SauceLabsReportingHelper.js

--- e2e/helpers/SauceLabsReportingHelper.js
+++ e2e/helpers/SauceLabsReportingHelper.js
@@ -6,29 +6,24 @@ const exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
   console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=fpl-ccd-configuration');
-  return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
+  return `curl -X PUT -s -d '{"passed": ${result}}' -u ${process.env.SAUCE_USERNAME}:${process.env.SAUCE_ACCESS_KEY} https://eu-central-1.saucelabs.com/rest/v1/${process.env.SAUCE_USERNAME}/jobs/${sessionId}`;
 }
 
 module.exports = function () {
-  let failedTests = [];
+  let failedTests = new Set();
 
   event.dispatcher.on(event.test.passed, (test) => {
-    if (failedTests.find(failedTest => failedTest === test.title)) {
-      // Remove test from failedTests list once it passes
-      failedTests = failedTests.filter(failedTest => failedTest !== test.title);
-    }
+    failedTests.delete(test.title);
   });
 
   event.dispatcher.on(event.test.failed, (test) => {
-    if (!failedTests.find(failedTest => failedTest === test.title)) {
-      failedTests.push(test.title);
-    }
+    failedTests.add(test.title);
   });
 
   // Setting overall test result on SauceLabs
   event.dispatcher.on(event.all.after, () => {
     const sessionId = container.helpers('WebDriver').browser.sessionId;
-    const overallResult = failedTests.length === 0;
+    const overallResult = failedTests.size === 0;
     console.log(`Updating Saucelabs with overall result: ${overallResult ? 'PASS' : 'FAIL'}`);
     if (!overallResult) {
       console.log('Failed Tests:');
