c1b00f7703d55be699271da6b8ac7fc9ddfb981e
Rob Doherty <robdohertykelly@gmail.com>
2021-06-10 12:04:06 +0100

    chore: only update Saucelabs at the end of a test run with the overall test result (i.e. any failed test marks the run failed). Also contains a fix to avoid the "Cannot read property 'map' of undefined" error that can be seen in the nightly logs

--- e2e/helpers/SauceLabsReportingHelper.js
+++ e2e/helpers/SauceLabsReportingHelper.js
@@ -9,23 +9,31 @@ function updateSauceLabsResult(result, sessionId) {
   return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://eu-central-1.saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
-module.exports = function() {
-  let overallResult;
+module.exports = function () {
+  let failedTests = [];
 
-  event.dispatcher.on(event.test.passed, () => {
-    // skip if the test run has already failed
-    if (overallResult === undefined) {
-      overallResult = true;
+  event.dispatcher.on(event.test.passed, (test) => {
+    if (failedTests.find(failedTest => failedTest === test.title)) {
+      // Remove test from failedTests list once it passes
+      failedTests = failedTests.filter(failedTest => failedTest === test.title);
     }
   });
 
-  event.dispatcher.on(event.test.failed, () => {
-    overallResult = false;
+  event.dispatcher.on(event.test.failed, (test) => {
+    if (!failedTests.find(failedTest => failedTest === test.title)) {
+      failedTests.push(test.title);
+    }
   });
 
   // Setting overall test result on SauceLabs
   event.dispatcher.on(event.all.after, () => {
     const sessionId = container.helpers('WebDriver').browser.sessionId;
-    exec(updateSauceLabsResult(!!overallResult, sessionId));
+    const overallResult = failedTests.length === 0;
+    console.log(`Updating Saucelabs with overall result: ${overallResult ? 'PASS' : 'FAIL'}`);
+    if (!overallResult) {
+      console.log('Failed Tests:');
+      failedTests.forEach(test => console.log(`"${test}"`));
+    }
+    exec(updateSauceLabsResult(overallResult, sessionId));
   });
 };
