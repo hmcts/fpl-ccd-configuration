a62032ee8a287d92ca90156629b6cc9ec97ee5f6
Rob Doherty <robdohertykelly@gmail.com>
2021-06-11 11:17:19 +0100

    fix: make codecept wait for unique /case-details page task dropdown element

--- e2e/actors/base.js
+++ e2e/actors/base.js
@@ -184,7 +184,7 @@ module.exports = {
         await this.goToPage(`${baseUrl}/cases/case-details/${caseId}`);
       }, signedInSelector);
     }
-    await this.waitForSelector('.ccd-dropdown');
+    await this.waitForSelector('#next-step');
   },
 
   async navigateToCaseDetailsAs(user, caseId) {
