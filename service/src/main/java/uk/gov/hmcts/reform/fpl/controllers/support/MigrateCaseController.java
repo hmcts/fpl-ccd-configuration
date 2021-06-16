f221e81ba17d5ae4e654e825f0079677b77110f4
aliveni <aliveni.choppa@hmcts.net>
2021-06-16 10:40:05 +0100

    migration: Remove other court admin document (FPLA-3135) (#2756)
    * FPLA-3135 Remove document from otherCourtAdminDocuments
    
    * fix checkstyle issues

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -13,9 +13,11 @@ import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.CourtAdminDocument;
 import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.service.document.DocumentListService;
 
 import java.util.List;
 import java.util.Objects;
@@ -27,6 +29,8 @@ import java.util.UUID;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Slf4j
 public class MigrateCaseController extends CallbackController {
+    private final DocumentListService documentListService;
+
     private static final String MIGRATION_ID_KEY = "migrationId";
 
     private static final UUID BUNDLE_ID = UUID.fromString("b02898e7-46dc-47ce-9639-9e5b04d03b9e");
@@ -35,10 +39,17 @@ public class MigrateCaseController extends CallbackController {
     private static final String FAMILY_MAN_CASE_NUMBER = "SA20C50050";
     private static final int BUNDLE_SIZE = 2;
 
+    private static final String FAMILY_MAN_ID = "DE21C50016";
+    private static final UUID DOCUMENT_TO_REMOVE_UUID = UUID.fromString("2acc1f5f-ff76-4c3e-b3fc-087ebebd2911");
+
     @PostMapping("/about-to-submit")
     public AboutToStartOrSubmitCallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
         Object migrationId = caseDetails.getData().get(MIGRATION_ID_KEY);
+
+        if ("FPLA-3135".equals(migrationId)) {
+            run3135(caseDetails);
+        }
 
         if ("FPLA-3125".equals(migrationId)) {
             run3125(caseDetails);
@@ -68,6 +79,26 @@ public class MigrateCaseController extends CallbackController {
         bundles.remove(1);
 
         caseDetails.getData().put("additionalApplicationsBundle", bundles);
+    }
+
+    private void run3135(CaseDetails caseDetails) {
+        CaseData caseData = getCaseData(caseDetails);
+
+        if (FAMILY_MAN_ID.equals(caseData.getFamilyManCaseNumber())) {
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = caseData.getOtherCourtAdminDocuments();
+
+            if (otherCourtAdminDocuments.stream().noneMatch(doc -> DOCUMENT_TO_REMOVE_UUID.equals(doc.getId()))) {
+                throw new IllegalStateException(String
+                    .format("Migration failed on case %s: Expected %s but not found",
+                        caseData.getFamilyManCaseNumber(), DOCUMENT_TO_REMOVE_UUID));
+            } else {
+                otherCourtAdminDocuments.removeIf(document -> document.getId().equals(DOCUMENT_TO_REMOVE_UUID));
+                caseDetails.getData().put("otherCourtAdminDocuments", otherCourtAdminDocuments);
+
+                caseDetails.getData().putAll(documentListService.getDocumentView(getCaseData(caseDetails)));
+            }
+        }
+
     }
 
     private void validateBundle(Element<AdditionalApplicationsBundle> bundle) {
