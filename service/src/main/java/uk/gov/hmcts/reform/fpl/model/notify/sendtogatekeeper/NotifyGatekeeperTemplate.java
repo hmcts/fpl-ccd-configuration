356b9d487701295a30d4d920782dc697f190eeb4
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2021-06-18 09:08:48 +0100

    impr: use eldest child last name in subject line (FPLA-3027) (#2726)
    * remove unused field
    
    * get the eldest child last name
    
    * add toggle
    
    * update sdo emails
    
    * returned case test
    
    * update template tests
    
    * update any order issued notifications
    *`bb40930c-a583-4868-a1f6-1e398e7554d3`
    *`1827ae2d-d7c3-4094-8978-c2f77d7713fa`
    
    * update application returned notifications
    *`2d8b5486-7527-42ea-9b56-e79bfc0f6c27`
    
    * update assertion errors to show difference
    
    * update application submitted notifications (introducing new templates to do so)
    * hmcts: `a1562b56-4ff7-4e3e-b62d-5fb9f086ee8f` -> `b125f8a1-f8d2-4704-bb5e-90c819b70117`
    * outsourced: `13516685-155c-4da3-9aaf-32f3f943bbcf` -> `c4d612a3-8384-468b-9b83-532e7e857c63`
    * cafcass: `e5630e8b-3b25-4773-a41a-a42af123bebc` -> `6fe52d5c-e661-4c05-9fd7-2a0202e9736d`
    * gatekeeper: `0aa6f272-9c96-468e-bffd-ea6e7369c762` -> `df37a22a-616b-4fc1-a20d-7bb115664081`
    
    * update cmo removed notification
    * `cd114602-4343-42ce-ab60-1be88b797cd2`
    
    * update cmo sent for approval notification
    * admin: `90c5f4b3-738b-4c8d-b433-c8c90095f815`
    * judge: `f9ef5385-258b-4d57-84b4-4915afd3ba5e`
    
    * minor updates for generated order issued
    * LA: `a0f911cf-4010-4022-bf34-a183cd711b8e`
    
    * update draft orders rejected notification
    * LA: `6d0ab7cb-8f1e-4ac0-b51d-11cf0b6ee568`
    
    * update interlocutory order uploaded notification
    * admin: `6d0ab7cb-8f1e-4ac0-b51d-11cf0b6ee568`
    
    * update judge approves draft orders notification
    * LA, cafcass, representatives: `632ea051-0ab0-447d-9d6c-54118b04a5c1`
    
    * take newlines into account
    
    * update LA uploaded evidence docs notification
    * LA + respondent solicitors: 917c07b2-b414-41ab-9d0b-14a570fad413
    
    * update message from admin regarding application notification
    * judge: `bf041444-5a4e-4045-878a-4833f57b7d96`
    
    * update message from judge or legal adviser regarding application notification
    * judge: `e0603270-376b-4d9b-8570-7fd6f2e295ec`
    
    * update notice of placement order uploaded notification
    * LA + digital-rep: `e0603270-376b-4d9b-8570-7fd6f2e295ec`
    
    * update placement application uploaded notification
    * admin: `ac0a0091-0b2e-44b9-afa1-321590b5d984`
    
    * update SDO removed notification
    * gatekeeper: `8f66e70f-e328-4a22-aa17-f52f44e69d45`
    
    * update CMO issued notification
    * LA, cafcass, representatives: `0ec7f651-4f0e-4f95-bff1-6ec232b9baf6`
    
    * update Draft CMO rejected notification
    * LA: `0ec7f651-4f0e-4f95-bff1-6ec232b9baf6` -> `d1454061-e2bc-4b89-9a66-7add40656bf8`
    
    * update Legal rep added by LA notification
    * legal rep: `7f3bf6f3-64cd-4b36-ba56-6c7a676a4816` -> `a17492b8-3724-4d24-8b5a-d815f64ce5ac`
    
    * update Notice of New Hearing notification
    * LA, cafcass, representatives: `c5525d1b-6431-489d-be0e-51de3f163583` -> `aa997c49-efe7-4443-a4ea-6198f62ce449`
    
    * update Representative added to case notification
    * email-rep: `ca15c188-14d2-49a3-ac61-fdd7c6c7dc9b` -> `38a4dba9-ab05-4a40-b72b-f26b05ea6e7b`
    * digital-rep: `f846a44e-9d1f-4cdb-aa3b-10af07bc4c66` -> `a53da518-bb11-43eb-9824-ad5f39d8b61e`
    
    * fix checkstyle
    
    * fix int tests
    
    * FPLA-3027: fix missing dep
    
    * remove unnecessary extension
    
    * update subject line of email test
    
    * code review comments
    
    * fix more tests
    
    Co-authored-by: Samuel Vrech <samuelzero@gmail.com>
    Co-authored-by: samuelzerozero <50642657+samuelzerozero@users.noreply.github.com>
    Co-authored-by: Rob Doherty-Kelly <Rob-Doherty@users.noreply.github.com>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/notify/sendtogatekeeper/NotifyGatekeeperTemplate.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/notify/sendtogatekeeper/NotifyGatekeeperTemplate.java
@@ -2,15 +2,17 @@ package uk.gov.hmcts.reform.fpl.model.notify.sendtogatekeeper;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import uk.gov.hmcts.reform.fpl.model.notify.SharedNotifyTemplate;
 
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @Data
 @SuperBuilder
 public final class NotifyGatekeeperTemplate extends SharedNotifyTemplate {
-    public SharedNotifyTemplate duplicate() {
-        return SharedNotifyTemplate.builder()
+    public NotifyGatekeeperTemplate duplicate() {
+        return NotifyGatekeeperTemplate.builder()
             .localAuthority(this.getLocalAuthority())
             .ordersAndDirections(this.getOrdersAndDirections())
             .dataPresent(this.getDataPresent())
@@ -22,6 +24,7 @@ public final class NotifyGatekeeperTemplate extends SharedNotifyTemplate {
             .firstRespondentName(this.getFirstRespondentName())
             .reference(this.getReference())
             .caseUrl(this.getCaseUrl())
+            .childLastName(this.getChildLastName())
             .build();
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/LocalAuthorityEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/LocalAuthorityEmailContentProvider.java
@@ -1,18 +1,23 @@
 package uk.gov.hmcts.reform.fpl.service.email.content;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.notify.BaseCaseNotifyData;
 import uk.gov.hmcts.reform.fpl.service.email.content.base.AbstractEmailContentProvider;
+import uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper;
 
 import static uk.gov.hmcts.reform.fpl.enums.TabUrlAnchor.PLACEMENT;
-import static uk.gov.hmcts.reform.fpl.utils.PeopleInCaseHelper.getFirstRespondentLastName;
 
 @Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class LocalAuthorityEmailContentProvider extends AbstractEmailContentProvider {
+    private final EmailNotificationHelper helper;
+
     public BaseCaseNotifyData buildNoticeOfPlacementOrderUploadedNotification(CaseData caseData) {
         return BaseCaseNotifyData.builder()
-            .respondentLastName(getFirstRespondentLastName(caseData))
+            .lastName(helper.getSubjectLineLastName(caseData))
             .caseUrl(getCaseUrl(caseData.getId(), PLACEMENT))
             .build();
     }
