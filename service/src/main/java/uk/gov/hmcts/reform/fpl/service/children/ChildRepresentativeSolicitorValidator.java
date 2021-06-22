000ed1cdb65b24445057dff16520786bfc6d5597
Samuel Vrech <samuelzero@gmail.com>
2021-06-22 08:39:09 +0100

    FPLA-3028: fix stylecheck

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/children/ChildRepresentativeSolicitorValidator.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/children/ChildRepresentativeSolicitorValidator.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.service.children;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
