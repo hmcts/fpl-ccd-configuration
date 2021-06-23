0600b9a5a9c86e2d62e2836f695acdfbdc13d72e
aliveni <aliveni.ch@gmail.com>
2021-06-23 12:42:13 +0100

    FPLA-3175 remove other court admin documents

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -9,8 +9,10 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.AbstractCallbackTest;
 import uk.gov.hmcts.reform.fpl.enums.State;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.CourtAdminDocument;
 import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
 import uk.gov.hmcts.reform.fpl.model.common.C2DocumentBundle;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.List;
@@ -19,12 +21,183 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+import static uk.gov.hmcts.reform.fpl.utils.TestDataHelper.testDocumentReference;
 
 @WebMvcTest(MigrateCaseController.class)
 @OverrideAutoConfiguration(enabled = true)
 class MigrateCaseControllerTest extends AbstractCallbackTest {
     MigrateCaseControllerTest() {
         super("migrate-case");
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class Fpla3175 {
+        String familyManNumber = "CV21C50026";
+        String migrationId = "FPLA-3175";
+
+        final UUID documentId1 = UUID.fromString("339ad24a-e83c-4b81-9743-46c7771b3975");
+        final UUID documentId2 = UUID.fromString("dc8ad08b-e7ba-4075-a9e4-0ff8fc85616b");
+        final UUID documentId3 = UUID.fromString("008f3053-d949-4aaa-9d65-1027e88ed288");
+        final UUID documentId4 = UUID.fromString("671d6805-c042-4b3e-88e8-7c7fd103a026");
+        final UUID documentId5 = UUID.fromString("74c918f2-6409-4651-bb7b-1f58c39aee94");
+        final UUID documentId6 = UUID.fromString("03343373-df27-4744-a63c-56a98442662a");
+
+        final String documentTitle1 = "Children's Guardian Position Statement 22 .06.2021";
+        final String documentTitle2 = "Supporting documents for Children's Guardian Position Statement no 1";
+        final String documentTitle3 = "Supporting documents for Children's Guardian Position Statement no 2";
+        final String documentTitle4 = "Supporting documents for Children's Guardian Position Statement no 3";
+        final String documentTitle5 = "Supporting documents for Children's Guardian Position Statement no 4";
+        final String documentTitle6 = "Supporting documents for Children's Guardian Position Statement no 5";
+
+        final DocumentReference document = testDocumentReference();
+
+        @Test
+        void shouldRemoveOtherCourtAdminDocuments() {
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = buildExpectedOtherCourtAdminDocuments();
+
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, familyManNumber, migrationId);
+
+            CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
+
+            assertThat(extractedCaseData.getOtherCourtAdminDocuments())
+                .isEqualTo(List.of(otherCourtAdminDocuments.get(0)));
+        }
+
+        @Test
+        void shouldThrowExceptionWhenOtherCourtAdminDocumentIdDoesNotMatch() {
+            DocumentReference document = testDocumentReference();
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = List.of(
+                element(documentId1,
+                    CourtAdminDocument.builder().documentTitle(documentTitle1).document(document).build()),
+                element(UUID.randomUUID(),
+                    CourtAdminDocument.builder().documentTitle(documentTitle2).document(document).build()),
+                element(documentId3,
+                    CourtAdminDocument.builder().documentTitle(documentTitle3).document(document).build()),
+                element(documentId4,
+                    CourtAdminDocument.builder().documentTitle(documentTitle4).document(document).build()),
+                element(documentId5,
+                    CourtAdminDocument.builder().documentTitle(documentTitle5).document(document).build()),
+                element(documentId6,
+                    CourtAdminDocument.builder().documentTitle(documentTitle6).document(document).build()));
+
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, familyManNumber, migrationId);
+
+            assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
+                .getRootCause()
+                .hasMessage(String.format(
+                    "Migration FPLA-3175: Expected other court admin document Id %s and document title %s "
+                        + "but not found", documentId2, documentTitle2));
+        }
+
+        @Test
+        void shouldThrowExceptionWhenOtherCourtAdminDocumentTitleDoesNotMatch() {
+            DocumentReference document = testDocumentReference();
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = List.of(
+                element(documentId1,
+                    CourtAdminDocument.builder().documentTitle(documentTitle1).document(document).build()),
+                element(documentId2,
+                    CourtAdminDocument.builder().documentTitle(documentTitle2).document(document).build()),
+                element(documentId3,
+                    CourtAdminDocument.builder().documentTitle("incorrect title").document(document).build()),
+                element(documentId4,
+                    CourtAdminDocument.builder().documentTitle(documentTitle4).document(document).build()),
+                element(documentId5,
+                    CourtAdminDocument.builder().documentTitle(documentTitle5).document(document).build()),
+                element(documentId6,
+                    CourtAdminDocument.builder().documentTitle(documentTitle6).document(document).build()));
+
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, familyManNumber, migrationId);
+
+            assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
+                .getRootCause()
+                .hasMessage(String.format(
+                    "Migration FPLA-3175: Expected other court admin document Id %s and document title %s "
+                        + "but not found", documentId3, documentTitle3));
+        }
+
+        @Test
+        void shouldThrowExceptionWhenAllExpectedOtherCourtDocumentsDoNotExist() {
+            DocumentReference document = testDocumentReference();
+            // documentId6 is missing
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = List.of(
+                element(documentId1,
+                    CourtAdminDocument.builder().documentTitle(documentTitle1).document(document).build()),
+                element(documentId2,
+                    CourtAdminDocument.builder().documentTitle(documentTitle2).document(document).build()),
+                element(documentId3,
+                    CourtAdminDocument.builder().documentTitle(documentTitle3).document(document).build()),
+                element(documentId4,
+                    CourtAdminDocument.builder().documentTitle(documentTitle4).document(document).build()),
+                element(documentId5,
+                    CourtAdminDocument.builder().documentTitle(documentTitle5).document(document).build()));
+
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, familyManNumber, migrationId);
+
+            assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
+                .getRootCause()
+                .hasMessage(String.format(
+                    "Migration FPLA-3175: Expected other court admin document Id %s and document title %s "
+                        + "but not found", documentId6, documentTitle6));
+        }
+
+        @Test
+        void shouldThrowExceptionForIncorrectFamilyManNumber() {
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = buildExpectedOtherCourtAdminDocuments();
+
+            String incorrectFamilyManId = "12456";
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, incorrectFamilyManId, migrationId);
+
+            assertThatThrownBy(() -> postAboutToSubmitEvent(caseDetails))
+                .getRootCause()
+                .hasMessage(String.format(
+                    "Migration FPLA-3175: Expected family man case number to be %s but was %s",
+                    familyManNumber, incorrectFamilyManId));
+        }
+
+        @Test
+        void shouldNotMigrateCaseForIncorrectMigrationId() {
+            List<Element<CourtAdminDocument>> otherCourtAdminDocuments = buildExpectedOtherCourtAdminDocuments();
+
+            String incorrectMigrationId = "incorrect migration id";
+            CaseDetails caseDetails = caseDetails(otherCourtAdminDocuments, familyManNumber, incorrectMigrationId);
+
+            CaseData extractedCaseData = extractCaseData(postAboutToSubmitEvent(caseDetails));
+
+            assertThat(extractedCaseData.getOtherCourtAdminDocuments()).isEqualTo(otherCourtAdminDocuments);
+        }
+
+        private List<Element<CourtAdminDocument>> buildExpectedOtherCourtAdminDocuments() {
+            return List.of(
+                element(UUID.randomUUID(),
+                    CourtAdminDocument.builder().documentTitle("document1").document(document).build()),
+                element(documentId1,
+                    CourtAdminDocument.builder().documentTitle(documentTitle1).document(document).build()),
+                element(documentId2,
+                    CourtAdminDocument.builder().documentTitle(documentTitle2).document(document).build()),
+                element(documentId3,
+                    CourtAdminDocument.builder().documentTitle(documentTitle3).document(document).build()),
+                element(documentId4,
+                    CourtAdminDocument.builder().documentTitle(documentTitle4).document(document).build()),
+                element(documentId5,
+                    CourtAdminDocument.builder().documentTitle(documentTitle5).document(document).build()),
+                element(documentId6,
+                    CourtAdminDocument.builder().documentTitle(documentTitle6).document(document).build()));
+        }
+
+        private CaseDetails caseDetails(List<Element<CourtAdminDocument>> otherCourtAdminDocuments,
+                                        String familyManCaseNumber,
+                                        String migrationId) {
+            CaseDetails caseDetails = asCaseDetails(CaseData.builder()
+                .state(State.CASE_MANAGEMENT)
+                .otherCourtAdminDocuments(otherCourtAdminDocuments)
+                .familyManCaseNumber(familyManCaseNumber)
+                .build());
+
+            caseDetails.getData().put("migrationId", migrationId);
+            return caseDetails;
+        }
+
     }
 
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
