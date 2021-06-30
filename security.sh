afd4bbcc5e6e3083aaa106335a1b9120b2532723
Rob Doherty <robdohertykelly@gmail.com>
2021-06-30 12:06:14 +0100

    fix: reorder zap arguments for nightly pipeline

--- security.sh
+++ security.sh
@@ -9,6 +9,6 @@ zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SE
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
 cp report.json functional-output/
 cp api-report.html functional-output/
-zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
