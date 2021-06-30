a392671234603b6943d272d226de38f06db4fdc6
Samuel Vrech <samuelzero@gmail.com>
2021-06-30 11:59:20 +0100

    FPLA-1830: fix code compile issue

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/others/OtherRecipientsInboxTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/others/OtherRecipientsInboxTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.Other;
 import uk.gov.hmcts.reform.fpl.model.Others;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
 
+import java.util.List;
 import java.util.UUID;
 
+import static java.util.Collections.emptyList;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static uk.gov.hmcts.reform.fpl.utils.TestDataHelper.testAddress;
 
 class OtherRecipientsInboxTest {
 
@@ -17,16 +21,17 @@ class OtherRecipientsInboxTest {
     void testShouldReturnNonSelectedRecipients() {
         CaseData caseData = CaseData.builder()
             .others(Others.builder().firstOther(buildOther("First other", emptyList()))
-            .additionalOthers(List.of())
-            .build()
+            .additionalOthers(List.of()).build())
+            .build();
     }
 
     public static Other buildOther(String name, List<Element<UUID>> representedBy) {
-        return Other.builder()
+        Other other = Other.builder()
             .name(name)
             .birthPlace(randomAlphanumeric(10))
             .address(testAddress())
-            .representedBy(representedBy)
             .build();
+        representedBy.forEach(x -> other.addRepresentative(x.getId()));
+        return other;
     }
 }
