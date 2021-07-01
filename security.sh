38f6be94cf66ecce2b7b0ea6badfbed06843d2e0
Rob Doherty <robdohertykelly@gmail.com>
2021-07-01 10:14:24 +0100

    debugging the env vars

--- security.sh
+++ security.sh
@@ -7,9 +7,12 @@ zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SE
 cat zap.out
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-export PYTHONDONTWRITEBYTECODE=1
+#export LC_ALL=C.UTF-8
+#export LANG=C.UTF-8
+#export PYTHONDONTWRITEBYTECODE=1
+echo "LC_ALL = ${LC_ALL}"
+echo "LANG = ${LANG}"
+echo "PYTHONDONTWRITEBYTECODE = ${PYTHONDONTWRITEBYTECODE}"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o api-report.html -f html
 zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Informational --exit-code False
 cp report.json functional-output/
