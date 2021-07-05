63ac977859613dadfef97da319cbf9c1463a162a
Joe Zazzaro-Francis <joez@kainos.com>
2021-07-05 10:43:26 +0100

    deleted old e2e flow for SDO

--- e2e/config.js
+++ e2e/config.js
@@ -148,7 +148,6 @@ module.exports = {
     createNoticeOfProceedings: 'Create notice of proceedings',
     addStatementOfService: 'Add statement of service (c9)',
     uploadC2Documents: 'Upload a C2',
-    draftStandardDirections: 'Add the gatekeeping order',
     addGatekeepingOrder: 'Add the gatekeeping order',
     createOrder: 'Create or upload an order',
     placement: 'Placement',
