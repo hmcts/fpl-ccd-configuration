480ab49b72d5de6e5397cf768f70a593643bc0f1
Tomasz Powroznik <tomas.powroznik@gmail.com>
2021-07-07 12:34:06 +0100

    feat: Allow multiple attendance options (FPLA-3117)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ManageHearingsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ManageHearingsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.events.AfterSubmissionCaseDataUpdated;
 import uk.gov.hmcts.reform.fpl.events.PopulateStandardDirectionsOrderDatesEvent;
 import uk.gov.hmcts.reform.fpl.events.SendNoticeOfHearing;
