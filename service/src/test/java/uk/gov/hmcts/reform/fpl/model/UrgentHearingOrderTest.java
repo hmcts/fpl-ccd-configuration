e2a5ce3b04cecbad9d4fc4c0035a97cf6b6d9243
Toireasa Moley <t.moley@kainos.com>
2021-07-16 15:24:30 +0100

    Fixing upper case

--- service/src/test/java/uk/gov/hmcts/reform/fpl/model/UrgentHearingOrderTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/model/UrgentHearingOrderTest.java
@@ -15,7 +15,7 @@ class UrgentHearingOrderTest {
     @Test
     void shouldReturnAmendedOrderType() {
         UrgentHearingOrder urgentHearingOrder = UrgentHearingOrder.builder().build();
-        assertThat(urgentHearingOrder.getAmendedOrderType()).isEqualTo("urgent hearing order");
+        assertThat(urgentHearingOrder.getAmendedOrderType()).isEqualTo("Urgent hearing order");
     }
 
     @Test
