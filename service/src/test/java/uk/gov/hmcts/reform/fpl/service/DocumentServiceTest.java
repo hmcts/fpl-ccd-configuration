2dbcac336b7e77c2ed2fcd819a7e6a39b1d31d87
Joe Zazzaro-Francis <joez@kainos.com>
2021-07-30 15:50:17 +0100

    deleted legacy docmosis template enum + api test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/DocumentServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/DocumentServiceTest.java
@@ -15,7 +15,7 @@ import uk.gov.hmcts.reform.fpl.service.docmosis.DocmosisDocumentGeneratorService
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.ORDER;
+import static uk.gov.hmcts.reform.fpl.enums.DocmosisTemplates.ORDER_V2;
 import static uk.gov.hmcts.reform.fpl.utils.DocumentManagementStoreLoader.document;
 
 @ExtendWith(SpringExtension.class)
@@ -42,7 +42,7 @@ class DocumentServiceTest {
 
         initMocks(template);
 
-        assertThat(service.getDocumentFromDocmosisOrderTemplate(template, ORDER)).isEqualTo(DOCUMENT);
+        assertThat(service.getDocumentFromDocmosisOrderTemplate(template, ORDER_V2)).isEqualTo(DOCUMENT);
         assertThat(captor.getValue()).isEqualTo("draft-order.pdf");
     }
 
@@ -51,14 +51,14 @@ class DocumentServiceTest {
         DocmosisOrder template = DocmosisOrder.builder().build();
         initMocks(template);
 
-        assertThat(service.getDocumentFromDocmosisOrderTemplate(template, ORDER)).isEqualTo(DOCUMENT);
+        assertThat(service.getDocumentFromDocmosisOrderTemplate(template, ORDER_V2)).isEqualTo(DOCUMENT);
         assertThat(captor.getValue()).isEqualTo("order.pdf");
     }
 
     private void initMocks(DocmosisOrder template) {
         byte[] bytes = new byte[] {1, 2, 3};
 
-        when(documentGeneratorService.generateDocmosisDocument(template, ORDER))
+        when(documentGeneratorService.generateDocmosisDocument(template, ORDER_V2))
             .thenReturn(DocmosisDocument.builder().bytes(bytes).documentTitle("order.pdf").build());
 
         when(uploadDocumentService.uploadPDF(eq(bytes), captor.capture())).thenReturn(DOCUMENT);
