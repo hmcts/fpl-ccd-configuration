917646c97d6860842d78ae3c98190b52dc7a8e2a
Rob Doherty <robdohertykelly@gmail.com>
2021-08-02 17:16:53 +0100

    chore: move security.sh debugging steps to end

--- security.sh
+++ security.sh
@@ -17,16 +17,14 @@ echo "Run ZAP scan"
 zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py
 echo "Generate report.json"
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-echo "Print zap.out logs"
-cat zap.out
 
-echo "LC_ALL: ${LC_ALL}"
-echo "LANG: ${LANG}"
-echo "PYTHONDONTWRITEBYTECODE: ${PYTHONDONTWRITEBYTECODE}"
 echo "Generate api-report.html"
 callZapCli "report -o api-report.html -f html"
 echo "Retrieve alerts"
 callZapCli "alerts -l Informational --exit-code False"
+echo "LC_ALL: ${LC_ALL}"
+echo "LANG: ${LANG}"
+echo "PYTHONDONTWRITEBYTECODE: ${PYTHONDONTWRITEBYTECODE}"
 echo "Print zap.out logs"
 cat zap.out
 echo "Copy artifacts for archiving"
