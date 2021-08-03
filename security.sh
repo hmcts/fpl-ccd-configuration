549469782e7dccbb02c808e9206d8389bd88e13e
Rob Doherty <robdohertykelly@gmail.com>
2021-08-03 09:09:06 +0100

    test: verify cnp-jenkins-library change is backwards compatible

--- security.sh
+++ security.sh
@@ -18,13 +18,15 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export PYTHONDONTWRITEBYTECODE=1
 echo "Run ZAP scan"
-zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py -J report.json -r api-report.html
-#echo "Generate report.json"
-#curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+#zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py -J report.json -r api-report.html
 
-#echo "Generate api-report.html"
+zap-api-scan.py -t ${URL_FOR_SECURITY_SCAN}/v2/api-docs -f openapi -S -d -u ${SECURITY_RULES} -P 1001 -l FAIL --hook=zap_hooks.py
+echo "Generate report.json"
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+echo "Generate api-report.html"
 #checkZapStatus
-#callZapCli "report -o api-report.html -f html"
+callZapCli "report -o api-report.html -f html"
+
 echo "Retrieve alerts"
 callZapCli "alerts -l Informational --exit-code False"
 checkZapStatus
