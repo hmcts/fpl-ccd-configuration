d7dba6ea29ed3b0cd8ebff8f9db1c6e80af07d5e
Soorya Suresh <soorya11@hotmail.co.uk>
2021-08-03 12:31:00 +0100

    feat: Choose recipients for Notice of Hearing (FPLA-2446) (#2878)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/representative/RepresentativeNotificationService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/representative/RepresentativeNotificationService.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.fpl.enums.RepresentativeServingPreferences;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.Other;
 import uk.gov.hmcts.reform.fpl.model.Representative;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.notify.NotifyData;
 import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
 import uk.gov.hmcts.reform.fpl.service.email.RepresentativesInbox;
+import uk.gov.hmcts.reform.fpl.service.others.OtherRecipientsInbox;
 
 import java.util.List;
 import java.util.Set;
@@ -21,12 +24,27 @@ public class RepresentativeNotificationService {
 
     private final NotificationService notificationService;
     private final RepresentativesInbox representativesInbox;
+    private final OtherRecipientsInbox otherRecipientsInbox;
 
     public void sendToRepresentativesByServedPreference(final RepresentativeServingPreferences servedPreference,
                                                         final String templateId,
                                                         final NotifyData notifyData,
                                                         final CaseData caseData) {
         Set<String> emailRepresentatives = representativesInbox.getEmailsByPreference(caseData, servedPreference);
+        sendNotificationToRepresentatives(caseData.getId(), notifyData, emailRepresentatives, templateId);
+    }
+
+    public void sendToRepresentativesByServedPreference(final RepresentativeServingPreferences servedPreference,
+                                                        final String templateId,
+                                                        final NotifyData notifyData,
+                                                        final CaseData caseData,
+                                                        final List<Element<Other>> othersSelected) {
+        Set<String> emailRepresentatives = representativesInbox.getEmailsByPreference(caseData, servedPreference);
+
+        emailRepresentatives.removeAll(otherRecipientsInbox.getNonSelectedRecipients(
+            servedPreference, caseData, othersSelected, element -> element.getValue().getEmail()
+        ));
+
         sendNotificationToRepresentatives(caseData.getId(), notifyData, emailRepresentatives, templateId);
     }
 
