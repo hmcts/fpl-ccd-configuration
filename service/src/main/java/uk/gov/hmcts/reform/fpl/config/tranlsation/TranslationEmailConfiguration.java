a5d3b7ab25edf48e3dfa66facfad6137a0bd1deb
Samuel Vrech <samuelzero@gmail.com>
2021-08-03 14:00:15 +0100

    FPLA-3149: fix spring stuff

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/tranlsation/TranslationEmailConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/tranlsation/TranslationEmailConfiguration.java
@@ -1,13 +1,19 @@
 package uk.gov.hmcts.reform.fpl.config.tranlsation;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Value
+@Getter
+@Setter
 @Configuration
+@NoArgsConstructor
+@AllArgsConstructor
 @ConfigurationProperties(prefix = "translation.unit.notification")
 public class TranslationEmailConfiguration {
-    String sender;
-    String recipient;
+    private String sender;
+    private String recipient;
 }
