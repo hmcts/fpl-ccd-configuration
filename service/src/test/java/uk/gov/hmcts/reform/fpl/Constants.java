e2dd59325ad8bb4ecbdb23bfec7146efea01f3d0
Tomasz Powroznik <tomas.powroznik@gmail.com>
2021-08-12 09:42:08 +0100

    chore: Remove fpl service token from integrtion tests Constants

--- service/src/test/java/uk/gov/hmcts/reform/fpl/Constants.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/Constants.java
@@ -1,21 +1,7 @@
 package uk.gov.hmcts.reform.fpl;
 
-@SuppressWarnings("LineLength")
 public interface Constants {
 
-    /**
-     * Service auth token issued for 'fpl_case_service' valid until end of the day on 31st December 2049.
-     *
-     * <p>Decoded payload:
-     *
-     * <code>
-     * {
-     *   "sub": "fpl_case_service",
-     *   "exp": 2524607999
-     * }
-     * </code>
-     */
-    String SERVICE_AUTH_TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmcGxfY2FzZV9zZXJ2aWNlIiwiZXhwIjoyNTI0NjA3OTk5fQ.LLvcAFmBnIpTbmuoEBuFUzRE6prE0_1ALbCkEkY6mS0kHPQVghgWPNBeVOKUoa-w1P1IUqZ5H-6eYnDzJRxTwA";
     String LOCAL_AUTHORITY_1_CODE = "test1";
     String LOCAL_AUTHORITY_2_CODE = "test2";
     String LOCAL_AUTHORITY_3_CODE = "test3";
