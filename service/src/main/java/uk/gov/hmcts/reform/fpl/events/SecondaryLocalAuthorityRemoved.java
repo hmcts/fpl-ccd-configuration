4abb57498baac4b3a5fad35d90fcd6d25f46c475
tomaszpowroznik <tomas.powroznik@gmail.com>
2021-08-19 13:29:32 +0200

    feat: Add secondary LA (FPLA-2002) (#2950)
    * feat: Add secondary LA (FPLA-2002)
    
    * Addess code review comments
    
    Co-authored-by: Rob Doherty-Kelly <Rob-Doherty@users.noreply.github.com>
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/events/SecondaryLocalAuthorityRemoved.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.fpl.events;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class SecondaryLocalAuthorityRemoved {
+
+    private final CaseData caseData;
+    private final CaseData caseDataBefore;
+}
