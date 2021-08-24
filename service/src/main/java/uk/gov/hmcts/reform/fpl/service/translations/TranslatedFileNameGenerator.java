942ff4e95b1734591217fa13023233f2542b22e6
Samuel Vrech <samuelzero@gmail.com>
2021-08-24 20:06:01 +0100

    FPLA-3325: suffix fix checkstyle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/translations/TranslatedFileNameGenerator.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/translations/TranslatedFileNameGenerator.java
@@ -11,9 +11,11 @@ public class TranslatedFileNameGenerator {
     public String generate(CaseData caseData,
                            LanguageTranslationRequirement translationRequirements) {
         String filename = caseData.getUploadTranslationsEventData().getUploadTranslationsOriginalDoc().getFilename();
-        String base = FilenameUtils.removeExtension(filename);
-        String extension = FilenameUtils.getExtension(filename);
-        String amendedFileName = base + "-" + translationRequirements.getTargetLanguage().get().getLabel() + "." + extension;
-        return amendedFileName;
+
+        return String.format("%s-%s.%s",
+            FilenameUtils.removeExtension(filename),
+            translationRequirements.getTargetLanguage().get().getLabel(),
+            FilenameUtils.getExtension(filename)
+        );
     }
 }
