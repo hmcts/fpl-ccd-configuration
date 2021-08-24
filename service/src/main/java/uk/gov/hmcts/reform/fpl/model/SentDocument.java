5fd8085168e1211936d345b859d5f8a75c889ed4
Samuel Vrech <samuelzero@gmail.com>
2021-08-24 18:08:31 +0100

    FPLA-2867: remove Language for serialisation

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/SentDocument.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/SentDocument.java
@@ -1,14 +1,9 @@
 package uk.gov.hmcts.reform.fpl.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
-import uk.gov.hmcts.reform.fpl.model.configuration.Language;
-
-import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
-import static uk.gov.hmcts.reform.fpl.model.configuration.Language.ENGLISH;
 
 @Data
 @Builder(toBuilder = true)
@@ -19,10 +14,4 @@ public class SentDocument {
     DocumentReference coversheet;
     String sentAt;
     String letterId;
-    Language language;
-
-    @JsonIgnore
-    public Language getLanguage() {
-        return defaultIfNull(language, ENGLISH);
-    }
 }
