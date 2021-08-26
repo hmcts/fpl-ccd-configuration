6ff8c3e7aa8c23290c8f26e7129e4ce98ca33409
Soorya <soorya11@hotmail.co.uk>
2021-08-26 14:13:06 +0100

    * Fix a failing integration test

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ManageHearingsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ManageHearingsController.java
@@ -114,6 +114,10 @@ public class ManageHearingsController extends CallbackController {
 
         List<String> errors = validateGroupService.validateGroup(caseData, HearingBookingGroup.class);
 
+        if (!errors.isEmpty()) {
+            return respond(caseDetails, errors);
+        }
+
         if (NEW_HEARING == caseData.getHearingOption()) {
             caseDetails.getData().putAll(hearingsService.initiateNewHearing(caseData));
             caseDetails.getData()
