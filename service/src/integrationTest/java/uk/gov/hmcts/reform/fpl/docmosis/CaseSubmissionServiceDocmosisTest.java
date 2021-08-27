0654bc88718f7cdbdbf2e78b912ed2363ec62f68
Samuel Vrech <samuelzero@gmail.com>
2021-08-27 09:07:28 +0100

    uncomment template test

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/docmosis/CaseSubmissionServiceDocmosisTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/docmosis/CaseSubmissionServiceDocmosisTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.fpl.docmosis;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -96,7 +95,6 @@ class CaseSubmissionServiceDocmosisTest extends AbstractDocmosisTest {
     }
 
     @Test
-    @Disabled
     void testC110aWelsh() throws IOException {
 
         caseData = populatedCaseData().toBuilder()
