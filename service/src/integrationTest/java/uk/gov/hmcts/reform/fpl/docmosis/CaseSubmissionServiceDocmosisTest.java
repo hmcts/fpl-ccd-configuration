0dc3715ca4d19a5a971a65852dd05cbb4c691ab4
samuelzerozero <50642657+samuelzerozero@users.noreply.github.com>
2021-09-22 12:41:30 +0200

    FPLA-3344:  Welsh C11a enhanced translation (#3048)
    * C110a welsh i11n
    
    * fix tests
    
    * ignore till docmosis is back
    
    * add extra tests
    
    * remove unused class
    
    * remove unused class
    
    * uncomment template test
    
    * FPLA-3325: Add review comment changes
    
    * add welsh translations
    
    * update c110a-wesh.txt content
    
    * apply feedback and fix tests
    
    * added missing translations
    
    * update test content
    
    Co-authored-by: Megan <meganaliceslattery@gmail.com>
    Co-authored-by: TSeelig <thomas.seelig@HMCTS.NET>

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/docmosis/CaseSubmissionServiceDocmosisTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/docmosis/CaseSubmissionServiceDocmosisTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.docmosis;
 
+import org.apache.commons.text.StringSubstitutor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.SpyBeans;
 import org.springframework.test.context.ContextConfiguration;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.common.DocmosisDocument;
+import uk.gov.hmcts.reform.fpl.model.group.C110A;
 import uk.gov.hmcts.reform.fpl.service.CourtService;
 import uk.gov.hmcts.reform.fpl.service.UploadDocumentService;
 import uk.gov.hmcts.reform.fpl.service.UserService;
@@ -24,12 +26,15 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.fpl.model.configuration.Language.ENGLISH;
+import static uk.gov.hmcts.reform.fpl.model.configuration.Language.WELSH;
 import static uk.gov.hmcts.reform.fpl.utils.CoreCaseDataStoreLoader.populatedCaseData;
 import static uk.gov.hmcts.reform.fpl.utils.ResourceReader.readString;
 
@@ -37,8 +42,8 @@ import static uk.gov.hmcts.reform.fpl.utils.ResourceReader.readString;
     CaseSubmissionService.class, DocmosisDocumentGeneratorService.class, CaseSubmissionGenerationService.class,
     DocmosisHelper.class, FixedTimeConfiguration.class, CaseSubmissionDocumentAnnexGenerator.class
 })
-@SpyBeans({@SpyBean(DocmosisDocumentGeneratorService.class)})
-@MockBeans({
+@SpyBeans(value = {@SpyBean(DocmosisDocumentGeneratorService.class)})
+@MockBeans(value = {
     @MockBean(UploadDocumentService.class), @MockBean(CourtService.class), @MockBean(UserService.class),
     @MockBean(Time.class)
 })
@@ -46,7 +51,6 @@ class CaseSubmissionServiceDocmosisTest extends AbstractDocmosisTest {
     private static final String LA_COURT = "La Court";
     private static final String USER_NAME = "user name";
     private static final LocalDate NOW = LocalDate.of(12, 12, 12);
-    private static final String EXPECTED_TEXT_PATH = "c110a.txt";
 
     @Autowired
     private Time time;
@@ -65,7 +69,6 @@ class CaseSubmissionServiceDocmosisTest extends AbstractDocmosisTest {
 
     @BeforeEach
     void setUp() {
-        caseData = populatedCaseData().toBuilder().languageRequirement("Yes").build();
 
         when(user.getUserName()).thenReturn(USER_NAME);
         when(courtService.getCourtName(any())).thenReturn(LA_COURT);
@@ -74,29 +77,77 @@ class CaseSubmissionServiceDocmosisTest extends AbstractDocmosisTest {
 
     @Test
     void testC110a() throws IOException {
+
+        caseData = populatedCaseData().toBuilder()
+            .languageRequirement("Yes")
+            .c110A(C110A.builder()
+                .languageRequirementApplication(ENGLISH)
+                .build())
+            .build();
+
         doAnswer(resultsCaptor).when(generatorService).generateDocmosisDocument(anyMap(), any(), any(), any());
 
         underTest.generateSubmittedFormPDF(caseData, false);
 
-        String c110aContent = getPdfContent();
+        String c110aContent = getPdfContent("c110a.pdf");
 
-        String expectedText = getExpectedText();
+        String expectedText = readString("c110a.txt");
+        expectedText = StringSubstitutor.replace(expectedText, Map.of("languageRequirement", "Yes"));
 
         assertThat(c110aContent).isEqualToNormalizingWhitespace(expectedText);
     }
 
-    private String getPdfContent() throws IOException {
+    @Test
+    void testC110aNoLanguageRequirement() throws IOException {
+
+        caseData = populatedCaseData().toBuilder()
+            .c110A(C110A.builder()
+                .build())
+            .build();
+
+        doAnswer(resultsCaptor).when(generatorService).generateDocmosisDocument(anyMap(), any(), any(), any());
+
+        underTest.generateSubmittedFormPDF(caseData, false);
+
+        String c110aContent = getPdfContent("c110a.pdf");
+
+        String expectedText = readString("c110a.txt");
+        expectedText = StringSubstitutor.replace(expectedText, Map.of("languageRequirement", "No"));
+
+        assertThat(c110aContent).isEqualToNormalizingWhitespace(expectedText);
+    }
+
+    @Test
+    void testC110aWelsh() throws IOException {
+
+        caseData = populatedCaseData().toBuilder()
+            .languageRequirement("Yes")
+            .c110A(C110A.builder()
+                .languageRequirementApplication(WELSH)
+                .build())
+            .build();
+
+
+        doAnswer(resultsCaptor).when(generatorService).generateDocmosisDocument(anyMap(), any(), any(), any());
+
+        underTest.generateSubmittedFormPDF(caseData, false);
+
+        String c110aContent = getPdfContent("c110a-Welsh.pdf");
+
+        String expectedText = readString("c110a-Welsh.txt");
+
+        assertThat(c110aContent).isEqualToNormalizingWhitespace(expectedText);
+    }
+
+    private String getPdfContent(String fileName) throws IOException {
         DocmosisDocument docmosisDocument = resultsCaptor.getResult();
 
         byte[] bytes = docmosisDocument.getBytes();
 
-        storeToOuputFolder("c110a.pdf", bytes);
+        storeToOuputFolder(fileName, bytes);
 
         String text = docmosisHelper.extractPdfContent(bytes);
         return docmosisHelper.remove(text, "C110A");
     }
 
-    private String getExpectedText() {
-        return readString(EXPECTED_TEXT_PATH);
-    }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/utils/EmergencyProtectionOrderReasonsType.java
@@ -1,22 +1,37 @@
 package uk.gov.hmcts.reform.fpl.config.utils;
 
+import uk.gov.hmcts.reform.fpl.model.configuration.Language;
+
 public enum EmergencyProtectionOrderReasonsType {
 
     HARM_IF_NOT_MOVED_TO_NEW_ACCOMMODATION(
         "There’s reasonable cause to believe the child is likely to suffer significant harm if they’re not moved"
-            + " to accommodation provided by you, or on your behalf"),
+            + " to accommodation provided by you, or on your behalf",
+        "Mae rheswm rhesymol dros gredu bod y plentyn yn debygol o ddioddef niwed sylweddol os"
+            + " na chaiff ei symud i lety a ddarperir gennych chi, neu ar eich rhan"),
     HARM_IF_KEPT_IN_CURRENT_ACCOMMODATION("There’s reasonable cause to believe the child is likely to"
-        + " suffer significant harm if they don’t stay in their current accommodation"),
+        + " suffer significant harm if they don’t stay in their current accommodation",
+        "Mae rheswm rhesymol dros gredu bod y plentyn yn debygol o"
+            + " ddioddef niwed sylweddol os nad yw'n aros yn ei lety presennol"),
     URGENT_ACCESS_TO_CHILD_IS_OBSTRUCTED("You’re making enquiries and need urgent access to the child to find out"
-        + " about their welfare, and access is being unreasonably refused");
+        + " about their welfare, and access is being unreasonably refused",
+        "Rydych yn gwneud ymholiadau ac angen cyswllt brys â'r plentyn i gael"
+            + " gwybod am ei les, ac mae'n afresymol bod mynediad yn cael ei wrthod");
 
     private final String label;
+    private final String welshLabel;
 
-    EmergencyProtectionOrderReasonsType(String label) {
+    EmergencyProtectionOrderReasonsType(String label, String welshLabel) {
         this.label = label;
+        this.welshLabel = welshLabel;
     }
 
     public String getLabel() {
         return label;
     }
+
+    public String getLabel(Language language) {
+        return language == Language.WELSH ? welshLabel : label;
+    }
+
 }
