9dc5edba30544bdf002d4fd7a6f9bf2c53e6029e
Soorya Suresh <soorya11@hotmail.co.uk>
2021-11-18 10:22:16 +0000

    impr: Update hearing judge after a hearing (FPLA-3098) (#3059)
    * * Initial commit
    
    * * Allow the user to specify when a hearing was vacated
    
    * * Fix a failing integration test
    
    * * Improve test coverage
     * Tweak E2E tests for the update journey for vacating a hearing
    
    * * Tweak some field names
     * Added more tests for CaseData
     * Fix a bug where it was not possible to select past hearings to be vacated
    
    * * Ensure the warning message for past hearings appear
    
    * * Tweak a variable name
    
    * * Minor correction
    
    * * Initial commit
    
    * * Rename a constant
    
    * * Remove an unnecessary field
     * Correct a test name
    
    * * Implement code review suggestions
     * Fix a broken E2E test
    
    * * Revert an earlier change for hearing date lists when vacating a hearing
    
    * * Fix E2E test
    * Ensure the Justices' Legal Adviser's full name is updated as expected when editing a hearing
    
    * * Fix E2E test
    
    * * Ensure vacated hearing list is in reverse chronological order
    
    * * Ensure the edit hearing list is not sorted (maintaining current behaviour)
    
    * * Fix a bug that prevents the user from editing a hearing if the only hearings available are in the past
    
    * Set new fee code :|
    
    * remove the GAOnNotice fee code for now
    
    * FPLA-3098: Fixing Fee payments and vulnerability
    
    * FPLA-3098: Fixing C2 keyword
    
    * DFPL-170: Add futureHearingList to split the edit event into two
    
    * DFPL-170: Utilise the right DateLists to choose past/future hearings separately
    
    * DFPL-170: Split the past+future hearing flag to use separately
    
    * DFPL-170: Update edit tests to deal with editing both past and future events
    
    * DFPL-170: Refactor hearingDateList -> pastHearingDateList
    
    * Bump spring boot
    
    * DFPL-170: Update unit tests to take account of the past-future split
    
    * DFPL-170: Add few tests so that both past + future hearings are tested
    
    * DFPL-170: Fix vacate hearings to stop utilising pastHearing flag and revert edit future hearing to work as before
    
    * DFPL-170: Add edit past event functional test
    
    * DFPL-170: Add old label to deprecated file
    
    * 4 hrs for nightly
    
    Co-authored-by: amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    Co-authored-by: Jason Paige <jason.r.paige@gmail.com>
    Co-authored-by: SomeshRA <somesh.acharya1@hmcts.net>
    Co-authored-by: nraghu32 <44773331+nraghu32@users.noreply.github.com>
    Co-authored-by: Daniel Catchpole <daniel.catchpole@justice.gov.uk>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/enums/HearingOptions.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/enums/HearingOptions.java
@@ -2,7 +2,8 @@ package uk.gov.hmcts.reform.fpl.enums;
 
 public enum HearingOptions {
     NEW_HEARING,
-    EDIT_HEARING,
+    EDIT_PAST_HEARING,
+    EDIT_FUTURE_HEARING,
     ADJOURN_HEARING,
     VACATE_HEARING,
     RE_LIST_HEARING
