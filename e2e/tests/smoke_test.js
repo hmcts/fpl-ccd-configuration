9fd692ed464daf2541444c920ae38588c4ebc290
Jason Paige <jason.r.paige@gmail.com>
2021-12-07 18:08:48 +0000

    reinstate smoke tests

--- e2e/tests/smoke_test.js
+++ e2e/tests/smoke_test.js
@@ -1,13 +1,23 @@
-// const config = require('../config');
-// const moment = require('moment');
+const config = require('../config');
+const moment = require('moment');
 
 Feature('Smoke tests @smoke-tests');
 
-Scenario('Sign in as local authority and create a case', async ({I}) => {
-  await I.goToPage('https://www.google.com/');
-  // const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
-  // const caseId = await I.logInAndCreateCase(config.swanseaLocalAuthorityUserOne, caseName);
-  // I.navigateToCaseList();
-  // await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), `//ccd-search-result/table//tr//td//a[contains(@href,'${caseId}')]`);
-  // I.seeCaseInSearchResult(caseId);
+// <<<<<<< HEAD
+// Scenario('Sign in as local authority and create a case', async ({I}) => {
+//   await I.goToPage('https://www.google.com/');
+//   // const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
+//   // const caseId = await I.logInAndCreateCase(config.swanseaLocalAuthorityUserOne, caseName);
+//   // I.navigateToCaseList();
+//   // await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), `//ccd-search-result/table//tr//td//a[contains(@href,'${caseId}')]`);
+//   // I.seeCaseInSearchResult(caseId);
+// =======
+Scenario('Sign in as local authority and create a case', async ({I, caseListPage}) => {
+  await I.goToPage(config.baseUrl);
+  const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
+  const caseId = await I.logInAndCreateCase(config.swanseaLocalAuthorityUserOne, caseName);
+  I.navigateToCaseList();
+  await I.retryUntilExists(() => caseListPage.searchForCasesWithName(caseName, 'Open'), `//ccd-search-result/table//tr//td//a[contains(@href,'${caseId}')]`);
+  I.seeCaseInSearchResult(caseId);
+// >>>>>>> reinstate smoke tests
 });
