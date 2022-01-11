f0e7901ae2b32c528d3775d1d769de006a955152
Ashley Wong <50691452+so99y@users.noreply.github.com>
2022-01-11 15:20:52 +0000

    fixed code style

--- service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/EncryptedPdfUploadedException.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/exceptions/EncryptedPdfUploadedException.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.fpl.exceptions;
 
-public class EncryptedPdfUploadedException extends LogAsWarningException
-    // AboutToStartOrSubmitCallbackException
-{
+// extends AboutToStartOrSubmitCallbackException
+public class EncryptedPdfUploadedException extends LogAsWarningException {
+
     public EncryptedPdfUploadedException(String message) {
         super("Encrypted PDF file was uploaded which cannot be processed.", message);
     }
