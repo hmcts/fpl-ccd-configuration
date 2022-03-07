c9ba2284bed7c2eee283401ff96163c855a10bc0
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-03-07 14:17:42 +0000

    DFPL-542: Check respondent solicitor field when checking if the respondent is represented

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/RepresentativesInbox.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/RepresentativesInbox.java
@@ -119,7 +119,7 @@ public class RepresentativesInbox {
     public Set<Recipient> getSelectedRecipientsWithNoRepresentation(List<Element<Respondent>> selectedRespondents) {
         return selectedRespondents.stream()
             .map(Element::getValue)
-            .filter(respondent -> isEmpty(respondent.getRepresentedBy()) && respondent.hasAddress())
+            .filter(respondent -> isEmpty(respondent.getRepresentedBy()) && isEmpty(respondent.getSolicitor()) && respondent.hasAddress())
             .map(Respondent::toParty)
             .collect(Collectors.toCollection(LinkedHashSet::new));
     }
