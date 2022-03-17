46ab1f53d7ac25cf15af2f767e10240a27db0c63
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2022-03-17 11:30:28 +0000

    Ensure document is converted to pdf before sending to print service

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendLetterService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendLetterService.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.fpl.model.SentDocument;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.configuration.Language;
 import uk.gov.hmcts.reform.fpl.service.docmosis.DocmosisCoverDocumentsService;
+import uk.gov.hmcts.reform.fpl.service.docmosis.DocumentConversionService;
 import uk.gov.hmcts.reform.fpl.service.time.Time;
 import uk.gov.hmcts.reform.sendletter.api.LetterWithPdfsRequest;
 import uk.gov.hmcts.reform.sendletter.api.SendLetterApi;
@@ -37,6 +38,7 @@ public class SendLetterService {
     private final Time time;
     private final SendLetterApi sendLetterApi;
     private final DocumentDownloadService documentDownloadService;
+    private final DocumentConversionService documentConversionService;
     private final DocmosisCoverDocumentsService docmosisCoverDocumentsService;
     private final AuthTokenGenerator authTokenGenerator;
     private final UploadDocumentService uploadDocumentService;
@@ -44,6 +46,7 @@ public class SendLetterService {
     public List<SentDocument> send(DocumentReference mainDocument, List<Recipient> recipients, Long caseId,
                                    String familyManCaseNumber, Language language) {
         List<SentDocument> sentDocuments = new ArrayList<>();
+        mainDocument = documentConversionService.convertToPdf(mainDocument); // make sure mainDocument is in PDF format
         byte[] mainDocumentBinary = documentDownloadService.downloadDocument(mainDocument.getBinaryUrl());
         var mainDocumentCopy = uploadDocument(mainDocumentBinary, mainDocument.getFilename());
 
@@ -62,20 +65,19 @@ public class SendLetterService {
                         SEND_LETTER_TYPE,
                         Map.of("caseId", caseId, "documentName", mainDocument.getFilename())));
                 letterId = Optional.ofNullable(response).map(r -> r.letterId.toString()).orElse(EMPTY);
+
+                sentDocuments.add(SentDocument.builder()
+                    .partyName(recipient.getFullName())
+                    .document(mainDocumentCopy)
+                    .coversheet(coversheet)
+                    .sentAt(formatLocalDateTimeBaseUsingFormat(time.now(), "h:mma, d MMMM yyyy"))
+                    .letterId(letterId)
+                    .build());
             } catch (Exception exception) {
                 log.error("Exception raised when sending letter for case id {} and document {}.",
                     caseId, mainDocument.getFilename(), exception);
             }
-
-            sentDocuments.add(SentDocument.builder()
-                .partyName(recipient.getFullName())
-                .document(mainDocumentCopy)
-                .coversheet(coversheet)
-                .sentAt(formatLocalDateTimeBaseUsingFormat(time.now(), "h:mma, d MMMM yyyy"))
-                .letterId(letterId)
-                .build());
         }
-
         return sentDocuments;
     }
 
