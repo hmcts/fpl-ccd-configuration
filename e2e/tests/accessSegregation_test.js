2fb90ae4956cbb234d4d13ce490f5cf9fa8a917e
Vijay K <Vijay.kayyam@HMCTS.NET>
2022-03-22 11:55:36 +0000

    DTSSE-1153: Move UI Formatted function to base.js

--- e2e/tests/accessSegregation_test.js
+++ e2e/tests/accessSegregation_test.js
@@ -8,12 +8,10 @@ async function setupScenario(I) {
   if (!caseId) { caseId = await I.submitNewCaseWithData(); }
 }
 
-function uiFormatted(id) { return id.match(/.{1,4}/g).join('-');}
-
 Scenario('Different user in the same local authority can see case created', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.swanseaLocalAuthorityUserTwo, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
 });
 
 Scenario('Different user in a different local authority cannot see case created', async ({I, caseListPage}) => {
@@ -25,30 +23,30 @@ Scenario('Different user in a different local authority cannot see case created'
 Scenario('HMCTS admin user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.hmctsAdminUser, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
 });
 
 Scenario('CAFCASS user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.cafcassUser, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
 });
 
 Scenario('Gatekeeper user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.gateKeeperUser, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
 });
 
 Scenario('Judiciary user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.judicaryUser, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
 });
 
 Scenario('Magistrate user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.magistrateUser, caseId);
-  I.see(uiFormatted(caseId));
+  I.see(I.uiFormatted(caseId));
   await I.seeAvailableEvents([config.administrationActions.manageDocuments, config.administrationActions.addCaseFlag]);
 });
