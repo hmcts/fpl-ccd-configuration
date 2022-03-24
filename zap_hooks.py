2ab05f767154251549c21d72bd8b9e5df913b951
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-03-24 16:11:34 +0000

    DFPL-554: Ignore zaps about 500 errors as callbacks cannot be called with no params

--- zap_hooks.py
+++ zap_hooks.py
@@ -1,4 +1,4 @@
 def zap_started(zap, target):
-  rules_to_ignore = ('90033', '10023', '10010', '10054', '1096')
+  rules_to_ignore = ('90033', '10023', '10010', '10054', '1096', '100000')
   for rule in rules_to_ignore:
     zap.pscan.set_scanner_alert_threshold(id=rule, alertthreshold='OFF')
