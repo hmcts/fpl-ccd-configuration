049e25dfd5ad63a53e5461c1c441992adc6406d1
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-03-24 16:26:00 +0000

    DFPL-554: Revert zap ignores

--- zap_hooks.py
+++ zap_hooks.py
@@ -1,4 +1,4 @@
 def zap_started(zap, target):
-  rules_to_ignore = ('90033', '10023', '10010', '10054', '1096', '100000', '100001')
+  rules_to_ignore = ('90033', '10023', '10010', '10054', '1096')
   for rule in rules_to_ignore:
     zap.pscan.set_scanner_alert_threshold(id=rule, alertthreshold='OFF')
