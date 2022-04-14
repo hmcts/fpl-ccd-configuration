98ad94037af09e6fc35b49a918cbe4de72af2483
alistair.osborne <alistair.osborne@justice.gov.uk>
2022-04-14 14:47:38 +0100

    remove repeated code into RespondentsCommonFormatHelper

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/RespondentsRefusedFormatter.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/RespondentsRefusedFormatter.java
@@ -4,51 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Other;
-import uk.gov.hmcts.reform.fpl.model.Respondent;
-import uk.gov.hmcts.reform.fpl.model.common.Element;
-import uk.gov.hmcts.reform.fpl.model.order.selector.Selector;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RespondentsRefusedFormatter {
 
     public String getRespondentsRefusedLabel(CaseData caseData) {
-        List<Element<Respondent>> respondents = caseData.getAllRespondents();
-        List<Element<Other>> others = caseData.getAllOthers();
-        if (isEmpty(respondents) && isEmpty(others)) {
-            return "No respondents or others to be given notice on the case";
-        }
-
-        Stream<String> respondentsNames = respondents.stream()
-            .map(respondent -> "Respondent - " + respondent.getValue().getParty().getFullName());
-
-        Stream<String> othersNames = others.stream()
-            .map(other -> "Other - " + other.getValue().getName());
-
-        List<String> respondentsAndOthersNames = Stream.concat(respondentsNames, othersNames).collect(
-            Collectors.toList());
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = 0; i < respondentsAndOthersNames.size(); i++) {
-            builder.append(String.format("Person %d: %s", i + 1, respondentsAndOthersNames.get(i)));
-            builder.append("\n");
-        }
-
-        return builder.toString();
+        return RespondentsCommonFormatHelper.getRespondentsLabel(caseData);
     }
 
     public String getRespondentsNamesForDocument(CaseData caseData) {
         StringBuilder builder = new StringBuilder();
-        List<String> selected = getSelectedApplicants(caseData);
+        List<String> selected = RespondentsCommonFormatHelper
+            .getSelectedARespondents(caseData, caseData.getRespondentsRefusedSelector());
 
         selected.forEach(builder::append);
 
@@ -56,44 +26,6 @@ public class RespondentsRefusedFormatter {
     }
 
     public String getRespondentsNamesForTab(CaseData caseData) {
-        StringBuilder builder = new StringBuilder();
-        List<String> selected = getSelectedApplicants(caseData);
-
-        if (selected.isEmpty()) {
-            return null;
-        }
-
-        selected.forEach(builder::append);
-
-        return builder.toString();
-    }
-
-    private List<String> getSelectedApplicants(CaseData caseData) {
-        List<String> selectedApplicants = new ArrayList<>();
-
-        Stream<String> respondentsNames = caseData.getAllRespondents().stream()
-            .map(respondent -> respondent.getValue().getParty().getFullName());
-
-        Stream<String> othersNames = caseData.getAllOthers().stream()
-            .map(other -> other.getValue().getName());
-
-        List<String> respondentsAndOthersNames = Stream.concat(respondentsNames, othersNames).collect(
-            Collectors.toList());
-
-        List<String> selected = defaultIfNull(caseData.getRespondentsRefusedSelector(), Selector.builder().build())
-            .getSelected().stream()
-            .map(respondentsAndOthersNames::get)
-            .collect(Collectors.toList());
-
-        for (int i = 0; i < selected.size(); i++) {
-            String name = selected.get(i);
-
-            if (i >= 1) {
-                selectedApplicants.add(String.format(", %s", name));
-            } else {
-                selectedApplicants.add(String.format("%s", name));
-            }
-        }
-        return selectedApplicants;
+        return RespondentsCommonFormatHelper.getRespondentsForTab(caseData, caseData.getRespondentsRefusedSelector());
     }
 }
