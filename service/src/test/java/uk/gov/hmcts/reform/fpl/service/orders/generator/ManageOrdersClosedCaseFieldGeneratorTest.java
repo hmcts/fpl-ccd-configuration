d9f42195d0dbb8036859cc8510a25d57409e8a38
SomeshRA <somesh.acharya1@hmcts.net>
2022-04-28 15:13:27 +0100

    DFPL-455: Final Order not closing case in manage orders event

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
@@ -1,10 +1,15 @@
 package uk.gov.hmcts.reform.fpl.service.orders.generator;
 
+import org.apache.commons.lang3.BooleanUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.CloseCase;
 import uk.gov.hmcts.reform.fpl.model.event.ManageOrdersEventData;
@@ -16,6 +21,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -110,5 +116,34 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
         return CaseData.builder()
             .manageOrdersEventData(manageOrdersEventData)
             .build();
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateOrderStates")
+    void shouldCloseCaseWhenOrderIsDefaultFinal(String isFinalOrder) {
+        System.out.println(BooleanUtils.toBoolean(isFinalOrder));
+        CaseData caseData = buildCaseData("Yes", isFinalOrder, C32A_CARE_ORDER);
+
+        when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
+                .thenReturn(Collections.emptyList());
+        when(time.now()).thenReturn(NOW);
+
+        Map<String, Object> generatedData = underTest.generate(caseData);
+        Map<String, Object> expectedData = Map.of(
+                "state", CLOSED,
+                "closeCaseTabField", CloseCase.builder().date(time.now().toLocalDate()).build()
+        );
+
+        assertThat(generatedData).containsAllEntriesOf(expectedData);
+        assertThat(generatedData).containsKey("children1");
+    }
+
+    private static Stream<Arguments> generateOrderStates() {
+        String data = null;
+        return Stream.of(
+                Arguments.of(YesNo.NO.getValue()),
+                Arguments.of(YesNo.YES.getValue()),
+                Arguments.of(data)
+        );
     }
 }
