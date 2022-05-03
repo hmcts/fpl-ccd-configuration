0bb6c284d9cd070ddddba12bcfa3321ae524b15b
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2022-05-03 16:14:39 +0100

    DFPL-521 get hearing details of ICO and ADC (#3458)

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -752,6 +752,13 @@ public class CaseData {
     }
 
     @JsonIgnore
+    public Optional<HearingBooking> getFirstHearingOfTypes(List<HearingType> types) {
+        return unwrapElements(hearingDetails).stream()
+            .filter(hearingBooking -> types.stream().anyMatch(type -> hearingBooking.isOfType(type)))
+            .min(comparing(HearingBooking::getStartDate));
+    }
+
+    @JsonIgnore
     public Optional<HearingBooking> getNextHearingAfter(LocalDateTime time) {
         return unwrapElements(hearingDetails).stream()
             .filter(hearingBooking -> hearingBooking.getStartDate().isAfter(time))
