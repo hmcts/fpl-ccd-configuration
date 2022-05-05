7e8964c7039438bcd9e1f1e49f0d2e6a4e2848f2
SomeshRA <somesh.acharya1@hmcts.net>
2022-05-05 16:41:40 +0100

    DFPL-455: Removing unused import

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.service.orders.generator;
 
-import org.apache.commons.lang3.BooleanUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
