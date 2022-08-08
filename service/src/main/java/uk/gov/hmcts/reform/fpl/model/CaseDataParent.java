63cd818db3b57e11f3645c21c6bbc877c3cb24d3
Ashley Wong <ashley.wong@justice.gov.uk>
2022-08-08 10:28:41 +0100

    DPFL-779 Reduce number of case data fields

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseDataParent.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.fpl.model;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import lombok.extern.jackson.Jacksonized;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+
+import java.util.List;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = CaseData.class)
+})
+@Jacksonized
+@SuperBuilder(toBuilder = true)
+@Data
+public class CaseDataParent {
+
+    protected final List<Element<Direction>> allParties;
+    protected final List<Element<Direction>> allPartiesCustom;
+    protected final List<Element<Direction>> localAuthorityDirections;
+    protected final List<Element<Direction>> localAuthorityDirectionsCustom;
+    protected final List<Element<Direction>> courtDirections;
+    protected final List<Element<Direction>> courtDirectionsCustom;
+    protected final List<Element<Direction>> cafcassDirections;
+    protected final List<Element<Direction>> cafcassDirectionsCustom;
+    protected final List<Element<Direction>> otherPartiesDirections;
+    protected final List<Element<Direction>> otherPartiesDirectionsCustom;
+    protected final List<Element<Direction>> respondentDirections;
+    protected final List<Element<Direction>> respondentDirectionsCustom;
+}
