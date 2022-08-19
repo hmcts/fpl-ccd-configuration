7373cc0ce40d8d955e2a527e7726120c8156dfc1
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-08-19 14:42:07 +0100

    DFPL-652: fix checkstyle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/events/ApplicationFormRemovedEvent.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/events/ApplicationFormRemovedEvent.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.fpl.events;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 
 @Getter
 @RequiredArgsConstructor
