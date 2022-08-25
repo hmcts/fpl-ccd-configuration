3154d147de057178b3c820b5c8c874d0642e7523
SomeshRA <somesh.acharya1@hmcts.net>
2022-08-25 12:11:55 +0100

    DFPL-743: Logging retrieved audit events

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/AuditEventService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/AuditEventService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.fpl.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class AuditEventService {
     private final CoreCaseDataApiV2 caseDataApi;
     private final AuthTokenGenerator authTokenGenerator;
@@ -30,12 +32,14 @@ public class AuditEventService {
     }
 
     public Optional<AuditEvent> getOldestAuditEventByName(String caseId, String eventName) {
+        log.info("Case id {} and event name {}", caseId, eventName);
         String userToken = systemUserService.getSysUserToken();
 
         AuditEventsResponse auditEventsResponse
             = caseDataApi.getAuditEvents(userToken, authTokenGenerator.generate(), false, caseId);
 
         return auditEventsResponse.getAuditEvents().stream()
+                .peek(auditEvent -> log.info("audit event name {}", auditEvent.getId()))
             .filter(auditEvent -> eventName.equals(auditEvent.getId()))
             .min(Comparator.comparing(AuditEvent::getCreatedDate));
     }
