fe224d4a241a16460116180eeab10851dab8d7fd
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2022-08-30 11:51:20 +0100

    Initial set up of CFTLib.

---
+++ service/src/cftlib/java/uk/gov/hmcts/reform/fpl/Wiremock.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.fpl;
+
+import javax.annotation.PostConstruct;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Wiremock {
+    WireMockServer server;
+    @PostConstruct
+    void init() {
+        server = new WireMockServer(options()
+            .port(8765)
+            .extensions("com.github.masonm.JwtMatcherExtension", "com.github.masonm.JwtStubMappingTransformer")
+            .usingFilesUnderClasspath("wiremock"));
+        server.start();
+    }
+}
