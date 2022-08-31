c61e0805faf9332539cc3cb9c9fe785a56e8f9ab
SomeshRA <somesh.acharya1@hmcts.net>
2022-08-31 16:17:37 +0100

    DFPL-844: Updated hearings model

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/Hearings.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/Hearings.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.fpl.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
@@ -7,7 +9,9 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 import java.util.List;
 
 @Data
+@Builder
 @Jacksonized
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Hearings {
     private List<Element<HearingBooking>> hearingDetails;
 }
