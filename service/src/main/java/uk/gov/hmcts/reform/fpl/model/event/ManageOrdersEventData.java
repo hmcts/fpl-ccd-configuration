f5516de9128219305ea50e1c36d60db986654862
Ashley Wong <ashley.wong@justice.gov.uk>
2022-09-12 11:25:38 +0100

    Add warning message to manage order

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/event/ManageOrdersEventData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/event/ManageOrdersEventData.java
@@ -66,6 +66,7 @@ public class ManageOrdersEventData {
     String manageOrdersWhoIsExcluded;
     String manageOrdersNeedSealing;
     DocumentReference manageOrdersUploadOrderFile;
+    List<String> documentAcknowledge;
     DocumentReference manageOrdersPowerOfArrest;
     Address manageOrdersEpoRemovalAddress;
     LocalDate manageOrdersExclusionStartDate;
