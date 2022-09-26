45daa459d67fcb28b20d9e969c591b0e24531e12
alistair.osborne <alistair.osborne@justice.gov.uk>
2022-09-26 13:07:41 +0100

    DFPL-790-V2: CCD Issue when editing future hearing

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsServiceTest.java
@@ -489,7 +489,9 @@ class ManageHearingsServiceTest {
             Map.entry("preHearingAttendanceDetails", "Pre attendance details"),
             Map.entry("sendNoticeOfHearingTranslationRequirements", TRANSLATION_REQUIREMENTS),
             Map.entry("hearingDuration", "DATE_TIME"),
-            Map.entry("hearingEndDateTime", endDate)
+            Map.entry("hearingEndDateTime", endDate),
+            Map.entry("hearingVenue", "OTHER"),
+            Map.entry("hearingVenueCustom", VENUE_CUSTOM_ADDRESS)
         ));
 
     }
