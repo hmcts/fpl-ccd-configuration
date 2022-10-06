a23689f7df3f28a707fcf0cbee2375c0b7663d1e
Qasim Nawaz <96177697+qasimnawaz-moj@users.noreply.github.com>
2022-10-06 11:52:49 +0100

    DFPL-569: Entry point for other solicitors to submit DOC app (#3551)
    * DFPL-493 Addition to application flow - Question on which court LA is issuing for?
    
    * Implement Court list in alphabetical Order covering England and Wales
    
    * typo
    
    * Test Case
    
    * temp fix for CVE-2020-36518
    
    * Revert "temp fix for CVE-2020-36518"
    
    This reverts commit cf2d9b2b321d49902c8fcd77d0c47135fb3b258c.
    
    * Remove dependencies on previous case
    
    * simplify the reuse case id logic (changes from DFPL-405 2c30fcd5470b5bc02b5c3a2b5f5eee2e5e832c62)
    
    * Update to numeric codes
    
    * DFPL-569: Entry point for other solicitors to submit DOC app
    
    * Allow non LA solicitors to start case application
    
    * Revert non necessary changes
    
    * Add permisssions for solicitor to view tasklist and access orders needed step
    
    * Add more permissions for respondent solicitor
    
    * Add court allocation functionality
    
    * Allow submission of new application by resp/child solicitors
    
    * Handle email notification for resp/child solicitor
    
    * Handle payment fot resp/child solicitor
    
    * Remove unused import
    
    * Update wording from LA to applicant
    
    * Update wording from local authority to applicant
    
    * Cleanup code
    
    * Add user roles for resp/child solicitors
    
    * Add more permissions
    
    * Add outsourcing LA question
    
    * Fix checkstyle issues
    
    * checkstyle issue fix
    
    * Handle if representative type isn't set
    
    * fix checkstyle issues
    
    * Fix tests
    
    * Remove unused imports
    
    * Add Tests
    
    * Fix checkstyle issues
    
    * Add tests
    
    * Add caseinitiationservice tests
    
    * Limit orders shown to respondent and child solicitors
    
    * Add tests
    
    * Fix checkstyle issues
    
    * Change local authority wording to applicant
    
    * Fix typo
    
    * Update naming of variables and clean up code
    
    * Fix typo
    
    * Refine ccd definitions and update controllers and tests
    
    * Remove unused imports
    
    * Fix CaseData fields issue
    
    * Smoke test update
    
    * fix conflicts
    
    * Fix all merge conflicts
    
    * define caseData in mid event
    
    * CR changes
    
    * Fixing merge conflict
    
    * Fix merge conflict
    
    * up functional test timeout (temp)
    
    * handle merge conflicts
    
    * Fixing merge conflicts
    
    * Fix merge conflicts
    
    * Temp disable launch darkly check for testing
    
    * Fixing merge conflict
    
    * Fix merge conflicts
    
    * Temp disable test for testing purposes
    
    * Dont allow solicitors to create a case as an LA
    
    * Fix error messages and tests
    
    * Fix tests
    
    * Error message update
    
    * Fix notification errors
    
    * Fix tests
    
    * Fix whitespace issue
    
    * Fix more tests
    
    * Fix api test
    
    * Handle failed PBA payment for solicitors
    
    * Fix checkstyle issues
    
    * Handle checkstyle issues
    
    * Fix tests
    
    * Add tests
    
    * chechstyle issues
    
    * Add delete application functionality for solicitors and fix wording for applicants
    
    * Re-enable test and launchdarkly check
    
    * Fix merge conflicts
    
    * Fix merge conflicts
    
    * Fix checkstyle issues
    
    * Change functionality so only solicitors see the new screen
    
    * Fix smoke tests
    
    * Fix tests
    
    * Checkstyle fix
    
    * Temp disable launch darkly for testing purposes
    
    * Fix merge conflicts
    
    * Fix formatting
    
    * Re-enable launch darkly
    
    * Update test
    
    Co-authored-by: Ashley Wong <ashley.wong@justice.gov.uk>
    Co-authored-by: Ashley Wong <50691452+so99y@users.noreply.github.com>
    Co-authored-by: Daniel Catchpole <daniel.catchpole@justice.gov.uk>
    Co-authored-by: randersonMOJ <97732206+randersonMOJ@users.noreply.github.com>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/HmctsEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/HmctsEmailContentProvider.java
@@ -5,9 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.fpl.config.LocalAuthorityNameLookupConfiguration;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.LocalAuthority;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.notify.submittedcase.SubmitCaseHmctsTemplate;
 import uk.gov.hmcts.reform.fpl.service.CourtService;
 import uk.gov.hmcts.reform.fpl.service.email.content.base.SharedNotifyContentProvider;
+
+import static java.util.Objects.nonNull;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
@@ -19,9 +23,19 @@ public class HmctsEmailContentProvider extends SharedNotifyContentProvider {
         SubmitCaseHmctsTemplate template = buildNotifyTemplate(SubmitCaseHmctsTemplate.builder().build(), caseData);
 
         template.setCourt(courtService.getCourtName(caseData));
-        template.setLocalAuthority(laNameLookup.getLocalAuthorityName(caseData.getCaseLocalAuthority()));
+        template.setLocalAuthority(nonNull(caseData.getCaseLocalAuthority())
+            ? laNameLookup.getLocalAuthorityName(caseData.getCaseLocalAuthority()) : getApplicantName(caseData));
         template.setDocumentLink(getDocumentUrl(caseData.getC110A().getSubmittedForm()));
 
         return template;
+    }
+
+    public String getApplicantName(CaseData caseData) {
+        LocalAuthority applicant = caseData.getLocalAuthorities().stream()
+            .map(Element::getValue)
+            .findFirst()
+            .orElse(null);
+
+        return nonNull(applicant) ? applicant.getName() : null;
     }
 }
