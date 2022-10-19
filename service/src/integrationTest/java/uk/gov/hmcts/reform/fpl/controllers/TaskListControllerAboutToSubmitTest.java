da7b44e7ae6c6dd5820ac858bcb84e1a02cb8b1e
Ashley Wong <ashley.wong@justice.gov.uk>
2022-10-19 11:46:22 +0100

    checkstyle fix

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/TaskListControllerAboutToSubmitTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/TaskListControllerAboutToSubmitTest.java
@@ -1,27 +1,13 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
-import java.util.List;
-import java.util.Map;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.CASE_TYPE;
-import static uk.gov.hmcts.reform.fpl.CaseDefinitionConstants.JURISDICTION;
-import static uk.gov.hmcts.reform.fpl.Constants.LOCAL_AUTHORITY_1_CODE;
-import static uk.gov.hmcts.reform.fpl.utils.ResourceReader.readString;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.hmcts.reform.calendar.model.BankHolidays;
 import uk.gov.hmcts.reform.fpl.enums.State;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
-import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
+
+import java.util.Map;
 
 @WebMvcTest(AddCaseNumberController.class)
 @OverrideAutoConfiguration(enabled = true)
