ecb7fff68748e62c0eb61d5065ac861e4d3cd940
chak-shing-lo-justice <ChakShing.Lo@justice.gov.uk>
2022-10-26 10:08:43 +0100

    fix checkstyle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendNewMessageJudgeService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/SendNewMessageJudgeService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.fpl.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
