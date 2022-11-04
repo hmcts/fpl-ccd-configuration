b1ca0dce5ce5c6f6c904bee55944110d68ee01f3
Ashley Wong <ashley.wong@justice.gov.uk>
2022-11-04 14:56:37 +0000

    update constants according to changed configuration

--- service/src/test/java/uk/gov/hmcts/reform/fpl/Constants.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/Constants.java
@@ -28,8 +28,8 @@ public interface Constants {
     String DEFAULT_CTSC_EMAIL = "FamilyPublicLaw+ctsc@gmail.com";
     String DEFAULT_ADMIN_EMAIL = "court1@family-court.com";
     String USER_AUTH_TOKEN = "token";
-    String LOCAL_AUTHORITY_1_COURT_ID = "11";
-    String LOCAL_AUTHORITY_3_COURT_A_ID = "344";
+    String LOCAL_AUTHORITY_1_COURT_ID = "344";
+    String LOCAL_AUTHORITY_3_COURT_A_ID = "31";
     String LOCAL_AUTHORITY_3_COURT_B_ID = "32";
     String LOCAL_AUTHORITY_1_COURT_NAME = "Family Court";
     String LOCAL_AUTHORITY_3_COURT_A_NAME = "Family Court 3 A";
@@ -38,7 +38,7 @@ public interface Constants {
     String LOCAL_AUTHORITY_3_COURT_A_EMAIL = "court3a@family-court.com";
     String LOCAL_AUTHORITY_3_COURT_B_EMAIL = "court3b@family-court.com";
     Court COURT_1 = Court.builder()
-        .code("11")
+        .code("344")
         .name("Family Court")
         .email("court1@family-court.com")
         .build();
@@ -48,7 +48,7 @@ public interface Constants {
         .email("court2@family-court.com")
         .build();
     Court COURT_3A = Court.builder()
-        .code("344")
+        .code("31")
         .name("Family Court 3 A")
         .email("court3a@family-court.com")
         .build();
