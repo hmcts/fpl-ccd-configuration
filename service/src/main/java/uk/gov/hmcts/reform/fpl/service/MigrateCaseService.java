1c6df95c494e092832cd3ae31c9ee14d1f81fb59
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-11-17 16:17:16 +0000

    checkstyle!

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/MigrateCaseService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/MigrateCaseService.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.fpl.service;
 
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
