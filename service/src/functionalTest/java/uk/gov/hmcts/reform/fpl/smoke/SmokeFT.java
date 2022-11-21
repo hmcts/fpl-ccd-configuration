5ce32c2fb65ff5196f73dfa31764553aba1b8295
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-11-21 11:25:29 +0000

    DFPL-1022: Add extra configuration around the smoke test

--- service/src/functionalTest/java/uk/gov/hmcts/reform/fpl/smoke/SmokeFT.java
+++ service/src/functionalTest/java/uk/gov/hmcts/reform/fpl/smoke/SmokeFT.java
@@ -1,22 +1,32 @@
 package uk.gov.hmcts.reform.fpl.smoke;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.fpl.util.TestConfiguration;
 
-@SpringBootTest()
+@ActiveProfiles("test-${env:local}")
+@SpringBootTest
+@ContextConfiguration(classes = {
+    TestConfiguration.class
+})
+@RunWith(SpringIntegrationSerenityRunner.class)
 public class SmokeFT {
 
-    @Value("${test-conf.fpl-url:http://localhost:4013}")
-    private String testUrl;
+    @Autowired
+    protected TestConfiguration testConfiguration;
 
     @Test
     public void testHealthEndpoint() {
         RestAssured.useRelaxedHTTPSValidation();
         RestAssured
                 .given()
-                .baseUri(testUrl)
+                .baseUri(testConfiguration.getFplUrl())
                 .get("/health")
                 .then()
                 .statusCode(200)
