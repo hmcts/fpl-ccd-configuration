5834eabb4034bf4a422780149a10aebc17cb70de
Ashley Wong <ashley.wong@justice.gov.uk>
2022-11-25 15:20:49 +0000

    move azurerm version to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.32.0"
+    }
     azuread = {
       source  = "hashicorp/azuread"
       version = "1.6.0"
