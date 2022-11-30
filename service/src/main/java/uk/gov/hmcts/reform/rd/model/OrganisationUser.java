cdfded91d3251a37d054eacde9832b94ac180cfa
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2022-11-30 14:49:13 +0000

    Add NoArgsConstructor for pact tests

--- service/src/main/java/uk/gov/hmcts/reform/rd/model/OrganisationUser.java
+++ service/src/main/java/uk/gov/hmcts/reform/rd/model/OrganisationUser.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrganisationUser {
     private String userIdentifier;
     private String email;
