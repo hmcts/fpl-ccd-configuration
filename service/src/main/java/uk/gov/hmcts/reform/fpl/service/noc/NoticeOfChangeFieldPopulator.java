8b58f5226ec750250169a26c404799f5a5e7728b
chak-shing-lo-justice <ChakShing.Lo@justice.gov.uk>
2022-12-12 10:01:30 +0000

    update notice of change

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/noc/NoticeOfChangeFieldPopulator.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/noc/NoticeOfChangeFieldPopulator.java
@@ -36,8 +36,6 @@ public class NoticeOfChangeFieldPopulator {
                                         NoticeOfChangeAnswersPopulationStrategy strategy) {
         Map<String, Object> data = new HashMap<>();
 
-        String applicant = getApplicantName(caseData);
-
         List<Element<WithSolicitor>> elements = representing.getTarget().apply(caseData);
         int numElements = elements.size();
 
@@ -56,7 +54,7 @@ public class NoticeOfChangeFieldPopulator {
             data.put(String.format(representing.getPolicyFieldTemplate(), i), organisationPolicy);
 
             Optional<NoticeOfChangeAnswers> possibleAnswer = populateAnswer(
-                strategy, applicant, solicitorContainer
+                strategy, solicitorContainer
             );
 
             if (possibleAnswer.isPresent()) {
@@ -68,21 +66,12 @@ public class NoticeOfChangeFieldPopulator {
     }
 
     private Optional<NoticeOfChangeAnswers> populateAnswer(NoticeOfChangeAnswersPopulationStrategy strategy,
-                                                           String applicantName,
                                                            Optional<Element<WithSolicitor>> element) {
         if (BLANK == strategy) {
             return Optional.of(NoticeOfChangeAnswers.builder().build());
         }
 
-        return element.map(e -> answersConverter.generateForSubmission(e, applicantName));
-    }
-
-    private String getApplicantName(CaseData caseData) {
-        if (isNotEmpty(caseData.getLocalAuthorities())) {
-            return caseData.getLocalAuthorities().get(0).getValue().getName();
-        }
-
-        return caseData.getAllApplicants().get(0).getValue().getParty().getOrganisationName();
+        return element.map(e -> answersConverter.generateForSubmission(e));
     }
 
     public enum NoticeOfChangeAnswersPopulationStrategy {
