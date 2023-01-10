b99a0a192373d0007a107f9606689e44f82e5989
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2023-01-10 09:38:17 +0000

    Remove unused imports

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/order/HearingOrdersBundle.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/order/HearingOrdersBundle.java
@@ -10,7 +10,6 @@ import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
