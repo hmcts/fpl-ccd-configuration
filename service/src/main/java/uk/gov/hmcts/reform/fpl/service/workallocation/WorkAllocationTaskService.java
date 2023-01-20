24cc25a1709c6c44a1801dbf52795da4473993c0
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-01-20 13:53:54 +0000

    DFPL-WA: Add utility function to create the work allocation dummy event

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/workallocation/WorkAllocationTaskService.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.fpl.service.workallocation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.fpl.enums.WorkAllocationTaskType;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.service.ccd.CoreCaseDataService;
+
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class WorkAllocationTaskService {
+
+    private static final String WORK_ALLOCATION_DUMMY_EVENT = "create-work-allocation-task";
+
+    private final CoreCaseDataService coreCaseDataService;
+
+    public void createWorkAllocationTask(CaseData caseData, WorkAllocationTaskType taskType) {
+        log.info("Creating work allocation task - " + taskType.name());
+        coreCaseDataService.triggerEvent(caseData.getId(), WORK_ALLOCATION_DUMMY_EVENT, Map.of(
+            "lastCreatedWATask", taskType
+        ));
+    }
+
+}
