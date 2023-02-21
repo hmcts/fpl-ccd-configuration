d6ec5b512708c801718cf8d17584ac22847e1779
Ashley Wong <ashley.wong@justice.gov.uk>
2023-02-21 14:37:17 +0000

    add missing test cases

--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CafcassHelperTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/CafcassHelperTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.CAFCASS_EMAIL_ADDRESS;
 import static uk.gov.hmcts.reform.fpl.handlers.NotificationEventHandlerTestData.LOCAL_AUTHORITY_CODE;
 import static uk.gov.hmcts.reform.fpl.utils.CafcassHelper.isNotifyingCafcassEngland;
+import static uk.gov.hmcts.reform.fpl.utils.CafcassHelper.isNotifyingCafcassWelsh;
 
 @ExtendWith(MockitoExtension.class)
 public class CafcassHelperTest {
@@ -39,6 +40,27 @@ public class CafcassHelperTest {
             .thenReturn(Optional.empty());
 
         assertThat(isNotifyingCafcassEngland(CaseData.builder()
+            .caseLocalAuthority("XX").build(), cafcassLookupConfiguration))
+            .isEqualTo(false);
+    }
+
+    @Test
+    void shouldReturTrueWhenCaseLocalAuthorityIsWelsh() {
+        when(cafcassLookupConfiguration.getCafcassWelsh(any()))
+            .thenReturn(Optional.of(
+                new CafcassLookupConfiguration.Cafcass(LOCAL_AUTHORITY_CODE, CAFCASS_EMAIL_ADDRESS)));
+
+        assertThat(isNotifyingCafcassWelsh(CaseData.builder()
+            .caseLocalAuthority(LOCAL_AUTHORITY_CODE).build(), cafcassLookupConfiguration))
+            .isEqualTo(true);
+    }
+
+    @Test
+    void shouldReturnFalseWhenCaseLocalAuthorityWhenCafcassIsNotWelsh() {
+        when(cafcassLookupConfiguration.getCafcassWelsh(any()))
+            .thenReturn(Optional.empty());
+
+        assertThat(isNotifyingCafcassWelsh(CaseData.builder()
             .caseLocalAuthority("XX").build(), cafcassLookupConfiguration))
             .isEqualTo(false);
     }
