634af79b2d18bf5f2b5d919d16962357da472fe6
Ashley Wong <ashley.wong@justice.gov.uk>
2023-02-23 14:51:03 +0000

    no message

--- service/src/main/java/uk/gov/hmcts/reform/fpl/utils/SecureDocStoreHelper.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/SecureDocStoreHelper.java
@@ -32,21 +32,23 @@ public class SecureDocStoreHelper {
     }
 
     @SneakyThrows
-    public byte[] download(final String documentUrlString, Callable<byte[]> callable) {
+    public byte[] download(final String documentUrlString, Callable<byte[]> oldDmStoreApproach) {
         try {
+            log.info(String.format("Downloading document: %s", documentUrlString));
             byte[] bytesFromSecureDocStore = secureDocStoreService.downloadDocument(documentUrlString);
             if (featureToggleService.isSecureDocstoreEnabled()) {
                 return bytesFromSecureDocStore;
             }
         } catch (Exception t) {
             if (!featureToggleService.isSecureDocstoreEnabled()) {
-                log.error("====== EXCEPTION CAUGHT (SECURE DOC STORE: DISABLED) ====== ", t);
-            } else if (callable == null) {
+                log.error("↑ ↑ ↑ ↑ ↑ ↑ ↑ EXCEPTION CAUGHT (SECURE DOC STORE: DISABLED) ↑ ↑ ↑ ↑ ↑ ↑ ↑", t);
+            } else if (oldDmStoreApproach == null) {
                 throw t;
             }
         }
-        if (!featureToggleService.isSecureDocstoreEnabled() && !isEmpty(callable)) {
-            return callable.call();
+        if (!featureToggleService.isSecureDocstoreEnabled() && !isEmpty(oldDmStoreApproach)) {
+            log.info(String.format("Using old dm-store approach to download the document (%s).", documentUrlString));
+            return oldDmStoreApproach.call();
         }
         throw new UnsupportedOperationException();
     }
