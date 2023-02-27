8d6ef58f74d7f00279468c4fd55baa535330babd
Ashley Wong <ashley.wong@justice.gov.uk>
2023-02-27 12:42:12 +0000

    update test cases

--- service/src/test/java/uk/gov/hmcts/reform/fpl/utils/SecureDocStoreHelperTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/utils/SecureDocStoreHelperTest.java
@@ -30,6 +30,63 @@ class SecureDocStoreHelperTest {
     private SecureDocStoreService secureDocStoreService;
     private static final String CONTENT_TYPE = "application/pdf";
     private static final String FILE_NAME = "FILE_NAME.pdf";
+    private static final String DOCUMENT_URL_STRING = "http://localhost/test123";
+
+    @Nested
+    class DocumentDownload {
+
+        @ParameterizedTest
+        @ValueSource(booleans = { false})
+        void shouldDownloadDocument(boolean toggleOn) {
+            byte[] resultFromSecureDocStore = "DATA_FROM_NEW".getBytes();
+            when(featureToggleService.isSecureDocstoreEnabled()).thenReturn(toggleOn);
+            when(secureDocStoreService.downloadDocument(DOCUMENT_URL_STRING)).thenReturn(resultFromSecureDocStore);
+
+            SecureDocStoreHelper underTest = new SecureDocStoreHelper(secureDocStoreService, featureToggleService);
+
+            if (toggleOn == false) {
+                byte[] resultFromOldDmStoreApproach = "DATA_FROM_OLD".getBytes();
+                byte[] actualData = underTest.download(DOCUMENT_URL_STRING, () -> resultFromOldDmStoreApproach);
+
+                assertThat(actualData).isEqualTo(resultFromOldDmStoreApproach);
+                assertThat(logs.getErrors()).isEmpty();
+                assertThat(logs.getInfos()).contains(
+                    format("Using old dm-store approach to download the document: %s.", DOCUMENT_URL_STRING));
+            } else {
+                byte[] actualData = underTest.download(DOCUMENT_URL_STRING);
+                assertThat(actualData).isEqualTo(resultFromSecureDocStore);
+                assertThat(logs.getInfos()).containsExactly(format("Downloading document: {}", DOCUMENT_URL_STRING));
+            }
+        }
+
+        @ParameterizedTest
+        @ValueSource(booleans = {true, false})
+        void shouldLogExceptionWhenDocStoreApiFailure(boolean toggleOn) {
+            when(featureToggleService.isSecureDocstoreEnabled()).thenReturn(toggleOn);
+            when(secureDocStoreService.downloadDocument(DOCUMENT_URL_STRING)).thenThrow(
+                new RuntimeException("TEST RUNTIME EXCEPTION"));
+
+            SecureDocStoreHelper underTest = new SecureDocStoreHelper(secureDocStoreService, featureToggleService);
+
+            if (toggleOn == false) {
+                byte[] resultFromOldDmStoreApproach = "DATA_FROM_OLD".getBytes();
+                byte[] actualData = underTest.download(DOCUMENT_URL_STRING, () -> resultFromOldDmStoreApproach);
+
+                assertThat(actualData).isEqualTo(resultFromOldDmStoreApproach);
+                assertThat(logs.getErrorThrowableClassNames()).contains("java.lang.RuntimeException");
+                assertThat(logs.getErrorThrowableMessages()).contains("TEST RUNTIME EXCEPTION");
+                assertThat(logs.getErrors()).contains(
+                    "↑ ↑ ↑ ↑ ↑ ↑ ↑ EXCEPTION CAUGHT (SECURE DOC STORE: DISABLED) ↑ ↑ ↑ ↑ ↑ ↑ ↑");
+                assertThat(logs.getInfos())
+                    .contains(
+                        format("Using old dm-store approach to download the document: %s.", DOCUMENT_URL_STRING));
+            } else {
+                assertThatThrownBy(() -> underTest.download(DOCUMENT_URL_STRING))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessage("TEST RUNTIME EXCEPTION");
+            }
+        }
+    }
 
     @Nested
     class DocumentUpload {
@@ -57,6 +114,8 @@ class SecureDocStoreHelperTest {
             } else {
                 Document actualDoc = underTest.uploadDocument("DATA".getBytes(), FILE_NAME, CONTENT_TYPE);
                 assertThat(actualDoc).isEqualTo(fromSecureDocStore);
+                assertThat(logs.getInfos()).containsExactly(format("Uploading document file name: %s (%s)",
+                    FILE_NAME, CONTENT_TYPE));
             }
         }
 
@@ -83,7 +142,9 @@ class SecureDocStoreHelperTest {
                     .contains(
                         format("Using old dm-store approach to upload document: %s (%s).", FILE_NAME, CONTENT_TYPE));
             } else {
-                assertThatThrownBy(() -> underTest.uploadDocument("DATA".getBytes(), FILE_NAME, CONTENT_TYPE));
+                assertThatThrownBy(() -> underTest.uploadDocument("DATA".getBytes(), FILE_NAME, CONTENT_TYPE))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessage("TEST RUNTIME EXCEPTION");
             }
         }
     }
