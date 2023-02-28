bc9fffb5c58a41b5fb48e89c34d4c3a083f0ad37
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2023-02-28 16:03:09 +0000

    Add tests

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -10,7 +10,9 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.AbstractCallbackTest;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.HearingDocuments;
 import uk.gov.hmcts.reform.fpl.model.Placement;
+import uk.gov.hmcts.reform.fpl.model.PositionStatementChild;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.event.PlacementEventData;
@@ -22,6 +24,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
+import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
@@ -136,7 +139,7 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
         private final String migrationId = "DFPL-1204";
         private final long validCaseId = 1638528543085011L;
         private final UUID placementToRemove = UUID.fromString("88125c8b-8466-4af4-967f-197c3b82773c");
-        private final UUID placementToRemain = UUID.randomUUID();
+        private final UUID placementToRemain = randomUUID();
         private final DocumentReference documentToRemain = testDocumentReference();
 
         @Test
@@ -181,7 +184,7 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
         private final String migrationId = "DFPL-1218";
         private final long validCaseId = 1651753104228873L;
         private final UUID placementToRemove = UUID.fromString("e32706b1-22e5-4bd9-ba05-355fe69028d0");
-        private final UUID placementToRemain = UUID.randomUUID();
+        private final UUID placementToRemain = randomUUID();
         private final DocumentReference documentToRemain = testDocumentReference();
 
         @Test
@@ -217,6 +220,37 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
                 .getPlacementsNonConfidential(true)).isEqualTo(placementsRemaining);
             assertThat(responseData.getPlacementEventData()
                 .getPlacementsNonConfidential(false)).isEqualTo(placementsRemaining);
+        }
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class Dfpl1243 {
+
+        private final String migrationId = "DFPL-1243";
+        private final long validCaseId = 1675697653441050L;
+
+        @Test
+        void shouldRemoveAllPositionStatements() {
+            PositionStatementChild positionStatementChild =
+                PositionStatementChild.builder()
+                    .hearing("Test hearing")
+                    .hearingId(randomUUID())
+                    .childId(randomUUID())
+                    .childName("Tom Smith")
+                    .build();
+
+            CaseData caseData = CaseData.builder()
+                .id(validCaseId)
+                .hearingDocuments(HearingDocuments.builder()
+                    .positionStatementChildListV2(List.of(element(positionStatementChild))).build())
+                .build();
+
+            AboutToStartOrSubmitCallbackResponse response = postAboutToSubmitEvent(
+                buildCaseDetails(caseData, migrationId));
+            CaseData responseData = extractCaseData(response);
+
+            assertThat(responseData.getHearingDocuments().getPositionStatementChildListV2()).isEmpty();
         }
     }
 }
