037c52c1ec57d8e763f09db95d382d1ec39cd1bf
Ashley Wong <ashley.wong@justice.gov.uk>
2023-03-08 12:28:02 +0000

    remove unused scripts

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -92,51 +92,6 @@ public class MigrateCaseController extends CallbackController {
         caseDetails.getData().remove(PLACEMENT_NON_CONFIDENTIAL);
         caseDetails.getData().remove(PLACEMENT_NON_CONFIDENTIAL_NOTICES);
     }
-    
-    private void run1262(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1262";
-        var possibleCaseIds = List.of(1651753104228873L);
-        final UUID placementToRemove = UUID.fromString("195e9334-a308-4992-a890-8d6c8643dc1f");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeSpecificPlacements(getCaseData(caseDetails),
-            placementToRemove));
-    }
-
-    private void run1274(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1274";
-        var possibleCaseIds = List.of(1665570034617577L);
-        final UUID placementToRemove = UUID.fromString("91217531-42de-4f1c-99b7-aded7233d832");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeSpecificPlacements(getCaseData(caseDetails),
-            placementToRemove));
-    }
-
-    private void run1277(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1277";
-        var possibleCaseIds = List.of(1659933720451883L);
-        final UUID placementToRemove = UUID.fromString("f1b6d2d8-e960-4b36-a9ae-56723c25ac31");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeSpecificPlacements(getCaseData(caseDetails),
-            placementToRemove));
-    }
-
-    private void run1290(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1290";
-        var possibleCaseIds = List.of(1644931377783283L);
-        final UUID placementToRemove = UUID.fromString("959bd38f-72d9-42ef-b01d-e5b02aabacfa");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeSpecificPlacements(getCaseData(caseDetails),
-            placementToRemove));
-    }
-
-    private void run1294(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1294";
-        var possibleCaseIds = List.of(1676971632816123L);
-        final UUID expectedPositionStatementId = UUID.fromString("d74874b8-3ee3-4f06-8e01-a86209ffa31e");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removePositionStatementChild(getCaseData(caseDetails),
-            migrationId, expectedPositionStatementId));
-    }
 
     private void run1261(CaseDetails caseDetails) {
         var migrationId = "DFPL-1261";
