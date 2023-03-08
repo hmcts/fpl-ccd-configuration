ff14e5195c520c37254c99ea046754f6dea44786
chak-shing-lo-justice <ChakShing.Lo@justice.gov.uk>
2023-03-08 16:59:53 +0000

    make allocation proposal mandatory for all case type

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/validators/CaseSubmissionChecker.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/validators/CaseSubmissionChecker.java
@@ -59,7 +59,7 @@ public class CaseSubmissionChecker extends CompoundEventChecker {
 
         events.add(CASE_NAME);
         events.add(ORDERS_SOUGHT);
-        if (!caseData.isC1Application() 
+        if (!caseData.isC1Application()
             || caseData.isRefuseContactWithChildApplication()
             || caseData.isContactWithChildInCareApplication()) {
             events.add(HEARING_URGENCY); // present but optional for C1s
@@ -77,13 +77,7 @@ public class CaseSubmissionChecker extends CompoundEventChecker {
 
         events.add(CHILDREN);
         events.add(RESPONDENTS);
-
-        if (!caseData.isC1Application() 
-            || caseData.isSecureAccommodationOrderType()
-            || caseData.isRefuseContactWithChildApplication()
-            || caseData.isContactWithChildInCareApplication()) {
-            events.add(ALLOCATION_PROPOSAL);
-        }
+        events.add(ALLOCATION_PROPOSAL);
 
         if (YES.equals(caseData.getMultiCourts())) {
             events.add(SELECT_COURT);
