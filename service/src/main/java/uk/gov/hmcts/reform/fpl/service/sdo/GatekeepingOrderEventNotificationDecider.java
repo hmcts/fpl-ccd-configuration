f85e26a60d337ec82775836c35ce0c50c774238b
KatKovacs1 <katalin.kovacs1@justice.gov.uk>
2023-03-10 13:28:16 +0000

    Added changes to send notifications

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/GatekeepingOrderEventNotificationDecider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/GatekeepingOrderEventNotificationDecider.java
@@ -48,6 +48,32 @@ public class GatekeepingOrderEventNotificationDecider {
         return Optional.of(event.build());
     }
 
+    public Optional<GatekeepingOrderEvent> buildGatekeepingEventToPublish(CaseData caseData, State previousState) {
+        GatekeepingOrderEvent.GatekeepingOrderEventBuilder event = GatekeepingOrderEvent.builder().caseData(caseData);
+        StandardDirectionOrder sdo = defaultIfNull(
+            caseData.getStandardDirectionOrder(), StandardDirectionOrder.builder().build()
+        );
+        UrgentHearingOrder urgentHearingOrder = caseData.getUrgentHearingOrder();
+        if (sdo.isDraft() && (null == urgentHearingOrder || !isInGatekeeping(previousState))) {
+            return Optional.empty();
+        }
+
+        if (null != sdo.getOrderDoc()) {
+            event.order(sdo.getOrderDoc());
+            event.languageTranslationRequirement(sdo.getTranslationRequirements());
+            // if we are in the gatekeeping-listing state send the NoP related notifications
+            event.notificationGroup(SDO_AND_NOP);
+            event.orderTitle(sdo.asLabel());
+        } else {
+            event.order(urgentHearingOrder.getOrder());
+            event.notificationGroup(URGENT_AND_NOP);
+            event.languageTranslationRequirement(urgentHearingOrder.getTranslationRequirements());
+            event.orderTitle(urgentHearingOrder.asLabel());
+        }
+
+        return Optional.of(event.build());
+    }
+
     private boolean isInGatekeeping(State previousState) {
         return GATEKEEPING.equals(previousState);
     }
