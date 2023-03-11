6468d3e5345a3f7a3607ed11f63a00e9e202cc09
KatKovacs1 <katalin.kovacs1@justice.gov.uk>
2023-03-11 11:07:52 +0000

    Fixing SDO and NoP template choice

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/GatekeepingOrderEventNotificationDecider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/GatekeepingOrderEventNotificationDecider.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static uk.gov.hmcts.reform.fpl.enums.State.GATEKEEPING;
+import static uk.gov.hmcts.reform.fpl.enums.State.GATEKEEPING_LISTING;
 import static uk.gov.hmcts.reform.fpl.enums.notification.GatekeepingOrderNotificationGroup.SDO;
 import static uk.gov.hmcts.reform.fpl.enums.notification.GatekeepingOrderNotificationGroup.SDO_AND_NOP;
 import static uk.gov.hmcts.reform.fpl.enums.notification.GatekeepingOrderNotificationGroup.URGENT_AND_NOP;
@@ -32,34 +33,9 @@ public class GatekeepingOrderEventNotificationDecider {
         if (null != sdo.getOrderDoc()) {
             event.order(sdo.getOrderDoc());
             event.languageTranslationRequirement(sdo.getTranslationRequirements());
-            // if we are in the gatekeeping state send the NoP related notifications
-            event.notificationGroup(isInGatekeeping(previousState) ? SDO_AND_NOP : SDO);
-            event.orderTitle(sdo.asLabel());
-        } else {
-            event.order(urgentHearingOrder.getOrder());
-            event.notificationGroup(URGENT_AND_NOP);
-            event.languageTranslationRequirement(urgentHearingOrder.getTranslationRequirements());
-            event.orderTitle(urgentHearingOrder.asLabel());
-        }
-
-        return Optional.of(event.build());
-    }
-
-    public Optional<GatekeepingOrderEvent> buildGatekeepingEventToPublish(CaseData caseData, State previousState) {
-        GatekeepingOrderEvent.GatekeepingOrderEventBuilder event = GatekeepingOrderEvent.builder().caseData(caseData);
-        StandardDirectionOrder sdo = defaultIfNull(
-            caseData.getStandardDirectionOrder(), StandardDirectionOrder.builder().build()
-        );
-        UrgentHearingOrder urgentHearingOrder = caseData.getUrgentHearingOrder();
-        if (sdo.isDraft() && (null == urgentHearingOrder || !isInGatekeeping(previousState))) {
-            return Optional.empty();
-        }
-
-        if (null != sdo.getOrderDoc()) {
-            event.order(sdo.getOrderDoc());
-            event.languageTranslationRequirement(sdo.getTranslationRequirements());
-            // if we are in the gatekeeping-listing state send the NoP related notifications
-            event.notificationGroup(SDO_AND_NOP);
+            // if we are in the gatekeeping or gatekeeeping-listing state send the NoP related notifications
+            event.notificationGroup((isInGatekeeping(previousState) || isInGatekeepingListing(previousState))
+                ? SDO_AND_NOP : SDO);
             event.orderTitle(sdo.asLabel());
         } else {
             event.order(urgentHearingOrder.getOrder());
@@ -73,5 +49,9 @@ public class GatekeepingOrderEventNotificationDecider {
 
     private boolean isInGatekeeping(State previousState) {
         return GATEKEEPING == previousState;
+    }
+
+    private boolean isInGatekeepingListing(State previousState) {
+        return GATEKEEPING_LISTING == previousState;
     }
 }
