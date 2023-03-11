084173f9591d0edd7fa97b644551ebd1e48d56ba
KatKovacs1 <katalin.kovacs1@justice.gov.uk>
2023-03-11 11:06:46 +0000

    Fixing NoH link

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ListGatekeepingHearingController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ListGatekeepingHearingController.java
@@ -101,7 +101,6 @@ public class ListGatekeepingHearingController extends CallbackController {
         }
 
         caseDetails.getData().putAll(hearingsService.populateHearingLists(caseData));
-        caseDetails.getData().put("sendNoticeOfHearing", YES.getValue());
 
         setNewHearing(caseDetails);
 
@@ -112,6 +111,7 @@ public class ListGatekeepingHearingController extends CallbackController {
     public CallbackResponse handleAboutToSubmit(@RequestBody CallbackRequest callbackRequest) {
 
         final CaseDetails caseDetails = callbackRequest.getCaseDetails();
+        caseDetails.getData().put("sendNoticeOfHearing", YES.getValue());
         CaseData eventData = orderService.updateStandardDirections(callbackRequest.getCaseDetails());
         final CaseDetailsMap caseData = caseDetailsMap(caseDetails);
 
