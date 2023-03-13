cf7a0e8db8a2472cb65549946842423ab1696d5f
Shaun Francis <shaun.francis1@hmcts.net>
2023-03-13 16:59:49 +0000

    Revert removal of sendNoticeOfHearing in about to start

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/listgatekeepinghearing/ListGatekeepingHearingControllerAboutToStartTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/listgatekeepinghearing/ListGatekeepingHearingControllerAboutToStartTest.java
@@ -64,6 +64,8 @@ class ListGatekeepingHearingControllerAboutToStartTest extends AbstractCallbackT
         assertThat(responseDataMap.get("pastAndTodayHearingDateList")).isEqualTo(emptyListMap);
         assertThat(responseDataMap.get("vacateHearingDateList")).isEqualTo(emptyListMap);
         assertThat(responseDataMap.get("toReListHearingDateList")).isEqualTo(emptyListMap);
+
+        assertThat(responseDataMap.get("sendNoticeOfHearing")).isEqualTo(YES.getValue());
     }
 
     @Test
@@ -90,6 +92,8 @@ class ListGatekeepingHearingControllerAboutToStartTest extends AbstractCallbackT
         assertThat(responseDataMap.get("pastAndTodayHearingDateList")).isEqualTo(emptyListMap);
         assertThat(responseDataMap.get("vacateHearingDateList")).isEqualTo(emptyListMap);
         assertThat(responseDataMap.get("toReListHearingDateList")).isEqualTo(emptyListMap);
+
+        assertThat(responseDataMap.get("sendNoticeOfHearing")).isEqualTo(YES.getValue());
     }
 
     @Test
@@ -119,6 +123,7 @@ class ListGatekeepingHearingControllerAboutToStartTest extends AbstractCallbackT
         assertThat(responseDataMap.get("vacateHearingDateList")).isEqualTo(vacateListMap);
         assertThat(responseDataMap.get("toReListHearingDateList")).isEqualTo(emptyListMap);
 
+        assertThat(responseDataMap.get("sendNoticeOfHearing")).isEqualTo(YES.getValue());
         assertThat(responseDataMap.get("hearingOption")).isEqualTo(NEW_HEARING.toString());
 
     }
