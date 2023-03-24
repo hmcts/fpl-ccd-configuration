437be1aba4bc30e0ed8c9262d25b30071916cac2
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-03-24 12:11:29 +0000

    Cache the IDAM system user token for 2 hours

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/SystemUserService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/SystemUserService.java
@@ -6,15 +6,28 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.fpl.config.SystemUpdateUserConfiguration;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+
 @Service
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class SystemUserService {
 
     private final SystemUpdateUserConfiguration userConfig;
     private final IdamClient idamClient;
+    private String cachedToken;
+    private LocalDateTime timeLastCached;
 
     public String getSysUserToken() {
-        return idamClient.getAccessToken(userConfig.getUserName(), userConfig.getPassword());
+        // no cached token? no cached time? token > 2 hours old? -> Generate new token
+        if (isEmpty(cachedToken) || isEmpty(timeLastCached)
+            || timeLastCached.until(LocalDateTime.now(), ChronoUnit.HOURS) > 2) {
+            cachedToken = idamClient.getAccessToken(userConfig.getUserName(), userConfig.getPassword());
+            timeLastCached = LocalDateTime.now();
+        }
+        return cachedToken;
     }
 
     public String getUserId(String userToken) {
