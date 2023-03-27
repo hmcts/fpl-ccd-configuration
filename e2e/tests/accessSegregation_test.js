8c1030202aee3d7b52e3ef1fec46cb53fbfe3e13
Ashley Wong <ashley.wong@justice.gov.uk>
2023-03-27 12:52:39 +0100

    Magistrate user has read only access

--- e2e/tests/accessSegregation_test.js
+++ e2e/tests/accessSegregation_test.js
@@ -48,6 +48,6 @@ Scenario('Magistrate user can see the case', async ({I}) => {
   await setupScenario(I);
   await I.navigateToCaseDetailsAs(config.magistrateUser, caseId);
   I.see(I.uiFormatted(caseId));
-  await I.seeAvailableEvents([config.administrationActions.manageDocuments, config.administrationActions.addCaseFlag]);
+  await I.seeAvailableEvents([]);
 
 });
