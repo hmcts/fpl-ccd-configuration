1237de02a427ae960347ad0b45375fcd08d8f596
SomeshRA <somesh.acharya1@hmcts.net>
2023-03-28 17:02:36 +0100

    DFPL-1133: Defaulting label to Other when hearingType not set

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/HearingBooking.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/HearingBooking.java
@@ -27,7 +27,6 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 import javax.validation.constraints.Future;
 
