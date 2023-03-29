2c07fcd05bf08f4d58acb2d97e354e93f20f9be0
Shaun Francis <shaun.francis1@hmcts.net>
2023-03-29 12:12:34 +0100

    Refactor ListAdminEventNotificationDecider and add unit test

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/ListAdminEventNotificationDecider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/sdo/ListAdminEventNotificationDecider.java
@@ -1,14 +1,15 @@
 package uk.gov.hmcts.reform.fpl.service.sdo;
 
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.fpl.enums.DirectionsOrderType;
 import uk.gov.hmcts.reform.fpl.events.ListAdminEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.StandardDirectionOrder;
 import uk.gov.hmcts.reform.fpl.model.event.GatekeepingOrderEventData;
 
-import java.util.Objects;
 import java.util.Optional;
 
+import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.fpl.enums.DirectionsOrderType.SDO;
 import static uk.gov.hmcts.reform.fpl.enums.DirectionsOrderType.UDO;
 
@@ -18,25 +19,26 @@ public class ListAdminEventNotificationDecider {
     public Optional<ListAdminEvent> buildEventToPublish(CaseData caseData) {
 
         final GatekeepingOrderEventData gatekeepingOrderEventData = caseData.getGatekeepingOrderEventData();
-        if (Objects.nonNull(gatekeepingOrderEventData) && gatekeepingOrderEventData.isSentToAdmin()) {
-            final ListAdminEvent.ListAdminEventBuilder listAdminEventBuilder = ListAdminEvent.builder()
+        final StandardDirectionOrder directionOrder;
+        final DirectionsOrderType directionsOrderType;
+
+        if (nonNull(caseData.getGatekeepingOrderRouter())) {
+            directionOrder = caseData.getStandardDirectionOrder();
+            directionsOrderType = SDO;
+        } else {
+            directionOrder = caseData.getUrgentDirectionsOrder();
+            directionsOrderType = UDO;
+        }
+
+        if (nonNull(gatekeepingOrderEventData) && gatekeepingOrderEventData.isSentToAdmin()
+            && nonNull(directionOrder) && nonNull(directionOrder.getOrderDoc())) {
+            return Optional.of(ListAdminEvent.builder()
                 .caseData(caseData)
                 .isSentToAdmin(gatekeepingOrderEventData.isSentToAdmin())
-                .sendToAdminReason(gatekeepingOrderEventData.getSendToAdminReason());
-
-            final StandardDirectionOrder directionOrder;
-            if (Objects.nonNull(caseData.getGatekeepingOrderRouter())) {
-                directionOrder = caseData.getStandardDirectionOrder();
-                listAdminEventBuilder.directionsOrderType(SDO);
-            } else {
-                directionOrder = caseData.getUrgentDirectionsOrder();
-                listAdminEventBuilder.directionsOrderType(UDO);
-            }
-
-            if (Objects.nonNull(directionOrder) && Objects.nonNull(directionOrder.getOrderDoc())) {
-                listAdminEventBuilder.order(directionOrder.getOrderDoc());
-            }
-            return Optional.of(listAdminEventBuilder.build());
+                .sendToAdminReason(gatekeepingOrderEventData.getSendToAdminReason())
+                .order(directionOrder.getOrderDoc())
+                .directionsOrderType(directionsOrderType)
+                .build());
         }
 
         return Optional.empty();
