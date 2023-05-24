192ccaf1a48d31a97d8d878a122431dfbd691755
Ashley Wong <ashley.wong@justice.gov.uk>
2023-05-24 15:13:50 +0100

    DFPL-1511 INC5512575 Confidential Document to be removed

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -35,7 +35,8 @@ public class MigrateCaseController extends CallbackController {
     private final Map<String, Consumer<CaseDetails>> migrations = Map.of(
         "DFPL-1359", this::run1359,
         "DFPL-1401", this::run1401,
-        "DFPL-1451", this::run1451
+        "DFPL-1451", this::run1451,
+        "DFPL-1511", this::run1511
     );
 
     @PostMapping("/about-to-submit")
@@ -80,5 +81,15 @@ public class MigrateCaseController extends CallbackController {
             migrationId, expectedOrderId));
         caseDetails.getData().putAll(migrateCaseService.removeHearingOrdersBundlesDrafts(getCaseData(caseDetails),
             migrationId, expectedHearingOrderBundleId));
+    }
+
+    private void run1511(CaseDetails caseDetails) {
+        var migrationId = "DFPL-1511";
+        var possibleCaseIds = List.of(1677685368445274L);
+        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
+        caseDetails.getData().putAll(migrateCaseService.removeHearingFurtherEvidenceDocuments(getCaseData(caseDetails),
+            migrationId, UUID.fromString("12671a55-219b-4a36-a9e7-84f8f9e4877c"),
+            UUID.fromString("c56032ea-4f5f-4aba-9b4f-30f459b2ffed")));
+        caseDetails.getData().putAll(migrateCaseService.refreshDocumentViews(getCaseData(caseDetails)));
     }
 }
