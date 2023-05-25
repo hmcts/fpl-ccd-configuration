7f8c8fa7afd71e0c65d39b9d699e370a043a01c1
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2023-05-25 11:06:47 +0100

    Dfpl 1047 New event for judges to review additional applications (#4394)
    * add new event - confirmApplicationReviewed
    
    * show review status in Tab
    
    * update unit test
    
    * fix ConfirmApplicationReviewedService
    
    * fix code review
    
    * update 1047
    
    * fix check style
    
    * fix err
    
    * Update judiciary.json
    
    * Update ReviewAdditionalApplicationServiceTest.java
    
    * fix typo
    
    * fix historical case data
    
    * fix default review status not set
    
    * fix code style check
    
    * Update UploadAdditionalApplicationsServiceTest.java
    
    * Update ReviewAdditionalApplicationServiceTest.java
    
    * remove magistrate access from reviewing add app
    
    ---------
    
    Co-authored-by: Ashley Wong <50691452+so99y@users.noreply.github.com>
    Co-authored-by: Qasim Nawaz <96177697+qasimnawaz-moj@users.noreply.github.com>

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReviewAdditionalApplicationControllerMidEventTest.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.common.AdditionalApplicationsBundle;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.model.event.ConfirmApplicationReviewedEventData;
+import uk.gov.hmcts.reform.fpl.service.additionalapplications.ReviewAdditionalApplicationService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
+
+@WebMvcTest(ReviewAdditionalApplicationController.class)
+@OverrideAutoConfiguration(enabled = true)
+public class ReviewAdditionalApplicationControllerMidEventTest extends AbstractCallbackTest {
+    @MockBean
+    private ReviewAdditionalApplicationService reviewAdditionalApplicationService;
+
+    private static final Element<AdditionalApplicationsBundle> APPLICATION_BUNDLE_ELEMENT =
+        element(AdditionalApplicationsBundle.builder()
+            .uploadedDateTime("1 January 2021, 12:00pm")
+            .author("TESTING")
+            .build());
+
+    ReviewAdditionalApplicationControllerMidEventTest() {
+        super("review-additional-application");
+    }
+
+    @BeforeEach
+    void initTest() {
+        when(reviewAdditionalApplicationService.getSelectedApplicationsToBeReviewed(any()))
+            .thenReturn(APPLICATION_BUNDLE_ELEMENT);
+    }
+
+    @Test
+    void shouldQuerySelectedAdditionalApplicationBundle() {
+        AboutToStartOrSubmitCallbackResponse response = postMidEvent(CaseData.builder().build());
+        CaseData resultCaseData = extractCaseData(response);
+        ConfirmApplicationReviewedEventData resultEventData = resultCaseData.getConfirmApplicationReviewedEventData();
+
+        assertThat(resultEventData.getAdditionalApplicationsBundleToBeReviewed())
+            .isEqualTo(APPLICATION_BUNDLE_ELEMENT.getValue());
+    }
+
+}
