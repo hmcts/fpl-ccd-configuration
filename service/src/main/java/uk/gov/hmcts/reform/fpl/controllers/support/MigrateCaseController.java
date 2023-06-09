a7018144a6e73a29fb1efb82023de05f1e43da6e
Ashley Wong <ashley.wong@justice.gov.uk>
2023-06-09 18:31:25 +0100

    Reverted

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -38,8 +38,7 @@ public class MigrateCaseController extends CallbackController {
         "DFPL-1451", this::run1451,
         "DFPL-1466", this::run1466,
         "DFPL-1501", this::run1501,
-        "DFPL-1484", this::run1484,
-        "DFPL-1491", this::run1491
+        "DFPL-1484", this::run1484
     );
 
     @PostMapping("/about-to-submit")
@@ -102,7 +101,7 @@ public class MigrateCaseController extends CallbackController {
         migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
         caseDetails.getData().putAll(migrateCaseService.removeFurtherEvidenceSolicitorDocuments(
             getCaseData(caseDetails), migrationId, UUID.fromString("43a9287c-f840-4104-958f-cbd98d28aea3")));
-    }
+    }  
   
     private void run1484(CaseDetails caseDetails) {
         var migrationId = "DFPL-1484";
@@ -112,12 +111,5 @@ public class MigrateCaseController extends CallbackController {
         migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
         caseDetails.getData().putAll(migrateCaseService.removeCourtBundleByBundleId(getCaseData(caseDetails),
             migrationId, hearingId, courtBundleId));
-    }
-
-    private void run1491(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1491";
-        var possibleCaseIds = List.of(1685631395607064L);
-        caseDetails.getData().putAll(migrateCaseService.migrateRespondentStatement(getCaseData(caseDetails),
-            migrationId));
     }
 }
