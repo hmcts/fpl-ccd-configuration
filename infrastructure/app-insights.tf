370e1a4dafaa32ed27dd4a756de1caa74b2dc7a4
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2023-06-15 16:09:16 +0100

    Update app insights terraform.

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -3,6 +3,15 @@ data "azurerm_application_insights" "ai" {
   resource_group_name = "${var.product}-${var.env}"
 }
 
+locals {
+  app_insights_config = jsonencode(
+    merge(
+      jsondecode(
+        file("${path.module}/../lib/applicationinsights.json")), { connectionString = data.azurerm_application_insights.ai.connection_string }
+    )
+  )
+}
+
 data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-${var.env}"
