cd2e0c4b4ef7cfd2ee3328e5dbe7e4f277f54d75
Ashley Wong <ashley.wong@justice.gov.uk>
2023-06-15 09:59:07 +0100

    [REVERT_ME] tentatively add a door to execute the migration script.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -38,7 +38,8 @@ public class MigrateCaseController extends CallbackController {
         "DFPL-1451", this::run1451,
         "DFPL-1466", this::run1466,
         "DFPL-1501", this::run1501,
-        "DFPL-1484", this::run1484
+        "DFPL-1484", this::run1484,
+        "DFPL-1489", this::run1489
     );
 
     @PostMapping("/about-to-submit")
@@ -111,5 +112,10 @@ public class MigrateCaseController extends CallbackController {
         migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
         caseDetails.getData().putAll(migrateCaseService.removeCourtBundleByBundleId(getCaseData(caseDetails),
             migrationId, hearingId, courtBundleId));
+    }
+
+    private void run1489(CaseDetails caseDetails) {
+        caseDetails.getData().putAll(migrateCaseService
+            .moveCaseSummaryWithConfidentialAddressToCaseSummaryListLA(getCaseData(caseDetails)));
     }
 }
