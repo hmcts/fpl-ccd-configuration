28f8b18460ee8f093e96627765212292fd78d423
Ashley Wong <50691452+so99y@users.noreply.github.com>
2023-06-19 12:49:08 +0100

    DFPL-1504 CFV - Respondent Statement (#4465)
    * DFPL-1504 CFV - Respondent Statement
    
    * add removalReason for all documents
    
    * WIP DFPL-1504 migration script
    
    * rename
    
    * update migration script
    
    * remove extra lines
    
    * Reverted
    
    * Fix test case
    
    * add javadoc
    
    * add unchecked.
    
    * Migration test cases
    
    * Resolving error warning: [unchecked] unchecked call to confidential(List<String>) as a member of the raw type SupportingEvidenceBundleBuilder https://build.platform.hmcts.net/blue/organizations/jenkins/HMCTS_d_to_i%2Ffpl-ccd-configuration/detail/PR-4465/8/pipeline/
    
    * checkstyle
    
    * more test cases
    
    * more test cases on respondent statements having confidential address.
    
    * REVERT ME: add migrations script entry point
    
    * Revert "REVERT ME: add migrations script entry point"
    
    This reverts commit d139cb3621294ebd4f32258878e848dcc5993b66.
    
    ---------
    
    Co-authored-by: Ashley Wong <ashley.wong@justice.gov.uk>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseDataParent.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseDataParent.java
@@ -20,6 +20,10 @@ import javax.validation.constraints.NotNull;
 @Data
 public class CaseDataParent {
 
+    protected final List<Element<RespondentStatementV2>> respStmtList;
+    protected final List<Element<RespondentStatementV2>> respStmtListLA;
+    protected final List<Element<RespondentStatementV2>> respStmtListCTSC;
+    protected final List<Element<RespondentStatementV2>> respStmtListRemoved;
     protected final List<Element<Direction>> allParties;
     protected final List<Element<Direction>> allPartiesCustom;
     protected final List<Element<Direction>> localAuthorityDirections;
