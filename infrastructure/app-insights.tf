7b0c6a5137367ab759ca065c7e03241f05c36122
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2023-06-23 11:06:39 +0100

    Correcting resource group names for app insights and key vault

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -1,6 +1,6 @@
 data "azurerm_application_insights" "ai" {
   name                = "${var.product}-appinsights-${var.env}"
-  resource_group_name = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-case-service-${var.env}"
 }
 
 locals {
@@ -14,7 +14,7 @@ locals {
 
 data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}"
-  resource_group_name = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-case-service-${var.env}"
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
