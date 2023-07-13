542e85a372be120f1f989ec74932d85f3cd5cb1d
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-07-13 16:52:55 +0100

    checkstyle

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/MessageJudgeControllerSubmittedTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/MessageJudgeControllerSubmittedTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.Constants.LOCAL_AUTHORITY_1_CODE;
--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeControllerSubmittedTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeControllerSubmittedTest.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
