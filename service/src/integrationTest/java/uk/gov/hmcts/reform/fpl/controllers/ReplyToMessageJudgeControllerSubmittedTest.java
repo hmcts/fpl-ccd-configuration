a0193fc0b072ed5826817dfbc5d9f3dda1ddc4f4
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-07-14 14:12:57 +0100

    Revert "checkstyle"
    This reverts commit 542e85a372be120f1f989ec74932d85f3cd5cb1d.

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeControllerSubmittedTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeControllerSubmittedTest.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
