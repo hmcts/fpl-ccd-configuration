5d250e1497f737e73353bdb994a30f9a4e1ecec9
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-07-25 10:27:41 +0100

    Add new endpoints to call on AM

--- service/src/main/java/uk/gov/hmcts/reform/am/client/AmApi.java
+++ service/src/main/java/uk/gov/hmcts/reform/am/client/AmApi.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.reform.am.client;
 
 
-import jdk.jfr.ContentType;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.hmcts.reform.am.model.AssignmentRequest;
+import uk.gov.hmcts.reform.am.model.DeleteRequest;
+import uk.gov.hmcts.reform.am.model.QueryRequest;
+import uk.gov.hmcts.reform.am.model.QueryResponse;
 import uk.gov.hmcts.reform.am.model.RoleAssignmentRequestResource;
 import uk.gov.hmcts.reform.fpl.config.feign.FeignClientConfiguration;
 
@@ -32,5 +36,38 @@ public interface AmApi {
         @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
         @RequestBody AssignmentRequest request
     );
+
+    @PostMapping(
+        value = "/am/role-assignments/query",
+        consumes = APPLICATION_JSON_VALUE,
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    QueryResponse queryRoleAssignments(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestBody QueryRequest request
+    );
+
+    @PostMapping(
+        value = "/am/role-assignments/query/delete",
+        consumes = APPLICATION_JSON_VALUE,
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    void deleteRoleAssignmentsByQuery(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @RequestBody DeleteRequest request
+    );
+
+    @DeleteMapping(
+        value = "/am/role-assignments/{assignmentId}",
+        headers = CONTENT_TYPE + "=" + APPLICATION_JSON_VALUE
+    )
+    void deleteRoleAssignment(
+        @RequestHeader(AUTHORIZATION) String authorisation,
+        @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuthorization,
+        @PathVariable("assignmentId") String assignmentId
+    );
+
 
 }
