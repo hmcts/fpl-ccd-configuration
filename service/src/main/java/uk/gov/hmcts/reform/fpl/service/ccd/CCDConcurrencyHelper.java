04024f0354a088bfc3a9f5c11640b2720118b864
alistair.osborne <alistair.osborne@justice.gov.uk>
2023-08-08 15:45:38 +0100

    change to system log

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/ccd/CCDConcurrencyHelper.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ccd/CCDConcurrencyHelper.java
@@ -28,7 +28,7 @@ public class CCDConcurrencyHelper {
     public StartEventResponse startEvent(Long caseId, String eventName) {
         String userToken = systemUserService.getSysUserToken();
         String systemUpdateUserId = systemUserService.getUserId(userToken);
-        log.info("Event name for childControllerSubmittedTest {}", eventName);
+        System.err.println("Event name for childControllerSubmittedTest " + eventName );
 
         return coreCaseDataApi.startEventForCaseWorker(
             userToken,
