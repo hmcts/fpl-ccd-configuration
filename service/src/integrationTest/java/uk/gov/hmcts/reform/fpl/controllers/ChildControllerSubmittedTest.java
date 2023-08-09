45357d1a469308232612ca8ad8f28bc0d15048d1
alistair.osborne <alistair.osborne@justice.gov.uk>
2023-08-09 11:55:18 +0100

    remove submit event check

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ChildControllerSubmittedTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/ChildControllerSubmittedTest.java
@@ -204,9 +204,6 @@ class ChildControllerSubmittedTest extends AbstractCallbackTest {
 
         verify(concurrencyHelper, timeout(ASYNC_METHOD_CALL_TIMEOUT))
             .startEvent(eq(CASE_ID), eq("internal-update-case-summary"));
-
-        verify(concurrencyHelper, timeout(ASYNC_METHOD_CALL_TIMEOUT).times(3))
-            .submitEvent(any(), eq(CASE_ID), any());
     }
 
     @Test
