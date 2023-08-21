f50f5f5e7b6e430439277f67b8d95cc3a0ec0a2c
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-08-21 16:25:34 +0100

    checkstyle

--- service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/judicial/HandleHearingModificationRolesEventHandlerTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/handlers/judicial/HandleHearingModificationRolesEventHandlerTest.java
@@ -15,7 +15,6 @@ import uk.gov.hmcts.reform.fpl.service.JudicialService;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
