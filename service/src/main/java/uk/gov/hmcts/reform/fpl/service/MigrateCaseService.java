6bdb4ee2e0deef9bbc91ce66be9393876b667094
alistair.osborne <alistair.osborne@justice.gov.uk>
2023-08-22 16:00:21 +0100

    DFPL-1681-3: Add NC documents to removal

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/MigrateCaseService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/MigrateCaseService.java
@@ -710,6 +710,24 @@ public class MigrateCaseService {
         return Map.of("correspondenceDocuments", newCorrespondenceDocuments);
     }
 
+    public Map<String, Object> removeCorrespondenceDocumentNC(CaseData caseData,
+                                                            String migrationId,
+                                                            UUID expectedDocumentId) {
+
+        List<Element<SupportingEvidenceBundle>> newCorrespondenceDocuments =
+            caseData.getCorrespondenceDocuments().stream()
+                .filter(el -> !expectedDocumentId.equals(el.getId()))
+                .collect(toList());
+
+        if (newCorrespondenceDocuments.size() != caseData.getCorrespondenceDocuments().size() - 1) {
+            throw new AssertionError(format(
+                "Migration {id = %s, case reference = %s}, correspondence document not found",
+                migrationId, caseData.getId()));
+        }
+
+        return Map.of("correspondenceDocumentsNC", newCorrespondenceDocuments);
+    }
+
     public Map<String, Object> addRelatingLA(String migrationId, Long caseId) {
         // lookup in map
         Optional<String> relatingLA = migrateRelatingLAService.getRelatingLAString(caseId.toString());
