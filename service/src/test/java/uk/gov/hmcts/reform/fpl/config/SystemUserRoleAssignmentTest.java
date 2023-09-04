97c4f153cbb768b44d03fbbc9fd35f97de8bce30
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-09-04 12:21:42 +0100

    DFPL-1561: Extract out code to load judges/legal-advisers + setup AM system-user role as prereq ONLY

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/config/SystemUserRoleAssignmentTest.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.fpl.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.fpl.service.RoleAssignmentService;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+class SystemUserRoleAssignmentTest {
+
+    @Mock
+    private RoleAssignmentService roleAssignmentService;
+
+    @Test
+    void shouldAttemptToCreateSystemUpdateRole() {
+        SystemUserRoleAssignment underTest = new SystemUserRoleAssignment(roleAssignmentService);
+
+        underTest.init();
+
+        verify(roleAssignmentService).assignSystemUserRole();
+    }
+
+}
