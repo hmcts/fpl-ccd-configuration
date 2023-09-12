9fedfbd58c158d5360b9b6e39ee08fc21a9b16c9
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-09-12 14:33:50 +0100

    Fix merge error + remove completed migrations

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -23,12 +23,8 @@ import uk.gov.hmcts.reform.fpl.model.Judge;
 import uk.gov.hmcts.reform.fpl.model.JudicialUser;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.request.RequestData;
-import uk.gov.hmcts.reform.fpl.service.CourtLookUpService;
-import uk.gov.hmcts.reform.fpl.service.DfjAreaLookUpService;
-import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
 import uk.gov.hmcts.reform.fpl.service.JudicialService;
 import uk.gov.hmcts.reform.fpl.service.MigrateCaseService;
-import uk.gov.hmcts.reform.fpl.service.orders.ManageOrderDocumentScopedFieldsCalculator;
 import uk.gov.hmcts.reform.fpl.utils.RoleAssignmentUtils;
 
 import java.time.ZonedDateTime;
@@ -37,7 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
@@ -54,22 +49,11 @@ public class MigrateCaseController extends CallbackController {
     private final CoreCaseDataApiV2 coreCaseDataApi;
     private final RequestData requestData;
     private final AuthTokenGenerator authToken;
-    private final CourtLookUpService courtLookUpService;
-    private final FeatureToggleService featureToggleService;
 
     private final MigrateCaseService migrateCaseService;
-    private final ManageOrderDocumentScopedFieldsCalculator fieldsCalculator;
-    private final DfjAreaLookUpService dfjAreaLookUpService;
     private final JudicialService judicialService;
 
     private final Map<String, Consumer<CaseDetails>> migrations = Map.of(
-        "DFPL-1359", this::run1359,
-        "DFPL-1401", this::run1401,
-        "DFPL-1451", this::run1451,
-        "DFPL-1501", this::run1616,
-        "DFPL-1584", this::run1612,
-        "DFPL-1649", this::run1649,
-        "DFPL-1486", this::run1486,
         "DFPL-AM", this::runAM,
         "DFPL-AM-Rollback", this::runAmRollback,
         "DFPL-1725", this::run1725
@@ -125,55 +109,6 @@ public class MigrateCaseController extends CallbackController {
 
         log.info("Attempting to create {} roles on case {}", rolesToAssign.size(), caseData.getId());
         judicialService.migrateJudgeRoles(rolesToAssign);
-    }
-
-    private void run1359(CaseDetails caseDetails) {
-        migrateCaseService.doDocumentViewNCCheck(caseDetails.getId(), "DFPL-1359", caseDetails);
-        caseDetails.getData().putAll(migrateCaseService.refreshDocumentViews(getCaseData(caseDetails)));
-    }
-
-    private void run1401(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1401";
-        var possibleCaseIds = List.of(1666959378667166L);
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().put("relatingLA", "NCC");
-    }
-
-    private void run1451(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1451";
-        var possibleCaseIds = List.of(1669909306379829L);
-        final UUID expectedOrderId = UUID.fromString("c93a824b-75ce-4ffd-ad30-ad7f42c01ed9");
-        final UUID expectedHearingOrderBundleId = UUID.fromString("ebdf7ea7-a2e8-4296-be98-109b9070348e");
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeDraftUploadedCMOs(getCaseData(caseDetails),
-            migrationId, expectedOrderId));
-        caseDetails.getData().putAll(migrateCaseService.removeHearingOrdersBundlesDrafts(getCaseData(caseDetails),
-            migrationId, expectedHearingOrderBundleId));
-    }
-
-    private void run1612(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1612";
-        var possibleCaseIds = List.of(1687780363265112L);
-        UUID documentId = UUID.fromString("db163749-7c8a-45fe-88dd-63641560a9d9");
-        CaseData caseData = getCaseData(caseDetails);
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        migrateCaseService.verifyUrgentDirectionsOrderExists(caseData, migrationId, documentId);
-        caseDetails.getData().remove("urgentDirectionsOrder");
-    }
-
-    private void run1616(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1616";
-        var possibleCaseIds = List.of(1687526651029623L);
-        UUID documentId = UUID.fromString("528bd6a2-3221-4edb-8dc6-f8060937d443");
-        CaseData caseData = getCaseData(caseDetails);
-        migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
-        migrateCaseService.verifyUrgentDirectionsOrderExists(caseData, migrationId, documentId);
-        caseDetails.getData().remove("urgentDirectionsOrder");
-    }
-
-    private void run1486(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1486";
-        caseDetails.getData().putAll(migrateCaseService.addRelatingLA(migrationId, caseDetails.getId()));
     }
 
     private void runAmRollback(CaseDetails caseDetails) {
@@ -259,19 +194,6 @@ public class MigrateCaseController extends CallbackController {
         // Perform migration synchronously - we DO NOT catch if this fails, we need to stop the migration for this case
         migrateRoles(newCaseData);
     }
-
-    private void run1649(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1649";
-        long expectedCaseId = 1686829053861234L;
-        UUID expectedHearingId = UUID.fromString("55ecd69a-d4f3-4a1b-81ff-7144aa5f46f8");
-        UUID expectedCourtBundleId = UUID.fromString("7f14382f-c16e-497e-ab8c-f3f76e212a6c");
-        String messageId = "dd7e4072-41dd-46fa-a3dc-de32ee9bde93";
-        CaseData caseData = getCaseData(caseDetails);
-
-        migrateCaseService.doCaseIdCheck(caseDetails.getId(), expectedCaseId, migrationId);
-        caseDetails.getData().putAll(migrateCaseService.removeJudicialMessage(caseData, migrationId, messageId));
-        caseDetails.getData().putAll(migrateCaseService.removeCourtBundleByBundleId(caseData, migrationId,
-            expectedHearingId, expectedCourtBundleId));
 
     private void run1725(CaseDetails caseDetails) {
         var migrationId = "DFPL-1725";
