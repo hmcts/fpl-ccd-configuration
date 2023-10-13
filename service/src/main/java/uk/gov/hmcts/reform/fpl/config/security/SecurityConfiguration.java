a49bf316e3773969ff4e730a4f0f27927825c838
gauravchawla <gaurav-chawla@hotmail.com>
2023-10-13 09:55:49 +0100

    dependencies update

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/security/SecurityConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/security/SecurityConfiguration.java
@@ -1,15 +1,14 @@
 package uk.gov.hmcts.reform.fpl.config.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
 import uk.gov.hmcts.reform.auth.checker.spring.useronly.AuthCheckerUserOnlyFilter;
@@ -20,35 +19,33 @@ public class SecurityConfiguration {
 
     @Configuration
     @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "true")
-    static class SecurityConfigurationWithUserTokenValidator {
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    static class SecurityConfigurationWithUserTokenValidator extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
             http
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .authorizeRequests()
                     .antMatchers("/callback/**")
                     .authenticated();
-            return http.build();
         }
     }
 
     @Configuration
     @ConditionalOnProperty(value = "spring.security.enabled", havingValue = "false", matchIfMissing = true)
-    static class DefaultSecurityConfiguration {
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    static class DefaultSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
             http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeRequests()
                     .anyRequest()
                     .permitAll();
-            return http.build();
         }
     }
 
     @Order(2)
     @Configuration
-    static class RoboticsSecurityConfiguration {
+    static class RoboticsSecurityConfiguration extends WebSecurityConfigurerAdapter {
         private AuthCheckerUserOnlyFilter<User> authCheckerUserOnlyFilter;
 
         public RoboticsSecurityConfiguration(RequestAuthorizer<User> userRequestAuthorizer,
@@ -57,8 +54,8 @@ public class SecurityConfiguration {
             authCheckerUserOnlyFilter.setAuthenticationManager(authenticationManager);
         }
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
             http.requestMatchers()
                 .antMatchers(HttpMethod.POST, "/sendRPAEmailByID/*", "/support/**/*")
                 .and()
@@ -67,7 +64,6 @@ public class SecurityConfiguration {
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
-            return http.build();
         }
 
     }
