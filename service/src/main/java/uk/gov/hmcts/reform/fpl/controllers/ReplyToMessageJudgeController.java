cb29777b3172c7183c84bf6518edaf97e9c5d843
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-11-03 11:26:58 +0000

    DFPL-1856: Completely remove publishing event

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/ReplyToMessageJudgeController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.fpl.events.AfterSubmissionCaseDataUpdated;
 import uk.gov.hmcts.reform.fpl.events.JudicialMessageReplyEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
@@ -95,10 +94,5 @@ public class ReplyToMessageJudgeController extends CallbackController {
             publishEvent(new JudicialMessageReplyEvent(caseData, judicialMessage));
         }
 
-        /*
-         * Disable as
-        publishEvent(new AfterSubmissionCaseDataUpdated(getCaseData(callbackRequest),
-            getCaseDataBefore(callbackRequest)));
-        */
     }
 }
