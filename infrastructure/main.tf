a70b3d30bd2c9681a5337a2d7fdff7af49039d52
Paul Trelease <paul.trelease@justice.gov.uk>
2023-11-06 16:25:18 +0000

    DFPL-1754: Updated Terraform to removed pg v11, and add pg v15

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,6 +79,8 @@ module "fpl-scheduler-postgres-v15-flexible-server" {
   component          = var.component
   business_area      = "cft"
 
+  subnet_suffix = "expanded"
+
   pgsql_databases = [
     {
       name : var.fpl_scheduler_db_name_v15
@@ -90,7 +92,7 @@ module "fpl-scheduler-postgres-v15-flexible-server" {
   pgsql_server_configuration = [
     {
       name  = "azure.extensions"
-      value = "plpgsql, pg_stat_statements, pg_buffercache"
+      value = "plpgsql,pg_stat_statements,pg_buffercache"
     }
   ]
 
@@ -100,28 +102,8 @@ module "fpl-scheduler-postgres-v15-flexible-server" {
 
 }
 
-module "fpl-scheduler-db" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = "${var.product}-${var.component}"
-  location           = var.location_db
-  env                = var.env
-  database_name      = "fpl_scheduler"
-  postgresql_user    = "fpl_scheduler"
-  postgresql_version = "11"
-  sku_name           = "GP_Gen5_2"
-  sku_tier           = "GeneralPurpose"
-  common_tags        = var.common_tags
-  subscription       = var.subscription
-}
-
 data "azurerm_key_vault_secret" "fpl_support_email_secret" {
   name      = "${var.product}-support-email"
-  key_vault_id = module.key-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "scheduler-db-password" {
-  name      = "scheduler-db-password"
-  value     = module.fpl-scheduler-db.postgresql_password
   key_vault_id = module.key-vault.key_vault_id
 }
 
@@ -157,7 +139,7 @@ resource "azurerm_key_vault_secret" "scheduler-db-password-v15" {
   value        = module.fpl-scheduler-postgres-v15-flexible-server.password
   key_vault_id = module.key-vault.key_vault_id
 }
-    
+
 resource "azurerm_key_vault_secret" "update-summary-tab-cron" {
   name         = "update-summary-tab-cron"
   value        = "0 0 3 ? * * *"
