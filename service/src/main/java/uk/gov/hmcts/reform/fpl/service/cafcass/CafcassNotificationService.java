8887dd8110aba685ca51b9e6579d9905f3ace7e9
Ashley Wong <ashley.wong@justice.gov.uk>
2023-11-10 13:57:06 +0000

    https://github.com/hmcts/fpl-ccd-configuration/pull/4820

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/cafcass/CafcassNotificationService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/cafcass/CafcassNotificationService.java
@@ -231,7 +231,7 @@ public class CafcassNotificationService {
 
             String subject = String.join(SUBJECT_DELIMITER,
                     oldestChildsLastName,
-                    caseData.getFamilyManCaseNumber(),
+                    Optional.ofNullable(caseData.getFamilyManCaseNumber()).orElse(""),
                     String.valueOf(caseData.getId()),
                     cafcassDocumentMappingType,
                     additionalInfo);
