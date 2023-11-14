7b43897200426f778158d1e72789c52e8388fbfa
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2023-11-14 17:29:51 +0000

    fix checking for testing

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -222,11 +222,11 @@ public class MigrateCaseController extends CallbackController {
     }
 
     private void run1887(CaseDetails caseDetails) {
-        var migrationId = "DFPL-1887";
-        var possibleCaseIds = List.of(1699979033505404L);
+        String migrationId = "DFPL-1887";
+        List<Long> possibleCaseIds = List.of(1699979033505404L);
         migrateCaseService.doCaseIdCheckList(caseDetails.getId(), possibleCaseIds, migrationId);
 
-        var orgId = "BDWCNNQ";
+        String orgId = "BDWCNNQ";
 
         CaseData caseData = getCaseData(caseDetails);
 
