f322dd1217a65d52fe9234feea8bd25ce854952f
Paul Trelease <paul.trelease@justice.gov.uk>
2023-11-27 15:00:23 +0000

    DFPL-1894: Terraform postgres deletion privs removed (#4939)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,19 +12,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
+  features {}
   skip_provider_registration = true
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
