d8582e9fb34374ceb322e69009cf5f1527985935
Ashley Wong <ashley.wong@justice.gov.uk>
2023-12-01 17:14:33 +0000

    Remove unused imports.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -19,10 +19,8 @@ import uk.gov.hmcts.reform.fpl.service.MigrateCaseService;
 
 import java.util.Collection;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 import java.util.function.Consumer;
 
 @Api
