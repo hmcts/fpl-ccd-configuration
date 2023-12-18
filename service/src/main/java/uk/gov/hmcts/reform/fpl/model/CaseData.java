30bc7af4cf532d29606680de246e4651e3eec06d
Ashley Wong <ashley.wong@justice.gov.uk>
2023-12-18 17:26:14 +0000

    revert and test cases for temporaryApplicationDocuments.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/CaseData.java
@@ -1107,7 +1107,7 @@ public class CaseData extends CaseDataParent {
         return hearingEndDate.isBefore(LocalDateTime.now()) || hearingStartDate.isBefore(LocalDateTime.now());
     }
 
-    @Deprecated
+    @Deprecated(since = "DFPL-1438")
     private final List<Element<ApplicationDocument>> applicationDocuments;
     // It will be used in "upload-documents" event (when the case is in Open state)
     private final List<Element<ApplicationDocument>> temporaryApplicationDocuments;
