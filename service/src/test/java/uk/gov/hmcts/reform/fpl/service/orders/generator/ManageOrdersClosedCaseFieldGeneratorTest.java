c60ad13dd2968cb0d0be471a171427381317569d
dawudgovuk <dawud.rahman@justice.gov.uk>
2023-12-19 15:00:57 +0000

    Update Test for Set CloseCaseTabField with Final order approval date

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
@@ -13,11 +13,9 @@ import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.CloseCase;
 import uk.gov.hmcts.reform.fpl.model.event.ManageOrdersEventData;
 import uk.gov.hmcts.reform.fpl.model.order.Order;
-import uk.gov.hmcts.reform.fpl.service.time.Time;
 import uk.gov.hmcts.reform.fpl.updaters.ChildrenSmartFinalOrderUpdater;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -25,17 +23,11 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.enums.State.CLOSED;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C21_BLANK_ORDER;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C32A_CARE_ORDER;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C32B_DISCHARGE_OF_CARE_ORDER;
+import static uk.gov.hmcts.reform.fpl.model.order.Order.*;
 
 @ExtendWith({MockitoExtension.class})
 public class ManageOrdersClosedCaseFieldGeneratorTest {
-    private static final LocalDate TODAY = LocalDate.of(2012, 12, 22);
-    private static final LocalDateTime NOW = TODAY.atStartOfDay();
-
-    @Mock
-    private Time time;
+    private static final LocalDate APPROVAL_DATE =  LocalDate.of(2010, 01, 01);
 
     @Mock
     private ChildrenSmartFinalOrderUpdater childrenSmartFinalOrderUpdater;
@@ -45,16 +37,15 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldCloseCaseWhenFinalOrder() {
-        CaseData caseData = buildCaseData("Yes", "Yes", C32A_CARE_ORDER);
+        CaseData caseData = buildCaseData("Yes", "Yes", C32A_CARE_ORDER, APPROVAL_DATE);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
-        when(time.now()).thenReturn(NOW);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
         Map<String, Object> expectedData = Map.of(
             "state", CLOSED,
-            "closeCaseTabField", CloseCase.builder().date(time.now().toLocalDate()).build()
+            "closeCaseTabField", CloseCase.builder().date(APPROVAL_DATE).build()
         );
 
         assertThat(generatedData).containsAllEntriesOf(expectedData);
@@ -63,7 +54,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldNotCloseCaseAndReturnEmptyMapWhenNotFinalOrder() {
-        CaseData caseData = buildCaseData("Yes", "No", C32B_DISCHARGE_OF_CARE_ORDER);
+        CaseData caseData = buildCaseData("Yes", "No", C32B_DISCHARGE_OF_CARE_ORDER, APPROVAL_DATE);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
 
@@ -73,7 +64,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
     @Test
     void shouldUpdateChildrenAndNotCloseCase() {
 
-        CaseData caseData = buildCaseData("No", "No", C32A_CARE_ORDER);
+        CaseData caseData = buildCaseData("No", "No", C32A_CARE_ORDER, APPROVAL_DATE);
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
 
@@ -86,7 +77,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldNotUpdateChildrenWhenNotFinalOrder() {
-        CaseData caseData = buildCaseData("No", "No", C21_BLANK_ORDER);
+        CaseData caseData = buildCaseData("No", "No", C21_BLANK_ORDER, APPROVAL_DATE);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
 
@@ -95,7 +86,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldUpdateChildrenWhenUserHasSelectedFinalOrder() {
-        CaseData caseData = buildCaseData("No", "Yes", C21_BLANK_ORDER);
+        CaseData caseData = buildCaseData("No", "Yes", C21_BLANK_ORDER, APPROVAL_DATE);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
@@ -105,11 +96,12 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
         assertThat(generatedData).containsKey("children1");
     }
 
-    private CaseData buildCaseData(String closeCase, String isFinalOrder, Order order) {
+    private CaseData buildCaseData(String closeCase, String isFinalOrder, Order order, LocalDate approvalDate) {
         ManageOrdersEventData manageOrdersEventData = ManageOrdersEventData.builder()
             .manageOrdersCloseCase(closeCase)
             .manageOrdersIsFinalOrder(isFinalOrder)
             .manageOrdersType(order)
+            .manageOrdersApprovalDate(approvalDate)
             .build();
 
         return CaseData.builder()
@@ -120,16 +112,15 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
     @ParameterizedTest
     @MethodSource("generateOrderStates")
     void shouldCloseCaseWhenOrderIsDefaultFinal(String isFinalOrder) {
-        CaseData caseData = buildCaseData("Yes", isFinalOrder, C32A_CARE_ORDER);
+        CaseData caseData = buildCaseData("Yes", isFinalOrder, C32A_CARE_ORDER, APPROVAL_DATE);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
                 .thenReturn(Collections.emptyList());
-        when(time.now()).thenReturn(NOW);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
         Map<String, Object> expectedData = Map.of(
                 "state", CLOSED,
-                "closeCaseTabField", CloseCase.builder().date(time.now().toLocalDate()).build()
+                "closeCaseTabField", CloseCase.builder().date(APPROVAL_DATE).build()
         );
 
         assertThat(generatedData).containsAllEntriesOf(expectedData);
