00630c83c7e534688631ecd686f58aa06de3314f
Ashley Wong <ashley.wong@justice.gov.uk>
2023-12-28 15:19:53 +0000

    Add test case for C1WithSupplementChecker

---
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/validators/C1WithSupplementCheckerTest.java
@@ -0,0 +1,63 @@
+package uk.gov.hmcts.reform.fpl.service.validators;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
+import uk.gov.hmcts.reform.fpl.model.common.SubmittedC1WithSupplementBundle;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static uk.gov.hmcts.reform.fpl.model.tasklist.TaskState.COMPLETED_FINISHED;
+
+@ExtendWith(MockitoExtension.class)
+class C1WithSupplementCheckerTest {
+
+    private static final CaseData ANY_CASE_DATA = mock(CaseData.class);
+
+    @InjectMocks
+    private C1WithSupplementChecker c1WithSupplementChecker;
+
+    @Test
+    void testValidate() {
+        assertThat(c1WithSupplementChecker.validate(ANY_CASE_DATA)).isEmpty();
+    }
+
+    @Test
+    void testCompletedState() {
+        assertThat(c1WithSupplementChecker.completedState()).isEqualTo(COMPLETED_FINISHED);
+    }
+
+    @Nested
+    class IsCompleted {
+
+        @Test
+        void shouldReturnEmptyErrorsAndNonCompletedState() {
+            final CaseData caseData = CaseData.builder()
+                .submittedC1WithSupplement(SubmittedC1WithSupplementBundle.builder()
+                    .document(null)
+                    .build())
+                .build();
+
+            final boolean isCompleted = c1WithSupplementChecker.isCompleted(caseData);
+
+            assertThat(isCompleted).isFalse();
+        }
+
+        @Test
+        void shouldReturnEmptyErrorsAndCompletedState() {
+            final CaseData caseData = CaseData.builder()
+                .submittedC1WithSupplement(SubmittedC1WithSupplementBundle.builder()
+                    .document(DocumentReference.builder().filename("ABC.docx").build())
+                    .build())
+                .build();
+
+            final boolean isCompleted = c1WithSupplementChecker.isCompleted(caseData);
+
+            assertThat(isCompleted).isTrue();
+        }
+    }
+}
