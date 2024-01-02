333ab7bc44e500fc7208ee10cb6691f525c7a280
Ashley Wong <ashley.wong@justice.gov.uk>
2024-01-02 13:45:05 +0000

    Test case for empty temporaryApplicationDocuments

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadDocumentsAboutToStartControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadDocumentsAboutToStartControllerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.fpl.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
@@ -15,6 +16,7 @@ import uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,6 +26,12 @@ import static org.mockito.Mockito.when;
 @OverrideAutoConfiguration(enabled = true)
 class UploadDocumentsAboutToStartControllerTest extends AbstractCallbackTest {
 
+    private static final UUID UUID_1 = UUID.randomUUID();
+    private static final String FILE_URL = "https://docuURL";
+    private static final String FILE_NAME = "mockChecklist.pdf";
+    private static final String FILE_BINARY_URL = "http://dm-store:8080/documents/fakeUrl/binary";
+    private static final String USER = "kurt@swansea.gov.uk";
+    
     @MockBean
     private ManageDocumentService manageDocumentService;
 
@@ -50,5 +58,39 @@ class UploadDocumentsAboutToStartControllerTest extends AbstractCallbackTest {
 
         assertThat(actualDocument).hasSize(1)
             .allMatch(c -> (allow ? "YES" : "NO").equals(c.getValue().getAllowMarkDocumentConfidential()));
+    }
+
+    @Test
+    void shouldReturnExistingTemporaryApplicationDocuments() {
+        CaseDetails caseDetailsBefore = CaseDetails.builder().data(
+            Map.of("temporaryApplicationDocuments", List.of(
+                Map.of(
+                    "id", UUID_1,
+                    "value", Map.of(
+                        "document", Map.of(
+                            "document_url", FILE_URL,
+                            "document_filename", FILE_NAME,
+                            "document_binary_url", FILE_BINARY_URL
+                        ),
+                        "uploadedBy", USER,
+                        "dateTimeUploaded", "2020-12-03T02:03:04.000010",
+                        "documentType", "SOCIAL_WORK_STATEMENT",
+                        "allowMarkDocumentConfidential", "NO"
+                    )
+                )
+            ))).build();
+
+        CallbackRequest callbackRequest = CallbackRequest.builder()
+            .caseDetails(caseDetailsBefore)
+            .caseDetailsBefore(caseDetailsBefore)
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse callbackResponse = postAboutToStartEvent(callbackRequest);
+
+        CaseData responseCaseData = mapper.convertValue(callbackResponse.getData(), CaseData.class);
+        List<Element<ApplicationDocument>> actualDocument = responseCaseData.getTemporaryApplicationDocuments();
+
+        assertThat(actualDocument).hasSize(1)
+            .allMatch(c -> "NO".equals(c.getValue().getAllowMarkDocumentConfidential()));
     }
 }
