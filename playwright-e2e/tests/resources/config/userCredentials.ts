0030ee8fb026e534e57e5e1726dac61ea8ca0194
Georgina <georgina.hall1@hmcts.net>
2024-01-08 13:55:24 +0000

    fixed issues

--- playwright-e2e/tests/resources/config/userCredentials.ts
+++
@@ -1,8 +0,0 @@
-const defaultPassword = "Password12";
-
-export const swanseaLocalAuthorityUserOne = {
-  email: "kurt@swansea.gov.uk",
-  password: process.env.LA_USER_PASSWORD || defaultPassword,
-  forename: "kurt@swansea.gov.uk",
-  surname: "(local-authority)",
-};
