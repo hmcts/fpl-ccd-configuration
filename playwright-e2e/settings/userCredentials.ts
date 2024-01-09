56c23f59208ce56f235d274b916dd986284fbbba
Georgina <georgina.hall1@hmcts.net>
2024-01-09 16:25:09 +0000

    testing out ignoring default pw

--- playwright-e2e/settings/userCredentials.ts
+++ playwright-e2e/settings/userCredentials.ts
@@ -1,8 +1,12 @@
 import { DefaultPassword } from "../local-config/config";
 
-const e2ePw = process.env.E2E_TEST_PASSWORD || DefaultPassword;
+let e2ePw = process.env.E2E_TEST_PASSWORD || DefaultPassword;
 
 export const newSwanseaLocalAuthorityUserOne = {
   email: "local-authority-swansea-0001@maildrop.cc",
   password: e2ePw,
 };
+
+if (process.env.JENKINS_BUILD) {
+  e2ePw = "";
+}
