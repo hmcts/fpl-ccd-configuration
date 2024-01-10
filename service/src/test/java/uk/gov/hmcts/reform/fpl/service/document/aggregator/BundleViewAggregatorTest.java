7348a511fbc81b1bc2915a4918bfc4b491d0940b
Ashley Wong <ashley.wong@justice.gov.uk>
2024-01-10 16:27:31 +0000

    add test case

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/document/aggregator/BundleViewAggregatorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/document/aggregator/BundleViewAggregatorTest.java
@@ -2,14 +2,23 @@ package uk.gov.hmcts.reform.fpl.service.document.aggregator;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import uk.gov.hmcts.reform.fpl.enums.cfv.DocumentType;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.HearingFurtherEvidenceBundle;
+import uk.gov.hmcts.reform.fpl.model.ManagedDocument;
+import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
 import uk.gov.hmcts.reform.fpl.model.documentview.DocumentContainerView;
+import uk.gov.hmcts.reform.fpl.model.documentview.DocumentView;
 import uk.gov.hmcts.reform.fpl.model.documentview.DocumentViewType;
+import uk.gov.hmcts.reform.fpl.service.document.ManageDocumentService;
 import uk.gov.hmcts.reform.fpl.service.document.transformer.ApplicationDocumentBundleTransformer;
 import uk.gov.hmcts.reform.fpl.service.document.transformer.FurtherEvidenceDocumentsBundlesTransformer;
 import uk.gov.hmcts.reform.fpl.service.document.transformer.OtherDocumentsTransformer;
@@ -25,9 +34,14 @@ import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.fpl.enums.cfv.ConfidentialLevel.CTSC;
+import static uk.gov.hmcts.reform.fpl.enums.cfv.ConfidentialLevel.LA;
+import static uk.gov.hmcts.reform.fpl.enums.notification.DocumentUploaderType.DESIGNATED_LOCAL_AUTHORITY;
+import static uk.gov.hmcts.reform.fpl.model.documentview.DocumentViewType.HMCTS;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.element;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class BundleViewAggregatorTest {
 
     private static final DocumentViewType DOCUMENT_VIEW_TYPE = DocumentViewType.HMCTS;
@@ -58,6 +72,9 @@ class BundleViewAggregatorTest {
 
     @InjectMocks
     private BundleViewAggregator underTest;
+
+    @Mock
+    private ManageDocumentService manageDocumentService;
 
     @Test
     void testGetDocumentBundleViews() {
@@ -100,5 +117,80 @@ class BundleViewAggregatorTest {
         List<DocumentContainerView> actual = underTest.getDocumentBundleViews(CASE_DATA, DOCUMENT_VIEW_TYPE);
 
         assertThat(actual).isEqualTo(Collections.emptyList());
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = DocumentType.class, names = {
+        "CASE_SUMMARY", "POSITION_STATEMENTS", "POSITION_STATEMENTS_CHILD",
+        "POSITION_STATEMENTS_RESPONDENT", "THRESHOLD", "SKELETON_ARGUMENTS", "JUDGEMENTS", "TRANSCRIPTS",
+        "DOCUMENTS_FILED_ON_ISSUE", "APPLICANTS_WITNESS_STATEMENTS", "CARE_PLAN", "PARENT_ASSESSMENTS",
+        "FAMILY_AND_VIABILITY_ASSESSMENTS", "APPLICANTS_OTHER_DOCUMENTS", "MEETING_NOTES", "CONTACT_NOTES",
+        "C1_APPLICATION_DOCUMENTS", "C2_APPLICATION_DOCUMENTS", "RESPONDENTS_STATEMENTS",
+        "RESPONDENTS_WITNESS_STATEMENTS", "GUARDIAN_EVIDENCE", "EXPERT_REPORTS", "DRUG_AND_ALCOHOL_REPORTS",
+        "LETTER_OF_INSTRUCTION", "POLICE_DISCLOSURE", "MEDICAL_RECORDS", "COURT_CORRESPONDENCE",
+        "NOTICE_OF_ACTING_OR_ISSUE"
+    })
+    void testGetConfidentialDocumentBundleViews(DocumentType documentType) {
+        String filename1 = "FILE_NAME1.pdf";
+        String filename2 = "FILE_NAME2.pdf";
+
+        Element<ManagedDocument> confDoc1 = element(ManagedDocument.builder()
+            .uploaderType(DESIGNATED_LOCAL_AUTHORITY)
+            .document(DocumentReference.builder().filename(filename1).build())
+            .build());
+        Element<ManagedDocument> confDoc2 = element(ManagedDocument.builder()
+            .document(DocumentReference.builder().filename(filename2).build())
+            .build());
+
+        when(manageDocumentService.retrieveDocuments(CASE_DATA, documentType, LA)).thenReturn(List.of(confDoc1));
+        when(manageDocumentService.retrieveDocuments(CASE_DATA, documentType, CTSC)).thenReturn(List.of(confDoc2));
+
+        List<DocumentView> actual = underTest.getConfidentialDocumentBundleViews(CASE_DATA, HMCTS);
+        assertThat(actual).hasSize(2).contains(
+            DocumentView.builder()
+                .title(filename1)
+                .fileName(filename1)
+                .confidential(true)
+                .confidentialToHmcts(false)
+                .uploaderType(DESIGNATED_LOCAL_AUTHORITY.name())
+                .build(),
+            DocumentView.builder()
+                .title(filename2)
+                .fileName(filename2)
+                .confidential(true)
+                .confidentialToHmcts(true)
+                .uploaderType("unknown")
+                .build());
+
+        actual = underTest.getConfidentialDocumentBundleViews(CASE_DATA, DocumentViewType.LA);
+        assertThat(actual).hasSize(1).contains(
+            DocumentView.builder()
+                .title(filename1)
+                .fileName(filename1)
+                .confidential(true)
+                .confidentialToHmcts(false)
+                .uploaderType(DESIGNATED_LOCAL_AUTHORITY.name())
+                .build());
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = DocumentType.class, names = {
+        "CASE_SUMMARY", "POSITION_STATEMENTS", "POSITION_STATEMENTS_CHILD",
+        "POSITION_STATEMENTS_RESPONDENT", "THRESHOLD", "SKELETON_ARGUMENTS", "JUDGEMENTS", "TRANSCRIPTS",
+        "DOCUMENTS_FILED_ON_ISSUE", "APPLICANTS_WITNESS_STATEMENTS", "CARE_PLAN", "PARENT_ASSESSMENTS",
+        "FAMILY_AND_VIABILITY_ASSESSMENTS", "APPLICANTS_OTHER_DOCUMENTS", "MEETING_NOTES", "CONTACT_NOTES",
+        "C1_APPLICATION_DOCUMENTS", "C2_APPLICATION_DOCUMENTS", "RESPONDENTS_STATEMENTS",
+        "RESPONDENTS_WITNESS_STATEMENTS", "GUARDIAN_EVIDENCE", "EXPERT_REPORTS", "DRUG_AND_ALCOHOL_REPORTS",
+        "LETTER_OF_INSTRUCTION", "POLICE_DISCLOSURE", "MEDICAL_RECORDS", "COURT_CORRESPONDENCE",
+        "NOTICE_OF_ACTING_OR_ISSUE"
+    })
+    void testGetEmptyConfidentialDocumentBundleViews(DocumentType documentType) {
+        when(manageDocumentService.retrieveDocuments(CASE_DATA, documentType, LA)).thenReturn(List.of());
+        when(manageDocumentService.retrieveDocuments(CASE_DATA, documentType, CTSC)).thenReturn(List.of());
+
+        List<DocumentView> actual = underTest.getConfidentialDocumentBundleViews(CASE_DATA, HMCTS);
+        assertThat(actual).isEmpty();
+        actual = underTest.getConfidentialDocumentBundleViews(CASE_DATA, DocumentViewType.LA);
+        assertThat(actual).isEmpty();
     }
 }
