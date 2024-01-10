ea24f23de1a535fe68cba10c6997cf3ffa718f26
Georgina <georgina.hall1@hmcts.net>
2024-01-10 16:38:29 +0000

    testing config changes for pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,6 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { outputFolder: "../fpl-ccd-configuration/output/" }],
     ["list"],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
