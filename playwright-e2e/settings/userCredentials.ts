a7b70c43d4a6ec45f5ef536648136044d680e8a9
Georgina Hall <georginahall@yahoo.com>
2024-01-10 16:25:31 +0000

    Set up Playwright Framework for FPL (#5043)
    * creating new branch for playwright e2e tests
    
    * adding script to run playwright in nightly
    
    * fix dependencies
    
    * update test user credentials
    
    * added readme.md and renamed folders
    
    * updated readme.md
    
    * added local config for e2epw
    
    * updating readme for local config
    
    * updating readme for local config
    
    * fix reporting for pipeline
    
    * updating framework
    
    * updating framework
    
    * testing reporting
    
    ---------
    
    Co-authored-by: Georgina <“georgina.hall1@hmcts.net”>

--- playwright-e2e/settings/userCredentials.ts
+++ playwright-e2e/settings/userCredentials.ts
@@ -1,6 +1,4 @@
-//import { DefaultPassword } from "../local-config/config";
-
-let e2ePw = process.env.E2E_TEST_PASSWORD || "Password1234";
+const e2ePw = process.env.E2E_TEST_PASSWORD || "";
 
 export const newSwanseaLocalAuthorityUserOne = {
   email: "local-authority-swansea-0001@maildrop.cc",
