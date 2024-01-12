8e2e7c7df07c42fcecd58b30a7a1bb568ada1ebc
dawudgovuk <dawud.rahman@justice.gov.uk>
2024-01-12 03:37:01 +0000

    Update Tests

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/orders/generator/ManageOrdersClosedCaseFieldGeneratorTest.java
@@ -13,9 +13,11 @@ import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.CloseCase;
 import uk.gov.hmcts.reform.fpl.model.event.ManageOrdersEventData;
 import uk.gov.hmcts.reform.fpl.model.order.Order;
+import uk.gov.hmcts.reform.fpl.service.time.Time;
 import uk.gov.hmcts.reform.fpl.updaters.ChildrenSmartFinalOrderUpdater;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -23,13 +25,17 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.enums.State.CLOSED;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C21_BLANK_ORDER;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C32A_CARE_ORDER;
-import static uk.gov.hmcts.reform.fpl.model.order.Order.C32B_DISCHARGE_OF_CARE_ORDER;
+import static uk.gov.hmcts.reform.fpl.model.order.Order.*;
 
 @ExtendWith({MockitoExtension.class})
 public class ManageOrdersClosedCaseFieldGeneratorTest {
     private static final LocalDate APPROVAL_DATE =  LocalDate.of(2010, 01, 01);
+    private static final LocalDateTime APPROVAL_DATE_TIME =  LocalDateTime.of(2010, 01, 01, 10, 00, 00 );
+    private static final LocalDate TODAY = LocalDate.of(2012, 12, 22);
+    private static final LocalDateTime NOW = TODAY.atStartOfDay();
+
+    @Mock
+    private Time time;
 
     @Mock
     private ChildrenSmartFinalOrderUpdater childrenSmartFinalOrderUpdater;
@@ -39,7 +45,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldCloseCaseWhenFinalOrder() {
-        CaseData caseData = buildCaseData("Yes", "Yes", C32A_CARE_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("Yes", "Yes", C32A_CARE_ORDER, APPROVAL_DATE, null);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
@@ -56,7 +62,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldNotCloseCaseAndReturnEmptyMapWhenNotFinalOrder() {
-        CaseData caseData = buildCaseData("Yes", "No", C32B_DISCHARGE_OF_CARE_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("Yes", "No", C32B_DISCHARGE_OF_CARE_ORDER, APPROVAL_DATE, null);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
 
@@ -66,7 +72,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
     @Test
     void shouldUpdateChildrenAndNotCloseCase() {
 
-        CaseData caseData = buildCaseData("No", "No", C32A_CARE_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("No", "No", C32A_CARE_ORDER, APPROVAL_DATE, null);
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
 
@@ -79,7 +85,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldNotUpdateChildrenWhenNotFinalOrder() {
-        CaseData caseData = buildCaseData("No", "No", C21_BLANK_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("No", "No", C21_BLANK_ORDER, APPROVAL_DATE, null);
 
         Map<String, Object> generatedData = underTest.generate(caseData);
 
@@ -88,7 +94,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
 
     @Test
     void shouldUpdateChildrenWhenUserHasSelectedFinalOrder() {
-        CaseData caseData = buildCaseData("No", "Yes", C21_BLANK_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("No", "Yes", C21_BLANK_ORDER, APPROVAL_DATE, null);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
             .thenReturn(Collections.emptyList());
@@ -98,12 +104,13 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
         assertThat(generatedData).containsKey("children1");
     }
 
-    private CaseData buildCaseData(String closeCase, String isFinalOrder, Order order, LocalDate approvalDate) {
+    private CaseData buildCaseData(String closeCase, String isFinalOrder, Order order, LocalDate approvalDate, LocalDateTime approvalDateTime) {
         ManageOrdersEventData manageOrdersEventData = ManageOrdersEventData.builder()
             .manageOrdersCloseCase(closeCase)
             .manageOrdersIsFinalOrder(isFinalOrder)
             .manageOrdersType(order)
             .manageOrdersApprovalDate(approvalDate)
+            .manageOrdersApprovalDateTime(approvalDateTime)
             .build();
 
         return CaseData.builder()
@@ -114,7 +121,7 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
     @ParameterizedTest
     @MethodSource("generateOrderStates")
     void shouldCloseCaseWhenOrderIsDefaultFinal(String isFinalOrder) {
-        CaseData caseData = buildCaseData("Yes", isFinalOrder, C32A_CARE_ORDER, APPROVAL_DATE);
+        CaseData caseData = buildCaseData("Yes", isFinalOrder, C32A_CARE_ORDER, APPROVAL_DATE, null);
 
         when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
                 .thenReturn(Collections.emptyList());
@@ -123,6 +130,39 @@ public class ManageOrdersClosedCaseFieldGeneratorTest {
         Map<String, Object> expectedData = Map.of(
                 "state", CLOSED,
                 "closeCaseTabField", CloseCase.builder().date(APPROVAL_DATE).build()
+        );
+
+        assertThat(generatedData).containsAllEntriesOf(expectedData);
+        assertThat(generatedData).containsKey("children1");
+    }
+    @Test
+    void shouldCloseCaseWhenOrderIsDefaultFinalAndApprovalDateTime() {
+        CaseData caseData = buildCaseData("Yes", "Yes", C26_SECURE_ACCOMMODATION_ORDER, null, APPROVAL_DATE_TIME);
+
+        when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
+            .thenReturn(Collections.emptyList());
+
+        Map<String, Object> generatedData = underTest.generate(caseData);
+        Map<String, Object> expectedData = Map.of(
+            "state", CLOSED,
+            "closeCaseTabField", CloseCase.builder().date(APPROVAL_DATE).build()
+        );
+
+        assertThat(generatedData).containsAllEntriesOf(expectedData);
+        assertThat(generatedData).containsKey("children1");
+    }
+    @Test
+    void shouldCloseCaseWhenOrderIsDefaultFinalWhenNoApprovalDate() {
+        CaseData caseData = buildCaseData("Yes", "Yes", C26_SECURE_ACCOMMODATION_ORDER, null, null);
+
+        when(childrenSmartFinalOrderUpdater.updateFinalOrderIssued(caseData))
+            .thenReturn(Collections.emptyList());
+        when(time.now()).thenReturn(NOW);
+
+        Map<String, Object> generatedData = underTest.generate(caseData);
+        Map<String, Object> expectedData = Map.of(
+            "state", CLOSED,
+            "closeCaseTabField", CloseCase.builder().date(time.now().toLocalDate()).build()
         );
 
         assertThat(generatedData).containsAllEntriesOf(expectedData);
