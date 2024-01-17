8269c825d231f156e0a8f7b095a2e59e902f5d6c
Georgina <georgina.hall1@hmcts.net>
2024-01-17 11:14:47 +0000

    adding in CI parallelism

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'html' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -57,10 +57,10 @@ export default defineConfig({
     //   name: "Mobile Chrome",
     //   use: { ...devices["Pixel 5"] },
     // },
-    {
-      name: "Mobile Safari",
-      use: { ...devices["iPad Mini"] },
-    },
+    // {
+    //   name: "Mobile Safari",
+    //   use: { ...devices["iPad Mini"] },
+    // },
 
     /* Test against branded browsers. */
     // {
