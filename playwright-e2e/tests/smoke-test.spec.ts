c9777b0236a389ccd53c0306e4f218788b8f927d
Georgina <georgina.hall1@hmcts.net>
2024-01-18 13:41:11 +0000

    DFPL-2095 Enhance Smoke Test to include initial step in Case Creation

--- playwright-e2e/tests/smoke-test.spec.ts
+++ playwright-e2e/tests/smoke-test.spec.ts
@@ -1,11 +1,16 @@
 import { test } from "../fixtures/create-fixture";
 import { newSwanseaLocalAuthorityUserOne } from "../settings/userCredentials";
 
-test("Smoke Test @smoke-test", async ({ signInPage }) => {
+test("Smoke Test @smoke-test", async ({ signInPage, smokeCreateCase }) => {
+
   await signInPage.visit();
   await signInPage.login(
     newSwanseaLocalAuthorityUserOne.email,
     newSwanseaLocalAuthorityUserOne.password,
   );
   await signInPage.isSignedIn();
+  await smokeCreateCase.CaseName();
+  await smokeCreateCase.CreateCase();
+  await smokeCreateCase.SubmitCase(smokeCreateCase.generatedCaseName);
+  await smokeCreateCase.CheckCaseIsCreated(smokeCreateCase.generatedCaseName);
 });
