0a98a55fd2f46b27a533666d74a7a5486c5fca0a
Georgina Hall <georginahall@yahoo.com>
2024-01-23 16:58:56 +0000

    DFPL-2106 Enhance Smoke Test to Include Hearing Urgency (#5097)
    * DFPL-2106 Enhance Smoke Test to Include Hearing Urgency
    
    * DFPL-2106 refactor: update member names for consistancy in code
    
    ---------
    
    Co-authored-by: Georgina <“georgina.hall1@hmcts.net”>

--- playwright-e2e/fixtures/create-fixture.ts
+++ playwright-e2e/fixtures/create-fixture.ts
@@ -1,14 +1,16 @@
 import { test as base } from "@playwright/test";
 import { SignInPage } from "../pages/sign-in";
-import { SmokeCreateCase } from "../pages/create-case";
+import { CreateCase } from "../pages/create-case";
 import { StartApplication } from "../pages/start-application";
 import { OrdersAndDirectionSought } from "../pages/orders-and-directions";
+import { HearingUrgency } from "../pages/hearing-urgency";
 
 type CreateFixtures = {
   signInPage: SignInPage;
-  smokeCreateCase: SmokeCreateCase;
+  createCase: CreateCase;
   startApplication: StartApplication;
   ordersAndDirectionSought: OrdersAndDirectionSought;
+  hearingUrgency : HearingUrgency;
 };
 
 export const test = base.extend<CreateFixtures>({
@@ -16,8 +18,8 @@ export const test = base.extend<CreateFixtures>({
     await use(new SignInPage(page));
   },
 
-  smokeCreateCase: async ({ page }, use) => {
-    await use(new SmokeCreateCase(page));
+  createCase: async ({ page }, use) => {
+    await use(new CreateCase(page));
   },
 
   startApplication: async ({ page }, use) => {
@@ -26,5 +28,9 @@ export const test = base.extend<CreateFixtures>({
 
   ordersAndDirectionSought: async ({ page }, use) => {
     await use(new OrdersAndDirectionSought(page));
+  },
+
+  hearingUrgency: async ({ page }, use) => {
+    await use(new HearingUrgency(page));
   },
 });
