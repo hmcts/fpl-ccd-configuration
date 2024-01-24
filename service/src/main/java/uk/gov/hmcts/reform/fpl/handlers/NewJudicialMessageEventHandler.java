cacbcc5c8e59cd6d99678f1394f34e08e38f6d42
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-01-24 16:49:53 +0000

    DFPL-1972: Toggle off Send + Reply to message emails

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/NewJudicialMessageEventHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/NewJudicialMessageEventHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -8,26 +9,34 @@ import uk.gov.hmcts.reform.fpl.events.NewJudicialMessageEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.judicialmessage.JudicialMessage;
 import uk.gov.hmcts.reform.fpl.model.notify.NewJudicialMessageTemplate;
+import uk.gov.hmcts.reform.fpl.service.FeatureToggleService;
 import uk.gov.hmcts.reform.fpl.service.email.NotificationService;
 import uk.gov.hmcts.reform.fpl.service.email.content.JudicialMessageContentProvider;
 
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.JUDICIAL_MESSAGE_ADDED_TEMPLATE;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class NewJudicialMessageEventHandler {
     private final NotificationService notificationService;
     private final JudicialMessageContentProvider newJudicialMessageContentProvider;
+    private final FeatureToggleService featureToggleService;
 
     @EventListener
     public void notifyJudicialMessageRecipient(NewJudicialMessageEvent event) {
-        CaseData caseData = event.getCaseData();
-        JudicialMessage newJudicialMessage = event.getJudicialMessage();
+        if (featureToggleService.isWATaskEmailsEnabled()) {
+            CaseData caseData = event.getCaseData();
+            JudicialMessage newJudicialMessage = event.getJudicialMessage();
 
-        NewJudicialMessageTemplate notifyData =
-            newJudicialMessageContentProvider.buildNewJudicialMessageTemplate(caseData, newJudicialMessage);
+            NewJudicialMessageTemplate notifyData =
+                newJudicialMessageContentProvider.buildNewJudicialMessageTemplate(caseData, newJudicialMessage);
 
-        notificationService.sendEmail(JUDICIAL_MESSAGE_ADDED_TEMPLATE, newJudicialMessage.getRecipient(),
-            notifyData, caseData.getId());
+            notificationService.sendEmail(JUDICIAL_MESSAGE_ADDED_TEMPLATE, newJudicialMessage.getRecipient(),
+                notifyData, caseData.getId());
+        } else {
+            log.info("Would have sent Judicial Message email on case {}, but was disabled, should have created a task",
+                event.getCaseData().getId());
+        }
     }
 }
