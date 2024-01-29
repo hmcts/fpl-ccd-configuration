c1331ca824948a952ac5dc9e9977150d6aa575fc
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-01-29 14:25:21 +0000

    Remove unused import

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/ManageHearingsService.java
@@ -61,7 +61,6 @@ import static uk.gov.hmcts.reform.fpl.enums.HearingStatus.ADJOURNED_TO_BE_RE_LIS
 import static uk.gov.hmcts.reform.fpl.enums.HearingStatus.VACATED;
 import static uk.gov.hmcts.reform.fpl.enums.HearingStatus.VACATED_AND_RE_LISTED;
 import static uk.gov.hmcts.reform.fpl.enums.HearingStatus.VACATED_TO_BE_RE_LISTED;
-import static uk.gov.hmcts.reform.fpl.enums.HearingType.OTHER;
 import static uk.gov.hmcts.reform.fpl.enums.YesNo.NO;
 import static uk.gov.hmcts.reform.fpl.enums.YesNo.YES;
 import static uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper.formatLocalDateTimeBaseUsingFormat;
