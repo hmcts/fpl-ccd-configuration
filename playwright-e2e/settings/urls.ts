826fd0eed28eb723c1e042b959fb298048e56f10
Georgina <georgina.hall1@hmcts.net>
2024-02-06 14:36:14 +0000

    DFPL-2161 updated e2e README.md

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -3,9 +3,9 @@ interface UrlConfig {
 }
 
 const urlConfig: UrlConfig = {
-  aatIdamUrl: process.env.AAT_IDAM_URL || "",
-  aatServiceUrl: process.env.AAT_SERVICE_URL || "",
-  frontEndBaseURL: process.env.FE_BASE_URL || "",
+  aatIdamUrl: process.env.AAT_IDAM_URL || "https://idam-api.aat.platform.hmcts.net/",
+  aatServiceUrl: process.env.AAT_SERVICE_URL || "http://fpl-case-service-aat.service.core-compute-aat.internal/",
+  frontEndBaseURL: process.env.FE_BASE_URL || "https://manage-case.aat.platform.hmcts.net/",
   // You can add other URLs as needed
 };
 
