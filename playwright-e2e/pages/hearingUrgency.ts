c3bd4c0e57e9e3fdcbe1485c7bfdc853abb81273
Braimah101 <41795070+Braimah101@users.noreply.github.com>
2024-02-06 07:53:49 +0000

    Update hearingUrgency.ts

--- playwright-e2e/pages/hearingUrgency.ts
+++ playwright-e2e/pages/hearingUrgency.ts
@@ -131,185 +131,21 @@ async ClickOnCancelButton(){
 
 }
 
-  // readonly page: Page;
-  // readonly timeFrameLabels: string[];
-  // readonly typeOfHearingLabels: string[];
-  // readonly doYouNeedAWithoutNoticeHearing: string[];
-  // readonly doYouNeedAHearingWithReducedNotice: string[];
-  // readonly areRespondentsAwareOfProceedings: string[];
-  // readonly hearingNeeded: Record<string, Locator>;
-  // readonly continueButton: Locator;
-  // readonly checkYourAnswers: Locator;
-  // readonly saveAndContinueButton: Locator;
+  
 
-  // readonly doYouNeedAWithoutNoticeGroup: Locator;
-  // readonly doYouNeedAHearingWithReducedNoiseGroup: Locator;
-  // readonly areRespondentsAwareOfProceedingsGroup: Locator;
-
-  // readonly giveReasonTextBox: Locator;
-  // HearingUrgencyHeading: any;
-  // Withing18daysRadioBtn: any;
-  // HearingWithoutNoticeRadioBtnYes: any;
-  // AwareOfProceedingsNoRadioBtn: any;
-  // CancelButton: any;
-  // PreviousButton: any;
-  // ReducedNoticeYesRadioBtn: any;
-  // GiveReasonOption1: any;
+  
   
   
 
 
-  //     this.timeFrameLabels = [
-  //       "Within 18 days",
-  //       "Within 12 days",
-  //       "Within 7 days",
-  //       "Within 2 days",
-  //       "Same day",
-  //       "Other",
-  //     ];
-  //     this.typeOfHearingLabels = [
-  //       "Standard case management",
-  //       "Urgent preliminary case",
-  //       "Contested interim care order",
-  //       "Accelerated discharge of care",
-  //       "Other",
-  //     ];
-//       this.doYouNeedAWithoutNoticeHearing = ["Yes", "No"];
-//       this.doYouNeedAHearingWithReducedNotice = ["Yes", "No"];
-//       this.areRespondentsAwareOfProceedings = ["Yes", "No"];
-  
-//       this.hearingNeeded = {};
-  
-//       [
-//         ...this.timeFrameLabels,
-//         ...this.typeOfHearingLabels,
-//         ...this.doYouNeedAWithoutNoticeHearing,
-//         ...this.doYouNeedAHearingWithReducedNotice,
-//         ...this.areRespondentsAwareOfProceedings,
-//       ].forEach((label) => {
-//         this.hearingNeeded[label] = page.getByLabel(label);
-//       });
-  
-//       this.continueButton = page.getByRole("button", { name: "Continue" });
-//       this.checkYourAnswers = page.getByRole("heading", {
-//         name: "Check your answers",
-//       });
-//       this.saveAndContinueButton = page.getByRole("button", {
-//         name: "Save and continue",
-//       });
-  
-//       this.giveReasonTextBox = page.getByRole("textbox", {
-//         name: "*Give reason (Optional)",
-//       });
-  
-//       // Adding group locators
-//       this.doYouNeedAWithoutNoticeGroup = page.getByRole("group", {
-//         name: "Do you need a without notice hearing",
-//       });
-//       this.doYouNeedAHearingWithReducedNoiseGroup = page.getByRole("group", {
-//         name: "Do you need a hearing with reduced notice",
-//       });
-//       this.areRespondentsAwareOfProceedingsGroup = page.getByRole("group", {
-//         name: "Are respondents aware of proceedings",
-//       });
-//     }
-  
-//     async handleHearingOption(group: Locator, option: string) {
-//       const locator = await group.getByLabel(option);
-//       await expect(locator).toBeVisible();
-//       await locator.click();
-//     }
-  
-//     async WhenDoYouNeedHearingRadio(timeFrame: string) {
-//       const locator = this.hearingNeeded[timeFrame];
-//       await expect(locator).toBeVisible();
-//       await locator.click();
-//     }
-  
-//     async WhatTypeOfHearingDoYouNeed(typeOfHearing: string) {
-//       const locator = this.hearingNeeded[typeOfHearing];
-//       await expect(locator).toBeVisible();
-//       await locator.click();
-//     }
-  
-//     async WithoutNoticeHearing(doYouNeedAWithoutNoticeHearing: string) {
-//       await this.handleHearingOption(
-//         this.doYouNeedAWithoutNoticeGroup,
-//         doYouNeedAWithoutNoticeHearing,
-//       );
-//     }
-  
-//     async NeedAHearingWithReducedNoise(
-//       doYouNeedAHearingWithReducedNoise: string,
-//     ) {
-//       await this.handleHearingOption(
-//         this.doYouNeedAHearingWithReducedNoiseGroup,
-//         doYouNeedAHearingWithReducedNoise,
-//       );
-//     }
-  
-//     async RespondentsAwareOfProceedings(
-//       areRespondentsAwareOfProceedings: string,
-//     ) {
-//       await this.handleHearingOption(
-//         this.areRespondentsAwareOfProceedingsGroup,
-//         areRespondentsAwareOfProceedings,
-//       );
-//     }
-  
-    // async GiveReasonTextBox() {       await this.GiveReasonTextBox.isVisible();
-    // await this.GiveReasonTextBox.fill("Eum laudantium tempor, yet magni beatae. Architecto tempor. Quae adipisci, and labore, but voluptate, but est voluptas. Ipsum error minima. Suscipit eiusmod excepteur veniam. Consequat aliqua ex. Nostrud elit nostrum fugiat, yet esse nihil. Natus anim perspiciatis, and illum, so magni. Consequuntur eiusmod, so error. Anim magna. Dolores nequeporro, yet tempora. Amet rem aliquid.");
-    // this.HearingUrgencyHeading = page.locator("h1.govuk-heading-l");
-
-    
-/////////==============================//////////////////
+ 
 
 
-// async assertPageURL(url: any){
-//   await this.page.waitForURL(url)
-//   expect(this.page).toHaveURL(url) 
-// }
-
-// async assertPageTitle(title: any){
-//   expect(this.page).toHaveTitle(title)
-// }
 
 
-// async assertHearingUrgencyText(){
-//   await this.HearingUrgencyHeading.waitFor();
-//   expect(this.HearingUrgencyHeading.isVisible).toBeTruthy();
-// }
+  
 
-//   async assertHearingNeededText(){
-//   //await this.hearingNeeded.waitFor();
-//   expect(this.hearingNeeded.isVisible).toBeTruthy();
-// }
 
-// async clickOnWithin18Days(){
-//   await this.Withing18daysRadioBtn.waitFor();
-//   await this.Withing18daysRadioBtn.click();
-// }
-// async ClickOnStandardCaseManagementHearing(){
- // await this.ClickOnStandardCaseManagementHearing.waitFor();
-  //await this.ClickOnStandardCaseManagementHearing.click();
 
-// checkYourAnswers: any;
-  // WithoutNoticeHearing(arg0: string) {
-  //   throw new Error("Method not implemented.");
-  // }
-  // static WhenDoYouNeedHearingRadio(arg0: string) {
-  //   throw new Error("Method not implemented.");
-  // }
-  // RespondentsAwareOfProceedings(arg0: string) {
-  //   throw new Error("Method not implemented.");
-  // }
-  // NeedAHearingWithReducedNoise(arg0: string) {
-  //   throw new Error("Method not implemented.");
-  // }
-  // GiveReasonTextBox() {
-  //   throw new Error("Method not implemented.");
-  // }
-  // WhatTypeOfHearingDoYouNeed(arg0: string) {
-  //   throw new Error("Method not implemented.");
-  // }
 
+ 
