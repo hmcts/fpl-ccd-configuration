7b0fa80bd86eaf188a998958b9a1878c31531ea9
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2024-02-10 14:54:26 +0000

    Update create-case.spec.ts

--- playwright-e2e/tests/create-case.spec.ts
+++ playwright-e2e/tests/create-case.spec.ts
@@ -4,7 +4,6 @@ import { newSwanseaLocalAuthorityUserOne } from "../settings/userCredentials";
 import { CreateCase } from "../pages/create-case";
 import { StartApplication } from "../pages/start-application";
 import { OrdersAndDirectionSought } from "../pages/orders-and-directions";
-import {FactorsAffectingParenting} from "../pages/factors-affecting-parenting";
 
 // WIP
 test.skip("Create a case", async ({ page }) => {
@@ -13,7 +12,6 @@ test.skip("Create a case", async ({ page }) => {
   const createCase = new CreateCase(page);
   const startApplication = new StartApplication(page);
   const ordersAndDirectionSought = new OrdersAndDirectionSought(page);
-  const factorsAffectingParenting = new FactorsAffectingParenting(page);
 
   await signInPage.visit();
   await signInPage.login(
@@ -45,9 +43,6 @@ test.skip("Create a case", async ({ page }) => {
   // 8. Risk and harm to children
 
   // 9. Factors affecting parenting
-  await startApplication.factorsAffectingParentingLink.isVisible();
-  await startApplication.factorsAffectingParentingLink.click();
-  await factorsAffectingParenting.factorsAffectingParentingHeading.isVisible();
 
   // Add application documents
   // 10. Upload documents
