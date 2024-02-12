3c7debcbf70ad32d73bf10a8ffb5feacec53b355
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2024-02-12 14:57:45 +0000

    Dfpl 2139 - Enhancing smoke test to add factors affecting parenting (#5137)
    * DFPL-2139
    
    * create nightly-dev branch
    
    * testing
    
    * testing
    
    * DFPL-2139 (#5119)
    
    * fix
    
    * Update Jenkinsfile_nightly
    
    * Update Jenkinsfile_nightly
    
    * Update Jenkinsfile_nightly
    
    * Update Jenkinsfile_nightly
    
    * Update package.json
    
    * Update orders-and-directions.ts
    
    * Update orders-and-directions.ts
    
    * fix
    
    * fix
    
    * linting and removed unused test
    
    ---------
    
    Co-authored-by: Georgina <“georgina.hall1@hmcts.net”>

--- playwright-e2e/fixtures/create-fixture.ts
+++ playwright-e2e/fixtures/create-fixture.ts
@@ -6,6 +6,7 @@ import { OrdersAndDirectionSought } from "../pages/orders-and-directions";
 import { HearingUrgency } from "../pages/hearing-urgency";
 import { GroundsForTheApplication } from "../pages/grounds-for-the-application";
 import { RiskAndHarmToChildren } from "../pages/risk-and-harm-to-children";
+import { FactorsAffectingParenting } from "../pages/factors-affecting-parenting";
 import { AllocationProposal } from "../pages/allocation-proposal";
 
 type CreateFixtures = {
@@ -16,6 +17,7 @@ type CreateFixtures = {
   hearingUrgency : HearingUrgency;
   groundsForTheApplication : GroundsForTheApplication;
   riskAndHarmToChildren : RiskAndHarmToChildren;
+  factorsAffectingParenting: FactorsAffectingParenting;
   allocationProposal : AllocationProposal;
 };
 
@@ -47,6 +49,11 @@ export const test = base.extend<CreateFixtures>({
   riskAndHarmToChildren: async ({ page }, use) => {
     await use(new RiskAndHarmToChildren(page));
   },
+
+  factorsAffectingParenting: async ({ page }, use) => {
+    await use(new FactorsAffectingParenting(page));
+  },
+
   allocationProposal: async ({ page }, use) => {
     await use(new AllocationProposal(page));
   },
