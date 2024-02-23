537c04dc3e8228efdf87040f3c77efed60e235ae
Ashley Wong <ashley.wong@justice.gov.uk>
2024-02-23 12:07:12 +0000

    DFPL-2101 Judge flags CMO as urgent when approving draft orders

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/ReviewDecision.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/ReviewDecision.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.fpl.enums.CMOReviewOutcome;
+import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
 
 @Data
@@ -14,6 +15,7 @@ public class ReviewDecision {
     private final String hearing;
     private final CMOReviewOutcome decision;
     private final String changesRequestedByJudge;
+    private final YesNo urgency;
 
     @JsonIgnore
     public boolean hasReviewOutcomeOf(CMOReviewOutcome reviewOutcome) {
