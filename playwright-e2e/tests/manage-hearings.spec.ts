6b6288a9cf266e3ec305de9858bc0779aefb431c
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-03-04 11:52:05 +0000

    Fix to be !waEnabled.

--- playwright-e2e/tests/manage-hearings.spec.ts
+++ playwright-e2e/tests/manage-hearings.spec.ts
@@ -60,7 +60,7 @@ test.describe('manage hearings', () => {
   test('CTSC admin updates future hearing judge',
     async({page,signInPage,gateKeepingListing,
             manageHearings, caseDetails}) => {
-      test.skip(testConfig.waEnabled, 'This test should only run when work allocation has been enabled');
+      test.skip(!testConfig.waEnabled, 'This test should only run when work allocation has been enabled');
       caseName = 'CTSC admin edits future hearing judge ' + dateTime.slice(0, 10);
       await apiDataSetup.updateCase(caseName, caseNumber, preJudgeAllocationCaseData);
       await signInPage.visit();
