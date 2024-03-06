9ed1dcb5475f0db29f551b78f59cd2d85fece7fc
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-03-06 13:49:23 +0000

    add new test UploadAdditionalApplicationsAboutToSubmitControllerTest

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadAdditionalApplicationsAboutToSubmitControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadAdditionalApplicationsAboutToSubmitControllerTest.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.enums.AdditionalApplicationType;
 import uk.gov.hmcts.reform.fpl.enums.OtherApplicationType;
 import uk.gov.hmcts.reform.fpl.enums.SupplementType;
+import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.PBAPayment;
 import uk.gov.hmcts.reform.fpl.model.Representative;
@@ -31,10 +32,12 @@ import uk.gov.hmcts.reform.fpl.model.order.HearingOrder;
 import uk.gov.hmcts.reform.fpl.model.order.HearingOrdersBundle;
 import uk.gov.hmcts.reform.fpl.model.order.selector.Selector;
 import uk.gov.hmcts.reform.fpl.request.RequestData;
+import uk.gov.hmcts.reform.fpl.service.UserService;
 import uk.gov.hmcts.reform.fpl.service.docmosis.DocumentConversionService;
 import uk.gov.hmcts.reform.fpl.service.time.Time;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -78,6 +81,9 @@ class UploadAdditionalApplicationsAboutToSubmitControllerTest extends AbstractCa
     @MockBean
     private RequestData requestData;
 
+    @MockBean
+    private UserService userService;
+
     @Autowired
     private Time time;
 
@@ -91,6 +97,7 @@ class UploadAdditionalApplicationsAboutToSubmitControllerTest extends AbstractCa
         given(requestData.userRoles()).willReturn(Set.of(ADMIN_ROLE));
         given(idamClient.getUserDetails(eq(USER_AUTH_TOKEN))).willReturn(createUserDetailsWithHmctsRole());
         given(documentConversionService.convertToPdf(UPLOADED_DOCUMENT)).willReturn(PDF_DOCUMENT);
+        given(userService.isHmctsUser()).willReturn(true);
     }
 
     @Test
@@ -308,6 +315,32 @@ class UploadAdditionalApplicationsAboutToSubmitControllerTest extends AbstractCa
         CaseData updatedCaseData = extractCaseData(postAboutToSubmitEvent(caseData, ADMIN_ROLE));
 
         assertThat(updatedCaseData.getHearingOrdersBundlesDrafts()).isEqualTo(hearingOrdersBundlesDrafts);
+    }
+
+    @Test
+    void shouldUpdateDraftBundleIfConfidentialDraftOrderUploaded() {
+        C2DocumentBundle firstBundleAdded = C2DocumentBundle.builder()
+            .type(WITHOUT_NOTICE)
+            .uploadedDateTime("14 December 2020, 4:24pm")
+            .document(DocumentReference.builder().filename("Document 1").build())
+            .build();
+
+        List<Element<HearingOrdersBundle>> hearingOrdersBundlesDrafts = new ArrayList<>();
+
+        CaseData caseData = CaseData.builder()
+            .id(1L)
+            .isC2Confidential(YesNo.YES)
+            .c2DocumentBundle(wrapElements(firstBundleAdded))
+            .applicantsList(createApplicantsDynamicList(APPLICANT))
+            .temporaryC2Document(createTemporaryC2Document())
+            .hearingOrdersBundlesDrafts(hearingOrdersBundlesDrafts)
+            .build();
+
+        CaseData updatedCaseData = extractCaseData(postAboutToSubmitEvent(caseData, ADMIN_ROLE));
+
+        HearingOrdersBundle actualBundle = updatedCaseData.getHearingOrdersBundlesDrafts().get(0).getValue();
+        assertThat(actualBundle.getOrdersCTSC().get(0).getValue().getOrderConfidential())
+            .isEqualTo(caseData.getTemporaryC2Document().getDraftOrdersBundle().get(0).getValue().getDocument());
     }
 
     private void assertC2DocumentBundle(C2DocumentBundle uploadedC2DocumentBundle) {
