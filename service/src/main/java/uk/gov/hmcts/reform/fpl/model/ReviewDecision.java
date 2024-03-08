ed3a536c6fcb625c5adef8f6131f3cd7acb457cc
Ashley Wong <ashley.wong@justice.gov.uk>
2024-03-08 17:00:12 +0000

    checkstyle

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/ReviewDecision.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/ReviewDecision.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.fpl.model;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
 import uk.gov.hmcts.reform.fpl.enums.CMOReviewOutcome;
 import uk.gov.hmcts.reform.fpl.model.common.DocumentReference;
+
+import java.util.List;
 
 @Data
 @Builder(toBuilder = true)
