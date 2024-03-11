e71f9a6a4ba612c21f7d4f85ec0244e257433f3d
Georgina <georgina.hall1@hmcts.net>
2024-03-11 12:12:55 +0000

    rm duplicate function in base-page

--- playwright-e2e/pages/base-page.ts
+++ playwright-e2e/pages/base-page.ts
@@ -32,10 +32,6 @@ export class BasePage {
   async clickContinue() {
     await this.continueButton.click();
   }
-  
-  async clickContinue(){
-    await this.continue.click();
-  }
 
   async signOut() {
     await this.page.getByText('Sign out').click();
