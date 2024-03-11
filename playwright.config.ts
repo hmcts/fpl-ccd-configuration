c05744903b20e9671a6dfb15f289b6a033ffd6b6
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-03-11 18:02:13 +0000

    UpdatedPWconfigwithReportPath

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'html' : 'list',
+  reporter: [[process.env.CI ? 'html' : 'list'],
+             ['html', { outputFolder: '../test-results/functionalTest' }]],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // Capture screenshot after each test failure.
