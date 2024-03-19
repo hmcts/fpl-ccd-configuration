fab0dc0e53aa1122e63ab369d2a85a323cd118c2
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-03-19 18:28:32 +0000

    deprecated isRestrictedFromPrimaryApplicantEmails

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/FeatureToggleService.java
@@ -31,6 +31,7 @@ public class FeatureToggleService {
             createLDUser(Map.of(LOCAL_AUTHORITY_NAME_KEY, LDValue.of(localAuthorityCode))), false);
     }
 
+    @Deprecated(since = "DFPL-2208")
     public boolean isRestrictedFromPrimaryApplicantEmails(String caseId) {
         return ldClient.boolVariation("restrict-primary-applicant-emails",
             createLDUser(Map.of("caseId", LDValue.of(caseId))), false);
