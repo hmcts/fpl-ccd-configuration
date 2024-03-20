dd0606e869124a18ce5992977517b96133cce070
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-03-20 14:40:03 +0000

    updatebasewithmaster

--- playwright-e2e/pages/base-page.ts
+++ playwright-e2e/pages/base-page.ts
@@ -69,12 +69,4 @@ export class BasePage {
   }
 }
 
-  async waitForAllUploadsToBeCompleted() {
-    let locs = await this.page.getByText('Cancel upload').all();
-    for (let i = 0; i < locs.length; i++) {
-      await expect(locs[i]).toBeDisabled();
-    }
-  }
-
-}
 
