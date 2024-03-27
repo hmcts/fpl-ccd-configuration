20d3ee3be76253cd171f5d5f4b0ec5c8faa034bd
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2024-03-27 15:21:02 +0000

    Update user-credentials.ts

--- playwright-e2e/settings/user-credentials.ts
+++ playwright-e2e/settings/user-credentials.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 
-const e2ePw = process.env.E2E_TEST_PASSWORD || 'Passowrd1234';
-const defaultPwd = process.env.SYSTEM_UPDATE_USER_PASSWORD || 'Password12';
+const e2ePw = process.env.E2E_TEST_PASSWORD || '';
+const defaultPwd = process.env.SYSTEM_UPDATE_USER_PASSWORD || '';
 const judgePwd = process.env.E2E_TEST_JUDGE_PASSWORD || '';
 
 
