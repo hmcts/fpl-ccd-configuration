2266e9ce9c8bc8c1bb51455d12c055bcd6510ee8
Charles Braimah <charles.braimah@justice.gov.uk>
2024-04-09 15:08:58 +0100

    DFPL-2239

---
+++ playwright-e2e/pages/international-element.ts
@@ -0,0 +1,37 @@
+import { type Page, type Locator, expect } from "@playwright/test";
+import { BasePage } from "./base-page";
+
+export class InternationalElement extends BasePage {
+  readonly internationalElementHeading: Locator;
+  readonly areThereAnySuitableCarers: Locator;
+  readonly anySignificantEventsOutsideUk: Locator;
+  readonly anyIssueWithJurisdictionOfThisCase: Locator;
+  readonly awareOfAnyProceedingsOutsideTheUk: Locator;
+  readonly aGovtOrCentralAuthorityOutsideUkInvolvedInCase: Locator;
+  readonly continueButton: Locator;
+  readonly saveAndContinueButton: Locator;
+  readonly checkYourAnswers: Locator;
+
+  public constructor(page: Page) {
+    super(page);
+    this.internationalElementHeading = page.getByRole('heading', { name: 'International element' });
+    this.areThereAnySuitableCarers = page.getByRole('button', { name: 'No' });
+    this.anySignificantEventsOutsideUk = page.getByRole('button', { name: 'No' });
+    this.awareOfAnyProceedingsOutsideTheUk = page.getByRole('button', { name: 'No' });
+    this.aGovtOrCentralAuthorityOutsideUkInvolvedInCase = page.getByRole('button', { name: 'No' });
+    this.awareOfAnyProceedingsOutsideTheUk = page.getByRole('button', { name: 'No' });
+    this.continueButton = page.getByRole('button', { name: 'Continue' });
+    this.checkYourAnswers = page.getByRole('heading', { name: 'Check your answers' });
+    this.saveAndContinueButton = page.getByRole('button', { name: 'Save and continue' });
+  }
+    async internationalElementSmokeTest(){
+      await this.page.getByRole('group', { name: 'Are there any suitable carers' }).getByLabel('No').check();
+      await this.page.getByRole('group', { name: 'Are you aware of any significant events that have happened outside the UK? (' }).getByLabel('No').check();
+      await this.page.getByRole('group', { name: 'Are you aware of any issues' }).getByLabel('No').check();
+      await this.page.getByRole('group', { name: 'Are you aware of any proceedings outside the UK? (Optional)' }).getByLabel('No').check();
+      await this.page.getByRole('group', { name: 'Has, or should, a government' }).getByLabel('No').check();
+      await this.page.getByRole('button', { name: 'Continue' }).click();
+      await expect(this.checkYourAnswersHeader).toBeVisible();
+      await this.page.getByRole('button', { name: 'Save and continue' }).click();
+    }
+  }
