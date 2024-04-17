9ef9844c67bbcb48a2f3f5ae757157d4631fe017
GauravChawlaMOJ <gaurav.chawla@justice.gov.uk>
2024-04-17 22:09:00 +0100

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], headless: false},
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
