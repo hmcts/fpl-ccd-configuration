349d4daf8de435e6e979842ad9163bc043ea7458
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-04-25 11:49:43 +0100

    formatCode

--- playwright-e2e/pages/legal-counsel.ts
+++ playwright-e2e/pages/legal-counsel.ts
@@ -1,44 +1,48 @@
-import { type Page, type Locator } from "@playwright/test";
-import { BasePage } from "./base-page";
+import {type Page, type Locator} from "@playwright/test";
+import {BasePage} from "./base-page";
 
-export class LegalCounsel extends BasePage
-{
-readonly addCounsel: Locator;
-readonly firstName: Locator;
-readonly lastName: Locator;
-readonly email: Locator;
-readonly organisation: Locator;
-readonly selectOrg: Locator;
-readonly removeCounsel:Locator;
-readonly removeCounselAlert: Locator;
-readonly isRepresented: Locator;
-    constructor(page:Page){
+export class LegalCounsel extends BasePage {
+    readonly addCounsel: Locator;
+    readonly firstName: Locator;
+    readonly lastName: Locator;
+    readonly email: Locator;
+    readonly organisation: Locator;
+    readonly selectOrg: Locator;
+    readonly removeCounsel: Locator;
+    readonly removeCounselAlert: Locator;
+    readonly isRepresented: Locator;
+
+    constructor(page: Page) {
         super(page);
-        this.addCounsel = page.getByRole('button', { name: 'Add new' });
-        this.firstName =  page.getByLabel('First name');
+        this.addCounsel = page.getByRole('button', {name: 'Add new'});
+        this.firstName = page.getByLabel('First name');
         this.lastName = page.getByLabel('Last name');
         this.email = page.getByLabel('Email address');
         this.organisation = page.getByLabel('You can only search for');
-        this.selectOrg = page.getByRole('link', { name: 'Select' });
+        this.selectOrg = page.getByRole('link', {name: 'Select'});
         this.removeCounsel = page.getByLabel('Remove Counsel');
-        this.removeCounselAlert = page.getByRole('button', { name: 'Remove' });
-        this.isRepresented =page.getByRole('group', { name: 'Do they have legal' }); 
+        this.removeCounselAlert = page.getByRole('button', {name: 'Remove'});
+        this.isRepresented = page.getByRole('group', {name: 'Do they have legal'});
     }
-    async toAddLegalCounsel(){
+
+    async toAddLegalCounsel() {
         await this.addCounsel.click();
     }
-    async toRemoveLegalCounsel(){
-       await  this.removeCounsel.click();
-       await this.removeCounselAlert.click();
+
+    async toRemoveLegalCounsel() {
+        await this.removeCounsel.click();
+        await this.removeCounselAlert.click();
     }
-    async enterLegalCounselDetails(){
+
+    async enterLegalCounselDetails() {
         await this.firstName.fill('FPLOrg');
         await this.lastName.fill('Solicitor');
         await this.email.fill('solicitoroneorg2@mailinator.com');
         await this.organisation.fill('FPLSolicitorOrg');
         await this.selectOrg.click();
     }
-    async removeRepresentative(){
+
+    async removeRepresentative() {
         await this.isRepresented.getByLabel('No').check();
     }
 
