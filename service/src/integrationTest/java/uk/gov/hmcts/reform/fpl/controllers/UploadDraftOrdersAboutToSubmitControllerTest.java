e8f5726d12735ea6031b2772137f184b8f70589c
Ashley Wong <ashley.wong@justice.gov.uk>
2024-05-01 14:55:48 +0100

    checkstyle

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadDraftOrdersAboutToSubmitControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/UploadDraftOrdersAboutToSubmitControllerTest.java
@@ -37,8 +37,6 @@ import java.util.UUID;
 import static com.google.common.collect.Lists.newArrayList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.enums.CMOStatus.DRAFT;
 import static uk.gov.hmcts.reform.fpl.enums.CMOStatus.SEND_TO_JUDGE;
