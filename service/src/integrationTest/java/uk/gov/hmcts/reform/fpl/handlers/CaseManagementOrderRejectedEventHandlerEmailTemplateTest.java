b98f46881b4ad99f1d2eea8a68818de61eb812e0
Ashley Wong <ashley.wong@justice.gov.uk>
2024-05-08 10:55:52 +0100

    CMO template unit tests

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/handlers/CaseManagementOrderRejectedEventHandlerEmailTemplateTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/handlers/CaseManagementOrderRejectedEventHandlerEmailTemplateTest.java
@@ -1,8 +1,14 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.context.ContextConfiguration;
+import uk.gov.hmcts.reform.fpl.enums.CaseRole;
 import uk.gov.hmcts.reform.fpl.enums.TabUrlAnchor;
 import uk.gov.hmcts.reform.fpl.events.cmo.CaseManagementOrderRejectedEvent;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
@@ -12,19 +18,28 @@ import uk.gov.hmcts.reform.fpl.model.Respondent;
 import uk.gov.hmcts.reform.fpl.model.RespondentParty;
 import uk.gov.hmcts.reform.fpl.model.order.HearingOrder;
 import uk.gov.hmcts.reform.fpl.service.CaseUrlService;
+import uk.gov.hmcts.reform.fpl.service.FurtherEvidenceNotificationService;
 import uk.gov.hmcts.reform.fpl.service.email.content.CaseManagementOrderEmailContentProvider;
 import uk.gov.hmcts.reform.fpl.testingsupport.email.EmailTemplateTest;
 import uk.gov.hmcts.reform.fpl.utils.EmailNotificationHelper;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.fpl.testingsupport.email.EmailContent.emailContent;
 import static uk.gov.hmcts.reform.fpl.testingsupport.email.SendEmailResponseAssert.assertThat;
 import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @ContextConfiguration(classes = {
     CaseManagementOrderRejectedEventHandler.class, CaseManagementOrderEmailContentProvider.class,
-    EmailNotificationHelper.class, CaseUrlService.class
+    EmailNotificationHelper.class, CaseUrlService.class, FurtherEvidenceNotificationService.class
+})
+@MockBeans(value = {
+    @MockBean(FurtherEvidenceNotificationService.class)
 })
 class CaseManagementOrderRejectedEventHandlerEmailTemplateTest extends EmailTemplateTest {
     private static final String CHILD_LAST_NAME = "slaanesh";
@@ -32,6 +47,14 @@ class CaseManagementOrderRejectedEventHandlerEmailTemplateTest extends EmailTemp
 
     @Autowired
     private CaseManagementOrderRejectedEventHandler underTest;
+    @Autowired
+    private FurtherEvidenceNotificationService furtherEvidenceNotificationService;
+
+    @BeforeEach
+    void setUp() {
+        when(furtherEvidenceNotificationService.getDesignatedLocalAuthorityRecipientsOnly(any()))
+            .thenReturn(Set.of("abc@gmail.com"));
+    }
 
     @Test
     void sendNotifications() {
@@ -78,6 +101,119 @@ class CaseManagementOrderRejectedEventHandlerEmailTemplateTest extends EmailTemp
                 .line()
                 .end("Do not reply to this email. If you need to contact us, call 0330 808 4424 or email "
                       + "contactfpl@justice.gov.uk")
+            );
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = CaseRole.class, names = {
+        "CHILDSOLICITORA", "CHILDSOLICITORB", "CHILDSOLICITORC", "CHILDSOLICITORD", "CHILDSOLICITORE",
+        "CHILDSOLICITORF" ,"CHILDSOLICITORG", "CHILDSOLICITORH", "CHILDSOLICITORI", "CHILDSOLICITORJ",
+        "CHILDSOLICITORK" ,"CHILDSOLICITORL", "CHILDSOLICITORM", "CHILDSOLICITORN", "CHILDSOLICITORO"
+    })
+    void sendNotificationsIfUploadedByChildSolicitor(CaseRole childCaseRole) {
+        when(furtherEvidenceNotificationService.getChildSolicitorEmails(any(), eq(childCaseRole)))
+            .thenReturn(Set.of("def@gmail.com"));
+
+        String familyManCaseNumber = "fam_num";
+        long caseId = 12345L;
+        CaseData caseData = CaseData.builder()
+            .id(caseId)
+            .familyManCaseNumber(familyManCaseNumber)
+            .respondents1(wrapElements(Respondent.builder()
+                .party(RespondentParty.builder().lastName(RESPONDENT_LAST_NAME).build())
+                .build()))
+            .children1(wrapElements(Child.builder()
+                .party(ChildParty.builder().dateOfBirth(LocalDate.now()).lastName(CHILD_LAST_NAME).build())
+                .build()))
+            .build();
+
+        String hearing = "some hearing here";
+        String changes = "something must be changed";
+        HearingOrder cmo = HearingOrder.builder()
+            .requestedChanges(changes)
+            .hearing(hearing)
+            .uploaderCaseRoles(List.of(childCaseRole))
+            .build();
+
+        underTest.sendNotifications(new CaseManagementOrderRejectedEvent(caseData, cmo));
+
+        assertThat(response())
+            .hasSubject("Changes needed on CMO, " + CHILD_LAST_NAME)
+            .hasBody(emailContent()
+                .line("The judge has requested changes to the CMO for:")
+                .line()
+                .callout(String.format("%s, %s, %s", RESPONDENT_LAST_NAME, familyManCaseNumber, hearing))
+                .line()
+                .line("Within 2 working days, you must make these changes:")
+                .line()
+                .line(changes)
+                .line()
+                .line("Sign in to upload a revised CMO: " + caseDetailsUrl(caseId, TabUrlAnchor.ORDERS))
+                .line()
+                .line("HM Courts & Tribunals Service")
+                .line()
+                .line("For local authority guidance navigate to this link:")
+                .line("https://www.gov.uk/government/publications/"
+                    + "myhmcts-how-to-apply-for-a-family-public-law-order")
+                .line()
+                .end("Do not reply to this email. If you need to contact us, call 0330 808 4424 or email "
+                    + "contactfpl@justice.gov.uk")
+            );
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = CaseRole.class, names = {
+        "SOLICITORA", "SOLICITORB", "SOLICITORC", "SOLICITORD", "SOLICITORE", "SOLICITORF" ,"SOLICITORG", "SOLICITORH",
+        "SOLICITORI", "SOLICITORJ"
+    })
+    void sendNotificationsIfUploadedByRespondentSolicitor(CaseRole respCaseRole) {
+        when(furtherEvidenceNotificationService.getRespondentSolicitorEmails(any(), eq(respCaseRole)))
+            .thenReturn(Set.of("def@gmail.com"));
+
+        String familyManCaseNumber = "fam_num";
+        long caseId = 12345L;
+        CaseData caseData = CaseData.builder()
+            .id(caseId)
+            .familyManCaseNumber(familyManCaseNumber)
+            .respondents1(wrapElements(Respondent.builder()
+                .party(RespondentParty.builder().lastName(RESPONDENT_LAST_NAME).build())
+                .build()))
+            .children1(wrapElements(Child.builder()
+                .party(ChildParty.builder().dateOfBirth(LocalDate.now()).lastName(CHILD_LAST_NAME).build())
+                .build()))
+            .build();
+
+        String hearing = "some hearing here";
+        String changes = "something must be changed";
+        HearingOrder cmo = HearingOrder.builder()
+            .requestedChanges(changes)
+            .hearing(hearing)
+            .uploaderCaseRoles(List.of(respCaseRole))
+            .build();
+
+        underTest.sendNotifications(new CaseManagementOrderRejectedEvent(caseData, cmo));
+
+        assertThat(response())
+            .hasSubject("Changes needed on CMO, " + CHILD_LAST_NAME)
+            .hasBody(emailContent()
+                .line("The judge has requested changes to the CMO for:")
+                .line()
+                .callout(String.format("%s, %s, %s", RESPONDENT_LAST_NAME, familyManCaseNumber, hearing))
+                .line()
+                .line("Within 2 working days, you must make these changes:")
+                .line()
+                .line(changes)
+                .line()
+                .line("Sign in to upload a revised CMO: " + caseDetailsUrl(caseId, TabUrlAnchor.ORDERS))
+                .line()
+                .line("HM Courts & Tribunals Service")
+                .line()
+                .line("For local authority guidance navigate to this link:")
+                .line("https://www.gov.uk/government/publications/"
+                    + "myhmcts-how-to-apply-for-a-family-public-law-order")
+                .line()
+                .end("Do not reply to this email. If you need to contact us, call 0330 808 4424 or email "
+                    + "contactfpl@justice.gov.uk")
             );
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/NotifyTemplates.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/NotifyTemplates.java
@@ -31,9 +31,9 @@ public final class NotifyTemplates {
     public static final String CMO_READY_FOR_JUDGE_REVIEW_NOTIFICATION_TEMPLATE =
         "90c5f4b3-738b-4c8d-b433-c8c90095f815";
     public static final String CMO_REJECTED_BY_JUDGE_DESIGNATED_LA = "acb99554-4806-41f9-a95b-df7ee9b9b878";
-    public static final String CMO_REJECTED_BY_JUDGE_2ND_LA = "bcb99554-4806-41f9-a95b-df7ee9b9b878";
-    public static final String CMO_REJECTED_BY_JUDGE_CHILD_SOL = "ccb99554-4806-41f9-a95b-df7ee9b9b878";
-    public static final String CMO_REJECTED_BY_JUDGE_RESP_SOL = "dcb99554-4806-41f9-a95b-df7ee9b9b878";
+    public static final String CMO_REJECTED_BY_JUDGE_2ND_LA = "b8c12baf-9e6b-43a4-8fab-6d4f74b58fb1";
+    public static final String CMO_REJECTED_BY_JUDGE_CHILD_SOL = "ccc06bc8-beed-4e9e-8510-d1b12503e600";
+    public static final String CMO_REJECTED_BY_JUDGE_RESP_SOL = "d9bc62a8-17b7-40e5-9b79-c573f8a9b6b2";
     public static final String PARTY_ADDED_TO_CASE_BY_EMAIL_NOTIFICATION_TEMPLATE
         = "38a4dba9-ab05-4a40-b72b-f26b05ea6e7b";
     public static final String PARTY_ADDED_TO_CASE_THROUGH_DIGITAL_SERVICE_NOTIFICATION_TEMPLATE
