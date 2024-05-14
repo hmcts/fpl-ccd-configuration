768172bbd61787920b911179607dfad3ca344c33
Charles Braimah <charles.braimah@justice.gov.uk>
2024-05-14 10:27:02 +0100

    DFPL-2191

--- playwright-e2e/pages/manage-documents.ts
+++ playwright-e2e/pages/manage-documents.ts
@@ -1,11 +1,46 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { BasePage } from "./base-page";
 import config from "../settings/test-docs/config";
 
 export class ManageDocuments extends BasePage {
+    readonly manageDocumentsTest: Locator;
+    readonly uploadNewDocuments: Locator;
+    readonly documentType: Locator;
+    readonly IsThisDocumentConfidential: Locator;
+    readonly isTranslationNeeded: Locator;
+    readonly InputFiles: Locator;
+    readonly NonconfidentialRadioButton: Locator;
+    readonly ConfidentialRadioButton: Locator;
+    readonly IsDocumentRelatedToCase: Locator;
+    readonly RemoveDocumentsTest: Locator;
+    readonly uploadedDocumentsTest: Locator;
+    readonly thereIsAMistakeOnTheDocument: Locator;
+    readonly caseFileviewTest: Locator;
+    readonly togglePositionStatements: Locator;
+    readonly moreDocumentsOptions: Locator;
+    readonly changeFolder: Locator;
+    readonly threshold: Locator;
 
     constructor(page: Page) {
         super(page);
+        this.manageDocumentsTest = page.getByRole('heading', { name: 'manage documents', exact: true });
+        this.uploadNewDocuments = page.getByRole('group', { name: 'upload new documents' });
+        this.documentType = page.getByLabel('Document type');
+        this.IsThisDocumentConfidential = page.getByRole('group', { name: 'Is this document  confidential?' });
+        this.isTranslationNeeded = page.getByRole('group', { name: 'Is translation needed?' });
+        this.InputFiles = page.getByRole('textbox', { name: 'Upload a document' });
+        this.NonconfidentialRadioButton = page.getByRole('radio', { name: 'No' });
+        this.ConfidentialRadioButton = page.getByRole('radio', { name: 'Yes' });
+        this.IsDocumentRelatedToCase = page.getByRole('checkbox', { name: 'Yes' });
+        this.RemoveDocumentsTest = page.getByLabel('Remove documents');
+        this.uploadedDocumentsTest = page.getByRole('group', { name: '1: hearingDocuments.posStmtList###3ad0ca08-1c4c-48' });
+        this.thereIsAMistakeOnTheDocument = page.getByLabel('There is a mistake on the');
+        this.caseFileviewTest = page.getByText('Case File View');
+        this.togglePositionStatements = page.getByRole('button', { name: 'toggle Position Statements' });
+        this.moreDocumentsOptions = page.getByRole('button', { name: 'More document options', exact: true });
+        this.changeFolder = page.getByText('Change folder');
+        this.threshold = page.getByLabel('Threshold', { exact: true });
+
     }
 
     async uploadDocuments(type: string) {
@@ -24,6 +59,46 @@ export class ManageDocuments extends BasePage {
         await this.page.getByRole('checkbox', { name: 'Yes' }).check();
 
         await this.waitForAllUploadsToBeCompleted();
+        await this.clickContinue();
+        await this.checkYourAnsAndSubmit();
+    }
+    async uploadedDocuments(type: string) {
+        await this.manageDocumentsTest.isVisible();
+        await this.uploadNewDocuments.check();
+        await this.clickContinue();
+        await this.InputFiles.setInputFiles('./playwright-e2e/files/draftOrder.docx');
+        await this.documentType.selectOption(type);
+        await this.NonconfidentialRadioButton.check();
+        await this.IsDocumentRelatedToCase.check();
+        await this.clickContinue();
+        await this.checkYourAnsAndSubmit();
+    }
+    async uploadConfidentialDocuments(type: string) {
+        await this.manageDocumentsTest.isVisible();
+        await this.uploadNewDocuments.check();
+        await this.clickContinue();
+        await this.InputFiles.setInputFiles('./playwright-e2e/files/draftOrder.docx');
+        await this.documentType.selectOption(type);
+        await this.ConfidentialRadioButton.check();
+        await this.IsDocumentRelatedToCase.check();
+        await this.clickContinue();
+        await this.checkYourAnsAndSubmit();
+    }
+    async RemoveDocuments(type: string) {
+        await this.manageDocumentsTest.isVisible();
+        await this.RemoveDocumentsTest.check();
+        await this.clickContinue();
+        await this.uploadedDocumentsTest.check();
+        await this.thereIsAMistakeOnTheDocument.check();
+        await this.clickContinue();
+        await this.checkYourAnsAndSubmit();
+    }
+    async caseFileView(type: string) {
+        await this.caseFileviewTest.check();
+        await this.togglePositionStatements.click();
+        await this.moreDocumentsOptions.click();
+        await this.changeFolder.click();
+        await this.threshold.check();
         await this.clickContinue();
         await this.checkYourAnsAndSubmit();
     }
--- playwright-e2e/tests/manage-documents.spec.ts
+++ playwright-e2e/tests/manage-documents.spec.ts
@@ -1,9 +1,11 @@
 import { test } from '../fixtures/create-fixture';
 import { testConfig } from '../settings/test-config';
-import { CTSCUser, newSwanseaLocalAuthorityUserOne } from '../settings/user-credentials';
+import { CTSCUser, newSwanseaLocalAuthorityUserOne, privateSolicitorOrgUser } from '../settings/user-credentials';
 import { Apihelp } from '../utils/api-helper';
 import caseData from '../caseData/mandatorySubmissionFields.json';
-import { expect } from '@playwright/test';
+import caseWithResSolicitor from '../caseData/caseWithRespondentSolicitor.json';
+import { expect } from '@playwright/test'
+import { POSITION_STATEMENTS } from '../pages/manage-documents-tests';
 
 test.describe('Manage Documents', () => {
     let apiDataSetup = new Apihelp();
@@ -49,5 +51,69 @@ test.describe('Manage Documents', () => {
             await expect(page.getByText('Review Correspondence')).toHaveCount(0);
         }
     });
-
+    test('LA uploads various documents visble in CFV', async ({ signInPage, manageDocuments }) => {
+        casename = 'LA uploads various documents ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseWithResSolicitor);
+        await apiDataSetup.giveAccessToCase(caseNumber, privateSolicitorOrgUser, '[SOLICITORA]');
+        await signInPage.visit();
+        await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await manageDocuments.gotoNextStep('Manage documents');
+        await manageDocuments.uploadDocuments(POSITION_STATEMENTS);
+    });
+    test('LA uploads confidential documents visible in CFV ', async ({ signInPage, manageDocuments, caseFileView }) => {
+        casename = 'LA uploads various documents ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseData);
+        await signInPage.visit();
+        await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await manageDocuments.gotoNextStep('Manage documents');
+        await manageDocuments.uploadConfidentialDocuments(POSITION_STATEMENTS);
+    });
+    test('HMCTS uploads confidential documents visible in CFV ', async ({ signInPage, manageDocuments }) => {
+        casename = 'HMCTS uploads variuos documents ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseWithResSolicitor);
+        await apiDataSetup.giveAccessToCase(caseNumber, privateSolicitorOrgUser, '[SOLICITORA]');
+        await signInPage.visit();
+        await signInPage.login(CTSCUser.email, CTSCUser.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await manageDocuments.gotoNextStep('Manage documents');
+        await manageDocuments.uploadConfidentialDocuments(POSITION_STATEMENTS);
+        await manageDocuments.clickSignOut();
+        await signInPage.login(privateSolicitorOrgUser.email, privateSolicitorOrgUser.password);
+    });
+    test('LA uploads correspodence documents visible in correct folder ', async ({ signInPage, manageDocuments, caseFileView }) => {
+        casename = 'LA uploads correspondence documents visible in correct order ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseData);
+        await signInPage.visit();
+        await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await manageDocuments.gotoNextStep('Manage documents');
+        await manageDocuments.uploadConfidentialDocuments(POSITION_STATEMENTS);
+        await manageDocuments.clickSignOut();
+        await signInPage.login('solicitor1@solicitors.uk', 'Password12');
+        await signInPage.login(CTSCUser.email, CTSCUser.password);
+    });
+    test('LA removes document ', async ({ page, signInPage, manageDocuments }) => {
+        casename = 'LA removes document ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseData);
+        await signInPage.visit();
+        await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await manageDocuments.gotoNextStep('Removes documents');
+        await page.getByLabel('Uploaded Document').selectOption('1: hearingDocuments.posStmtList###3ad0ca08-1c4c-48');
+        await page.getByLabel('There is a mistake on the').check();
+    });
+    test('CTSC user can move document between folder ', async ({ page, signInPage, manageDocuments }) => {
+        casename = 'CTSC moved documents between folder ' + dateTime.slice(0, 10);
+        await apiDataSetup.updateCase(casename, caseNumber, caseData);
+        await signInPage.visit();
+        await signInPage.login(CTSCUser.email, CTSCUser.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await page.getByText('Case File View').click();
+        await page.getByRole('button', { name: 'toggle Position Statements' }).click();
+        await page.getByRole('button', { name: 'More document options', exact: true }).click();
+        await page.getByText('Change folder').click();
+        await page.getByLabel('Threshold', { exact: true }).check();
+    });
 });
