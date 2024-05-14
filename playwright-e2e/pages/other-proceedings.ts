620170fc12978d566ba13e620522fad074dd4433
Braimah101 <41795070+Braimah101@users.noreply.github.com>
2024-05-14 11:41:30 +0100

    Update other-proceedings.ts

--- playwright-e2e/pages/other-proceedings.ts
+++ playwright-e2e/pages/other-proceedings.ts
@@ -4,21 +4,15 @@ export class OtherProceedings {
   readonly page: Page;
   readonly otherProceedingsHeading: Locator;
   readonly areThereAnyPastOrOngoingProccedingsReleventToCase: Locator;
-  readonly continueButton: Locator;
-  readonly saveAndContinueButton: Locator;
 
-  public constructor(page: Page) {
+public constructor(page: Page) {
     this.page = page;
     this.otherProceedingsHeading = page.getByRole('heading', { name: 'Other Proceedings' });
     this.areThereAnyPastOrOngoingProccedingsReleventToCase = page.getByRole('radio',  { name: 'No', exact: true });
-    this.continueButton = page.getByRole('button', { name: 'Continue' });
-    this.saveAndContinueButton = page.getByRole('button', { name: 'Save and continue' });
        }
     
   async otherProceedingsSmokeTest() {
     await this.otherProceedingsHeading.isVisible();
-    await this.areThereAnyPastOrOngoingProccedingsReleventToCase.check();
-    await this.continueButton.click();
-    await this.saveAndContinueButton.click();
+    await this.areThereAnyPastOrOngoingProccedingsReleventToCase.check();  
   }
 }
