5732d357efb7fe50104dda961373cb2c781c0ccf
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-05-15 13:29:54 +0100

    Add docmosis parameter generator for transparency order

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/orders/docmosis/TransparencyOrderDocmosisParameters.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.fpl.service.orders.docmosis;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Value
+@SuperBuilder(toBuilder = true)
+public class TransparencyOrderDocmosisParameters extends DocmosisParameters {
+    String orderByConsent;
+    String orderExpiration;
+    String publishInformationDetails;
+    String publishIdentityDetails;
+    String publishDocumentsDetails;
+}
