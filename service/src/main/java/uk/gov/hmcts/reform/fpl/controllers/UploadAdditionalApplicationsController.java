60d15aea1c157626910ca2ae488b767fc0881d54
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2024-05-15 10:15:45 +0100

    DFPL-1680 C2 application allowed to be confidential (#5287)
    * DFPL-1680 C2 application allowed to be confidential
    
    * update unit test
    
    * update unit test
    
    * update unit test
    
    * update unit test
    
    * fix code style check
    
    * update confidential C2 notification
    
    * fix test
    
    * update la shared inbox
    
    * update la share inbox test
    
    * fix noti
    
    * fix unit test
    
    * remove confidentialC2WarningLabel
    
    * fix code check
    
    * move logic from event listener to service
    
    * fix test
    
    * update code review
    
    * add confidential folder for C2
    
    * remove NA/LA field from ccd-def
    
    * json ignore NC and LA
    
    * fix confidential c2 JsonIgnoreProperties
    
    * update review comment
    
    * fix test
    
    * update CAFCASSSOLICITOR, BARRISTER
    
    * deprecate supportingEvidenceLA and supportingEvidenceNC
    
    * Remove an extra comma
    
    * Revert "deprecate supportingEvidenceLA and supportingEvidenceNC"
    
    This reverts commit b815fd9e22b1c78df8bbdb1bb72d2063a7e8f028.
    
    * update upload confidential C2
    
    * confidential draft order
    
    * fix confidential display
    
    * CFV confidential draft orders
    
    * approve order
    
    * fix draft order CRUD
    
    * refused order
    
    * update label
    
    * update label
    
    * Manage order
    
    * notify admin, la and cafcass
    
    * approve order - notify uploader
    
    * fix notify uploader
    
    * fix existing unit test and code style
    
    * fix check style
    
    * fix test
    
    * fix unit test
    
    * fix existing test
    
    * fix existing test
    
    * add ConfidentialOrderBundleTest
    
    * refactor to constant
    
    * add new unit test to DraftOrderServiceTest
    
    * add new test UploadAdditionalApplicationsAboutToSubmitControllerTest
    
    * add new unit test
    
    * add unit test for DraftOrdersApprovedEventHandlerTest
    
    * add unit test to DraftOrdersReviewDataBuilderTest
    
    * add unit test to CaseDataTest
    
    * add unit test to AmendGeneratedOrderActionTest
    
    * fix flaky ListGatekeepingControllerSubmittedTest
    
    * fix ConfidentialOrderBundleTest
    
    * fix check style
    
    * update show hide
    
    * Update OrdersBundleForApproval.json
    
    * Update OrdersBundleForApproval.json
    
    * Update OrdersBundleForApproval.json
    
    * fix noti
    
    * Update DraftOrdersApprovedEventHandler.java
    
    * Update DraftOrdersApprovedEventHandler.java
    
    * Update DraftOrdersApprovedEventHandler.java
    
    * Update DraftOrdersApprovedEventHandler.java
    
    * fix bug
    
    * fix import
    
    * return if no confidential order
    
    * fix notiification
    
    * fix refused tab
    
    * fix return
    
    * remove parentheses
    
    * remove blank line
    
    * add PolicyHelperTest
    
    * remove unusedMethod
    
    * rename as getAllPolicies
    
    * rename as PolicyData
    
    * change log message
    
    * log message
    
    * remove public
    
    * add superuser
    
    * fix unit test
    
    * remove public
    
    * fix draft Hearing Orders
    
    * Revert "fix draft Hearing Orders"
    
    This reverts commit 7debfc214c19b1263aa9c8f31df65cc1e5d6ce81.
    
    * json ignore null
    
    * Missing permission for orders
    
    * Bug fixing
    
    * Remove comment
    
    * Revert "Remove comment"
    
    This reverts commit fe3ece518dbc62b2e248d4ccb050b7f07eaf526c.
    
    * Revert "Bug fixing"
    
    This reverts commit f72818de849d7b2a7421bee27fca2509a12a1c54.
    
    * NullPointerException fixes
    
    * checkstyle fix
    
    * update Application with notice. wording
    
    * fix C2 test
    
    * fix c2 e2e test
    
    * fix unit test
    
    ---------
    
    Co-authored-by: Ashley Wong <50691452+so99y@users.noreply.github.com>
    Co-authored-by: Ashley Wong <ashley.wong@justice.gov.uk>
    Co-authored-by: Braimah101 <41795070+Braimah101@users.noreply.github.com>

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadAdditionalApplicationsController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/UploadAdditionalApplicationsController.java
@@ -68,6 +68,7 @@ public class UploadAdditionalApplicationsController extends CallbackController {
     private static final String TEMPORARY_C2_DOCUMENT = "temporaryC2Document";
     private static final String TEMPORARY_OTHER_APPLICATIONS_BUNDLE = "temporaryOtherApplicationsBundle";
     private static final String SKIP_PAYMENT_PAGE = "skipPaymentPage";
+    private static final String IS_C2_CONFIDENTIAL = "isC2Confidential";
 
     private final ObjectMapper mapper;
     private final DraftOrderService draftOrderService;
@@ -180,7 +181,12 @@ public class UploadAdditionalApplicationsController extends CallbackController {
 
             HearingOrdersBundles hearingOrdersBundles = draftOrderService.migrateCmoDraftToOrdersBundles(caseData);
 
-            draftOrderService.additionalApplicationUpdateCase(newDrafts, hearingOrdersBundles.getAgreedCmos());
+            if (YES.equals(caseData.getIsC2Confidential())) {
+                draftOrderService.confidentialAdditionalApplicationUpdateCase(caseData, newDrafts,
+                    hearingOrdersBundles.getAgreedCmos());
+            } else {
+                draftOrderService.additionalApplicationUpdateCase(newDrafts, hearingOrdersBundles.getAgreedCmos());
+            }
 
             caseDetails.getData().put("hearingOrdersBundlesDrafts", hearingOrdersBundles.getAgreedCmos());
         }
@@ -204,7 +210,8 @@ public class UploadAdditionalApplicationsController extends CallbackController {
 
         removeTemporaryFields(caseDetails, TEMPORARY_C2_DOCUMENT, "c2Type",
             "additionalApplicationType", AMOUNT_TO_PAY, "temporaryPbaPayment",
-            TEMPORARY_OTHER_APPLICATIONS_BUNDLE, "applicantsList", "otherApplicant", SKIP_PAYMENT_PAGE);
+            TEMPORARY_OTHER_APPLICATIONS_BUNDLE, "applicantsList", "otherApplicant", SKIP_PAYMENT_PAGE,
+            IS_C2_CONFIDENTIAL);
 
         return respond(caseDetails);
     }
@@ -233,6 +240,10 @@ public class UploadAdditionalApplicationsController extends CallbackController {
                         uploadAdditionalApplicationsService.convertC2Bundle(lastBundle.getC2DocumentBundle(),
                             caseDataCurrent)
                     );
+                }
+                if (!isEmpty(lastBundle.getC2DocumentBundleConfidential())) {
+                    uploadAdditionalApplicationsService.convertConfidentialC2Bundle(caseDataCurrent,
+                        lastBundle.getC2DocumentBundleConfidential(), bundleBuilder);
                 }
 
                 // If we have a other application, do conversion if needed
