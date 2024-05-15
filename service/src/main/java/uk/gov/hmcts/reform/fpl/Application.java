e983cdbd251ad090ca6b16185924d3ff76c83454
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-05-15 10:30:35 +0100

    Update Application.java as @ComponentScan no longer required.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -4,24 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "uk.gov.hmcts.reform.fpl")
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.idam.client",
     "uk.gov.hmcts.reform.rd.client",
     "uk.gov.hmcts.reform.fnp.client",
     "uk.gov.hmcts.reform.calendar.client",
     "uk.gov.hmcts.reform.aac.client",
-    "uk.gov.hmcts.reform.am.client"
+    "uk.gov.hmcts.reform.am.client",
+    "uk.gov.hmcts.reform.ccd.client"
 })
-@ComponentScan
 @EnableRetry
 @EnableAsync
 @EnableCaching
-@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, it's not a utility class
 public class Application {
 
     public static void main(final String[] args) {
