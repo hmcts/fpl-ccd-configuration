119ca40fe8d533f08ddfc445d2a53d78211e28ab
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-05-22 20:54:14 +0100

    CaseOutsource

--- playwright-e2e/pages/manage-organisation.ts
+++ playwright-e2e/pages/manage-organisation.ts
@@ -18,9 +18,11 @@ export class Organisation extends BasePage {
     async searchUnassignedCase(caseNumber: string, caseName: string) {
         await this.unassignedCaseLink.click();
         await expect(this.page.getByRole('heading', {name: 'Unassigned Cases'})).toBeVisible();
+        await expect(this.page.getByLabel('CARE_SUPERVISION_EPO').getByText('CARE_SUPERVISION_EPO', { exact: true })).toBeVisible();
         await this.showcaseFilterButton.click();
         await this.caseNumberSearchText.fill(caseNumber);
         await this.applyFilterButton.click();
+        await expect(this.page.getByText('Showing 1 to 1 of 1')).toBeVisible();
         await expect(this.page.getByRole('cell', {name: `${caseName}`})).toBeVisible();
     }
 }
