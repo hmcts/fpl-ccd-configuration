3280a0c1579dcb3e4cf18ee47ff9f45f6fe0ffb8
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-05-23 14:17:59 +0100

    Remove usused import.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/config/HttpClientConfiguration.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/config/HttpClientConfiguration.java
@@ -4,7 +4,6 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.core5.util.Timeout;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
