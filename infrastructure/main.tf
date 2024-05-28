1c25bc8392a1b41df44055f0e95170b2c07bc152
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-05-28 15:23:20 +0100

    Update main.tf to latest version on master

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.97.1"
+      version = "3.103.1"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.47.0"
+      version = "2.49.1"
     }
   }
 }
