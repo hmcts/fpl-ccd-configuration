57a8bdc49bb3179907343fb12225e5eee1342e02
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-05-30 11:16:09 +0100

    Adding back @ComponentScan notification to check if this fixes pipeline.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -4,10 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.fpl", "uk.gov.hmcts.reform.idam.client"})
+//@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.fpl", "uk.gov.hmcts.reform.idam.client",
+//    "uk.gov.hmcts.reform.document"})
+//@EnableFeignClients(basePackages = {
+//    "uk.gov.hmcts.reform.idam.client",
+//    "uk.gov.hmcts.reform.rd.client",
+//    "uk.gov.hmcts.reform.fnp.client",
+//    "uk.gov.hmcts.reform.calendar.client",
+//    "uk.gov.hmcts.reform.aac.client",
+//    "uk.gov.hmcts.reform.am.client",
+//    "uk.gov.hmcts.reform.ccd.client",
+//    "uk.gov.hmcts.reform.authorisation",
+//    "uk.gov.hmcts.reform.document",
+//    "uk.gov.hmcts.reform.ccd.document"
+//})
+//@EnableRetry
+//@EnableAsync
+//@EnableCaching
+
+
+
+@SpringBootApplication
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.idam.client",
     "uk.gov.hmcts.reform.rd.client",
@@ -18,8 +39,9 @@ import org.springframework.scheduling.annotation.EnableAsync;
     "uk.gov.hmcts.reform.ccd.client",
     "uk.gov.hmcts.reform.authorisation",
     "uk.gov.hmcts.reform.document",
-    "uk.gov.hmcts.reform.ccd.document.am"
+    "uk.gov.hmcts.reform.ccd.document"
 })
+@ComponentScan
 @EnableRetry
 @EnableAsync
 @EnableCaching
