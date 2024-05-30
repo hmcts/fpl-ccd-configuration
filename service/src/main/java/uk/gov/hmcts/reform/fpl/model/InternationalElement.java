188c308606a3139b869e9d5197bef6f855b80543
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-05-30 09:47:57 +0100

    Add old fields to avoid deserialization issues

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/InternationalElement.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/InternationalElement.java
@@ -8,6 +8,16 @@ import lombok.Data;
 @Builder
 @AllArgsConstructor
 public class InternationalElement {
+    private final String issues;
+    private final String proceedings;
+    private final String issuesReason;
+    private final String possibleCarer;
+    private final String proceedingsReason;
+    private final String significantEvents;
+    private final String possibleCarerReason;
+    private final String significantEventsReason;
+    private final String internationalAuthorityInvolvement;
+    private final String internationalAuthorityInvolvementDetails;
     private final String whichCountriesInvolved;
     private final String outsideHagueConvention;
     private final String importantDetails;
--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/docmosis/DocmosisInternationalElement.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/docmosis/DocmosisInternationalElement.java
@@ -10,6 +10,11 @@ import lombok.Data;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class DocmosisInternationalElement {
+    private final String possibleCarer;
+    private final String significantEvents;
+    private final String proceedings;
+    private final String internationalAuthorityInvolvement;
+    private final String issues;
     private final String whichCountriesInvolved;
     private final String outsideHagueConvention;
     private final String importantDetails;
