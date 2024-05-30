694a56ce1912fc1eb6c9bbe89aa9b77308cd575c
Abigail Smith <abigail.smith@hmcts.net>
2024-05-30 16:59:32 +0100

    DFPL 2344: tidy up smoke test

--- playwright-e2e/pages/submit-case.ts
+++ playwright-e2e/pages/submit-case.ts
@@ -1,7 +1,5 @@
 import { type Page, type Locator, expect } from "@playwright/test";
 import { BasePage } from "./base-page";
-import { join } from "path";
-import { ApplicantDetails } from "./applicant-details";
 
 export class SubmitCase extends BasePage{
  // readonly page: Page;
@@ -10,7 +8,6 @@ export class SubmitCase extends BasePage{
   readonly applicationSentHeading: Locator;
   readonly closeReturnToCase: Locator;
   readonly caseInfoHeading: Locator;
-  readonly teamManagerNameText: Locator;
   readonly paymentAmountLocator: Locator;
   readonly paymentAmountText: Locator;
 
@@ -21,26 +18,22 @@ export class SubmitCase extends BasePage{
     this.applicationSentHeading = page.getByRole('heading', { name: 'Application sent' });
     this.closeReturnToCase = page.getByRole('button', { name: 'Close and Return to case' });
     this.caseInfoHeading = page.getByRole('heading', { name: 'Case information' });
-    this.teamManagerNameText = page.getByText(`believe that the facts stated in this application are true`);
     this.paymentAmountLocator = page.locator('dd').filter({ hasText: '£' });
     this.paymentAmountText = page.getByText('£');
   }
 
   async submitCaseSmokeTest() {
     //first page
-    await expect(this.declarationHeading).toBeVisible();
-    await expect(this.teamManagerNameText).toBeVisible();
+    await expect(this.declarationHeading).toHaveText('Declaration');
     await this.statementAgree.check();
-    await expect(this.paymentAmountLocator).toBeVisible();
+    await expect(this.paymentAmountLocator).toHaveText('£');
     await this.clickSubmit();
     //second page
-    await expect(this.checkYourAnswersHeader).toBeVisible();
-    await expect(this.declarationHeading).toBeVisible();
-    await expect(this.teamManagerNameText).toBeVisible();
-    await expect(this.paymentAmountText).toBeVisible();
+    await expect(this.checkYourAnswersHeader).toHaveText('Check your answers');
+    await expect(this.declarationHeading).toHaveText('Declaration');
+    await expect(this.paymentAmountText).toHaveText('£');
     await this.clickSubmit();
-    await expect(this.applicationSentHeading).toBeVisible();
+    await expect(this.applicationSentHeading).toHaveText('Application sent');
     await this.closeReturnToCase.click();
-   // await expect(this.caseInfoHeading).toBeVisible();
   }
 }
