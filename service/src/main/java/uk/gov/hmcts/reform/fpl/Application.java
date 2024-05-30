5bad4e5aec2cc0ee3dfe992e9583a83da5839d4f
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-05-30 16:04:42 +0100

    Update list of base packages to scan.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/Application.java
@@ -4,31 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-//@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.fpl", "uk.gov.hmcts.reform.idam.client",
-//    "uk.gov.hmcts.reform.document"})
-//@EnableFeignClients(basePackages = {
-//    "uk.gov.hmcts.reform.idam.client",
-//    "uk.gov.hmcts.reform.rd.client",
-//    "uk.gov.hmcts.reform.fnp.client",
-//    "uk.gov.hmcts.reform.calendar.client",
-//    "uk.gov.hmcts.reform.aac.client",
-//    "uk.gov.hmcts.reform.am.client",
-//    "uk.gov.hmcts.reform.ccd.client",
-//    "uk.gov.hmcts.reform.authorisation",
-//    "uk.gov.hmcts.reform.document",
-//    "uk.gov.hmcts.reform.ccd.document"
-//})
-//@EnableRetry
-//@EnableAsync
-//@EnableCaching
-
-
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.reform.fpl", "uk.gov.hmcts.reform.idam.client",
+    "uk.gov.hmcts.reform.document"})
 @EnableFeignClients(basePackages = {
     "uk.gov.hmcts.reform.idam.client",
     "uk.gov.hmcts.reform.rd.client",
@@ -41,7 +21,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
     "uk.gov.hmcts.reform.document",
     "uk.gov.hmcts.reform.ccd.document"
 })
-@ComponentScan
 @EnableRetry
 @EnableAsync
 @EnableCaching
