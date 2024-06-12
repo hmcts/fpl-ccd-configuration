55c90bcf22fb0e1a0b91ac2c37c59b763058928c
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-06-12 14:43:58 +0100

    WaitforPageLoad

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     // Record trace only when retrying a test for the first time.
     trace: 'on-first-retry',
       // Record video only when retrying a test for the first time.
-       video: 'on-first-retry'
+       video: 'on'
 
   },
 
