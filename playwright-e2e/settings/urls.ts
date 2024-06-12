c145c9585b36b8f93829df324c93e2bd3376ccfb
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-06-12 20:31:01 +0100

    rollbackURLChangesFromMaster

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -6,10 +6,10 @@ interface UrlConfig {
 }
 
 export const urlConfig: UrlConfig = {
-
-  idamUrl: process.env.IDAM_API_URL || `https://idam-api.${env}.platform.hmcts.net`,
-  serviceUrl: process.env.CASE_SERVICE_URL || `http://fpl-case-service-${env}.service.core-compute-${env}.internal`,
+    idamUrl: process.env.AAT_IDAM_URL || `https://idam-api.${env}.platform.hmcts.net`,
+    serviceUrl: process.env.AAT_SERVICE_URL || `http://fpl-case-service-${env}.service.core-compute-${env}.internal`,
   frontEndBaseURL: process.env.FE_BASE_URL || `https://manage-case.${env}.platform.hmcts.net/`,
+    manageOrgURL: process.env.MO_BASE_URL || `https://manage-org.${env}.platform.hmcts.net/`,
   // You can add other URLs as needed
 
 };
