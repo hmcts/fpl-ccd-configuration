b9e4d1b8492cd234b5b2834241da7e176552680e
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-06-12 13:01:22 +0100

    WaitforPageLoad

--- playwright-e2e/pages/sign-in.ts
+++ playwright-e2e/pages/sign-in.ts
@@ -31,6 +31,7 @@ export class SignInPage extends BasePage{
   }
   async navigateTOCaseDetails(caseNumber:string) {
     await this.page.goto(`${urlConfig.frontEndBaseURL}case-details/${caseNumber}`);
+    expect(this.page.getByText('CCD ID',{exact:true})).toBeVisible();
     await  this.tabNavigation('History');
   }
 
