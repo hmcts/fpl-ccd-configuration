5a3ce92026cce2deb5aca1d845c53395f01d93bb
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2024-06-26 14:49:53 +0100

    Dummy change

--- e2e/tests/smoke_test.js
+++ e2e/tests/smoke_test.js
@@ -8,7 +8,7 @@ Feature('Smoke tests @smoke-tests @fixed');
 Scenario(
   'Sign in as Local Authority and create a case',
   async ({ I, caseListPage }) => {
-    output.print('Smoke test triggered');
+    output.print('Smoke test triggered NOW');
     await I.goToPage(config.baseUrl, config.newSwanseaLocalAuthorityUserOne);
     const caseName = `Smoke test case (${moment().format('YYYY-MM-DD HH:MM')})`;
     const caseId = await I.createCaseSmokeTest(
