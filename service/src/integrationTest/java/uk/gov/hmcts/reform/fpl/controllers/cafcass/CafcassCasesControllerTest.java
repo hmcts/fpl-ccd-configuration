1eb519df0bb67771079020e2adc49db83b5212fb
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-07-02 16:41:41 +0100

    update CafcassCasesController for POC

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesControllerTest.java
@@ -10,8 +10,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.fpl.controllers.AbstractTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CafcassCasesController.class)
 @OverrideAutoConfiguration(enabled = true)
@@ -20,16 +22,50 @@ public class CafcassCasesControllerTest extends AbstractTest {
     private MockMvc mockMvc;
 
     @Test
-    void searchCases() {
-        getEvent("/cases/start_date=2023-03-28T12:32:54.541&end_date=2024-03-27T12:32:54.54", Object.class);
-        assertTrue(true);
+    void searchCases() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "2023-03-28T12:32:54.541")
+                .queryParam("endDate", "2024-03-27T12:32:54.542"))
+            .andExpect(status().is(200))
+            .andReturn();
+
+        assertEquals("searchCases - Start date: [2023-03-28], End date: [2024-03-27]",
+            response.getResponse().getContentAsString());
     }
 
+    @Test
+    void searchCasesEmptyParam400() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases"))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void searchCases500() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "2024-03-28T12:32:54.541")
+                .queryParam("endDate", "2023-03-27T12:32:54.542"))
+            .andExpect(status().is(500))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 500);
+    }
 
     private <T> T getEvent(String path, Class<T> responseType, String... userRoles) {
         try {
             MvcResult response = mockMvc
-                .perform(post(path)
+                .perform(get(path)
                     .header("user-roles", String.join(",", userRoles)))
                 .andReturn();
 
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesController.java
@@ -4,20 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper;
 
 import java.time.DateTimeException;
-import java.time.LocalDateTime;
-import java.util.Map;
-import java.util.UUID;
+import java.time.LocalDate;
 
 import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 
@@ -25,7 +18,7 @@ import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 @RequestMapping("/cases")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CafcassCasesController {
-    private static final String DATE_TIME_FORMAT_IN = "yyyy-MM-ddThh:mm:ss.SSS";
+    private static final String DATE_TIME_FORMAT_IN = "yyyy-MM-dd'T'hh:mm:ss.SSS";
     private static final String DATE_TIME_FORMAT_OUT = "yyyy-MM-dd";
 
     @GetMapping("")
@@ -39,82 +32,86 @@ public class CafcassCasesController {
                 throw new IllegalArgumentException("endDate empty");
             }
 
-            LocalDateTime startLocalDateTime =
-                DateFormatterHelper.parseLocalDateTimeFromStringUsingFormat(startDate, DATE_TIME_FORMAT_IN);
-            LocalDateTime endLocalDateTime =
-                DateFormatterHelper.parseLocalDateTimeFromStringUsingFormat(endDate, DATE_TIME_FORMAT_IN);
+            LocalDate startLocalDateTime =
+                DateFormatterHelper.parseLocalDateFromStringUsingFormat(startDate, DATE_TIME_FORMAT_IN);
+            LocalDate endLocalDateTime =
+                DateFormatterHelper.parseLocalDateFromStringUsingFormat(endDate, DATE_TIME_FORMAT_IN);
+
+            if (startLocalDateTime.isAfter(endLocalDateTime)) {
+                throw new Exception("startDate after endDate");
+            }
 
             return ResponseEntity.ok(String.format("searchCases - Start date: [%s], End date: [%s]",
-                DateFormatterHelper.formatLocalDateTimeBaseUsingFormat(startLocalDateTime, DATE_TIME_FORMAT_OUT),
-                DateFormatterHelper.formatLocalDateTimeBaseUsingFormat(endLocalDateTime, DATE_TIME_FORMAT_OUT)
+                DateFormatterHelper.formatLocalDateToString(startLocalDateTime, DATE_TIME_FORMAT_OUT),
+                DateFormatterHelper.formatLocalDateToString(endLocalDateTime, DATE_TIME_FORMAT_OUT)
             ));
         } catch (DateTimeException e) {
             return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
         } catch (Exception e) {
-            return ResponseEntity.status(500).body("Internal server error");
+            return ResponseEntity.status(500).body("Internal server error - " + e.getMessage());
         }
     }
 
-    @GetMapping("documents/{documentId}/binary")
-    public ResponseEntity<Object> getDocumentBinary(@PathVariable String documentId) {
-        try {
-            if (isEmpty(documentId)) {
-                throw new IllegalArgumentException("documentId empty");
-            }
-
-            return ResponseEntity.ok(String.format("getDocumentBinary - document id: [%s]",
-                UUID.fromString(documentId)));
-
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Internal server error");
-        }
-    }
-
-    @PostMapping("{caseId}/document")
-    public ResponseEntity<Object> uploadDocument(@PathVariable String caseId,
-                                                 @RequestPart(value = "file") MultipartFile file,
-                                                 @RequestPart(value = "typeOfDocument") String typeOfDocument) {
-        try {
-            if (isEmpty(caseId)) {
-                throw new IllegalArgumentException("caseId empty");
-            }
-            if (isEmpty(file)) {
-                throw new IllegalArgumentException("file empty");
-            }
-            if (isEmpty(typeOfDocument)) {
-                throw new IllegalArgumentException("typeOfDocument empty");
-            }
-
-            return ResponseEntity.ok(String.format(
-                "uploadDocument - caseId: [%s], file length: [%s], typeOfDocument: [%s]",
-                UUID.fromString(caseId), file.getSize(), typeOfDocument));
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Internal server error");
-        }
-    }
-
-    @PostMapping("{caseId}/guardians")
-    public ResponseEntity<Object> uploadGuardians(@PathVariable String caseId,
-                                                  @RequestBody Map<String, Object> requestBody) {
-        try {
-            if (isEmpty(caseId)) {
-                throw new IllegalArgumentException("caseId empty");
-            }
-            if (isEmpty(requestBody)) {
-                throw new IllegalArgumentException("requestBody empty");
-            }
-
-            return ResponseEntity.ok(String.format(
-                "uploadGuardians - caseId: [%s], guardianName: [%s], children: [%s]",
-                UUID.fromString(caseId), requestBody.get("guardianName"), requestBody.get("children")));
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Internal server error");
-        }
-    }
+//    @GetMapping("documents/{documentId}/binary")
+//    public ResponseEntity<Object> getDocumentBinary(@PathVariable String documentId) {
+//        try {
+//            if (isEmpty(documentId)) {
+//                throw new IllegalArgumentException("documentId empty");
+//            }
+//
+//            return ResponseEntity.ok(String.format("getDocumentBinary - document id: [%s]",
+//                UUID.fromString(documentId)));
+//
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body("Internal server error");
+//        }
+//    }
+//
+//    @PostMapping("{caseId}/document")
+//    public ResponseEntity<Object> uploadDocument(@PathVariable String caseId,
+//                                                 @RequestPart(value = "file") MultipartFile file,
+//                                                 @RequestPart(value = "typeOfDocument") String typeOfDocument) {
+//        try {
+//            if (isEmpty(caseId)) {
+//                throw new IllegalArgumentException("caseId empty");
+//            }
+//            if (isEmpty(file)) {
+//                throw new IllegalArgumentException("file empty");
+//            }
+//            if (isEmpty(typeOfDocument)) {
+//                throw new IllegalArgumentException("typeOfDocument empty");
+//            }
+//
+//            return ResponseEntity.ok(String.format(
+//                "uploadDocument - caseId: [%s], file length: [%s], typeOfDocument: [%s]",
+//                UUID.fromString(caseId), file.getSize(), typeOfDocument));
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body("Internal server error");
+//        }
+//    }
+//
+//    @PostMapping("{caseId}/guardians")
+//    public ResponseEntity<Object> uploadGuardians(@PathVariable String caseId,
+//                                                  @RequestBody Map<String, Object> requestBody) {
+//        try {
+//            if (isEmpty(caseId)) {
+//                throw new IllegalArgumentException("caseId empty");
+//            }
+//            if (isEmpty(requestBody)) {
+//                throw new IllegalArgumentException("requestBody empty");
+//            }
+//
+//            return ResponseEntity.ok(String.format(
+//                "uploadGuardians - caseId: [%s], guardianName: [%s], children: [%s]",
+//                UUID.fromString(caseId), requestBody.get("guardianName"), requestBody.get("children")));
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body("Internal server error");
+//        }
+//    }
 }
