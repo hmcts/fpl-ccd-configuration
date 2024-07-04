3c5f49355c6319f9f003d1df33053128e34c9b22
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-07-04 11:07:39 +0100

    addimplicitWaitforNextstepNavigation

--- playwright-e2e/pages/base-page.ts
+++ playwright-e2e/pages/base-page.ts
@@ -27,9 +27,10 @@ export class BasePage {
   async gotoNextStep(eventName: string) {
     await this.nextStep.selectOption(eventName);
     await this.goButton.dblclick();
-    await this.page.waitForTimeout(20000);
+    await expect(this.page.getByRole("heading",{name:`${eventName}`})).toBeVisible();
     if (await this.goButton.isVisible()) {
       await this.goButton.click();
+      await expect(this.page.getByRole("heading",{name:`${eventName}`})).toBeVisible();
     }
   }
 
