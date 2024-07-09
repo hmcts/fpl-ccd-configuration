c7111f2028f4efa08402d2f52c26d2e14857b689
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2024-07-09 10:25:36 +0100

    DFPL-2427 POC controllers for FPL API gateway (#5481)
    * add dummy controller for API gateway POC
    
    * update CafcassCasesController for POC
    
    * update CafcassCasesController for POC
    
    * Update CafcassCasesController.java
    
    * Update CafcassCasesControllerTest.java
    
    * Update CafcassCasesController.java
    
    * add all endpoint for POC
    
    * remove unused import
    
    * Update CafcassCasesControllerTest.java
    
    * Update CafcassCasesController.java
    
    * Update CafcassCasesController.java
    
    * update CafcassCasesController

---
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesControllerTest.java
@@ -0,0 +1,283 @@
+package uk.gov.hmcts.reform.fpl.controllers.cafcass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import uk.gov.hmcts.reform.fpl.controllers.AbstractTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CafcassCasesController.class)
+@OverrideAutoConfiguration(enabled = true)
+public class CafcassCasesControllerTest extends AbstractTest {
+    private static final UUID CASE_ID = UUID.randomUUID();
+    private static final  byte[] FILE_BYTES = "This is a file. Trust me!".getBytes();
+    private static final MockMultipartFile FILE = new MockMultipartFile(
+        "file", "MOCK_FILE.pdf", MediaType.TEXT_PLAIN_VALUE, FILE_BYTES);
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void searchCases() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "2023-03-28T12:32:54.541")
+                .queryParam("endDate", "2024-03-27T12:32:54.542"))
+            .andExpect(status().is(200))
+            .andReturn();
+
+        assertEquals("searchCases - Start date: [2023-03-28], End date: [2024-03-27]",
+            response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void searchCasesInvalidFormat400() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "123")
+                .queryParam("endDate", "321"))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void searchCasesEmptyParam400() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases"))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+
+        response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "2023-03-28T12:32:54.541"))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+
+        response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("endDate", "2024-03-27T12:32:54.542"))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void searchCases500() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(","))
+                .queryParam("startDate", "2024-03-28T12:32:54.541")
+                .queryParam("endDate", "2023-03-27T12:32:54.542"))
+            .andExpect(status().is(500))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 500);
+    }
+
+    @Test
+    void getDocumentBinary() throws Exception {
+        UUID docId = UUID.randomUUID();
+        MvcResult response = mockMvc
+            .perform(get("/cases/documents/%s/binary".formatted(docId))
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(200))
+            .andReturn();
+
+        assertEquals("getDocumentBinary - document id: [%s]".formatted(docId),
+            response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void getDocumentBinary400() throws Exception {
+        MvcResult response = mockMvc
+            .perform(get("/cases/documents/123/binary")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+
+        response = mockMvc
+            .perform(get("/cases/documents/ /binary")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void uploadDocument() throws Exception {
+
+        MvcResult response = mockMvc
+            .perform(MockMvcRequestBuilders.multipart("/cases/%s/document".formatted(CASE_ID))
+                .file(FILE)
+                .param("typeOfDocument", "type Of Document")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(200))
+            .andReturn();
+
+        assertEquals("uploadDocument - caseId: [%s], file length: [%s], typeOfDocument: [%s]"
+                .formatted(CASE_ID, FILE_BYTES.length, "type Of Document"),
+            response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void uploadDocument400() throws Exception {
+        UUID caseId = UUID.randomUUID();
+
+        MvcResult response = mockMvc
+            .perform(MockMvcRequestBuilders.multipart("/cases/%s/document".formatted(caseId))
+                .param("typeOfDocument", "type Of Document")
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+
+
+        byte[] fileBytes = "This is a file. Trust me!".getBytes();
+        MockMultipartFile file = new MockMultipartFile(
+            "file", "MOCK_FILE.pdf", MediaType.TEXT_PLAIN_VALUE, fileBytes);
+
+        response = mockMvc
+            .perform(MockMvcRequestBuilders.multipart("/cases/%s/document".formatted(caseId))
+                .file(file)
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void uploadGuardians() throws Exception {
+        UUID caseId = UUID.randomUUID();
+
+        MvcResult response = mockMvc.perform(post("/cases/%s/guardians".formatted(caseId))
+                .content("[\n"
+                         + "  {\n"
+                         + "    \"guardianName\": \"John Smith\",\n"
+                         + "    \"telephoneNumber\": \"01234567890\",\n"
+                         + "    \"email\": \"john.smith@example.com\",\n"
+                         + "    \"children\": [\n"
+                         + "      \"Joe Bloggs\"\n"
+                         + "    ]\n"
+                         + "  }\n"
+                         + "]")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(200))
+            .andReturn();
+
+        assertEquals("uploadGuardians - caseId: [%s], no of guardians: [%s]\nguardianName: [%s], children: [%s]\n"
+                .formatted(caseId, 1, "John Smith", "Joe Bloggs"),
+            response.getResponse().getContentAsString());
+    }
+
+    @Test
+    void uploadGuardians400() throws Exception {
+        UUID caseId = UUID.randomUUID();
+
+        MvcResult response = mockMvc.perform(post("/cases/%s/guardians".formatted(caseId))
+                .content("[]")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+
+        response = mockMvc.perform(post("/cases/%s/guardians".formatted(" "))
+                .content("[\n"
+                         + "  {\n"
+                         + "    \"guardianName\": \"John Smith\",\n"
+                         + "    \"telephoneNumber\": \"01234567890\",\n"
+                         + "    \"email\": \"john.smith@example.com\",\n"
+                         + "    \"children\": [\n"
+                         + "      \"Joe Bloggs\"\n"
+                         + "    ]\n"
+                         + "  }\n"
+                         + "]")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(400))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 400);
+    }
+
+    @Test
+    void uploadGuardians500() throws Exception {
+        UUID caseId = UUID.randomUUID();
+
+        MvcResult response = mockMvc.perform(post("/cases/%s/guardians".formatted(caseId))
+                .content("[\n"
+                         + "  {\n"
+                         + "    \"guardianName\": \"John Smith\",\n"
+                         + "    \"telephoneNumber\": \"01234567890\",\n"
+                         + "    \"email\": \"john.smith@example.com\",\n"
+                         + "    \"children\": \"12313\""
+                         + "  }\n"
+                         + "]")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("authorization", USER_AUTH_TOKEN)
+                .header("user-id", USER_ID)
+                .header("user-roles", String.join(",")))
+            .andExpect(status().is(500))
+            .andReturn();
+
+        assertEquals(response.getResponse().getStatus(), 500);
+    }
+}
---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/cafcass/CafcassCasesController.java
@@ -0,0 +1,119 @@
+package uk.gov.hmcts.reform.fpl.controllers.cafcass;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import uk.gov.hmcts.reform.fpl.utils.DateFormatterHelper;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+
+@Slf4j
+@RestController
+@RequestMapping("/cases")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CafcassCasesController {
+    private static final String DATE_TIME_FORMAT_IN = "yyyy-MM-dd'T'hh:mm:ss.SSS";
+    private static final String DATE_TIME_FORMAT_OUT = "yyyy-MM-dd";
+
+    @GetMapping("")
+    public ResponseEntity<Object> searchCases(@RequestParam(name = "startDate") String startDate,
+                                              @RequestParam(name = "endDate") String endDate) {
+        log.info("searchCases request received");
+        try {
+            log.info("searchCases, " + startDate + ", " + endDate);
+            LocalDate startLocalDateTime =
+                DateFormatterHelper.parseLocalDateFromStringUsingFormat(startDate, DATE_TIME_FORMAT_IN);
+            LocalDate endLocalDateTime =
+                DateFormatterHelper.parseLocalDateFromStringUsingFormat(endDate, DATE_TIME_FORMAT_IN);
+
+            if (startLocalDateTime.isAfter(endLocalDateTime)) {
+                throw new Exception("startDate after endDate");
+            }
+
+            return ResponseEntity.ok(String.format("searchCases - Start date: [%s], End date: [%s]",
+                DateFormatterHelper.formatLocalDateToString(startLocalDateTime, DATE_TIME_FORMAT_OUT),
+                DateFormatterHelper.formatLocalDateToString(endLocalDateTime, DATE_TIME_FORMAT_OUT)
+            ));
+        } catch (DateTimeException e) {
+            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal server error - " + e.getMessage());
+        }
+    }
+
+    @GetMapping("documents/{documentId}/binary")
+    public ResponseEntity<Object> getDocumentBinary(@PathVariable String documentId) {
+        log.info("getDocumentBinary request received");
+        try {
+            return ResponseEntity.ok(String.format("getDocumentBinary - document id: [%s]",
+                UUID.fromString(documentId)));
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal server error");
+        }
+    }
+
+    @PostMapping("{caseId}/document")
+    public ResponseEntity<Object> uploadDocument(@PathVariable String caseId,
+                                                 @RequestParam(value = "file") MultipartFile file,
+                                                 @RequestParam(value = "typeOfDocument") String typeOfDocument) {
+        log.info("uploadDocument request received");
+
+        try {
+            return ResponseEntity.ok(String.format(
+                "uploadDocument - caseId: [%s], file length: [%s], typeOfDocument: [%s]",
+                UUID.fromString(caseId), file.getSize(), typeOfDocument));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal server error");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @PostMapping("{caseId}/guardians")
+    public ResponseEntity<Object> uploadGuardians(@PathVariable String caseId,
+                                                  @RequestBody List<Map<String, Object>> guardians) {
+        log.info("uploadGuardians request received");
+        try {
+            if (isEmpty(guardians)) {
+                throw new IllegalArgumentException("list empty");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("uploadGuardians - caseId: [%s], no of guardians: [%s]\n"
+                    .formatted(UUID.fromString(caseId), guardians.size()));
+            log.info("uploadGuardians guardians size " + guardians.size());
+
+            guardians.forEach(guardian -> {
+                sb.append("guardianName: [%s], ".formatted(guardian.get("guardianName")));
+                sb.append("children: [%s]".formatted(String.join(", ",
+                    ((List<String>) guardian.get("children")))));
+                sb.append("\n");
+            });
+
+            return ResponseEntity.ok(sb);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body("bad input parameter - " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Internal server error");
+        }
+    }
+}
