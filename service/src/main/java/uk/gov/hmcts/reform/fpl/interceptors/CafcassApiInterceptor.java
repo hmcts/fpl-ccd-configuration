80db602cb0f42368e92f9e9805552486d62ce366
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2024-07-09 14:04:25 +0100

    DFPL-2449 Add interceptor and exception handling for Cafcass API (#5493)
    * Fix ES/logstash configuration in preview (#5488)
    
    * Copy adoption configuration to attempt to persist ES a bit more between pipeline runs
    
    * fix template variable in ES
    
    * fix template variable in ES
    
    * DFPL-2427 POC controllers for FPL API gateway (#5481)
    
    * add dummy controller for API gateway POC
    
    * update CafcassCasesController for POC
    
    * update CafcassCasesController for POC
    
    * Update CafcassCasesController.java
    
    * Update CafcassCasesControllerTest.java
    
    * Update CafcassCasesController.java
    
    * add all endpoint for POC
    
    * remove unused import
    
    * Update CafcassCasesControllerTest.java
    
    * Update CafcassCasesController.java
    
    * Update CafcassCasesController.java
    
    * update CafcassCasesController
    
    * add interceptor for verifying user role
    
    * add other exception
    
    * add unit test
    
    * fix check style
    
    ---------
    
    Co-authored-by: Daniel Catchpole <daniel.catchpole@justice.gov.uk>

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/interceptors/CafcassApiInterceptor.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.fpl.interceptors;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.HandlerInterceptor;
+import uk.gov.hmcts.reform.fpl.exceptions.api.AuthorizationException;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
+import static uk.gov.hmcts.reform.fpl.enums.UserRole.CAFCASS_SYSTEM_UPDATE;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class CafcassApiInterceptor implements HandlerInterceptor {
+    private final IdamClient idamClient;
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        String authToken = request.getHeader("Authorization");
+        if (isNotEmpty(authToken)) {
+            UserInfo userInfo = idamClient.getUserInfo(authToken);
+            if (userInfo != null && userInfo.getRoles().contains(CAFCASS_SYSTEM_UPDATE.getRoleName())) {
+                return true;
+            }
+        }
+        throw new AuthorizationException();
+    }
+}
