fe57a0fd8812536284116db642350f616493394f
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2024-07-15 18:35:31 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
   testMatch:'*spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  timeout: 4*60*1000, //each test execution time is set to 4 min
-  expect: { timeout: 1*60*1000 }, //wait time for the assertion to be true 60 sec
+  timeout: 3*60*1000, //each test execution time is set to 3 min
+  expect: { timeout: 1*45*1000 }, //wait time for the assertion to be true 45 sec
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
