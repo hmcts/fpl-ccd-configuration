35c88ec8a5d99e848a80082efa9edeccaf5f79db
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-07-16 13:01:47 +0100

    remove converters chain

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/cafcass/apibuilders/CafcassApiCaseDataConverter.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.fpl.service.cafcass.apibuilders;
-
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.api.cafcass.CafcassApiCaseData;
-
-public interface CafcassApiCaseDataConverter {
-    CafcassApiCaseData.CafcassApiCaseDataBuilder convert(CaseData caseData,
-                                                         CafcassApiCaseData.CafcassApiCaseDataBuilder builder);
-}
