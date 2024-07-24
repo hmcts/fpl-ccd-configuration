c7ea80089d1c4d13e851251e9db60377807d68d1
Charles Braimah <charles.braimah@justice.gov.uk>
2024-07-24 07:32:34 +0100

    DFPL-2388

--- playwright-e2e/tests/return-application.spec.ts
+++ playwright-e2e/tests/return-application.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '../fixtures/create-fixture';
 import { createCase, updateCase } from "../utils/api-helper";
-import caseData from '../caseData/caseInPrepareForHearing.json' assert { type: "json" };
+import caseData from  '../caseData/caseWithJudgeMessage.json' assert { type: 'json' };
 import { CTSCUser, newSwanseaLocalAuthorityUserOne, HighCourtAdminUser } from "../settings/user-credentials";
 import { expect } from "@playwright/test";
 import { testConfig } from "../settings/test-config";
@@ -31,7 +31,6 @@ test.describe('Return application', () => {
       await page.getByRole('button', { name: 'Submit' }).click();
       await page.getByRole('button', { name: 'Save and continue' }).click();
       await signInPage.logout();
-
 
     });
 
