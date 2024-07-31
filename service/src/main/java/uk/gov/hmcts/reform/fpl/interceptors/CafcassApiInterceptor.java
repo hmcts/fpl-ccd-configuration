8dbdea805712f2a13615d8c4b694e1f94cd14c63
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-07-31 11:28:35 +0100

    use cafcass role until new role is set up

--- service/src/main/java/uk/gov/hmcts/reform/fpl/interceptors/CafcassApiInterceptor.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/interceptors/CafcassApiInterceptor.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.hmcts.reform.fpl.exceptions.api.AuthorizationException;
@@ -20,13 +21,14 @@ import static uk.gov.hmcts.reform.fpl.enums.UserRole.CAFCASS_SYSTEM_UPDATE;
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CafcassApiInterceptor implements HandlerInterceptor {
-    private final ObjectProvider<IdamClient> idamClient;
+    @Lazy
+    private final IdamClient idamClient;
 
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         String authToken = request.getHeader("Authorization");
         if (isNotEmpty(authToken)) {
-            UserInfo userInfo = idamClient.getIfAvailable().getUserInfo(authToken);
+            UserInfo userInfo = idamClient.getUserInfo(authToken);
             if (userInfo != null && userInfo.getRoles().contains(CAFCASS_SYSTEM_UPDATE.getRoleName())) {
                 return true;
             }
