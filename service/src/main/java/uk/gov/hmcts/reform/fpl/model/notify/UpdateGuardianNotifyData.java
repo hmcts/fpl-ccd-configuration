8e2598a949e47879bcdcaa01e5248a291ae102e8
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-08-19 17:16:48 +0100

    Create UpdateGuardianNotifyData.java

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/notify/UpdateGuardianNotifyData.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.fpl.model.notify;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@Builder
+@EqualsAndHashCode
+public class UpdateGuardianNotifyData implements NotifyData {
+    private final String firstRespondentLastName;
+    private final String familyManCaseNumber;
+    private final String caseUrl;
+}
