04e8d11bac0d5841c452d34d0e07d91b15f2f3f5
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-08-20 18:40:28 +0100

    fix guardian

--- service/src/main/java/uk/gov/hmcts/reform/fpl/model/Guardian.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/model/Guardian.java
@@ -3,8 +3,12 @@ package uk.gov.hmcts.reform.fpl.model;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import uk.gov.hmcts.reform.fpl.model.common.Element;
 
 import java.util.List;
+
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.unwrapElements;
+import static uk.gov.hmcts.reform.fpl.utils.ElementUtils.wrapElements;
 
 @Getter
 @EqualsAndHashCode
@@ -14,4 +18,14 @@ public class Guardian {
     private String telephoneNumber;
     private String email;
     private List<String> children;
+
+    // getter/setter work around for CCD persisting only
+    public List<Element<String>> getChildrenRepresenting() {
+        return wrapElements(children);
+    }
+
+    // getter/setter work around for CCD persisting only
+    public void setChildrenRepresenting(List<Element<String>> childrenRepresenting) {
+        children = unwrapElements(childrenRepresenting);
+    }
 }
