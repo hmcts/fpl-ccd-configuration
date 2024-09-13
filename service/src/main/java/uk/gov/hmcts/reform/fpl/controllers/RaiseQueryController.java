248c0c95de88c5cc15d731e8a6df40f791786f3e
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-09-13 17:07:25 +0100

    Initialise all qmcollections to emptylist for raise query event

---
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryController.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.reform.fpl.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import static java.util.Collections.emptyList;
+
+@RestController
+@RequestMapping("/callback/raise-query")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class RaiseQueryController extends CallbackController {
+
+    @PostMapping("/about-to-start")
+    public AboutToStartOrSubmitCallbackResponse handleAboutToStart(@RequestBody CallbackRequest callbackrequest) {
+        CaseDetails caseDetails = callbackrequest.getCaseDetails();
+
+        caseDetails.getData().put("qmCaseQueriesCollectionChildSolA", emptyList());
+        caseDetails.getData().put("qmCaseQueriesCollectionChildSolB", emptyList());
+
+        return respond(caseDetails);
+    }
+}
