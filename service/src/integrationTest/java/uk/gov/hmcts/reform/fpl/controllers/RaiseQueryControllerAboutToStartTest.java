6cff50f60900898da6e2be373f3e4d86a4e4f8c8
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-10-01 14:13:21 +0100

    Fixes for the about to start event

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryControllerAboutToStartTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryControllerAboutToStartTest.java
@@ -3,27 +3,89 @@ package uk.gov.hmcts.reform.fpl.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.enums.CaseRole;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.service.UserService;
+
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(RaiseQueryController.class)
 @OverrideAutoConfiguration(enabled = true)
 public class RaiseQueryControllerAboutToStartTest extends AbstractCallbackTest {
+
+    @MockBean
+    private UserService userService;
 
     RaiseQueryControllerAboutToStartTest() {
         super("raise-query");
     }
 
     @Test
-    void shouldPrePopulateFields() {
-        CaseData caseData = CaseData.builder().build();
+    void shouldPrePopulateCorrectCollectionForChildSolAOnly() {
+        when(userService.getCaseRoles(any())).thenReturn(Set.of(CaseRole.CHILDSOLICITORA));
+        Long caseId = 1L;
+
+        CaseData caseData = CaseData.builder()
+            .id(caseId)
+            .build();
+
         AboutToStartOrSubmitCallbackResponse response = postAboutToStartEvent(caseData);
 
-        assertThat(response.getData()).containsKeys(
-            "qmCaseQueriesCollectionChildSolA",
+        assertThat(response.getData()).containsKey(
+            "qmCaseQueriesCollectionChildSolA"
+        );
+        assertThat(response.getData()).doesNotContainKey(
             "qmCaseQueriesCollectionChildSolB"
         );
+    }
+
+    @Test
+    void shouldPrePopulateCorrectCollectionForChildSolBOnly() {
+        when(userService.getCaseRoles(any())).thenReturn(Set.of(CaseRole.CHILDSOLICITORB));
+        Long caseId = 1L;
+
+        CaseData caseData = CaseData.builder()
+            .id(caseId)
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse response = postAboutToStartEvent(caseData);
+
+        assertThat(response.getData()).containsKey(
+            "qmCaseQueriesCollectionChildSolB"
+        );
+        assertThat(response.getData()).doesNotContainKey(
+            "qmCaseQueriesCollectionChildSolA"
+        );
+    }
+
+    @Test
+    void shouldNotInitialiseFieldIfAlreadyPopulated() {
+        when(userService.getCaseRoles(any())).thenReturn(Set.of(CaseRole.CHILDSOLICITORA));
+        Long caseId = 1L;
+
+        CaseDetails caseDetails = CaseDetails.builder()
+            .data(Map.ofEntries(
+                Map.entry("Id", caseId),
+                Map.entry("qmCaseQueriesCollectionChildSolA", "some data")
+            ))
+            .build();
+
+        AboutToStartOrSubmitCallbackResponse response = postAboutToStartEvent(caseDetails);
+
+        assertThat(response.getData()).containsKey(
+            "qmCaseQueriesCollectionChildSolA"
+        );
+        assertThat(response.getData()).doesNotContainKey(
+            "qmCaseQueriesCollectionChildSolB"
+        );
+        assertThat(response.getData().get("qmCaseQueriesCollectionChildSolA")).isEqualTo("some data");
     }
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryController.java
@@ -9,19 +9,47 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.fpl.enums.CaseRole;
+import uk.gov.hmcts.reform.fpl.model.CaseData;
+import uk.gov.hmcts.reform.fpl.service.UserService;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Objects.isNull;
 
 @RestController
 @RequestMapping("/callback/raise-query")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RaiseQueryController extends CallbackController {
 
+    private final UserService userService;
+
+    private static final Map<CaseRole, String> COLLECTION_MAPPING = initialiseUserTypeToQMCollectionMapping();
+
     @PostMapping("/about-to-start")
     public AboutToStartOrSubmitCallbackResponse handleAboutToStart(@RequestBody CallbackRequest callbackrequest) {
         CaseDetails caseDetails = callbackrequest.getCaseDetails();
+        CaseData caseData = getCaseData(caseDetails);
 
-        caseDetails.getData().put("qmCaseQueriesCollectionChildSolA", null);
-        caseDetails.getData().put("qmCaseQueriesCollectionChildSolB", null);
+        Set<CaseRole> currentUserRoles = userService.getCaseRoles(caseData.getId());
+
+        for (CaseRole user : currentUserRoles) {
+            if (isNull(caseDetails.getData().getOrDefault(COLLECTION_MAPPING.get(user), null))) {
+                caseDetails.getData().put(COLLECTION_MAPPING.get(user), null);
+            }
+        }
 
         return respond(caseDetails);
+    }
+
+    private static Map<CaseRole, String> initialiseUserTypeToQMCollectionMapping() {
+        Map<CaseRole, String> collectionMapping = new LinkedHashMap<>();
+
+        collectionMapping.put(CaseRole.CHILDSOLICITORA, "qmCaseQueriesCollectionChildSolA");
+        collectionMapping.put(CaseRole.CHILDSOLICITORB, "qmCaseQueriesCollectionChildSolB");
+
+        return collectionMapping;
     }
 }
