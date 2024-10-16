377719cd0e5f822b8b70424d2750107d87509b37
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-10-16 16:25:37 +0100

    Update CafcassApiCaseDataConverter.java

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/cafcass/api/CafcassApiCaseDataConverter.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/cafcass/api/CafcassApiCaseDataConverter.java
@@ -11,7 +11,8 @@ public interface CafcassApiCaseDataConverter {
      * When querying via Elastic Search, retrieving the whole case data should be avoided.
      * If only a sub-set of case information is required, e.g. state, jurisdiction, or only a subset of the case fields,
      * only those should be requested by providing "_source". e.g. "_source": ["jurisdiction", "data.respondents1"]
-     * @return A list of string containing the "source" required by this converter. Null or empty list if no case information is required.
+     * @return A list of string containing the "source" required by this converter.
+     * Null or empty list if no case information is required.
      */
     List<String> getEsSearchSources();
     CafcassApiCaseData.CafcassApiCaseDataBuilder convert(CaseData caseData,
