5dd1373f86273e31263928991782b8e74acc5c61
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-10-21 17:58:13 +0100

    rollback

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,80 +11,81 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
 
-  testDir: "./playwright-e2e",
-  testMatch:'*spec.ts',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  timeout: 3*60*1000, //each test execution time is set to 3 min
-  expect: { timeout: 1*110*1000 }, //wait time for the assertion to be true 110 sec
+    testDir: "./playwright-e2e",
+    testMatch:'*spec.ts',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    timeout: 3*60*1000, //each test execution time is set to 3 min
+    expect: { timeout: 1*110*1000 }, //wait time for the assertion to be true 110 sec
 
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 5 : 0,
-  /*build fails when reaches 35 failed test - fail fast*/
-  maxFailures: process.env.CI ? 35 : 0,
-  /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 4 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [[process.env.CI ? 'html' : 'list'],
-             ['html', { outputFolder: '../test-results/functionalTest' }]],
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 3 : 0,
+    /*build fails when reaches 35 failed test - fail fast*/
+    maxFailures: process.env.CI ? 35 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 5 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: [[process.env.CI ? 'html' : 'list'],
+        ['html', { outputFolder: '../test-results/functionalTest' }]],
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    // Record trace only when retrying a test for the first time.
-    trace: 'on-first-retry',
-  },
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        // Record trace only when retrying a test for the first time.
+        trace: 'on-first-retry',
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: "chromium",
+            use: { ...devices["Desktop Chrome"] },
+        },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-    {
-      name: "preview",
-      use: { ...devices["Desktop Chrome"] },
-      retries: 3,
-      timeout: 3*60*1000,
-      expect: { timeout: 1*60*1000 },
-    },
+        {
+            name: "firefox",
+            use: { ...devices["Desktop Firefox"] },
+        },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: "Mobile Chrome",
-    //   use: { ...devices["Pixel 5"] },
-    // },
-    // {
-    //   name: "Mobile Safari",
-    //   use: { ...devices["iPad Mini"] },
-    // },
+        {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+        },
+        {
+            name: "preview",
+            use: { ...devices["Desktop Chrome"] },
+            retries: 3,
+            timeout: 3*60*1000,
+            expect: { timeout: 1*60*1000 },
+        },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+        /* Test against mobile viewports. */
+        // {
+        //   name: "Mobile Chrome",
+        //   use: { ...devices["Pixel 5"] },
+        // },
+        // {
+        //   name: "Mobile Safari",
+        //   use: { ...devices["iPad Mini"] },
+        // },
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
+    ],
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
 });
