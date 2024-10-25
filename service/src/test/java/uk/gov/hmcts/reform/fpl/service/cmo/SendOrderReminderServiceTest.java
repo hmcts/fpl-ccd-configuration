16a46677671c22a5b6e6aed51b356d274b092cc1
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-10-25 17:31:40 +0100

    Add test

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/cmo/SendOrderReminderServiceTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/cmo/SendOrderReminderServiceTest.java
@@ -5,8 +5,10 @@ import uk.gov.hmcts.reform.fpl.enums.HearingType;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.HearingBooking;
 import uk.gov.hmcts.reform.fpl.model.common.Element;
+import uk.gov.hmcts.reform.fpl.model.order.HearingOrder;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -59,6 +61,30 @@ class SendOrderReminderServiceTest {
         assertThat(actual).isEqualTo(caseData.getHearingDetails().stream()
             .map(Element::getValue)
             .collect(Collectors.toList()));
+    }
+
+    @Test
+    void shouldReturnEmptyListIfNoHearingsMissingCMOsButNoCMOIdPresent() {
+        LocalDateTime startDate = LocalDateTime.of(2020, Month.JUNE, 21, 0, 0);
+        CaseData caseData = CaseData.builder()
+            .id(12345L)
+            .hearingDetails(List.of(
+                element(HearingBooking.builder()
+                    .type(HearingType.CASE_MANAGEMENT)
+                    .startDate(startDate)
+                    .endDate(startDate.plusHours(1))
+                    .build())
+            ))
+            .sealedCMOs(List.of(
+                element(HearingOrder.builder()
+                    .hearing("Case management hearing, 21 June 2020")
+                    .build())
+            ))
+            .build();
+
+        List<HearingBooking> actual = underTest.getPastHearingBookingsWithoutCMOs(caseData);
+
+        assertThat(actual).isEqualTo(emptyList());
     }
 
 }
