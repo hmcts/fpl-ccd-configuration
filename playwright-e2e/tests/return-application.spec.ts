7fac6208dff05a40d4b92cb0bb2cd415e54cb4f9
prabhamuthu15 <78162288+prabhamuthu15@users.noreply.github.com>
2024-10-25 09:18:10 +0100

    Dfpl 2388 UI test return application (#5661)
    * FixCaseFlagAPICaseCreationAssertion
    
    * enableMutationTest
    
    * RemoveForbidForTesting
    
    * EnableMutationTest
    
    * EnableForbidonlyCheck
    
    * RemoveOnlyTag
    
    * rollbackTries
    
    * UseDefaultWorker
    
    * UpdateWorker
    
    * disableMutationTest
    
    * UseDefaultWorker
    
    * UseDefaultWorker
    
    * RollbackWorkers
    
    * EnabaleMutationtest
    
    * Update playwright.config.ts
    
    * Update playwright.config.ts
    
    * Update playwright.config.ts
    
    * Update playwright.config.ts
    
    * rollback
    
    * rollback
    
    * rollback
    
    * rollback
    
    * rollback
    
    * rollback
    
    * rollback
    
    * Dfpl 2388 (#5652)
    
    * DFPL-2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * fix test
    
    * Upped timeout to 110 seconds
    
    * Undo timeout change
    
    * DFPL-2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * Delete playwright-e2e/example.spec.ts
    
    * DFPL-2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * Update return-application.spec.ts
    
    * DFPL-2388
    
    * Delete e2e/example.spec.ts
    
    * 2388
    
    * DFPL-2388
    
    * DFPL-2388
    
    * Dfpl-2388
    
    * DFPL-2388
    
    ---------
    
    Co-authored-by: Charles Braimah <charles.braimah@justice.gov.uk>
    Co-authored-by: GauravChawlaMOJ <gaurav.chawla@justice.gov.uk>
    Co-authored-by: gavinkeys <gdkeys6@gmail.com>
    
    * updateYarnLock
    
    * disableMutationAndFortify
    
    * disableMutationAndFortify
    
    * updatepackageToinstallPlaywright
    
    * Delete yarn.lock
    
    * updateyarnlock
    
    * updateyarnlock
    
    * RollbackVersion
    
    * DFPL-2388
    
    * nightly-dev
    
    * Update Jenkinsfile_nightly
    
    ---------
    
    Co-authored-by: Braimah101 <41795070+Braimah101@users.noreply.github.com>
    Co-authored-by: Charles Braimah <charles.braimah@justice.gov.uk>
    Co-authored-by: GauravChawlaMOJ <gaurav.chawla@justice.gov.uk>
    Co-authored-by: gavinkeys <gdkeys6@gmail.com>

---
+++ playwright-e2e/tests/return-application.spec.ts
@@ -0,0 +1,49 @@
+import { test } from '../fixtures/create-fixture';
+import { createCase, updateCase } from "../utils/api-helper";
+import caseData from '../caseData/mandatorySubmissionFields.json' with { type: "json" };
+import returnedCase from '../caseData/returnCase.json' with { type: "json" };
+import { CTSCUser, newSwanseaLocalAuthorityUserOne, HighCourtAdminUser } from "../settings/user-credentials";
+import { expect } from "@playwright/test";
+import { testConfig } from "../settings/test-config";
+import { setHighCourt } from '../utils/update-case-details';
+import { SubmitCase } from '../pages/submit-case';
+import { ReturnApplication } from '../pages/return-application';
+
+test.describe('Return application', () => {
+  const dateTime = new Date().toISOString();
+  let caseNumber: string;
+  let caseName: string;
+  test.beforeEach(async () => {
+    caseNumber = await createCase('e2e case', newSwanseaLocalAuthorityUserOne);
+
+  });
+
+  test('CTSC return Application',
+    async ({ page, signInPage, returnApplication }) => {
+      caseName = 'CTSC return Application ' + dateTime.slice(0, 10);
+      await updateCase(caseName, caseNumber, caseData);
+      await signInPage.visit();
+      await signInPage.login(CTSCUser.email, CTSCUser.password);
+      await signInPage.navigateTOCaseDetails(caseNumber);
+      await returnApplication.gotoNextStep('Return application');
+      await returnApplication.ReturnApplication();
+
+     // complete task
+      await returnApplication.tabNavigation('History');
+      await expect(page.getByText('Returned')).toBeVisible();
+      await signInPage.logout();
+
+    });
+
+  test('LA submit application',
+    async ({ page, signInPage, returnApplication }) => {
+      caseName = 'LA submit application ' + dateTime.slice(0, 10);
+      await updateCase(caseName, caseNumber, returnedCase);
+      await signInPage.visit();
+      await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+      await signInPage.navigateTOCaseDetails(caseNumber);
+      await page.getByRole('link', { name: 'Make changes to the respondents\' details' }).click();
+      await returnApplication.UpdateRespondent();
+      
+   });
+})
