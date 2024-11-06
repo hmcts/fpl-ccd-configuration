8cd044a284795aeb68207786f6d89ec1b28b1328
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2024-11-06 12:33:19 +0000

    Make all collections active and update about to start controller

--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/RaiseQueryController.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.enums.CaseRole;
+import uk.gov.hmcts.reform.fpl.enums.UserRole;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.service.UserService;
 
@@ -30,22 +31,25 @@ public class RaiseQueryController extends CallbackController {
     @PostMapping("/about-to-start")
     public AboutToStartOrSubmitCallbackResponse handleAboutToStart(@RequestBody CallbackRequest callbackRequest) {
         CaseDetails caseDetails = callbackRequest.getCaseDetails();
-
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolOne");
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolTwo");
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolThree");
-
         CaseData caseData = getCaseData(caseDetails);
 
-        Set<CaseRole> currentUserRoles = userService.getCaseRoles(caseData.getId());
-        log.info("Current logged-in user's case roles are: {}", currentUserRoles);
+        logAllQueryCollections(caseDetails);
 
-        initialiseRelevantQueryCollectionsForUser(caseDetails, currentUserRoles);
+        if (userService.isCafcassUser()) {
+            String cafcassCollection = "qmCaseQueriesCollectionCafcass";
+
+            log.info("Current logged-in user's case role is {}", UserRole.CAFCASS);
+            log.info("Query collection for user role {} is {}.", UserRole.CAFCASS, cafcassCollection);
+            caseDetails.getData().putIfAbsent(cafcassCollection, null);
+            log.info("Setting {} to value {}", cafcassCollection, caseDetails.getData().get(cafcassCollection));
+        } else {
+            Set<CaseRole> currentUserRoles = userService.getCaseRoles(caseData.getId());
+            log.info("Current logged-in user's case roles are: {}", currentUserRoles);
+            initialiseRelevantQueryCollectionsForUser(caseDetails, currentUserRoles);
+        }
 
         log.info("Final values of query collections: ");
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolOne");
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolTwo");
-        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolThree");
+        logAllQueryCollections(caseDetails);
 
         return respond(caseDetails);
     }
@@ -72,12 +76,78 @@ public class RaiseQueryController extends CallbackController {
         }
     }
 
+    private void logAllQueryCollections(CaseDetails caseDetails) {
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionCafcass");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionLASol");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionEPSManaging");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionLAManaging");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionLABarrister");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionLAShared");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionBarrister");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitor");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorA");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorB");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorC");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorD");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorE");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorA");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorG");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorH");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorI");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionSolicitorJ");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolOne");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolTwo");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolThree");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolD");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolE");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolF");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolG");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolH");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolI");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolJ");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolK");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolL");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolM");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolN");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionChildSolO");
+        logQueryCollection(caseDetails, "qmCaseQueriesCollectionCafcassSol");
+    }
+
     private static Map<CaseRole, String> initialiseCollectionMapping() {
         Map<CaseRole, String> collectionMapping = new LinkedHashMap<>();
+        collectionMapping.put(CaseRole.LASOLICITOR, "qmCaseQueriesCollectionLASol");
+        collectionMapping.put(CaseRole.EPSMANAGING, "qmCaseQueriesCollectionEPSManaging");
+        collectionMapping.put(CaseRole.LAMANAGING, "qmCaseQueriesCollectionLAManaging");
+        collectionMapping.put(CaseRole.LABARRISTER, "qmCaseQueriesCollectionLABarrister");
+        collectionMapping.put(CaseRole.LASHARED, "qmCaseQueriesCollectionLAShared");
+        collectionMapping.put(CaseRole.BARRISTER, "qmCaseQueriesCollectionBarrister");
+        collectionMapping.put(CaseRole.SOLICITOR, "qmCaseQueriesCollectionSolicitor");
+        collectionMapping.put(CaseRole.SOLICITORA, "qmCaseQueriesCollectionSolicitorA");
+        collectionMapping.put(CaseRole.SOLICITORB, "qmCaseQueriesCollectionSolicitorB");
+        collectionMapping.put(CaseRole.SOLICITORC, "qmCaseQueriesCollectionSolicitorC");
+        collectionMapping.put(CaseRole.SOLICITORD, "qmCaseQueriesCollectionSolicitorD");
+        collectionMapping.put(CaseRole.SOLICITORE, "qmCaseQueriesCollectionSolicitorE");
+        collectionMapping.put(CaseRole.SOLICITORF, "qmCaseQueriesCollectionSolicitorF");
+        collectionMapping.put(CaseRole.SOLICITORG, "qmCaseQueriesCollectionSolicitorG");
+        collectionMapping.put(CaseRole.SOLICITORH, "qmCaseQueriesCollectionSolicitorH");
+        collectionMapping.put(CaseRole.SOLICITORI, "qmCaseQueriesCollectionSolicitorI");
+        collectionMapping.put(CaseRole.SOLICITORJ, "qmCaseQueriesCollectionSolicitorJ");
         collectionMapping.put(CaseRole.CHILDSOLICITORA, "qmCaseQueriesCollectionChildSolOne");
         collectionMapping.put(CaseRole.CHILDSOLICITORB, "qmCaseQueriesCollectionChildSolTwo");
         collectionMapping.put(CaseRole.CHILDSOLICITORC, "qmCaseQueriesCollectionChildSolThree");
-        collectionMapping.put(CaseRole.LASOLICITOR, "qmCaseQueriesCollectionLASol");
+        collectionMapping.put(CaseRole.CHILDSOLICITORD, "qmCaseQueriesCollectionChildSolD");
+        collectionMapping.put(CaseRole.CHILDSOLICITORE, "qmCaseQueriesCollectionChildSolE");
+        collectionMapping.put(CaseRole.CHILDSOLICITORF, "qmCaseQueriesCollectionChildSolF");
+        collectionMapping.put(CaseRole.CHILDSOLICITORG, "qmCaseQueriesCollectionChildSolG");
+        collectionMapping.put(CaseRole.CHILDSOLICITORH, "qmCaseQueriesCollectionChildSolH");
+        collectionMapping.put(CaseRole.CHILDSOLICITORI, "qmCaseQueriesCollectionChildSolI");
+        collectionMapping.put(CaseRole.CHILDSOLICITORJ, "qmCaseQueriesCollectionChildSolJ");
+        collectionMapping.put(CaseRole.CHILDSOLICITORK, "qmCaseQueriesCollectionChildSolK");
+        collectionMapping.put(CaseRole.CHILDSOLICITORL, "qmCaseQueriesCollectionChildSolL");
+        collectionMapping.put(CaseRole.CHILDSOLICITORM, "qmCaseQueriesCollectionChildSolM");
+        collectionMapping.put(CaseRole.CHILDSOLICITORN, "qmCaseQueriesCollectionChildSolN");
+        collectionMapping.put(CaseRole.CHILDSOLICITORO, "qmCaseQueriesCollectionChildSolO");
+        collectionMapping.put(CaseRole.CAFCASSSOLICITOR, "qmCaseQueriesCollectionCafcassSol");
         return collectionMapping;
     }
 }
