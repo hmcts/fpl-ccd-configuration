5771f3fc3899a20b35911136acbe04969e661bdc
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-11-11 19:08:32 +0000

    DFPL-2537: Copy/modify IAC scripts to enable org roles in preview

---
+++ bin/add-role-assignments.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+dir=$(dirname ${0})
+
+jq -c '(.[])' service/src/cftlib/resources/cftlib-am-role-assignments.json | while read user; do
+  email=$(jq -r '.email' <<< $user)
+  idamId=$(jq -r '.id' <<< $user)
+
+  jq -c '(.roleAssignments[])' <<< $user | while read assignment; do
+    roleName=$(jq -r '.roleName' <<< $assignment)
+    roleCategory=$(jq -r '.roleCategory' <<< $assignment)
+    classification=$(jq -r '.classification' <<< $assignment)
+    readOnly=$(jq -r '.readOnly' <<< $assignment)
+    attributes=$(jq -r '.attributes | tostring' <<< $assignment)
+
+    authorisations=$(jq -r 'if .authorisations | length > 0 then "'"'"'{" + (.authorisations | join(",")) + "}'"'"'" else null end' <<< $assignment)
+
+    echo "Creating '${roleName}' assignment for user ${email}"
+    ${dir}/utils/organisational-role-assignment.sh $email ${SYSTEM_UPDATE_USER_PASSWORD} $classification $roleName $attributes $roleCategory $authorisations
+  done
+  echo
+done
