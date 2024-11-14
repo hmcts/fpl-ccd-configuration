d6d90f8805e6305c9c895bd37d8943d82562bfdc
prabhamuthu15 <praba.muthu@justice.gov.uk>
2024-11-14 14:39:26 +0000

    updateOrderDirectionSought

--- playwright-e2e/pages/orders-and-directions.ts
+++ playwright-e2e/pages/orders-and-directions.ts
@@ -1,26 +1,22 @@
 import { type Page, type Locator, expect } from "@playwright/test";
+import {BasePage} from "./base-page";
 
-export class OrdersAndDirectionSought {
+export class OrdersAndDirectionSought extends BasePage{
   readonly page: Page;
   readonly OrdersAndDirectionsHeading: Locator;
   readonly OrdersAndDirectionsSought: Locator;
   readonly WhichOrdersDoYouNeedCareOrder: Locator;
   readonly DoYouNeedAnyOtherDirectionsRadioNo: Locator;
   readonly WhichCourtAreYouIssuingFor: Locator;
-  readonly Continue: Locator;
-  readonly CheckYourAnswers: Locator;
-  readonly SaveAndContinue: Locator;
 
   public constructor(page: Page) {
+    super(page);
     this.page = page;
-    this.OrdersAndDirectionsHeading = page.getByRole("heading", {name: "Orders and directions needed"});
+    this.OrdersAndDirectionsHeading = page.getByRole("heading", {name: "Orders and directions sought",level:1});
     this.OrdersAndDirectionsSought = page.getByRole("link", { name: "Orders and directions sought" });
     this.WhichOrdersDoYouNeedCareOrder = page.getByLabel("Care order", { exact: true });
     this.DoYouNeedAnyOtherDirectionsRadioNo = page.getByRole('radio', { name: 'No' });
-    this.WhichCourtAreYouIssuingFor = page.getByRole('group', { name: 'Orders and directions needed' }).getByLabel('*Which court are you issuing');
-    this.Continue = page.getByRole('button', { name: 'Continue' });
-    this.CheckYourAnswers = page.getByRole('heading', { name: 'Check your answers' });
-    this.SaveAndContinue = page.getByRole('button', { name: 'Save and continue' });
+    this.WhichCourtAreYouIssuingFor = page.getByRole('group', { name: 'Orders and directions sought' }).getByLabel('Which court are you issuing');
   }
 
   async ordersAndDirectionsNeeded() {
@@ -28,9 +24,8 @@ export class OrdersAndDirectionSought {
     await expect(this.OrdersAndDirectionsHeading).toBeVisible();
     await this.WhichOrdersDoYouNeedCareOrder.check();
     await this.DoYouNeedAnyOtherDirectionsRadioNo.check();
-    await this.WhichCourtAreYouIssuingFor.selectOption('2: 117');
-    await this.Continue.click();
-    await expect(this.CheckYourAnswers).toBeVisible;
-    await this.SaveAndContinue.click();
+    await this.WhichCourtAreYouIssuingFor.selectOption('Barnet');
+    await this.clickContinue();
+    await this.checkYourAnsAndSubmit();
   }
 }
