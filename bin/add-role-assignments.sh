16f2eaf3683c3fac757da5f093f2894f1b60330c
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-11-14 19:29:42 +0000

    add south-west judge

--- bin/add-role-assignments.sh
+++ bin/add-role-assignments.sh
@@ -7,6 +7,11 @@ dir=$(dirname ${0})
 jq -c '(.[])' service/src/cftlib/resources/cftlib-am-role-assignments.json | while read user; do
   email=$(jq -r '.email' <<< $user)
   idamId=$(jq -r '.id' <<< $user)
+  password=${SYSTEM_UPDATE_USER_PASSWORD}
+
+  if [[ $email == *"ejudiciary"* ]]; then
+    password=${E2E_TEST_JUDGE_PASSWORD}
+  fi
 
   jq -c '(.roleAssignments[])' <<< $user | while read assignment; do
     roleName=$(jq -r '.roleName' <<< $assignment)
@@ -16,10 +21,10 @@ jq -c '(.[])' service/src/cftlib/resources/cftlib-am-role-assignments.json | whi
     readOnly=$(jq -r '.readOnly' <<< $assignment)
     attributes=$(jq -r '.attributes | tostring' <<< $assignment)
 
-    authorisations=$(jq -r 'if .authorisations | length > 0 then "'"'"'{" + (.authorisations | join(",")) + "}'"'"'" else null end' <<< $assignment)
+    authorisations=$(jq -r '.authorisations | tostring' <<< $assignment)
 
     echo "Creating '${roleName}' assignment for user ${email}"
-    ${dir}/utils/organisational-role-assignment.sh $email ${SYSTEM_UPDATE_USER_PASSWORD} $classification $roleName $attributes $roleCategory $authorisations $grantType
+    ${dir}/utils/organisational-role-assignment.sh $email $password $classification $roleName $attributes $roleCategory $authorisations $grantType
   done
   echo
 done
