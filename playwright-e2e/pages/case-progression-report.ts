1a5f9f93b6d1daa3f062a06420febf1a3ad90afe
Braimah101 <41795070+Braimah101@users.noreply.github.com>
2024-12-02 12:47:50 +0000

    DFPL-2577 (#5728)
    * DFPL-2577
    
    * DFPL-2577
    
    Made changes to fixtures
    
    * Update user-credentials.ts

---
+++ playwright-e2e/pages/case-progression-report.ts
@@ -0,0 +1,29 @@
+import { Locator, Page, expect } from "@playwright/test";
+import { BasePage } from "./base-page";
+
+export class CaseProgressionReort extends BasePage {
+    readonly caseProgressionReport: Locator;
+    readonly region: Locator;
+    readonly selectDfjArea: Locator;
+    readonly selectDfjCourt: Locator;
+    readonly type: Locator;
+
+    public constructor(page: Page) {
+        super(page);
+        this.caseProgressionReport = page.getByRole('heading', { name: 'Case progression report', exact: true });
+        this.region = page.getByText('Select region?');
+        this.selectDfjArea = page.locator('#londonDFJ');
+        this.selectDfjCourt = page.locator('#westLondonDFJCourts')
+        this.type = page.getByLabel('Select report type');
+    }
+
+    async CaseProgressionReport() {
+        await expect(this.caseProgressionReport).toBeVisible;
+        await this.region.selectOption('London');
+        await this.selectDfjArea.selectOption('West London');
+        await this.selectDfjCourt.selectOption('West London Family');
+        await this.type.selectOption('At risk')
+        await this.continueButton.click();
+        await this.clickSubmit();
+    }
+};
