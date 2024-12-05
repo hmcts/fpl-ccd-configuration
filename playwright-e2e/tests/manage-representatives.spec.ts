6a4ae229151301ad387e9f705264fac969d045c0
Braimah101 <41795070+Braimah101@users.noreply.github.com>
2024-12-05 09:38:53 +0000

    Update manage-representatives.spec.ts

--- playwright-e2e/tests/manage-representatives.spec.ts
+++ playwright-e2e/tests/manage-representatives.spec.ts
@@ -23,7 +23,6 @@ test.describe('Manage representatives', () => {
 
             await manageRepresentatives.gotoNextStep('Manage representatives');
             await manageRepresentatives.updateRepresentatives();
-            await manageRepresentatives.tabNavigation('People in the case')
             await page.getByText('Representatives 1').scrollIntoViewIfNeeded();
         })
     })
