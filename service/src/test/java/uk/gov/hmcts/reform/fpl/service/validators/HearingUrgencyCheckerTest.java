e84e1602d404b0263a60428e8e7707c1ae9c5289
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2024-12-09 18:40:37 +0000

    Update HearingUrgencyCheckerTest.java

--- service/src/test/java/uk/gov/hmcts/reform/fpl/service/validators/HearingUrgencyCheckerTest.java
+++ service/src/test/java/uk/gov/hmcts/reform/fpl/service/validators/HearingUrgencyCheckerTest.java
@@ -10,8 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import uk.gov.hmcts.reform.fpl.enums.YesNo;
 import uk.gov.hmcts.reform.fpl.enums.hearing.HearingUrgencyType;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
 import uk.gov.hmcts.reform.fpl.model.Hearing;
