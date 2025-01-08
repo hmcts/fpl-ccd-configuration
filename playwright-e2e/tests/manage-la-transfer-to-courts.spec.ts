a87f4229ba280960ab15ef508dcec39970ed4ca8
Charles Braimah <41795070+Braimah101@users.noreply.github.com>
2025-01-08 16:14:42 +0000

    DFPL-2566

--- playwright-e2e/tests/manage-la-transfer-to-courts.spec.ts
+++ playwright-e2e/tests/manage-la-transfer-to-courts.spec.ts
@@ -70,5 +70,9 @@ test.describe('Manage LAs / Transfer to court', () => {
             await manageLaTransferToCourts.tabNavigation('Summary');
             await expect(page.getByText('Family Court sitting at Swansea')).toBeHidden();
             await expect(page.getByText('Family Court sitting at West London')).toBeVisible();
-        })
+            await manageLaTransferToCourts.tabNavigation('People in the case');
+            await expect(page.getByText('Swansea City Council')).toBeHidden();
+            await expect(page.getByText('London Borough Hillingdon')).toBeVisible();
+
+    })
 });
