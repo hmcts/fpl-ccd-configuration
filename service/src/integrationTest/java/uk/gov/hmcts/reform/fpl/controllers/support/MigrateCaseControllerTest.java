aabb0ab50f55ad7cc2ef458ec7d7eb921c61b284
Qasim Nawaz <qasim.nawaz@justice.gov.uk>
2025-01-14 12:53:18 +0000

    Fix unused imports and update tests

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseControllerTest.java
@@ -2,37 +2,21 @@ package uk.gov.hmcts.reform.fpl.controllers.support;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import uk.gov.hmcts.reform.am.model.RoleCategory;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.AbstractCallbackTest;
-import uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle;
 import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Judge;
-import uk.gov.hmcts.reform.fpl.model.JudicialUser;
 import uk.gov.hmcts.reform.fpl.service.JudicialService;
-import uk.gov.hmcts.reform.fpl.utils.RoleAssignmentUtils;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static uk.gov.hmcts.reform.fpl.enums.JudgeCaseRole.ALLOCATED_JUDGE;
-import static uk.gov.hmcts.reform.fpl.enums.LegalAdviserRole.ALLOCATED_LEGAL_ADVISER;
 
 @WebMvcTest(MigrateCaseController.class)
 @OverrideAutoConfiguration(enabled = true)
 class MigrateCaseControllerTest extends AbstractCallbackTest {
-
-    @MockBean
-    private JudicialService judicialService;
 
     MigrateCaseControllerTest() {
         super("migrate-case");
@@ -61,57 +45,4 @@ class MigrateCaseControllerTest extends AbstractCallbackTest {
         givenSystemUser();
         givenFplService();
     }
-
-    @ParameterizedTest
-    @EnumSource(value = JudgeOrMagistrateTitle.class, names = {"LEGAL_ADVISOR"}, mode = EnumSource.Mode.EXCLUDE)
-    void shouldAttemptRoleAssignmentForAllocatedJudge(JudgeOrMagistrateTitle title) {
-        when(judicialService.getHearingJudgeRolesForMigration(any())).thenReturn(List.of());
-
-        CaseData caseData = CaseData.builder()
-            .allocatedJudge(Judge.builder()
-                .judgeTitle(title)
-                .judgeJudicialUser(JudicialUser.builder()
-                    .idamId("allocated-idam-id")
-                    .build())
-                .build())
-            .build();
-
-        postAboutToSubmitEvent(buildCaseDetails(caseData, "DFPL-2585Rollback"));
-
-        verify(judicialService).migrateJudgeRoles(List.of(RoleAssignmentUtils.buildRoleAssignment(
-            1L,
-            "allocated-idam-id",
-            ALLOCATED_JUDGE.getRoleName(),
-            RoleCategory.JUDICIAL,
-            any(),
-            null // no end date
-        )));
-    }
-
-    @Test
-    void shouldAttemptRoleAssignmentForAllocatedLegalAdvisor() {
-        when(judicialService.getHearingJudgeRolesForMigration(any())).thenReturn(List.of());
-
-        CaseData caseData = CaseData.builder()
-            .allocatedJudge(Judge.builder()
-                .judgeTitle(JudgeOrMagistrateTitle.LEGAL_ADVISOR)
-                .judgeJudicialUser(JudicialUser.builder()
-                    .idamId("allocated-idam-id")
-                    .build())
-                .build())
-            .build();
-
-        postAboutToSubmitEvent(buildCaseDetails(caseData, "DFPL-2585Rollback"));
-
-        verify(judicialService).migrateJudgeRoles(List.of(RoleAssignmentUtils.buildRoleAssignment(
-            1L,
-            "allocated-idam-id",
-            ALLOCATED_LEGAL_ADVISER.getRoleName(),
-            RoleCategory.LEGAL_OPERATIONS,
-            any(),
-            null // no end date
-        )));
-    }
-
-
 }
--- service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/controllers/support/MigrateCaseController.java
@@ -7,34 +7,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.hmcts.reform.am.model.RoleAssignment;
-import uk.gov.hmcts.reform.am.model.RoleCategory;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackRequest;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.fpl.controllers.CallbackController;
-import uk.gov.hmcts.reform.fpl.enums.State;
-import uk.gov.hmcts.reform.fpl.model.CaseData;
-import uk.gov.hmcts.reform.fpl.model.Judge;
 import uk.gov.hmcts.reform.fpl.service.CaseConverter;
 import uk.gov.hmcts.reform.fpl.service.JudicialService;
 import uk.gov.hmcts.reform.fpl.service.MigrateCaseService;
 import uk.gov.hmcts.reform.fpl.service.RoleAssignmentService;
 import uk.gov.hmcts.reform.fpl.service.orders.ManageOrderDocumentScopedFieldsCalculator;
-import uk.gov.hmcts.reform.fpl.utils.RoleAssignmentUtils;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.function.Consumer;
-
-import static org.apache.commons.lang3.ObjectUtils.isEmpty;
-import static uk.gov.hmcts.reform.fpl.enums.JudgeCaseRole.ALLOCATED_JUDGE;
-import static uk.gov.hmcts.reform.fpl.enums.JudgeOrMagistrateTitle.LEGAL_ADVISOR;
-import static uk.gov.hmcts.reform.fpl.enums.LegalAdviserRole.ALLOCATED_LEGAL_ADVISER;
 
 @Slf4j
 @RestController
