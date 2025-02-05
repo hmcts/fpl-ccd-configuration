0589cd4724dc43e74c3c3c7bf2bb5b6362bdeaac
Braimah101 <41795070+Braimah101@users.noreply.github.com>
2025-02-05 10:44:32 +0000

    Update urls.ts

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -10,8 +10,7 @@ export const urlConfig: UrlConfig = {
   idamUrl: process.env.IDAM_API_URL || `https://idam-api.${env}.platform.hmcts.net`,
   serviceUrl: process.env.CASE_SERVICE_URL || `http://fpl-case-service-${env}.service.core-compute-${env}.internal`,
   frontEndBaseURL: process.env.FE_BASE_URL || `https://manage-case.${env}.platform.hmcts.net`,
-  //serviceUrl: 'https://fpl-case-service-pr-5772.preview.platform.hmcts.net',
-  //frontEndBaseURL : 'https://xui-fpl-case-service-pr-5772.preview.platform.hmcts.net/cases'
+    
   // You can add other URLs as needed
 
 };
