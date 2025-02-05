8138181827a0d9b2dd1ab1db827f8270dc36fac5
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-02-05 16:48:24 +0000

    DFPL-2360: Rearrange

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/AbstractCallbackTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/AbstractCallbackTest.java
@@ -283,11 +283,6 @@ public abstract class AbstractCallbackTest extends AbstractTest {
         return postEvent(path, data, expectedStatus, AboutToStartOrSubmitCallbackResponse.class, userRoles);
     }
 
-    @SuppressWarnings("unchecked")
-    public Map<String, Object> postMetadataCallback(String path, CallbackRequest callbackRequest) {
-        return postEvent(path, toBytes(callbackRequest), SC_OK, Map.class);
-    }
-
     private <T> T postEvent(String path, byte[] data, int expectedStatus, Class<T> responseType, String... userRoles) {
         try {
             MvcResult response = mockMvc
@@ -310,6 +305,11 @@ public abstract class AbstractCallbackTest extends AbstractTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> postMetadataCallback(String path, CallbackRequest callbackRequest) {
+        return postEvent(path, toBytes(callbackRequest), SC_OK, Map.class);
     }
 
     private byte[] toBytes(Object o) {
