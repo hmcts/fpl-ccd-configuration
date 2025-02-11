176e54eaec614aac74f119db58f58009616ae762
Charles Braimah <41795070+Braimah101@users.noreply.github.com>
2025-02-11 08:29:31 +0000

    DFPL-2361
    Added changes to figma

--- playwright-e2e/pages/respondent-details.ts
+++ playwright-e2e/pages/respondent-details.ts
@@ -1,4 +1,4 @@
-import {type Page, type Locator, expect} from "@playwright/test";
+import { type Page, type Locator, expect } from "@playwright/test";
 
 export class RespondentDetails {
 
@@ -6,69 +6,62 @@ export class RespondentDetails {
   readonly respondentDetailsHeading: Locator;
   readonly firstName: Locator;
   readonly lastName: Locator;
-  readonly dobDay: Locator; //DOB (date of birth)
+  readonly dobDay: Locator;
   readonly dobMonth: Locator;
   readonly dobYear: Locator;
   readonly gender: Locator;
   readonly currentAddress: Locator;
+  readonly addressUnknown: Locator;
+  readonly giveMoreDetails: Locator;
   readonly telephone: Locator;
   readonly relationToChild: Locator;
-  readonly relationToChildContact: Locator; //corresponds to yes or no radio feature: 'Do you need contact details hidden from other parties? (Optional)'
-  readonly relationToChildContactReason: Locator;
-  readonly litigationCapacity: Locator; //ie Ability to take part in proceedings
-  readonly litigationCapacityReason: Locator;
+  readonly difficultyCapacity: Locator;
+  readonly difficultyCapacityReason: Locator;
   readonly legalRepresentation: Locator;
   readonly continue: Locator;
   readonly saveAndContinue: Locator;
   readonly addressNotKnownReason: Locator;
+  readonly confirmationCheckbox: Locator;
 
   public constructor(page: Page) {
     this.page = page;
     this.respondentDetailsHeading = page.getByRole("heading", { name: 'Respondents\' details' });
-    this.firstName = page.getByLabel('*First name (Optional)');
-    this.lastName = page.getByLabel('*Last name (Optional)');
+    this.firstName = page.getByLabel('First name', { exact: true });
+    this.lastName = page.getByLabel('Last name', { exact: true })
     this.dobDay = page.getByLabel('Day');
     this.dobMonth = page.getByLabel('Month');
     this.dobYear = page.getByLabel('Year');
-    this.gender = page.getByLabel('Gender (Optional)');
-    this.currentAddress = page.getByRole('group', { name: '*Current address known?' });
+    this.gender = page.getByLabel('What is the respondent\'s gender? (Optional)');
+    this.currentAddress = page.getByRole('group', { name: 'Current address known?' });
+    this.addressUnknown = page.getByRole('group', { name: 'Why is this address unknown?' });
     this.addressNotKnownReason = page.getByLabel('*Reason the address is not known');
+    this.giveMoreDetails = page.getByLabel('Give more details');
     this.telephone = page.getByRole('group', { name: 'Telephone (Optional)' }).locator('#respondents1_0_party_telephoneNumber_telephoneNumber');
-    this.relationToChild = page.getByLabel('*What is the respondent\'s relationship to the child or children in this case? (Optional)');
-    this.relationToChildContact = page.getByRole('group', { name: 'Do you need contact details hidden from other parties? (Optional)' });
-    this.relationToChildContactReason = page.getByLabel('Give reason (Optional)');
-    this.litigationCapacity = page.getByRole('group', { name: 'Do you believe this person will have problems with litigation capacity (understanding what\'s happening in the case)? (Optional)' });
-    this.litigationCapacityReason = page.getByLabel('Give details, including assessment outcomes and referrals to health services (Optional)');
-    this.legalRepresentation = page.getByRole('group', { name: '*Do they have legal representation? (Optional)' });
+    this.relationToChild = page.getByLabel('Which children does the respondent have parental responsibility for and what is their relationship?');
+    this.difficultyCapacity = page.getByRole('group', { name: 'Do you believe this person will have difficulty understanding what\'s happening with the case? (Optional)' });
+    this.difficultyCapacityReason = page.getByLabel('Give details, including assessment outcomes and referrals to health services (Optional)');
+    this.legalRepresentation = page.getByRole('group', { name: 'Do they have legal representation?' });
     this.continue = page.getByRole('button', { name: 'Continue' });
     this.saveAndContinue = page.getByRole('button', { name: 'Save and continue' });
+    this.confirmationCheckbox = page.getByLabel('I confirm that each person');
   }
 
   async respondentDetailsNeeded() {
-    await expect(this.respondentDetailsHeading).toBeVisible();
-    await this.firstName.click();
-    await this.firstName.fill('John');
-    await this.lastName.click();
-    await this.lastName.fill('Smith');
-    await this.dobDay.click();
-    await this.dobDay.fill('10');
-    await this.dobMonth.click();
-    await this.dobMonth.fill('11');
-    await this.dobYear.click();
-    await this.dobYear.fill('2001');
-    await this.gender.click(); //not sure if click needed
+    await expect.soft(this.respondentDetailsHeading).toBeVisible();
+    await this.confirmationCheckbox.check();
+    await this.firstName.fill('Tom');
+    await this.lastName.fill('Jones');
+    await this.dobDay.fill('01');
+    await this.dobMonth.fill('01');
+    await this.dobYear.fill('1990');
     await this.gender.selectOption('1: Male');
+    await this.currentAddress.getByLabel('No').click();
     await this.currentAddress.getByLabel('No').check();
-    await this.addressNotKnownReason.selectOption('2: Person deceased');
-    await this.telephone.fill('01234567890');
-    await this.relationToChild.click();
-    await this.relationToChild.fill('aunt');
-    await this.relationToChildContact.getByLabel('Yes').check();
-    await this.relationToChildContactReason.click();
-    await this.relationToChildContactReason.fill('this is the reason');
-    await this.litigationCapacity.getByLabel('Yes').check();
-    await this.litigationCapacityReason.click();
-    await this.litigationCapacityReason.fill('these are the details');
+    await this.addressUnknown.getByLabel('Whereabouts unknown').check();
+    await this.giveMoreDetails.fill('Test');
+    await this.relationToChild.fill('uncle');
+    await this.difficultyCapacity.getByLabel('Yes').check();
+    await this.difficultyCapacityReason.fill('test')
     await this.legalRepresentation.getByLabel('No').check();
     await this.continue.click();
     await this.saveAndContinue.click();
