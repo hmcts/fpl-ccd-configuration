908b1fc21b23e47c0536b3b62c86e5f5cd29757b
Chak Shing, Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2025-02-14 15:27:42 +0000

    fix notification using old hearing urgency field

--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/CafcassEmailContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/CafcassEmailContentProvider.java
@@ -46,7 +46,8 @@ public class CafcassEmailContentProvider extends SharedNotifyContentProvider {
             .collect(Collectors.joining("\n"));
 
         Optional<String> timeFrame = Optional.ofNullable(caseData.getHearing())
-            .map(Hearing::getTimeFrame)
+            .map(hearing -> nonNull(hearing.getHearingUrgencyType())
+                ? hearing.getHearingUrgencyType().getLabel() : hearing.getTimeFrame())
             .filter(StringUtils::isNotBlank);
 
         String eldestChildLastName = helper.getEldestChildLastName(caseData.getAllChildren());
--- service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/base/SharedNotifyContentProvider.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/service/email/content/base/SharedNotifyContentProvider.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
 import static org.apache.commons.lang3.StringUtils.uncapitalize;
 import static uk.gov.hmcts.reform.fpl.enums.YesNo.NO;
@@ -31,7 +32,8 @@ public abstract class SharedNotifyContentProvider extends AbstractEmailContentPr
         final Long caseId = caseData.getId();
         List<String> ordersAndDirections = buildOrdersAndDirections(caseData.getOrders());
         Optional<String> timeFrame = Optional.ofNullable(caseData.getHearing())
-            .map(Hearing::getTimeFrame)
+            .map(hearing -> nonNull(hearing.getHearingUrgencyType())
+                ? hearing.getHearingUrgencyType().getLabel() : hearing.getTimeFrame())
             .filter(StringUtils::isNotBlank);
 
         template.setOrdersAndDirections(ordersAndDirections);
