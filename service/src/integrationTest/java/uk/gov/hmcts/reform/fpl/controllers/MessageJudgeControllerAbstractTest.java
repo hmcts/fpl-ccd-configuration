49729f5d89dd6fe181863cedf1c92d20857b41cb
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-02-24 14:17:36 +0000

    Remove unnecessary tests for removed callback

--- service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/MessageJudgeControllerAbstractTest.java
+++ service/src/integrationTest/java/uk/gov/hmcts/reform/fpl/controllers/MessageJudgeControllerAbstractTest.java
@@ -19,15 +19,6 @@ public abstract class MessageJudgeControllerAbstractTest extends AbstractCallbac
             null, null);
     }
 
-    protected DynamicList buildRecipientDynamicListCTSCLocalCourtOther() {
-        return buildRecipientDynamicList(
-            List.of(JudicialMessageRoleType.CTSC.toString(),
-                JudicialMessageRoleType.LOCAL_COURT_ADMIN.toString(),
-                JudicialMessageRoleType.OTHER.toString()),
-            null, null);
-    }
-
-
     protected DynamicList buildRecipientDynamicList(List<String> codesToInclude,
                                           String allocatedLabel,
                                           String hearingLabel) {
