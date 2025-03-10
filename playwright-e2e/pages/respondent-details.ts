e682edfc0ed56fea6d8fae58e2d916bf1333ad09
GauravChawlaMOJ <gaurav.chawla@justice.gov.uk>
2025-03-10 16:01:17 +0000

    changes

--- playwright-e2e/pages/respondent-details.ts
+++ playwright-e2e/pages/respondent-details.ts
@@ -32,26 +32,24 @@ export class RespondentDetails {
   readonly legalRepresentation: Locator;
   readonly continue: Locator;
   readonly saveAndContinue: Locator;
-  readonly addressNotKnownReason: Locator;
   readonly confirmationCheckbox: Locator;
   readonly addNew: Locator;
 
   public constructor(page: Page) {
     this.page = page;
     this.respondentDetailsHeading = page.getByRole("heading", { name: 'Respondents\' details' });
-    this.firstName = page.getByLabel('First name', { exact: true });
-    this.lastName = page.getByLabel('Last name', { exact: true })
+    this.firstName = page.getByLabel('*First name (Optional)', { exact: true });
+    this.lastName = page.getByLabel('*Last name (Optional)', { exact: true })
     this.dobDay = page.getByLabel('Day');
     this.dobMonth = page.getByLabel('Month');
     this.dobYear = page.getByLabel('Year');
-    this.gender = page.getByLabel('What is the respondent\'s gender? (Optional)');
-    this.currentAddress = page.getByRole('group', { name: 'Current address known?' });
-    this.addressUnknown = page.getByRole('group', { name: 'Why is this address unknown?' });
-    this.addressNotKnownReason = page.getByLabel('*Reason the address is not known');
+    this.gender = page.getByLabel('Gender (Optional)');
+    this.currentAddress = page.getByRole('group', { name: '*Current address known?' });
+    this.addressUnknown = page.getByLabel('*Reason the address is not');
     this.giveMoreDetails = page.getByLabel('Give more details');
     this.telephone = page.getByRole('group', { name: 'Telephone (Optional)' }).locator('#respondents1_0_party_telephoneNumber_telephoneNumber');
-    this.relationToChild = page.getByLabel('Which children does the respondent have parental responsibility for and what is their relationship?');
-    this.difficultyCapacity = page.getByRole('group', { name: 'Do you believe this person will have difficulty understanding what\'s happening with the case? (Optional)' });
+    this.relationToChild = page.getByLabel('*What is the respondent\'s relationship to the child or children in this case? (Optional)');
+    this.difficultyCapacity = page.getByLabel('Do you believe this person will have problems with litigation capacity (understanding what\'s happening in the case)? (Optional)' );
     this.difficultyCapacityReason = page.getByLabel('Give details, including assessment outcomes and referrals to health services (Optional)');
     this.legalRepresentation = page.getByRole('group', { name: 'Do they have legal representation?' });
     this.addNew = page.getByRole('button', { name: 'Add new' }).nth(1);
@@ -74,17 +72,18 @@ export class RespondentDetails {
 
   async respondentDetailsNeeded() {
     await expect.soft(this.respondentDetailsHeading).toBeVisible();
-    await this.confirmationCheckbox.check();
+    //await this.confirmationCheckbox.check();
     await this.firstName.fill('Tom');
     await this.lastName.fill('Jones');
     await this.dobDay.fill('01');
     await this.dobMonth.fill('01');
     await this.dobYear.fill('1990');
     await this.gender.selectOption('1: Male');
-    await this.currentAddress.getByLabel('No').click();
-    await this.currentAddress.getByLabel('No').check();
-    await this.addressUnknown.getByLabel('Whereabouts unknown').check();
-    await this.giveMoreDetails.fill('Test');
+    //await this.page.getByRole('group', { name: '*Current address known?' }).getByLabel('No').check();
+    //await this.currentAddress.getByLabel('No').check();
+   // await this.currentAddress.getByLabel('No').click({force: true});
+    await this.addressUnknown.selectOption('1: No fixed abode');
+   // await this.giveMoreDetails.fill('Test');
     await this.relationToChild.fill('uncle');
     await this.difficultyCapacity.getByLabel('Yes').check();
     await this.difficultyCapacityReason.fill('test')
