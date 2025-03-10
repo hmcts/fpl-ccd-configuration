f36552b781dd334f431c4df731985efc46f3f71c
Charles Braimah <41795070+Braimah101@users.noreply.github.com>
2025-03-10 11:52:40 +0000

    DFPL-5831

--- playwright-e2e/pages/respondent-details.ts
+++ playwright-e2e/pages/respondent-details.ts
@@ -1,19 +1,31 @@
-import { type Page, type Locator, expect } from "@playwright/test";
+import { type Page, type Locator, expect } from "@playwright/test"
 
 export class RespondentDetails {
 
   readonly page: Page;
   readonly respondentDetailsHeading: Locator;
   readonly firstName: Locator;
+  readonly respondentFirstName: Locator;
   readonly lastName: Locator;
+  readonly respondentLastName: Locator;
   readonly dobDay: Locator;
+  readonly respondentdobDay: Locator;
+  readonly respondentdobMonth: Locator;
+  readonly respondentdobYear: Locator;
   readonly dobMonth: Locator;
   readonly dobYear: Locator;
   readonly gender: Locator;
+  readonly respondent2Gender: Locator;
+  readonly respondent2CurrentAddressKnown: Locator;
+  readonly respondent2AddressUnknown: Locator;
+  readonly respondet2RelationshipToChild: Locator;
   readonly currentAddress: Locator;
   readonly addressUnknown: Locator;
   readonly giveMoreDetails: Locator;
   readonly telephone: Locator;
+  readonly respondent2DifficultyUnderstandingCapacity: Locator;
+  readonly respondent2DificultyCapacityReason: Locator;
+  readonly respondent2HiddenNumber: Locator;
   readonly relationToChild: Locator;
   readonly difficultyCapacity: Locator;
   readonly difficultyCapacityReason: Locator;
@@ -22,6 +34,7 @@ export class RespondentDetails {
   readonly saveAndContinue: Locator;
   readonly addressNotKnownReason: Locator;
   readonly confirmationCheckbox: Locator;
+  readonly addNew: Locator;
 
   public constructor(page: Page) {
     this.page = page;
@@ -41,9 +54,22 @@ export class RespondentDetails {
     this.difficultyCapacity = page.getByRole('group', { name: 'Do you believe this person will have difficulty understanding what\'s happening with the case? (Optional)' });
     this.difficultyCapacityReason = page.getByLabel('Give details, including assessment outcomes and referrals to health services (Optional)');
     this.legalRepresentation = page.getByRole('group', { name: 'Do they have legal representation?' });
+    this.addNew = page.getByRole('button', { name: 'Add new' }).nth(1);
     this.continue = page.getByRole('button', { name: 'Continue' });
     this.saveAndContinue = page.getByRole('button', { name: 'Save and continue' });
     this.confirmationCheckbox = page.getByLabel('I confirm that each person');
+    this.respondentFirstName = page.locator('#respondents1_1_party_firstName');
+    this.respondentLastName = page.locator('#respondents1_1_party_lastName');
+    this.respondentdobDay = page.locator('#respondents1_1_party_party #dateOfBirth-day');
+    this.respondentdobMonth = page.locator('#respondents1_1_party_party #dateOfBirth-month');
+    this.respondentdobYear = page.locator('#respondents1_1_party_party #dateOfBirth-year');
+    this.respondent2Gender = page.locator('#respondents1_1_party_gender');
+    this.respondent2CurrentAddressKnown = page.locator('#respondents1_1_party_addressKnow-No');
+    this.respondent2AddressUnknown = page.locator('[id="respondents1_1_party_addressNotKnowReason-No\\ fixed\\ abode"]');
+    this.respondent2HiddenNumber = page.locator('#respondents1_1_party_hideTelephone_No');
+    this.respondet2RelationshipToChild = page.locator('#respondents1_1_party_relationshipToChild');
+    this.respondent2DifficultyUnderstandingCapacity = page.locator('#respondents1_1_party_litigationIssues-NO');
+    this.respondent2DificultyCapacityReason = page.locator('#respondents1_1_legalRepresentation_No')
   }
 
   async respondentDetailsNeeded() {
@@ -63,6 +89,20 @@ export class RespondentDetails {
     await this.difficultyCapacity.getByLabel('Yes').check();
     await this.difficultyCapacityReason.fill('test')
     await this.legalRepresentation.getByLabel('No').check();
+    await this.addNew.click();
+    await this.respondentFirstName.fill('Thierry');
+    await this.respondentLastName.fill('Jordan');
+    await this.respondentdobDay.fill('31');
+    await this.respondentdobMonth.fill('03');
+    await this.respondentdobYear.fill('1980');
+    await this.respondent2Gender.selectOption('1: Male');
+    await this.respondent2CurrentAddressKnown.click();
+    await this.respondent2AddressUnknown.click();
+    await this.respondent2HiddenNumber.click();
+    await this.respondet2RelationshipToChild.fill('uncle');
+    await this.respondent2DifficultyUnderstandingCapacity.click();
+    await this.respondent2DificultyCapacityReason.click();
+    //await this.page.waitForTimeout(1000);
     await this.continue.click();
     await this.saveAndContinue.click();
   }
