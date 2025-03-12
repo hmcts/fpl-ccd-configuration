ba361b3b48ec55b13889dd612b519256c20bc67a
jamesrferguson1 <james.ferguson1@justice.gov.uk>
2025-03-12 15:16:34 +0000

    Adding some extra logging to NoC event handler.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/NoticeOfChangeThirdPartyEventHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/NoticeOfChangeThirdPartyEventHandler.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.fpl.handlers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -13,6 +14,7 @@ import uk.gov.hmcts.reform.fpl.service.email.content.NoticeOfChangeContentProvid
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.NOTICE_OF_CHANGE_FORMER_REPRESENTATIVE;
 import static uk.gov.hmcts.reform.fpl.NotifyTemplates.NOTICE_OF_CHANGE_NEW_REPRESENTATIVE;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class NoticeOfChangeThirdPartyEventHandler {
@@ -26,6 +28,7 @@ public class NoticeOfChangeThirdPartyEventHandler {
         NotifyData notifyData = noticeOfChangeContentProvider.buildNoticeOfChangeThirdPartySolicitorTemplate(
             event.getCaseData());
 
+        log.info("Sending email to new third party solicitor {} with access granted.", event.getNewThirdPartyOrg().getEmail());
         notificationService.sendEmail(NOTICE_OF_CHANGE_NEW_REPRESENTATIVE, event.getNewThirdPartyOrg().getEmail(),
             notifyData, event.getCaseData().getId());
     }
@@ -36,6 +39,7 @@ public class NoticeOfChangeThirdPartyEventHandler {
         NotifyData notifyData = noticeOfChangeContentProvider.buildNoticeOfChangeThirdPartySolicitorTemplate(
             event.getCaseData());
 
+        log.info("Sending email to old third party solicitor {} with access removed.", event.getOldThirdPartyOrg().getEmail());
         notificationService.sendEmail(
             NOTICE_OF_CHANGE_FORMER_REPRESENTATIVE,
             event.getOldThirdPartyOrg().getEmail(),
