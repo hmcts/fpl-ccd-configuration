cfb1aefc92ec492cefb558e434881c2e1437d78d
Chak Shing, Lo <ChakShing.Lo@justice.gov.uk>
2025-05-14 09:37:26 +0100

    try fix pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,9 @@ export default defineConfig({
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [[process.env.CI ? 'html' : 'list'],
-             ['html', { outputFolder: '../test-results/functionalTest' }]],
+             ['html', { outputFolder: '../test-results/functionalTest' }],
+             ['junit', { outputFolder: '../test-results/functionalTest' }]
+  ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
