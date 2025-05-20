e2bbd87d982ad8eeabc467d58ab8990eb9f1d2b8
Braimah101 <charlesbraimah@googlemail.com>
2025-05-20 08:49:18 +0100

    DFPL-2385

--- playwright-e2e/settings/urls.ts
+++ playwright-e2e/settings/urls.ts
@@ -1,8 +1,8 @@
 // aat, demo, perftest, ithc
 const env = process.env.ENVIRONMENT || "aat";
-const e2ePw: string = process.env.E2E_TEST_PASSWORD || "";
-const defaultPwd: string = process.env.SYSTEM_UPDATE_USER_PASSWORD || "";
-const judgePwd: string = process.env.E2E_TEST_JUDGE_PASSWORD || "";
+const e2ePw: string = process.env.E2E_TEST_PASSWORD || "Password1234";
+const defaultPwd: string = process.env.SYSTEM_UPDATE_USER_PASSWORD || "Password12";
+const judgePwd: string = process.env.E2E_TEST_JUDGE_PASSWORD || "Hmcts1234";
 
 interface UrlConfig {
   [key: string]: string;
