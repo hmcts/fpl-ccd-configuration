38d25986d7a6ca626159c93178a550d3ea7f9b52
prabha15 <prabha.muthu@justice.gov.uk>
2025-07-15 10:53:45 +0100

    FollowUpquery

--- playwright-e2e/pages/query-management.ts
+++ playwright-e2e/pages/query-management.ts
@@ -12,7 +12,7 @@ export class QueryManagement extends BasePage {
     respondDetail: Locator;
     addDocument: Locator;
     documentInput: Locator;
-    backToCaseDetails: Locator;
+    backToCaseDetailsLink: Locator;
 
 
     public constructor(page: Page) {
@@ -26,7 +26,7 @@ export class QueryManagement extends BasePage {
         this.respondDetail = page.getByRole('textbox', {name: 'Response detail'});
         this.addDocument = page.getByRole('button', {name: 'Add new'});
         this.documentInput = page.locator('#documentCollection_value');
-        this.backToCaseDetails = page.getByRole('link', {name: 'Go back to the case'});
+        this.backToCaseDetailsLink = page.getByRole('link', {name: 'Go back to the case'});
 
 
     }
@@ -53,31 +53,39 @@ export class QueryManagement extends BasePage {
         await this.page.getByText('Assign to me').click();
     }
 
-    async respondToQuery(closeTheQuery: boolean = true) {
+    async goBackToCaseDetails() {
+        await this.backToCaseDetailsLink.click();
+    }
 
+    async askFollowupQuery() {
+        await this.page.getByRole('link', {name: 'query by LA'}).click();
+        await expect(this.page.getByText('Query details')).toBeVisible();
+        await expect(this.page.getByText('Response', {exact: true})).toBeVisible();
+        await this.page.getByRole('button', {name: 'Ask a follow-up question'}).click();
+        await expect.soft(this.page.getByText('Response', {exact: true})).toBeVisible();
+        await expect.soft(this.page.getByRole('cell', {name: 'Yes, the venue of the hearing'})).toBeVisible();
+        await expect.soft(this.page.locator('ccd-query-details')).toContainText('Yes, the venue of the hearing on 12/5/2026, the center has access to wheelchairs chair access');
+        await this.page.getByRole('textbox', {name: 'Query Body'}).fill('Needed translater  for German language for the next hearing');
+
+    }
+
+    async respondToQuery(closeTheQuery: boolean = false) {
         await this.page.getByRole('link', {name: 'Respond to a query'}).click();
-        // await expect(page.getByRole('caption').getByText('Query details')).toBeVisible();
         await this.respondDetail.fill('Answering to the query raised');
         await this.addDocument.click();
-        await expect.soft(this.page.getByRole('checkbox', { name: 'I want to close this query' })).toBeVisible();
+        await expect.soft(this.page.getByRole('checkbox', {name: 'I want to close this query'})).toBeVisible();
 
-if (closeTheQuery) {
-    //
-    await this.page.getByRole('checkbox', {name: 'I want to close this query'}).check();
-}
-         await expect.soft(this.page.getByText('Closing the query means the parties can no longer send message in this thread.')).toBeVisible();
-
-
-
+        if (closeTheQuery) {
+            await this.page.getByRole('checkbox', {name: 'I want to close this query'}).check();
+        }
+        await expect.soft(this.page.getByText('Closing the query means the parties can no longer send message in this thread.')).toBeVisible();
         await expect.soft(this.page.getByText('Only attach documents related')).toBeVisible();
         await this.documentInput.setInputFiles(config.testWordFile);
         await this.clickContinue();
         await this.clickSubmit();
         await expect(this.page.getByRole('heading', {name: 'Query response submitted'})).toBeVisible();
         await expect(this.page.getByText('This query response has been added to the case')).toBeVisible();
-
-
-        await this.backToCaseDetails.click();
+        await this.backToCaseDetailsLink.click();
 
     }
 }
--- playwright-e2e/tests/query-management.spec.ts
+++ playwright-e2e/tests/query-management.spec.ts
@@ -11,6 +11,7 @@ import caseWithChildrenCafcassSolicitorDemo
     from '../caseData/caseWithMultipleChildCafcassSolicitorDemo.json' assert {type: "json"};
 import caseWithChildrenCafcassSolicitor
     from '../caseData/caseWithMultipleChildCafcassSolicitor.json' assert {type: "json"};
+import caseWtihQuery from '../caseData/caseWithQuery.json' assert {type: "json"};
 
 test.describe('Query management', () => {
     const dateTime = new Date().toISOString();
@@ -20,7 +21,7 @@ test.describe('Query management', () => {
         caseNumber = await createCase('e2e case', newSwanseaLocalAuthorityUserOne);
     });
 
-    test.only
+    test
     ('LA raise query',
         async ({
                    page, signInPage, queryManagement, caseFileView
@@ -32,11 +33,11 @@ test.describe('Query management', () => {
             } else {
                 await updateCase(caseName, caseNumber, caseWithChildrenCafcassSolicitor);
             }
+
             await giveAccessToCase(caseNumber, privateSolicitorOrgUser, '[CHILDSOLICITORA]');
             await signInPage.visit();
             await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
             await signInPage.navigateTOCaseDetails(caseNumber);
-
             await queryManagement.gotoNextStep('Raise a new query');
             await expect.soft(queryManagement.page.getByText('Access issues or adding a new user')).toBeVisible();
             await expect.soft(queryManagement.page.getByText('Make a change of representation on a case (notice of change)')).toBeVisible();
@@ -51,12 +52,10 @@ test.describe('Query management', () => {
             await queryManagement.clickSubmit();
             await expect(page.getByRole('heading', {name: 'Query submitted'})).toBeVisible();
             await expect(page.getByText('Your query has been sent to HMCTS')).toBeVisible();
-
-
             await expect.soft(page.getByText('Our team will read your query and respond.')).toBeVisible();
             await expect.soft(page.getByText('When the response is available it will be added to the \'Queries\' section')).toBeVisible();
 
-            await queryManagement.page.getByRole('link', {name: 'Go back to the case'}).click();
+            await queryManagement.goBackToCaseDetails();
             await queryManagement.tabNavigation('Queries');
 
             await expect(page.getByRole('columnheader', {name: 'Query subject'})).toBeVisible();
@@ -68,6 +67,7 @@ test.describe('Query management', () => {
             await expect(page.getByRole('cell', {name: 'Local Authority '})).toBeVisible();
             await expect(page.getByRole('cell', {name: 'Awaiting Response'})).toBeVisible();
             await expect(page.getByRole('cell', {name: `${queryManagement.getCurrentDate()}`})).toBeVisible();
+
             await queryManagement.tabNavigation('Case File View');
             await caseFileView.openFolder('Uncategorised');
             await expect(caseFileView.page.getByText('testWordDoc.docx')).toBeVisible();
@@ -96,7 +96,55 @@ test.describe('Query management', () => {
             await queryManagement.page.getByRole('link', {name: 'Birth certificate format'}).click();
             await expect(page.getByText('Response', {exact: true})).toBeVisible();
             await expect(page.getByText('Answering to the query raised')).toBeVisible();
-            await expect(page.getByRole('cell', { name: 'Closed' })).toBeVisible();
+            await expect(page.getByRole('cell', {name: 'Closed'})).toBeVisible();
 
         });
+    test('LA raise follow up query',
+        async ({
+                   page, signInPage, queryManagement, caseFileView
+
+               }) => {
+            caseName = 'LA raise a follow up query ' + dateTime.slice(0, 10);
+
+            await updateCase(caseName, caseNumber, caseWtihQuery);
+            await giveAccessToCase(caseNumber, privateSolicitorOrgUser, '[CHILDSOLICITORA]');
+            await signInPage.visit();
+            await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password);
+            await signInPage.navigateTOCaseDetails(caseNumber);
+            await queryManagement.tabNavigation('Queries');
+            await queryManagement.askFollowupQuery();
+            await queryManagement.clickContinue();
+            await queryManagement.clickSubmit();
+            await expect(page.getByRole('heading', {name: 'Query submitted'})).toBeVisible();
+            await expect(page.getByText('Your query has been sent to HMCTS')).toBeVisible();
+            await queryManagement.goBackToCaseDetails();
+            await queryManagement.tabNavigation('Queries');
+            await expect(page.getByText('Awaiting Response')).toBeVisible();
+
+            await queryManagement.clickSignOut();
+            await signInPage.login(privateSolicitorOrgUser.email, privateSolicitorOrgUser.password);
+            await signInPage.navigateTOCaseDetails(caseNumber);
+
+            await queryManagement.tabNavigation('Queries');
+            await expect(page.getByRole('link', {name: 'Birth certificate format'})).toBeHidden();
+
+
+            await queryManagement.clickSignOut();
+            await signInPage.login(CTSCUser.email, CTSCUser.password);
+            await signInPage.navigateTOCaseDetails(caseNumber);
+            await queryManagement.tabNavigation('Tasks');
+            await queryManagement.waitForTask('Respond to a Query');
+            await queryManagement.assignToMe();
+            await page.pause();
+            await queryManagement.respondToQuery(true);
+            await queryManagement.tabNavigation('Queries');
+
+            await expect(page.getByText('Responded')).toBeVisible();
+            await queryManagement.page.getByRole('link', {name: 'Birth certificate format'}).click();
+            await expect(page.getByText('Response', {exact: true})).toBeVisible();
+            await expect(page.getByText('Answering to the query raised')).toBeVisible();
+            await expect(page.getByRole('cell', {name: 'Closed'})).toBeVisible();
+
+        });
+
 })
