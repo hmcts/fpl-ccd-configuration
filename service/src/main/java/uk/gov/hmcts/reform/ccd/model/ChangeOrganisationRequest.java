f21332ec093e9bed67e4ba463dcb38b940bf3625
james.ferguson <james.ferguson1@justice.gov.uk>
2025-07-25 15:05:29 +0100

    Add NON_NULL annotation above new createdBy on ChangeOrganisationRequest - was causing data validation error on LastNoCRequestedBy if not present.

--- service/src/main/java/uk/gov/hmcts/reform/ccd/model/ChangeOrganisationRequest.java
+++ service/src/main/java/uk/gov/hmcts/reform/ccd/model/ChangeOrganisationRequest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.ccd.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
@@ -37,6 +38,7 @@ public class ChangeOrganisationRequest {
     private ChangeOrganisationApprovalStatus approvalStatus;
 
     @JsonProperty("CreatedBy")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String createdBy;
 
     @JsonIgnore
