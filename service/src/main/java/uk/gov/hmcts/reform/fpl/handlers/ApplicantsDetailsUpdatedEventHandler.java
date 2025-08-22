436c71514dcaa9ec5afb6a0a40a0d524b7ac3c45
hfilmore <hannah.filmore@justice.gov.uk>
2025-08-22 15:08:47 +0100

    DFPL-2692 don't exclude LA recipients

--- service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/ApplicantsDetailsUpdatedEventHandler.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/handlers/ApplicantsDetailsUpdatedEventHandler.java
@@ -29,8 +29,6 @@ public class ApplicantsDetailsUpdatedEventHandler {
         final CaseData caseData = event.getCaseData();
         final RecipientsRequest recipientsRequest = RecipientsRequest.builder()
             .caseData(caseData)
-            .secondaryLocalAuthorityExcluded(true) //???
-            .legalRepresentativesExcluded(true) //???
             .build();
 
         final Set<String> recipients = localAuthorityRecipients.getRecipients(recipientsRequest);
