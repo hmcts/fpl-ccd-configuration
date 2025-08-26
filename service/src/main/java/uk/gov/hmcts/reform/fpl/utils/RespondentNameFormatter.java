e56143be1d8a109388492a80d92918bec6a97d2b
james.ferguson <james.ferguson1@justice.gov.uk>
2025-08-26 15:42:48 +0100

    Refactor formatRespondentNames.

--- service/src/main/java/uk/gov/hmcts/reform/fpl/utils/RespondentNameFormatter.java
+++ service/src/main/java/uk/gov/hmcts/reform/fpl/utils/RespondentNameFormatter.java
@@ -6,11 +6,13 @@ public class RespondentNameFormatter {
     private RespondentNameFormatter() {}
 
     public static String formatRespondentNames(List<String> names) {
-        return switch (names.size()) {
-            case 1 -> names.get(0) + " is";
-            case 2 -> names.get(0) + " and " + names.get(1) + " are";
-            default -> String.join(", ", names.subList(0, names.size() - 1))
-                    + " and " + names.get(names.size() - 1) + " are";
-        };
+        if (names.size() == 1) {
+            return names.get(0) + " is";
+        }
+        if (names.size() == 2) {
+            return names.get(0) + " and " + names.get(1) + " are";
+        }
+        return String.join(", ", names.subList(0, names.size() - 1) + " and "
+            + names.get(names.size() - 1) + " are");
     }
 }
