bec293d492696262d0316ccfa6fdc8fe7aaeb9bb
prabha15 <prabha.muthu@justice.gov.uk>
2025-09-12 08:52:50 +0100

    InPreviewTestInEdge

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
 
         {
             name: "preview",
-            use: {...devices["Desktop Firefox"]},
+            use: {...devices["desktop edge"], viewport: {width: 1280, height: 1024}},
             retries: 3,
             timeout: 3 * 60 * 1000,
             expect: {timeout: 1 * 60 * 1000},
