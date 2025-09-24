1cd185fd30e72e733799c824cf8e2e13d6d359c3
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-09-24 11:30:06 +0100

    Smoke test reconstruction completed

--- playwright-e2e/tests/smoke-test.spec.ts
+++ playwright-e2e/tests/smoke-test.spec.ts
@@ -3,7 +3,153 @@ import { newSwanseaLocalAuthorityUserOne, privateSolicitorOrgUser } from "../set
 import { CreateCaseName } from "../utils/create-case-name";
 import { CaseFileView } from "../pages/case-file-view";
 
+test.slow();
+
 test.describe('Smoke Test @xbrowser', () => {
+
+    test('Local Authority C110A application submission', async ({
+                                                                    signInPage,
+        page,
+        createCase,
+        startApplication,
+        ordersAndDirectionSought,
+        groundsForTheApplication,
+        hearingUrgency,
+        addApplicationDocuments,
+        applicantDetails,
+        childDetails,
+        respondentDetails,
+        allocationProposal,
+        submitCase,
+        makeAxeBuilder
+                                                                }, testInfo) => {
+        test.info().annotations.push({ type: 'tag', description: 'xbrowser' });
+        test.info().annotations.push({ type: 'tag', description: 'smoke-test' });
+        test.info().annotations.push({ type: 'tag', description: 'accessibility' });
+
+        await test.step('Login and create Case', async () => {
+            await signInPage.visit();
+            await signInPage.login(
+                newSwanseaLocalAuthorityUserOne.email,
+                newSwanseaLocalAuthorityUserOne.password,
+            );
+            await expect(signInPage.signoutButton).toBeVisible();
+            await createCase.createCase();
+        });
+
+        await test.step('Submit Case', async () => {
+            await createCase.submitCase()
+        });
+
+        await test.step('Fill orders and direction', async () => {
+            await ordersAndDirectionSought.ordersAndDirectionsNeeded();
+        });
+
+        await test.step('Hearing urgency', async() => {
+
+            await startApplication.hearingUrgency();
+            await hearingUrgency.hearingUrgency();
+        });
+
+        await test.step('Grounds for the application', async() => {
+            await startApplication.groundsForTheApplication();
+            await groundsForTheApplication.checkChildBeyondParentalControl();
+            await groundsForTheApplication.checkNoProvideSummaryRadioButton();
+            await groundsForTheApplication.fillSummary('test');
+            await Promise.all([
+                page.waitForResponse(response =>
+                    response.url().includes('CARE_SUPERVISION_EPO/validate?pageId=enterGrounds1') &&
+                    response.request().method() === 'POST' &&
+                    response.status() === 200
+                ),
+                groundsForTheApplication.continueButton.click()
+            ]);
+            await Promise.all([
+                page.waitForResponse(response =>
+                    response.url().includes('/api/wa-supported-jurisdiction/get') &&
+                    response.request().method() === 'GET' &&
+                    response.status() === 200
+                ),
+                groundsForTheApplication.saveAndContinueButton.click()
+            ]);
+        });
+
+        await test.step('Upload documents', async() => {
+            await startApplication.addApplicationDocuments();
+            await addApplicationDocuments.uploadDocumentSmokeTest();
+        });
+
+        await test.step('Applicant details', async() => {
+            await Promise.all([
+                page.waitForResponse(response =>
+                    !!response.url().match(/\/event-triggers\/enterApplicantDetailsLA/) &&
+                    response.request().method() === 'GET' &&
+                    response.status() === 200
+                ),
+                startApplication.applicantDetails()
+            ]);
+            await applicantDetails.applicantDetailsNeeded();
+        });
+
+        await test.step('Child details', async() => {
+            await Promise.all([
+                page.waitForResponse(response =>
+                    response.url().includes('enterChildren') &&
+                    response.request().method() === 'GET' &&
+                    response.status() === 200
+                ),
+                startApplication.childDetails()
+            ]);
+
+            await childDetails.addChildDetailsForC110AApplication();
+        });
+
+        await test.step('Add respondent details', async() => {
+            await Promise.all([
+                page.waitForResponse(response =>
+                    response.url().includes('enterRespondents') &&
+                    response.request().method() === 'GET' &&
+                    response.status() === 200
+                ),
+                startApplication.respondentDetails()
+            ]);
+            await respondentDetails.respondentDetailsNeeded();
+        });
+
+        await test.step('Allocation proposal', async() => {
+           await Promise.all([
+               page.waitForResponse(response =>
+                   response.url().includes('otherProposal') &&
+                   response.request().method() === 'GET' &&
+                   response.status() === 200
+               ),
+               startApplication.allocationProposal()
+           ]);
+           await allocationProposal.allocationProposalSmokeTest();
+        });
+
+        await test.step('Submit case', async() => {
+            await Promise.all([
+                page.waitForResponse(response =>
+                    response.url().includes('submitApplication') &&
+                    response.request().method() === 'GET' &&
+                    response.status() === 200
+                ),
+                startApplication.submitCase()
+            ]);
+            await submitCase.submitCaseSmokeTest('2,515.00');
+        });
+
+        const accessibilityScanResults = await makeAxeBuilder()
+            // Automatically uses the shared AxeBuilder configuration,
+            // but supports additional test-specific configuration too
+            .analyze();
+
+        await testInfo.attach('accessibility-scan-results', {
+            body: JSON.stringify(accessibilityScanResults, null, 2),
+            contentType: 'application/json'
+        });
+    });
 
     test("Local Authority submit C110A application @smoke-test @accessibility", async ({
         signInPage,
