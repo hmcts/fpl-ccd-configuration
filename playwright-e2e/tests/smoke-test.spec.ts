e4382356974a9509cef890ec2f5b4bdaa9c9150f
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-09-29 11:31:06 +0100

    playwright config changes

--- playwright-e2e/tests/smoke-test.spec.ts
+++ playwright-e2e/tests/smoke-test.spec.ts
@@ -3,11 +3,10 @@ import { newSwanseaLocalAuthorityUserOne, privateSolicitorOrgUser } from "../set
 import { CreateCaseName } from "../utils/create-case-name";
 import { CaseFileView } from "../pages/case-file-view";
 
-test.slow();
 
 test.describe('Smoke Test @xbrowser', () => {
 
-    test('Local Authority C110A application submission', async ({
+    test('Local Authority C110A application submission @smoke-test @accessibility', async ({
                                                                     signInPage,
         page,
         createCase,
@@ -150,117 +149,6 @@ test.describe('Smoke Test @xbrowser', () => {
             contentType: 'application/json'
         });
     });
-
-    test("Local Authority submit C110A application @smoke-test @accessibility", async ({
-        signInPage,
-        createCase,
-        ordersAndDirectionSought,
-        startApplication,
-        hearingUrgency,
-        groundsForTheApplication,
-        applicantDetails,
-        allocationProposal,
-        addApplicationDocuments,
-        childDetails,
-        respondentDetails,
-        submitCase,
-        page,
-        makeAxeBuilder
-    }, testInfo) => {
-        // Marking this test slow to increase the time for 3 times of other test
-
-        // 1. Sign in as local-authority user
-        await signInPage.visit();
-        await signInPage.login(
-            newSwanseaLocalAuthorityUserOne.email,
-            newSwanseaLocalAuthorityUserOne.password,
-        );
-        //sign in page
-        await signInPage.isSignedIn();
-
-        // Add application details
-        // Start new case, get case id and assert case id is created
-        await createCase.caseName();
-        await createCase.createCase();
-        await createCase.submitCase(createCase.generatedCaseName);
-        await startApplication.tabNavigation('View application');
-        //this has to be refracted to new test as the test execution time exceed 8m
-        //await createCase.checkCaseIsCreated(createCase.generatedCaseName);
-
-        // Orders and directions sought
-        await startApplication.tabNavigation('Start application');
-        await ordersAndDirectionSought.ordersAndDirectionsNeeded();
-        await startApplication.addApplicationDetailsHeading.isVisible();
-        await startApplication.tabNavigation('View application');
-
-        // // Hearing urgency
-        await startApplication.tabNavigation('Start application');
-        await startApplication.hearingUrgency();
-        await expect(hearingUrgency.hearingUrgencyHeading).toBeVisible();
-        await hearingUrgency.hearingUrgencySmokeTest();
-        await startApplication.tabNavigation('View application');
-
-        // Grounds for the application
-        await startApplication.tabNavigation('Start application');
-        await startApplication.groundsForTheApplication();
-        await groundsForTheApplication.groundsForTheApplicationSmokeTest();
-        await startApplication.groundsForTheApplicationHasBeenUpdated();
-        await startApplication.tabNavigation('View application');
-
-        //Add application documents
-        await startApplication.tabNavigation('Start application');
-        await startApplication.addApplicationDetailsHeading.isVisible();
-        await startApplication.addApplicationDocuments();
-        await addApplicationDocuments.uploadDocumentSmokeTest();
-        await startApplication.addApplicationDocumentsInProgress();
-        await startApplication.tabNavigation('View application');
-
-        // Applicant Details
-        await startApplication.tabNavigation('Start application');
-        await startApplication.applicantDetails();
-        await applicantDetails.applicantDetailsNeeded();
-        await startApplication.applicantDetailsHasBeenUpdated();
-        await startApplication.tabNavigation('View application');
-
-        // Child details
-         await startApplication.tabNavigation('Start application');
-         await startApplication.childDetails();
-         await childDetails.addChildDetailsForC110AApplication();
-         await startApplication.childDetailsHasBeenUpdated();
-         await startApplication.tabNavigation('Confidential Information');
-         await childDetails.assertChildConfidentialInformation();
-         await startApplication.tabNavigation('View application');
-
-        // // Add respondents' details
-        await startApplication.tabNavigation('Start application');
-        await startApplication.respondentDetails();
-        await respondentDetails.respondentDetailsNeeded();
-        await startApplication.tabNavigation('View application');
-
-        // Allocation Proposal
-        await startApplication.tabNavigation('Start application');
-        await startApplication.allocationProposal();
-        await allocationProposal.allocationProposalSmokeTest();
-        await startApplication.allocationProposalHasBeenUpdated();
-        await startApplication.tabNavigation('View application');
-
-        // Submit the case
-        await startApplication.tabNavigation('Start application');
-        await startApplication.submitCase();
-        await submitCase.submitCaseSmokeTest('2,515.00');
-
-        const accessibilityScanResults = await makeAxeBuilder()
-            // Automatically uses the shared AxeBuilder configuration,
-            // but supports additional test-specific configuration too
-            .analyze();
-
-        await testInfo.attach('accessibility-scan-results', {
-            body: JSON.stringify(accessibilityScanResults, null, 2),
-            contentType: 'application/json'
-        });
-        //Assert is skipped due the EXUI issue with the CFV
-        // expect(accessibilityScanResults.violations).toEqual([]);
-    })
 
     test('Private solicitor applies C110a application', async ({
         signInPage,
