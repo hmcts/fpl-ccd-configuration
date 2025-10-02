b1d94fda35d4eaa703ff2292605385532a9e2bf6
prabha15 <prabha.muthu@justice.gov.uk>
2025-10-02 16:51:04 +0100

    FixFailingUITests

--- playwright-e2e/tests/manage-orders.spec.ts
+++ playwright-e2e/tests/manage-orders.spec.ts
@@ -664,7 +664,7 @@ test.describe('manage orders', () => {
 
     })
 
-    test.only('Judge uploads Special guardianship order (C43A) ', async ({ page, signInPage, orders }) => {
+    test('Judge uploads Special guardianship order (C43A) ', async ({ page, signInPage, orders }) => {
         caseName = 'Special guardianship order (C43A) ' + dateTime.slice(0, 10);
         await updateCase(caseName, caseNumber, caseWithOrderData);
         await signInPage.visit();
