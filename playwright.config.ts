b8833ccb91419405181b0c53c39025968c1cf729
AustenStevensMOJ <austen.stevens@justice.gov.uk>
2025-11-07 16:14:15 +0000

    Allure incorporation

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,11 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 4 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [[process.env.CI ? 'html' : 'list'],
-        ['html', {outputFolder: '../test-results/functionalTest'}]],
+    reporter: [
+        [process.env.CI ? 'html' : 'list'],
+        ['html', {outputFolder: '../test-results/functionalTest'}],
+        ['allure-playwright', { outputFolder: '../allure-results'}]
+    ],
 
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
