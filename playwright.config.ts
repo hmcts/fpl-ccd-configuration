0d211268b7819e29d5ac806a1c75cb75d0d3f80b
prabha15 <prabha.muthu@justice.gov.uk>
2025-11-10 13:32:35 +0000

    reduceUiTestWorkerForPreview

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,6 +77,7 @@ export default defineConfig({
         {
             name: "preview",
             use: { ...devices['Desktop Edge'], channel: 'msedge' },
+            workers: process.env.CI ? 2 : undefined,
             retries: 2,
             timeout: 5 * 60 * 1000,
             expect: {timeout: 1 * 60 * 1000},
