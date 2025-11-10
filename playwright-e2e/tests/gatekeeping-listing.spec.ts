3f757515192b7d217c74d5382afcdaadc7afabdc
prabha15 <prabha.muthu@justice.gov.uk>
2025-11-10 11:44:36 +0000

    merge latest code from master

--- playwright-e2e/tests/gatekeeping-listing.spec.ts
+++ playwright-e2e/tests/gatekeeping-listing.spec.ts
@@ -13,58 +13,58 @@ import { testConfig } from "../settings/test-config";
 import { setHighCourt } from '../utils/update-case-details';
 
 test.describe('Gatekeeping Listing', () => {
-    const dateTime = new Date().toISOString();
-    let caseNumber: string;
-    let caseName: string;
-    test.beforeEach(async () => {
-        caseNumber = await createCase('e2e case', newSwanseaLocalAuthorityUserOne);
+  const dateTime = new Date().toISOString();
+  let caseNumber: string;
+  let caseName: string;
+  test.beforeEach(async () => {
+    caseNumber = await createCase('e2e case', newSwanseaLocalAuthorityUserOne);
+  });
+
+  test('Review Standard Direction Order High Court WA Task @xbrowser',
+    async ({ page, signInPage, gateKeepingListing,
+      caseFileView }) => {
+      caseName = 'Review Standard Direction Order High Court WA Task ' + dateTime.slice(0, 10);
+      setHighCourt(caseData);
+      await updateCase(caseName, caseNumber, caseData);
+      await signInPage.visit();
+      await signInPage.login(judgeLondonUser.email, judgeLondonUser.password)
+      await signInPage.navigateTOCaseDetails(caseNumber);
+
+      await gateKeepingListing.gotoNextStep('Judicial Gatekeeping');
+      await gateKeepingListing.completeJudicialGatekeepingWithUploadedOrder();
+      await gateKeepingListing.gotoNextStep('List Gatekeeping Hearing');
+      await gateKeepingListing.addHighCourtJudgeAndCompleteGatekeepingListing();
+
+       //assert
+        await gateKeepingListing.tabNavigation('People in the case');
+        await expect.soft(gateKeepingListing.page.getByText('Fee paid judge')).toBeVisible();
+        await expect.soft(gateKeepingListing.page.getByText('Recorder')).toBeVisible();
+        await expect.soft(gateKeepingListing.page.getByText('Ramirez KC',{ exact: true })).toBeVisible();
+        await gateKeepingListing.tabNavigation('Hearings');
+        await expect.soft(gateKeepingListing.page.getByText('Recorder Ramirez KC',{exact:true})).toHaveCount(2);
+        //Check CFV
+      await caseFileView.goToCFVTab();
+      await caseFileView.openFolder('Orders');
+      await expect(page.getByRole('tree')).toContainText('testWordDoc.pdf');
+      await gateKeepingListing.clickSignOut();
+
+      if (testConfig.waEnabled) {
+        //Test WA Task exists
+        await signInPage.visit();
+        await signInPage.login(HighCourtAdminUser.email, HighCourtAdminUser.password);
+        await signInPage.navigateTOCaseDetails(caseNumber);
+        await gateKeepingListing.tabNavigation('Tasks');
+        await gateKeepingListing.waitForTask('Review Standard Direction Order (High Court)');
+
+        // Assign and complete the task
+        await page.getByText('Assign to me').click();
+        await page.getByText('Mark as done').click();
+        await page.getByRole('button', { name: "Mark as done" }).click();
+
+        // Should be no more tasks on the page
+        await expect(page.getByText('Review Standard Direction Order (High Court)')).toHaveCount(0);
+      }
     });
-
-    test('Review Standard Direction Order High Court WA Task @xbrowser',
-        async ({ page, signInPage, gateKeepingListing,
-                   caseFileView }) => {
-            caseName = 'Review Standard Direction Order High Court WA Task ' + dateTime.slice(0, 10);
-            setHighCourt(caseData);
-            await updateCase(caseName, caseNumber, caseData);
-            await signInPage.visit();
-            await signInPage.login(judgeLondonUser.email, judgeLondonUser.password)
-            await signInPage.navigateTOCaseDetails(caseNumber);
-
-            await gateKeepingListing.gotoNextStep('Judicial Gatekeeping');
-            await gateKeepingListing.completeJudicialGatekeepingWithUploadedOrder();
-            await gateKeepingListing.gotoNextStep('List Gatekeeping Hearing');
-            await gateKeepingListing.addHighCourtJudgeAndCompleteGatekeepingListing();
-
-            //assert
-            await gateKeepingListing.tabNavigation('People in the case');
-            await expect.soft(gateKeepingListing.page.getByText('Fee paid judge')).toBeVisible();
-            await expect.soft(gateKeepingListing.page.getByText('Recorder')).toBeVisible();
-            await expect.soft(gateKeepingListing.page.getByText('Ramirez KC',{ exact: true })).toBeVisible();
-            await gateKeepingListing.tabNavigation('Hearings');
-            await expect.soft(gateKeepingListing.page.getByText('Recorder Ramirez KC',{exact:true})).toHaveCount(2);
-            //Check CFV
-            await caseFileView.goToCFVTab();
-            await caseFileView.openFolder('Orders');
-            await expect(page.getByRole('tree')).toContainText('testWordDoc.pdf');
-            await gateKeepingListing.clickSignOut();
-
-            if (testConfig.waEnabled) {
-                //Test WA Task exists
-                await signInPage.visit();
-                await signInPage.login(HighCourtAdminUser.email, HighCourtAdminUser.password);
-                await signInPage.navigateTOCaseDetails(caseNumber);
-                await gateKeepingListing.tabNavigation('Tasks');
-                await gateKeepingListing.waitForTask('Review Standard Direction Order (High Court)');
-
-                // Assign and complete the task
-                await page.getByText('Assign to me').click();
-                await page.getByText('Mark as done').click();
-                await page.getByRole('button', { name: "Mark as done" }).click();
-
-                // Should be no more tasks on the page
-                await expect(page.getByText('Review Standard Direction Order (High Court)')).toHaveCount(0);
-            }
-        });
     test('List Urgent Direction Order - CTS User @xbrowser',
         async ({page, signInPage, gateKeepingListing}) => {
 
@@ -88,4 +88,4 @@ test.describe('Gatekeeping Listing', () => {
 
         })
 
-});
+  });
