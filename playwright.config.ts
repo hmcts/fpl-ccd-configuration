9a543ae3aee2e989a14e624785a83eb1d8590404
prabha15 <prabha.muthu@justice.gov.uk>
2025-11-18 17:40:52 +0000

    RunSmokeTestOnly

--- playwright.config.ts
+++ playwright.config.ts
@@ -48,6 +48,7 @@ export default defineConfig({
         {
             ...ProjectsConfig.edge,
             teardown: process.env.CI ? 'AMRoleCleanup' : undefined,
+            grep: /@smoke-test/
 
         },
         {
