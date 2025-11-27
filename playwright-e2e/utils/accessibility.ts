8557d4afb3167b89ea36c238459429c5561f21e2
prabha15 <prabha.muthu@justice.gov.uk>
2025-11-27 10:38:11 +0000

    GenerateAXEreportonlyforfailedChecks

--- playwright-e2e/utils/accessibility.ts
+++ playwright-e2e/utils/accessibility.ts
@@ -1,25 +1,25 @@
-
-import { TestInfo, Page } from "@playwright/test";
-import { AxeResults } from "axe-core";
+import {TestInfo, Page} from "@playwright/test";
+import {AxeResults} from "axe-core";
 import {createHtmlReport} from "axe-html-reporter"
 
 export async function a11yHTMLReport(axeScanResult: AxeResults, testInfo: TestInfo, label: string) {
 
+
+    const violationCount = axeScanResult.violations.length;
+    if (violationCount > 0) {
         createHtmlReport({
             results: axeScanResult,
             options: {
-                outputDir: '../test-results/functionalTest',
+                outputDir: '../test-results/functionalTest/axe-reports',
                 reportFileName: `axe-report-${label.replace(/\s+/g, '_')}.html`,
             },
         });
-    const violationCount = axeScanResult.violations.length;
-    if (violationCount > 0) {
-        console.error(`[A11Y WARNING]: Found ${violationCount} accessibility violations.`);
+        await testInfo.attach(label, {
+            path: `../test-results/functionalTest/axe-reports/axe-report-${label.replace(/\s+/g, '_')}.html`,
+        });
+
 
     }
-    await testInfo.attach(label, {
-        path: `../test-results/functionalTest/axe-report-${label.replace(/\s+/g, '_')}.html`,
-    });
 
 }
 
