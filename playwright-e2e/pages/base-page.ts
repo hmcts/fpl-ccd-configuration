b6aeb2cafaccd4701c69b181a367772a475d8d1b
prabha15 <prabha.muthu@justice.gov.uk>
2026-01-19 16:15:56 +0000

    Enhance Notice of Change tests and update client details validation

--- playwright-e2e/pages/base-page.ts
+++ playwright-e2e/pages/base-page.ts
@@ -166,12 +166,11 @@ export class BasePage {
         let hypenatedCaseNumber: string;
         hypenatedCaseNumber = caseNumber.slice(0, 4) + "-" + caseNumber.slice(4, 8) + "-" + caseNumber.slice(8, 12) + "-" + caseNumber.slice(12, 16);
         return hypenatedCaseNumber
+    }
     async enterDate(date: Date){
         await this.dateOfHearing.getByText('Day').fill(date.getDay().toString());
         await this.dateOfHearing.getByText('Month').fill(date.getMonth().toString());
         await this.dateOfHearing.getByText('Year').fill(date.getFullYear().toString())
-
-
     }
 
     async getCellValueInTable(tableName: string, cellHeading: string): Promise<string> {
