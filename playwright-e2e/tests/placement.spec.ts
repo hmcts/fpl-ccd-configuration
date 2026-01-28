3224ac4bb8ee62fd3a7de9dd19117b07457a5c35
prabha15 <prabha.muthu@justice.gov.uk>
2026-01-28 16:49:28 +0000

    updatePlacementapplicationforDynamic PBA number

--- playwright-e2e/tests/placement.spec.ts
+++ playwright-e2e/tests/placement.spec.ts
@@ -26,7 +26,9 @@ test.describe('Placement', () => {
       await signInPage.navigateToCaseDetails(caseNumber);
 
       await placement.gotoNextStep('Placement');
-      await placement.submitPlacementOrder();
+      await placement.PlacementApplicationDetails();
+        await placement.payForApplicationCTSC();
+        await placement.checkYourAnsAndSubmit();
 
       //Check CFV
       await caseFileView.goToCFVTab();
@@ -56,21 +58,40 @@ test.describe('Placement', () => {
       }
 
     });
+    test('LA upload placement application @xbrowser',
+        async ({ page, signInPage, placement,
+                   caseFileView,envDataConfig }) => {
+            caseName = 'Placement Application High Court WA Task ' + dateTime.slice(0, 10);
+            setHighCourt(caseData);
+            expect(await updateCase(caseName, caseNumber, caseData)).toBeTruthy();
+            await signInPage.visit();
+            await signInPage.login(newSwanseaLocalAuthorityUserOne.email, newSwanseaLocalAuthorityUserOne.password)
+            await signInPage.navigateToCaseDetails(caseNumber);
 
-  test('CTSC actions notice of placement @xbrowser',
-    async ({ page, signInPage, placement,
-      caseFileView }) => {
-      caseName = 'CTSC actions notice of placement' + dateTime.slice(0, 10);
-      setHighCourt(caseData);
-      expect(await updateCase(caseName, caseNumber, caseData)).toBeTruthy();
-      await signInPage.visit();
-      await signInPage.login(CTSCUser.email, CTSCUser.password)
-      await signInPage.navigateToCaseDetails(caseNumber);
+            await placement.gotoNextStep('Placement');
+            await placement.PlacementApplicationDetails();
+            await placement.payForApplicationLA(envDataConfig.swanseaOrgPBA);
+            await placement.checkYourAnsAndSubmit();
 
-      await placement.gotoNextStep('Placement');
-      await placement.noticeOfPlacement();
+            //Check CFV
+            await caseFileView.goToCFVTab();
+            await caseFileView.openFolder('Placement applications and responses');
+            await expect(page.getByRole('tree')).toContainText('testPdf.pdf');
+            await expect(page.getByRole('tree')).toContainText('testPdf2.pdf');
+            await expect(page.getByRole('tree')).toContainText('testPdf3.pdf');
+            await caseFileView.openFolder('Confidential');
+            await expect(page.getByRole('tree')).toContainText('testPdf4.pdf');
 
-      await placement.tabNavigation('Placement');
-      await expect(page.getByText(' Notice of hearing for placement ')).toBeVisible();
-    })
+            await placement.tabNavigation('Placement')
+            await expect(placement.page.getByText('Timothy Jones')).toBeVisible();
+            await expect(placement.page.getByRole('term').filter({ hasText: 'Child' })).toBeVisible();
+
+            await placement.clickSignOut();
+            await signInPage.login(CTSCUser.email, CTSCUser.password);
+            await signInPage.navigateToCaseDetails(caseNumber);
+            await placement.gotoNextStep('Notice of Placement - Hearing');
+            await placement.noticeOfPlacement();
+            await placement.tabNavigation('Placement');
+            await expect(page.getByText(' Notice of hearing for placement ')).toBeVisible();
+        });
 });
