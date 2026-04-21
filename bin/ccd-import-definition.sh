2bc5d54a7734350d28215c5fa196f6390ac1363a
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-04-21 09:29:02 +0100

    typo

--- bin/ccd-import-definition.sh
+++ bin/ccd-import-definition.sh
@@ -8,7 +8,7 @@ filename=$(basename ${filepath})
 uploadFilename="$(date +"%Y%m%d-%H%M%S")-${filename}"
 
 echo "Getting userToken and serviceToken"
-userToken=$(${dir}/utils/idam-access-user-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12})
+userToken=$(${dir}/utils/idam-access-token.sh ${CCD_CONFIGURER_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${CCD_CONFIGURER_IMPORTER_PASSWORD:-Password12})
 echo ">>> Got userToken"
 serviceToken=$(${dir}/utils/idam-lease-service-token.sh ccd_gw $(docker run --rm hmctsprod.azurecr.io/imported/toolbelt/oathtool:latest --totp -b ${CCD_API_GATEWAY_S2S_SECRET:-AAAAAAAAAAAAAAAC}))
 echo ">>> Got serviceToken"
