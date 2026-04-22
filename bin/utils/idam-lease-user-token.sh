0c51fe7c6cd2370aab0a32ca42ee60896f4affe5
Chak Shing Lo <95437003+chak-shing-lo-justice@users.noreply.github.com>
2026-04-22 09:34:49 +0100

    update script
    sanitise add -eu back
    
    sanitise again
    
    sanitise

--- bin/utils/idam-lease-user-token.sh
+++ bin/utils/idam-lease-user-token.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-username=$1
-password=$2
+set -eu
+
+username=${1}
+password=${2}
 clientId=ccd_gateway
 clientSecret=${CCD_API_GATEWAY_IDAM_CLIENT_SECRET:-ccd_gateway_secret}
-redirectUri=http://localhost:3451/oauth2redirect
+redirectUri=${CCD_IDAM_REDIRECT_URL:-http://localhost:3451/oauth2redirect}
 
-curl --silent --location "${IDAM_API_BASE_URL}/o/token" \
+curl --silent --location --show-error "${IDAM_API_BASE_URL}/o/token" \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --data-urlencode "client_id=$clientId" \
 --data-urlencode "client_secret=$clientSecret" \
