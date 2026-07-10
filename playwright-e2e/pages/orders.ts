348f30b8a74385386537790d0365b0c4bebdc4ce
prabha15 <prabha.muthu@justice.gov.uk>
2026-07-10 10:31:04 +0100

    fix empty space

--- playwright-e2e/pages/orders.ts
+++ playwright-e2e/pages/orders.ts
@@ -260,7 +260,6 @@ export class Orders extends BasePage {
             await this.powerOfExclusionStart.getByLabel('Month').fill('3');
             await this.powerOfExclusionStart.getByLabel('Year').fill('2024');
             await this.powerOfExclusionStart.getByLabel('Day').fill('12');
-
         }
         await this.page.getByRole('group', { name: 'Include: "Any person who can produce the children to the applicant must do so"' }).getByLabel('Yes').click();
         await this.page.getByLabel('Add description of children (').fill('Children description');
