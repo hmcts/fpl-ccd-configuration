8bbe5193cbff91fd9ce9d1d0be9a3ad95f4f13c7
JustGC-01 <george.cope@justice.gov.uk>
2026-09-07 11:30:28 +0100

    DFPL-3273-2: Re-adding line to grant Jenkins preview access in AAT env

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,6 +70,8 @@ module "key-vault" {
   common_tags             = var.common_tags
   create_managed_identity = true
   jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
+
+  grant_preview_jenkins_access = var.env == "aat"
 }
 
 data "azurerm_user_assigned_identity" "jenkins" {
