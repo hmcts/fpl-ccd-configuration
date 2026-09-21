a1e6a4edc9d2c78ebe21ba220740d1e7ab97ef92
JustGC-01 <george.cope@justice.gov.uk>
2026-09-21 14:55:34 +0100

    DFPL-3165: Create new app insights in UK South

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -41,6 +41,18 @@ module "application_insights" {
   common_tags         = var.common_tags
 }
 
+module "application_insights_uksouth" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}-${var.component}-appinsights-uksouth"
+
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  common_tags         = var.common_tags
+}
+
 moved {
   from = azurerm_application_insights.appinsights
   to   = module.application_insights.azurerm_application_insights.this
